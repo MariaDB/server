813ffe0e0deb3982adfa04872e2b5489b2928903
Igor Babaev <igor@askmonty.org>
2012-08-20 12:05:37 -0700

    Fixed bug mdev-463. RBR should be turned off when an ANALYZE TABLE command is executed.

--- sql/sql_admin.cc
+++ sql/sql_admin.cc
@@ -322,6 +322,8 @@ static bool mysql_admin_table(THD* thd, TABLE_LIST* tables,
   int result_code;
   int compl_result_code;
   bool need_repair_or_alter= 0;
+  bool save_binlog_row_based= 0;
+
   DBUG_ENTER("mysql_admin_table");
   DBUG_PRINT("enter", ("extra_open_options: %u", extra_open_options));
 
@@ -415,6 +417,11 @@ static bool mysql_admin_table(THD* thd, TABLE_LIST* tables,
         */
 
         open_error= open_and_lock_tables(thd, table, TRUE, 0);
+        if (lex->sql_command == SQLCOM_ANALYZE)
+	{
+          if ((save_binlog_row_based= thd->is_current_stmt_binlog_format_row()))
+            thd->clear_current_stmt_binlog_format_row();
+        }
       }
       thd->prepare_derived_at_open= FALSE;
 
@@ -954,6 +961,8 @@ send_result_message:
     trans_commit_stmt(thd);
     trans_commit_implicit(thd);
     close_thread_tables(thd);
+    if (save_binlog_row_based)
+      thd->set_current_stmt_binlog_format_row();
     thd->mdl_context.release_transactional_locks();
 
     /*
@@ -986,6 +995,8 @@ err:
   trans_rollback_stmt(thd);
   trans_rollback(thd);
   close_thread_tables(thd);			// Shouldn't be needed
+  if (save_binlog_row_based)
+    thd->set_current_stmt_binlog_format_row();
   thd->mdl_context.release_transactional_locks();
   if (table)
     table->table=0;
