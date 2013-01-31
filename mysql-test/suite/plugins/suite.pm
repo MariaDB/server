792d6b348c52986f43b3f9b868b64ef02fbe836d
Sergei Golubchik <sergii@pisem.net>
2013-01-31 13:19:53 +0100

    skip cassandra.test unless cassandra is running

--- mysql-test/suite/plugins/suite.pm
+++ mysql-test/suite/plugins/suite.pm
@@ -2,10 +2,18 @@ package My::Suite::Plugins;
 
 @ISA = qw(My::Suite);
 
+sub cassandra_running() { 
+  return 0 if IS_WINDOW; 
+  system 'echo show version | cqlsh -3 2>/dev/null >/dev/null'; 
+  return $? == 0; 
+} 
+
 sub skip_combinations {
   my %skip;
   $skip{'t/pam.test'} = 'No pam setup for mtr'
              unless -e '/etc/pam.d/mariadb_mtr';
+  $skip{'t/cassandra.test'} = 'Cassandra is not running'
+             unless cassandra_running();
   %skip;
 }
 
