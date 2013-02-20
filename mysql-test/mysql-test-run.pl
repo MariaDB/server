1e13640f4ddd8909ea7061155c2e62ae5bca88cf
Alexander Barkov <bar@mnogosearch.org>
2013-02-20 16:34:07 +0400

    Fixing problems with running tests caused by the fact that "mysqld --connect" conflicts with "mysqld --connect-timeout".
    (unfortunate design decision in how mysqld handles command line options).
    
    modified:
      mysql-test/mysql-test-run.pl
      mysql-test/suite/connect/suite.opt

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -1925,7 +1925,7 @@ sub collect_mysqld_features {
       # here we want to detect all not mandatory plugins
       # they are listed in the --help output as
       #  --archive[=name]    Enable or disable ARCHIVE plugin. Possible values are ON, OFF, FORCE (don't start if the plugin fails to load).
-      push @optional_plugins, $1
+      push @optional_plugins, "plugin-$1"
         if /^  --([-a-z0-9]+)\[=name\] +Enable or disable \w+ plugin. Possible values are ON, OFF, FORCE/;
       next;
     }
