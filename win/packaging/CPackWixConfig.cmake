0b034c1b2c619e3de6c8cbaddaede75578b5ef1a
Vladislav Vaintroub <wlad@montyprogram.com>
2013-04-19 18:45:54 +0200

    Fix to handle CMake component names with dash in the name, when producing MSI package. Rename connect_engine component back to connect-engine.

--- win/packaging/CPackWixConfig.cmake
+++ win/packaging/CPackWixConfig.cmake
@@ -9,7 +9,7 @@ IF(ESSENTIALS)
  ENDIF()
 ELSE()
   SET(CPACK_COMPONENTS_USED 
-    "Server;Client;Development;SharedLibraries;Embedded;Documentation;IniFiles;Readme;Debuginfo;Common;connect_engine")
+    "Server;Client;Development;SharedLibraries;Embedded;Documentation;IniFiles;Readme;Debuginfo;Common;connect-engine")
 ENDIF()
 
 SET( WIX_FEATURE_MySQLServer_EXTRA_FEATURES "DBInstance;SharedClientServerComponents")
@@ -63,10 +63,10 @@ SET(CPACK_COMPONENT_GROUP_MYSQLSERVER_DESCRIPTION "Install server")
  SET(CPACK_COMPONENT_DATAFILES_HIDDEN 1)
  
  #Subfeature "Connect Engine" 
- SET(CPACK_COMPONENT_CONNECT_ENGINE_GROUP "MySQLServer")
- SET(CPACK_COMPONENT_CONNECT_ENGINE_DISPLAY_NAME "Server data files")
- SET(CPACK_COMPONENT_CONNECT_ENGINE_DESCRIPTION "Server data files" )
- SET(CPACK_COMPONENT_CONNECT_ENGINE_HIDDEN 1)
+ SET(CPACK_COMPONENT_CONNECT-ENGINE_GROUP "MySQLServer")
+ SET(CPACK_COMPONENT_CONNECT-ENGINE_DISPLAY_NAME "Server data files")
+ SET(CPACK_COMPONENT_CONNECT-ENGINE_DESCRIPTION "Server data files" )
+ SET(CPACK_COMPONENT_CONNECT-ENGINE_HIDDEN 1)
  
  
 #Feature "Devel"
