8776be3c42f109a443f90561e5adbc6ce4090323
Sergey Petrunya <psergey@askmonty.org>
2013-07-04 16:36:43 +0400

    Fix typo in scripts/mysql_system_tables_fix.sql

--- scripts/mysql_system_tables_fix.sql
+++ scripts/mysql_system_tables_fix.sql
@@ -630,7 +630,7 @@ UPDATE user SET Create_tablespace_priv = Super_priv WHERE @hadCreateTablespacePr
 
 ALTER TABLE user ADD plugin char(64) DEFAULT '',  ADD authentication_string TEXT;
 ALTER TABLE user ADD password_expired ENUM('N', 'Y') COLLATE utf8_general_ci DEFAULT 'N' NOT NULL;
-ALTER TABLE user MODIFY plugin char(64) CHARACTER SET latin1 DEFAULT '' NOT NULL, user MODIFY authentication_string TEXT NOT NULL;
+ALTER TABLE user MODIFY plugin char(64) CHARACTER SET latin1 DEFAULT '' NOT NULL, MODIFY authentication_string TEXT NOT NULL;
 
 -- Need to pre-fill mysql.proxies_priv with access for root even when upgrading from
 -- older versions
