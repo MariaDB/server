3be10c768bacaa40f2d4bb5e83bf498ce9f9bc9b
Sergei Golubchik <sergii@pisem.net>
2013-07-13 22:29:17 +0200

    fix for maria.maria test

--- storage/maria/ha_maria.cc
+++ storage/maria/ha_maria.cc
@@ -3210,11 +3210,16 @@ bool ha_maria::check_if_incompatible_data(HA_CREATE_INFO *create_info,
 {
   DBUG_ENTER("check_if_incompatible_data");
   uint options= table->s->db_options_in_use;
+  enum ha_choice page_checksum= table->s->page_checksum;
+
+  if (page_checksum == HA_CHOICE_UNDEF)
+    page_checksum= file->s->options & HA_OPTION_PAGE_CHECKSUM ? HA_CHOICE_YES
+                                                              : HA_CHOICE_NO;
 
   if (create_info->auto_increment_value != stats.auto_increment_value ||
       create_info->data_file_name != data_file_name ||
       create_info->index_file_name != index_file_name ||
-      create_info->page_checksum != table->s->page_checksum ||
+      create_info->page_checksum != page_checksum ||
       create_info->transactional != table->s->transactional ||
       (maria_row_type(create_info) != data_file_type &&
        create_info->row_type != ROW_TYPE_DEFAULT) ||
