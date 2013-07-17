8a23ae088dc38f591efeab9eccdef5eb9094add9
unknown <sanja@montyprogram.com>
2013-07-17 17:46:16 +0300

    MDEV-4647: Valgrind warnings (Conditional jump or move depends on uninitialised value) in Item_equal::fix_fields Fix to calm down valgrind.

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -5953,8 +5953,8 @@ bool Item_equal::fix_fields(THD *thd, Item **ref)
   DBUG_ASSERT(fixed == 0);
   Item_equal_fields_iterator it(*this);
   Item *item;
-  Field *first_equal_field;
-  Field *last_equal_field;
+  Field *first_equal_field= NULL;
+  Field *last_equal_field= NULL;
   Field *prev_equal_field= NULL;
   not_null_tables_cache= used_tables_cache= 0;
   const_item_cache= 0;
