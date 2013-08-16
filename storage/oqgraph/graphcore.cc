c18c903c114d29b699a97b316158e16d876f44ac
Andrew McDonnell <bugs@andrewmcdonnell.net>
2013-08-16 20:05:09 +0930

    Revert half the fix for 1133093 - superfluous

--- storage/oqgraph/graphcore.cc
+++ storage/oqgraph/graphcore.cc
@@ -947,7 +947,7 @@ namespace open_query
     if (cursor)
       cursor->current(ref);
     else
-      new (ref_ptr) reference(); // avoid assignment operator because the intrusive_ptr swaps for unknown reasons, which means if ref is uninitialised it segfaults
+      ref = reference(); // avoid assignment operator because the intrusive_ptr swaps for unknown reasons, which means if ref is uninitialised it segfaults
   }
 
   int oqgraph::random(bool scan) throw()
