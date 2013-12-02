9379edb63d85bcb256a4e145827db9c4c76f15c4
Jan Lindström <jplindst@mariadb.org>
2013-12-02 08:58:06 +0200

    MDEV-4227: Galera server should stop crashing on setting binlog_format STATEMENT
    Analysis: Currently you can set binlog_format to STATEMENT or MIXED 
    even when they are not really supported by galera.
    
    Fix: Produce an error message if binlog_format is set to STATEMENT
    or MIXED and wsrep_on = ON. Added a test case for this.

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -308,6 +308,26 @@ static bool binlog_format_check(sys_var *self, THD *thd, set_var *var)
          ER_INSIDE_TRANSACTION_PREVENTS_SWITCH_BINLOG_FORMAT))
     return true;
 
+#ifdef WITH_WSREP
+  /* MariaDB Galera does not support STATEMENT or MIXED binlog
+  format currently */
+  if (WSREP(thd) &&
+     (var->save_result.ulonglong_value == BINLOG_FORMAT_STMT ||
+      var->save_result.ulonglong_value == BINLOG_FORMAT_MIXED))
+  {
+    WSREP_DEBUG("MariaDB Galera does not support binlog format : %s",
+                var->save_result.ulonglong_value == BINLOG_FORMAT_STMT ?
+                "STATEMENT" : "MIXED");
+    /* Push also warning, because error message is general */
+     push_warning_printf(thd, MYSQL_ERROR::WARN_LEVEL_WARN,
+                        ER_UNKNOWN_ERROR,
+                        "MariaDB Galera does not support binlog format: %s",
+                        var->save_result.ulonglong_value == BINLOG_FORMAT_STMT ?
+                        "STATEMENT" : "MIXED");
+    return true;
+  }
+#endif
+
   return false;
 }
 
