258bf32d3c8d7462d54c3acce724308faf270b94
Sergey Vojtovich <svoj@mariadb.org>
2013-12-13 12:42:45 +0400

    MDEV-4748 - metadata_lock_info plugin
    - include metadata_lock_info plugin into debian packages
    - ignore metadata_lock_info plugin in mysqld--help test
    - let test suite run with built-in metadata_lock_info plugin

--- plugin/metadata_lock_info/mysql-test/metadata_lock_info/suite.pm
+++ plugin/metadata_lock_info/mysql-test/metadata_lock_info/suite.pm
@@ -2,7 +2,8 @@ package My::Suite::Metadata_lock_info;
 
 @ISA = qw(My::Suite);
 
-return "No Metadata_lock_info plugin" unless $ENV{METADATA_LOCK_INFO_SO};
+return "No Metadata_lock_info plugin" unless $ENV{METADATA_LOCK_INFO_SO} or
+  $::mysqld_variables{'metadata-lock-info'} eq "ON";;
 
 bless { };
 
