2b6c6b44c0cc9136b256ce7ffbe7af69ceecf311
Sergei Golubchik <sergii@pisem.net>
2013-12-16 13:32:03 +0100

    move oqgraph and sphinx suites into storage/*/mysql-test/

--- storage/oqgraph/mysql-test/oqgraph/suite.pm
+++ storage/oqgraph/mysql-test/oqgraph/suite.pm
@@ -5,5 +5,7 @@ package My::Suite::OQGraph;
 return "No OQGraph" unless $ENV{HA_OQGRAPH_SO} or
                            $::mysqld_variables{'oqgraph'} eq "ON";
 
+sub is_default { 1 }
+
 bless { };
 
