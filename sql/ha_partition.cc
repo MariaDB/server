afce0f90c98d2542b60e009a09494d2bcb17ab75
Sergei Golubchik <sergii@pisem.net>
2014-02-03 15:26:58 +0100

    mysql 5.6 partitioning bugfix: doubly-reported error

--- sql/ha_partition.cc
+++ sql/ha_partition.cc
@@ -7887,7 +7887,10 @@ void ha_partition::print_error(int error, myf errflag)
 
   if ((error == HA_ERR_NO_PARTITION_FOUND) &&
       ! (thd->lex->alter_info.flags & Alter_info::ALTER_TRUNCATE_PARTITION))
+  {
     m_part_info->print_no_partition_found(table, errflag);
+    DBUG_VOID_RETURN;
+  }
   else if (error == HA_ERR_ROW_IN_WRONG_PARTITION)
   {
     /* Should only happen on DELETE or UPDATE! */
