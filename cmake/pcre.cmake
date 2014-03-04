a9f3d72c214d24ca4dd573fcb4356b2c812b2e33
Sergei Golubchik <sergii@pisem.net>
2014-03-04 01:22:53 +0100

    MDEV-5620 CMake option to compile against an external PCRE library

---
+++ cmake/pcre.cmake
@@ -0,0 +1,16 @@
+SET(WITH_PCRE "auto" CACHE STRING
+   "Which pcre to use (possible values are 'bundled', 'system', or 'auto')")
+
+MACRO (CHECK_PCRE)
+  IF(WITH_PCRE STREQUAL "system" OR WITH_PCRE STREQUAL "auto")
+    CHECK_LIBRARY_EXISTS(pcre pcre_stack_guard "" HAVE_PCRE)
+  ENDIF()
+  IF(NOT HAVE_PCRE)
+    IF (WITH_PCRE STREQUAL "system")
+      MESSAGE(FATAL_ERROR "system pcre is not found or unusable")
+    ENDIF()
+    SET(PCRE_INCLUDES ${CMAKE_BINARY_DIR}/pcre ${CMAKE_SOURCE_DIR}/pcre)
+    ADD_SUBDIRECTORY(pcre)
+  ENDIF()
+ENDMACRO()
+
