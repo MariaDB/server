a498f4e4b2c4e1a01590fa671ac68efbf3b4a127
Michael Widenius <monty@mariadb.org>
2014-03-10 14:08:12 +0200

    Fixed MDEV-5780 "create-big fails in 10.0"
    The issue was that create...trigger part of the test suite used a debug_sync point that before was never triggered (in other words, wrong meaningless test).
    With the new create ... replace code the debug sync point is triggered and the test case could not handled that.
    
    I fixed this by adding a wait and go for the debug syncpoint in the test.
    
    Removed some compiler warnings from mysql_cond_timedwait
    
    
    include/mysql/psi/mysql_thread.h:
      Removed compiler warnings
    mysql-test/r/create-big.result:
      New test result
    mysql-test/t/create-big.test:
      Fixed test case as create_table_select_before_check_if_exists was not before triggered by the code.

--- include/mysql/psi/mysql_thread.h
+++ include/mysql/psi/mysql_thread.h
@@ -518,7 +518,7 @@ typedef struct st_mysql_cond mysql_cond_t;
   @c mysql_cond_timedwait is a drop-in replacement
   for @c pthread_cond_timedwait.
 */
-#if defined(HAVE_PSI_INTERFACE) || defined(SAFE_MUTEX)
+#ifdef HAVE_PSI_COND_INTERFACE
   #define mysql_cond_timedwait(C, M, W) \
     inline_mysql_cond_timedwait(C, M, W, __FILE__, __LINE__)
 #else
@@ -1171,7 +1171,7 @@ static inline int inline_mysql_cond_timedwait(
   mysql_cond_t *that,
   mysql_mutex_t *mutex,
   const struct timespec *abstime
-#if defined(HAVE_PSI_INTERFACE) || defined(SAFE_MUTEX)
+#ifdef HAVE_PSI_COND_INTERFACE
   , const char *src_file, uint src_line
 #endif
   )
