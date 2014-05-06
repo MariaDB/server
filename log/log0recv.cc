6cb3146af896eb7d27aed6815428008f105e8ae8
Sergei Golubchik <sergii@pisem.net>
2014-05-06 21:18:00 +0200

    percona-server-5.6.17-65.0

--- log/log0recv.cc
+++ log/log0recv.cc
@@ -3035,8 +3035,6 @@ recv_init_crash_recovery(void)
 	ib_logf(IB_LOG_LEVEL_INFO,
 		"Reading tablespace information from the .ibd files...");
 
-	buf_dblwr_init_or_load_pages(true);
-
 	fil_load_single_table_tablespaces();
 
 	/* If we are using the doublewrite method, we will
@@ -3094,6 +3092,7 @@ recv_recovery_from_checkpoint_start_func(
 	byte*		log_hdr_buf;
 	byte		log_hdr_buf_base[LOG_FILE_HDR_SIZE + OS_FILE_LOG_BLOCK_SIZE];
 	dberr_t		err;
+	ut_when_dtor<recv_dblwr_t> tmp(recv_sys->dblwr);
 
 	log_hdr_buf = static_cast<byte *>
 		(ut_align(log_hdr_buf_base, OS_FILE_LOG_BLOCK_SIZE));
@@ -3110,11 +3109,6 @@ recv_recovery_from_checkpoint_start_func(
 /** Recover up to this log sequence number */
 # define LIMIT_LSN		LSN_MAX
 #endif /* UNIV_LOG_ARCHIVE */
-
-	if (TYPE_CHECKPOINT) {
-		recv_sys_create();
-		recv_sys_init(buf_pool_get_curr_size());
-	}
 
 	if (srv_force_recovery >= SRV_FORCE_NO_LOG_REDO) {
 
@@ -3364,11 +3358,6 @@ recv_recovery_from_checkpoint_start_func(
 					return(DB_READ_ONLY);
 				}
 			}
-		}
-
-		if (!recv_needed_recovery && !srv_read_only_mode) {
-			/* Init the doublewrite buffer memory structure */
-			buf_dblwr_init_or_load_pages(false);
 		}
 	}
 
@@ -4079,7 +4068,7 @@ void recv_dblwr_t::add(byte* page)
 	pages.push_back(page);
 }
 
-byte* recv_dblwr_t::find_first_page(ulint space_id)
+byte* recv_dblwr_t::find_page(ulint space_id, ulint page_no)
 {
 	std::vector<byte*> matches;
 	byte*	result = 0;
@@ -4088,7 +4077,7 @@ byte* recv_dblwr_t::find_first_page(ulint space_id)
 	     i != pages.end(); ++i) {
 
 		if ((page_get_space_id(*i) == space_id)
-		    && (page_get_page_no(*i) == 0)) {
+		    && (page_get_page_no(*i) == page_no)) {
 			matches.push_back(*i);
 		}
 	}
