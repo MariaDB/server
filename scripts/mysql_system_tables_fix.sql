aafe43b7e049b1537e63eef6509c8a8075533b0e
Sergei Golubchik <sergii@pisem.net>
2014-08-05 14:37:05 +0200

    cleanup (move ALTER TABLE for comment to be applicable again)

--- scripts/mysql_system_tables_fix.sql
+++ scripts/mysql_system_tables_fix.sql
@@ -648,6 +648,12 @@ DROP TABLE tmp_proxies_priv;
 # Convering the host name to lower case for existing users
 UPDATE user SET host=LOWER( host ) WHERE LOWER( host ) <> host;
 
+SET @innodb_index_stats_fk= (select count(*) from information_schema.referential_constraints where constraint_schema='mysql' and table_name = 'innodb_index_stats' and referenced_table_name = 'innodb_table_stats' and constraint_name = 'innodb_index_stats_ibfk_1');
+SET @str=IF(@innodb_index_stats_fk > 0 and @have_innodb > 0, "ALTER TABLE mysql.innodb_index_stats DROP FOREIGN KEY `innodb_index_stats_ibfk_1`", "SET @dummy = 0");
+PREPARE stmt FROM @str;
+EXECUTE stmt;
+DROP PREPARE stmt; 
+
 # MDEV-4332 longer user names
 alter table user         modify User         char(80)  binary not null default '';
 alter table db           modify User         char(80)  binary not null default '';
@@ -666,13 +672,6 @@ alter table tables_priv  modify Grantor      char(141) COLLATE utf8_bin not null
 # Activate the new, possible modified privilege tables
 # This should not be needed, but gives us some extra testing that the above
 # changes was correct
-
-set @have_innodb= (select count(engine) from information_schema.engines where engine='INNODB' and support != 'NO');
-SET @innodb_index_stats_fk= (select count(*) from information_schema.referential_constraints where constraint_schema='mysql' and table_name = 'innodb_index_stats' and referenced_table_name = 'innodb_table_stats' and constraint_name = 'innodb_index_stats_ibfk_1');
-SET @str=IF(@innodb_index_stats_fk > 0 and @have_innodb > 0, "ALTER TABLE mysql.innodb_index_stats DROP FOREIGN KEY `innodb_index_stats_ibfk_1`", "SET @dummy = 0");
-PREPARE stmt FROM @str;
-EXECUTE stmt;
-DROP PREPARE stmt; 
 
 flush privileges;
 
