9f0bdda8abffc182eeb0b8a2195a68a328fe5813
Sergei Golubchik <sergii@pisem.net>
2014-09-07 20:19:12 +0200

    MDEV-6580 Assertion `thd' failed in my_malloc_size_cb_func upon writing status report into error log
    thd is NULL in the signal handler, display_table_locks() should expect that

--- sql/sql_test.cc
+++ sql/sql_test.cc
@@ -468,8 +468,7 @@ static void display_table_locks(void)
   DYNAMIC_ARRAY saved_table_locks;
 
   (void) my_init_dynamic_array(&saved_table_locks,sizeof(TABLE_LOCK_INFO),
-                               tc_records() + 20, 50,
-                               MYF(MY_THREAD_SPECIFIC));
+                               tc_records() + 20, 50, MYF(0));
   mysql_mutex_lock(&THR_LOCK_lock);
   for (list= thr_lock_thread_list; list; list= list_rest(list))
   {
