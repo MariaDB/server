af4d469a8d32aa04272d49767e8b8e05e0118441
Sergey Petrunya <psergey@askmonty.org>
2014-10-16 22:58:08 +0400

    MDEV-6879: Dereference of NULL primary_file->table in DsMrr_impl::get_disk_sweep_mrr_cost()
    - Don't attempt to use BKA for materialized derived tables. The 
      table is neither filled nor fully opened yet, so attempt to 
      call handler->multi_range_read_info() causes crash.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -10614,7 +10614,20 @@ uint check_join_cache_usage(JOIN_TAB *tab,
       goto no_join_cache;
     if (tab->ref.is_access_triggered())
       goto no_join_cache;
-      
+    
+    /*
+      Don't use BKA for materialized tables. We could actually have a
+      meaningful use of BKA when linked join buffers are used.
+
+      The problem is, the temp.table is not filled (actually not even opened
+      properly) yet, and this doesn't let us call 
+      handler->multi_range_read_info(). It is possible to come up with
+      estimates, etc. without acessing the table, but it seems not to worth the
+      effort now.
+    */
+    if (tab->table->pos_in_table_list->is_materialized_derived())
+      goto no_join_cache;
+
     if (!tab->is_ref_for_hash_join())
     {
       flags= HA_MRR_NO_NULL_ENDPOINTS | HA_MRR_SINGLE_POINT;
