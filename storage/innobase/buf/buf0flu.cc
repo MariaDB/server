78e31f0a2edd620cc0f642f592a9168f40a5e333
Jan Lindström <jplindst@mariadb.org>
2014-10-24 18:58:04 +0300

    MDEV-6931: Page cleaner should do LRU flushing regardless of server activity
    Merge Facebook commit 926a077b14b73c14094de7fc7aa913241b801b4d
    authored by Inaam Rana from https://github.com/facebook/mysql-5.6.
    
    This is fix for upstream bugs
    http://bugs.mysql.com/bug.php?id=71988
    http://bugs.mysql.com/bug.php?id=70500
    
    page_cleaner should work whether or not there is server activity.
    Its iterations become a noop when there is no work to do but we
    should not tie it to the server activity.
    
    The page_cleaner thread does spurious background flushing
    because of conditional sleep between iterations. The solution
    is not to make sleep dependent on server activity etc.

--- storage/innobase/buf/buf0flu.cc
+++ storage/innobase/buf/buf0flu.cc
@@ -2411,25 +2411,16 @@ DECLARE_THREAD(buf_flush_page_cleaner_thread)(
 
 	while (srv_shutdown_state == SRV_SHUTDOWN_NONE) {
 
-		/* The page_cleaner skips sleep if the server is
-		idle and there are no pending IOs in the buffer pool
-		and there is work to do. */
-		if (srv_check_activity(last_activity)
-		    || buf_get_n_pending_read_ios()
-		    || n_flushed == 0) {
-			page_cleaner_sleep_if_needed(next_loop_time);
-		}
+		page_cleaner_sleep_if_needed(next_loop_time);
 
 		next_loop_time = ut_time_ms() + 1000;
 
 		if (srv_check_activity(last_activity)) {
 			last_activity = srv_get_activity_count();
 
-			/* Flush pages from end of LRU if required */
-			n_flushed = buf_flush_LRU_tail();
-
 			/* Flush pages from flush_list if required */
-			n_flushed += page_cleaner_flush_pages_if_needed();
+			page_cleaner_flush_pages_if_needed();
+			n_flushed = 0;
 		} else {
 			n_flushed = page_cleaner_do_flush_batch(
 							PCT_IO(100),
@@ -2443,6 +2434,9 @@ DECLARE_THREAD(buf_flush_page_cleaner_thread)(
 					n_flushed);
 			}
 		}
+
+		/* Flush pages from end of LRU if required */
+		n_flushed = buf_flush_LRU_tail();
 	}
 
 	ut_ad(srv_shutdown_state > 0);
