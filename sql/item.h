d1ca1c1faeb1e43df1533a10dfa0ac79433227f9
Alexander Barkov <bar@mnogosearch.org>
2014-11-02 01:08:09 +0400

    MDEV-7001 Bad result for NOT NOT STRCMP('a','b') and NOT NOT NULLIF(2,3) The bug is not very important per se, but it was helpful to move Item_func_strcmp out of Item_bool_func2 (to Item_int_func), for the purposes of "MDEV-4912 Add a plugin to field types (column types)".

--- sql/item.h
+++ sql/item.h
@@ -164,6 +164,12 @@ public:
       default: return "UNKNOWN";
     }
   }
+  int sortcmp(const String *s, const String *t) const
+  {
+    return collation->coll->strnncollsp(collation,
+                                        (uchar *) s->ptr(), s->length(),
+                                        (uchar *) t->ptr(), t->length(), 0);
+  }
 };
 
 /*************************************************************************/
