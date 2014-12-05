zhujzhuo/MariaDB-source-code

my_alloc.c - Changed 0x%lx -> %p array.c: - Static (preallocated) buffer can now be anywhere my_sys.h - Define MY_INIT_BUFFER_USED sql_delete.cc & sql_lex.cc - Use memroot when allocating classes (avoids call to current_thd) sql_explain.h: - Use preallocated buffers sql_explain.cc: - Use preallocated buffers and memroot sql_select.cc: - Use multi_alloc_root() instead of many alloc_root() - Update calls to Explain

sql/sql_explain.cc
@@ -22,10 +22,11 @@
 #include "sql_select.h"
 
 
-Explain_query::Explain_query(THD *thd_arg) : 
-  upd_del_plan(NULL), insert_plan(NULL), thd(thd_arg), apc_enabled(false)
+Explain_query::Explain_query(THD *thd_arg, MEM_ROOT *root) : 
+  mem_root(root), upd_del_plan(NULL),  insert_plan(NULL),
+  unions(root), selects(root),  thd(thd_arg), apc_enabled(false),
+  operations(0)
 {
-  operations= 0;
 }
 
 
@@ -135,7 +136,7 @@ int Explain_query::send_explain(THD *thd)
   select_result *result;
   LEX *lex= thd->lex;
  
-  if (!(result= new select_send()) || 
+  if (!(result= new (thd->mem_root) select_send()) || 
       thd->send_explain_fields(result))
     return 1;
 
@@ -187,7 +188,8 @@ bool print_explain_query(LEX *lex, THD *thd, String *str)
   Return tabular EXPLAIN output as a text string
 */
 
-bool Explain_query::print_explain_str(THD *thd, String *out_str,  bool is_analyze)
+bool Explain_query::print_explain_str(THD *thd, String *out_str,
+                                      bool is_analyze)
 {
   List<Item> fields;
   thd->make_explain_field_list(fields);
@@ -344,6 +346,20 @@ int Explain_node::print_explain_for_children(Explain_query *query,
   return 0;
 }
 
+bool Explain_select::add_table(Explain_table_access *tab, Explain_query *query)
+{
+  if (!join_tabs)
+  {
+    n_join_tabs= 0;
+    if (!(join_tabs= ((Explain_table_access**)
+                      alloc_root(query->mem_root,
+                                 sizeof(Explain_table_access*) *
+                                 MAX_TABLES))))
+      return true;
+  }
+  join_tabs[n_join_tabs++]= tab;
+  return false;
+}
 
 void Explain_select::replace_table(uint idx, Explain_table_access *new_tab)
 {
@@ -358,7 +374,6 @@ Explain_select::~Explain_select()
   {
     for (uint i= 0; i< n_join_tabs; i++)
       delete join_tabs[i];
-    my_free(join_tabs);
   }
 } 
 
@@ -1016,9 +1031,12 @@ void delete_explain_query(LEX *lex)
 void create_explain_query(LEX *lex, MEM_ROOT *mem_root)
 {
   DBUG_ASSERT(!lex->explain);
-  lex->explain= new Explain_query(lex->thd);
+  DBUG_ENTER("create_explain_query");
+
+  lex->explain= new (mem_root) Explain_query(lex->thd, mem_root);
   DBUG_ASSERT(mem_root == current_thd->mem_root);
-  lex->explain->mem_root= mem_root;
+
+  DBUG_VOID_RETURN;
 }
 
 void create_explain_query_if_not_exists(LEX *lex, MEM_ROOT *mem_root)

sql/sql_select.cc
@@ -3414,23 +3414,29 @@ make_join_statistics(JOIN *join, List<TABLE_LIST> &tables_list,
   LINT_INIT(table); /* inited in all loops */
   table_count=join->table_count;
 
-  stat=(JOIN_TAB*) join->thd->calloc(sizeof(JOIN_TAB)*(table_count));
-  stat_ref=(JOIN_TAB**) join->thd->alloc(sizeof(JOIN_TAB*)*
-                                         (MAX_TABLES + table_count + 1));
-  stat_vector= stat_ref + MAX_TABLES;
-  table_vector=(TABLE**) join->thd->calloc(sizeof(TABLE*)*(table_count*2));
-  join->positions= new (join->thd->mem_root) POSITION[(table_count+1)];
   /*
     best_positions is ok to allocate with alloc() as we copy things to it with
     memcpy()
   */
-  join->best_positions= (POSITION*) join->thd->alloc(sizeof(POSITION)*
-                                                     (table_count +1));
 
-  if (join->thd->is_fatal_error)
-    DBUG_RETURN(1);				// Eom /* purecov: inspected */
+  if (!multi_alloc_root(join->thd->mem_root,
+                        &stat, sizeof(JOIN_TAB)*(table_count),
+                        &stat_ref, sizeof(JOIN_TAB*)* MAX_TABLES,
+                        &stat_vector, sizeof(JOIN_TAB*)* (table_count +1),
+                        &table_vector, sizeof(TABLE*)*(table_count*2),
+                        &join->positions, sizeof(POSITION)*(table_count + 1),
+                        &join->best_positions,
+                        sizeof(POSITION)*(table_count + 1),
+                        NullS))
+    DBUG_RETURN(1);
 
-  join->best_ref=stat_vector;
+  /* The following should be optimized to only clear critical things */
+  bzero(stat, sizeof(JOIN_TAB)* table_count);
+  /* Initialize POSITION objects */
+  for (i=0 ; i <= table_count ; i++)
+    (void) new ((char*) (join->positions + i)) POSITION;
+
+  join->best_ref= stat_vector;
 
   stat_end=stat+table_count;
   found_const_table_map= all_table_map=0;
@@ -23279,15 +23285,18 @@ void append_possible_keys(String *str, TABLE *table, key_map possible_keys)
   }
 }
 
-// TODO: this function is only applicable for the first non-const optimization
-// join tab. 
+/*
+  TODO: this function is only applicable for the first non-const optimization
+  join tab. 
+*/
+
 void JOIN_TAB::update_explain_data(uint idx)
 {
   if (this == first_breadth_first_tab(join, WALK_OPTIMIZATION_TABS) + join->const_tables &&
       join->select_lex->select_number != INT_MAX &&
       join->select_lex->select_number != UINT_MAX)
   {
-    Explain_table_access *eta= new Explain_table_access();
+    Explain_table_access *eta= new (join->thd->mem_root) Explain_table_access(join->thd->mem_root);
     JOIN_TAB* const first_top_tab= first_breadth_first_tab(join, WALK_OPTIMIZATION_TABS);
     save_explain_data(eta, join->const_table_map, join->select_distinct, first_top_tab);
 
@@ -23740,7 +23749,7 @@ int JOIN::save_explain_data_intern(Explain_query *output, bool need_tmp_table,
   if (message)
   {
     Explain_select *xpl_sel;
-    explain_node= xpl_sel= new (output->mem_root) Explain_select;
+    explain_node= xpl_sel= new (output->mem_root) Explain_select(output->mem_root);
     join->select_lex->set_explain_type(true);
 
     xpl_sel->select_id= join->select_lex->select_number;
@@ -23757,7 +23766,7 @@ int JOIN::save_explain_data_intern(Explain_query *output, bool need_tmp_table,
            join->select_lex->master_unit()->derived->is_materialized_derived())
   {
     Explain_select *xpl_sel;
-    explain_node= xpl_sel= new (output->mem_root) Explain_select;
+    explain_node= xpl_sel= new (output->mem_root) Explain_select(output->mem_root);
     table_map used_tables=0;
 
     join->select_lex->set_explain_type(true);
@@ -23787,8 +23796,9 @@ int JOIN::save_explain_data_intern(Explain_query *output, bool need_tmp_table,
         tab= pre_sort_join_tab;
       }
 
-      Explain_table_access *eta= new (output->mem_root) Explain_table_access;
-      xpl_sel->add_table(eta);
+      Explain_table_access *eta= (new (output->mem_root)
+                                  Explain_table_access(output->mem_root));
+      xpl_sel->add_table(eta, output);
 
       tab->save_explain_data(eta, used_tables, distinct, first_top_tab);
 

MariaDB/server

sql/sql_explain.cc
@@ -27,10 +27,11 @@ const char * STR_DELETING_ALL_ROWS= "Deleting all rows";
 const char * STR_IMPOSSIBLE_WHERE= "Impossible WHERE";
 const char * STR_NO_ROWS_AFTER_PRUNING= "No matching rows after partition pruning";
 
-Explain_query::Explain_query(THD *thd_arg) : 
-  upd_del_plan(NULL), insert_plan(NULL), thd(thd_arg), apc_enabled(false)
+Explain_query::Explain_query(THD *thd_arg, MEM_ROOT *root) : 
+  mem_root(root), upd_del_plan(NULL),  insert_plan(NULL),
+  unions(root), selects(root),  thd(thd_arg), apc_enabled(false),
+  operations(0)
 {
-  operations= 0;
 }
 
 
@@ -140,7 +141,7 @@ int Explain_query::send_explain(THD *thd)
   select_result *result;
   LEX *lex= thd->lex;
  
-  if (!(result= new select_send()) || 
+  if (!(result= new (thd->mem_root) select_send()) || 
       thd->send_explain_fields(result, lex->describe, lex->analyze_stmt))
     return 1;
 
@@ -225,7 +226,8 @@ bool print_explain_for_slow_log(LEX *lex, THD *thd, String *str)
   Return tabular EXPLAIN output as a text string
 */
 
-bool Explain_query::print_explain_str(THD *thd, String *out_str,  bool is_analyze)
+bool Explain_query::print_explain_str(THD *thd, String *out_str,
+                                      bool is_analyze)
 {
   List<Item> fields;
   thd->make_explain_field_list(fields, thd->lex->describe, is_analyze);
@@ -551,6 +553,20 @@ int Explain_node::print_explain_for_children(Explain_query *query,
   return 0;
 }
 
+bool Explain_basic_join::add_table(Explain_table_access *tab, Explain_query *query)
+{
+  if (!join_tabs)
+  {
+    n_join_tabs= 0;
+    if (!(join_tabs= ((Explain_table_access**)
+                      alloc_root(query->mem_root,
+                                 sizeof(Explain_table_access*) *
+                                 MAX_TABLES))))
+      return true;
+  }
+  join_tabs[n_join_tabs++]= tab;
+  return false;
+}
 
 /*
   This tells whether a child subquery should be printed in JSON output.
@@ -609,7 +625,6 @@ Explain_basic_join::~Explain_basic_join()
   {
     for (uint i= 0; i< n_join_tabs; i++)
       delete join_tabs[i];
-    my_free(join_tabs);
   }
 } 
 
@@ -1895,9 +1910,12 @@ void delete_explain_query(LEX *lex)
 void create_explain_query(LEX *lex, MEM_ROOT *mem_root)
 {
   DBUG_ASSERT(!lex->explain);
-  lex->explain= new Explain_query(lex->thd);
+  DBUG_ENTER("create_explain_query");
+
+  lex->explain= new (mem_root) Explain_query(lex->thd, mem_root);
   DBUG_ASSERT(mem_root == current_thd->mem_root);
-  lex->explain->mem_root= mem_root;
+
+  DBUG_VOID_RETURN;
 }
 
 void create_explain_query_if_not_exists(LEX *lex, MEM_ROOT *mem_root)

sql/sql_select.cc
@@ -3413,23 +3413,29 @@ make_join_statistics(JOIN *join, List<TABLE_LIST> &tables_list,
   LINT_INIT(table); /* inited in all loops */
   table_count=join->table_count;
 
-  stat=(JOIN_TAB*) join->thd->calloc(sizeof(JOIN_TAB)*(table_count));
-  stat_ref=(JOIN_TAB**) join->thd->alloc(sizeof(JOIN_TAB*)*
-                                         (MAX_TABLES + table_count + 1));
-  stat_vector= stat_ref + MAX_TABLES;
-  table_vector=(TABLE**) join->thd->calloc(sizeof(TABLE*)*(table_count*2));
-  join->positions= new (join->thd->mem_root) POSITION[(table_count+1)];
   /*
     best_positions is ok to allocate with alloc() as we copy things to it with
     memcpy()
   */
-  join->best_positions= (POSITION*) join->thd->alloc(sizeof(POSITION)*
-                                                     (table_count +1));
 
-  if (join->thd->is_fatal_error)
-    DBUG_RETURN(1);				// Eom /* purecov: inspected */
+  if (!multi_alloc_root(join->thd->mem_root,
+                        &stat, sizeof(JOIN_TAB)*(table_count),
+                        &stat_ref, sizeof(JOIN_TAB*)* MAX_TABLES,
+                        &stat_vector, sizeof(JOIN_TAB*)* (table_count +1),
+                        &table_vector, sizeof(TABLE*)*(table_count*2),
+                        &join->positions, sizeof(POSITION)*(table_count + 1),
+                        &join->best_positions,
+                        sizeof(POSITION)*(table_count + 1),
+                        NullS))
+    DBUG_RETURN(1);
 
-  join->best_ref=stat_vector;
+  /* The following should be optimized to only clear critical things */
+  bzero(stat, sizeof(JOIN_TAB)* table_count);
+  /* Initialize POSITION objects */
+  for (i=0 ; i <= table_count ; i++)
+    (void) new ((char*) (join->positions + i)) POSITION;
+
+  join->best_ref= stat_vector;
 
   stat_end=stat+table_count;
   found_const_table_map= all_table_map=0;
@@ -23267,15 +23273,18 @@ int append_possible_keys(MEM_ROOT *alloc, String_list &list, TABLE *table,
   return 0;
 }
 
-// TODO: this function is only applicable for the first non-const optimization
-// join tab. 
+/*
+  TODO: this function is only applicable for the first non-const optimization
+  join tab. 
+*/
+
 void JOIN_TAB::update_explain_data(uint idx)
 {
   if (this == first_breadth_first_tab(join, WALK_OPTIMIZATION_TABS) + join->const_tables &&
       join->select_lex->select_number != INT_MAX &&
       join->select_lex->select_number != UINT_MAX)
   {
-    Explain_table_access *eta= new Explain_table_access();
+    Explain_table_access *eta= new (join->thd->mem_root) Explain_table_access(join->thd->mem_root);
     JOIN_TAB* const first_top_tab= first_breadth_first_tab(join, WALK_OPTIMIZATION_TABS);
     save_explain_data(eta, join->const_table_map, join->select_distinct, first_top_tab);
 
@@ -23721,7 +23730,7 @@ int JOIN::save_explain_data_intern(Explain_query *output, bool need_tmp_table,
   if (message)
   {
     Explain_select *xpl_sel;
-    explain_node= xpl_sel= new (output->mem_root) Explain_select;
+    explain_node= xpl_sel= new (output->mem_root) Explain_select(output->mem_root);
     join->select_lex->set_explain_type(true);
 
     xpl_sel->select_id= join->select_lex->select_number;
@@ -23740,7 +23749,7 @@ int JOIN::save_explain_data_intern(Explain_query *output, bool need_tmp_table,
            join->select_lex->master_unit()->derived->is_materialized_derived())
   {
     Explain_select *xpl_sel;
-    explain_node= xpl_sel= new (output->mem_root) Explain_select;
+    explain_node= xpl_sel= new (output->mem_root) Explain_select(output->mem_root);
     table_map used_tables=0;
 
     join->select_lex->set_explain_type(true);
@@ -23781,7 +23790,8 @@ int JOIN::save_explain_data_intern(Explain_query *output, bool need_tmp_table,
         tab= pre_sort_join_tab;
       }
 
-      Explain_table_access *eta= new (output->mem_root) Explain_table_access;
+      Explain_table_access *eta= (new (output->mem_root)
+                                  Explain_table_access(output->mem_root));
 
       if (tab->bush_root_tab != prev_bush_root_tab)
       {
@@ -23790,7 +23800,7 @@ int JOIN::save_explain_data_intern(Explain_query *output, bool need_tmp_table,
           /* 
             We've entered an SJ-Materialization nest. Create an object for it.
           */
-          cur_parent= new Explain_basic_join;
+          cur_parent= new (output->mem_root) Explain_basic_join(output->mem_root);
 
           JOIN_TAB *first_child= tab->bush_root_tab->bush_children->start;
           cur_parent->select_id=
@@ -23809,7 +23819,7 @@ int JOIN::save_explain_data_intern(Explain_query *output, bool need_tmp_table,
       }
       prev_bush_root_tab= tab->bush_root_tab;
 
-      cur_parent->add_table(eta);
+      cur_parent->add_table(eta, output);
       tab->save_explain_data(eta, used_tables, distinct, first_top_tab);
 
       if (saved_join_tab)