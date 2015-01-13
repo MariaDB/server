33b4fab8679a2e34a97e167a2a88dc5ea0a5686a
Nirbhay Choubey <nirbhay@mariadb.com>
2015-01-13 13:10:07 -0500

    MDEV-6771 : Incorrect Size for Transfer Reported to pv
    xargs may split the input and execute them in batches for
    large number of files. As a result xtrabackup sst script
    may feed incorrect size to pv.
    Fixed by piping the files to du directly (du --files0-from=-).
    
    [Based on suggestions from Brad Jorgensen]

--- scripts/wsrep_sst_xtrabackup.sh
+++ scripts/wsrep_sst_xtrabackup.sh
@@ -205,7 +205,7 @@ parse_cnf()
 get_footprint()
 {
     pushd $WSREP_SST_OPT_DATA 1>/dev/null
-    payload=$(find . -regex '.*\.ibd$\|.*\.MYI$\|.*\.MYD$\|.*ibdata1$' -type f -print0 | xargs -0 du --block-size=1 -c | awk 'END { print $1 }')
+    payload=$(find . -regex '.*\.ibd$\|.*\.MYI$\|.*\.MYD$\|.*ibdata1$' -type f -print0 | du --files0-from=- --block-size=1 -c | awk 'END { print $1 }')
     if my_print_defaults -c $WSREP_SST_OPT_CONF xtrabackup | grep -q -- "--compress";then 
         # QuickLZ has around 50% compression ratio
         # When compression/compaction used, the progress is only an approximate.
