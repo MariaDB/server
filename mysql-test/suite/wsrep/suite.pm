248c662cd7b7421f0a87c21a3903395c99c04939
Nirbhay Choubey <nirbhay@mariadb.com>
2015-01-26 22:43:46 -0500

    Minor test modifications.

--- mysql-test/suite/wsrep/suite.pm
+++ mysql-test/suite/wsrep/suite.pm
@@ -22,6 +22,15 @@ return "No SST scripts" unless $spath;
 my ($epath) = grep { -f "$_/my_print_defaults"; } "$::bindir/extra", $::path_client_bindir;
 return "No my_print_defaults" unless $epath;
 
+push @::global_suppressions,
+  (
+     qr(WSREP: Failed to guess base node address),
+     qr(WSREP: Guessing address for incoming client connections failed),
+     qr(WSREP: Could not open saved state file for reading: ),
+     qr(WSREP: option --wsrep-casual-reads is deprecated),
+     qr(WSREP: --wsrep-casual-reads=ON takes precedence over --wsrep-sync-wait=0),
+   );
+
 $ENV{PATH}="$epath:$ENV{PATH}";
 $ENV{PATH}="$spath:$ENV{PATH}" unless $epath eq $spath;
 
