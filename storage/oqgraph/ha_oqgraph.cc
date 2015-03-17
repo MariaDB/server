116d4010576b2f782aeb68e9315c0e53a24c667d
Andrew McDonnell <bugs@andrewmcdonnell.net>
2015-03-17 20:23:05 +1030

    Forgot my own name...

--- storage/oqgraph/ha_oqgraph.cc
+++ storage/oqgraph/ha_oqgraph.cc
@@ -1,4 +1,5 @@
 /* Copyright (C) 2007-2013 Arjen G Lentz & Antony T Curtis for Open Query
+   Copyright (C) 2013-2015 Andrew McDonnell
    Copyright (C) 2014 Sergei Golubchik
    Portions of this file copyright (C) 2000-2006 MySQL AB
 
