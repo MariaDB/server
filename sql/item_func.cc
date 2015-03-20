91bd1cca8ee6fba65033db595a91263b4dfabaaf
f4rnham <f4rnham@PC.none>
2015-03-20 23:58:14 +0100

    MDEV-7130 MASTER_POS_WAIT(log_name,log_pos,timeout,"connection_name") hangs, does not respect the timeout
    Changed also check for connection_name to prevent same bug if fifth argument is introduced in future

--- sql/item_func.cc
+++ sql/item_func.cc
@@ -3956,11 +3956,11 @@ longlong Item_master_pos_wait::val_int()
   }
 #ifdef HAVE_REPLICATION
   longlong pos = (ulong)args[1]->val_int();
-  longlong timeout = (arg_count==3) ? args[2]->val_int() : 0 ;
+  longlong timeout = (arg_count>=3) ? args[2]->val_int() : 0 ;
   String connection_name_buff;
   LEX_STRING connection_name;
   Master_info *mi;
-  if (arg_count == 4)
+  if (arg_count >= 4)
   {
     String *con;
     if (!(con= args[3]->val_str(&connection_name_buff)))
