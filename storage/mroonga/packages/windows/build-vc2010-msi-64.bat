d18beccc1d7f44d21987ee8da48c3b33a62b26e1
Kentoku SHIBA <kentokushiba@gmail.com>
2015-05-05 02:43:19 +0900

    add -fPIC for groonga-normalizer-mysql

--- storage/mroonga/packages/windows/build-vc2010-msi-64.bat
+++
@@ -1,8 +0,0 @@
-rmdir /S /Q build-vc2010-msi-64
-mkdir build-vc2010-msi-64
-cd build-vc2010-msi-64
-cmake ..\source -G "Visual Studio 10 Win64" > config.log
-cmake --build . --config RelWithDebInfo > build.log
-cmake --build . --config RelWithDebInfo --target msi > msi.log
-move *.msi ..\
-cd ..
--- storage/mroonga/packages/windows/build-vc2010.bat
+++
@@ -1,4 +0,0 @@
-build-vc2010-zip-32.bat
-build-vc2010-zip-64.bat
-build-vc2010-msi-32.bat
-build-vc2010-msi-64.bat
