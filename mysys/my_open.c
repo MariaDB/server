0f4478105f5027ce589a0f08c6019d906d632029
Nirbhay Choubey <nirbhay@mariadb.com>
2015-06-24 17:02:33 -0400

    Add close-on-exec flag to open(), socket(), accept() & fopen().

--- mysys/my_open.c
+++ mysys/my_open.c
@@ -46,9 +46,9 @@ File my_open(const char *FileName, int Flags, myf MyFlags)
 #if defined(_WIN32)
   fd= my_win_open(FileName, Flags);
 #elif !defined(NO_OPEN_3)
-  fd = open(FileName, Flags, my_umask);	/* Normal unix */
+  fd = open(FileName, Flags | O_CLOEXEC, my_umask);     /* Normal unix */
 #else
-  fd = open((char *) FileName, Flags);
+  fd = open((char *) FileName, Flags | O_CLOEXEC);
 #endif
 
   fd= my_register_filename(fd, FileName, FILE_BY_OPEN,
