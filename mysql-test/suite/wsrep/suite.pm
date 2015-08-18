98bebad67274a87517a94b204cd8684a2e48d8c6
Nirbhay Choubey <nirbhay@mariadb.com>
2015-08-18 17:03:28 -0400

    Fix for a typo.

--- mysql-test/suite/wsrep/suite.pm
+++ mysql-test/suite/wsrep/suite.pm
@@ -25,8 +25,8 @@ return "No my_print_defaults" unless $epath;
 push @::global_suppressions,
   (
      qr(WSREP: Could not open saved state file for reading: ),
-     qr(WSREP: option --wsrep-casual-reads is deprecated),
-     qr(WSREP: --wsrep-casual-reads=ON takes precedence over --wsrep-sync-wait=0),
+     qr(WSREP: option --wsrep-causal-reads is deprecated),
+     qr(WSREP: --wsrep-causal-reads=ON takes precedence over --wsrep-sync-wait=0),
      qr|WSREP: access file\(.*gvwstate.dat\) failed\(No such file or directory\)|,
    );
 
