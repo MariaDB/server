fc0e419e434e827a7a6d9549dbf66a02082169e4
Daniel Black <grooverdan@users.sourceforge.net>
2015-09-20 20:51:39 +1000

    CPackRPM - workaround for cmake bug #14782

--- cmake/CPackRPM.cmake
+++ cmake/CPackRPM.cmake
@@ -3,10 +3,16 @@
 #
 
 # load the original CPackRPM.cmake
-set(orig_CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
-unset(CMAKE_MODULE_PATH)
-include(CPackRPM)
-set(CMAKE_MODULE_PATH ${orig_CMAKE_MODULE_PATH})
+# http://public.kitware.com/Bug/view.php?id=14782
+if (CMAKE_VERSION VERSION_LESS "3.3.1" AND CMAKE_VERSION VERSION_GREATER "2.8.12.2")
+  message("CPackRPM: using dodgy workaround since CMake < 3.3.1")
+  include(CPackRPM-3.2.2)
+else()
+  set(orig_CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
+  unset(CMAKE_MODULE_PATH)
+  include(CPackRPM)
+  set(CMAKE_MODULE_PATH ${orig_CMAKE_MODULE_PATH})
+endif()
 
 # per-component cleanup
 foreach(_RPM_SPEC_HEADER URL REQUIRES SUGGESTS PROVIDES OBSOLETES PREFIX CONFLICTS AUTOPROV AUTOREQ AUTOREQPROV)
