180c44e0f600dc9e220887e213679f0d60f29a68
Jan Lindström <jan.lindstrom@mariadb.com>
2015-09-23 10:15:38 +0300

    MDEV-8817: Failing assertion: new_state->key_version != ENCRYPTION_KEY_VERSION_INVALID
    Folloup: Made encryption rules too strict (and incorrect). Allow creating
    table with ENCRYPTED=OFF with all values of ENCRYPTION_KEY_ID but create
    warning that nondefault values are ignored. Allow creating table with
    ENCRYPTED=DEFAULT if used key_id is found from key file (there was
    bug on this) and give error if key_id is not found.

--- storage/xtradb/handler/ha_innodb.cc
+++ storage/xtradb/handler/ha_innodb.cc
@@ -11973,6 +11973,7 @@ ha_innobase::check_table_options(
 		}
 	}
 
+	/* If encryption is set up make sure that used key_id is found */
 	if (encrypt == FIL_SPACE_ENCRYPTION_ON ||
             (encrypt == FIL_SPACE_ENCRYPTION_DEFAULT && srv_encrypt_tables)) {
 		if (!encryption_key_id_exists((unsigned int)options->encryption_key_id)) {
@@ -11987,18 +11988,33 @@ ha_innobase::check_table_options(
 		}
 	}
 
-	/* Do not allow creating unencrypted table with nondefault
-	encryption key */
-	if ((encrypt == FIL_SPACE_ENCRYPTION_OFF ||
-	    (encrypt == FIL_SPACE_ENCRYPTION_DEFAULT && !srv_encrypt_tables)) &&
-		options->encryption_key_id != FIL_DEFAULT_ENCRYPTION_KEY) {
+	/* Ignore nondefault key_id if encryption is set off */
+	if (encrypt == FIL_SPACE_ENCRYPTION_OFF &&
+		options->encryption_key_id != THDVAR(thd, default_encryption_key_id)) {
 		push_warning_printf(
 			thd, Sql_condition::WARN_LEVEL_WARN,
 			HA_WRONG_CREATE_OPTION,
-			"InnoDB: Incorrect ENCRYPTION_KEY_ID %u when encryption is disabled",
+			"InnoDB: Ignored ENCRYPTION_KEY_ID %u when encryption is disabled",
 			(uint)options->encryption_key_id
 		);
-		return "ENCRYPTION_KEY_ID";
+		options->encryption_key_id = FIL_DEFAULT_ENCRYPTION_KEY;
+	}
+
+	/* If default encryption is used make sure that used kay is found
+	from key file. */
+	if (encrypt == FIL_SPACE_ENCRYPTION_DEFAULT &&
+		!srv_encrypt_tables &&
+		options->encryption_key_id != FIL_DEFAULT_ENCRYPTION_KEY) {
+		if (!encryption_key_id_exists((unsigned int)options->encryption_key_id)) {
+			push_warning_printf(
+				thd, Sql_condition::WARN_LEVEL_WARN,
+				HA_WRONG_CREATE_OPTION,
+				"InnoDB: ENCRYPTION_KEY_ID %u not available",
+				(uint)options->encryption_key_id
+			);
+			return "ENCRYPTION_KEY_ID";
+
+		}
 	}
 
 	/* Check atomic writes requirements */
