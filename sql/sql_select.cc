8ed5fde3e1d2f49d140c166ef1e142215f90f572
Monty <monty@mariadb.org>
2015-09-27 21:28:07 +0200

    bug in JOIN_TAB::cleanup() that caused freed memory to be accessed

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -11472,14 +11472,16 @@ void JOIN_TAB::cleanup()
       }
       else
       {
+        TABLE_LIST *tmp= table->pos_in_table_list;
         end_read_record(&read_record);
-        table->pos_in_table_list->jtbm_subselect->cleanup();
+        tmp->jtbm_subselect->cleanup();
         /* 
           The above call freed the materializedd temptable. Set it to NULL so
           that we don't attempt to touch it if JOIN_TAB::cleanup() is invoked
           multiple times (it may be)
         */
-        table=NULL;
+        tmp->table= NULL;
+        table= NULL;
       }
       DBUG_VOID_RETURN;
     }
