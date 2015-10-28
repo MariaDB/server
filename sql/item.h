d88aaaa841aefabce770ad73373363275f606d22
Oleksandr Byelkin <sanja@mariadb.com>
2015-10-28 08:34:08 +0100

    MDEV-8525 mariadb 10.0.20 crashing when data is read by Kodi media center (http://kodi.tv).
    Item_direct_view_ref maintains its own item_const() method so should use it when asked of temporary table field to be in sync with it.

--- sql/item.h
+++ sql/item.h
@@ -3770,6 +3770,8 @@ public:
   bool eq(const Item *item, bool binary_cmp) const;
   Item *get_tmp_table_item(THD *thd)
   {
+    if (const_item())
+      return copy_or_same(thd);
     Item *item= Item_ref::get_tmp_table_item(thd);
     item->name= name;
     return item;
