d5d87c98c045138d1141f97eeeda265591bb7dca
Kristian Nielsen <knielsen@knielsen-hq.org>
2015-11-13 15:30:37 +0100

    Fix embedded server build after MDEV-7818 patch

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -4284,6 +4284,7 @@ end_with_restore_list:
       break;
     }
 
+#ifdef HAVE_REPLICATION
     if (lex->type & REFRESH_READ_LOCK)
     {
       /*
@@ -4295,6 +4296,7 @@ end_with_restore_list:
       if (rpl_pause_for_ftwrl(thd))
         goto error;
     }
+#endif
     /*
       reload_acl_and_cache() will tell us if we are allowed to write to the
       binlog or not.
@@ -4325,8 +4327,10 @@ end_with_restore_list:
       if (!res)
         my_ok(thd);
     } 
+#ifdef HAVE_REPLICATION
     if (lex->type & REFRESH_READ_LOCK)
       rpl_unpause_after_ftwrl(thd);
+#endif
     
     break;
   }
