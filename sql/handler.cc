60343871a73220e2a44d1cebb36e4d109c52c11c
Sergei Golubchik <serg@mariadb.org>
2015-11-13 17:53:16 +0100

    MDEV-8453 Alter table not returning engine errors
    remove ~15 years old print_lock_error() function, use
    handler::print_error() instead
    
    Backport from 10.1

--- sql/handler.cc
+++ sql/handler.cc
@@ -3650,6 +3650,8 @@ void handler::print_error(int error, myf errflag)
 */
 bool handler::get_error_message(int error, String* buf)
 {
+  DBUG_EXECUTE_IF("external_lock_failure",
+                  buf->set_ascii(STRING_WITH_LEN("KABOOM!")););
   return FALSE;
 }
 
@@ -5943,6 +5945,8 @@ int handler::ha_external_lock(THD *thd, int lock_type)
   */
   MYSQL_TABLE_LOCK_WAIT(m_psi, PSI_TABLE_EXTERNAL_LOCK, lock_type,
     { error= external_lock(thd, lock_type); })
+
+  DBUG_EXECUTE_IF("external_lock_failure", error= HA_ERR_GENERIC;);
 
   if (error == 0 || lock_type == F_UNLCK)
   {
