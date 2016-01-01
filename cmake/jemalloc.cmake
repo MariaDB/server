97a1807dc1a65173fcaa875408fb9ed23eac3d2d
Bernard Spil <Sp1l@users.noreply.github.com>
2016-01-01 15:21:26 +0100

    Fix jemalloc detection for FreeBSD
    As of FreeBSD 10.0 (1000012 https://svnweb.freebsd.org/changeset/base/234924) FreeBSD has jemalloc in base.
    /usr/include/malloc_np.h:void malloc_stats_print(void (*write_cb)(void *, const char *),

--- cmake/jemalloc.cmake
+++ cmake/jemalloc.cmake
@@ -12,7 +12,12 @@ MACRO(JEMALLOC_TRY_STATIC)
 ENDMACRO()
 
 MACRO(JEMALLOC_TRY_DYNAMIC)
-  SET(libname jemalloc)
+  IF(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD" AND
+    CMAKE_SYSTEM_VERSION STRGREATER "10.0")
+    SET(libname c)
+  ELSE()
+    SET(libname jemalloc)
+  ENDIF()
   SET(what system)
   CHECK_LIBRARY_EXISTS(${libname} malloc_stats_print "" HAVE_DYNAMIC_JEMALLOC)
 ENDMACRO()
