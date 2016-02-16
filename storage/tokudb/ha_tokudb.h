ae6cc54a4ae6a0764330a6a1c42baae4ec34442f
Sergei Golubchik <serg@mariadb.org>
2016-02-16 19:44:10 +0100

    5.6.28-76.1

--- storage/tokudb/ha_tokudb.h
+++ storage/tokudb/ha_tokudb.h
@@ -837,9 +837,6 @@ public:
     bool primary_key_is_clustered() {
         return true;
     }
-    bool supports_clustered_keys() {
-        return true;
-    }
     int cmp_ref(const uchar * ref1, const uchar * ref2);
     bool check_if_incompatible_data(HA_CREATE_INFO * info, uint table_changes);
 
