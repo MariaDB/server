4fe1c9f4649fd7f982dae367d98e7d81b7223a95
Daniel Black <daniel.black@au.ibm.com>
2016-04-01 11:51:28 +1100

    LF memory allocator to align to line cache size
    Rather than using a constant 128 line size, this uses the
    CPU_LEVEL1_DCACHE_LINESIZE as the pad size.

--- include/lf.h
+++ include/lf.h
@@ -68,14 +68,6 @@ typedef struct {
   void  *purgatory;
   uint32 purgatory_count;
   uint32 volatile link;
-  /*
-    we want sizeof(LF_PINS) to be CPU_LEVEL1_DCACHE_LINESIZE * 2
-    to avoid false sharing
-  */
-  char pad[CPU_LEVEL1_DCACHE_LINESIZE * 2 - sizeof(uint32) * 2
-                                          - sizeof(LF_PINBOX*)
-                                          - sizeof(void*)
-                                          - sizeof(void*) * (LF_PINBOX_PINS + 1)];
 } LF_PINS;
 
 /* compile-time assert to make sure we have enough pins.  */
