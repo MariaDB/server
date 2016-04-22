b233b15ccda8d8a1633e82113e5731cefce5eb84
Sergei Golubchik <serg@mariadb.org>
2016-04-22 08:25:36 +0200

    MDEV-9868 Altering a partitioned table comment does a full copy
    let handler::check_if_supported_inplace_alter()
    (that detects frm-only changes) to work for partitioned
    tables too.

--- sql/handler.cc
+++ sql/handler.cc
@@ -4209,6 +4209,7 @@ handler::check_if_supported_inplace_alter(TABLE *altered_table,
     Alter_inplace_info::ALTER_COLUMN_DEFAULT |
     Alter_inplace_info::ALTER_COLUMN_OPTION |
     Alter_inplace_info::CHANGE_CREATE_OPTION |
+    Alter_inplace_info::ALTER_PARTITIONED |
     Alter_inplace_info::ALTER_RENAME;
 
   /* Is there at least one operation that requires copy algorithm? */
