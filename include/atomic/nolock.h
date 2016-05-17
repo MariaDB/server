77349cb99b0a1cacccb1bcb1dd65325da6edea73
flynn1973 <christian.tremel@itsv.at>
2016-05-17 12:42:54 +0200

    include/nolock.h

--- include/atomic/nolock.h
+++ include/atomic/nolock.h
@@ -17,7 +17,7 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
 #if defined(_AIX)
-# include "aix_builtins.h"
+# include "gcc_builtins.h"
 #elif defined(__i386__) || defined(_MSC_VER) || defined(__x86_64__)   \
     || defined(HAVE_GCC_ATOMIC_BUILTINS) \
     || defined(HAVE_SOLARIS_ATOMIC)
