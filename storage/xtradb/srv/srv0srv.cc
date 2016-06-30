10880d67b95d29dc1a764f2ee5c2010dc89659df
Nirbhay Choubey <nirbhay@mariadb.com>
2016-06-30 10:24:54 -0400

    Postfix: memory leak in XtraDB

--- storage/xtradb/srv/srv0srv.cc
+++ storage/xtradb/srv/srv0srv.cc
@@ -1209,12 +1209,13 @@ srv_free(void)
 		os_event_free(srv_buf_dump_event);
 		os_event_free(srv_checkpoint_completed_event);
 		os_event_free(srv_redo_log_tracked_event);
-#ifdef WITH_INNODB_DISALLOW_WRITES
-		os_event_free(srv_allow_writes_event);
-#endif /* WITH_INNODB_DISALLOW_WRITES */
 		mutex_free(&srv_sys->mutex);
 		mutex_free(&srv_sys->tasks_mutex);
 	}
+
+#ifdef WITH_INNODB_DISALLOW_WRITES
+	os_event_free(srv_allow_writes_event);
+#endif /* WITH_INNODB_DISALLOW_WRITES */
 
 #ifndef HAVE_ATOMIC_BUILTINS
 	mutex_free(&server_mutex);
