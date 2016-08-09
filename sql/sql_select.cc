e20e28bd2eca6bb4961a298c2765885155bfdd57
Sergei Petrunia <psergey@askmonty.org>
2016-08-10 01:13:09 +0300

    Fix for the previous cset: make first_explain_order_tab handle degenerate joins

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -8365,6 +8365,8 @@ JOIN_TAB *first_explain_order_tab(JOIN* join)
 {
   JOIN_TAB* tab;
   tab= join->join_tab;
+  if (!tab)
+    return NULL; /* Can happen when when the tables were optimized away */
   return (tab->bush_children) ? tab->bush_children->start : tab;
 }
 
