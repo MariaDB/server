97b4461233e51d2af2dcc0218c81c49cb85c94ef
SachinSetiya <sachinsetia1001@gmail.com>
2016-08-12 21:09:55 +0530

    hidden fields test added and problems in view solved and problem in copy table containing hidden fields solved

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -7992,28 +7992,42 @@ fill_record(THD *thd, TABLE *table, Field **ptr, List<Item> &values,
     only one row.
   */
   table->auto_increment_field_not_null= FALSE;
-  List_iterator<Item> i_iter(values);
-  Name_resolution_context *context= & thd->lex->select_lex.context;
   Field **f;
+  List_iterator<Item> i_iter(values);
+  uint field_count= 0;
   for (f= ptr; f && (field= *f); f++)
+    field_count++;
+  /*
+    This if is required in query like
+    suppose table
+      create table t1 (a int , b int hidden , c int , d int hidden );
+    and query is
+      create table t2 as select a,b,c,d from t1;
+    in this case field count will be equal to values.elements
+   */
+  if (field_count != values.elements)
   {
-    if (field->field_visibility!=NOT_HIDDEN)
+    Name_resolution_context *context= & thd->lex->select_lex.context;
+    for (f= ptr; f && (field= *f); f++)
     {
-      if (f == ptr)
+      if (field->field_visibility!=NOT_HIDDEN)
       {
-        values.push_front(new (thd->mem_root)
-                          Item_default_value(thd,context),thd->mem_root);
-        i_iter.rewind();
-        i_iter++;
+        if (f == ptr)
+        {
+          values.push_front(new (thd->mem_root)
+                            Item_default_value(thd,context),thd->mem_root);
+          i_iter.rewind();
+          i_iter++;
+        }
+        else
+          i_iter.after(new (thd->mem_root) Item_default_value(thd,context));
       }
       else
-        i_iter.after(new (thd->mem_root) Item_default_value(thd,context));
+        i_iter++;
     }
-    else
-      i_iter++;
+    f= ptr;
+    i_iter.rewind();
   }
-  f= ptr;
-  i_iter.rewind();
   while ((field = *ptr++) && ! thd->is_error())
   {
     /* Ensure that all fields are from the same table */
@@ -8056,11 +8070,14 @@ fill_record(THD *thd, TABLE *table, Field **ptr, List<Item> &values,
      it will only use only one value list and each time we
      add new default it will increase size of value list.
    */
-  for ( ; f && (field= *f) && i_iter++ ; f++)
+  if (field_count != values.elements)
   {
-    if (field->field_visibility!=NOT_HIDDEN)
+    for ( ; f && (field= *f) && i_iter++ ; f++)
     {
-       i_iter.remove();
+      if (field->field_visibility!=NOT_HIDDEN)
+      {
+        i_iter.remove();
+      }
     }
   }
   DBUG_RETURN(thd->is_error());
