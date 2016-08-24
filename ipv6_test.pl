8f146b400b4ed5db9ba1585a5d00e422617f212d
Daniel Black <daniel.black@au.ibm.com>
2016-08-25 08:15:39 +1000

    break apart ipv6 test suite

--- ipv6_test.pl
+++ ipv6_test.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl
     use Socket;
-    return 0 unless socket my $sock, PF_INET6, SOCK_STREAM, getprotobyname('tcp');
+    use Data::Dumper;
+    socket my $sock, PF_INET6, SOCK_STREAM, getprotobyname('tcp');
+    print Dumper($sock);
     # eval{}, if there's no Socket::sockaddr_in6 at all, old Perl installation
-    eval { connect $sock, sockaddr_in6(7, Socket::IN6ADDR_LOOPBACK) };
+    connect $sock, sockaddr_in6(7, Socket::IN6ADDR_LOOPBACK);
+    print Dumper($@);
+    print Dumper($sock);
     exit $@ eq "";
