3dd88fbd1132ae36c00adb67cbe2fc4ff97a6789
Nirbhay Choubey <nirbhay@mariadb.com>
2016-09-01 12:59:11 -0400

    MDEV-10714: Could not execute Delete_rows event on table; wsrep_max_ws_rows exceeded. Error_Code 1180
    The wsrep_max_ws_rows related implementation should be skipped
    when server is running with wsrep disabled.

--- sql/handler.cc
+++ sql/handler.cc
@@ -6109,14 +6109,17 @@ int handler::ha_write_row(uchar *buf)
   if (unlikely(error= binlog_log_row(table, 0, buf, log_func)))
     DBUG_RETURN(error); /* purecov: inspected */
 #ifdef WITH_WSREP
-  current_thd->wsrep_affected_rows++;
-  if (wsrep_max_ws_rows &&
-      current_thd->wsrep_exec_mode != REPL_RECV &&
-      current_thd->wsrep_affected_rows > wsrep_max_ws_rows)
+  if (WSREP(current_thd))
   {
-    trans_rollback_stmt(current_thd) || trans_rollback(current_thd);
-    my_message(ER_ERROR_DURING_COMMIT, "wsrep_max_ws_rows exceeded", MYF(0));
-    DBUG_RETURN(ER_ERROR_DURING_COMMIT);
+    current_thd->wsrep_affected_rows++;
+    if (wsrep_max_ws_rows &&
+        current_thd->wsrep_exec_mode != REPL_RECV &&
+        current_thd->wsrep_affected_rows > wsrep_max_ws_rows)
+    {
+      trans_rollback_stmt(current_thd) || trans_rollback(current_thd);
+      my_message(ER_ERROR_DURING_COMMIT, "wsrep_max_ws_rows exceeded", MYF(0));
+      DBUG_RETURN(ER_ERROR_DURING_COMMIT);
+    }
   }
 #endif /* WITH_WSREP */
 
@@ -6153,14 +6156,17 @@ int handler::ha_update_row(const uchar *old_data, uchar *new_data)
   if (unlikely(error= binlog_log_row(table, old_data, new_data, log_func)))
     return error;
 #ifdef WITH_WSREP
-  current_thd->wsrep_affected_rows++;
-  if (wsrep_max_ws_rows &&
-      current_thd->wsrep_exec_mode != REPL_RECV &&
-      current_thd->wsrep_affected_rows > wsrep_max_ws_rows)
+  if (WSREP(current_thd))
   {
-    trans_rollback_stmt(current_thd) || trans_rollback(current_thd);
-    my_message(ER_ERROR_DURING_COMMIT, "wsrep_max_ws_rows exceeded", MYF(0));
-    return ER_ERROR_DURING_COMMIT;
+    current_thd->wsrep_affected_rows++;
+    if (wsrep_max_ws_rows &&
+        current_thd->wsrep_exec_mode != REPL_RECV &&
+        current_thd->wsrep_affected_rows > wsrep_max_ws_rows)
+    {
+      trans_rollback_stmt(current_thd) || trans_rollback(current_thd);
+      my_message(ER_ERROR_DURING_COMMIT, "wsrep_max_ws_rows exceeded", MYF(0));
+      return ER_ERROR_DURING_COMMIT;
+    }
   }
 #endif /* WITH_WSREP */
   return 0;
@@ -6191,14 +6197,17 @@ int handler::ha_delete_row(const uchar *buf)
   if (unlikely(error= binlog_log_row(table, buf, 0, log_func)))
     return error;
 #ifdef WITH_WSREP
-  current_thd->wsrep_affected_rows++;
-  if (wsrep_max_ws_rows &&
-      current_thd->wsrep_exec_mode != REPL_RECV &&
-      current_thd->wsrep_affected_rows > wsrep_max_ws_rows)
+  if (WSREP(current_thd))
   {
-    trans_rollback_stmt(current_thd) || trans_rollback(current_thd);
-    my_message(ER_ERROR_DURING_COMMIT, "wsrep_max_ws_rows exceeded", MYF(0));
-    return ER_ERROR_DURING_COMMIT;
+    current_thd->wsrep_affected_rows++;
+    if (wsrep_max_ws_rows &&
+        current_thd->wsrep_exec_mode != REPL_RECV &&
+        current_thd->wsrep_affected_rows > wsrep_max_ws_rows)
+    {
+      trans_rollback_stmt(current_thd) || trans_rollback(current_thd);
+      my_message(ER_ERROR_DURING_COMMIT, "wsrep_max_ws_rows exceeded", MYF(0));
+      return ER_ERROR_DURING_COMMIT;
+    }
   }
 #endif /* WITH_WSREP */
   return 0;
