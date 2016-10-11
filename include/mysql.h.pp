ef77847ccf521b4b0c1c5b81cae83baff0f82b90
vinchen <vinchen13@gmail.com>
2016-10-11 15:21:20 +0800

    fix common test suite

--- include/mysql.h.pp
+++ include/mysql.h.pp
@@ -35,6 +35,7 @@ typedef struct st_net {
   my_bool thread_specific_malloc;
   unsigned char compress;
   my_bool unused3;
+  unsigned long real_network_read_len;
   void *thd;
   unsigned int last_errno;
   unsigned char error;
