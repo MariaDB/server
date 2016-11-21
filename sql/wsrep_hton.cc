471dd1138185210b47f11cf6333a989861254105
sjaakola <seppo.jaakola@iki.fi>
2016-11-21 10:38:20 +0200

    refs: MW-319 * silenced the WSREP_ERROR, this fires for all replication filtered DDL,   and is false positive

--- sql/wsrep_hton.cc
+++ sql/wsrep_hton.cc
@@ -313,7 +313,7 @@ wsrep_run_wsrep_commit(THD *thd, handlerton *hton, bool all)
   int replay_round= 0;
 
   if (thd->get_stmt_da()->is_error()) {
-    WSREP_ERROR("commit issue, error: %d %s",
+    WSREP_DEBUG("commit issue, error: %d %s",
                 thd->get_stmt_da()->sql_errno(), thd->get_stmt_da()->message());
   }
 
