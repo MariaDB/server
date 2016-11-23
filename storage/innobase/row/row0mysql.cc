48d924693f49c197763a286362c7cd4f322373a1
kevg <claprix@yandex.ru>
2016-11-23 22:30:14 +0300

    IB: do not insert into VTQ on optimized fields only update [closes #80]

--- storage/innobase/row/row0mysql.cc
+++ storage/innobase/row/row0mysql.cc
@@ -2198,7 +2198,11 @@ run_again:
 		}
 	}
 
-	if (DICT_TF2_FLAG_IS_SET(node->table, DICT_TF2_VERSIONED)) {
+	if (DICT_TF2_FLAG_IS_SET(node->table, DICT_TF2_VERSIONED) &&
+		(node->versioned || node->vers_delete ||
+			// TODO: imrove this check (check if we touch only
+			// unversioned fields in foreigh table
+			node->foreign)) {
 		trx->vtq_notify_on_commit = true;
 	}
 
