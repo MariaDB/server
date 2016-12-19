a4868c3509772da1666eb3d492515e7d39f8834d
Oleksandr Byelkin <sanja@mariadb.com>
2016-12-19 22:03:28 +0100

    MDEV-9208: Function->Function->View = Mysqld segfault (Server crashes in Dependency_marker::visit_field on 2nd execution with merged subquery)
    Prevent crossing name resolution border in finding item tables.

--- sql/item.cc
+++ sql/item.cc
@@ -6876,7 +6876,11 @@ public:
     // Find which select the field is in. This is achieved by walking up 
     // the select tree and looking for the table of interest.
     st_select_lex *sel;
-    for (sel= current_select; sel; sel= sel->outer_select())
+    for (sel= current_select;
+         sel ;
+         sel= (sel->context.outer_context ?
+               sel->context.outer_context->select_lex:
+               NULL))
     {
       List_iterator<TABLE_LIST> li(sel->leaf_tables);
       TABLE_LIST *tbl;
