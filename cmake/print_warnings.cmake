e7ac369a72f6b564aeb607e376411d25cb149e25
Aleksey Midenkov <midenok@gmail.com>
2017-02-02 16:21:06 +0300

    Scripts: print_warnings deps fix

--- cmake/print_warnings.cmake
+++ cmake/print_warnings.cmake
@@ -21,5 +21,17 @@ ADD_CUSTOM_TARGET(rm_compile.warnings ALL
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
 ADD_CUSTOM_TARGET(print_warnings ALL
   COMMAND bash -c '[ -f compile.warnings ] && { echo "Warnings found:" \; cat compile.warnings \; echo "" \; } \; true'
-  DEPENDS mysql udf_example rm_compile.warnings
+  DEPENDS mysqld rm_compile.warnings
   WORKING_DIRECTORY "${CMAKE_BINARY_DIR}")
+
+IF(TARGET explain_filename-t)
+  ADD_DEPENDENCIES(print_warnings explain_filename-t)
+ENDIF()
+
+IF(TARGET mysql_client_test)
+  ADD_DEPENDENCIES(print_warnings mysql_client_test)
+ENDIF()
+
+IF(TARGET udf_example)
+  ADD_DEPENDENCIES(print_warnings udf_example)
+ENDIF()
