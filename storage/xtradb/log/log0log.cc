20e8347447d55c06e426821de207091059ebd214
Marko Mäkelä <marko.makela@mariadb.com>
2017-02-03 12:25:42 +0200

    MDEV-11985 Make innodb_read_only shutdown more robust
    If InnoDB is started in innodb_read_only mode such that
    recovered incomplete transactions exist at startup
    (but the redo logs are clean), an assertion will fail at shutdown,
    because there would exist some non-prepared transactions.
    
    logs_empty_and_mark_files_at_shutdown(): Do not wait for incomplete
    transactions to finish if innodb_read_only or innodb_force_recovery>=3.
    Wait for purge to finish in only one place.
    
    trx_sys_close(): Relax the assertion that would fail first.
    
    trx_free_prepared(): Also free recovered TRX_STATE_ACTIVE transactions
    if innodb_read_only or innodb_force_recovery>=3.

--- storage/xtradb/log/log0log.cc
+++ storage/xtradb/log/log0log.cc
@@ -2,6 +2,7 @@
 
 Copyright (c) 1995, 2016, Oracle and/or its affiliates. All Rights Reserved.
 Copyright (c) 2009, Google Inc.
+Copyright (c) 2017, MariaDB Corporation. All Rights Reserved.
 
 Portions of this file contain modifications contributed and copyrighted by
 Google, Inc. Those modifications are gratefully acknowledged and are described
@@ -3556,10 +3557,9 @@ loop:
 	shutdown, because the InnoDB layer may have committed or
 	prepared transactions and we don't want to lose them. */
 
-	total_trx = trx_sys_any_active_transactions();
-
-	if (total_trx > 0) {
-
+	if (ulint total_trx = srv_was_started && !srv_read_only_mode
+	    && srv_force_recovery < SRV_FORCE_NO_TRX_UNDO
+	    ? trx_sys_any_active_transactions() : 0) {
 		if (srv_print_verbose_log && count > 600) {
 			ib_logf(IB_LOG_LEVEL_INFO,
 				"Waiting for %lu active transactions to finish",
