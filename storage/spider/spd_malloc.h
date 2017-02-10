db0d48170ff9de5b21baa9f3f46841011c920e0e
iangilfillan <github@greenman.co.za>
2017-02-10 13:26:55 +0200

    Correct FSF address

--- storage/spider/spd_malloc.h
+++ storage/spider/spd_malloc.h
@@ -11,7 +11,7 @@
 
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1301 USA */
 
 #define spider_free(A,B,C) spider_free_mem(A,B,C)
 #define spider_malloc(A,B,C,D) \
