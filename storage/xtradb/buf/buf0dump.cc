6da41e17d8cf833a5eb648fe8204e7031bb0e410
Daniele Sciascia <daniele.sciascia@galeracluster.com>
2017-03-02 17:53:16 +0100

    MW-365 Do not load/dump innodb buffer pool with wsrep_recover

--- storage/xtradb/buf/buf0dump.cc
+++ storage/xtradb/buf/buf0dump.cc
@@ -41,6 +41,9 @@ Created April 08, 2011 Vasil Dimov
 #include "sync0rw.h" /* rw_lock_s_lock() */
 #include "ut0byte.h" /* ut_ull_create() */
 #include "ut0sort.h" /* UT_SORT_FUNCTION_BODY */
+#ifdef WITH_WSREP
+extern my_bool wsrep_recovery;
+#endif /* WITH_WSREP */
 
 enum status_severity {
 	STATUS_INFO,
@@ -690,7 +693,13 @@ DECLARE_THREAD(buf_dump_thread)(
 	buf_load_status(STATUS_INFO, "not started");
 
 	if (srv_buffer_pool_load_at_startup) {
+#ifdef WITH_WSREP
+		if (!wsrep_recovery) {
+#endif /* WITH_WSREP */
 		buf_load();
+#ifdef WITH_WSREP
+                }
+#endif /* WITH_WSREP */
 	}
 
 	while (!SHUTTING_DOWN()) {
@@ -714,8 +723,14 @@ DECLARE_THREAD(buf_dump_thread)(
 	}
 
 	if (srv_buffer_pool_dump_at_shutdown && srv_fast_shutdown != 2) {
+#ifdef WITH_WSREP
+		if (!wsrep_recovery) {
+#endif /* WITH_WSREP */
 		buf_dump(FALSE /* ignore shutdown down flag,
 		keep going even if we are in a shutdown state */);
+#ifdef WITH_WSREP
+		}
+#endif /* WITH_WSREP */
 	}
 
 	srv_buf_dump_thread_active = FALSE;
