00f1ed6655b51b8cb496a452471c76b16d50e8e7
Sachin Setiya <sachinsetia1001@gmail.com>
2017-03-14 14:42:52 +0530

    Galera MTR Tests: fix variable output in galera_as_slave_gtid_replicate_do_db.result
    Signed-off-by: Sachin Setiya <sachinsetia1001@gmail.com>

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -69,6 +69,7 @@ struct wsrep_thd_shadow {
   char                 *db;
   size_t               db_length;
   my_hrtime_t          user_time;
+  longlong             row_count_func;
 };
 #endif
 class Reprepare_observer;
