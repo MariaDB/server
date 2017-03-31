16a99c5ad9ac3f73e307488041caedc4e2b5c8c9
Sergei Golubchik <serg@mariadb.org>
2017-03-31 16:28:27 +0200

    MariaRocks tests: various cleanups
    remove hard-coded paths (that assumed we're in a source tree)
    remove various shell/perl/awk/whatsnot scripts, use mysqltest and perl
    remove numerous --exec /some/unix/tool commands, use mysqltest and perl

--- storage/rocksdb/mysql-test/rocksdb/suite.pm
+++ storage/rocksdb/mysql-test/rocksdb/suite.pm
@@ -1,8 +1,12 @@
-package My::Suite::Rocksdb_sys_vars;
+package My::Suite::Rocksdb;
 
 @ISA = qw(My::Suite);
 
 sub is_default { not $::opt_embedded_server }
+
+my ($sst_dump) = grep { -x "$_/sst_dump" } "$::bindir/storage/rocksdb", $::path_client_bindir;
+return "RocksDB is not compiled, no sst_dump" unless $sst_dump;
+$ENV{MARIAROCKS_SST_DUMP}="$sst_dump/sst_dump";
 
 bless { };
 
