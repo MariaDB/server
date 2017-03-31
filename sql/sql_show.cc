MariaDB/server

commit da5c3e03f622a31626605cd43a8623d760caf5a8

MDEV-9255 Add generation_expression to information_schema.columns.
        Added IS_GENERATED and GENERATION_EXPRESSION columns required by
        the SQL standard

sql/sql_show.cc
@@ -5505,13 +5505,24 @@ static int get_schema_column_record(THD *thd, TABLE_LIST *tables,
       table->field[17]->store(STRING_WITH_LEN("auto_increment"), cs);
     if (print_on_update_clause(field, &type, true))
       table->field[17]->store(type.ptr(), type.length(), cs);
+
     if (field->vcol_info)
     {
+      String gen_s(tmp,sizeof(tmp), system_charset_info);
+      gen_s.length(0);
+      field->vcol_info->print(&gen_s);
+      table->field[21]->store(gen_s.ptr(), gen_s.length(), cs);
+      table->field[21]->set_notnull();
+      table->field[20]->store(STRING_WITH_LEN("ALWAYS"), cs);
+
       if (field->vcol_info->stored_in_db)
         table->field[17]->store(STRING_WITH_LEN("STORED GENERATED"), cs);
       else
         table->field[17]->store(STRING_WITH_LEN("VIRTUAL GENERATED"), cs);
     }
+    else
+      table->field[20]->store(STRING_WITH_LEN("NEVER"), cs);
+
     table->field[19]->store(field->comment.str, field->comment.length, cs);
     if (schema_table_store_record(thd, table))
       DBUG_RETURN(1);
@@ -8444,6 +8455,9 @@ ST_FIELD_INFO columns_fields_info[]=
   {"PRIVILEGES", 80, MYSQL_TYPE_STRING, 0, 0, "Privileges", OPEN_FRM_ONLY},
   {"COLUMN_COMMENT", COLUMN_COMMENT_MAXLEN, MYSQL_TYPE_STRING, 0, 0, 
    "Comment", OPEN_FRM_ONLY},
+  {"IS_GENERATED", 6, MYSQL_TYPE_STRING, 0, 0, 0, OPEN_FRM_ONLY},
+  {"GENERATION_EXPRESSION", MAX_FIELD_VARCHARLENGTH, MYSQL_TYPE_STRING, 0, 1,
+    0, OPEN_FRM_ONLY},
   {0, 0, MYSQL_TYPE_STRING, 0, 0, 0, SKIP_OPEN_TABLE}
 };
 

mysql-test/r/information_schema.result
@@ -222,8 +222,8 @@ Field	Type	Collation	Null	Key	Default	Extra	Privileges	Comment
 c	varchar(64)	utf8_general_ci	NO				select,insert,update,references	
 select * from information_schema.COLUMNS where table_name="t1"
 and column_name= "a";
-TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT
-def	mysqltest	t1	a	1	NULL	YES	int	NULL	NULL	10	0	NULL	NULL	NULL	int(11)			select,insert,update,references	
+TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT	IS_GENERATED	GENERATION_EXPRESSION
+def	mysqltest	t1	a	1	NULL	YES	int	NULL	NULL	10	0	NULL	NULL	NULL	int(11)			select,insert,update,references		NEVER	NULL
 show columns from mysqltest.t1 where field like "%a%";
 Field	Type	Null	Key	Default	Extra
 a	int(11)	YES		NULL	
@@ -844,6 +844,7 @@ table_schema	table_name	column_name
 information_schema	ALL_PLUGINS	PLUGIN_DESCRIPTION
 information_schema	COLUMNS	COLUMN_DEFAULT
 information_schema	COLUMNS	COLUMN_TYPE
+information_schema	COLUMNS	GENERATION_EXPRESSION
 information_schema	EVENTS	EVENT_DEFINITION
 information_schema	PARAMETERS	DTD_IDENTIFIER
 information_schema	PARTITIONS	PARTITION_EXPRESSION
@@ -1577,9 +1578,9 @@ WHERE TABLE_SCHEMA='mysql' and TABLE_NAME= 'db';
 TABLE_COLLATION
 utf8_bin
 select * from information_schema.columns where table_schema = NULL;
-TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT
+TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT	IS_GENERATED	GENERATION_EXPRESSION
 select * from `information_schema`.`COLUMNS` where `TABLE_NAME` = NULL;
-TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT
+TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT	IS_GENERATED	GENERATION_EXPRESSION
 select * from `information_schema`.`KEY_COLUMN_USAGE` where `TABLE_SCHEMA` = NULL;
 CONSTRAINT_CATALOG	CONSTRAINT_SCHEMA	CONSTRAINT_NAME	TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	POSITION_IN_UNIQUE_CONSTRAINT	REFERENCED_TABLE_SCHEMA	REFERENCED_TABLE_NAME	REFERENCED_COLUMN_NAME
 select * from `information_schema`.`KEY_COLUMN_USAGE` where `TABLE_NAME` = NULL;
@@ -1920,7 +1921,7 @@ LEFT JOIN INFORMATION_SCHEMA.COLUMNS
 USING (TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME)
 WHERE COLUMNS.TABLE_SCHEMA = 'test'
 AND COLUMNS.TABLE_NAME = 't1';
-TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	CONSTRAINT_CATALOG	CONSTRAINT_SCHEMA	CONSTRAINT_NAME	TABLE_CATALOG	ORDINAL_POSITION	POSITION_IN_UNIQUE_CONSTRAINT	REFERENCED_TABLE_SCHEMA	REFERENCED_TABLE_NAME	REFERENCED_COLUMN_NAME	TABLE_CATALOG	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT
+TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	CONSTRAINT_CATALOG	CONSTRAINT_SCHEMA	CONSTRAINT_NAME	TABLE_CATALOG	ORDINAL_POSITION	POSITION_IN_UNIQUE_CONSTRAINT	REFERENCED_TABLE_SCHEMA	REFERENCED_TABLE_NAME	REFERENCED_COLUMN_NAME	TABLE_CATALOG	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT	IS_GENERATED	GENERATION_EXPRESSION
 #
 # A test case for Bug#56540 "Exception (crash) in sql_show.cc
 # during rqg_info_schema test on Windows"

mysql-test/r/information_schema_parameters.result
@@ -44,6 +44,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -64,6 +66,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -84,6 +88,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -104,6 +110,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -124,6 +132,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -144,6 +154,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -164,6 +176,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -184,6 +198,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -204,6 +220,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -224,6 +242,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -244,6 +264,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -264,6 +286,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -284,6 +308,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -304,6 +330,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -324,6 +352,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	PARAMETERS
@@ -344,6 +374,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 DESCRIBE INFORMATION_SCHEMA.PARAMETERS;
 Field	Type	Null	Key	Default	Extra
 SPECIFIC_CATALOG	varchar(512)	NO			

mysql-test/r/information_schema_routines.result
@@ -61,6 +61,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -81,6 +83,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -101,6 +105,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -121,6 +127,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -141,6 +149,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -161,6 +171,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -181,6 +193,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -201,6 +215,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -221,6 +237,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -241,6 +259,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -261,6 +281,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -281,6 +303,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -301,6 +325,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -321,6 +347,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -341,6 +369,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -361,6 +391,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -381,6 +413,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -401,6 +435,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -421,6 +457,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -441,6 +479,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -461,6 +501,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -481,6 +523,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -501,6 +545,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -521,6 +567,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -541,6 +589,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -561,6 +611,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -581,6 +633,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -601,6 +655,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -621,6 +677,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -641,6 +699,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 TABLE_CATALOG	def
 TABLE_SCHEMA	information_schema
 TABLE_NAME	ROUTINES
@@ -661,6 +721,8 @@ COLUMN_KEY
 EXTRA	
 PRIVILEGES	#
 COLUMN_COMMENT	
+IS_GENERATED	NEVER
+GENERATION_EXPRESSION	NULL
 DESCRIBE INFORMATION_SCHEMA.ROUTINES;
 Field	Type	Null	Key	Default	Extra
 SPECIFIC_NAME	varchar(64)	NO			

mysql-test/suite/funcs_1/r/is_cml_innodb.result
@@ -16,17 +16,17 @@ f11 LONGTEXT    UNICODE
 SELECT * FROM information_schema.columns
 WHERE table_schema LIKE 'test%'
 ORDER BY table_schema, table_name, column_name;
-TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT
-def	test	t1	f1	1	NULL	YES	char	1	2	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(1)			select,insert,update,references	
-def	test	t1	f10	9	NULL	YES	mediumtext	8388607	16777215	NULL	NULL	NULL	ucs2	ucs2_general_ci	mediumtext			select,insert,update,references	
-def	test	t1	f11	10	NULL	YES	longtext	2147483647	4294967295	NULL	NULL	NULL	ucs2	ucs2_general_ci	longtext			select,insert,update,references	
-def	test	t1	f2	2	NULL	YES	char	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(0)			select,insert,update,references	
-def	test	t1	f3	3	NULL	YES	char	10	20	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(10)			select,insert,update,references	
-def	test	t1	f5	4	NULL	YES	varchar	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(0)			select,insert,update,references	
-def	test	t1	f6	5	NULL	YES	varchar	255	510	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(255)			select,insert,update,references	
-def	test	t1	f7	6	NULL	YES	varchar	260	520	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(260)			select,insert,update,references	
-def	test	t1	f8	7	NULL	YES	text	32767	65535	NULL	NULL	NULL	ucs2	ucs2_general_ci	text			select,insert,update,references	
-def	test	t1	f9	8	NULL	YES	tinytext	127	255	NULL	NULL	NULL	ucs2	ucs2_general_ci	tinytext			select,insert,update,references	
+TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT	IS_GENERATED	GENERATION_EXPRESSION
+def	test	t1	f1	1	NULL	YES	char	1	2	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(1)			select,insert,update,references		NEVER	NULL
+def	test	t1	f10	9	NULL	YES	mediumtext	8388607	16777215	NULL	NULL	NULL	ucs2	ucs2_general_ci	mediumtext			select,insert,update,references		NEVER	NULL
+def	test	t1	f11	10	NULL	YES	longtext	2147483647	4294967295	NULL	NULL	NULL	ucs2	ucs2_general_ci	longtext			select,insert,update,references		NEVER	NULL
+def	test	t1	f2	2	NULL	YES	char	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(0)			select,insert,update,references		NEVER	NULL
+def	test	t1	f3	3	NULL	YES	char	10	20	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(10)			select,insert,update,references		NEVER	NULL
+def	test	t1	f5	4	NULL	YES	varchar	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(0)			select,insert,update,references		NEVER	NULL
+def	test	t1	f6	5	NULL	YES	varchar	255	510	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(255)			select,insert,update,references		NEVER	NULL
+def	test	t1	f7	6	NULL	YES	varchar	260	520	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(260)			select,insert,update,references		NEVER	NULL
+def	test	t1	f8	7	NULL	YES	text	32767	65535	NULL	NULL	NULL	ucs2	ucs2_general_ci	text			select,insert,update,references		NEVER	NULL
+def	test	t1	f9	8	NULL	YES	tinytext	127	255	NULL	NULL	NULL	ucs2	ucs2_general_ci	tinytext			select,insert,update,references		NEVER	NULL
 ##########################################################################
 # Show the quotient of CHARACTER_OCTET_LENGTH and CHARACTER_MAXIMUM_LENGTH
 ##########################################################################

mysql-test/suite/funcs_1/r/is_cml_memory.result
@@ -13,13 +13,13 @@ f7 VARCHAR(260) UNICODE
 SELECT * FROM information_schema.columns
 WHERE table_schema LIKE 'test%'
 ORDER BY table_schema, table_name, column_name;
-TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT
-def	test	t1	f1	1	NULL	YES	char	1	2	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(1)			select,insert,update,references	
-def	test	t1	f2	2	NULL	YES	char	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(0)			select,insert,update,references	
-def	test	t1	f3	3	NULL	YES	char	10	20	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(10)			select,insert,update,references	
-def	test	t1	f5	4	NULL	YES	varchar	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(0)			select,insert,update,references	
-def	test	t1	f6	5	NULL	YES	varchar	255	510	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(255)			select,insert,update,references	
-def	test	t1	f7	6	NULL	YES	varchar	260	520	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(260)			select,insert,update,references	
+TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT	IS_GENERATED	GENERATION_EXPRESSION
+def	test	t1	f1	1	NULL	YES	char	1	2	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(1)			select,insert,update,references		NEVER	NULL
+def	test	t1	f2	2	NULL	YES	char	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(0)			select,insert,update,references		NEVER	NULL
+def	test	t1	f3	3	NULL	YES	char	10	20	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(10)			select,insert,update,references		NEVER	NULL
+def	test	t1	f5	4	NULL	YES	varchar	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(0)			select,insert,update,references		NEVER	NULL
+def	test	t1	f6	5	NULL	YES	varchar	255	510	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(255)			select,insert,update,references		NEVER	NULL
+def	test	t1	f7	6	NULL	YES	varchar	260	520	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(260)			select,insert,update,references		NEVER	NULL
 ##########################################################################
 # Show the quotient of CHARACTER_OCTET_LENGTH and CHARACTER_MAXIMUM_LENGTH
 ##########################################################################

mysql-test/suite/funcs_1/r/is_cml_myisam.result
@@ -17,17 +17,17 @@ f11 LONGTEXT    UNICODE
 SELECT * FROM information_schema.columns
 WHERE table_schema LIKE 'test%'
 ORDER BY table_schema, table_name, column_name;
-TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT
-def	test	t1	f1	1	NULL	YES	char	1	2	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(1)			select,insert,update,references	
-def	test	t1	f10	9	NULL	YES	mediumtext	8388607	16777215	NULL	NULL	NULL	ucs2	ucs2_general_ci	mediumtext			select,insert,update,references	
-def	test	t1	f11	10	NULL	YES	longtext	2147483647	4294967295	NULL	NULL	NULL	ucs2	ucs2_general_ci	longtext			select,insert,update,references	
-def	test	t1	f2	2	NULL	YES	char	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(0)			select,insert,update,references	
-def	test	t1	f3	3	NULL	YES	char	10	20	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(10)			select,insert,update,references	
-def	test	t1	f5	4	NULL	YES	varchar	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(0)			select,insert,update,references	
-def	test	t1	f6	5	NULL	YES	varchar	255	510	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(255)			select,insert,update,references	
-def	test	t1	f7	6	NULL	YES	varchar	260	520	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(260)			select,insert,update,references	
-def	test	t1	f8	7	NULL	YES	text	32767	65535	NULL	NULL	NULL	ucs2	ucs2_general_ci	text			select,insert,update,references	
-def	test	t1	f9	8	NULL	YES	tinytext	127	255	NULL	NULL	NULL	ucs2	ucs2_general_ci	tinytext			select,insert,update,references	
+TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION	COLUMN_DEFAULT	IS_NULLABLE	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	CHARACTER_OCTET_LENGTH	NUMERIC_PRECISION	NUMERIC_SCALE	DATETIME_PRECISION	CHARACTER_SET_NAME	COLLATION_NAME	COLUMN_TYPE	COLUMN_KEY	EXTRA	PRIVILEGES	COLUMN_COMMENT	IS_GENERATED	GENERATION_EXPRESSION
+def	test	t1	f1	1	NULL	YES	char	1	2	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(1)			select,insert,update,references		NEVER	NULL
+def	test	t1	f10	9	NULL	YES	mediumtext	8388607	16777215	NULL	NULL	NULL	ucs2	ucs2_general_ci	mediumtext			select,insert,update,references		NEVER	NULL
+def	test	t1	f11	10	NULL	YES	longtext	2147483647	4294967295	NULL	NULL	NULL	ucs2	ucs2_general_ci	longtext			select,insert,update,references		NEVER	NULL
+def	test	t1	f2	2	NULL	YES	char	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(0)			select,insert,update,references		NEVER	NULL
+def	test	t1	f3	3	NULL	YES	char	10	20	NULL	NULL	NULL	ucs2	ucs2_general_ci	char(10)			select,insert,update,references		NEVER	NULL
+def	test	t1	f5	4	NULL	YES	varchar	0	0	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(0)			select,insert,update,references		NEVER	NULL
+def	test	t1	f6	5	NULL	YES	varchar	255	510	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(255)			select,insert,update,references		NEVER	NULL
+def	test	t1	f7	6	NULL	YES	varchar	260	520	NULL	NULL	NULL	ucs2	ucs2_general_ci	varchar(260)			select,insert,update,references		NEVER	NULL
+def	test	t1	f8	7	NULL	YES	text	32767	65535	NULL	NULL	NULL	ucs2	ucs2_general_ci	text			select,insert,update,references		NEVER	NULL
+def	test	t1	f9	8	NULL	YES	tinytext	127	255	NULL	NULL	NULL	ucs2	ucs2_general_ci	tinytext			select,insert,update,references		NEVER	NULL
 ##########################################################################
 # Show the quotient of CHARACTER_OCTET_LENGTH and CHARACTER_MAXIMUM_LENGTH
 ##########################################################################

commit 6fbcf4134af1f0df21fbcc06870f50b6cb2d3c41

properly close the table in fill_schema_table_from_frm()
this closes the memory leak, that started to appear after da5c3e03f62

sql/sql_show.cc
@@ -4667,7 +4667,7 @@ static int fill_schema_table_from_frm(THD *thd, TABLE *table,
     table_list.view= (LEX*) share->is_view;
     res= schema_table->process_table(thd, &table_list, table,
                                      res, db_name, table_name);
-    free_root(&tbl.mem_root, MYF(0));
+    closefrm(&tbl);
   }
 
 