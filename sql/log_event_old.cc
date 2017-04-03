2e2e0d9105bb8cde5ebebfaca412e98ae5f262ef
Sachin Setiya <sachinsetia1001@gmail.com>
2017-04-03 11:55:51 +0530

    MDEV-12019 FLASHBACK: Server crashes in bitmap_bits_set / pack_row / ...
    Reason for this crash is that table->rpl_write_set is NULL. In
    Rows_log_event::do_apply_event we set table->rpl_write_set equal to
    table->write_set. But we do not set table->rpl_write_set in
    Old_rows_log_event::do_apply_event.

--- sql/log_event_old.cc
+++ sql/log_event_old.cc
@@ -221,6 +221,8 @@ Old_rows_log_event::do_apply_event(Old_rows_log_event *ev, rpl_group_info *rgi)
     /* A small test to verify that objects have consistent types */
     DBUG_ASSERT(sizeof(ev_thd->variables.option_bits) == sizeof(OPTION_RELAXED_UNIQUE_CHECKS));
 
+    table->rpl_write_set= table->write_set;
+
     error= do_before_row_operations(table);
     while (error == 0 && row_start < ev->m_rows_end)
     {
