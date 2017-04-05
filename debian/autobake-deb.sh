75b8fe4f18c6e15196acd6f45b38bdce56ced919
Vicențiu Ciorbaru <vicentiu@mariadb.org>
2017-04-05 15:58:24 +0300

    Update debian packaging to work in 10.3
    * Rename all files to use 10.3 in the filename
    * Adjust rules to delete extra lines from control file
    * Adjust control file to have correct Replaces/Breaks list for each
      package
    * Add libcurl3-dev as build dependency for mariadb server in debian

--- debian/autobake-deb.sh
+++ debian/autobake-deb.sh
@@ -24,8 +24,8 @@ then
   sed -i -e '/Add support for verbose builds/,+2d' debian/rules
 
   # Don't include test suite package on Travis-CI to make the build time shorter
-  sed '/Package: mariadb-test-data/,+26d' -i debian/control
-  sed '/Package: mariadb-test/,+34d' -i debian/control
+  sed '/Package: mariadb-test-data/,+28d' -i debian/control
+  sed '/Package: mariadb-test/,+36d' -i debian/control
 fi
 
 
@@ -67,9 +67,9 @@ then
   sed 's/ --with systemd//' -i debian/rules
   sed '/systemd/d' -i debian/rules
   sed '/\.service/d' -i debian/rules
-  sed '/galera_new_cluster/d' -i debian/mariadb-server-10.2.install
-  sed '/galera_recovery/d' -i debian/mariadb-server-10.2.install
-  sed '/mariadb-service-convert/d' -i debian/mariadb-server-10.2.install
+  sed '/galera_new_cluster/d' -i debian/mariadb-server-10.3.install
+  sed '/galera_recovery/d' -i debian/mariadb-server-10.3.install
+  sed '/mariadb-service-convert/d' -i debian/mariadb-server-10.3.install
 fi
 
 # Convert gcc version to numberical value. Format is Mmmpp where M is Major
