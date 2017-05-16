40c7778e05120fe7972fdfea15fb2db5b98579c6
Vladislav Vaintroub <wlad@mariadb.com>
2017-05-16 17:11:25 +0000

    MDEV-12814  mariabackup : don't try io throttling in copy-back
    Throttling only works with when creating backup.  Attempt to use it with
     --copy-back results in crash, since throttle events are not initialized.
    Thus, ignore throttling unless --backup is given.

--- extra/mariabackup/xtrabackup.cc
+++ extra/mariabackup/xtrabackup.cc
@@ -2169,7 +2169,7 @@ xb_write_delta_metadata(const char *filename, const xb_delta_info_t *info)
 void
 xtrabackup_io_throttling(void)
 {
-	if (xtrabackup_throttle && (io_ticket--) < 0) {
+	if (xtrabackup_backup && xtrabackup_throttle && (io_ticket--) < 0) {
 		os_event_reset(wait_throttle);
 		os_event_wait(wait_throttle);
 	}
