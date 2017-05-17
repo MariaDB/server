0e3ca225ad65f14da79c9a73aeadcc2a3e1c0c2a
Vicențiu Ciorbaru <vicentiu@mariadb.org>
2017-05-17 22:09:58 +0300

    Change lower_case_file_system definition to feature MYSQL_PLUGIN_IMPORT

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -106,7 +106,7 @@ extern ulong slave_run_triggers_for_rbr;
 #endif //RBR_TRIGGERS
 extern ulonglong slave_type_conversions_options;
 extern my_bool read_only, opt_readonly;
-extern my_bool lower_case_file_system;
+extern MYSQL_PLUGIN_IMPORT my_bool lower_case_file_system;
 extern my_bool opt_enable_named_pipe, opt_sync_frm, opt_allow_suspicious_udfs;
 extern my_bool opt_secure_auth;
 extern char* opt_secure_file_priv;
