c661b4d0fb38c103fc0b659a95e8552ea72574cf
Sergei Golubchik <serg@mariadb.org>
2017-06-14 00:48:34 +0200

    MDEV-13017 LOCK TABLE fails with irrelevant error while working with tables affected by ANSI_QUOTES

--- sql/sql_partition.cc
+++ sql/sql_partition.cc
@@ -1918,9 +1918,12 @@ static int add_name_string(File fptr, const char *name)
   int err;
   String name_string("", 0, system_charset_info);
   THD *thd= current_thd;
+  ulonglong save_sql_mode= thd->variables.sql_mode;
+  thd->variables.sql_mode&= ~MODE_ANSI_QUOTES;
   ulonglong save_options= thd->variables.option_bits;
   thd->variables.option_bits&= ~OPTION_QUOTE_SHOW_CREATE;
   append_identifier(thd, &name_string, name, strlen(name));
+  thd->variables.sql_mode= save_sql_mode;
   thd->variables.option_bits= save_options;
   err= add_string_object(fptr, &name_string);
   return err;
