918e47030b49867d449bd9aaa54257616d5ebe42
Sergei Golubchik <serg@mariadb.org>
2017-06-14 11:30:32 +0200

    MDEV-13063 Server crashes in intern_plugin_lock or assertion `plugin_ptr->ref_count == 1' fails in plugin_init
    if a forced plugin is disabled (for any reason, e.g. invalid command-line
    option) - it's an error

--- sql/sql_plugin.cc
+++ sql/sql_plugin.cc
@@ -1359,7 +1359,7 @@ static int plugin_initialize(MEM_ROOT *tmp_root, struct st_plugin_int *plugin,
 
   if (options_only || state == PLUGIN_IS_DISABLED)
   {
-    ret= 0;
+    ret= !options_only && plugin_is_forced(plugin);
     goto err;
   }
 
