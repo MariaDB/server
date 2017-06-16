9b7afd4188dfe5db64fbf8a58360b09f90f77fed
Oleksandr Byelkin <sanja@mariadb.com>
2017-06-16 16:24:36 +0200

    MDEV-12819 order by ordering expression changed to empty string when creatin view with union
    prepare of "fake_select" for union made in JOIN::prepare only if
    we do not execute it then before reset, i.e it was for PS prepare
    and now required for CREATE VIEW to make global ORDER BY which
    belongs to "fake_select" prepared.

--- sql/sql_union.cc
+++ sql/sql_union.cc
@@ -618,7 +618,9 @@ bool st_select_lex_unit::prepare(THD *thd_arg, select_result *sel_result,
       if (saved_error)
         goto err;
 
-      if (fake_select_lex != NULL && thd->stmt_arena->is_stmt_prepare())
+      if (fake_select_lex != NULL &&
+          (thd->stmt_arena->is_stmt_prepare() ||
+           (thd->lex->context_analysis_only & CONTEXT_ANALYSIS_ONLY_VIEW)))
       {
         /* Validate the global parameters of this union */
 
