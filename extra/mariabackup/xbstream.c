ba9daddcd831e04be59bf1d60cb666624642f07c
Vladislav Vaintroub <wlad@mariadb.com>
2017-06-20 20:02:20 +0000

    Cherrypick Perconas fix for leaking descriptors with new xbstream.
    https://github.com/percona/percona-xtrabackup/pull/368/commits/72c55f4fc2ba9ede7b960eceb01261ccc339c041

--- extra/mariabackup/xbstream.c
+++ extra/mariabackup/xbstream.c
@@ -444,7 +444,11 @@ extract_worker_thread_func(void *arg)
 		}
 
 		if (chunk.type == XB_CHUNK_TYPE_EOF) {
+			pthread_mutex_lock(ctxt->mutex);
 			pthread_mutex_unlock(&entry->mutex);
+			my_hash_delete(ctxt->filehash, (uchar *) entry);
+			pthread_mutex_unlock(ctxt->mutex);
+
 			continue;
 		}
 
