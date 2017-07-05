MariaDB/server

commit bb60a832ede139d24a2470a1b1217d30799d726e

Minor cleanup of InnoDB shutdown
os_thread_active(): Remove.

srv_shutdown_all_bg_threads(): Assert that high-level threads
have already exited. Do not sleep if os_thread_count=0.

storage/innobase/srv/srv0start.cc
@@ -1234,6 +1234,7 @@ static
 void
 srv_shutdown_all_bg_threads()
 {
+	ut_ad(!srv_undo_sources);
 	srv_shutdown_state = SRV_SHUTDOWN_EXIT_THREADS;
 
 	/* All threads end up waiting for certain events. Put those events
@@ -1285,13 +1286,7 @@ srv_shutdown_all_bg_threads()
 
 			os_event_set(buf_flush_event);
 
-			/* f. dict_stats_thread is signaled from
-			logs_empty_and_mark_files_at_shutdown() and
-			should have already quit or is quitting right
-			now. */
-
 			if (srv_use_mtflush) {
-				/* g. Exit the multi threaded flush threads */
 				buf_mtflu_io_thread_exit();
 			}
 		}
@@ -1300,13 +1295,11 @@ srv_shutdown_all_bg_threads()
 			os_aio_wake_all_threads_at_shutdown();
 		}
 
-		const bool active = os_thread_active();
-
-		os_thread_sleep(100000);
-
-		if (!active) {
+		if (!os_thread_count) {
 			return;
 		}
+
+		os_thread_sleep(100000);
 	}
 
 	ib::warn() << os_thread_count << " threads created by InnoDB"

storage/innobase/include/os0thread.h
@@ -151,10 +151,4 @@ os_thread_sleep(
 /*============*/
 	ulint	tm);	/*!< in: time in microseconds */
 
-/*****************************************************************//**
-Check if there are threads active.
-@return true if the thread count > 0. */
-bool
-os_thread_active();
-
 #endif

storage/innobase/os/os0thread.cc
@@ -237,21 +237,3 @@ os_thread_sleep(
 	select(0, NULL, NULL, NULL, &t);
 #endif /* _WIN32 */
 }
-
-/*****************************************************************//**
-Check if there are threads active.
-@return true if the thread count > 0. */
-bool
-os_thread_active()
-/*==============*/
-{
-	/* All the threads have exited or are just exiting;
-	NOTE that the threads may not have completed their
-	exit yet. Should we use pthread_join() to make sure
-	they have exited? If we did, we would have to
-	remove the pthread_detach() from
-	os_thread_exit().  Now we just sleep 0.1
-	seconds and hope that is enough! */
-
-	return(my_atomic_loadlint(&os_thread_count) > 0);
-}

commit e417af2cb8ee615ba3b0a3f557797271e7bf7c42

MDEV-13143 Server crashes in srv_init_abort_low() when started with inaccessible tmpdir
This is a regression caused by
commit bb60a832ede139d24a2470a1b1217d30799d726e

srv_shutdown_all_bg_threads(): If os_thread_count indicates that
no threads are running, do not bother checking thread status.
This avoids a crash when InnoDB startup is aborted before
os_aio_init() has been invoked. (os_aio_all_slots_free() would
dereference AIO::s_reads even though it is NULL.)

storage/innobase/srv/srv0start.cc
@@ -1302,6 +1302,10 @@ srv_shutdown_all_bg_threads()
 			}
 		}
 
+		if (!os_thread_count) {
+			return;
+		}
+
 		switch (srv_operation) {
 		case SRV_OPERATION_BACKUP:
 			break;
@@ -1311,10 +1315,6 @@ srv_shutdown_all_bg_threads()
 			    && os_aio_all_slots_free()) {
 				os_aio_wake_all_threads_at_shutdown();
 			}
-		}
-
-		if (!os_thread_count) {
-			return;
 		}
 
 		os_thread_sleep(100000);

mysql-test/suite/innodb/r/temporary_table.result
@@ -137,6 +137,8 @@ Tables_in_test
 create temporary table t1 (keyc int, c1 char(100), c2 char(100)) engine = innodb;
 ERROR HY000: Can't create table `test`.`t1` (errno: 165 "Table is read only")
 # test various bad start-up parameters
+FOUND 3 /Can't create/write to file '.dev.null.nonexistent.ib/ in mysqld.1.err
+FOUND 3 /InnoDB: Unable to create temporary file/ in mysqld.1.err
 FOUND 1 /innodb_temporary and innodb_system file names seem to be the same/ in mysqld.1.err
 SELECT * FROM INFORMATION_SCHEMA.ENGINES WHERE engine = 'innodb'
 AND support IN ('YES', 'DEFAULT', 'ENABLED');

mysql-test/suite/innodb/t/temporary_table.test
@@ -10,6 +10,8 @@
 --source include/no_valgrind_without_big.inc
 
 --disable_query_log
+call mtr.add_suppression("Can't create/write to file '.dev.null.nonexistent.ib");
+call mtr.add_suppression("InnoDB: Unable to create temporary file");
 call mtr.add_suppression("last file in setting innodb_temp_data_file_path");
 call mtr.add_suppression("The table 't1' is full");
 call mtr.add_suppression("Plugin 'InnoDB' init function returned error");
@@ -124,6 +126,35 @@ let SEARCH_FILE = $MYSQLTEST_VARDIR/log/mysqld.1.err;
 let SEARCH_ABORT = NOT FOUND;
 let $check_no_innodb=SELECT * FROM INFORMATION_SCHEMA.ENGINES WHERE engine = 'innodb'
 AND support IN ('YES', 'DEFAULT', 'ENABLED');
+
+# We cannot use include/restart_mysqld.inc in this particular test,
+# because SHOW STATUS would fail due to unwritable (nonexistent) tmpdir.
+--source include/shutdown_mysqld.inc
+--exec echo "restart: --tmpdir=/dev/null/nonexistent" > $_expect_file_name
+--enable_reconnect
+--disable_result_log
+--disable_query_log
+let $counter= 5000;
+let $mysql_errno= 9999;
+while ($mysql_errno)
+{
+  --error 0,ER_SERVER_SHUTDOWN,ER_CONNECTION_KILLED,2002,2006,2013
+  select 1;
+
+  dec $counter;
+  if (!$counter)
+  {
+    --die Server failed to restart
+  }
+  --sleep 0.1
+}
+--enable_query_log
+--enable_result_log
+--disable_reconnect
+--let SEARCH_PATTERN= Can't create/write to file '.dev.null.nonexistent.ib
+--source include/search_pattern_in_file.inc
+--let SEARCH_PATTERN= InnoDB: Unable to create temporary file
+--source include/search_pattern_in_file.inc
 
 --let $restart_parameters= --innodb_data_file_path=ibdata1:12M:autoextend --innodb_temp_data_file_path=ibdata1:12M:autoextend
 --source include/restart_mysqld.inc