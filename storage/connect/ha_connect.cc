2ce3f95151a424f950be40140c45b0ba583e07ab
Sergei Golubchik <serg@mariadb.org>
2017-07-22 13:10:10 +0200

    Connect engine is Gamma

--- storage/connect/ha_connect.cc
+++ storage/connect/ha_connect.cc
@@ -7083,6 +7083,6 @@ maria_declare_plugin(connect)
   NULL,                                         /* status variables */
   connect_system_variables,                     /* system variables */
   "1.06.0001",                                  /* string version */
-  MariaDB_PLUGIN_MATURITY_BETA                  /* maturity */
+  MariaDB_PLUGIN_MATURITY_GAMMA                 /* maturity */
 }
 maria_declare_plugin_end;
