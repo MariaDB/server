f3435fc37e7e0cfd5384c609d16b0d6cc3048454
sjaakola <seppo.jaakola@iki.fi>
2017-07-25 10:56:44 +0300

    MW-394
    * ported the partition.test from MariaDB 10.3 code tree
    * fix to TOI replicate ALTER TABLE EXCHANGE PARTITION statement

--- sql/sql_partition_admin.cc
+++ sql/sql_partition_admin.cc
@@ -89,9 +89,15 @@ bool Sql_cmd_alter_table_exchange_partition::execute(THD *thd)
 
   /* Not allowed with EXCHANGE PARTITION */
   DBUG_ASSERT(!create_info.data_file_name && !create_info.index_file_name);
+  WSREP_TO_ISOLATION_BEGIN_WRTCHK(NULL, NULL, first_table);
 
   thd->enable_slow_log= opt_log_slow_admin_statements;
   DBUG_RETURN(exchange_partition(thd, first_table, &alter_info));
+#ifdef WITH_WSREP
+ error:
+  /* handle errors in TO_ISOLATION here */
+  DBUG_RETURN(true);
+#endif /* WITH_WSREP */
 }
 
 
