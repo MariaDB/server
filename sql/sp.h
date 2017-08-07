cf9e0bf3e6d266f9b56fd13ceef82d66b1d92a46
Monty <monty@mariadb.org>
2017-08-07 20:43:44 +0300

    Fixed compiler warning

--- sql/sp.h
+++ sql/sp.h
@@ -81,6 +81,7 @@ class Sp_handler
                                      const Database_qualified_name *name,
                                      sp_head *sp) const;
 public:
+  virtual ~Sp_handler() {}
   static const Sp_handler *handler(enum enum_sql_command cmd);
   static const Sp_handler *handler(stored_procedure_type type);
   static const Sp_handler *handler(MDL_key::enum_mdl_namespace ns);
