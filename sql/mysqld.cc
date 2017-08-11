34319403a510b92317d7bc8993f0d675e4841757
Sergei Golubchik <serg@mariadb.org>
2017-08-11 16:17:08 +0200

    fix a comment

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -1691,7 +1691,7 @@ static void close_connections(void)
   {
     DBUG_PRINT("quit",("Informing thread %ld that it's time to die",
 		       (ulong) tmp->thread_id));
-    /* We skip slave threads & scheduler on this first loop through. */
+    /* We skip slave threads on this first loop through. */
     if (tmp->slave_thread)
       continue;
 
