a23a19989714606904bc9461287fe3f91187da8b
sjaakola <seppo.jaakola@iki.fi>
2017-08-24 10:34:21 +0300

    MW-402 cascading FK issues
    * created tests focusing in multi-master conflicts during cascading foreign key
      processing
    * in row0upd.cc, calling wsrep_row_ups_check_foreign_constraints only when
      running in cluster
    * in row0ins.cc fixed regression from MW-369, which caused crash with MW-402.test

--- storage/innobase/row/row0upd.cc
+++ storage/innobase/row/row0upd.cc
@@ -2682,6 +2682,9 @@ row_upd_clust_rec_by_insert(
 	rec_t*		rec;
 	ulint*		offsets			= NULL;
 
+#ifdef WITH_WSREP
+	que_node_t *parent = que_node_get_parent(node);
+#endif /* WITH_WSREP */
 	ut_ad(node);
 	ut_ad(dict_index_is_clust(index));
 
@@ -2769,6 +2772,7 @@ check_fk:
 			if (err != DB_SUCCESS) {
 				goto err_exit;
 			}
+		}
 #ifdef WITH_WSREP
 		} else if (foreign && wsrep_must_process_fk(node, trx)) {
 			err = wsrep_row_upd_check_foreign_constraints(
@@ -2792,8 +2796,8 @@ check_fk:
 					    << " table " << index->table->name;
 				goto err_exit;
 			}
-#endif /* WITH_WSREP */
 		}
+#endif /* WITH_WSREP */
 	}
 
 	mtr_commit(mtr);
@@ -2996,8 +3000,12 @@ row_upd_del_mark_clust_rec(
 
 		err = row_upd_check_references_constraints(
 			node, pcur, index->table, index, offsets, thr, mtr);
+	}
 #ifdef WITH_WSREP
-	} else if (foreign && wsrep_must_process_fk(node, trx)) {
+	else if (trx && wsrep_on(trx->mysql_thd)  &&  err == DB_SUCCESS  &&
+	    (!parent || (que_node_get_type(parent) != QUE_NODE_UPDATE) ||
+	    ((upd_node_t*)parent)->cascade_upd_nodes->empty())
+	) {
 		err = wsrep_row_upd_check_foreign_constraints(
 			node, pcur, index->table, index, offsets, thr, mtr);
 
