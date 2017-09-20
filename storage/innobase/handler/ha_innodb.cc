71cd7e6fbc46398aab3e5db7d9aacfb25e9ae09e
sjaakola <seppo.jaakola@iki.fi>
2017-09-20 16:53:49 +0300

    Fix after previous cherry picks to make the code build

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -19694,7 +19694,7 @@ wsrep_abort_transaction(
 		WSREP_DEBUG("abort transaction: BF: %s victim: %s conf: %d",
 			wsrep_thd_query(bf_thd),
 			wsrep_thd_query(victim_thd),
-			wsrep_thd_conflict_state(victim_thd));
+                            wsrep_thd_conflict_state(victim_thd, FALSE));
 		wsrep_thd_UNLOCK(victim_thd);
 	}
 
