f2a0fa8685a5bab0a13d5ac720a68eecf3541dc9
Jan Lindström <jan.lindstrom@mariadb.com>
2017-10-10 16:53:15 +0300

    Fix test failure on galera.view caused by incorrect location
    of WSREP_TO_ISOLATION_BEGIN.

--- sql/sql_view.cc
+++ sql/sql_view.cc
@@ -426,10 +426,11 @@ bool mysql_create_view(THD *thd, TABLE_LIST *views,
 
   if ((res= create_view_precheck(thd, tables, view, mode)))
     goto err;
-  WSREP_TO_ISOLATION_BEGIN(WSREP_MYSQL_DB, NULL, NULL)
 
   lex->link_first_table_back(view, link_to_local);
   view->open_type= OT_BASE_ONLY;
+
+  WSREP_TO_ISOLATION_BEGIN(WSREP_MYSQL_DB, NULL, NULL)
 
   if (check_dependencies_in_with_clauses(lex->with_clauses_list))
   {
