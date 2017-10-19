7c6da26a1eec8cae38e8cd3308563b1aa261f3dc
Eugene Kosov <claprix@yandex.ru>
2017-10-19 20:13:59 +0300

    build fix

--- include/my_sys.h
+++ include/my_sys.h
@@ -716,8 +716,6 @@ extern int my_sync_dir(const char *dir_name, myf my_flags);
 extern int my_sync_dir_by_file(const char *file_name, myf my_flags);
 extern const char *my_get_err_msg(uint nr);
 extern void my_error_as(uint nr1, uint nr2, myf MyFlags, ...);
-extern void my_printv_error(uint error, const char *format, myf MyFlags,
-                            va_list ap);
 extern int my_error_register(const char** (*get_errmsgs) (int nr),
                              uint first, uint last);
 extern my_bool my_error_unregister(uint first, uint last);
