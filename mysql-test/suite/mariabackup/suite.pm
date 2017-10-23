433050562965d83228bcab0d39c8ec64058fe8e3
Vladislav Vaintroub <wlad@mariadb.com>
2017-10-23 14:17:50 +0000

    Do not use File::Which, it is not always available.

--- mysql-test/suite/mariabackup/suite.pm
+++ mysql-test/suite/mariabackup/suite.pm
@@ -3,7 +3,6 @@ package My::Suite::MariaBackup;
 @ISA = qw(My::Suite);
 use My::Find;
 use File::Basename;
-use File::Which;
 use strict;
 
 return "Not run for embedded server" if $::opt_embedded_server;
@@ -24,10 +23,13 @@ $ENV{XBSTREAM}= ::mtr_exe_maybe_exists(
 
 $ENV{INNOBACKUPEX}= "$mariabackup_exe --innobackupex";
 
+my $have_qpress = index(`qpress 2>&1`,"Compression") > 0;
+
+
 sub skip_combinations {
   my %skip;
   $skip{'include/have_file_key_management.inc'} = 'needs file_key_management plugin'  unless $ENV{FILE_KEY_MANAGEMENT_SO};
-  $skip{'compress_qpress.test'}= 'needs qpress executable in PATH' unless which 'qpress';
+  $skip{'compress_qpress.test'}= 'needs qpress executable in PATH' unless $have_qpress;
   %skip;
 }
 
