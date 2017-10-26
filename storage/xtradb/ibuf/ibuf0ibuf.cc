b933a8c354159c9a22c76b5d6f430a76e8a920cd
Marko Mäkelä <marko.makela@mariadb.com>
2017-10-26 13:29:28 +0300

    MDEV-12569 InnoDB suggests filing bugs at MySQL bug tracker
    Replace all references in InnoDB and XtraDB error log messages
    to bugs.mysql.com with references to https://jira.mariadb.org/.
    
    The original merge
    commit 4274d0bf578aeac9780ffd9ab06aebba48a0fac9
    was accidentally reverted by the subsequent merge
    commit 3b35d745c3245f63fcc9757bfa90392bf923fcc3

--- storage/xtradb/ibuf/ibuf0ibuf.cc
+++ storage/xtradb/ibuf/ibuf0ibuf.cc
@@ -4014,7 +4014,7 @@ ibuf_insert_to_index_page_low(
 		(ulong) zip_size, (ulong) old_bits);
 
 	fputs("InnoDB: Submit a detailed bug report"
-	      " to http://bugs.mysql.com\n", stderr);
+	      " to https://jira.mariadb.org/\n", stderr);
 	ut_ad(0);
 	DBUG_RETURN(NULL);
 }
@@ -4087,7 +4087,7 @@ dump:
 		      " Please run CHECK TABLE on\n"
 		      "InnoDB: your tables.\n"
 		      "InnoDB: Submit a detailed bug report to"
-		      " http://bugs.mysql.com!\n", stderr);
+		      " https://jira.mariadb.org/\n", stderr);
 
 		DBUG_VOID_RETURN;
 	}
@@ -4263,7 +4263,7 @@ ibuf_set_del_mark(
 		fprintf(stderr, "\nspace %u offset %u"
 			" (%u records, index id %llu)\n"
 			"InnoDB: Submit a detailed bug report"
-			" to http://bugs.mysql.com\n",
+			" to https://jira.mariadb.org/\n",
 			(unsigned) buf_block_get_space(block),
 			(unsigned) buf_block_get_page_no(block),
 			(unsigned) page_get_n_recs(page),
@@ -4327,7 +4327,7 @@ ibuf_delete(
 			fprintf(stderr, "\nspace %u offset %u"
 				" (%u records, index id %llu)\n"
 				"InnoDB: Submit a detailed bug report"
-				" to http://bugs.mysql.com\n",
+				" to https://jira.mariadb.org/\n",
 				(unsigned) buf_block_get_space(block),
 				(unsigned) buf_block_get_page_no(block),
 				(unsigned) page_get_n_recs(page),
@@ -4398,7 +4398,7 @@ ibuf_restore_pos(
 	} else {
 		fprintf(stderr,
 			"InnoDB: ERROR: Submit the output to"
-			" http://bugs.mysql.com\n"
+			" https://jira.mariadb.org/\n"
 			"InnoDB: ibuf cursor restoration fails!\n"
 			"InnoDB: ibuf record inserted to page %lu:%lu\n",
 			(ulong) space, (ulong) page_no);
@@ -4707,7 +4707,7 @@ ibuf_merge_or_delete_for_page(
 				"InnoDB: to determine if they are corrupt"
 				" after this.\n\n"
 				"InnoDB: Please submit a detailed bug report"
-				" to http://bugs.mysql.com\n\n",
+				" to https://jira.mariadb.org/\n\n",
 				(ulong) page_no,
 				(ulong)
 				fil_page_get_type(block->frame));
