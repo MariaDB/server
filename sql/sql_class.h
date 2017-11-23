e2d65228475b764349a5ca255c7baf8b0894bacd
sjaakola <seppo.jaakola@iki.fi>
2017-11-23 16:05:35 +0200

    Fixes for making native mtr tests to pass
    - added missing break; in parser code
    - added missing 10.3 style test case checking in mtr

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -4255,10 +4255,10 @@ public:
     query_id= new_query_id;
 #ifdef WITH_WSREP
     if (WSREP(this) && wsrep_next_trx_id() == WSREP_UNDEFINED_TRX_ID)
-  {
-    set_wsrep_next_trx_id(query_id);
-    WSREP_DEBUG("assigned new next trx id: %lu", wsrep_next_trx_id());
-  }
+    {
+      set_wsrep_next_trx_id(query_id);
+      WSREP_DEBUG("assigned new next trx id: %lu", wsrep_next_trx_id());
+    }
 #endif /* WITH_WSREP */
   }
   void set_open_tables(TABLE *open_tables_arg)
