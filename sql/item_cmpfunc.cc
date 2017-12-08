20089f5a39fa8f6f08113f12ed87c34e844c4fc6
Alexander Barkov <bar@mariadb.org>
2017-12-08 14:40:27 +0400

    MDEV-14596 Crash in INTERVAL(ROW(..),ROW(..))

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -2001,6 +2001,19 @@ bool Item_func_opt_neg::eq(const Item *item, bool binary_cmp) const
 }
 
 
+bool Item_func_interval::fix_fields(THD *thd, Item **ref)
+{
+  if (Item_int_func::fix_fields(thd, ref))
+    return true;
+  for (uint i= 0 ; i < row->cols(); i++)
+  {
+    if (row->element_index(i)->check_cols(1))
+      return true;
+  }
+  return false;
+}
+
+
 void Item_func_interval::fix_length_and_dec()
 {
   uint rows= row->cols();
