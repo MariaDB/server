656f66def27b7a2cf42a28f873f1eeef0416aa71
Marko Mäkelä <marko.makela@mariadb.com>
2018-01-17 15:53:30 +0200

    Follow-up fix to MDEV-14585 Automatically remove #sql- tables in InnoDB dictionary during recovery
    If InnoDB is killed while ALTER TABLE...ALGORITHM=COPY is in progress,
    after recovery there could be undo log records some records that were
    inserted into an intermediate copy of the table. Due to these undo log
    records, InnoDB would resurrect locks at recovery, and the intermediate
    table would be locked while we are trying to drop it. This would cause
    a call to row_rename_table_for_mysql(), either from
    row_mysql_drop_garbage_tables() or from the rollback of a RENAME
    operation that was part of the ALTER TABLE.
    
    row_rename_table_for_mysql(): Do not attempt to parse FOREIGN KEY
    constraints when renaming from #sql-something to #sql-something-else,
    because it does not make any sense.
    
    row_drop_table_for_mysql(): When deferring DROP TABLE due to locks,
    do not rename the table if its name already starts with the #sql-
    prefix, which is what row_mysql_drop_garbage_tables() uses.
    Previously, the too strict prefix #sql-ib was used, and some
    tables were renamed unnecessarily.

--- storage/innobase/row/row0mysql.cc
+++ storage/innobase/row/row0mysql.cc
@@ -3712,7 +3712,7 @@ row_drop_table_for_mysql(
 
 	if (table->n_foreign_key_checks_running > 0) {
 defer:
-		if (!strstr(table->name.m_name, "/" TEMP_FILE_PREFIX_INNODB)) {
+		if (!strstr(table->name.m_name, "/" TEMP_FILE_PREFIX)) {
 			heap = mem_heap_create(FN_REFLEN);
 			const char* tmp_name
 				= dict_mem_create_temporary_tablename(
@@ -4477,7 +4477,7 @@ row_rename_table_for_mysql(
 
 		goto funct_exit;
 
-	} else if (new_is_tmp) {
+	} else if (!old_is_tmp && new_is_tmp) {
 		/* MySQL is doing an ALTER TABLE command and it renames the
 		original table to a temporary table name. We want to preserve
 		the original foreign key constraint definitions despite the
