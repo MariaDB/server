ded07724eebb6c3afe882884fbad32e8dc907b0c
Vicențiu Ciorbaru <vicentiu@mariadb.org>
2018-01-29 19:46:59 +0200

    MDEV-15014 Assertion `m_cache_lock_status == LOCKED_NO_WAIT || m_cache_status == DISABLE_REQUEST' failed in Query_cache::free_cache on startup
    The assert guards against not-locked or not-requested query cache
    disabling. If during startup we disable query cache, we failed to
    request disabling.

--- sql/sql_cache.cc
+++ sql/sql_cache.cc
@@ -2477,6 +2477,7 @@ void Query_cache::init()
   */
   if (global_system_variables.query_cache_type == 0)
   {
+    m_cache_status= DISABLE_REQUEST;
     free_cache();
     m_cache_status= DISABLED;
   }
