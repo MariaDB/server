867d8680b2fd5bb9169d14ef0352f70cdc410262
Vladislav Vaintroub <wlad@mariadb.com>
2018-02-01 09:26:03 +0000

    Fix sql_print_warning formatting

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -2054,7 +2054,7 @@ extern "C" sig_handler print_signal_warning(int sig)
 {
   if (global_system_variables.log_warnings)
     sql_print_warning("Got signal %d from thread %u", sig,
-                      my_thread_id());
+                      (uint)my_thread_id());
 #ifdef SIGNAL_HANDLER_RESET_ON_DELIVERY
   my_sigset(sig,print_signal_warning);		/* int. thread system calls */
 #endif
