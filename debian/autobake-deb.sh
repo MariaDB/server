699123fdc3b770451771d9e976bb2728c1db1b5b
Vicentiu Ciorbaru <vicentiu@mariadb.org>
2018-02-07 11:13:29 +0000

    Fix rocksdb compiler version identification with GCC
    GCC 7 with only dumpversion flag returns the major version only.
    dumpfullversion flag makes it work with both new and old gcc.

--- debian/autobake-deb.sh
+++ debian/autobake-deb.sh
@@ -74,7 +74,10 @@ fi
 
 # Convert gcc version to numberical value. Format is Mmmpp where M is Major
 # version, mm is minor version and p is patch.
-GCCVERSION=$(gcc -dumpversion | sed -e 's/\.\([0-9][0-9]\)/\1/g' -e 's/\.\([0-9]\)/0\1/g' -e 's/^[0-9]\{3,4\}$/&00/')
+# -dumpfullversion & -dumpversion to make it uniform across old and new (>=7)
+GCCVERSION=$(gcc -dumpfullversion -dumpversion | sed -e 's/\.\([0-9][0-9]\)/\1/g' \
+                                                     -e 's/\.\([0-9]\)/0\1/g'     \
+						     -e 's/^[0-9]\{3,4\}$/&00/')
 # Don't build rocksdb package if gcc version is less than 4.8 or we are running on
 # x86 32 bit.
 if [[ $GCCVERSION -lt 40800 ]] || [[ $(arch) =~ i[346]86 ]]
