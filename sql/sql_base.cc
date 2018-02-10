d0f5e56a20097d516737a7e5df41394ed49c8ed9
Sergei Golubchik <serg@mariadb.org>
2018-02-10 20:34:18 +0100

    MDEV-14785 SYSTEM_INVISIBLE behaviour not consistent
    Hide INVISIBLE_SYSTEM columns from writes and from fix_vcol_expr().

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -5648,6 +5648,11 @@ find_field_in_table(THD *thd, TABLE *table, const char *name, size_t length,
     if (field->invisible == INVISIBLE_FULL &&
         DBUG_EVALUATE_IF("test_completely_invisible", 0, 1))
       DBUG_RETURN((Field*)0);
+
+    if (field->invisible == INVISIBLE_SYSTEM &&
+        thd->column_usage != MARK_COLUMNS_READ &&
+        thd->column_usage != COLUMNS_READ)
+      DBUG_RETURN((Field*)0);
   }
   else
   {
