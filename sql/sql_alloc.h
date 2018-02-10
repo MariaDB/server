2232784f0094ae7e81f5033fe8f3499e15224fed
Vladislav Vaintroub <wlad@mariadb.com>
2018-02-10 00:07:39 +0000

    Fix inadverent change in Sql_alloc
    For placement new, throw() exception specification was removed by
    accident in 8fe04a3df37d65254142c6d1297eda32c1013f7f , making  debug
    tests, that simulate out-of-memory situations, fail (e.g filesort_debug)

--- sql/sql_alloc.h
+++ sql/sql_alloc.h
@@ -37,7 +37,7 @@ public:
   }
   static void *operator new[](size_t size, MEM_ROOT *mem_root) throw ()
   { return alloc_root(mem_root, size); }
-  static void *operator new(size_t size, MEM_ROOT *mem_root)
+  static void *operator new(size_t size, MEM_ROOT *mem_root) throw()
   { return alloc_root(mem_root, size); }
   static void operator delete(void *ptr, size_t size) { TRASH(ptr, size); }
   static void operator delete(void *, MEM_ROOT *){}
