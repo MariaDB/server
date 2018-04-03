606e21867c0f5765f61efd2478c9f12cf33da71e
Alexander Barkov <bar@mariadb.com>
2018-04-03 16:28:52 +0400

    MDEV-15630 uuid() function evaluates at wrong time in query

--- sql/item_strfunc.h
+++ sql/item_strfunc.h
@@ -997,6 +997,7 @@ public:
                   DERIVATION_COERCIBLE, MY_REPERTOIRE_ASCII);
     fix_char_length(MY_UUID_STRING_LENGTH);
   }
+  table_map used_tables() const { return RAND_TABLE_BIT; }
   const char *func_name() const{ return "uuid"; }
   String *val_str(String *);
   bool check_vcol_func_processor(uchar *int_arg) 
