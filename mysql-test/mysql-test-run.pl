5441bbd3b1f9e4d0232f8df83a79c51638e51a58
Sergei Golubchik <serg@mariadb.org>
2018-04-18 13:28:22 +0200

    ./mtr --gdb testname
    Make --gdb to take an optional argument *only* if it's
    written after '=', not after a space.
    
    followup for 339b9055791

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -1133,7 +1133,7 @@ sub command_line_setup {
              'debug'                    => \$opt_debug,
              'debug-common'             => \$opt_debug_common,
              'debug-server'             => \$opt_debug_server,
-             'gdb:s'                    => sub { $opt_gdb = $_[1] || '#' },
+             'gdb=s'                    => \$opt_gdb,
              'client-gdb'               => \$opt_client_gdb,
              'manual-gdb'               => \$opt_manual_gdb,
              'manual-lldb'              => \$opt_manual_lldb,
@@ -1228,6 +1228,9 @@ sub command_line_setup {
              'skip-test-list=s'         => \@opt_skip_test_list
            );
 
+  # fix options (that take an optional argument and *only* after = sign
+  my %fixopt = ( '--gdb' => '--gdb=#' );
+  @ARGV = map { $fixopt{$_} or $_ } @ARGV;
   GetOptions(%options) or usage("Can't read options");
   usage("") if $opt_usage;
   list_options(\%options) if $opt_list_options;
