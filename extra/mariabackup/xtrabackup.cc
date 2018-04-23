5b79303b40e31d77f616c2b797f58de41973e6a3
Marko Mäkelä <marko.makela@mariadb.com>
2018-04-23 18:14:15 +0300

    MDEV-15988 Crash in ./mtr mariabackup.undo_space_id
    xb_assign_undo_space_start(): Correctly pass the length of
    the buffer, so that the file name will not be truncated.

--- extra/mariabackup/xtrabackup.cc
+++ extra/mariabackup/xtrabackup.cc
@@ -3107,8 +3107,8 @@ static dberr_t xb_assign_undo_space_start()
 		name[dirnamelen++] = SRV_PATH_SEPARATOR;
 	}
 
-	ut_snprintf(name + dirnamelen, strlen(name) + strlen("ibdata1"),
-		 "%s", "ibdata1");
+	ut_snprintf(name + dirnamelen, (sizeof name) - dirnamelen,
+		    "%s", "ibdata1");
 
 	file = os_file_create(innodb_file_data_key, name, OS_FILE_OPEN,
 			      OS_FILE_NORMAL, OS_DATA_FILE, &ret, 0);
