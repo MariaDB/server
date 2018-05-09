8ba0eea65cd7cc02314a9e1ab954808d16461888
Vladislav Vaintroub <wlad@mariadb.com>
2018-05-09 23:04:18 +0100

    Fix  warning  VS2017 15.7 update.
    This previously unreported warning comes from casting size_t to ulong
    in sql_hset.h in Hash_Set::at().
    
    Change my_hash_element to accept size_t index parameter.

--- include/hash.h
+++ include/hash.h
@@ -73,7 +73,7 @@ my_bool my_hash_init2(HASH *hash, uint growth_size, CHARSET_INFO *charset,
                       uint flags);
 void my_hash_free(HASH *tree);
 void my_hash_reset(HASH *hash);
-uchar *my_hash_element(HASH *hash, ulong idx);
+uchar *my_hash_element(HASH *hash, size_t idx);
 uchar *my_hash_search(const HASH *info, const uchar *key, size_t length);
 uchar *my_hash_search_using_hash_value(const HASH *info,
                                        my_hash_value_type hash_value,
