cff7e2b58367c83f903e52094b0b01928ea71ee0
Daniel Black <daniel@linux.ibm.com>
2018-05-22 07:49:44 +1000

    hash (storage): hp_hashnr is local
    Remove 'register' quantifer from args too.

--- storage/heap/heapdef.h
+++ storage/heap/heapdef.h
@@ -81,7 +81,6 @@ extern uchar *hp_search(HP_INFO *info,HP_KEYDEF *keyinfo,const uchar *key,
 		       uint nextflag);
 extern uchar *hp_search_next(HP_INFO *info, HP_KEYDEF *keyinfo,
 			    const uchar *key, HASH_INFO *pos);
-extern ulong hp_hashnr(HP_KEYDEF *keyinfo,const uchar *key);
 extern ulong hp_rec_hashnr(HP_KEYDEF *keyinfo,const uchar *rec);
 extern void hp_movelink(HASH_INFO *pos,HASH_INFO *next_link,
 			 HASH_INFO *newlink);
