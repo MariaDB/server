9dd860b2c32d951a3ac473e45cda04d3032e4468
Michal Schorm <mschorm@redhat.com>
2018-06-05 17:39:01 +0200

    Fix FSF addresses

--- extra/mariabackup/crc/crc-intel-pclmul.c
+++ extra/mariabackup/crc/crc-intel-pclmul.c
@@ -35,7 +35,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Place - Suite 330, Boston, MA MA 02110-1335, USA
+ * Foundation, Inc., 51 Franklin Place - Suite 330, Boston, MA 02110-1335, USA
  *
  */
 
