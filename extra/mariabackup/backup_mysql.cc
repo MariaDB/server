a0cd5a6443cf6bcc9870a3fa6cbe8108e77e6bcd
Michal Schorm <mschorm@redhat.com>
2018-06-05 17:31:46 +0200

    Fix FSF addresses

--- extra/mariabackup/backup_mysql.cc
+++ extra/mariabackup/backup_mysql.cc
@@ -35,7 +35,7 @@ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
 this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-Street, Fifth Floor, Boston, MA 02111-1301 USA
+Street, Fifth Floor, Boston, MA MA 02110-1335 USA
 
 *******************************************************/
 #define MYSQL_CLIENT
