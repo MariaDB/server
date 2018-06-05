c8c0a1b41214068c5c0f889eb8e039680341b6eb
Michal Schorm <mschorm@redhat.com>
2018-06-05 17:05:10 +0200

    Fix FSF addresses

--- storage/spider/spd_param.h
+++ storage/spider/spd_param.h
@@ -11,7 +11,7 @@
 
   You should have received a copy of the GNU General Public License
   along with this program); if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1301 USA */
 
 my_bool spider_param_support_xa();
 my_bool spider_param_connect_mutex();
