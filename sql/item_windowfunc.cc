ff09512e07045b0997265d37c45f075daebe9531
Alexander Barkov <bar@mariadb.com>
2018-06-20 19:36:37 +0400

    MDEV-16489 when lead() returns null on a datetime field, the result is treated as the literal string '[NULL]'

--- sql/item_windowfunc.cc
+++ sql/item_windowfunc.cc
@@ -444,7 +444,7 @@ bool Item_sum_hybrid_simple::get_date(MYSQL_TIME *ltime, ulonglong fuzzydate)
 {
   DBUG_ASSERT(fixed == 1);
   if (null_value)
-    return 0;
+    return true;
   bool retval= value->get_date(ltime, fuzzydate);
   if ((null_value= value->null_value))
     DBUG_ASSERT(retval == true);
