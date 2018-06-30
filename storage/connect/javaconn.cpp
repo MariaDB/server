f682d8b04cfc4b90a4ad87327aa6aee6b46418d4
Daniel Black <grooverdan@users.sourceforge.net>
2018-06-30 21:23:21 +1000

    connect engine: GetStringUTFChars takes pointer arg
    Avoids compile errors of the form:
    
    /storage/connect/jdbconn.cpp:1473:41: error: cannot initialize a parameter of type 'jboolean *' (aka 'unsigned char *') with an rvalue of type 'jboolean' (aka 'unsigned char')
       name = env->GetStringUTFChars(label, (jboolean)false);
                                            ^~~~~~~~~~~~~~~
    /usr/lib/jvm/java-8-oracle/include/jni.h:1616:58: note: passing argument to parameter 'isCopy' here
        const char* GetStringUTFChars(jstring str, jboolean *isCopy) {

--- storage/connect/javaconn.cpp
+++ storage/connect/javaconn.cpp
@@ -153,7 +153,7 @@ bool JAVAConn::Check(jint rc)
 
 		if (exc != nullptr && tid != nullptr) {
 			jstring s = (jstring)env->CallObjectMethod(exc, tid);
-			const char *utf = env->GetStringUTFChars(s, (jboolean)false);
+			const char *utf = env->GetStringUTFChars(s, NULL);
 			env->DeleteLocalRef(s);
 			Msg = PlugDup(m_G, utf);
 		} else
@@ -162,7 +162,7 @@ bool JAVAConn::Check(jint rc)
 		env->ExceptionClear();
 	} else if (rc < 0) {
 		s = (jstring)env->CallObjectMethod(job, errid);
-		Msg = (char*)env->GetStringUTFChars(s, (jboolean)false);
+		Msg = (char*)env->GetStringUTFChars(s, NULL);
 	} else
 		Msg = NULL;
 
