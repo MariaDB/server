38ecd541e887bef151860030a0c1978393d932a2
Monty <monty@mariadb.org>
2018-08-20 17:50:34 +0300

    MDEV-16986 Unitialized mutex, SIGSEGV and assorted assertion failures in Aria code
    The problem was that when a mysql.proc table was
    opened for reading it was added to the current Aria
    transaction context but never properly deleted from
    it. Normally this isn't a problem, except if the
    mysql.proc table is closed before the transaction
    ended, which happened in this test case.
    
    Fixed by removing mysql.proc from the transaction
    context before closing the table.

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -8772,6 +8772,13 @@ open_system_tables_for_read(THD *thd, TABLE_LIST *table_list,
 void
 close_system_tables(THD *thd, Open_tables_backup *backup)
 {
+  /*
+    Inform the transaction handler that we are closing the
+    system tables and we don't need the read view anymore.
+  */
+  for (TABLE *table= thd->open_tables ; table ; table= table->next)
+    table->file->extra(HA_EXTRA_PREPARE_FOR_FORCED_CLOSE);
+
   close_thread_tables(thd);
   thd->restore_backup_open_tables_state(backup);
 }
