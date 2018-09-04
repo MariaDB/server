MariaDB/server

commit 723f87e9d318aedad30dfb9dde104312d6612662

lock_table_create(), lock_rec_create(): Clean up the WSREP code
By definition, c_lock->trx->lock.wait_lock==c_lock cannot hold.
That is, the owner transaction of a lock cannot be waiting for that
particular lock. It must have been waiting for some other lock.
Remove the dead code related to that. Also, test c_lock for NULLness
only once.

storage/innobase/lock/lock0lock.cc
@@ -898,7 +898,7 @@ UNIV_INLINE
 void
 lock_reset_lock_and_trx_wait(
 /*=========================*/
-	lock_t*	lock)	/*!< in/out: record lock */
+	lock_t* lock)	/*!< in/out: record lock */
 {
 	ut_ad(lock_get_wait(lock));
 	ut_ad(lock_mutex_own());
@@ -2214,13 +2214,6 @@ lock_rec_create(
 
 			if (caller_owns_trx_mutex) {
 				trx_mutex_enter(trx);
-			}
-
-			/* trx might not wait for c_lock, but some other lock
-			   does not matter if wait_lock was released above
-			 */
-			if (c_lock->trx->lock.wait_lock == c_lock) {
-				lock_reset_lock_and_trx_wait(lock);
 			}
 
 			trx_mutex_exit(c_lock->trx);
@@ -4970,19 +4963,18 @@ lock_table_create(
 	UT_LIST_ADD_LAST(trx_locks, trx->lock.trx_locks, lock);
 
 #ifdef WITH_WSREP
-	if (wsrep_thd_is_wsrep(trx->mysql_thd)) {
-		if (c_lock && wsrep_thd_is_BF(trx->mysql_thd, FALSE)) {
+	if (c_lock) {
+		if (wsrep_thd_is_wsrep(trx->mysql_thd)
+		    && wsrep_thd_is_BF(trx->mysql_thd, FALSE)) {
 			UT_LIST_INSERT_AFTER(
 				un_member.tab_lock.locks, table->locks, c_lock, lock);
 		} else {
 			UT_LIST_ADD_LAST(un_member.tab_lock.locks, table->locks, lock);
 		}
 
-		if (c_lock) {
-			trx_mutex_enter(c_lock->trx);
-		}
+		trx_mutex_enter(c_lock->trx);
 
-		if (c_lock && c_lock->trx->lock.que_state == TRX_QUE_LOCK_WAIT) {
+		if (c_lock->trx->lock.que_state == TRX_QUE_LOCK_WAIT) {
 
 			c_lock->trx->lock.was_chosen_as_deadlock_victim = TRUE;
 
@@ -4991,36 +4983,21 @@ lock_table_create(
 				wsrep_print_wait_locks(c_lock->trx->lock.wait_lock);
 			}
 
-			/* have to release trx mutex for the duration of
-			victim lock release. This will eventually call
-			lock_grant, which wants to grant trx mutex again
-			*/
-			/* caller has trx_mutex, have to release for lock cancel */
+			/* The lock release will call lock_grant(),
+			which would acquire trx->mutex again. */
 			trx_mutex_exit(trx);
 			lock_cancel_waiting_and_release(c_lock->trx->lock.wait_lock);
 			trx_mutex_enter(trx);
-
-			/* trx might not wait for c_lock, but some other lock
-			does not matter if wait_lock was released above
-			*/
-			if (c_lock->trx->lock.wait_lock == c_lock) {
-				lock_reset_lock_and_trx_wait(lock);
-			}
 
 			if (wsrep_debug) {
 				fprintf(stderr, "WSREP: c_lock canceled " TRX_ID_FMT "\n",
 					c_lock->trx->id);
 			}
 		}
-		if (c_lock) {
-			trx_mutex_exit(c_lock->trx);
-		}
-	} else {
+		trx_mutex_exit(c_lock->trx);
+	} else
 #endif /* WITH_WSREP */
 	UT_LIST_ADD_LAST(un_member.tab_lock.locks, table->locks, lock);
-#ifdef WITH_WSREP
-	}
-#endif /* WITH_WSREP */
 
 	if (UNIV_UNLIKELY(type_mode & LOCK_WAIT)) {
 

storage/xtradb/lock/lock0lock.cc
@@ -910,7 +910,7 @@ UNIV_INLINE
 void
 lock_reset_lock_and_trx_wait(
 /*=========================*/
-	lock_t*	lock)	/*!< in/out: record lock */
+	lock_t* lock)	/*!< in/out: record lock */
 {
 	ut_ad(lock_get_wait(lock));
 	ut_ad(lock_mutex_own());
@@ -2356,13 +2356,6 @@ lock_rec_create(
 
 			if (caller_owns_trx_mutex) {
 				trx_mutex_enter(trx);
-			}
-
-			/* trx might not wait for c_lock, but some other lock
-			   does not matter if wait_lock was released above
-			 */
-			if (c_lock->trx->lock.wait_lock == c_lock) {
-				lock_reset_lock_and_trx_wait(lock);
 			}
 
 			trx_mutex_exit(c_lock->trx);
@@ -5010,19 +5003,18 @@ lock_table_create(
 	UT_LIST_ADD_LAST(trx_locks, trx->lock.trx_locks, lock);
 
 #ifdef WITH_WSREP
-	if (wsrep_thd_is_wsrep(trx->mysql_thd)) {
-		if (c_lock && wsrep_thd_is_BF(trx->mysql_thd, FALSE)) {
+	if (c_lock) {
+		if (wsrep_thd_is_wsrep(trx->mysql_thd)
+		    && wsrep_thd_is_BF(trx->mysql_thd, FALSE)) {
 			UT_LIST_INSERT_AFTER(
 				un_member.tab_lock.locks, table->locks, c_lock, lock);
 		} else {
 			UT_LIST_ADD_LAST(un_member.tab_lock.locks, table->locks, lock);
 		}
 
-		if (c_lock) {
-			trx_mutex_enter(c_lock->trx);
-		}
+		trx_mutex_enter(c_lock->trx);
 
-		if (c_lock && c_lock->trx->lock.que_state == TRX_QUE_LOCK_WAIT) {
+		if (c_lock->trx->lock.que_state == TRX_QUE_LOCK_WAIT) {
 
 			c_lock->trx->lock.was_chosen_as_deadlock_victim = TRUE;
 
@@ -5031,36 +5023,21 @@ lock_table_create(
 				wsrep_print_wait_locks(c_lock->trx->lock.wait_lock);
 			}
 
-			/* have to release trx mutex for the duration of
-			victim lock release. This will eventually call
-			lock_grant, which wants to grant trx mutex again
-			*/
-			/* caller has trx_mutex, have to release for lock cancel */
+			/* The lock release will call lock_grant(),
+			which would acquire trx->mutex again. */
 			trx_mutex_exit(trx);
 			lock_cancel_waiting_and_release(c_lock->trx->lock.wait_lock);
 			trx_mutex_enter(trx);
-
-			/* trx might not wait for c_lock, but some other lock
-			does not matter if wait_lock was released above
-			*/
-			if (c_lock->trx->lock.wait_lock == c_lock) {
-				lock_reset_lock_and_trx_wait(lock);
-			}
 
 			if (wsrep_debug) {
 				fprintf(stderr, "WSREP: c_lock canceled " TRX_ID_FMT "\n",
 					c_lock->trx->id);
 			}
 		}
-		if (c_lock) {
-			trx_mutex_exit(c_lock->trx);
-		}
-	} else {
+		trx_mutex_exit(c_lock->trx);
+	} else
 #endif /* WITH_WSREP */
 	UT_LIST_ADD_LAST(un_member.tab_lock.locks, table->locks, lock);
-#ifdef WITH_WSREP
-	}
-#endif /* WITH_WSREP */
 
 	if (UNIV_UNLIKELY(type_mode & LOCK_WAIT)) {
 

commit 4caf3e08a853e413b4a08c1bf93cf737266a1451

Add MDEV-11080, MDEV-16709 tests for the MDEV-13333 fix
The regression that was introduced in
commit 723f87e9d318aedad30dfb9dde104312d6612662
was fixed as part of MDEV-13333
(commit 3b37edee1a5121e9523fa8a7f483185f402905e2)
without a test case, because the MDEV-13333 test case
is even less deterministic than these ones.

mysql-test/suite/innodb/r/innodb-lock.result
@@ -2,7 +2,6 @@ set global innodb_table_locks=1;
 select @@innodb_table_locks;
 @@innodb_table_locks
 1
-drop table if exists t1;
 set @@innodb_table_locks=1;
 create table t1 (id integer, x integer) engine=INNODB;
 insert into t1 values(0, 0);
@@ -110,3 +109,31 @@ a	b
 3	1
 COMMIT;
 DROP TABLE t1;
+#
+# MDEV-11080 InnoDB: Failing assertion:
+# table->n_waiting_or_granted_auto_inc_locks > 0
+#
+CREATE TABLE t1 (pk INTEGER AUTO_INCREMENT PRIMARY KEY) ENGINE=InnoDB;
+INSERT INTO t1 VALUES (NULL),(NULL);
+CREATE TABLE t2 LIKE t1;
+BEGIN;
+BEGIN;
+DELETE FROM t2;
+LOCK TABLE t2 READ;;
+SET innodb_lock_wait_timeout= 1, lock_wait_timeout= 2;
+INSERT INTO t2 SELECT * FROM t1;
+COMMIT;
+UNLOCK TABLES;
+DROP TABLE t1, t2;
+#
+# MDEV-16709 InnoDB: Error: trx already had an AUTO-INC lock
+#
+CREATE TABLE t1 (pk INT AUTO_INCREMENT PRIMARY KEY) ENGINE=InnoDB
+PARTITION BY key (pk) PARTITIONS 2;
+CREATE TABLE t2 (a INT) ENGINE=InnoDB;
+INSERT INTO t2 VALUES (1),(2),(3),(4),(5),(6);
+CREATE TABLE t3 (b INT) ENGINE=InnoDB;
+INSERT INTO t3 VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9);
+INSERT t1 SELECT NULL FROM t2;
+INSERT t1 SELECT NULL FROM t3;
+DROP TABLE t1, t2, t3;

mysql-test/suite/innodb/t/innodb-lock.test
@@ -1,4 +1,5 @@
 --source include/have_innodb.inc
+--source include/have_partition.inc
 
 #
 # Check and select innodb lock type
@@ -12,26 +13,15 @@ select @@innodb_table_locks;
 # Testing of explicit table locks with enforced table locks
 #
 
-connect (con1,localhost,root,,);
-connect (con2,localhost,root,,);
-
---disable_warnings
-drop table if exists t1;
---enable_warnings
-
-#
-# Testing of explicit table locks with enforced table locks
-#
-
 set @@innodb_table_locks=1;
 
-connection con1;
+connect (con1,localhost,root,,);
 create table t1 (id integer, x integer) engine=INNODB;
 insert into t1 values(0, 0);
 set autocommit=0;
 SELECT * from t1 where id = 0 FOR UPDATE;
 
-connection con2;
+connect (con2,localhost,root,,);
 set autocommit=0;
 
 # The following statement should hang because con1 is locking the page
@@ -157,14 +147,66 @@ let $wait_condition=
   where state = 'Sending data' and
         info = 'SELECT * FROM t1 FOR UPDATE';
 --source include/wait_condition.inc
+disconnect con2;
 
 connection default;
 COMMIT;
 connection con1;
 reap;
 COMMIT;
-disconnect con1;
-disconnect con2;
-
 connection default;
 DROP TABLE t1;
+
+--echo #
+--echo # MDEV-11080 InnoDB: Failing assertion:
+--echo # table->n_waiting_or_granted_auto_inc_locks > 0
+--echo #
+
+CREATE TABLE t1 (pk INTEGER AUTO_INCREMENT PRIMARY KEY) ENGINE=InnoDB;
+INSERT INTO t1 VALUES (NULL),(NULL);
+CREATE TABLE t2 LIKE t1;
+
+BEGIN;
+
+connection con1;
+BEGIN;
+DELETE FROM t2;
+connection default;
+--send LOCK TABLE t2 READ;
+connection con1;
+SET innodb_lock_wait_timeout= 1, lock_wait_timeout= 2;
+--error 0,ER_LOCK_WAIT_TIMEOUT
+INSERT INTO t2 SELECT * FROM t1;
+COMMIT;
+
+connection default;
+reap;
+UNLOCK TABLES;
+DROP TABLE t1, t2;
+
+--echo #
+--echo # MDEV-16709 InnoDB: Error: trx already had an AUTO-INC lock
+--echo #
+
+CREATE TABLE t1 (pk INT AUTO_INCREMENT PRIMARY KEY) ENGINE=InnoDB
+PARTITION BY key (pk) PARTITIONS 2;
+
+CREATE TABLE t2 (a INT) ENGINE=InnoDB;
+INSERT INTO t2 VALUES (1),(2),(3),(4),(5),(6);
+
+CREATE TABLE t3 (b INT) ENGINE=InnoDB;
+INSERT INTO t3 VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9);
+
+connection con1;
+send INSERT t1 SELECT NULL FROM t2;
+
+connection default;
+--error 0,ER_DUP_ENTRY,ER_LOCK_DEADLOCK
+INSERT t1 SELECT NULL FROM t3;
+
+connection con1;
+--error 0,ER_DUP_ENTRY,ER_LOCK_DEADLOCK
+reap;
+disconnect con1;
+connection default;
+DROP TABLE t1, t2, t3;