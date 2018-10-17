2fa4ed031ce6cc6370671058ccc3d119b8cb3d91
Marko Mäkelä <marko.makela@mariadb.com>
2018-10-17 18:55:46 +0300

    MDEV-17483 Insert on delete-marked record can wrongly inherit old values for instantly added column
    row_ins_clust_index_entry_low(): Do not call dtuple_t::trim()
    before row_ins_clust_index_entry_by_modify(), so that the values
    of all columns will be available in row_upd_build_difference_binary().
    If applicable, the tuple can be trimmed in btr_cur_optimistic_update()
    or btr_cur_pessimistic_update(), which will be called by
    row_ins_clust_index_entry_by_modify().

--- storage/innobase/row/row0ins.cc
+++ storage/innobase/row/row0ins.cc
@@ -2671,8 +2671,6 @@ row_ins_clust_index_entry_low(
 		}
 	}
 
-	if (index->is_instant()) entry->trim(*index);
-
 	if (rec_is_metadata(btr_cur_get_rec(cursor), index)) {
 		goto do_insert;
 	}
@@ -2739,6 +2737,7 @@ err_exit:
 		mtr_commit(&mtr);
 		mem_heap_free(entry_heap);
 	} else {
+		if (index->is_instant()) entry->trim(*index);
 do_insert:
 		rec_t*	insert_rec;
 
