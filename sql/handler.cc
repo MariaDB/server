ccb7201831cad7c082fef9b0fe762a51dc38c9b1
Teemu Ollakka <teemu.ollakka@galeracluster.com>
2018-10-28 07:44:40 +0200

    Use sql_log_bin to suppress binlogging only for high prio appliers
    All row operations should be binlogged for local active wsrep
    transactions in order to make row events to get replicated.
    Therefore restrict the effect of THD::variables.sql_log_bin
    in handler::check_table_binlog_row_based() only to high priority
    appliers.

--- sql/handler.cc
+++ sql/handler.cc
@@ -6073,10 +6073,10 @@ bool handler::check_table_binlog_row_based(bool binlog_row)
   if (unlikely((table->in_use->variables.sql_log_bin_off)))
     return 0;                            /* Called by partitioning engine */
 #ifdef WITH_WSREP
-  if (!table->in_use->variables.sql_log_bin)
-    return 0;      /* wsrep patch sets sql_log_bin directly to silence
-                      binlogging
-                   */
+  if (!table->in_use->variables.sql_log_bin &&
+      wsrep_thd_is_applying(table->in_use))
+    return 0;      /* wsrep patch sets sql_log_bin to silence binlogging
+                      from high priority threads */
 #endif /* WITH_WSREP */
   if (unlikely((!check_table_binlog_row_based_done)))
   {
