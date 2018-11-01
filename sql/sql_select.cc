e058a251c10350f3727ca1df022dc5786933535b
Sergei Petrunia <psergey@askmonty.org>
2018-11-01 11:35:28 +0300

    MDEV-17518: Range optimization doesn't use ON expressions from nested outer joins
    Part#2: take into account that join nest that we are marking as constant
    might already have constant tables in it. Don't count these tables twice.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -4316,14 +4316,17 @@ void mark_join_nest_as_const(JOIN *join,
     }
     JOIN_TAB *tab= tbl->table->reginfo.join_tab;
 
-    tab->type= JT_CONST;
-    tab->info= ET_IMPOSSIBLE_ON_CONDITION;
-    tab->table->const_table= 1;
+    if (!(join->const_table_map & tab->table->map))
+    {
+      tab->type= JT_CONST;
+      tab->info= ET_IMPOSSIBLE_ON_CONDITION;
+      tab->table->const_table= 1;
 
-    join->const_table_map|= tab->table->map;
-    *found_const_table_map|= tab->table->map;
-    set_position(join,(*const_count)++,tab,(KEYUSE*) 0);
-    mark_as_null_row(tab->table);		// All fields are NULL
+      join->const_table_map|= tab->table->map;
+      *found_const_table_map|= tab->table->map;
+      set_position(join,(*const_count)++,tab,(KEYUSE*) 0);
+      mark_as_null_row(tab->table);		// All fields are NULL
+    }
   }
 }
 
