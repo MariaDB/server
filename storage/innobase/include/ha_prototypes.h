d8c34323faad16db0299aa2df45f51af85fbad35
Jan Lindström <jan.lindstrom@mariadb.com>
2018-11-29 14:56:46 +0200

    Revert unnecessary change on ha_prototypes.h

--- storage/innobase/include/ha_prototypes.h
+++ storage/innobase/include/ha_prototypes.h
@@ -236,11 +236,13 @@ innobase_casedn_str(
 #ifdef WITH_WSREP
 UNIV_INTERN
 int
-wsrep_innobase_kill_one_trx(void *thd_ptr,
-                            const trx_t *bf_trx, trx_t *victim_trx, ibool signal);
+wsrep_innobase_kill_one_trx(void * const thd_ptr,
+                            const trx_t * const bf_trx,
+                            trx_t *victim_trx,
+                            ibool signal);
 ulint wsrep_innobase_mysql_sort(int mysql_type, uint charset_number,
-			      unsigned char* str, unsigned int str_length,
-			      unsigned int buf_length);
+                             unsigned char* str, unsigned int str_length,
+                             unsigned int buf_length);
 #endif /* WITH_WSREP */
 
 /**********************************************************************//**
