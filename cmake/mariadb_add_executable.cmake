2f3b2a9e0b1ca72adf49dbf8d31bd38220bebae2
Rasmus Johansson <razze@iki.fi>
2018-12-05 12:04:23 +0000

    MariaDB symlinks

--- cmake/mariadb_add_executable.cmake
+++ cmake/mariadb_add_executable.cmake
@@ -3,5 +3,5 @@ MACRO(CREATE_MARIADB_SYMLINK filepath sympath)
     install(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${filepath} ${sympath})" COMPONENT Client)
     install(CODE "message(\"-- Created symlink: ${sympath} -> ${filepath}\")" COMPONENT Client)
 
-    install(FILES ${sympath} DESTINATION ${CMAKE_INSTALL_PREFIX}/bin COMPONENT Client)
+    install(FILES ${sympath} DESTINATION ${INSTALL_BINDIR} COMPONENT Client)
 ENDMACRO(CREATE_MARIADB_SYMLINK)
