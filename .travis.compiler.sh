829fce9ea6ee984f49b6a77a442435e38fd98cfa
Teodor Mircea Ionita <teodor@mariadb.org>
2018-12-25 12:01:13 +0000

    travis: upgrade Ubuntu target dist to 16.04 Xenial
    Ubuntu 14.04 Trusty is LTS until April 2019 and since we are
    approaching that, upgrade to Xenial which is LTS until April 2021.
    
    One bonus of this upgrade is that the Travis Xenial virtual images
    have a much bigger disk size, thus no need to limit ccache below
    the default of 5G anymore.
    
    Also had to remove linux/clang-8 toolchain testing since
    llvm-toolchain-xenial is not listed at:
    https://github.com/travis-ci/apt-source-safelist/blob/master/ubuntu.json
    
    And fails with:
    Disallowing sources: llvm-toolchain-xenial
    ...
    E: Unable to locate package clang-8
    E: Unable to locate package llvm-8-dev

--- .travis.compiler.sh
+++ .travis.compiler.sh
@@ -22,7 +22,6 @@ if [[ "${TRAVIS_OS_NAME}" == 'linux' ]]; then
   exclude_modules;
   if which ccache ; then
     CMAKE_OPT="${CMAKE_OPT} -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
-    ccache --max-size=2200M
   fi
   if [[ "${CXX}" == 'clang++' ]]; then
     if [[ "${CC_VERSION}" == '6' ]]; then
