995eed4fd2a34e2750498cc73cfba30f3fdee399
Anel Husakovic <anel@mariadb.org>
2019-01-20 10:41:52 -0800

    MDEV-18284 using JSON_COMPACT with JSON_OBJECT
    JSON casting using JSON_COMPACT doesn't always work with values from subqueries.

--- sql/item_jsonfunc.cc
+++ sql/item_jsonfunc.cc
@@ -1448,8 +1448,8 @@ static int append_json_value(String *str, Item *item, String *tmp_val)
     String *sv= item->val_json(tmp_val);
     if (item->null_value)
       goto append_null;
-    if (item->is_json_type())
-      return str->append(sv->ptr(), sv->length());
+    if (item->is_json_type() || item->type()==Item::FUNC_ITEM || item->type()==Item::REF_ITEM)
+      return st_append_escaped(str, sv);
 
     if (item->result_type() == STRING_RESULT)
     {
@@ -3225,6 +3225,28 @@ String *Item_func_json_format::val_str(String *str)
 String *Item_func_json_format::val_json(String *str)
 {
   String *js= args[0]->val_json(&tmp_js);
+  /* In case when args[0] is of type Item_direct_ref JSON_COMPACT doesnt work inside JSON_OBJECT
+     Item_func_json_format::val_str is not invoked
+  */
+  if(this->fmt==COMPACT && js!=NULL)
+  {
+    js=(static_cast <Item_func_json_format*> (args[0]))->val_str(js);
+    json_engine_t je;
+    int tab_size= 4;
+    json_scan_start(&je, js->charset(), (const uchar *) js->ptr(),
+                  (const uchar *) js->ptr()+js->length());
+
+    str->length(0);
+    str->set_charset(js->charset());
+    if (json_nice(&je, str, fmt, tab_size))
+    {
+      null_value= 1;
+      report_json_error(js, &je, 0);
+      return 0;
+    }
+    return str;
+
+  }
   if ((null_value= args[0]->null_value))
     return 0;
   return js;
