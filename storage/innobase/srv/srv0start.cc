2a0f1d613219ad7962c3394b9c1996ece40926df
Sergei Golubchik <serg@mariadb.org>
2019-01-22 11:06:15 +0100

    Bug#28867993: POSSIBLE ISSUE WITH MYSQL SERVER RESTART
    on startup innodb is checking whether files "ib_logfileN"
    (for N from 1 to 100) exist, and whether they're readable.
    A non-existent file aborted the scan.
    A directory instead of a file made InnoDB to fail.
    
    Now it treats "directory exists" as "file doesn't exist".

--- storage/innobase/srv/srv0start.cc
+++ storage/innobase/srv/srv0start.cc
@@ -2255,6 +2255,10 @@ innobase_start_or_create_for_mysql()
 				break;
 			}
 
+			if (stat_info.type != OS_FILE_TYPE_FILE) {
+				break;
+			}
+
 			if (!srv_file_check_mode(logfilename)) {
 				return(DB_ERROR);
 			}
--- storage/xtradb/srv/srv0start.cc
+++ storage/xtradb/srv/srv0start.cc
@@ -2335,6 +2335,9 @@ innobase_start_or_create_for_mysql()
 				break;
 			}
 
+			if (stat_info.type != OS_FILE_TYPE_FILE) {
+				break;
+			}
 			if (!srv_file_check_mode(logfilename)) {
 				return(DB_ERROR);
 			}
