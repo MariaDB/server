09cea8703f3ec4e4f9e23855a339c9e3d5e84d3b
Alexey Botchkov <holyfoot@askmonty.org>
2019-02-01 17:10:27 +0400

    MDEV-17148 DROP DATABASE throw "Directory not empty" after changed lower_case_table_names.
    No need to lowercase table names on case-sensitive file systems, as the
    cache won't contain the 'lowercased' table anyway. And it prevents the
    UPPERCASE.frm from being deleted.

--- sql/sql_db.cc
+++ sql/sql_db.cc
@@ -1107,8 +1107,12 @@ static bool find_db_tables_and_rm_known_files(THD *thd, MY_DIR *dirp,
     table_list->table_name_length= table->length;
     table_list->open_type= OT_BASE_ONLY;
 
-    /* To be able to correctly look up the table in the table cache. */
-    if (lower_case_table_names)
+    /*
+      On the case-insensitive file systems table is opened
+      with the lowercased file name. So we should lowercase
+      as well to look up the cache properly.
+    */
+    if (lower_case_file_system)
       table_list->table_name_length= my_casedn_str(files_charset_info,
                                                    table_list->table_name);
 
