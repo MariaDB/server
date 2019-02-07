1ff09c98734e0922f3475ac2b6f5dcbd0c49f178
Aleksey Midenkov <midenok@gmail.com>
2019-02-07 18:40:06 +0300

    MDEV-17553 Enable setting start datetime for interval partitioned history of system versioned tables
    * Interactive STARTS syntax
    * SHOW CREATE
    * Default STARTS rounding depending on INTERVAL type
    * Warn when STARTS timestamp is further than INTERVAL
    
    [Closes tempesta-tech/mariadb#574]
    
    === Dependencies (auto-detected by git-deps) ===
    f07fc874977 MDEV-16370 row-based binlog events (updates by primary key) can not be applied multiple times to system versioned tables

--- sql/partition_info.h
+++ sql/partition_info.h
@@ -395,27 +395,9 @@ public:
   bool field_in_partition_expr(Field *field) const;
 
   bool vers_init_info(THD *thd);
-  bool vers_set_interval(THD *thd, Item *item,
-                         interval_type int_type, my_time_t start)
-  {
-    DBUG_ASSERT(part_type == VERSIONING_PARTITION);
-    vers_info->interval.type= int_type;
-    vers_info->interval.start= start;
-    if (item->fix_fields_if_needed_for_scalar(thd, &item))
-      return true;
-    bool error= get_interval_value(thd, item, int_type, &vers_info->interval.step) ||
-           vers_info->interval.step.neg || vers_info->interval.step.second_part ||
-          !(vers_info->interval.step.year || vers_info->interval.step.month ||
-            vers_info->interval.step.day || vers_info->interval.step.hour ||
-            vers_info->interval.step.minute || vers_info->interval.step.second);
-    if (error)
-    {
-      my_error(ER_PART_WRONG_VALUE, MYF(0),
-               thd->lex->create_last_non_select_table->table_name.str,
-               "INTERVAL");
-    }
-    return error;
-  }
+  bool vers_set_interval(THD *thd, Item *interval,
+                         interval_type int_type, Item *starts,
+                         const char *table_name);
   bool vers_set_limit(ulonglong limit)
   {
     DBUG_ASSERT(part_type == VERSIONING_PARTITION);
