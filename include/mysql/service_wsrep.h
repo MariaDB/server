mariadb-corporation/codership-mariadb-server

WSREP debug log levels support
Global variable wsrep_debug now can be used to filter wsrep-lib messages based on debug level provided.
Type of wsrep_debug is now set to be unsigned int, so tests and configuration files changed accordingly.

include/mysql/service_wsrep.h
@@ -122,7 +122,7 @@ extern struct wsrep_service_st {
 #else
 
 #define MYSQL_SERVICE_WSREP_STATIC_INCLUDED
-extern my_bool wsrep_debug;
+extern uint    wsrep_debug;
 extern my_bool wsrep_log_conflicts;
 extern my_bool wsrep_certify_nonPK;
 extern my_bool wsrep_load_data_splitting;

sql/wsrep_mysqld.cc
@@ -85,7 +85,7 @@ const char *wsrep_data_home_dir;
 const char *wsrep_dbug_option;
 const char *wsrep_notify_cmd;
 
-my_bool wsrep_debug;                            // Enable debug level logging
+uint    wsrep_debug;                            // Debug level logging
 my_bool wsrep_convert_LOCK_to_trx;              // Convert locking sessions to trx
 my_bool wsrep_auto_increment_control;           // Control auto increment variables
 my_bool wsrep_drupal_282555_workaround;         // Retry autoinc insert after dupkey
@@ -638,6 +638,7 @@ int wsrep_init_server()
                                   working_dir,
                                   initial_position,
                                   wsrep_max_protocol_version);
+    Wsrep_server_state::instance().debug_log_level(wsrep_debug);
   }
   catch (const wsrep::runtime_error& e)
   {

sql/wsrep_mysqld.h
@@ -64,7 +64,7 @@ extern const char* wsrep_data_home_dir;
 extern const char* wsrep_dbug_option;
 extern long        wsrep_slave_threads;
 extern int         wsrep_slave_count_change;
-extern MYSQL_PLUGIN_IMPORT my_bool wsrep_debug;
+extern uint        wsrep_debug;
 extern my_bool     wsrep_convert_LOCK_to_trx;
 extern ulong       wsrep_retry_autocommit;
 extern my_bool     wsrep_auto_increment_control;
@@ -75,7 +75,7 @@ extern ulong       wsrep_max_ws_size;
 extern ulong       wsrep_max_ws_rows;
 extern const char* wsrep_notify_cmd;
 extern my_bool     wsrep_certify_nonPK;
-extern long int        wsrep_protocol_version;
+extern long int    wsrep_protocol_version;
 extern ulong       wsrep_forced_binlog_format;
 extern my_bool     wsrep_desync;
 extern ulong       wsrep_reject_queries;

MariaDB/server

include/mysql/service_wsrep.h
@@ -122,7 +122,7 @@ extern struct wsrep_service_st {
 #else
 
 #define MYSQL_SERVICE_WSREP_STATIC_INCLUDED
-extern my_bool wsrep_debug;
+extern ulong   wsrep_debug;
 extern my_bool wsrep_log_conflicts;
 extern my_bool wsrep_certify_nonPK;
 extern my_bool wsrep_load_data_splitting;

sql/wsrep_mysqld.cc
@@ -85,7 +85,7 @@ const char *wsrep_data_home_dir;
 const char *wsrep_dbug_option;
 const char *wsrep_notify_cmd;
 
-my_bool wsrep_debug;                            // Enable debug level logging
+ulong   wsrep_debug;                            // Debug level logging
 my_bool wsrep_convert_LOCK_to_trx;              // Convert locking sessions to trx
 my_bool wsrep_auto_increment_control;           // Control auto increment variables
 my_bool wsrep_drupal_282555_workaround;         // Retry autoinc insert after dupkey
@@ -638,6 +638,7 @@ int wsrep_init_server()
                                   working_dir,
                                   initial_position,
                                   wsrep_max_protocol_version);
+    Wsrep_server_state::instance().debug_log_level(wsrep_debug);
   }
   catch (const wsrep::runtime_error& e)
   {

sql/wsrep_mysqld.h
@@ -64,7 +64,7 @@ extern const char* wsrep_data_home_dir;
 extern const char* wsrep_dbug_option;
 extern long        wsrep_slave_threads;
 extern int         wsrep_slave_count_change;
-extern MYSQL_PLUGIN_IMPORT my_bool wsrep_debug;
+extern ulong       wsrep_debug;
 extern my_bool     wsrep_convert_LOCK_to_trx;
 extern ulong       wsrep_retry_autocommit;
 extern my_bool     wsrep_auto_increment_control;
@@ -75,7 +75,7 @@ extern ulong       wsrep_max_ws_size;
 extern ulong       wsrep_max_ws_rows;
 extern const char* wsrep_notify_cmd;
 extern my_bool     wsrep_certify_nonPK;
-extern long int        wsrep_protocol_version;
+extern long int    wsrep_protocol_version;
 extern ulong       wsrep_forced_binlog_format;
 extern my_bool     wsrep_desync;
 extern ulong       wsrep_reject_queries;