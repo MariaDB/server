932e29d4ad3cd6ffb445f0080191c88c8b2693c0
Sergei Golubchik <serg@mariadb.org>
2019-03-22 01:55:35 +0100

    don't run SysV scripts in scriptlets if systemd is used

--- support-files/rpm/server-postun.sh
+++ support-files/rpm/server-postun.sh
@@ -1,9 +1,10 @@
 if [ $1 -ge 1 ]; then
-  if [ -x %{_sysconfdir}/init.d/mysql ] ; then
-    # only restart the server if it was alredy running
-    if %{_sysconfdir}/init.d/mysql status > /dev/null 2>&1; then
-      %{_sysconfdir}/init.d/mysql restart
-    fi
+  # only restart the server if it was alredy running
+  if [ -x /usr/bin/systemctl ] ; then
+    /usr/bin/systemctl daemon-reload > /dev/null 2>&1
+    /usr/bin/systemctl try-restart mariadb.service > /dev/null 2>&1
+  elif %{_sysconfdir}/init.d/mysql status > /dev/null 2>&1; then
+    %{_sysconfdir}/init.d/mysql restart
   fi
 fi
 
