b9ba3e0ccea2b124ecb5dc070a10d1fa0f5664b0
chriscalender <chriscalender@gmail.com>
2019-03-24 23:25:20 -0400

    Fix for MDEV-4968, update advice in mysql-log-rotate.sh

--- support-files/mysql-log-rotate.sh
+++ support-files/mysql-log-rotate.sh
@@ -1,9 +1,9 @@
 # This logname can be set in /etc/my.cnf
-# by setting the variable "err-log"
-# in the [safe_mysqld] section as follows:
+# by setting the variable "log-error"
+# in the [mysqld_safe] section as follows:
 #
-# [safe_mysqld]
-# err-log=@localstatedir@/mysqld.log
+# [mysqld_safe]
+# log-error=@localstatedir@/mysqld.log
 #
 # If the root user has a password you have to create a
 # /root/.my.cnf configuration file with the following
