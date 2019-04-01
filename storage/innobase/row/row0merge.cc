0fb6a3dc19a689c640d35415fc21e1165aae37cf
Eugene Kosov <claprix@yandex.ru>
2019-04-01 20:04:01 +0300

    a bit of refactoring

--- storage/innobase/row/row0merge.cc
+++ storage/innobase/row/row0merge.cc
@@ -1692,7 +1692,8 @@ public:
 
 	bool convert_if_needed(const dtuple_t* row, mem_heap_t* row_heap,
 			       const dict_table_t* old_table,
-			       const TABLE* mariadb_table, ulong n_rows) const
+			       const TABLE_SHARE* table_share,
+			       ulong n_rows) const
 	{
 		for (const auto& info : infos) {
 			dfield_t* dfield
@@ -1702,54 +1703,9 @@ public:
 				continue;
 			}
 
-			const size_t old_max_chars
-			    = dfield->len / info.old_col->mbminlen;
-			const size_t buf_size
-			    = old_max_chars * dfield->type.mbmaxlen;
-
-			char* new_data = static_cast<char*>(
-			    mem_heap_alloc(row_heap, buf_size));
-
-			MY_STRCOPY_STATUS copy_status;
-			MY_STRCONV_STATUS conv_status;
-			const auto converted_len = my_convert_fix(
-			    info.new_charset, new_data, buf_size,
-			    info.old_charset, static_cast<char*>(dfield->data),
-			    dfield->len, old_max_chars, &copy_status,
-			    &conv_status);
-
-			if (const char* err_pos
-			    = conv_status.m_cannot_convert_error_pos) {
-				char tmp[32];
-				convert_to_printable(
-				    tmp, sizeof(tmp), err_pos,
-				    static_cast<const char*>(dfield->data)
-					+ dfield->len - err_pos,
-				    info.old_charset, 6);
-				my_error(
-				    ER_TRUNCATED_WRONG_VALUE_FOR_FIELD, MYF(0),
-				    "string", tmp, mariadb_table->s->db.str,
-				    mariadb_table->s->table_name.str,
-				    info.old_col->name(*old_table), n_rows);
+			if (!convert(info, dfield, row_heap, old_table,
+				     table_share, n_rows)) {
 				return false;
-			}
-
-			dfield->data = new_data;
-			if (info.mysql_type == MYSQL_TYPE_VARCHAR
-			    || info.mysql_type == MYSQL_TYPE_BLOB) {
-				dfield->len = converted_len;
-			} else {
-				ut_ad(info.mysql_type == MYSQL_TYPE_STRING);
-
-				const auto new_len = dfield->type.len;
-				if (new_len > converted_len) {
-					info.new_charset->cset->fill(
-					    info.new_charset,
-					    new_data + converted_len,
-					    new_len - converted_len,
-					    info.new_charset->pad_char);
-				}
-				dfield->len = new_len;
 			}
 		}
 
@@ -1763,6 +1719,69 @@ private:
 		CHARSET_INFO *old_charset;
 		CHARSET_INFO *new_charset;
 	};
+
+	bool convert(const conversion_info_t& info, dfield_t* dfield,
+		     mem_heap_t* row_heap, const dict_table_t* old_table,
+		     const TABLE_SHARE* table_share, ulong n_rows) const
+	{
+		const size_t old_max_chars
+		    = dfield->len / info.old_col->mbminlen;
+		const size_t buf_size = old_max_chars * dfield->type.mbmaxlen;
+
+		auto* new_data
+		    = static_cast<char*>(mem_heap_alloc(row_heap, buf_size));
+
+		MY_STRCOPY_STATUS copy_status;
+		MY_STRCONV_STATUS conv_status;
+		auto new_len = my_convert_fix(
+		    info.new_charset, new_data, buf_size, info.old_charset,
+		    dfield_get_data(dfield), dfield_get_len(dfield),
+		    old_max_chars, &copy_status, &conv_status);
+
+		if (const char* err_pos
+		    = conv_status.m_cannot_convert_error_pos) {
+			issue_mariadb_error(err_pos, dfield, info, table_share,
+					    old_table, n_rows);
+			return false;
+		}
+
+		if (info.mysql_type == MYSQL_TYPE_STRING) {
+			const auto type_len = dfield->type.len;
+			if (type_len > new_len) {
+				info.new_charset->cset->fill(
+				    info.new_charset, new_data + new_len,
+				    type_len - new_len,
+				    info.new_charset->pad_char);
+			}
+			new_len = type_len;
+		}
+
+		dfield_set_data(dfield, new_data, new_len);
+
+		return true;
+	}
+
+	static void issue_mariadb_error(const char* err_pos,
+					const dfield_t* dfield,
+					const conversion_info_t& info,
+					const TABLE_SHARE* table_share,
+					const dict_table_t* old_table,
+					ulong n_rows)
+	{
+		char tmp[32];
+		const auto err_len = dfield_get_data(dfield)
+				     + dfield_get_len(dfield) - err_pos;
+		convert_to_printable(tmp, sizeof(tmp), err_pos, err_len,
+				     info.old_charset, 6);
+		my_error(ER_TRUNCATED_WRONG_VALUE_FOR_FIELD, MYF(0), "string",
+			 tmp, table_share->db.str, table_share->table_name.str,
+			 info.old_col->name(*old_table), n_rows);
+	}
+
+	static char* dfield_get_data(const dfield_t* dfield)
+	{
+		return static_cast<char*>(::dfield_get_data(dfield));
+	}
 
 	std::vector<conversion_info_t> infos;
 };
@@ -2450,7 +2469,7 @@ end_of_index:
 		}
 
 		if (!charset_converter.convert_if_needed(
-			row, row_heap, old_table, table,
+			row, row_heap, old_table, table->s,
 			static_cast<ulong>(n_rows + 1))) {
 			err = DB_TRUNCATED_WRONG_VALUE_FOR_FIELD;
 			break;
