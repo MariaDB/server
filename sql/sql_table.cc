eabbb7371c4f97bd3113cf42e6349763492336a1
Anel Husakovic <anel@mariadb.org>
2019-04-01 04:18:05 -0700

    MDEV-18463: After reivew 1

--- sql/sql_table.cc
+++ sql/sql_table.cc
@@ -4192,7 +4192,7 @@ mysql_prepare_create_table(THD *thd, HA_CREATE_INFO *create_info,
         /* Check that there's no repeating CHECK constraint names. */
         List_iterator_fast<Virtual_column_info>
           dup_it(alter_info->check_constraint_list);
-        Virtual_column_info *dup_check;
+        const Virtual_column_info *dup_check;
         while ((dup_check= dup_it++) && dup_check != check)
         {
           if (check->name.length == dup_check->name.length &&
@@ -4203,19 +4203,17 @@ mysql_prepare_create_table(THD *thd, HA_CREATE_INFO *create_info,
             DBUG_RETURN(TRUE);
           }
         }
-
-        /* Check that there's no repeating foreign key constraint names. */
-        List_iterator_fast<Key> key_it(alter_info->key_list);
-        Key *key;
-        while ((key= key_it++))
+      }
+      /* Check that there's no repeating foreign key constraint names. */
+      List_iterator_fast<Key> key_it(alter_info->key_list);
+      while (const Key *key= key_it++)
+      {
+        if (check->name.length == key->name.length &&
+            my_strcasecmp(system_charset_info,
+                          check->name.str, key->name.str) == 0)
         {
-          if (check->name.length == key->name.length &&
-              my_strcasecmp(system_charset_info,
-                            check->name.str, key->name.str) == 0)
-          {
-            my_error(ER_DUP_CONSTRAINT_NAME, MYF(0), "CHECK", check->name.str);
-            DBUG_RETURN(TRUE);
-          }
+          my_error(ER_DUP_CONSTRAINT_NAME, MYF(0), "CHECK", check->name.str);
+          DBUG_RETURN(TRUE);
         }
       }
 
