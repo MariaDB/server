3dcfddad89c6694b1610f55c78d4362c732e21bc
Kentoku <kentokushiba@gmail.com>
2019-04-17 21:25:12 +0900

    MDEV-19238 Mariadb spider - crashes on where null

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -3094,7 +3094,7 @@ bool JOIN::make_aggr_tables_info()
     distinct in the engine, so we do this for all queries, not only
     GROUP BY queries.
   */
-  if (tables_list && !procedure)
+  if (tables_list && !procedure && join_tab)
   {
     /*
       At the moment we only support push down for queries where
