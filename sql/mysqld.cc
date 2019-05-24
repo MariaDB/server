4d89931a4b76b0170e30fa7bae6991b58201d97f
Alexey Botchkov <holyfoot@askmonty.org>
2019-05-24 16:53:11 +0400

    ping the bb.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -1,5 +1,6 @@
 /* Copyright (c) 2000, 2015, Oracle and/or its affiliates.
    Copyright (c) 2008, 2019, MariaDB Corporation.
+   x
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
