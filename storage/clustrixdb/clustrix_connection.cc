70cb38cf7cd1cc92775ae9536339a744d50fd371
Will DeVries <wdevries@clustrix.com>
2019-06-06 00:59:37 -0700

    Inital commit of Clustrix backend plugin.

---
+++ storage/clustrixdb/clustrix_connection.cc
@@ -0,0 +1,589 @@
+/*****************************************************************************
+Copyright (c) 2019, MariaDB Corporation.
+*****************************************************************************/
+
+/** @file clustrix_connection.cc */
+
+#include "clustrix_connection.h"
+#include <string>
+#include "errmsg.h"
+#include "handler.h"
+#include "table.h"
+
+extern int clustrix_connect_timeout;
+extern int clustrix_read_timeout;
+extern int clustrix_write_timeout;
+extern char *clustrix_host;
+extern char *clustrix_username;
+extern char *clustrix_password;
+extern uint clustrix_port;
+extern char *clustrix_socket;
+
+static const char charset_name[] = "latin1";
+
+enum clustrix_commands {
+  CLUSTRIX_WRITE_ROW = 1,
+  CLUSTRIX_SCAN_INIT,
+  CLUSTRIX_SCAN_NEXT,
+  CLUSTRIX_SCAN_STOP,
+  CLUSTRIX_KEY_READ,
+  CLUSTRIX_KEY_DELETE,
+};
+
+/****************************************************************************
+** Class clustrix_connection
+****************************************************************************/
+
+void clustrix_connection::disconnect(bool is_destructor)
+{
+  if (is_destructor)
+  {
+    /*
+      Connection object destruction occurs after the destruction of
+      the thread used by the network has begun, so usage of that
+      thread object now is not reliable
+    */
+    clustrix_net.net.thd = NULL;
+  }
+  mysql_close(&clustrix_net);
+}
+
+int clustrix_connection::connect()
+{
+  int error_code = 0;
+  my_bool my_true = 1;
+  DBUG_ENTER("connect");
+
+  /* Validate the connection parameters */
+  if (!strcmp(clustrix_socket, ""))
+    if (!strcmp(clustrix_host, "127.0.0.1"))
+      if (clustrix_port == MYSQL_PORT_DEFAULT)
+        DBUG_RETURN(ER_CONNECT_TO_FOREIGN_DATA_SOURCE);
+
+  //clustrix_net.methods = &connection_methods;
+
+  if (!mysql_init(&clustrix_net))
+    DBUG_RETURN(HA_ERR_OUT_OF_MEM);
+
+  mysql_options(&clustrix_net, MYSQL_OPT_READ_TIMEOUT,
+                &clustrix_read_timeout);
+  mysql_options(&clustrix_net, MYSQL_OPT_WRITE_TIMEOUT,
+                &clustrix_write_timeout);
+  mysql_options(&clustrix_net, MYSQL_OPT_CONNECT_TIMEOUT,
+                &clustrix_connect_timeout);
+  mysql_options(&clustrix_net, MYSQL_OPT_USE_REMOTE_CONNECTION,
+                NULL);
+  mysql_options(&clustrix_net, MYSQL_SET_CHARSET_NAME, charset_name);
+  mysql_options(&clustrix_net, MYSQL_OPT_USE_THREAD_SPECIFIC_MEMORY,
+                (char *) &my_true);
+  mysql_options(&clustrix_net, MYSQL_INIT_COMMAND,"SET autocommit=0");
+
+#ifdef CLUSTRIX_CONNECTION_SSL
+  if (opt_ssl_ca_length | conn->tgt_ssl_capath_length |
+      conn->tgt_ssl_cert_length | conn->tgt_ssl_key_length)
+  {
+    mysql_ssl_set(&clustrix_net, conn->tgt_ssl_key, conn->tgt_ssl_cert,
+                  conn->tgt_ssl_ca, conn->tgt_ssl_capath, conn->tgt_ssl_cipher);
+    if (conn->tgt_ssl_vsc)
+    {
+      my_bool verify_flg = TRUE;
+      mysql_options(&clustrix_net, MYSQL_OPT_SSL_VERIFY_SERVER_CERT,
+                    &verify_flg);
+    }
+  }
+#endif
+
+  if (!mysql_real_connect(&clustrix_net, clustrix_host,
+                          clustrix_username, clustrix_password,
+                          NULL, clustrix_port, clustrix_socket,
+                          CLIENT_MULTI_STATEMENTS))
+  {
+    error_code = mysql_errno(&clustrix_net);
+    disconnect();
+
+    if (error_code != CR_CONN_HOST_ERROR &&
+        error_code != CR_CONNECTION_ERROR)
+    {
+      if (error_code == ER_CON_COUNT_ERROR)
+      {
+        my_error(ER_CON_COUNT_ERROR, MYF(0));
+        DBUG_RETURN(ER_CON_COUNT_ERROR);
+      }
+      my_error(ER_CONNECT_TO_FOREIGN_DATA_SOURCE, MYF(0), clustrix_host);
+      DBUG_RETURN(ER_CONNECT_TO_FOREIGN_DATA_SOURCE);
+    }
+  }
+
+  clustrix_net.reconnect = 1;
+
+  DBUG_RETURN(0);
+}
+
+int clustrix_connection::send_command()
+{
+  my_bool com_error;
+  com_error = simple_command(&clustrix_net,
+                             (enum_server_command)CLUSTRIX_SERVER_REQUEST,
+                             command_buffer, command_length, TRUE);
+
+  if (com_error)
+  {
+    my_printf_error(mysql_errno(&clustrix_net),
+                    "Clustrix error: %s", MYF(0),
+                    mysql_error(&clustrix_net));
+    return ER_QUERY_ON_FOREIGN_DATA_SOURCE;
+  }
+
+  return 0;
+}
+
+int clustrix_connection::read_query_response()
+{
+  my_bool comerr = clustrix_net.methods->read_query_result(&clustrix_net);
+  if (comerr)
+  {
+    my_printf_error(mysql_errno(&clustrix_net),
+                    "Clustrix error: %s", MYF(0),
+                    mysql_error(&clustrix_net));
+    return ER_QUERY_ON_FOREIGN_DATA_SOURCE;
+  }
+
+  return 0;
+}
+
+int clustrix_connection::begin_trans()
+{
+  const char *stmt = "BEGIN TRANSACTION";
+  int error_code = mysql_real_query(&clustrix_net, stmt, strlen(stmt));
+  if (error_code)
+    return mysql_errno(&clustrix_net);
+  return error_code;
+}
+
+int clustrix_connection::commit_trans()
+{
+  const char *stmt = "COMMIT TRANSACTION";
+  int error_code = mysql_real_query(&clustrix_net, stmt, strlen(stmt));
+  if (error_code)
+    return mysql_errno(&clustrix_net);
+  return error_code;
+}
+
+int clustrix_connection::rollback_trans()
+{
+  const char *stmt = "ROLLBACK TRANSACTION";
+  int error_code = mysql_real_query(&clustrix_net, stmt, strlen(stmt));
+  if (error_code)
+    return mysql_errno(&clustrix_net);
+  return error_code;
+}
+
+int clustrix_connection::create_table(char *stmt)
+{
+  int error_code = mysql_real_query(&clustrix_net, stmt, strlen(stmt));
+  if (error_code)
+    return mysql_errno(&clustrix_net);
+
+  return error_code;
+}
+
+int clustrix_connection::delete_table(char *stmt)
+{
+  int error_code = mysql_real_query(&clustrix_net, stmt, strlen(stmt));
+  if (error_code)
+    return mysql_errno(&clustrix_net);
+  return error_code;
+}
+
+int clustrix_connection::write_row(ulonglong clustrix_table_oid,
+                                   uchar *packed_row, size_t packed_size)
+{
+  int error_code;
+  command_length = 0;
+
+  if ((error_code = add_command_operand_uchar(CLUSTRIX_WRITE_ROW)))
+    return error_code;
+
+  if ((error_code = add_command_operand_ulonglong(clustrix_table_oid)))
+    return error_code;
+
+  if ((error_code = add_command_operand_str(packed_row, packed_size)))
+    return error_code;
+
+  if ((error_code = send_command()))
+    return error_code;
+
+  if ((error_code = read_query_response()))
+    return error_code;
+
+  last_insert_id = clustrix_net.insert_id;
+  return error_code;
+}
+
+int clustrix_connection::key_delete(ulonglong clustrix_table_oid,
+                                    uchar *packed_key, size_t packed_key_length)
+{
+  int error_code;
+  command_length = 0;
+
+  if ((error_code = add_command_operand_uchar(CLUSTRIX_KEY_DELETE)))
+    return error_code;
+
+  if ((error_code = add_command_operand_ulonglong(clustrix_table_oid)))
+    return error_code;
+
+  if ((error_code = add_command_operand_str(packed_key, packed_key_length)))
+    return error_code;
+
+  if ((error_code = send_command()))
+    return error_code;
+
+  if ((error_code = read_query_response()))
+    return mysql_errno(&clustrix_net);
+
+  return error_code;
+}
+
+int clustrix_connection::key_read(ulonglong clustrix_table_oid, uint index,
+                                  MY_BITMAP *read_set, uchar *packed_key,
+                                  ulong packed_key_length, uchar **rowdata,
+                                  ulong *rowdata_length)
+{
+  int error_code;
+  command_length = 0;
+
+  if ((error_code = add_command_operand_uchar(CLUSTRIX_KEY_READ)))
+    return error_code;
+
+  if ((error_code = add_command_operand_ulonglong(clustrix_table_oid)))
+    return error_code;
+
+  if ((error_code = add_command_operand_uint(index)))
+    return error_code;
+
+  if ((error_code = add_command_operand_bitmap(read_set)))
+    return error_code;
+
+  if ((error_code = add_command_operand_str(packed_key, packed_key_length)))
+    return error_code;
+
+  if ((error_code = send_command()))
+    return error_code;
+
+  ulong packet_length = cli_safe_read(&clustrix_net);
+  if (packet_length == packet_error)
+    return mysql_errno(&clustrix_net);
+
+  *rowdata = clustrix_net.net.read_pos;
+  *rowdata_length = safe_net_field_length_ll(rowdata, packet_length);
+
+  return 0;
+}
+
+int clustrix_connection::scan_init(ulonglong clustrix_table_oid, uint index,
+                                   enum sort_order sort, MY_BITMAP *read_set,
+                                   ulonglong *scan_refid)
+{
+  int error_code;
+  command_length = 0;
+
+  if ((error_code = add_command_operand_uchar(CLUSTRIX_SCAN_INIT)))
+    return error_code;
+
+  if ((error_code = add_command_operand_ulonglong(clustrix_table_oid)))
+    return error_code;
+
+  if ((error_code = add_command_operand_uint(index)))
+    return error_code;
+
+  if ((error_code = add_command_operand_uchar(sort)))
+    return error_code;
+
+  if ((error_code = add_command_operand_bitmap(read_set)))
+    return error_code;
+
+  if ((error_code = send_command()))
+    return error_code;
+
+  ulong packet_length = cli_safe_read(&clustrix_net);
+  if (packet_length == packet_error)
+    return mysql_errno(&clustrix_net);
+
+  unsigned char *pos = clustrix_net.net.read_pos;
+  *scan_refid = safe_net_field_length_ll(&pos, packet_length);
+  return error_code;
+}
+
+int clustrix_connection::scan_next(ulonglong scan_refid, uchar **rowdata,
+                                   ulong *rowdata_length)
+{
+  int error_code;
+  command_length = 0;
+
+  if ((error_code = add_command_operand_uchar(CLUSTRIX_SCAN_NEXT)))
+    return error_code;
+
+  if ((error_code = add_command_operand_lcb(scan_refid)))
+    return error_code;
+
+  if ((error_code = send_command()))
+    return error_code;
+
+  ulong packet_length = cli_safe_read(&clustrix_net);
+  if (packet_length == packet_error)
+    return mysql_errno(&clustrix_net);
+
+  *rowdata = clustrix_net.net.read_pos;
+  *rowdata_length =  safe_net_field_length_ll(rowdata, packet_length);
+
+  return 0;
+}
+
+int clustrix_connection::scan_end(ulonglong scan_refid)
+{
+  int error_code;
+  command_length = 0;
+
+  if ((error_code = add_command_operand_uchar(CLUSTRIX_SCAN_STOP)))
+    return error_code;
+
+  if ((error_code = add_command_operand_lcb(scan_refid)))
+    return error_code;
+
+  if ((error_code = send_command()))
+    return error_code;
+
+  ulong packet_length = cli_safe_read(&clustrix_net);
+  if (packet_length == packet_error)
+    return mysql_errno(&clustrix_net);
+
+  return 0;
+}
+
+int clustrix_connection::populate_table_list(LEX_CSTRING *db,
+                                             handlerton::discovered_list *result)
+{
+  int error_code = 0;
+  String stmt;
+  stmt.append("SHOW FULL TABLES FROM ");
+  stmt.append(db);
+  stmt.append(" WHERE table_type = 'BASE TABLE'");
+
+  if (mysql_real_query(&clustrix_net, stmt.c_ptr(), stmt.length())) {
+    int error_code = mysql_errno(&clustrix_net);
+    if (error_code == ER_BAD_DB_ERROR)
+      return 0;
+    else
+      return error_code;
+  }
+
+  MYSQL_RES *results = mysql_store_result(&clustrix_net);
+  if (mysql_num_fields(results) != 2) {
+    error_code = HA_ERR_CORRUPT_EVENT;
+    goto error;
+  }
+
+  MYSQL_ROW row;
+  while((row = mysql_fetch_row(results)))
+    result->add_table(row[0], strlen(row[0]));
+
+error:
+  mysql_free_result(results);
+  return error_code;
+}
+
+int clustrix_connection::discover_table_details(LEX_CSTRING *db,
+                                                LEX_CSTRING *name, THD *thd,
+                                                TABLE_SHARE *share)
+{
+  DBUG_ENTER("clustrix_connection::discover_table_details");
+  int error_code = 0;
+  MYSQL_RES *results = NULL;
+  MYSQL_ROW row;
+  String get_oid, show;
+
+  /* get oid */
+  get_oid.append("select r.table "
+                 "from system.databases d "
+                 "     inner join ""system.relations r on d.db = r.db "
+                 "where d.name = '");
+  get_oid.append(db);
+  get_oid.append("' and r.name = '");
+  get_oid.append(name);
+  get_oid.append("'");
+  if (mysql_real_query(&clustrix_net, get_oid.c_ptr(), get_oid.length())) {
+    if ((error_code = mysql_errno(&clustrix_net)))
+      return error_code;
+  }
+
+  results = mysql_store_result(&clustrix_net);
+  DBUG_PRINT("oid results",
+             ("rows: %llu, fields: %u", mysql_num_rows(results),
+              mysql_num_fields(results)));
+
+  if (mysql_num_rows(results) != 1) {
+    error_code = HA_ERR_NO_SUCH_TABLE;
+    goto error;
+  }
+
+  while((row = mysql_fetch_row(results))) {
+    DBUG_PRINT("row", ("%s", row[0]));
+    uchar *to = (uchar*)alloc_root(&share->mem_root, strlen(row[0]) + 1);
+    if (!to) {
+      error_code = HA_ERR_OUT_OF_MEM;
+      goto error;
+    }
+
+    strcpy((char *)to, (char *)row[0]);
+    share->tabledef_version.str = to;
+    share->tabledef_version.length = strlen(row[0]);
+  }
+
+  mysql_free_result(results);
+
+  /* get show create statement */
+  show.append("show create table ");
+  show.append(db);
+  show.append(".");
+  show.append(name);
+  if (mysql_real_query(&clustrix_net, show.c_ptr(), show.length())) {
+    if ((error_code = mysql_errno(&clustrix_net)))
+      return error_code;
+  }
+
+  results = mysql_store_result(&clustrix_net);
+  DBUG_PRINT("show table results",
+             ("rows: %llu, fields: %u", mysql_num_rows(results),
+              mysql_num_fields(results)));
+
+  if (mysql_num_rows(results) != 1) {
+    error_code = HA_ERR_NO_SUCH_TABLE;
+    goto error;
+  }
+
+  if (mysql_num_fields(results) != 2) {
+    error_code = HA_ERR_CORRUPT_EVENT;
+    goto error;
+  }
+
+  while((row = mysql_fetch_row(results))) {
+    DBUG_PRINT("row", ("%s - %s", row[0], row[1]));
+    error_code = share->init_from_sql_statement_string(thd, false, row[1],
+                                                       strlen(row[1]));
+  }
+
+error:
+  if (results)
+    mysql_free_result(results);
+  DBUG_RETURN(error_code);
+}
+
+int clustrix_connection::expand_command_buffer(size_t add_length)
+{
+  size_t expanded_length;
+
+  if (command_buffer_length >= command_length + add_length)
+    return 0;
+
+  expanded_length = command_buffer_length +
+                    ((add_length >> COMMAND_BUFFER_SIZE_INCREMENT_BITS)
+                                 << COMMAND_BUFFER_SIZE_INCREMENT_BITS) +
+                     COMMAND_BUFFER_SIZE_INCREMENT;
+
+  if (!command_buffer_length)
+    command_buffer = (uchar *) my_malloc(expanded_length, MYF(MY_WME));
+  else
+    command_buffer = (uchar *) my_realloc(command_buffer, expanded_length,
+                                          MYF(MY_WME));
+  if (!command_buffer)
+    return HA_ERR_OUT_OF_MEM;
+
+  command_buffer_length = expanded_length;
+
+  return 0;
+}
+
+int clustrix_connection::add_command_operand_uchar(uchar value)
+{
+  int error_code = expand_command_buffer(sizeof(value));
+  if (error_code)
+    return error_code;
+
+  memcpy(command_buffer + command_length, &value, sizeof(value));
+  command_length += sizeof(value);
+
+  return 0;
+}
+
+int clustrix_connection::add_command_operand_uint(uint value)
+{
+  uint be_value = htobe32(value);
+  int error_code = expand_command_buffer(sizeof(be_value));
+  if (error_code)
+    return error_code;
+
+  memcpy(command_buffer + command_length, &be_value, sizeof(be_value));
+  command_length += sizeof(be_value);
+  return 0;
+}
+
+int clustrix_connection::add_command_operand_ulonglong(ulonglong value)
+{
+  ulonglong be_value = htobe64(value);
+  int error_code = expand_command_buffer(sizeof(be_value));
+  if (error_code)
+    return error_code;
+
+  memcpy(command_buffer + command_length, &be_value, sizeof(be_value));
+  command_length += sizeof(be_value);
+  return 0;
+}
+
+int clustrix_connection::add_command_operand_lcb(ulonglong value)
+{
+  int len = net_length_size(value);
+  int error_code = expand_command_buffer(len);
+  if (error_code)
+    return error_code;
+
+  net_store_length(command_buffer + command_length, value);
+  command_length += len;
+  return 0;
+}
+
+int clustrix_connection::add_command_operand_str(const uchar *str,
+                                                 size_t str_length)
+{
+  int error_code = add_command_operand_lcb(str_length);
+  if (error_code)
+    return error_code;
+
+  error_code = expand_command_buffer(str_length);
+  if (error_code)
+    return error_code;
+
+  memcpy(command_buffer + command_length, str, str_length);
+  command_length += str_length;
+  return 0;
+}
+
+int clustrix_connection::add_command_operand_lex_string(LEX_CSTRING str)
+{
+  return add_command_operand_str((const uchar *)str.str, str.length);
+}
+
+int clustrix_connection::add_command_operand_bitmap(MY_BITMAP *bitmap)
+{
+  int error_code = add_command_operand_lcb(bitmap->n_bits);
+  if (error_code)
+    return error_code;
+
+  int no_bytes = no_bytes_in_map(bitmap);
+  error_code = expand_command_buffer(no_bytes);
+  if (error_code)
+    return error_code;
+
+  memcpy(command_buffer + command_length, bitmap->bitmap, no_bytes);
+  command_length += no_bytes;
+  return 0;
+}
---
+++ storage/clustrixdb/ha_clustrixdb.h
@@ -0,0 +1,124 @@
+/*****************************************************************************
+Copyright (c) 2019, MariaDB Corporation.
+*****************************************************************************/
+
+#ifndef _ha_clustrixdb_h
+#define _ha_clustrixdb_h
+
+#ifdef USE_PRAGMA_INTERFACE
+#pragma interface     /* gcc class implementation */
+#endif
+
+#define MYSQL_SERVER 1
+#include "clustrix_connection.h"
+#include "my_bitmap.h"
+#include "table.h"
+#include "rpl_rli.h"
+#include "handler.h"
+#include "sql_class.h"
+#include "sql_show.h"
+#include "mysql.h"
+#include "../../sql/rpl_record.h"
+
+class ha_clustrixdb;
+
+class st_clustrixdb_trx
+{
+public:
+  THD *thd;
+  clustrix_connection *clustrix_net;
+  //query_id_t query_id;
+  //MEM_ROOT mem_root;  /* Memory allocated for the executing transaction */
+  bool has_transaction;
+
+  st_clustrixdb_trx(THD* trx_thd);
+  ~st_clustrixdb_trx();
+  int net_init();
+  int begin_trans();
+};
+
+class ha_clustrixdb : public handler
+{
+private:
+# define CLUSTRIXDB_ROW_LIMIT 1024
+
+  ulonglong clustrix_table_oid;
+  rpl_group_info *rgi;
+  Relay_log_info *rli;
+  RPL_TABLE_LIST *rpl_table_list;
+
+  Field *auto_inc_field;
+  ulonglong auto_inc_value;
+
+  bool has_hidden_key;
+  ulonglong last_hidden_key;
+  ulonglong scan_refid;
+  bool is_scan;
+  MY_BITMAP scan_fields;
+
+public:
+  ha_clustrixdb(handlerton *hton, TABLE_SHARE *table_arg);
+  ~ha_clustrixdb();
+  int create(const char *name, TABLE *form, HA_CREATE_INFO *info);
+  int delete_table(const char *name);
+  int open(const char *name, int mode, uint test_if_locked);
+  int close(void);
+  int reset();
+  int write_row(uchar *buf);
+  // start_bulk_update exec_bulk_update
+  int update_row(const uchar *old_data, const uchar *new_data);
+  // start_bulk_delete exec_bulk_delete
+  int delete_row(const uchar *buf);
+
+  Table_flags table_flags(void) const;
+  ulong index_flags(uint idx, uint part, bool all_parts) const;
+  uint max_supported_keys() const { return MAX_KEY; }
+
+  ha_rows records();
+  ha_rows records_in_range(uint inx, key_range *min_key,
+                           key_range *max_key);
+
+  int info(uint flag); // see my_base.h for full description
+
+  // multi_read_range
+  // read_range
+  int index_init(uint idx, bool sorted);
+  int index_read(uchar * buf, const uchar * key, uint key_len,
+                 enum ha_rkey_function find_flag);
+  int index_first(uchar *buf);
+  int index_prev(uchar *buf);
+  int index_last(uchar *buf);
+  int index_next(uchar *buf);
+  //int index_next_same(uchar *buf, const uchar *key, uint keylen);
+  int index_end();
+
+  int rnd_init(bool scan);
+  int rnd_next(uchar *buf);
+  int rnd_pos(uchar * buf, uchar *pos);
+  int rnd_end();
+
+  void position(const uchar *record);
+  uint lock_count(void) const;
+  THR_LOCK_DATA **store_lock(THD *thd,
+                             THR_LOCK_DATA **to,
+                             enum thr_lock_type lock_type);
+  int external_lock(THD *thd, int lock_type);
+
+  uint8 table_cache_type()
+  {
+    return(HA_CACHE_TBL_NOCACHE);
+  }
+
+  const COND *cond_push(const COND *cond);
+  void cond_pop();
+  int info_push(uint info_type, void *info);
+
+private:
+  st_clustrixdb_trx *get_trx(THD *thd, int *error_code);
+  void add_current_table_to_rpl_table_list();
+  void remove_current_table_from_rpl_table_list();
+  void build_key_packed_row(uint index, uchar *packed_key,
+                            size_t *packed_key_len);
+};
+
+#endif  // _ha_clustrixdb_h
