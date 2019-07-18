10c163e4a18e30a63b45732673de91b68433e6b2
Nikita Malyavin <nikitamalyavin@gmail.com>
2019-07-18 23:11:16 +1000

    MDEV-16026: Forbid global system_versioning_asof in non-default time zone
    * store `system_versioning_asof` in unix time;
    * both session and global vars are processed in session timezone;
    * setting `default` does not copy global varibale abymore. Instead, it sets
     system_time to SYSTEM_TIME_UNSPECIFIED, which means that no 'AS OF' time
     is applied and `now()` can be assumed
    
    As a regression, we cannot assign values below 1970 (UTC) anymore

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -687,8 +687,12 @@ bool vers_select_conds_t::init_from_sysvar(THD *thd)
   if (type != SYSTEM_TIME_UNSPECIFIED && type != SYSTEM_TIME_ALL)
   {
     DBUG_ASSERT(type == SYSTEM_TIME_AS_OF);
+    MYSQL_TIME ltime;
+    thd->variables.time_zone->gmt_sec_to_TIME(&ltime, in.unix_time);
+    ltime.second_part = in.second_part;
+
     start.item= new (thd->mem_root)
-        Item_datetime_literal(thd, &in.ltime, TIME_SECOND_PART_DIGITS);
+        Item_datetime_literal(thd, &ltime, TIME_SECOND_PART_DIGITS);
     if (!start.item)
       return true;
   }
