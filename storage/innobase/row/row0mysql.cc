609ea2f37b8169a7c282fe2d607c2412467ccbbb
Marko Mäkelä <marko.makela@mariadb.com>
2019-08-12 18:50:54 +0300

    MDEV-17614: After-merge fix
    MDEV-17614 flags INSERT…ON DUPLICATE KEY UPDATE unsafe for statement-based
    replication when there are multiple unique indexes. This correctly fixes
    something whose attempted fix in MySQL 5.7
    in mysql/mysql-server@c93b0d9a972cb6f98fd445f2b69d924350f9128a
    caused lock conflicts. That change was reverted in MySQL 5.7.26
    in mysql/mysql-server@066b6fdd433aa6673622341f1a2f0a3a20018043
    (with a substantial amount of other changes).
    
    In MDEV-17073 we already disabled the unfortunate MySQL change when
    statement-based replication was not being used. Now, thanks to MDEV-17614,
    we can actually remove the change altogether.
    
    This reverts commit 8a346f31b913daa011085afec2b2d38450c73e00 (MDEV-17073)
    and mysql/mysql-server@c93b0d9a972cb6f98fd445f2b69d924350f9128a while
    keeping the test cases.

--- storage/innobase/row/row0mysql.cc
+++ storage/innobase/row/row0mysql.cc
@@ -1435,7 +1435,6 @@ error_exit:
 			goto run_again;
 		}
 
-		node->duplicate = NULL;
 		trx->op_info = "";
 
 		if (blob_heap != NULL) {
@@ -1444,8 +1443,6 @@ error_exit:
 
 		return(err);
 	}
-
-	node->duplicate = NULL;
 
 	if (dict_table_has_fts_index(table)) {
 		doc_id_t	doc_id;
