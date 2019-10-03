6621be137f1807303c967329f536231a510ee3c0
Nikita Malyavin <nikitamalyavin@gmail.com>
2019-10-03 23:31:40 +1000

    Fix memory leak.
    The fix is to not set up the table memroot if PS is executed

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -5387,6 +5387,24 @@ static void mark_real_tables_as_free_for_reuse(TABLE_LIST *table_list)
     }
 }
 
+static int fix_table_vcol_exprs(THD *thd, TABLE *t)
+{
+  for (Field **vf= t->vfield; vf && *vf; vf++)
+    if (fix_session_vcol_expr(thd, (*vf)->vcol_info))
+      return 1;
+
+  for (Field **df= t->default_field; df && *df; df++)
+    if ((*df)->default_value &&
+        fix_session_vcol_expr(thd, (*df)->default_value))
+      return 1;
+
+  for (Virtual_column_info **cc= t->check_constraints; cc && *cc; cc++)
+    if (fix_session_vcol_expr(thd, (*cc)))
+      return 1;
+
+  return 0;
+}
+
 
 static bool fix_all_session_vcol_exprs(THD *thd, TABLE_LIST *tables)
 {
@@ -5394,7 +5412,8 @@ static bool fix_all_session_vcol_exprs(THD *thd, TABLE_LIST *tables)
   TABLE_LIST *first_not_own= thd->lex->first_not_own_table();
   DBUG_ENTER("fix_session_vcol_expr");
 
-  for (TABLE_LIST *table= tables; table && table != first_not_own;
+  int error= 0;
+  for (TABLE_LIST *table= tables; table && table != first_not_own && !error;
        table= table->next_global)
   {
     TABLE *t= table->table;
@@ -5402,31 +5421,19 @@ static bool fix_all_session_vcol_exprs(THD *thd, TABLE_LIST *tables)
          table->lock_type >= TL_WRITE_ALLOW_WRITE)
     {
       Query_arena backup;
-      thd->set_n_backup_active_arena(t->expr_arena, &backup);
+      if (thd->stmt_arena->is_conventional())
+        thd->set_n_backup_active_arena(t->expr_arena, &backup);
       if (table->security_ctx)
         thd->security_ctx= table->security_ctx;
 
-      for (Field **vf= t->vfield; vf && *vf; vf++)
-        if (fix_session_vcol_expr(thd, (*vf)->vcol_info))
-          goto err;
-
-      for (Field **df= t->default_field; df && *df; df++)
-        if ((*df)->default_value &&
-            fix_session_vcol_expr(thd, (*df)->default_value))
-          goto err;
-
-      for (Virtual_column_info **cc= t->check_constraints; cc && *cc; cc++)
-        if (fix_session_vcol_expr(thd, (*cc)))
-          goto err;
+      error= fix_table_vcol_exprs(thd, t);
 
       thd->security_ctx= save_security_ctx;
-      thd->restore_active_arena(t->expr_arena, &backup);
+      if (thd->stmt_arena->is_conventional())
+        thd->restore_active_arena(t->expr_arena, &backup);
     }
   }
-  DBUG_RETURN(0);
-err:
-  thd->security_ctx= save_security_ctx;
-  DBUG_RETURN(1);
+  DBUG_RETURN(error);
 }
 
 
