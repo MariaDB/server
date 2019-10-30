f4ba775914d9d997d3a0116a05d09f1a34c28707
Daniele Sciascia <daniele.sciascia@galeracluster.com>
2019-10-30 09:45:22 +0100

    MDEV-17099 Preliminary changes for Galera XA support (#1404)
    Redo changes reverted in commit
    8f46e3833c7acb3ec3e4c7b389ae8cdd98eff1ce, this time without build
    issues in wsrep-lib.

--- sql/wsrep_storage_service.h
+++ sql/wsrep_storage_service.h
@@ -33,7 +33,8 @@ public:
   int append_fragment(const wsrep::id&,
                       wsrep::transaction_id,
                       int flags,
-                      const wsrep::const_buffer&);
+                      const wsrep::const_buffer&,
+                      const wsrep::xid&);
   int update_fragment_meta(const wsrep::ws_meta&);
   int remove_fragments();
   int commit(const wsrep::ws_handle&, const wsrep::ws_meta&);
