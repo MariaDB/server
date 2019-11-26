b423e60f743bc45f74219895fe5e05398a0d6d96
Nikita Malyavin <nikitamalyavin@gmail.com>
2019-11-26 19:22:04 +1000

    MDEV-16978 Application-time periods: WITHOUT OVERLAPS
    * The overlaps check is implemented on a handler level per row command. It creates a separate cursor (actually, another handler instance) and caches it inside the original handler, when ha_update_row or ha_insert_row is issued. Cursor closes on unlocking the handler.
    
    * Containing the same key in index means unique constraint violation even in usual terms. So we fetch left and right neighbours and check that they have same key prefix, excluding from the key only the period part. If it doesnt match, then there's no such neighbour, and the check passes. Otherwise, we check if this neighbour intersects with the considered key.
    
    * the check does introduce new error and fails with ER_DUPP_KEY error. This might break REPLACE workflow and should be fixed separately

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -2225,6 +2225,14 @@ int show_create_table(THD *thd, TABLE_LIST *table_list, String *packet,
 
   }
 
+  if (period.name)
+  {
+    append_period(thd, packet,
+                  period.start_field(share)->field_name,
+                  period.end_field(share)->field_name,
+                  period.name, true);
+  }
+
   key_info= table->s->key_info;
   primary_key= share->primary_key;
 
@@ -2259,7 +2267,11 @@ int show_create_table(THD *thd, TABLE_LIST *table_list, String *packet,
 
     packet->append(STRING_WITH_LEN(" ("));
 
-    for (uint j=0 ; j < key_info->user_defined_key_parts ; j++,key_part++)
+    uint key_parts= key_info->user_defined_key_parts;
+    if (key_info->without_overlaps)
+      key_parts-= 2;
+
+    for (uint j=0 ; j < key_parts ; j++,key_part++)
     {
       Field *field= key_part->field;
       if (field->invisible > INVISIBLE_USER)
@@ -2279,6 +2291,14 @@ int show_create_table(THD *thd, TABLE_LIST *table_list, String *packet,
                                       key_part->field->charset()->mbmaxlen);
       }
     }
+
+    if (key_info->without_overlaps)
+    {
+      packet->append(',');
+      append_identifier(thd, packet, &share->period.name);
+      packet->append(STRING_WITH_LEN(" WITHOUT OVERLAPS"));
+    }
+
     packet->append(')');
     store_key_options(thd, packet, table, &table->key_info[i]);
     if (key_info->parser)
@@ -2311,15 +2331,6 @@ int show_create_table(THD *thd, TABLE_LIST *table_list, String *packet,
       DBUG_ASSERT(fe->invisible == INVISIBLE_SYSTEM);
     }
   }
-
-  if (period.name)
-  {
-    append_period(thd, packet,
-                  period.start_field(share)->field_name,
-                  period.end_field(share)->field_name,
-                  period.name, true);
-  }
-
 
   /*
     Get possible foreign key definitions stored in InnoDB and append them
