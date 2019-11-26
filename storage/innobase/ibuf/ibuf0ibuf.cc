a8395853ccf767aa22d6a7e0a2e79d0e6f890f7d
Marko Mäkelä <marko.makela@mariadb.com>
2019-11-26 16:32:51 +0200

    MDEV-21152 Bogus debug assertion btr_pcur_is_after_last_in_tree() in ibuf code
    As noted in commit abd45cdc38e72ce329365ffe0df4c6f8c319b407
    a search with PAGE_CUR_GE may land on the supremum record on
    a leaf page that is not the rightmost leaf page. This could occur
    when all keys on the current page are smaller than the search key,
    and the smallest key on the successor page is larger than the search key.
    
    Hence, after a failed PAGE_CUR_GE search, assertions
    btr_pcur_is_after_last_in_tree() are bogus
    and should be replaced with btr_pcur_is_after_last_on_page().

--- storage/innobase/ibuf/ibuf0ibuf.cc
+++ storage/innobase/ibuf/ibuf0ibuf.cc
@@ -4574,8 +4574,7 @@ loop:
 	}
 
 	if (!btr_pcur_is_on_user_rec(&pcur)) {
-		ut_ad(btr_pcur_is_after_last_in_tree(&pcur, &mtr));
-
+		ut_ad(btr_pcur_is_after_last_on_page(&pcur));
 		goto reset_bit;
 	}
 
@@ -4805,8 +4804,7 @@ loop:
 		&pcur, &mtr);
 
 	if (!btr_pcur_is_on_user_rec(&pcur)) {
-		ut_ad(btr_pcur_is_after_last_in_tree(&pcur, &mtr));
-
+		ut_ad(btr_pcur_is_after_last_on_page(&pcur));
 		goto leave_loop;
 	}
 
