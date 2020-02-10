a90f9792408fe349e96b35073848a54d8b689e97
Sujatha <sujatha.sivakumar@mariadb.com>
2020-02-10 12:43:26 +0530

    MDEV-21469: Implement crash-safe binary logging of the user XA
    Make XA PREPARE, XA COMMIT and XA ROLLBACK statements
    crash-safe when --log-bin is specified.
    
    At execution of XA PREPARE, XA COMMIT and XA ROLLBACK their replication
    events are made written into the binary log prior to execution
    of the commands in storage engines.
    In case the server crashes, after writing to binary log but before all
    involved engines have processed the command, the following recovery
    will execute the command's replication events to equalize the states
    of involved engines with that of binlog.
    That applies to all XA PREPARE *group* and XA COMMIT or ROLLBACK.
    
    On the implementation level the recovery time binary log parsing
    is augmented to pay attention to
    the user XA xids to identify the XA transactions' state:s in binary log, and
    eventually match them against their states in engines, see
    MYSQL_BIN_LOG::recover_explicit_xa_prepare().
    In discrepancy cases the outdated state in the engines is corrected with
    resubmitting the transaction prepare group of events, or completion
    ones. The multi-engine partly prepared XA PREPARE case
    the XA is rolled back first.
    The fact of multiple-engine involved is registered into
    Gtid_log_event::flags2 as one bit. The boolean value is
    sufficient and precise to deal with two engines in XA transaction.
    With more than 2 recoverable engines the flag method is still correct though
    may be pessimistic, as it treats all recoverable engines as XA
    participants. So when the number of such Engines exceeds the number
    of prepared engines of the XA that XA is treated
    as partially completed, with all that ensued.
    As an optimization no new bit is allocated in flags2, instead a
    pre-existing ones (of MDEV-742) are reused, observing that
    A. XA "COMPLETE" does not require multi-engine hint for its recovery and that
    B. the MDEV-742 XA-completion bit is not anyhow used by
       XA-PREPARE and its GTID log event.
    
    Notice the multi-engine flagging is superceded by MDEV-21117 extension
    in Gtid log event so this part should be taken from there.

--- sql/log.h
+++ sql/log.h
@@ -62,6 +62,7 @@ class TC_LOG
   virtual int unlog(ulong cookie, my_xid xid)=0;
   virtual int unlog_xa_prepare(THD *thd, bool all)= 0;
   virtual void commit_checkpoint_notify(void *cookie)= 0;
+  virtual void execute_xa_for_recovery() {};
 
 protected:
   /*
@@ -720,6 +721,8 @@ public:
   void commit_checkpoint_notify(void *cookie);
   int recover(LOG_INFO *linfo, const char *last_log_name, IO_CACHE *first_log,
               Format_description_log_event *fdle, bool do_xa);
+  bool recover_explicit_xa_prepare();
+
   int do_binlog_recovery(const char *opt_name, bool do_xa_recovery);
 #if !defined(MYSQL_CLIENT)
 
@@ -959,7 +962,7 @@ public:
   }
 
   int wait_for_update_binlog_end_pos(THD* thd, struct timespec * timeout);
-
+  void execute_xa_for_recovery();
   /*
     Binlog position of end of the binlog.
     Access to this is protected by LOCK_binlog_end_pos
@@ -972,6 +975,10 @@ public:
   */
   my_off_t binlog_end_pos;
   char binlog_end_pos_file[FN_REFLEN];
+  MEM_ROOT mem_root;
+  char *xa_binlog_checkpoint_name;  // binlog file to start off xa recovery
+  HASH xa_recover_list;             // user xids with their binlog/engine status
+  uint xa_recover_htons;            // number of detected recoverable hton:s
 };
 
 class Log_event_handler
