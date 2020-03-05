1312b4ebb652bf5eb9e97292ca62f0de3b0ff243
Marko Mäkelä <marko.makela@mariadb.com>
2020-03-05 07:31:55 +0200

    MDEV-14425 preparation: Provide ut_crc32_low()
    The ut_crc32() function uses a hard-coded initial CRC-32C value of 0.
    Replace it with ut_crc32_low(), which allows to specify the initial
    checksum value, and provide an inlined compatibility wrapper ut_crc32().
    
    Also, remove non-inlined wrapper functions on ARMv8 and POWER8,
    and remove dead code (the generic implementation) on POWER8.
    
    Note: The original AMD64 instruction set architecture in 2003 only
    included SSE2. The CRC-32C instructions are part of the SSE4.2
    instruction set extension for IA-32 and AMD64, with first processors
    released in November 2007 (using the AMD Barcelona microarchitecture)
    and November 2008 (Intel Nehalem microarchiteture). It might be safe
    to assume that SSE4.2 is available on all currently used AMD64 based
    systems, but we are not taking that step yet.

--- storage/innobase/include/ut0crc32.h
+++ storage/innobase/include/ut0crc32.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
 
 Copyright (c) 2011, 2015, Oracle and/or its affiliates. All Rights Reserved.
-Copyright (c) 2016, 2018, MariaDB Corporation.
+Copyright (c) 2016, 2020, MariaDB Corporation.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -32,22 +32,29 @@ Created Aug 10, 2011 Vasil Dimov
 /********************************************************************//**
 Initializes the data structures used by ut_crc32*(). Does not do any
 allocations, would not hurt if called twice, but would be pointless. */
-void
-ut_crc32_init();
-/*===========*/
+void ut_crc32_init();
 
-/********************************************************************//**
-Calculates CRC32.
-@param ptr - data over which to calculate CRC32.
-@param len - data length in bytes.
-@return CRC32 (CRC-32C, using the GF(2) primitive polynomial 0x11EDC6F41,
-or 0x1EDC6F41 without the high-order bit) */
-typedef uint32_t	(*ut_crc32_func_t)(const byte* ptr, ulint len);
+/** Append data to a CRC-32C checksum.
+@param crc   current checksum
+@param s     data to append to the checksum
+@param size  data length in bytes
+@return CRC-32C, using the GF(2) primitive polynomial 0x11EDC6F41,
+or 0x1EDC6F41 without the highest degree term */
+typedef uint32_t (*ut_crc32_func_t)(uint32_t crc, const byte *s, size_t size);
 
 /** Pointer to CRC32 calculation function. */
-extern ut_crc32_func_t	ut_crc32;
+extern ut_crc32_func_t ut_crc32_low;
 
 /** Text description of CRC32 implementation */
 extern const char*	ut_crc32_implementation;
+
+/** Compute CRC-32C over a string of bytes.
+@param s     data
+@param len   data length in bytes
+@return the CRC-32C of the data */
+static inline uint32_t ut_crc32(const byte *s, size_t size)
+{
+  return ut_crc32_low(0, s, size);
+}
 
 #endif /* ut0crc32_h */
