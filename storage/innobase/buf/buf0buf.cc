6df48d248043ad09d2726cd06f4225bda5bb2840
Vladislav Vaintroub <wlad@mariadb.com>
2020-03-07 22:30:31 +0100

    MDEV-15053 Split bufferpool mutex
    cherry-pick mysql commit bd914aebb3ec510784f364e5e71cd35bb8a0cad5
    
    BUG#23136397 ASSERTION FAILURE: BUF0BUF.CC:5410:RW_LOCK_IS_LOCKED
                 (&BLOCK->LOCK, RW_LOCK_X)
    
    It's regression of wl#8423 InnoDB: Split the buffer pool mutex.
    
    We should protect buf_page_set_io_fix(&block->page, BUF_IO_READ)
    and rw_lock_x_lock(&block->lock) by buf_pool->LRU_list_mutex.
    
    Reviewed-by: Debarun Banerjee <debarun.banerjee@oracle.com>
    RB: 12707
    
    # Conflicts:
    #	storage/innobase/buf/buf0buf.cc

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -4920,19 +4920,19 @@ buf_page_create(
 
 	/* The block must be put to the LRU list */
 	buf_LRU_add_block(&block->page, FALSE);
-	mutex_exit(&buf_pool->LRU_list_mutex);
 
 	buf_block_buf_fix_inc(block, __FILE__, __LINE__);
 	buf_pool->stat.n_pages_created++;
 
 	if (zip_size) {
 		/* Prevent race conditions during buf_buddy_alloc(),
-		which may release and reacquire buf_pool->mutex,
+		which may release and reacquire buf_pool->LRU_list_mutex,
 		by IO-fixing and X-latching the block. */
 
 		buf_page_set_io_fix(&block->page, BUF_IO_READ);
 		rw_lock_x_lock(&block->lock);
 
+		mutex_exit(&buf_pool->LRU_list_mutex);
 		buf_page_mutex_exit(block);
 		block->page.zip.data = buf_buddy_alloc(zip_size);
 		mutex_enter(&buf_pool->LRU_list_mutex);
@@ -4945,11 +4945,12 @@ buf_page_create(
 		block->page.zip.data is set. */
 		ut_ad(buf_page_belongs_to_unzip_LRU(&block->page));
 		buf_unzip_LRU_add_block(block, FALSE);
-		mutex_exit(&buf_pool->LRU_list_mutex);
 
 		buf_page_set_io_fix(&block->page, BUF_IO_NONE);
 		rw_lock_x_unlock(&block->lock);
 	}
+
+	mutex_exit(&buf_pool->LRU_list_mutex);
 
 	mtr_memo_push(mtr, block, MTR_MEMO_BUF_FIX);
 
