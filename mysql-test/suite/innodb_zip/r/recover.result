MariaDB/server

commit 08ba388713946c03aa591899cd3a446a6202f882

MDEV-12353: Replace MLOG_REC_INSERT,MLOG_COMP_REC_INSERT
page_mem_alloc_free(), page_dir_set_n_heap(), page_ptr_set_direction():
Merge with the callers.

page_direction_reset(), page_direction_increment(),
page_zip_dir_insert(), page_zip_write_rec_ext(), page_zip_write_rec():
Add the parameter mtr, and write log.

PageBulk::insert(), PageBulk::finish(): Write log for all changes.

page_cur_rec_insert(), page_cur_insert_rec_write_log(),
page_cur_insert_rec_write_log(): Remove.

page_rec_set_next(), page_header_set_field(), page_header_set_ptr():
Remove. Use lower-level operations with or without logging.

page_zip_dir_add_slot(): Move to the same compilation unit with
its only caller, page_cur_insert_rec_zip().

page_cur_insert_rec_zip(): Mark pieces of code that must be skipped
once this task is completed.

btr_defragment_chunk(): Before starting a mini-transaction that
is writing (a lot), invoke log_free_check(). This should allow
the test innodb.innodb_defrag_concurrent to pass with the
mtr default_mysqld.cnf setting of innodb_log_file_size=10M.

MLOG_BUF_MARGIN: Remove.

storage/innobase/btr/btr0cur.cc
@@ -3878,31 +3878,6 @@ btr_cur_upd_lock_and_undo(
 		       cmpl_info, rec, offsets, roll_ptr));
 }
 
-/** Copy DB_TRX_ID,DB_ROLL_PTR to the redo log.
-@param[in]	index	clustered index
-@param[in]	trx_id_t	DB_TRX_ID
-@param[in]	roll_ptr	DB_ROLL_PTR
-@param[in,out]	log_ptr		redo log buffer
-@return current end of the redo log buffer */
-static byte*
-btr_cur_log_sys(
-	const dict_index_t*	index,
-	trx_id_t		trx_id,
-	roll_ptr_t		roll_ptr,
-	byte*			log_ptr)
-{
-	log_ptr += mach_write_compressed(log_ptr, index->db_trx_id());
-	/* Yes, we are writing DB_ROLL_PTR,DB_TRX_ID in reverse order,
-	after emitting the position of DB_TRX_ID in the index.
-	This is how row_upd_write_sys_vals_to_log()
-	originally worked, and it is part of the redo log format. */
-	trx_write_roll_ptr(log_ptr, roll_ptr);
-	log_ptr += DATA_ROLL_PTR_LEN;
-	log_ptr += mach_u64_write_compressed(log_ptr, trx_id);
-
-	return log_ptr;
-}
-
 /** Write DB_TRX_ID,DB_ROLL_PTR to a clustered index entry.
 @param[in,out]	entry		clustered index entry
 @param[in]	index		clustered index
@@ -3920,63 +3895,6 @@ static void btr_cur_write_sys(
 	dfield_t* r = dtuple_get_nth_field(entry, index->db_roll_ptr());
 	ut_ad(r->len == DATA_ROLL_PTR_LEN);
 	trx_write_roll_ptr(static_cast<byte*>(r->data), roll_ptr);
-}
-
-/***********************************************************//**
-Writes a redo log record of updating a record in-place. */
-void
-btr_cur_update_in_place_log(
-/*========================*/
-	ulint		flags,		/*!< in: flags */
-	const rec_t*	rec,		/*!< in: record */
-	dict_index_t*	index,		/*!< in: index of the record */
-	const upd_t*	update,		/*!< in: update vector */
-	trx_id_t	trx_id,		/*!< in: transaction id */
-	roll_ptr_t	roll_ptr,	/*!< in: roll ptr */
-	mtr_t*		mtr)		/*!< in: mtr */
-{
-	byte*		log_ptr;
-	const page_t*	page	= page_align(rec);
-	ut_ad(flags < 256);
-	ut_ad(!!page_is_comp(page) == dict_table_is_comp(index->table));
-
-	log_ptr = mlog_open_and_write_index(mtr, rec, index, page_is_comp(page)
-					    ? MLOG_COMP_REC_UPDATE_IN_PLACE
-					    : MLOG_REC_UPDATE_IN_PLACE,
-					    1 + DATA_ROLL_PTR_LEN + 14 + 2
-					    + MLOG_BUF_MARGIN);
-
-	if (!log_ptr) {
-		/* Logging in mtr is switched off during crash recovery */
-		return;
-	}
-
-	/* For secondary indexes, we could skip writing the dummy system fields
-	to the redo log but we have to change redo log parsing of
-	MLOG_REC_UPDATE_IN_PLACE/MLOG_COMP_REC_UPDATE_IN_PLACE or we have to add
-	new redo log record. For now, just write dummy sys fields to the redo
-	log if we are updating a secondary index record.
-	*/
-	mach_write_to_1(log_ptr, flags);
-	log_ptr++;
-
-	if (dict_index_is_clust(index)) {
-		log_ptr = btr_cur_log_sys(index, trx_id, roll_ptr, log_ptr);
-	} else {
-		/* Dummy system fields for a secondary index */
-		/* TRX_ID Position */
-		log_ptr += mach_write_compressed(log_ptr, 0);
-		/* ROLL_PTR */
-		trx_write_roll_ptr(log_ptr, 0);
-		log_ptr += DATA_ROLL_PTR_LEN;
-		/* TRX_ID */
-		log_ptr += mach_u64_write_compressed(log_ptr, 0);
-	}
-
-	mach_write_to_2(log_ptr, page_offset(rec));
-	log_ptr += 2;
-
-	row_upd_index_write_log(update, log_ptr, mtr);
 }
 
 /** Update DB_TRX_ID, DB_ROLL_PTR in a clustered index record.
@@ -4060,6 +3978,190 @@ row_upd_parse_sys_vals(
 }
 
 /***********************************************************//**
+Sets the value of the ith field SQL null bit of an old-style record. */
+static
+void
+rec_set_nth_field_null_bit(
+/*=======================*/
+	rec_t*	rec,	/*!< in: record */
+	ulint	i,	/*!< in: ith field */
+	ibool	val)	/*!< in: value to set */
+{
+	ulint	info;
+
+	if (rec_get_1byte_offs_flag(rec)) {
+
+		info = rec_1_get_field_end_info(rec, i);
+
+		if (val) {
+			info = info | REC_1BYTE_SQL_NULL_MASK;
+		} else {
+			info = info & ~REC_1BYTE_SQL_NULL_MASK;
+		}
+
+		rec_1_set_field_end_info(rec, i, info);
+
+		return;
+	}
+
+	info = rec_2_get_field_end_info(rec, i);
+
+	if (val) {
+		info = info | REC_2BYTE_SQL_NULL_MASK;
+	} else {
+		info = info & ~REC_2BYTE_SQL_NULL_MASK;
+	}
+
+	rec_2_set_field_end_info(rec, i, info);
+}
+
+/***********************************************************//**
+Sets an old-style record field to SQL null.
+The physical size of the field is not changed. */
+static
+void
+rec_set_nth_field_sql_null(
+/*=======================*/
+	rec_t*	rec,	/*!< in: record */
+	ulint	n)	/*!< in: index of the field */
+{
+	ulint	offset;
+
+	offset = rec_get_field_start_offs(rec, n);
+
+	data_write_sql_null(rec + offset, rec_get_nth_field_size(rec, n));
+
+	rec_set_nth_field_null_bit(rec, n, TRUE);
+}
+
+/***********************************************************//**
+This is used to modify the value of an already existing field in a record.
+The previous value must have exactly the same size as the new value. If len
+is UNIV_SQL_NULL then the field is treated as an SQL null.
+For records in ROW_FORMAT=COMPACT (new-style records), len must not be
+UNIV_SQL_NULL unless the field already is SQL null. */
+static
+void
+rec_set_nth_field(
+/*==============*/
+	rec_t*		rec,	/*!< in: record */
+	const offset_t*	offsets,/*!< in: array returned by rec_get_offsets() */
+	ulint		n,	/*!< in: index number of the field */
+	const void*	data,	/*!< in: pointer to the data
+				if not SQL null */
+	ulint		len)	/*!< in: length of the data or UNIV_SQL_NULL */
+{
+	byte*	data2;
+	ulint	len2;
+
+	ut_ad(rec_offs_validate(rec, NULL, offsets));
+	ut_ad(!rec_offs_nth_default(offsets, n));
+
+	if (len == UNIV_SQL_NULL) {
+		if (!rec_offs_nth_sql_null(offsets, n)) {
+			ut_a(!rec_offs_comp(offsets));
+			rec_set_nth_field_sql_null(rec, n);
+		}
+
+		return;
+	}
+
+	data2 = (byte*)rec_get_nth_field(rec, offsets, n, &len2);
+	if (len2 == UNIV_SQL_NULL) {
+		ut_ad(!rec_offs_comp(offsets));
+		rec_set_nth_field_null_bit(rec, n, FALSE);
+		ut_ad(len == rec_get_nth_field_size(rec, n));
+	} else {
+		ut_ad(len2 == len);
+	}
+
+	memcpy(data2, data, len);
+}
+
+/***********************************************************//**
+Replaces the new column values stored in the update vector to the
+record given. No field size changes are allowed. This function is
+usually invoked on a clustered index. The only use case for a
+secondary index is row_ins_sec_index_entry_by_modify() or its
+counterpart in ibuf_insert_to_index_page(). */
+static
+void
+row_upd_rec_in_place(
+/*=================*/
+	rec_t*		rec,	/*!< in/out: record where replaced */
+	dict_index_t*	index,	/*!< in: the index the record belongs to */
+	const offset_t*	offsets,/*!< in: array returned by rec_get_offsets() */
+	const upd_t*	update,	/*!< in: update vector */
+	page_zip_des_t*	page_zip,/*!< in: compressed page with enough space
+				available, or NULL */
+	mtr_t*		mtr)	/*!< in/out: mini-transaction */
+{
+	const upd_field_t*	upd_field;
+	const dfield_t*		new_val;
+	ulint			n_fields;
+	ulint			i;
+
+	ut_ad(rec_offs_validate(rec, index, offsets));
+	ut_ad(!index->table->skip_alter_undo);
+
+	if (rec_offs_comp(offsets)) {
+#ifdef UNIV_DEBUG
+		switch (rec_get_status(rec)) {
+		case REC_STATUS_ORDINARY:
+			break;
+		case REC_STATUS_INSTANT:
+			ut_ad(index->is_instant());
+			break;
+		case REC_STATUS_NODE_PTR:
+			if (index->is_dummy
+			    && fil_page_get_type(page_align(rec))
+			    == FIL_PAGE_RTREE) {
+				/* The function rtr_update_mbr_field_in_place()
+				is generating MLOG_COMP_REC_UPDATE_IN_PLACE
+				and MLOG_REC_UPDATE_IN_PLACE records for
+				node pointer pages. */
+				break;
+			}
+			/* fall through */
+		case REC_STATUS_INFIMUM:
+		case REC_STATUS_SUPREMUM:
+			ut_ad(!"wrong record status in update");
+		}
+#endif /* UNIV_DEBUG */
+
+		rec_set_bit_field_1(rec, update->info_bits, REC_NEW_INFO_BITS,
+				    REC_INFO_BITS_MASK, REC_INFO_BITS_SHIFT);
+	} else {
+		rec_set_bit_field_1(rec, update->info_bits, REC_OLD_INFO_BITS,
+				    REC_INFO_BITS_MASK, REC_INFO_BITS_SHIFT);
+	}
+
+	n_fields = upd_get_n_fields(update);
+
+	for (i = 0; i < n_fields; i++) {
+		upd_field = upd_get_nth_field(update, i);
+
+		/* No need to update virtual columns for non-virtual index */
+		if (upd_fld_is_virtual_col(upd_field)
+		    && !dict_index_has_virtual(index)) {
+			continue;
+		}
+
+		new_val = &(upd_field->new_val);
+		ut_ad(!dfield_is_ext(new_val) ==
+		      !rec_offs_nth_extern(offsets, upd_field->field_no));
+
+		rec_set_nth_field(rec, offsets, upd_field->field_no,
+				  dfield_get_data(new_val),
+				  dfield_get_len(new_val));
+	}
+
+	if (page_zip) {
+		page_zip_write_rec(page_zip, rec, index, offsets, 0, mtr);
+	}
+}
+
+/***********************************************************//**
 Parses a redo log record of updating a record in-place.
 @return end of log record or NULL */
 ATTRIBUTE_COLD /* only used when crash-upgrading */
@@ -4070,7 +4172,8 @@ btr_cur_parse_update_in_place(
 	const byte*	end_ptr,/*!< in: buffer end */
 	page_t*		page,	/*!< in/out: page or NULL */
 	page_zip_des_t*	page_zip,/*!< in/out: compressed page, or NULL */
-	dict_index_t*	index)	/*!< in: index corresponding to page */
+	dict_index_t*	index,	/*!< in: index corresponding to page */
+	mtr_t*		mtr)	/*!< in/out: mini-transaction */
 {
 	ulint		flags;
 	rec_t*		rec;
@@ -4146,7 +4249,7 @@ btr_cur_parse_update_in_place(
 		trx_write_roll_ptr(field + DATA_TRX_ID_LEN, roll_ptr);
 	}
 
-	row_upd_rec_in_place(rec, index, offsets, update, page_zip);
+	row_upd_rec_in_place(rec, index, offsets, update, page_zip, mtr);
 
 func_exit:
 	mem_heap_free(heap);
@@ -4239,6 +4342,114 @@ out_of_space:
 	}
 
 	return(false);
+}
+
+/** Apply an update vector to a record. No field size changes are allowed.
+
+This is usually invoked on a clustered index. The only use case for a
+secondary index is row_ins_sec_index_entry_by_modify() or its
+counterpart in ibuf_insert_to_index_page().
+@param[in,out]  rec     index record
+@param[in]      index   the index of the record
+@param[in]      offsets rec_get_offsets(rec, index)
+@param[in]      update  update vector
+@param[in,out]  block   index page
+@param[in,out]  mtr     mini-transaction */
+void btr_cur_upd_rec_in_place(rec_t *rec, const dict_index_t *index,
+                              const offset_t *offsets, const upd_t *update,
+                              buf_block_t *block, mtr_t *mtr)
+{
+	ut_ad(rec_offs_validate(rec, index, offsets));
+	ut_ad(!index->table->skip_alter_undo);
+	ut_ad(!block->page.zip.data || index->table->not_redundant());
+
+#ifdef UNIV_DEBUG
+	if (rec_offs_comp(offsets)) {
+		switch (rec_get_status(rec)) {
+		case REC_STATUS_ORDINARY:
+			break;
+		case REC_STATUS_INSTANT:
+			ut_ad(index->is_instant());
+			break;
+		case REC_STATUS_NODE_PTR:
+		case REC_STATUS_INFIMUM:
+		case REC_STATUS_SUPREMUM:
+			ut_ad(!"wrong record status in update");
+		}
+	}
+#endif /* UNIV_DEBUG */
+
+	byte* info_bits = &rec[rec_offs_comp(offsets)
+			       ? -REC_NEW_INFO_BITS
+			       : -REC_OLD_INFO_BITS];
+	compile_time_assert(REC_INFO_BITS_SHIFT == 0);
+	if ((*info_bits & REC_INFO_BITS_MASK) == update->info_bits) {
+	} else if (UNIV_LIKELY_NULL(block->page.zip.data)) {
+		*info_bits &= ~REC_INFO_BITS_MASK;
+		*info_bits |= update->info_bits;
+	} else {
+		mtr->write<1>(*block, info_bits,
+			      (*info_bits & ~REC_INFO_BITS_MASK)
+			      | update->info_bits);
+	}
+
+	for (ulint i = 0; i < update->n_fields; i++) {
+		const upd_field_t* uf = upd_get_nth_field(update, i);
+		if (upd_fld_is_virtual_col(uf) && !index->has_virtual()) {
+			continue;
+		}
+		const ulint n = uf->field_no;
+
+		ut_ad(!dfield_is_ext(&uf->new_val)
+		      == !rec_offs_nth_extern(offsets, n));
+		ut_ad(!rec_offs_nth_default(offsets, n));
+
+		if (UNIV_UNLIKELY(dfield_is_null(&uf->new_val))) {
+			ut_ad(!rec_offs_nth_sql_null(offsets, n));
+			ut_ad(!index->table->not_redundant());
+			mtr->memset(block,
+				    page_offset(rec + rec_get_field_start_offs(
+							rec, n)),
+				    rec_get_nth_field_size(rec, n), 0);
+			ulint l = rec_get_1byte_offs_flag(rec)
+				? (n + 1) : (n + 1) * 2;
+			byte* b = &rec[-REC_N_OLD_EXTRA_BYTES - l];
+			compile_time_assert(REC_1BYTE_SQL_NULL_MASK << 8
+					    == REC_2BYTE_SQL_NULL_MASK);
+			mtr->write<1>(*block, b,
+				      byte(*b | REC_1BYTE_SQL_NULL_MASK));
+			continue;
+		}
+
+		ulint len;
+		byte* data = rec_get_nth_field(rec, offsets, n, &len);
+		if (UNIV_LIKELY_NULL(block->page.zip.data)) {
+			ut_ad(len == uf->new_val.len);
+			memcpy(data, uf->new_val.data, len);
+			continue;
+		}
+
+		if (UNIV_UNLIKELY(len != uf->new_val.len)) {
+			ut_ad(len == UNIV_SQL_NULL);
+			ut_ad(!rec_offs_comp(offsets));
+			len = uf->new_val.len;
+			ut_ad(len == rec_get_nth_field_size(rec, n));
+			ulint l = rec_get_1byte_offs_flag(rec)
+				? (n + 1) : (n + 1) * 2;
+			byte* b = &rec[-REC_N_OLD_EXTRA_BYTES - l];
+			compile_time_assert(REC_1BYTE_SQL_NULL_MASK << 8
+					    == REC_2BYTE_SQL_NULL_MASK);
+			mtr->write<1>(*block, b,
+				      byte(*b & ~REC_1BYTE_SQL_NULL_MASK));
+		}
+
+		mtr->memcpy(block, page_offset(data), uf->new_val.data, len);
+	}
+
+	if (UNIV_LIKELY_NULL(block->page.zip.data)) {
+		page_zip_write_rec(&block->page.zip, rec, index, offsets, 0,
+				   mtr);
+	}
 }
 
 /*************************************************************//**
@@ -4364,7 +4575,8 @@ btr_cur_update_in_place(
 		assert_block_ahi_valid(block);
 #endif /* BTR_CUR_HASH_ADAPT */
 
-		row_upd_rec_in_place(rec, index, offsets, update, page_zip);
+		btr_cur_upd_rec_in_place(rec, index, offsets, update, block,
+					 mtr);
 
 #ifdef BTR_CUR_HASH_ADAPT
 		if (ahi_latch) {
@@ -4372,9 +4584,6 @@ btr_cur_update_in_place(
 		}
 	}
 #endif /* BTR_CUR_HASH_ADAPT */
-
-	btr_cur_update_in_place_log(flags, rec, index, update,
-				    trx_id, roll_ptr, mtr);
 
 	if (was_delete_marked
 	    && !rec_get_deleted_flag(

mysql-test/suite/innodb/t/innodb-index-online.opt
@@ -1,6 +1,5 @@
 --loose-innodb-sort-buffer-size=64k
 --loose-innodb-online-alter-log-max-size=128k
 --loose-innodb-buffer-pool-size=5M
---loose-innodb-log-buffer-size=256k
 --loose-innodb-sys-indexes
 --loose-innodb-sys-fields

mysql-test/suite/innodb/t/innodb_defrag_concurrent.opt
@@ -1,5 +1,5 @@
 --loose-innodb-buffer-pool-stats
 --loose-innodb-buffer-page
 --loose-innodb-buffer-page-lru
---innodb-log-buffer-size=2m
+--innodb-log-buffer-size=6m
 --innodb-defragment=1

storage/innobase/btr/btr0bulk.cc
@@ -163,83 +163,74 @@ PageBulk::init()
 template<PageBulk::format fmt>
 inline void PageBulk::insertPage(const rec_t *rec, offset_t *offsets)
 {
-	ut_ad((m_page_zip != nullptr) == (fmt == COMPRESSED));
-	ut_ad((fmt != REDUNDANT) == m_is_comp);
-	ulint		rec_size;
+  ut_ad((m_page_zip != nullptr) == (fmt == COMPRESSED));
+  ut_ad((fmt != REDUNDANT) == m_is_comp);
 
-	ut_ad(m_heap != NULL);
+  ut_ad(m_heap);
 
-	rec_size = rec_offs_size(offsets);
-	ut_d(const bool is_leaf = page_rec_is_leaf(m_cur_rec));
+  ulint rec_size= rec_offs_size(offsets);
+  ut_d(const bool is_leaf= page_rec_is_leaf(m_cur_rec));
 
 #ifdef UNIV_DEBUG
-	/* Check whether records are in order. */
-	if (!page_rec_is_infimum_low(page_offset(m_cur_rec))) {
-		rec_t*	old_rec = m_cur_rec;
-		offset_t* old_offsets = rec_get_offsets(
-			old_rec, m_index, NULL,	is_leaf,
-			ULINT_UNDEFINED, &m_heap);
+  /* Check whether records are in order. */
+  if (!page_rec_is_infimum_low(page_offset(m_cur_rec)))
+  {
+    const rec_t *old_rec = m_cur_rec;
+    offset_t *old_offsets= rec_get_offsets(old_rec, m_index, nullptr, is_leaf,
+                                           ULINT_UNDEFINED, &m_heap);
+    ut_ad(cmp_rec_rec(rec, old_rec, offsets, old_offsets, m_index) > 0);
+  }
 
-		ut_ad(cmp_rec_rec(rec, old_rec, offsets, old_offsets, m_index)
-		      > 0);
-	}
-
-	m_total_data += rec_size;
+  m_total_data+= rec_size;
 #endif /* UNIV_DEBUG */
 
-	/* 1. Copy the record to page. */
-	rec_t*	insert_rec = rec_copy(m_heap_top, rec, offsets);
-	ut_ad(page_align(insert_rec) == m_page);
-	rec_offs_make_valid(insert_rec, m_index, is_leaf, offsets);
+  /* Copy the record payload. */
+  rec_t *insert_rec= rec_copy(m_heap_top, rec, offsets);
+  ut_ad(page_align(insert_rec) == m_page);
+  rec_offs_make_valid(insert_rec, m_index, is_leaf, offsets);
 
-	/* 2. Insert the record in the linked list. */
-	if (fmt != REDUNDANT) {
-		rec_t* next_rec = m_page
-			+ page_offset(m_cur_rec
-				      + mach_read_from_2(m_cur_rec
-							 - REC_NEXT));
-		mach_write_to_2(insert_rec - REC_NEXT,
-				static_cast<uint16_t>(next_rec - insert_rec));
-		mach_write_to_2(m_cur_rec - REC_NEXT,
-				static_cast<uint16_t>(insert_rec - m_cur_rec));
-		rec_set_bit_field_1(insert_rec, 0, REC_NEW_N_OWNED,
-				    REC_N_OWNED_MASK, REC_N_OWNED_SHIFT);
-		rec_set_bit_field_2(insert_rec,
-				    PAGE_HEAP_NO_USER_LOW + m_rec_no,
-				    REC_NEW_HEAP_NO,
-				    REC_HEAP_NO_MASK, REC_HEAP_NO_SHIFT);
-	} else {
-		mach_write_to_2(insert_rec - REC_NEXT,
-				mach_read_from_2(m_cur_rec - REC_NEXT));
-		mach_write_to_2(m_cur_rec - REC_NEXT, page_offset(insert_rec));
-		rec_set_bit_field_1(insert_rec, 0, REC_OLD_N_OWNED,
-				    REC_N_OWNED_MASK, REC_N_OWNED_SHIFT);
-		rec_set_bit_field_2(insert_rec,
-				    PAGE_HEAP_NO_USER_LOW + m_rec_no,
-				    REC_OLD_HEAP_NO,
-				    REC_HEAP_NO_MASK, REC_HEAP_NO_SHIFT);
-	}
+  /* Insert the record in the linked list. */
+  if (fmt != REDUNDANT)
+  {
+    rec_t *next_rec= m_page +
+      page_offset(m_cur_rec + mach_read_from_2(m_cur_rec - REC_NEXT));
+    mach_write_to_2(insert_rec - REC_NEXT,
+                    static_cast<uint16_t>(next_rec - insert_rec));
+    if (fmt != COMPRESSED)
+      m_mtr.write<2>(*m_block, m_cur_rec - REC_NEXT,
+                     static_cast<uint16_t>(insert_rec - m_cur_rec));
+    else
+      mach_write_to_2(m_cur_rec - REC_NEXT,
+                      static_cast<uint16_t>(insert_rec - m_cur_rec));
+    rec_set_bit_field_1(insert_rec, 0, REC_NEW_N_OWNED, REC_N_OWNED_MASK,
+                        REC_N_OWNED_SHIFT);
+    rec_set_bit_field_2(insert_rec, PAGE_HEAP_NO_USER_LOW + m_rec_no,
+                        REC_NEW_HEAP_NO, REC_HEAP_NO_MASK, REC_HEAP_NO_SHIFT);
+  }
+  else
+  {
+    memcpy(insert_rec - REC_NEXT, m_cur_rec - REC_NEXT, 2);
+    m_mtr.write<2>(*m_block, m_cur_rec - REC_NEXT, page_offset(insert_rec));
+    rec_set_bit_field_1(insert_rec, 0, REC_OLD_N_OWNED, REC_N_OWNED_MASK,
+                        REC_N_OWNED_SHIFT);
+    rec_set_bit_field_2(insert_rec, PAGE_HEAP_NO_USER_LOW + m_rec_no,
+                        REC_OLD_HEAP_NO, REC_HEAP_NO_MASK, REC_HEAP_NO_SHIFT);
+  }
 
-	/* 4. Set member variables. */
-	ulint		slot_size;
-	slot_size = page_dir_calc_reserved_space(m_rec_no + 1)
-		- page_dir_calc_reserved_space(m_rec_no);
+  if (fmt != COMPRESSED)
+    m_mtr.memcpy(*m_block, page_offset(m_heap_top), rec_offs_size(offsets));
 
-	ut_ad(m_free_space >= rec_size + slot_size);
-	ut_ad(m_heap_top + rec_size < m_page + srv_page_size);
+  /* Update the member variables. */
+  ulint slot_size= page_dir_calc_reserved_space(m_rec_no + 1) -
+    page_dir_calc_reserved_space(m_rec_no);
 
-	m_free_space -= rec_size + slot_size;
-	m_heap_top += rec_size;
-	m_rec_no += 1;
+  ut_ad(m_free_space >= rec_size + slot_size);
+  ut_ad(m_heap_top + rec_size < m_page + srv_page_size);
 
-	if (fmt != COMPRESSED) {
-		/* For ROW_FORMAT=COMPRESSED, redo log may be written
-		in PageBulk::compress(). */
-		page_cur_insert_rec_write_log(insert_rec, rec_size,
-					      m_cur_rec, m_index, &m_mtr);
-	}
-
-	m_cur_rec = insert_rec;
+  m_free_space-= rec_size + slot_size;
+  m_heap_top+= rec_size;
+  m_rec_no++;
+  m_cur_rec= insert_rec;
 }
 
 /** Insert a record in the page.
@@ -253,6 +244,14 @@ inline void PageBulk::insert(const rec_t *rec, offset_t *offsets)
     insertPage<DYNAMIC>(rec, offsets);
   else
     insertPage<REDUNDANT>(rec, offsets);
+}
+
+/** Set the number of owned records in the uncompressed page of
+a ROW_FORMAT=COMPRESSED record without redo-logging. */
+static void rec_set_n_owned_zip(rec_t *rec, ulint n_owned)
+{
+  rec_set_bit_field_1(rec, n_owned, REC_NEW_N_OWNED,
+                      REC_N_OWNED_MASK, REC_N_OWNED_SHIFT);
 }
 
 /** Mark end of insertion to the page. Scan all records to set page dirs,
@@ -287,9 +286,19 @@ inline void PageBulk::finishPage()
       if (count == (PAGE_DIR_SLOT_MAX_N_OWNED + 1) / 2)
       {
         slot-= PAGE_DIR_SLOT_SIZE;
-        mach_write_to_2(slot, offset);
-        rec_set_bit_field_1(m_page + offset, count, REC_NEW_N_OWNED,
-                            REC_N_OWNED_MASK, REC_N_OWNED_SHIFT);
+
+        if (fmt != COMPRESSED)
+        {
+          m_mtr.write<2,mtr_t::OPT>(*m_block, slot, offset);
+          page_rec_set_n_owned<false>(m_block, m_page + offset, count, true,
+                                      &m_mtr);
+        }
+        else
+        {
+          mach_write_to_2(slot, offset);
+          rec_set_n_owned_zip(m_page + offset, count);
+        }
+
         count= 0;
       }
 
@@ -299,6 +308,33 @@ inline void PageBulk::finishPage()
       offset= next;
     }
     while (offset != PAGE_NEW_SUPREMUM);
+
+    if (slot0 != slot && (count + 1 + (PAGE_DIR_SLOT_MAX_N_OWNED + 1) / 2 <=
+                          PAGE_DIR_SLOT_MAX_N_OWNED))
+    {
+      /* Merge the last two slots, like page_cur_insert_rec_low() does. */
+      count+= (PAGE_DIR_SLOT_MAX_N_OWNED + 1) / 2;
+
+      rec_t *rec= const_cast<rec_t*>(page_dir_slot_get_rec(slot));
+      if (fmt != COMPRESSED)
+        page_rec_set_n_owned<false>(m_block, rec, 0, true, &m_mtr);
+      else
+        rec_set_n_owned_zip(rec, 0);
+    }
+    else
+      slot-= PAGE_DIR_SLOT_SIZE;
+
+    if (fmt != COMPRESSED)
+    {
+      m_mtr.write<2,mtr_t::OPT>(*m_block, slot, PAGE_NEW_SUPREMUM);
+      page_rec_set_n_owned<false>(m_block, m_page + PAGE_NEW_SUPREMUM,
+                                  count + 1, true, &m_mtr);
+    }
+    else
+    {
+      mach_write_to_2(slot, PAGE_NEW_SUPREMUM);
+      rec_set_n_owned_zip(m_page + PAGE_NEW_SUPREMUM, count + 1);
+    }
   }
   else
   {
@@ -314,46 +350,30 @@ inline void PageBulk::finishPage()
       if (count == (PAGE_DIR_SLOT_MAX_N_OWNED + 1) / 2)
       {
         slot-= PAGE_DIR_SLOT_SIZE;
-        mach_write_to_2(slot, page_offset(insert_rec));
-        rec_set_bit_field_1(insert_rec, count, REC_OLD_N_OWNED,
-                            REC_N_OWNED_MASK, REC_N_OWNED_SHIFT);
+        m_mtr.write<2,mtr_t::OPT>(*m_block, slot, page_offset(insert_rec));
+        page_rec_set_n_owned<false>(m_block, insert_rec, count, false, &m_mtr);
         count= 0;
       }
 
       insert_rec= m_page + mach_read_from_2(insert_rec - REC_NEXT);
     }
     while (insert_rec != m_page + PAGE_OLD_SUPREMUM);
-  }
 
-  if (slot0 != slot && (count + 1 + (PAGE_DIR_SLOT_MAX_N_OWNED + 1) / 2 <=
-                        PAGE_DIR_SLOT_MAX_N_OWNED)) {
-    /* We can merge the two last dir slots. This operation is here to
-    make this function imitate exactly the equivalent task made using
-    page_cur_insert_rec(), which we use in database recovery to
-    reproduce the task performed by this function. To be able to
-    check the correctness of recovery, it is good that it imitates exactly. */
+    if (slot0 != slot && (count + 1 + (PAGE_DIR_SLOT_MAX_N_OWNED + 1) / 2 <=
+                          PAGE_DIR_SLOT_MAX_N_OWNED))
+    {
+      /* Merge the last two slots, like page_cur_insert_rec_low() does. */
+      count+= (PAGE_DIR_SLOT_MAX_N_OWNED + 1) / 2;
 
-    count+= (PAGE_DIR_SLOT_MAX_N_OWNED + 1) / 2;
+      rec_t *rec= const_cast<rec_t*>(page_dir_slot_get_rec(slot));
+      page_rec_set_n_owned<false>(m_block, rec, 0, false, &m_mtr);
+    }
+    else
+      slot-= PAGE_DIR_SLOT_SIZE;
 
-    rec_t *rec= const_cast<rec_t*>(page_dir_slot_get_rec(slot));
-    rec_set_bit_field_1(rec, 0, m_is_comp ? REC_NEW_N_OWNED : REC_OLD_N_OWNED,
-                        REC_N_OWNED_MASK, REC_N_OWNED_SHIFT);
-    slot+= PAGE_DIR_SLOT_SIZE;
-  }
-
-  slot-= PAGE_DIR_SLOT_SIZE;
-
-  if (m_is_comp)
-  {
-    mach_write_to_2(slot, PAGE_NEW_SUPREMUM);
-    rec_set_bit_field_1(m_page + PAGE_NEW_SUPREMUM, count + 1, REC_NEW_N_OWNED,
-                        REC_N_OWNED_MASK, REC_N_OWNED_SHIFT);
-  }
-  else
-  {
-    mach_write_to_2(slot, PAGE_OLD_SUPREMUM);
-    rec_set_bit_field_1(m_page + PAGE_OLD_SUPREMUM, count + 1, REC_OLD_N_OWNED,
-                        REC_N_OWNED_MASK, REC_N_OWNED_SHIFT);
+    m_mtr.write<2,mtr_t::OPT>(*m_block, slot, PAGE_OLD_SUPREMUM);
+    page_rec_set_n_owned<false>(m_block, m_page + PAGE_OLD_SUPREMUM, count + 1,
+                                false, &m_mtr);
   }
 
   ut_ad(!dict_index_is_spatial(m_index));
@@ -386,8 +406,7 @@ inline void PageBulk::finishPage()
     mach_write_to_2(PAGE_HEADER + PAGE_HEAP_TOP + m_page,
                     static_cast<ulint>(m_heap_top - m_page));
     mach_write_to_2(PAGE_HEADER + PAGE_N_HEAP + m_page,
-                    (PAGE_HEAP_NO_USER_LOW + m_rec_no) |
-                    uint16_t{fmt != REDUNDANT} << 15);
+                    (PAGE_HEAP_NO_USER_LOW + m_rec_no) | 1U << 15);
     mach_write_to_2(PAGE_HEADER + PAGE_N_RECS + m_page, m_rec_no);
     mach_write_to_2(PAGE_HEADER + PAGE_LAST_INSERT + m_page,
                     static_cast<ulint>(m_cur_rec - m_page));
@@ -563,7 +582,10 @@ PageBulk::copyOut(
 	offsets = rec_get_offsets(rec, m_index, offsets,
 				  page_rec_is_leaf(split_rec),
 				  ULINT_UNDEFINED, &m_heap);
-	page_rec_set_next(rec, page_get_supremum_rec(m_page));
+	mach_write_to_2(rec - REC_NEXT, m_is_comp
+			? static_cast<uint16_t>
+			(PAGE_NEW_SUPREMUM - page_offset(rec))
+			: PAGE_OLD_SUPREMUM);
 
 	/* Set related members */
 	m_cur_rec = rec;

commit adb411763119873e37176015a97d749ad531240a

MDEV-21892: Assertion ...row_get_rec_trx_id... failed on SELECT
btr_cur_upd_rec_in_place(): Invoke page_zip_rec_set_deleted()
for ROW_FORMAT=COMPRESSED pages, so that the change will be
written to the redo log.

This part of crash recovery was broken in
commit 08ba388713946c03aa591899cd3a446a6202f882 (MDEV-12353).

storage/innobase/btr/btr0cur.cc
@@ -4110,16 +4110,25 @@ void btr_cur_upd_rec_in_place(rec_t *rec, const dict_index_t *index,
 	}
 #endif /* UNIV_DEBUG */
 
-	byte* info_bits = &rec[rec_offs_comp(offsets)
-			       ? -REC_NEW_INFO_BITS
-			       : -REC_OLD_INFO_BITS];
-	compile_time_assert(REC_INFO_BITS_SHIFT == 0);
-	if ((*info_bits & REC_INFO_BITS_MASK) == update->info_bits) {
-	} else if (UNIV_LIKELY_NULL(block->page.zip.data)) {
+	static_assert(REC_INFO_BITS_SHIFT == 0, "compatibility");
+	if (UNIV_LIKELY_NULL(block->page.zip.data)) {
+		ut_ad(rec_offs_comp(offsets));
+		byte* info_bits = &rec[-REC_NEW_INFO_BITS];
+		const bool flip_del_mark = (*info_bits ^ update->info_bits)
+			& REC_INFO_DELETED_FLAG;
 		*info_bits &= ~REC_INFO_BITS_MASK;
 		*info_bits |= update->info_bits;
+
+		if (flip_del_mark) {
+			page_zip_rec_set_deleted(block, rec, update->info_bits
+						 & REC_INFO_DELETED_FLAG, mtr);
+		}
 	} else {
-		mtr->write<1>(*block, info_bits,
+		byte* info_bits = &rec[rec_offs_comp(offsets)
+				       ? -REC_NEW_INFO_BITS
+				       : -REC_OLD_INFO_BITS];
+
+		mtr->write<1,mtr_t::OPT>(*block, info_bits,
 			      (*info_bits & ~REC_INFO_BITS_MASK)
 			      | update->info_bits);
 	}

mysql-test/suite/innodb_zip/r/recover.result
@@ -1,3 +1,14 @@
+FLUSH TABLES;
+#
+# MDEV-21892 Assertion 'index != clust_index || row_get_rec_trx_id()'
+#
+connect  con1,localhost,root;
+CREATE TABLE t1 (pk INT PRIMARY KEY) ENGINE=InnoDB ROW_FORMAT=COMPRESSED;
+INSERT INTO t1 VALUES (1),(2);
+BEGIN;
+UPDATE t1 SET pk=1;
+ERROR 23000: Duplicate entry '1' for key 'PRIMARY'
+connection default;
 #
 # MDEV-12720 recovery fails with "Generic error"
 # for ROW_FORMAT=compressed
@@ -12,6 +23,12 @@ insert into a select null, uuid() from a a, a b, a c;
 SET GLOBAL innodb_flush_log_at_trx_commit=1;
 COMMIT;
 # restart
+disconnect con1;
+SELECT * FROM t1;
+pk
+1
+2
+DROP TABLE t1;
 SELECT COUNT(*) from a;
 COUNT(*)
 1010

mysql-test/suite/innodb_zip/t/recover.test
@@ -1,12 +1,21 @@
 --source include/innodb_page_size_small.inc
 --source include/not_embedded.inc
 
---disable_query_log
 # This test kills the server, which could corrupt some mysql.* tables
 # that are not created with ENGINE=InnoDB.
 # Flush any non-InnoDB tables to prevent that from happening.
 FLUSH TABLES;
---enable_query_log
+
+--echo #
+--echo # MDEV-21892 Assertion 'index != clust_index || row_get_rec_trx_id()'
+--echo #
+connect (con1,localhost,root);
+CREATE TABLE t1 (pk INT PRIMARY KEY) ENGINE=InnoDB ROW_FORMAT=COMPRESSED;
+INSERT INTO t1 VALUES (1),(2);
+BEGIN;
+--error ER_DUP_ENTRY
+UPDATE t1 SET pk=1;
+connection default;
 
 --echo #
 --echo # MDEV-12720 recovery fails with "Generic error"
@@ -25,6 +34,9 @@ COMMIT;
 
 --let $shutdown_timeout=0
 --source include/restart_mysqld.inc
+disconnect con1;
 
+SELECT * FROM t1;
+DROP TABLE t1;
 SELECT COUNT(*) from a;
 DROP TABLE a;