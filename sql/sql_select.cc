b30446c85dbf0fd402a7833e1042bc13be7fece2
Sergei Petrunia <psergey@askmonty.org>
2020-03-11 13:46:57 +0300

    Fix compile warning:
    It was:
    implicit conversion from 'ha_rows' (aka 'unsigned long long') to 'double'
    changes value from 18446744073709551615 to 18446744073709551616
    
    Follow what JOIN::get_examined_rows() does for similar code.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -4267,7 +4267,7 @@ make_join_statistics(JOIN *join, List<TABLE_LIST> &tables_list,
       for (i= 0; i < join->table_count ; i++)
         if (double rr= join->best_positions[i].records_read)
           records= COST_MULT(records, rr);
-      ha_rows rows= records > HA_ROWS_MAX ? HA_ROWS_MAX : (ha_rows) records;
+      ha_rows rows= records > (double) HA_ROWS_MAX ? HA_ROWS_MAX : (ha_rows) records;
       set_if_smaller(rows, unit->select_limit_cnt);
       join->select_lex->increase_derived_records(rows);
     }
