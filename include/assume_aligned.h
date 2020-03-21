76cdc1d73ec0215c91725a8507da7bda416c52ec
Marko Mäkelä <marko.makela@mariadb.com>
2020-03-21 10:25:54 +0200

    Remove a misleading copyright message
    The code that commit bb24fa31fa28d68ed4e38ecbd8d49773d22c96df
    moved to a separate file assume_aligned.h was introduced in
    commit 25e2a556de2e125784d52a0c7ccda4fa6595df50 and developed
    by employees of MariaDB Corporation.

--- include/assume_aligned.h
+++ include/assume_aligned.h
@@ -1,5 +1,4 @@
 /*
-   Copyright (c) 2000, 2012, Oracle and/or its affiliates.
    Copyright (c) 2020, MariaDB Corporation.
 
    This program is free software; you can redistribute it and/or modify
