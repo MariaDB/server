a7cbce06d432cbcb88e071731089aacfd41750fd
Eugene Kosov <claprix@yandex.ru>
2020-03-23 16:47:43 +0300

    unoptimized -fsanitize=undefined build on clang requires more stack space

--- include/my_pthread.h
+++ include/my_pthread.h
@@ -693,7 +693,7 @@ extern void my_mutex_end(void);
   We need to have at least 256K stack to handle calls to myisamchk_init()
   with the current number of keys and key parts.
 */
-#ifdef __SANITIZE_ADDRESS__
+#if defined(__SANITIZE_ADDRESS__) || defined(WITH_UBSAN)
 #define DEFAULT_THREAD_STACK	(364*1024L)
 #else
 #define DEFAULT_THREAD_STACK	(292*1024L)
