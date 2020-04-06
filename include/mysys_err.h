68e2d95d5e829d4ebaee1f16fdf6b14688c3d25b
Daniel Black <daniel@linux.ibm.com>
2020-04-06 13:35:34 +1000

    add EE_BADMEMORY_RELEASE

--- include/mysys_err.h
+++ include/mysys_err.h
@@ -69,7 +69,8 @@ extern const char *globerrs[];  /* my_error_messages is here */
 #define EE_CANT_SEEK            33
 #define EE_CANT_CHMOD           34
 #define EE_CANT_COPY_OWNERSHIP  35
-#define EE_ERROR_LAST           35 /* Copy last error nr */
+#define EE_BADMEMORYRELEASE     36
+#define EE_ERROR_LAST           36 /* Copy last error nr */
 
 /* Add error numbers before EE_ERROR_LAST and change it accordingly. */
 
