610bb1d2c078075d780aee644428326ac5511e7e
Monty <monty@mariadb.org>
2020-04-26 15:01:12 +0300

    Fixed bug thr_lock_info_init
    This bug didn't cause any issues with current usage

--- mysys/thr_lock.c
+++ mysys/thr_lock.c
@@ -468,7 +468,7 @@ void thr_lock_delete(THR_LOCK *lock)
 
 void thr_lock_info_init(THR_LOCK_INFO *info, struct st_my_thread_var *tmp)
 {
-  if (tmp)
+  if (!tmp)
     tmp= my_thread_var;
   info->thread=    tmp->pthread_self;
   info->thread_id= tmp->id;
