d01d94d77bbb663caca2f34f4b25d5db028f4c4d
Sergei Petrunia <psergey@askmonty.org>
2020-05-06 23:44:34 +0300

    MDEV-17568: LATERAL DERIVED is not clearly visible in EXPLAIN FORMAT=JSON
    Make LATERAL DERIVED tables visible in EXPLAIN FORMAT=JSON output.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -26014,6 +26014,8 @@ int JOIN::save_explain_data_intern(Explain_query *output,
     xpl_sel->select_id= join->select_lex->select_number;
     xpl_sel->select_type= join->select_lex->type;
     xpl_sel->linkage= select_lex->linkage;
+    xpl_sel->is_lateral= ((select_lex->linkage == DERIVED_TABLE_TYPE) &&
+                          (select_lex->uncacheable & UNCACHEABLE_DEPENDENT));
     if (select_lex->master_unit()->derived)
       xpl_sel->connection_type= Explain_node::EXPLAIN_NODE_DERIVED;
     
