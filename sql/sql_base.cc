6b521ac003bbfaa346814b28fe652060c8638658
Sergei Golubchik <serg@mariadb.org>
2020-05-08 01:52:31 +0200

    MDEV-22180 Planner opens unnecessary tables when updated table is referenced by foreign keys
    under LOCK TABLES we still have to open everything, otherwise DML
    prelocking will try to take an MDL on a table that wasn't in the
    LOCK TABLES list.

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -5037,9 +5037,18 @@ bool Lock_tables_prelocking_strategy::
 handle_table(THD *thd, Query_tables_list *prelocking_ctx,
              TABLE_LIST *table_list, bool *need_prelocking)
 {
+  TABLE_LIST **last= prelocking_ctx->query_tables_last;
+
   if (DML_prelocking_strategy::handle_table(thd, prelocking_ctx, table_list,
                                             need_prelocking))
     return TRUE;
+
+  /*
+    normally we don't need to open FK-prelocked tables for RESTRICT,
+    MDL is enough. But under LOCK TABLES we have to open everything
+  */
+  for (TABLE_LIST *tl= *last; tl; tl= tl->next_global)
+    tl->open_strategy= TABLE_LIST::OPEN_NORMAL;
 
   /* We rely on a caller to check that table is going to be changed. */
   DBUG_ASSERT(table_list->lock_type >= TL_WRITE_ALLOW_WRITE);
