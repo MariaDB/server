5661e7538ece6cacfd9b40be680725d09ca44910
Daniel Black <daniel@linux.ibm.com>
2020-06-17 21:29:16 +1000

    libutils: merge_archives_unix
    MRI scripts cannot handle + in paths, and ubuntu CI makes use of
    these.
    
    So we remove the top level build dir from the script and
    transform it into a relative path script.

--- cmake/merge_archives_unix.cmake
+++ cmake/merge_archives_unix.cmake
@@ -14,7 +14,21 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1335  USA 
 
+
+# MRI scripts have a problem with +. It's a line contination character
+# unfortunately there is no escape character. We know we don't have
+# "+" in libraries or the MariaDB paths, but Ubuntu CI builds will have
+# in their CI path due to the package names that Ubuntu generates.
+# So here we replace the fully expanded paths in the TARGET_SCRIPT,
+# strip off the TOP_DIR to make it a relative path to the top level directory
+# and then execute AR on the top level directory.
+
+FILE(READ ${TARGET_SCRIPT} SCRIPT_CONTENTS)
+STRING(REPLACE "${TOP_DIR}/" "" SCRIPT_CONTENTS_TRIMMED "${SCRIPT_CONTENTS}")
+FILE(WRITE "${TARGET_SCRIPT}.mri" ${SCRIPT_CONTENTS_TRIMMED})
+
 EXECUTE_PROCESS(
+  WORKING_DIRECTORY ${TOP_DIR}
   COMMAND ${CMAKE_AR} -M
-  INPUT_FILE ${TARGET_SCRIPT}
+  INPUT_FILE ${TARGET_SCRIPT}.mri
 )
