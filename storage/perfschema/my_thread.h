fecc6caa27971bd9c049671ddedef8c334e71062
Daniel Black <daniel@linux.ibm.com>
2020-06-22 13:31:59 +1000

    MDEV-22621: perfschema add FreeBSD include header for pthread_getthreadid_np

--- storage/perfschema/my_thread.h
+++ storage/perfschema/my_thread.h
@@ -10,6 +10,10 @@
 #include <sys/syscall.h>
 #endif
 
+#ifdef HAVE_PTHREAD_GETTHREADID_NP
+#include <pthread_np.h>
+#endif
+
 typedef pthread_key_t thread_local_key_t;
 typedef pthread_t my_thread_handle;
 typedef pthread_attr_t my_thread_attr_t;
