b0f836053b094b09999c102d10bf0ad6ed761ac6
Oleksandr Byelkin <sanja@mariadb.com>
2020-06-23 17:07:03 +0200

    MDEV-22983: Mariabackup's --help option disappeared
    return --help option

--- extra/mariabackup/xtrabackup.cc
+++ extra/mariabackup/xtrabackup.cc
@@ -119,6 +119,7 @@ my_bool xtrabackup_move_back;
 my_bool xtrabackup_decrypt_decompress;
 my_bool xtrabackup_print_param;
 my_bool xtrabackup_mysqld_args;
+my_bool xtrabackup_help;
 
 my_bool xtrabackup_export;
 
@@ -1456,6 +1457,11 @@ struct my_option xb_server_options[] =
      "options, and if some of them are not supported by mariabackup, they "
      "will be ignored.",
      (G_PTR *) &xtrabackup_mysqld_args, (G_PTR *) &xtrabackup_mysqld_args, 0,
+     GET_BOOL, NO_ARG, 0, 0, 0, 0, 0, 0},
+
+    {"help", '?',
+     "Display this help and exit.",
+     (G_PTR *) &xtrabackup_help, (G_PTR *) &xtrabackup_help, 0,
      GET_BOOL, NO_ARG, 0, 0, 0, 0, 0, 0},
 
   { 0, 0, 0, 0, 0, 0, GET_NO_ARG, NO_ARG, 0, 0, 0, 0, 0, 0}
