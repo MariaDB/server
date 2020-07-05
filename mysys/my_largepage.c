MariaDB/server

commit ab4069909d1f8d0c4740b5bd90e1422ce1978187

After-merge fix for ASAN and MSAN
The merge commit 0fd89a1a89da73cec8e87e1f007637eaec51dcc0
of commit b6ec1e8bbf0ffca2d715aded694722e0c4b5d484
seems to cause occasional MemorySanitizer failures,
because it failed to replace some MEM_UNDEFINED() calls
with MEM_MAKE_ADDRESSABLE().

my_large_free(): Correctly invoke MEM_MAKE_ADDRESSABLE() after
freeing memory. Failure to do so could cause bogus
AddressSanitizer failures for memory allocated by my_large_malloc().
On MemorySanitizer, we will do nothing.

buf_pool_t::chunk_t::create(): Replace the MEM_MAKE_ADDRESSABLE()
that had been added in commit 484931325e7bcecddc6daf1a55c008ddd67497e4
to work around the issue.

mysys/my_largepage.c
@@ -421,9 +421,11 @@ void my_large_free(void *ptr, size_t size)
   {
     my_error(EE_BADMEMORYRELEASE, MYF(ME_ERROR_LOG_ONLY), ptr, size, errno);
   }
+# if !__has_feature(memory_sanitizer)
   else
   {
-    MEM_UNDEFINED(ptr, size);
+    MEM_MAKE_ADDRESSABLE(ptr, size);
+# endif
   }
 #elif defined(_WIN32)
   /*
@@ -435,9 +437,11 @@ void my_large_free(void *ptr, size_t size)
     my_error(EE_BADMEMORYRELEASE, MYF(ME_ERROR_LOG_ONLY), ptr, size,
              GetLastError());
   }
+# if !__has_feature(memory_sanitizer)
   else
   {
-    MEM_UNDEFINED(ptr, size);
+    MEM_MAKE_ADDRESSABLE(ptr, size);
+# endif
   }
 #else
   my_free_lock(ptr);

storage/innobase/buf/buf0buf.cc
@@ -1356,7 +1356,7 @@ inline bool buf_pool_t::chunk_t::create(size_t bytes)
   if (UNIV_UNLIKELY(!mem))
     return false;
 
-  MEM_MAKE_ADDRESSABLE(mem, mem_size());
+  MEM_UNDEFINED(mem, mem_size());
 
 #ifdef HAVE_LIBNUMA
   if (srv_numa_interleave)

commit 79c166c56db7b48fac682c87f68a221539e58551

Fix cmake -DWITH_MSAN=ON
commit ab4069909d1f8d0c4740b5bd90e1422ce1978187 accidentally
misplaced #endif directives.

mysys/my_largepage.c
@@ -425,8 +425,8 @@ void my_large_free(void *ptr, size_t size)
   else
   {
     MEM_MAKE_ADDRESSABLE(ptr, size);
-# endif
   }
+# endif
 #elif defined(_WIN32)
   /*
      When RELEASE memory, the size parameter must be 0.
@@ -441,8 +441,8 @@ void my_large_free(void *ptr, size_t size)
   else
   {
     MEM_MAKE_ADDRESSABLE(ptr, size);
-# endif
   }
+# endif
 #else
   my_free_lock(ptr);
 #endif