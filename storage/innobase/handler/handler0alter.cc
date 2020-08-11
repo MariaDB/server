c96be848d3bbb668d27645c36ca66c697fdc2752
Marko Mäkelä <marko.makela@mariadb.com>
2020-08-11 18:52:38 +0300

    MDEV-14119 Assertion cmp_rec_rec() in ALTER TABLE
    innobase_pk_order_preserved(): Treat an added AUTO_INCREMENT
    column in the same way as an added existing column.
    In either case, the column values are not guaranteed to
    be constant, and thus the ordering may change if such a column
    is added before any existing PRIMARY KEY columns.
    
    prepare_inplace_alter_table_dict(): Initialize
    dict_table_t::persistent_autoinc before invoking
    innobase_pk_order_preserved().

--- storage/innobase/handler/handler0alter.cc
+++ storage/innobase/handler/handler0alter.cc
@@ -3362,7 +3362,11 @@ innobase_pk_order_preserved(
 		if (old_pk_column) {
 			new_field_order = old_field;
 		} else if (innobase_pk_col_is_existing(new_col_no, col_map,
-						       old_n_cols)) {
+						       old_n_cols)
+			   || new_clust_index->table->persistent_autoinc
+			   == new_field + 1) {
+			/* Adding an existing column or an AUTO_INCREMENT
+			column may change the existing ordering. */
 			new_field_order = old_n_uniq + existing_field_count++;
 		} else {
 			/* Skip newly added column. */
@@ -4913,12 +4917,6 @@ index_created:
 			user_table);
 		dict_index_t*	new_clust_index = dict_table_get_first_index(
 			ctx->new_table);
-		ctx->skip_pk_sort = innobase_pk_order_preserved(
-			ctx->col_map, clust_index, new_clust_index);
-
-		DBUG_EXECUTE_IF("innodb_alter_table_pk_assert_no_sort",
-			DBUG_ASSERT(ctx->skip_pk_sort););
-
 		DBUG_ASSERT(!ctx->new_table->persistent_autoinc);
 		if (const Field* ai = altered_table->found_next_number_field) {
 			const unsigned	col_no = innodb_col_no(ai);
@@ -4936,6 +4934,12 @@ index_created:
 				btr_write_autoinc(new_clust_index, autoinc);
 			}
 		}
+
+		ctx->skip_pk_sort = innobase_pk_order_preserved(
+			ctx->col_map, clust_index, new_clust_index);
+
+		DBUG_EXECUTE_IF("innodb_alter_table_pk_assert_no_sort",
+			DBUG_ASSERT(ctx->skip_pk_sort););
 
 		if (ctx->online) {
 			/* Allocate a log for online table rebuild. */
