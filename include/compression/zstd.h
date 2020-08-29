5cdb1646011e726c07391d21f422e5a7be8ca7dc
Kartik Soneji <kartiksoneji@rocketmail.com>
2020-08-29 05:55:04 +0530

    Add ZStd   support for RocksDB.

--- include/compression/zstd.h
+++ include/compression/zstd.h
@@ -15,6 +15,73 @@ extern "C" {
 
 extern bool COMPRESSION_LOADED_ZSTD;
 
+// TODO: See if we should use a higher version to get newer functions,
+// or not define this to reduce number of functions loaded
+// But versions > 10103 use an additional header, <zdict.h>
+// Also need to look at the defines:
+//    ROCKSDB_ZSTD_CUSTOM_MEM
+//    ROCKSDB_ZSTD_DDICT
+#define ZSTD_VERSION_NUMBER     800
+
+/*
+Mroonga
+
+ZSTD_compress
+ZSTD_compressBound
+ZSTD_decompress
+ZSTD_getErrorName
+ZSTD_isError
+*/
+
+/*
+RocksDB
+
+Undefined ZSTD_VERSION_NUMBER
+
+ZSTD_compress
+ZSTD_compressBound
+ZSTD_decompress
+ZSTD_versionNumber
+
+With ZSTD_VERSION_NUMBER = 800
+
+ZSTD_compressBound
+ZSTD_compress_usingCDict
+ZSTD_compress_usingDict
+ZSTD_createCCtx
+ZSTD_createCDict
+ZSTD_createDCtx
+ZSTD_decompress_usingDict
+ZSTD_freeCCtx
+ZSTD_freeCDict
+ZSTD_freeDCtx
+ZSTD_versionNumber
+
+With ROCKSDB_ZSTD_CUSTOM_MEM
+ZSTD_createCCtx_advanced
+ZSTD_createDCtx_advanced
+
+With ROCKSDB_ZSTD_DDICT
+ZSTD_createDDict_byReference
+ZSTD_decompress_usingDDict
+ZSTD_freeDDict
+ZSTD_sizeof_DDict
+*/
+
+typedef struct ZSTD_CCtx_s ZSTD_CCtx;
+typedef struct ZSTD_CDict_s ZSTD_CDict;
+typedef struct ZSTD_DCtx_s ZSTD_DCtx;
+typedef struct ZSTD_DDict_s ZSTD_DDict;
+
+typedef void* (*ZSTD_allocFunction) (void* opaque, size_t size);
+typedef void  (*ZSTD_freeFunction) (void* opaque, void* address);
+
+typedef struct {
+    ZSTD_allocFunction customAlloc;
+    ZSTD_freeFunction customFree;
+    void* opaque;
+} ZSTD_customMem;
+
 #define DEFINE_ZSTD_compress(NAME) size_t NAME( \
     void *dst,                                  \
     size_t dstCapacity,                         \
@@ -23,8 +90,51 @@ extern bool COMPRESSION_LOADED_ZSTD;
     int compressionLevel                        \
 )
 
+#define DEFINE_ZSTD_compress_usingCDict(NAME) size_t NAME(  \
+    ZSTD_CCtx *cctx,                                        \
+    void *dst,                                              \
+    size_t dstCapacity,                                     \
+    const void *src,                                        \
+    size_t srcSize,                                         \
+    const ZSTD_CDict *cdict                                 \
+)
+
+#define DEFINE_ZSTD_compress_usingDict(NAME) size_t NAME(   \
+    ZSTD_CCtx *ctx,                                         \
+     void *dst,                                             \
+    size_t dstCapacity,                                     \
+    const void *src,                                        \
+    size_t srcSize,                                         \
+    const void *dict,                                       \
+    size_t dictSize,                                        \
+    int compressionLevel                                    \
+)
+
 #define DEFINE_ZSTD_compressBound(NAME) size_t NAME(    \
     size_t srcSize                                      \
+)
+
+#define DEFINE_ZSTD_createCCtx(NAME) ZSTD_CCtx * NAME()
+
+#define DEFINE_ZSTD_createCCtx_advanced(NAME) ZSTD_CCtx * NAME( \
+    ZSTD_customMem customMem                                    \
+)
+
+#define DEFINE_ZSTD_createCDict(NAME) ZSTD_CDict * NAME(    \
+    const void *dictBuffer,                                 \
+    size_t dictSize,                                        \
+    int compressionLevel                                    \
+)
+
+#define DEFINE_ZSTD_createDCtx(NAME) ZSTD_DCtx * NAME()
+
+#define DEFINE_ZSTD_createDCtx_advanced(NAME) ZSTD_DCtx * NAME( \
+    ZSTD_customMem customMem                                    \
+)
+
+#define DEFINE_ZSTD_createDDict_byReference(NAME) ZSTD_DDict * NAME(    \
+    const void *dictBuffer,                                             \
+    size_t dictSize                                                     \
 )
 
 #define DEFINE_ZSTD_decompress(NAME) size_t NAME(   \
@@ -32,6 +142,41 @@ extern bool COMPRESSION_LOADED_ZSTD;
     size_t dstCapacity,                             \
     const void *src,                                \
     size_t compressedSize                           \
+)
+
+#define DEFINE_ZSTD_decompress_usingDDict(NAME) size_t NAME(    \
+    ZSTD_DCtx *dctx,                                            \
+    void *dst,                                                  \
+    size_t dstCapacity,                                         \
+    const void *src,                                            \
+    size_t srcSize,                                             \
+    const ZSTD_DDict *ddict                                     \
+)
+
+#define DEFINE_ZSTD_decompress_usingDict(NAME) size_t NAME( \
+    ZSTD_DCtx *dctx,                                        \
+    void *dst,                                              \
+    size_t dstCapacity,                                     \
+    const void *src,                                        \
+    size_t srcSize,                                         \
+    const void *dict,                                       \
+    size_t dictSize                                         \
+)
+
+#define DEFINE_ZSTD_freeCCtx(NAME) size_t NAME( \
+    ZSTD_CCtx *cctx                             \
+)
+
+#define DEFINE_ZSTD_freeCDict(NAME) size_t NAME(    \
+    ZSTD_CDict *CDict                               \
+)
+
+#define DEFINE_ZSTD_freeDCtx(NAME) size_t NAME( \
+    ZSTD_DCtx *dctx                             \
+)
+
+#define DEFINE_ZSTD_freeDDict(NAME) size_t NAME(    \
+    ZSTD_DDict *ddict                               \
 )
 
 #define DEFINE_ZSTD_getErrorName(NAME) const char* NAME(    \
@@ -42,32 +187,84 @@ extern bool COMPRESSION_LOADED_ZSTD;
     size_t code                                     \
 )
 
+#define DEFINE_ZSTD_sizeof_DDict(NAME) size_t NAME( \
+    const ZSTD_DDict *ddict                         \
+)
 
-typedef DEFINE_ZSTD_compress      ((*PTR_ZSTD_compress));
-typedef DEFINE_ZSTD_compressBound ((*PTR_ZSTD_compressBound));
-typedef DEFINE_ZSTD_decompress    ((*PTR_ZSTD_decompress));
-typedef DEFINE_ZSTD_getErrorName  ((*PTR_ZSTD_getErrorName));
-typedef DEFINE_ZSTD_isError       ((*PTR_ZSTD_isError));
+#define DEFINE_ZSTD_versionNumber(NAME) unsigned NAME()
+
+
+typedef DEFINE_ZSTD_compress                ((*PTR_ZSTD_compress));
+typedef DEFINE_ZSTD_compressBound           ((*PTR_ZSTD_compressBound));
+typedef DEFINE_ZSTD_compress_usingCDict     ((*PTR_ZSTD_compress_usingCDict));
+typedef DEFINE_ZSTD_compress_usingDict      ((*PTR_ZSTD_compress_usingDict));
+typedef DEFINE_ZSTD_createCCtx              ((*PTR_ZSTD_createCCtx));
+typedef DEFINE_ZSTD_createCCtx_advanced     ((*PTR_ZSTD_createCCtx_advanced));
+typedef DEFINE_ZSTD_createCDict             ((*PTR_ZSTD_createCDict));
+typedef DEFINE_ZSTD_createDCtx              ((*PTR_ZSTD_createDCtx));
+typedef DEFINE_ZSTD_createDCtx_advanced     ((*PTR_ZSTD_createDCtx_advanced));
+typedef DEFINE_ZSTD_createDDict_byReference ((*PTR_ZSTD_createDDict_byReference));
+typedef DEFINE_ZSTD_decompress              ((*PTR_ZSTD_decompress));
+typedef DEFINE_ZSTD_decompress_usingDDict   ((*PTR_ZSTD_decompress_usingDDict));
+typedef DEFINE_ZSTD_decompress_usingDict    ((*PTR_ZSTD_decompress_usingDict));
+typedef DEFINE_ZSTD_freeCCtx                ((*PTR_ZSTD_freeCCtx));
+typedef DEFINE_ZSTD_freeCDict               ((*PTR_ZSTD_freeCDict));
+typedef DEFINE_ZSTD_freeDCtx                ((*PTR_ZSTD_freeDCtx));
+typedef DEFINE_ZSTD_freeDDict               ((*PTR_ZSTD_freeDDict));
+typedef DEFINE_ZSTD_getErrorName            ((*PTR_ZSTD_getErrorName));
+typedef DEFINE_ZSTD_isError                 ((*PTR_ZSTD_isError));
+typedef DEFINE_ZSTD_sizeof_DDict            ((*PTR_ZSTD_sizeof_DDict));
+typedef DEFINE_ZSTD_versionNumber           ((*PTR_ZSTD_versionNumber));
 
 
 struct compression_service_zstd_st{
-    PTR_ZSTD_compress      ZSTD_compress_ptr;
-    PTR_ZSTD_compressBound ZSTD_compressBound_ptr;
-    PTR_ZSTD_decompress    ZSTD_decompress_ptr;
-    PTR_ZSTD_getErrorName  ZSTD_getErrorName_ptr;
-    PTR_ZSTD_isError       ZSTD_isError_ptr;
+    PTR_ZSTD_compress                ZSTD_compress_ptr;
+    PTR_ZSTD_compressBound           ZSTD_compressBound_ptr;
+    PTR_ZSTD_compress_usingCDict     ZSTD_compress_usingCDict_ptr;
+    PTR_ZSTD_compress_usingDict      ZSTD_compress_usingDict_ptr;
+    PTR_ZSTD_createCCtx              ZSTD_createCCtx_ptr;
+    PTR_ZSTD_createCCtx_advanced     ZSTD_createCCtx_advanced_ptr;
+    PTR_ZSTD_createCDict             ZSTD_createCDict_ptr;
+    PTR_ZSTD_createDCtx              ZSTD_createDCtx_ptr;
+    PTR_ZSTD_createDCtx_advanced     ZSTD_createDCtx_advanced_ptr;
+    PTR_ZSTD_createDDict_byReference ZSTD_createDDict_byReference_ptr;
+    PTR_ZSTD_decompress              ZSTD_decompress_ptr;
+    PTR_ZSTD_decompress_usingDDict   ZSTD_decompress_usingDDict_ptr;
+    PTR_ZSTD_decompress_usingDict    ZSTD_decompress_usingDict_ptr;
+    PTR_ZSTD_freeCCtx                ZSTD_freeCCtx_ptr;
+    PTR_ZSTD_freeCDict               ZSTD_freeCDict_ptr;
+    PTR_ZSTD_freeDCtx                ZSTD_freeDCtx_ptr;
+    PTR_ZSTD_freeDDict               ZSTD_freeDDict_ptr;
+    PTR_ZSTD_getErrorName            ZSTD_getErrorName_ptr;
+    PTR_ZSTD_isError                 ZSTD_isError_ptr;
+    PTR_ZSTD_sizeof_DDict            ZSTD_sizeof_DDict_ptr;
+    PTR_ZSTD_versionNumber           ZSTD_versionNumber_ptr;
 };
-
 
 extern struct compression_service_zstd_st *compression_service_zstd;
 
 
-#define ZSTD_compress(...)      compression_service_zstd->ZSTD_compress_ptr      (__VA_ARGS__)
-#define ZSTD_compressBound(...) compression_service_zstd->ZSTD_compressBound_ptr (__VA_ARGS__)
-#define ZSTD_decompress(...)    compression_service_zstd->ZSTD_decompress_ptr    (__VA_ARGS__)
-#define ZSTD_getErrorName(...)  compression_service_zstd->ZSTD_getErrorName_ptr  (__VA_ARGS__)
-#define ZSTD_isError(...)       compression_service_zstd->ZSTD_isError_ptr       (__VA_ARGS__)
-
+#define ZSTD_compress(...)                compression_service_zstd->ZSTD_compress_ptr                (__VA_ARGS__)
+#define ZSTD_compressBound(...)           compression_service_zstd->ZSTD_compressBound_ptr           (__VA_ARGS__)
+#define ZSTD_compress_usingCDict(...)     compression_service_zstd->ZSTD_compress_usingCDict_ptr     (__VA_ARGS__)
+#define ZSTD_compress_usingDict(...)      compression_service_zstd->ZSTD_compress_usingDict_ptr      (__VA_ARGS__)
+#define ZSTD_createCCtx(...)              compression_service_zstd->ZSTD_createCCtx_ptr              (__VA_ARGS__)
+#define ZSTD_createCCtx_advanced(...)     compression_service_zstd->ZSTD_createCCtx_advanced_ptr     (__VA_ARGS__)
+#define ZSTD_createCDict(...)             compression_service_zstd->ZSTD_createCDict_ptr             (__VA_ARGS__)
+#define ZSTD_createDCtx(...)              compression_service_zstd->ZSTD_createDCtx_ptr              (__VA_ARGS__)
+#define ZSTD_createDCtx_advanced(...)     compression_service_zstd->ZSTD_createDCtx_advanced_ptr     (__VA_ARGS__)
+#define ZSTD_createDDict_byReference(...) compression_service_zstd->ZSTD_createDDict_byReference_ptr (__VA_ARGS__)
+#define ZSTD_decompress(...)              compression_service_zstd->ZSTD_decompress_ptr              (__VA_ARGS__)
+#define ZSTD_decompress_usingDDict(...)   compression_service_zstd->ZSTD_decompress_usingDDict_ptr   (__VA_ARGS__)
+#define ZSTD_decompress_usingDict(...)    compression_service_zstd->ZSTD_decompress_usingDict_ptr    (__VA_ARGS__)
+#define ZSTD_freeCCtx(...)                compression_service_zstd->ZSTD_freeCCtx_ptr                (__VA_ARGS__)
+#define ZSTD_freeCDict(...)               compression_service_zstd->ZSTD_freeCDict_ptr               (__VA_ARGS__)
+#define ZSTD_freeDCtx(...)                compression_service_zstd->ZSTD_freeDCtx_ptr                (__VA_ARGS__)
+#define ZSTD_freeDDict(...)               compression_service_zstd->ZSTD_freeDDict_ptr               (__VA_ARGS__)
+#define ZSTD_getErrorName(...)            compression_service_zstd->ZSTD_getErrorName_ptr            (__VA_ARGS__)
+#define ZSTD_isError(...)                 compression_service_zstd->ZSTD_isError_ptr                 (__VA_ARGS__)
+#define ZSTD_sizeof_DDict(...)            compression_service_zstd->ZSTD_sizeof_DDict_ptr            (__VA_ARGS__)
+#define ZSTD_versionNumber(...)           compression_service_zstd->ZSTD_versionNumber_ptr           (__VA_ARGS__)
 
 #ifdef __cplusplus
 }
--- sql/compression/zstd.cc
+++ sql/compression/zstd.cc
@@ -13,7 +13,63 @@ DEFINE_ZSTD_compressBound(DUMMY_ZSTD_compressBound){
     return 0;
 }
 
+DEFINE_ZSTD_compress_usingCDict(DUMMY_ZSTD_compress_usingCDict){
+    return 0;
+}
+
+DEFINE_ZSTD_compress_usingDict(DUMMY_ZSTD_compress_usingDict){
+    return 0;
+}
+
+DEFINE_ZSTD_createCCtx(DUMMY_ZSTD_createCCtx){
+    return nullptr;
+}
+
+DEFINE_ZSTD_createCCtx_advanced(DUMMY_ZSTD_createCCtx_advanced){
+    return nullptr;
+}
+
+DEFINE_ZSTD_createCDict(DUMMY_ZSTD_createCDict){
+    return nullptr;
+}
+
+DEFINE_ZSTD_createDCtx(DUMMY_ZSTD_createDCtx){
+    return nullptr;
+}
+
+DEFINE_ZSTD_createDCtx_advanced(DUMMY_ZSTD_createDCtx_advanced){
+    return nullptr;
+}
+
+DEFINE_ZSTD_createDDict_byReference(DUMMY_ZSTD_createDDict_byReference){
+    return nullptr;
+}
+
 DEFINE_ZSTD_decompress(DUMMY_ZSTD_decompress){
+    return 0;
+}
+
+DEFINE_ZSTD_decompress_usingDDict(DUMMY_ZSTD_decompress_usingDDict){
+    return 0;
+}
+
+DEFINE_ZSTD_decompress_usingDict(DUMMY_ZSTD_decompress_usingDict){
+    return 0;
+}
+
+DEFINE_ZSTD_freeCCtx(DUMMY_ZSTD_freeCCtx){
+    return 0;
+}
+
+DEFINE_ZSTD_freeCDict(DUMMY_ZSTD_freeCDict){
+    return 0;
+}
+
+DEFINE_ZSTD_freeDCtx(DUMMY_ZSTD_freeDCtx){
+    return 0;
+}
+
+DEFINE_ZSTD_freeDDict(DUMMY_ZSTD_freeDDict){
     return 0;
 }
 
@@ -25,47 +81,121 @@ DEFINE_ZSTD_isError(DUMMY_ZSTD_isError){
     return 1;
 }
 
+DEFINE_ZSTD_sizeof_DDict(DUMMY_ZSTD_sizeof_DDict){
+    return 0;
+}
+
+DEFINE_ZSTD_versionNumber(DUMMY_ZSTD_versionNumber){
+    return 0;
+}
+
 
 void init_zstd(struct compression_service_zstd_st *handler, bool link_library){
     //point struct to right place for static plugins
     compression_service_zstd = handler;
 
-    compression_service_zstd->ZSTD_compress_ptr      = DUMMY_ZSTD_compress;
-    compression_service_zstd->ZSTD_compressBound_ptr = DUMMY_ZSTD_compressBound;
-    compression_service_zstd->ZSTD_decompress_ptr    = DUMMY_ZSTD_decompress;
-    compression_service_zstd->ZSTD_getErrorName_ptr  = DUMMY_ZSTD_getErrorName;
-    compression_service_zstd->ZSTD_isError_ptr       = DUMMY_ZSTD_isError;
+    compression_service_zstd->ZSTD_compress_ptr                = DUMMY_ZSTD_compress;
+    compression_service_zstd->ZSTD_compressBound_ptr           = DUMMY_ZSTD_compressBound;
+    compression_service_zstd->ZSTD_compress_usingCDict_ptr     = DUMMY_ZSTD_compress_usingCDict;
+    compression_service_zstd->ZSTD_compress_usingDict_ptr      = DUMMY_ZSTD_compress_usingDict;
+    compression_service_zstd->ZSTD_createCCtx_ptr              = DUMMY_ZSTD_createCCtx;
+    compression_service_zstd->ZSTD_createCCtx_advanced_ptr     = DUMMY_ZSTD_createCCtx_advanced;
+    compression_service_zstd->ZSTD_createCDict_ptr             = DUMMY_ZSTD_createCDict;
+    compression_service_zstd->ZSTD_createDCtx_ptr              = DUMMY_ZSTD_createDCtx;
+    compression_service_zstd->ZSTD_createDCtx_advanced_ptr     = DUMMY_ZSTD_createDCtx_advanced;
+    compression_service_zstd->ZSTD_createDDict_byReference_ptr = DUMMY_ZSTD_createDDict_byReference;
+    compression_service_zstd->ZSTD_decompress_ptr              = DUMMY_ZSTD_decompress;
+    compression_service_zstd->ZSTD_decompress_usingDDict_ptr   = DUMMY_ZSTD_decompress_usingDDict;
+    compression_service_zstd->ZSTD_decompress_usingDict_ptr    = DUMMY_ZSTD_decompress_usingDict;
+    compression_service_zstd->ZSTD_freeCCtx_ptr                = DUMMY_ZSTD_freeCCtx;
+    compression_service_zstd->ZSTD_freeCDict_ptr               = DUMMY_ZSTD_freeCDict;
+    compression_service_zstd->ZSTD_freeDCtx_ptr                = DUMMY_ZSTD_freeDCtx;
+    compression_service_zstd->ZSTD_freeDDict_ptr               = DUMMY_ZSTD_freeDDict;
+    compression_service_zstd->ZSTD_getErrorName_ptr            = DUMMY_ZSTD_getErrorName;
+    compression_service_zstd->ZSTD_isError_ptr                 = DUMMY_ZSTD_isError;
+    compression_service_zstd->ZSTD_sizeof_DDict_ptr            = DUMMY_ZSTD_sizeof_DDict;
+    compression_service_zstd->ZSTD_versionNumber_ptr           = DUMMY_ZSTD_versionNumber;
 
     if(!link_library)
         return;
 
     //Load ZStd library dynamically
+    
+    //TODO: enforce library version on load.
     void *library_handle = dlopen("libzstd.so", RTLD_LAZY | RTLD_GLOBAL);
     if(!library_handle || dlerror()){
         //sql_print_warning("Could not open libzstd.so\n");
         return;
     }
 
-    void *ZSTD_compress_ptr      = dlsym(library_handle, "ZSTD_compress");
-    void *ZSTD_compressBound_ptr = dlsym(library_handle, "ZSTD_compressBound");
-    void *ZSTD_decompress_ptr    = dlsym(library_handle, "ZSTD_decompress");
-    void *ZSTD_getErrorName_ptr  = dlsym(library_handle, "ZSTD_getErrorName");
-    void *ZSTD_isError_ptr       = dlsym(library_handle, "ZSTD_isError");
+    void *ZSTD_compress_ptr                = dlsym(library_handle, "ZSTD_compress");
+    void *ZSTD_compressBound_ptr           = dlsym(library_handle, "ZSTD_compressBound");
+    void *ZSTD_compress_usingCDict_ptr     = dlsym(library_handle, "ZSTD_compress_usingCDict");
+    void *ZSTD_compress_usingDict_ptr      = dlsym(library_handle, "ZSTD_compress_usingDict");
+    void *ZSTD_createCCtx_ptr              = dlsym(library_handle, "ZSTD_createCCtx");
+    void *ZSTD_createCCtx_advanced_ptr     = dlsym(library_handle, "ZSTD_createCCtx_advanced");
+    void *ZSTD_createCDict_ptr             = dlsym(library_handle, "ZSTD_createCDict");
+    void *ZSTD_createDCtx_ptr              = dlsym(library_handle, "ZSTD_createDCtx");
+    void *ZSTD_createDCtx_advanced_ptr     = dlsym(library_handle, "ZSTD_createDCtx_advanced");
+    void *ZSTD_createDDict_byReference_ptr = dlsym(library_handle, "ZSTD_createDDict_byReference");
+    void *ZSTD_decompress_ptr              = dlsym(library_handle, "ZSTD_decompress");
+    void *ZSTD_decompress_usingDDict_ptr   = dlsym(library_handle, "ZSTD_decompress_usingDDict");
+    void *ZSTD_decompress_usingDict_ptr    = dlsym(library_handle, "ZSTD_decompress_usingDict");
+    void *ZSTD_freeCCtx_ptr                = dlsym(library_handle, "ZSTD_freeCCtx");
+    void *ZSTD_freeCDict_ptr               = dlsym(library_handle, "ZSTD_freeCDict");
+    void *ZSTD_freeDCtx_ptr                = dlsym(library_handle, "ZSTD_freeDCtx");
+    void *ZSTD_freeDDict_ptr               = dlsym(library_handle, "ZSTD_freeDDict");
+    void *ZSTD_sizeof_DDict_ptr            = dlsym(library_handle, "ZSTD_sizeof_DDict");
+    void *ZSTD_getErrorName_ptr            = dlsym(library_handle, "ZSTD_getErrorName");
+    void *ZSTD_isError_ptr                 = dlsym(library_handle, "ZSTD_isError");
+    void *ZSTD_versionNumber_ptr           = dlsym(library_handle, "ZSTD_versionNumber");
     
     if(
-        !ZSTD_compress_ptr      ||
-        !ZSTD_compressBound_ptr ||
-        !ZSTD_decompress_ptr    ||
-        !ZSTD_getErrorName_ptr  ||
-        !ZSTD_isError_ptr
+        !ZSTD_compress_ptr                ||
+        !ZSTD_compressBound_ptr           ||
+        !ZSTD_compress_usingCDict_ptr     ||
+        !ZSTD_compress_usingDict_ptr      ||
+        !ZSTD_createCCtx_ptr              ||
+        !ZSTD_createCCtx_advanced_ptr     ||
+        !ZSTD_createCDict_ptr             ||
+        !ZSTD_createDCtx_ptr              ||
+        !ZSTD_createDCtx_advanced_ptr     ||
+        !ZSTD_createDDict_byReference_ptr ||
+        !ZSTD_decompress_ptr              ||
+        !ZSTD_decompress_usingDDict_ptr   ||
+        !ZSTD_decompress_usingDict_ptr    ||
+        !ZSTD_freeCCtx_ptr                ||
+        !ZSTD_freeCDict_ptr               ||
+        !ZSTD_freeDCtx_ptr                ||
+        !ZSTD_freeDDict_ptr               ||
+        !ZSTD_sizeof_DDict_ptr            ||
+        !ZSTD_getErrorName_ptr            ||
+        !ZSTD_isError_ptr                 ||
+        !ZSTD_versionNumber_ptr
     )
         return;
     
-    compression_service_zstd->ZSTD_compress_ptr      = (PTR_ZSTD_compress)      ZSTD_compress_ptr;
-    compression_service_zstd->ZSTD_compressBound_ptr = (PTR_ZSTD_compressBound) ZSTD_compressBound_ptr;
-    compression_service_zstd->ZSTD_decompress_ptr    = (PTR_ZSTD_decompress)    ZSTD_decompress_ptr;
-    compression_service_zstd->ZSTD_getErrorName_ptr  = (PTR_ZSTD_getErrorName)  ZSTD_getErrorName_ptr;
-    compression_service_zstd->ZSTD_isError_ptr       = (PTR_ZSTD_isError)       ZSTD_isError_ptr;
+    compression_service_zstd->ZSTD_compress_ptr                = (PTR_ZSTD_compress)                ZSTD_compress_ptr;
+    compression_service_zstd->ZSTD_compressBound_ptr           = (PTR_ZSTD_compressBound)           ZSTD_compressBound_ptr;
+    compression_service_zstd->ZSTD_compress_usingCDict_ptr     = (PTR_ZSTD_compress_usingCDict)     ZSTD_compress_usingCDict_ptr;
+    compression_service_zstd->ZSTD_compress_usingDict_ptr      = (PTR_ZSTD_compress_usingDict)      ZSTD_compress_usingDict_ptr;
+    compression_service_zstd->ZSTD_createCCtx_ptr              = (PTR_ZSTD_createCCtx)              ZSTD_createCCtx_ptr;
+    compression_service_zstd->ZSTD_createCCtx_advanced_ptr     = (PTR_ZSTD_createCCtx_advanced)     ZSTD_createCCtx_advanced_ptr;
+    compression_service_zstd->ZSTD_createCDict_ptr             = (PTR_ZSTD_createCDict)             ZSTD_createCDict_ptr;
+    compression_service_zstd->ZSTD_createDCtx_ptr              = (PTR_ZSTD_createDCtx)              ZSTD_createDCtx_ptr;
+    compression_service_zstd->ZSTD_createDCtx_advanced_ptr     = (PTR_ZSTD_createDCtx_advanced)     ZSTD_createDCtx_advanced_ptr;
+    compression_service_zstd->ZSTD_createDDict_byReference_ptr = (PTR_ZSTD_createDDict_byReference) ZSTD_createDDict_byReference_ptr;
+    compression_service_zstd->ZSTD_decompress_ptr              = (PTR_ZSTD_decompress)              ZSTD_decompress_ptr;
+    compression_service_zstd->ZSTD_decompress_usingDDict_ptr   = (PTR_ZSTD_decompress_usingDDict)   ZSTD_decompress_usingDDict_ptr;
+    compression_service_zstd->ZSTD_decompress_usingDict_ptr    = (PTR_ZSTD_decompress_usingDict)    ZSTD_decompress_usingDict_ptr;
+    compression_service_zstd->ZSTD_freeCCtx_ptr                = (PTR_ZSTD_freeCCtx)                ZSTD_freeCCtx_ptr;
+    compression_service_zstd->ZSTD_freeCDict_ptr               = (PTR_ZSTD_freeCDict)               ZSTD_freeCDict_ptr;
+    compression_service_zstd->ZSTD_freeDCtx_ptr                = (PTR_ZSTD_freeDCtx)                ZSTD_freeDCtx_ptr;
+    compression_service_zstd->ZSTD_freeDDict_ptr               = (PTR_ZSTD_freeDDict)               ZSTD_freeDDict_ptr;
+    compression_service_zstd->ZSTD_getErrorName_ptr            = (PTR_ZSTD_getErrorName)            ZSTD_getErrorName_ptr;
+    compression_service_zstd->ZSTD_isError_ptr                 = (PTR_ZSTD_isError)                 ZSTD_isError_ptr;
+    compression_service_zstd->ZSTD_sizeof_DDict_ptr            = (PTR_ZSTD_sizeof_DDict)            ZSTD_sizeof_DDict_ptr;
+    compression_service_zstd->ZSTD_versionNumber_ptr           = (PTR_ZSTD_versionNumber)           ZSTD_versionNumber_ptr;
     
     COMPRESSION_LOADED_ZSTD = true;
 } 
