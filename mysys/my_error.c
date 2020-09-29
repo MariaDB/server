4a97e25aec94d2e3a9f37ee309bd5ba2d1d15466
Alice Sherepa <alice.sherepa@gmail.com>
2020-09-29 15:26:04 +0200

    minor fixes of rpl_start_stop_slave and rpl_slave_grp_exec tests, expanding rpl_gtid_delete_domain for easier later analysis

--- mysys/my_error.c
+++ mysys/my_error.c
@@ -113,6 +113,9 @@ void my_error(uint nr, myf MyFlags, ...)
   DBUG_ENTER("my_error");
   DBUG_PRINT("my", ("nr: %d  MyFlags: %lu  errno: %d", nr, MyFlags, errno));
 
+  if (errno == 1213)
+    DBUG_ASSERT(0);
+  
   if (!(format = my_get_err_msg(nr)))
     (void) my_snprintf(ebuff, sizeof(ebuff), "Unknown error %d", nr);
   else
