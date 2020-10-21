a79c6e369e8d07786395a04493f2142f41f7befc
Nikita Malyavin <nikitamalyavin@gmail.com>
2020-10-22 01:32:44 +1000

    MDEV-22677 UPDATE crashes on partitioned HEAP table WITHOUT OVERLAPS
    `ha_heap::clone` was creating a handler by share's handlerton, which is
    partition handlerton.
    
    handler's handlerton should be used instead.
    
    Here in particular, HEAP handlerton will be used and it will create ha_heap
    handler.

--- storage/heap/ha_heap.cc
+++ storage/heap/ha_heap.cc
@@ -153,7 +153,7 @@ int ha_heap::close(void)
 
 handler *ha_heap::clone(const char *name, MEM_ROOT *mem_root)
 {
-  handler *new_handler= get_new_handler(table->s, mem_root, table->s->db_type());
+  handler *new_handler= get_new_handler(table->s, mem_root, ht);
   if (new_handler && !new_handler->ha_open(table, file->s->name, table->db_stat,
                                            HA_OPEN_IGNORE_IF_LOCKED))
     return new_handler;
