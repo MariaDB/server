bb328a2a274b0b5cc024f298fcac70b3f0fb75aa
Marko Mäkelä <marko.makela@mariadb.com>
2020-11-13 20:12:29 +0200

    MDEV-24188 Hang in buf_page_create() after reusing a previously freed page
    The fix of MDEV-23456 (commit b1009ae5c16697d5eef443cc6a60a74301148c73)
    introduced a livelock between page flushing and a thread that is
    executing buf_page_create().
    
    buf_page_create(): If the current mini-transaction is holding
    an exclusive latch on the page, do not attempt to acquire another
    one, and do not care about any I/O fix.
    
    mtr_t::have_x_latch(): Replaces mtr_t::get_fix_count().
    
    dyn_buf_t::for_each_block(const Functor&) const: A new variant.
    
    rw_lock_own(): Add a const qualifier.
    
    Reviewed by: Thirunarayanan Balathandayuthapani

--- storage/innobase/include/dyn0buf.h
+++ storage/innobase/include/dyn0buf.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
 
 Copyright (c) 2013, 2016, Oracle and/or its affiliates. All Rights Reserved.
-Copyright (c) 2019, MariaDB Corporation.
+Copyright (c) 2019, 2020, MariaDB Corporation.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -334,6 +334,24 @@ public:
 	@return	false if iteration was terminated. */
 	template <typename Functor>
 	bool for_each_block(Functor& functor) const
+	{
+		for (typename list_t::iterator it = m_list.begin(),
+					       end = m_list.end();
+		     it != end; ++it) {
+
+			if (!functor(&*it)) {
+				return false;
+			}
+		}
+
+		return(true);
+	}
+
+	/**
+	Iterate over each block and call the functor.
+	@return	false if iteration was terminated. */
+	template <typename Functor>
+	bool for_each_block(const Functor& functor) const
 	{
 		for (typename list_t::iterator it = m_list.begin(),
 					       end = m_list.end();
