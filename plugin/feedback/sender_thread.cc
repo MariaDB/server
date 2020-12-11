121582647ce999a5418cf705646744f2f7fe4065
Sergei Golubchik <serg@mariadb.org>
2020-12-11 20:35:25 +0100

    MDEV-24279 Segfault after 1 day and 5 minutes uptime
    feedback plugin now fakes a SHOW command to force
    create_schema_table() to instantiate the table at once,
    not lazily.
    
    The test from plugins.feedback_plugin_send applies.
    
    Caused by e64084d5a3a7

--- plugin/feedback/sender_thread.cc
+++ plugin/feedback/sender_thread.cc
@@ -117,6 +117,7 @@ static int prepare_for_fill(TABLE_LIST *tables)
 
   tables->init_one_table(&INFORMATION_SCHEMA_NAME, &tbl_name, 0, TL_READ);
   tables->schema_table= i_s_feedback;
+  tables->schema_table_reformed= 1;
   tables->table= create_schema_table(thd, tables);
   if (!tables->table)
     return 1;
