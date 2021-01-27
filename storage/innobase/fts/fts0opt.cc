MariaDB/server

commit bf1f9b59c7d0b619d8bf350b96436970c6edc118

MDEV-24638 Avoid repetitive FTS SYNC request for table
fts_optimize_request_sync_table() can avoid the repetitive
FTS SYNC request of the table if the table already has FTS_SYNC
message in fts_optimize_wq queue.

Reviewed-by: Marko Mäkelä

storage/innobase/fts/fts0opt.cc
@@ -2658,6 +2658,13 @@ fts_optimize_request_sync_table(
 
 	mutex_enter(&fts_optimize_wq->mutex);
 
+	if (table->fts->sync_message) {
+		/* If the table already has SYNC message in
+		fts_optimize_wq queue then ignore it */
+		mutex_exit(&fts_optimize_wq->mutex);
+		return;
+	}
+
 	add_msg(msg, true);
 
 	table->fts->sync_message = true;

commit 700ae20d458ff47f53a4df0cf9f1283b015a6bd9

MDEV-24693 LeakSanitizer: detected memory leaks in mem_heap_create_block_func / fts_optimize_create_msg
- This issue is caused by the commit bf1f9b59c7d0b619d8bf350b96436970c6edc118
(MDEV-24638). Delay the creation of SYNC message in
fts_optimize_request_sync_table. So that InnoDB can avoid creating
the message if the table already has SYNC message in fts_optimize_wq queue

storage/innobase/fts/fts0opt.cc
@@ -2654,8 +2654,6 @@ fts_optimize_request_sync_table(
 		return;
 	}
 
-	fts_msg_t* msg = fts_optimize_create_msg(FTS_MSG_SYNC_TABLE, table);
-
 	mutex_enter(&fts_optimize_wq->mutex);
 
 	if (table->fts->sync_message) {
@@ -2664,6 +2662,8 @@ fts_optimize_request_sync_table(
 		mutex_exit(&fts_optimize_wq->mutex);
 		return;
 	}
+
+	fts_msg_t* msg = fts_optimize_create_msg(FTS_MSG_SYNC_TABLE, table);
 
 	add_msg(msg, true);
 