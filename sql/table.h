b87c342da5e51e112e06b36d8b95037f182bdb0e
Varun Gupta <varun.gupta@mariadb.com>
2021-01-29 00:27:19 +0530

    MDEV-11172: EXPLAIN shows non-sensical value for key_len with type=index
    The issue happens when the secondary keys are extended with primary
    key parts. Inside the function TABLE_SHARE::init_from_binary_frm_image()
    adds the length bytes for the primary key key parts to the length of the
    secondary key. This is not needed because when the extended keys are
    used we recalculate the length for the used key parts.
    
    Also removed TABLE_SHARE::total_key_length as it is not used in the code
    
    Apporved-by: Monty <monty@mariadb.org>

--- sql/table.h
+++ sql/table.h
@@ -773,7 +773,7 @@ struct TABLE_SHARE
   uint rec_buff_length;                 /* Size of table->record[] buffer */
   uint keys, key_parts;
   uint ext_key_parts;       /* Total number of key parts in extended keys */
-  uint max_key_length, max_unique_length, total_key_length;
+  uint max_key_length, max_unique_length;
   uint uniques;                         /* Number of UNIQUE index */
   uint db_create_options;		/* Create options from database */
   uint db_options_in_use;		/* Options in use */
