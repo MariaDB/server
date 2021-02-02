MariaDB/server

commit c92f7e287fc0e21dc1b181284b1f8e2139d1c331

MDEV-8139 Fix Scrubbing
fil_space_t::freed_ranges: Store ranges of freed page numbers.

fil_space_t::last_freed_lsn: Store the most recent LSN of
freeing a page.

fil_space_t::freed_mutex: Protects freed_ranges, last_freed_lsn.

fil_space_create(): Initialize the freed_range mutex.

fil_space_free_low(): Frees the freed_range mutex.

range_set: Ranges of page numbers.

buf_page_create(): Removes the page from freed_ranges when page
is being reused.

btr_free_root(): Remove the PAGE_INDEX_ID invalidation. Because
btr_free_root() and dict_drop_index_tree() are executed in
the same atomic mini-transaction, there is no need to
invalidate the root page.

buf_release_freed_page(): Split from buf_flush_freed_page().
Skip any I/O

buf_flush_freed_pages(): Get the freed ranges from tablespace and
Write punch-hole or zeroes of the freed ranges.

buf_flush_try_neighbors(): Handles the flushing of freed ranges.

mtr_t::freed_pages: Variable to store the list of freed pages.

mtr_t::add_freed_pages(): To add freed pages.

mtr_t::clear_freed_pages(): To clear the freed pages.

mtr_t::m_freed_in_system_tablespace: Variable to indicate whether page has
been freed in system tablespace.

mtr_t::m_trim_pages: Variable to indicate whether the space has been trimmed.

mtr_t::commit(): Add the freed page and update the last freed lsn
in the tablespace and clear the tablespace freed range if space is
trimmed.

file_name_t::freed_pages: Store the freed pages during recovery.

file_name_t::add_freed_page(), file_name_t::remove_freed_page(): To
add and remove freed page during recovery.

store_freed_or_init_rec(): Store or remove the freed pages while
encountering FREE_PAGE or INIT_PAGE redo log record.

recv_init_crash_recovery_spaces(): Add the freed page encountered
during recovery to respective tablespace.

storage/innobase/fsp/fsp0fsp.cc
@@ -554,7 +554,7 @@ void fsp_header_init(fil_space_t* space, ulint size, mtr_t* mtr)
 	mtr_x_lock_space(space, mtr);
 
 	const auto savepoint = mtr->get_savepoint();
-	buf_block_t* block = buf_page_create(page_id, zip_size, mtr);
+	buf_block_t* block = buf_page_create(space, 0, zip_size, mtr);
 	mtr->sx_latch_at_savepoint(savepoint, block);
 	buf_block_dbg_add_level(block, SYNC_FSP_PAGE);
 
@@ -875,8 +875,9 @@ fsp_fill_free_list(
 
 			if (i > 0) {
 				const auto savepoint = mtr->get_savepoint();
-				block= buf_page_create(page_id_t(space->id, i),
-						       zip_size, mtr);
+				block= buf_page_create(
+					space, static_cast<uint32_t>(i),
+					zip_size, mtr);
 				mtr->sx_latch_at_savepoint(savepoint, block);
 
 				buf_block_dbg_add_level(block, SYNC_FSP_PAGE);
@@ -898,8 +899,9 @@ fsp_fill_free_list(
 				ibuf_mtr.set_named_space(space);
 
 				block = buf_page_create(
-					page_id_t(space->id,
-						  i + FSP_IBUF_BITMAP_OFFSET),
+					space,
+					static_cast<uint32_t>(
+						i + FSP_IBUF_BITMAP_OFFSET),
 					zip_size, &ibuf_mtr);
 				ibuf_mtr.sx_latch_at_savepoint(0, block);
 				buf_block_dbg_add_level(block, SYNC_FSP_PAGE);
@@ -1059,8 +1061,9 @@ fsp_page_create(
 	rw_lock_type_t		rw_latch,
 	mtr_t*			mtr)
 {
-	buf_block_t*	block = buf_page_create(page_id_t(space->id, offset),
-						space->zip_size(), mtr);
+	buf_block_t*	block = buf_page_create(
+					space, static_cast<uint32_t>(offset),
+					space->zip_size(), mtr);
 
 	/* The latch may already have been acquired, so we cannot invoke
 	mtr_t::x_latch_at_savepoint() or mtr_t::sx_latch_at_savepoint(). */
@@ -1251,7 +1254,7 @@ static void fsp_free_page(fil_space_t* space, page_no_t offset, mtr_t* mtr)
 		return;
 	}
 
-	mtr->free(page_id_t(space->id, offset));
+	mtr->free(*space, static_cast<uint32_t>(offset));
 
 	const ulint	bit = offset % FSP_EXTENT_SIZE;
 
@@ -2557,7 +2560,7 @@ fseg_free_page_low(
 		fsp_free_extent(space, offset, mtr);
 	}
 
-	mtr->free(page_id_t(space->id, offset));
+	mtr->free(*space, static_cast<uint32_t>(offset));
 }
 
 /** Free a page in a file segment.
@@ -2674,7 +2677,7 @@ fseg_free_extent(
 	for (ulint i = 0; i < FSP_EXTENT_SIZE; i++) {
 		if (!xdes_is_free(descr, i)) {
 			buf_page_free(
-			  page_id_t(space->id, first_page_in_extent + i),
+			  page_id_t(space->id, first_page_in_extent + 1),
 			  mtr, __FILE__, __LINE__);
 		}
 	}

mysql-test/suite/encryption/disabled.def
@@ -1,14 +1,0 @@
-##############################################################################
-#
-#  List the test cases that are to be disabled temporarily.
-#
-#  Separate the test case name and the comment with ':'.
-#
-#    <testcasename> : BUG#<xxxx> <date disabled> <disabler> <comment>
-#
-#  Do not use any TAB characters for whitespace.
-#
-##############################################################################
-
-innodb_scrub            : MDEV-8139 scrubbing does not work reliably
-innodb_scrub_background : MDEV-8139 scrubbing does not work reliably

mysql-test/suite/encryption/r/innodb_scrub.result
@@ -1,138 +1,0 @@
-create table snapshot_status engine = myisam
-select * from information_schema.global_status
-where variable_name like 'innodb_scrub%';
-# MDEV-8139 Fix scrubbing tests
-# FIXME: Add index(b) to each table; ensure that undo logs are scrubbed.
-create table delete_3 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=compressed;
-delete from delete_3;
-create table delete_rollback_delete_3 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=compressed;
-begin;
-delete from delete_rollback_delete_3;
-rollback;
-delete from delete_rollback_delete_3;
-create table insert_rollback_3 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=compressed;
-begin;
-rollback;
-create table delete_2 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=compact;
-delete from delete_2;
-create table delete_rollback_delete_2 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=compact;
-begin;
-delete from delete_rollback_delete_2;
-rollback;
-delete from delete_rollback_delete_2;
-create table insert_rollback_2 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=compact;
-begin;
-rollback;
-create table delete_1 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=redundant;
-delete from delete_1;
-create table delete_rollback_delete_1 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=redundant;
-begin;
-delete from delete_rollback_delete_1;
-rollback;
-delete from delete_rollback_delete_1;
-create table insert_rollback_1 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=redundant;
-begin;
-rollback;
-create table delete_0 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=dynamic;
-delete from delete_0;
-create table delete_rollback_delete_0 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=dynamic;
-begin;
-delete from delete_rollback_delete_0;
-rollback;
-delete from delete_rollback_delete_0;
-create table insert_rollback_0 (
-a int auto_increment primary key,
-b varchar(256),
-c text) engine = innodb row_format=dynamic;
-begin;
-rollback;
-SET GLOBAL innodb_fast_shutdown=0;
-# delete_3.ibd
-# delete_rollback_delete_3.ibd
-# insert_rollback_3.ibd
-# delete_2.ibd
-# delete_rollback_delete_2.ibd
-# insert_rollback_2.ibd
-# delete_1.ibd
-# delete_rollback_delete_1.ibd
-# insert_rollback_1.ibd
-# delete_0.ibd
-# delete_rollback_delete_0.ibd
-# insert_rollback_0.ibd
-check table delete_3, delete_rollback_delete_3, insert_rollback_3;
-Table	Op	Msg_type	Msg_text
-test.delete_3	check	status	OK
-test.delete_rollback_delete_3	check	status	OK
-test.insert_rollback_3	check	status	OK
-drop table delete_3, delete_rollback_delete_3, insert_rollback_3;
-check table delete_2, delete_rollback_delete_2, insert_rollback_2;
-Table	Op	Msg_type	Msg_text
-test.delete_2	check	status	OK
-test.delete_rollback_delete_2	check	status	OK
-test.insert_rollback_2	check	status	OK
-drop table delete_2, delete_rollback_delete_2, insert_rollback_2;
-check table delete_1, delete_rollback_delete_1, insert_rollback_1;
-Table	Op	Msg_type	Msg_text
-test.delete_1	check	status	OK
-test.delete_rollback_delete_1	check	status	OK
-test.insert_rollback_1	check	status	OK
-drop table delete_1, delete_rollback_delete_1, insert_rollback_1;
-check table delete_0, delete_rollback_delete_0, insert_rollback_0;
-Table	Op	Msg_type	Msg_text
-test.delete_0	check	status	OK
-test.delete_rollback_delete_0	check	status	OK
-test.insert_rollback_0	check	status	OK
-drop table delete_0, delete_rollback_delete_0, insert_rollback_0;
-show variables like 'innodb_%scrub_data%';
-Variable_name	Value
-innodb_background_scrub_data_check_interval	3600
-innodb_background_scrub_data_compressed	OFF
-innodb_background_scrub_data_interval	604800
-innodb_background_scrub_data_uncompressed	OFF
-innodb_immediate_scrub_data_uncompressed	ON
-# verify that this test have not caused any background scrubbing
-select ss.variable_name, gs.variable_value - ss.variable_value as variable_value
-from snapshot_status ss,
-information_schema.global_status gs
-where ss.variable_name = gs.variable_name;
-variable_name	variable_value
-INNODB_SCRUB_BACKGROUND_PAGE_REORGANIZATIONS	0
-INNODB_SCRUB_BACKGROUND_PAGE_SPLITS	0
-INNODB_SCRUB_BACKGROUND_PAGE_SPLIT_FAILURES_MISSING_INDEX	0
-INNODB_SCRUB_BACKGROUND_PAGE_SPLIT_FAILURES_OUT_OF_FILESPACE	0
-INNODB_SCRUB_BACKGROUND_PAGE_SPLIT_FAILURES_UNDERFLOW	0
-INNODB_SCRUB_BACKGROUND_PAGE_SPLIT_FAILURES_UNKNOWN	0
-drop table snapshot_status;

mysql-test/suite/encryption/r/innodb_scrub_background.result
@@ -1,160 +1,0 @@
-#
-# immediate scrubbing is off
-# background scrubbing is on
-#
-show variables like 'innodb_%scrub_data%';
-Variable_name	Value
-innodb_background_scrub_data_check_interval	3600
-innodb_background_scrub_data_compressed	ON
-innodb_background_scrub_data_interval	604800
-innodb_background_scrub_data_uncompressed	ON
-innodb_immediate_scrub_data_uncompressed	OFF
-# make sure spaces are checked quickly
-SET GLOBAL innodb_background_scrub_data_check_interval=1;
-create table delete_3 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=compressed;
-# Populate table with rows
-delete from delete_3;
-create table delete_rollback_delete_3 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=compressed;
-# Populate table with rows
-begin;
-delete from delete_rollback_delete_3;
-rollback;
-delete from delete_rollback_delete_3;
-create table insert_rollback_3 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=compressed;
-# Populate table with rows
-begin;
-rollback;
-create table delete_2 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=compact;
-# Populate table with rows
-delete from delete_2;
-create table delete_rollback_delete_2 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=compact;
-# Populate table with rows
-begin;
-delete from delete_rollback_delete_2;
-rollback;
-delete from delete_rollback_delete_2;
-create table insert_rollback_2 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=compact;
-# Populate table with rows
-begin;
-rollback;
-create table delete_1 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=redundant;
-# Populate table with rows
-delete from delete_1;
-create table delete_rollback_delete_1 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=redundant;
-# Populate table with rows
-begin;
-delete from delete_rollback_delete_1;
-rollback;
-delete from delete_rollback_delete_1;
-create table insert_rollback_1 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=redundant;
-# Populate table with rows
-begin;
-rollback;
-create table delete_0 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=dynamic;
-# Populate table with rows
-delete from delete_0;
-create table delete_rollback_delete_0 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=dynamic;
-# Populate table with rows
-begin;
-delete from delete_rollback_delete_0;
-rollback;
-delete from delete_rollback_delete_0;
-create table insert_rollback_0 (
-a int auto_increment primary key,
-b varchar(256),
-c text,
-index(b)) engine = innodb row_format=dynamic;
-# Populate table with rows
-begin;
-rollback;
-# start scrubbing threads
-SET GLOBAL innodb_encryption_threads=5;
-# Wait max 10 min for scrubbing
-SET GLOBAL innodb_fast_shutdown=0;
-# delete_3.ibd
-# delete_rollback_delete_3.ibd
-# insert_rollback_3.ibd
-# delete_2.ibd
-# delete_rollback_delete_2.ibd
-# insert_rollback_2.ibd
-# delete_1.ibd
-# delete_rollback_delete_1.ibd
-# insert_rollback_1.ibd
-# delete_0.ibd
-# delete_rollback_delete_0.ibd
-# insert_rollback_0.ibd
-check table delete_3, delete_rollback_delete_3, insert_rollback_3;
-Table	Op	Msg_type	Msg_text
-test.delete_3	check	status	OK
-test.delete_rollback_delete_3	check	status	OK
-test.insert_rollback_3	check	status	OK
-drop table delete_3, delete_rollback_delete_3, insert_rollback_3;
-check table delete_2, delete_rollback_delete_2, insert_rollback_2;
-Table	Op	Msg_type	Msg_text
-test.delete_2	check	status	OK
-test.delete_rollback_delete_2	check	status	OK
-test.insert_rollback_2	check	status	OK
-drop table delete_2, delete_rollback_delete_2, insert_rollback_2;
-check table delete_1, delete_rollback_delete_1, insert_rollback_1;
-Table	Op	Msg_type	Msg_text
-test.delete_1	check	status	OK
-test.delete_rollback_delete_1	check	status	OK
-test.insert_rollback_1	check	status	OK
-drop table delete_1, delete_rollback_delete_1, insert_rollback_1;
-check table delete_0, delete_rollback_delete_0, insert_rollback_0;
-Table	Op	Msg_type	Msg_text
-test.delete_0	check	status	OK
-test.delete_rollback_delete_0	check	status	OK
-test.insert_rollback_0	check	status	OK
-drop table delete_0, delete_rollback_delete_0, insert_rollback_0;
-show variables like 'innodb_%scrub_data%';
-Variable_name	Value
-innodb_background_scrub_data_check_interval	3600
-innodb_background_scrub_data_compressed	ON
-innodb_background_scrub_data_interval	604800
-innodb_background_scrub_data_uncompressed	ON
-innodb_immediate_scrub_data_uncompressed	OFF

mysql-test/suite/encryption/t/innodb_scrub.opt
@@ -1,7 +1,0 @@
---innodb-background-scrub-data-compressed=OFF
---innodb-background-scrub-data-uncompressed=OFF
---innodb-encrypt-tables=OFF
---innodb-encryption-threads=0
---innodb-immediate-scrub-data-uncompressed=ON
---loose-innodb-debug-force-scrubbing=ON
---innodb-tablespaces-scrubbing

mysql-test/suite/encryption/t/innodb_scrub.test
@@ -1,147 +1,0 @@
--- source include/have_innodb.inc
--- source include/not_embedded.inc
--- source include/have_example_key_management_plugin.inc
-
-let $MYSQLD_DATADIR=`select @@datadir`;
-let INNODB_PAGE_SIZE= `select @@innodb_page_size`;
-
-create table snapshot_status engine = myisam
-select * from information_schema.global_status
-where variable_name like 'innodb_scrub_background%';
-
-let $rowcount=500;
-let $maxformatno= 4;
-let $formatno= $maxformatno;
-
---echo # MDEV-8139 Fix scrubbing tests
---echo # FIXME: Add index(b) to each table; ensure that undo logs are scrubbed.
-let $tableformat= (
-  a int auto_increment primary key,
-  b varchar(256),
-  c text) engine = innodb row_format;
-
-while ($formatno)
-{
-dec $formatno;
-let $format = `select case $formatno
-               when 0 then 'dynamic'
-               when 1 then 'redundant'
-               when 2 then 'compact'
-	       when 3 then 'compressed'
-               end`;
-
-let $t= delete_$formatno;
-eval create table $t $tableformat=$format;
-
-let $numinserts = $rowcount;
---disable_query_log
-begin;
-while ($numinserts)
-{
-  dec $numinserts;
-  eval insert into $t(b,c) values ('repairman', repeat('unicycle', 1000));
-}
-commit;
---enable_query_log
-
-eval delete from $t;
-
-let $t= delete_rollback_delete_$formatno;
-eval create table $t $tableformat=$format;
-
-let $numinserts = $rowcount;
---disable_query_log
-begin;
-while ($numinserts)
-{
-  dec $numinserts;
-  eval insert into $t(b,c) values ('breakhuman', repeat('bicycle', 1000));
-}
-commit;
---enable_query_log
-
-begin;
-eval delete from $t;
-rollback;
-eval delete from $t;
-
-let $t= insert_rollback_$formatno;
-
-eval create table $t $tableformat=$format;
-
-let $numinserts = $rowcount;
-begin;
---disable_query_log
-while ($numinserts)
-{
-  dec $numinserts;
-  eval insert into $t(b,c) values ('wonderwoman', repeat('tricycle', 1000));
-}
---enable_query_log
-
-rollback;
-}
-
-SET GLOBAL innodb_fast_shutdown=0;
--- source include/shutdown_mysqld.inc
-
-let SEARCH_ABORT= FOUND;
-let SEARCH_PATTERN= (un|b|tr)icycle|(repair|breakhu|wonderwo)man;
-let SEARCH_RANGE= 12582912;
-let SEARCH_FILE= $MYSQLD_DATADIR/ibdata1;
-
-# We may randomly find copies of unscrubbed pages in the doublewrite buffer.
-# Let us scrub the doublewrite buffer ourselves.
-perl;
-use Fcntl 'SEEK_SET';
-my $page_size = $ENV{INNODB_PAGE_SIZE};
-open(FILE, "+<", "$ENV{SEARCH_FILE}") or die "cannot open: $!\n";
-seek(FILE, $page_size * 64, SEEK_SET) or die "cannot seek: $!\n";
-print(FILE chr(0) x ($page_size * 128)) or die "cannot write: $!\n";
-close FILE or die "cannot close: $!\n";;
-EOF
-
--- source include/search_pattern_in_file.inc
-
-let $formatno= $maxformatno;
-while ($formatno)
-{
-dec $formatno;
-
-let $t= delete_$formatno.ibd;
-let SEARCH_FILE= $MYSQLD_DATADIR/test/$t;
--- echo # $t
--- source include/search_pattern_in_file.inc
-let $t= delete_rollback_delete_$formatno.ibd;
-let SEARCH_FILE= $MYSQLD_DATADIR/test/$t;
--- echo # $t
--- source include/search_pattern_in_file.inc
-let $t= insert_rollback_$formatno.ibd;
-let SEARCH_FILE= $MYSQLD_DATADIR/test/$t;
--- echo # $t
--- source include/search_pattern_in_file.inc
-}
-
--- source include/start_mysqld.inc
-
-let $formatno= $maxformatno;
-while ($formatno)
-{
-dec $formatno;
-
-let $t= delete_$formatno, delete_rollback_delete_$formatno, insert_rollback_$formatno;
-
-eval check table $t;
-eval drop table $t;
-}
-
-show variables like 'innodb_%scrub_data%';
-
---echo # verify that this test have not caused any background scrubbing
---sorted_result
-select ss.variable_name, gs.variable_value - ss.variable_value as variable_value
-from snapshot_status ss,
-     information_schema.global_status gs
-where ss.variable_name = gs.variable_name;
-
-drop table snapshot_status;

mysql-test/suite/encryption/t/innodb_scrub_background.opt
@@ -1,7 +1,0 @@
---innodb-immediate-scrub-data-uncompressed=OFF
---innodb-background-scrub-data-uncompressed=ON
---innodb-background-scrub-data-compressed=ON
---loose-innodb-debug-force-scrubbing=ON
---innodb-encryption-threads=0
---innodb-encrypt-tables=OFF
---innodb-tablespaces-scrubbing

mysql-test/suite/encryption/t/innodb_scrub_background.test
@@ -1,170 +1,0 @@
--- source include/have_innodb.inc
--- source include/not_embedded.inc
--- source include/have_example_key_management_plugin.inc
-
-let $MYSQLD_DATADIR=`select @@datadir`;
-let INNODB_PAGE_SIZE= `select @@innodb_page_size`;
-
---echo #
---echo # immediate scrubbing is off
---echo # background scrubbing is on
---echo #
-show variables like 'innodb_%scrub_data%';
-
--- echo # make sure spaces are checked quickly
-SET GLOBAL innodb_background_scrub_data_check_interval=1;
-
-let $rowcount=500;
-let $maxformatno= 4;
-let $formatno= $maxformatno;
-
-let $tableformat= (
-  a int auto_increment primary key,
-  b varchar(256),
-  c text,
-  index(b)) engine = innodb row_format;
-
-while ($formatno)
-{
-dec $formatno;
-let $format = `select case $formatno
-               when 0 then 'dynamic'
-               when 1 then 'redundant'
-               when 2 then 'compact'
-               when 3 then 'compressed'
-               end`;
-
-let $t= delete_$formatno;
-eval create table $t $tableformat=$format;
-
-let $numinserts = $rowcount;
--- echo # Populate table with rows
---disable_query_log
-begin;
-while ($numinserts)
-{
-  dec $numinserts;
-  eval insert into $t(b,c) values ('unicycle', repeat('wonderwoman', 1000));
-}
-commit;
---enable_query_log
-
-eval delete from $t;
-
-let $t= delete_rollback_delete_$formatno;
-
-eval create table $t $tableformat=$format;
-
-let $numinserts = $rowcount;
--- echo # Populate table with rows
---disable_query_log
-begin;
-while ($numinserts)
-{
-  dec $numinserts;
-  eval insert into $t(b,c) values ('bicycle', repeat('repairman', 1000));
-}
-commit;
---enable_query_log
-
-begin;
-eval delete from $t;
-rollback;
-eval delete from $t;
-
-let $t= insert_rollback_$formatno;
-
-eval create table $t $tableformat=$format;
-
-let $numinserts = $rowcount;
--- echo # Populate table with rows
-begin;
---disable_query_log
-while ($numinserts)
-{
-  dec $numinserts;
-  eval insert into $t(b,c) values ('tricycle', repeat('superhuman', 1000));
-}
---enable_query_log
-
-rollback;
-}
-
--- echo # start scrubbing threads
-SET GLOBAL innodb_encryption_threads=5;
--- echo # Wait max 10 min for scrubbing
-let $cnt=600;
-while ($cnt)
-{
-    let $success=`SELECT COUNT(*) = 0 FROM INFORMATION_SCHEMA.INNODB_TABLESPACES_SCRUBBING WHERE LAST_SCRUB_COMPLETED IS NULL AND (NAME LIKE 'test/%' OR SPACE = 0)`;
-    if ($success)
-    {
-        let $cnt=0;
-    }
-    if (!$success)
-    {
-        real_sleep 1;
-        dec $cnt;
-    }
-}
-if (!$success)
-{
-    SELECT * FROM INFORMATION_SCHEMA.INNODB_TABLESPACES_SCRUBBING;
-    SHOW STATUS LIKE 'innodb_%scrub%';
-    -- die Timeout waiting for background threads
-}
-
-SET GLOBAL innodb_fast_shutdown=0;
--- source include/shutdown_mysqld.inc
-
-let SEARCH_ABORT= FOUND;
-let SEARCH_PATTERN= (un|b|tr)icycle|(repair|breakhu|wonderwo)man;
-let SEARCH_RANGE= 12582912;
-let SEARCH_FILE= $MYSQLD_DATADIR/ibdata1;
-
-# We may randomly find copies of unscrubbed pages in the doublewrite buffer.
-# Let us scrub the doublewrite buffer ourselves.
-perl;
-use Fcntl 'SEEK_SET';
-my $page_size = $ENV{INNODB_PAGE_SIZE};
-open(FILE, "+<", "$ENV{SEARCH_FILE}") or die "cannot open: $!\n";
-seek(FILE, $page_size * 64, SEEK_SET) or die "cannot seek: $!\n";
-print(FILE chr(0) x ($page_size * 128)) or die "cannot write: $!\n";
-close FILE or die "cannot close: $!\n";;
-EOF
-
--- source include/search_pattern_in_file.inc
-
-let $formatno= $maxformatno;
-while ($formatno)
-{
-dec $formatno;
-
-let $t= delete_$formatno.ibd;
-let SEARCH_FILE= $MYSQLD_DATADIR/test/$t;
--- echo # $t
--- source include/search_pattern_in_file.inc
-let $t= delete_rollback_delete_$formatno.ibd;
-let SEARCH_FILE= $MYSQLD_DATADIR/test/$t;
--- echo # $t
--- source include/search_pattern_in_file.inc
-let $t= insert_rollback_$formatno.ibd;
-let SEARCH_FILE= $MYSQLD_DATADIR/test/$t;
--- echo # $t
--- source include/search_pattern_in_file.inc
-}
-
--- source include/start_mysqld.inc
-
-let $formatno= $maxformatno;
-while ($formatno)
-{
-dec $formatno;
-
-let $t= delete_$formatno, delete_rollback_delete_$formatno, insert_rollback_$formatno;
-
-eval check table $t;
-eval drop table $t;
-}
-
-show variables like 'innodb_%scrub_data%';

commit b76e5c66107d75d0161d8f8ab3cf05fc360c831e

MDEV-24765 fseg_free_extent fails to call buf_page_free() for the whole segment
This is caused by commit c92f7e287fc0e21dc1b181284b1f8e2139d1c331(MDEV-8139).
InnoDB fails to set the page status as FREED in buffer pool while freeing
the extent.

storage/innobase/fsp/fsp0fsp.cc
@@ -2635,9 +2635,9 @@ fseg_free_extent(
 
 	fsp_free_extent(space, page, mtr);
 
-	for (ulint i = 0; i < FSP_EXTENT_SIZE; i++) {
+	for (uint32_t i = 0; i < FSP_EXTENT_SIZE; i++) {
 		if (!xdes_is_free(descr, i)) {
-			buf_page_free(space, first_page_in_extent + 1, mtr,
+			buf_page_free(space, first_page_in_extent + i, mtr,
 				      __FILE__, __LINE__);
 		}
 	}