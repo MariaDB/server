dc31627c2d3100ecb45ef71d8040fedff202a2f7
Olivier Bertrand <bertrandop@gmail.com>
2021-02-05 11:24:05 +0100

    Fix of connect engine crashes

--- storage/connect/bsonudf.cpp
+++ storage/connect/bsonudf.cpp
@@ -3710,6 +3710,7 @@ char *bson_get_item(UDF_INIT *initid, UDF_ARGS *args, char *result,
 			PUSH_WARNING("CheckMemory error");
 			goto fin;
 		} else {
+                        bnx.Reset();
 			jvp = bnx.MakeValue(args, 0, true);
 
 			if (g->Mrr) {			 // First argument is a constant
@@ -4055,6 +4056,7 @@ double bsonget_real(UDF_INIT *initid, UDF_ARGS *args,
 			*is_null = 1;
 			return 0.0;
 		} else {
+                        bnx.Reset();
 			jvp = bnx.MakeValue(args, 0);
 
 			if ((p = bnx.GetString(jvp))) {
