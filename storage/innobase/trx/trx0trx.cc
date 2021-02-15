MariaDB/server

commit b08448de64f2af9c849154647bdd61d1725f8928

MDEV-20612: Partition lock_sys.latch
We replace the old lock_sys.mutex (which was renamed to lock_sys.latch)
with a combination of a global lock_sys.latch and table or page hash lock
mutexes.

The global lock_sys.latch can be acquired in exclusive mode, or
it can be acquired in shared mode and another mutex will be acquired
to protect the locks for a particular page or a table.

This is inspired by
mysql/mysql-server@1d259b87a63defa814e19a7534380cb43ee23c48
but the optimization of lock_release() will be done in the next commit.
Also, we will interleave mutexes with the hash table elements, similar
to how buf_pool.page_hash was optimized
in commit 5155a300fab85e97217c75e3ba3c3ce78082dd8a (MDEV-22871).

dict_table_t::autoinc_trx: Use Atomic_relaxed.

dict_table_t::autoinc_mutex: Use srw_mutex in order to reduce the
memory footprint. On 64-bit Linux or OpenBSD, both this and the new
dict_table_t::lock_mutex should be 32 bits and be stored in the same
64-bit word. On Microsoft Windows, the underlying SRWLOCK is 32 or 64
bits, and on other systems, sizeof(pthread_mutex_t) can be much larger.

ib_lock_t::trx_locks, trx_lock_t::trx_locks: Document the new rules.
Writers must assert lock_sys.is_writer() || trx->mutex_is_owner().

LockGuard: A RAII wrapper for acquiring a page hash table lock.

LockGGuard: Like LockGuard, but when Galera Write-Set Replication
is enabled, we must acquire all shards, for updating arbitrary trx_locks.

LockMultiGuard: A RAII wrapper for acquiring two page hash table locks.

lock_rec_create_wsrep(), lock_table_create_wsrep(): Special
Galera conflict resolution in non-inlined functions in order
to keep the common code paths shorter.

lock_sys_t::prdt_page_free_from_discard(): Refactored from
lock_prdt_page_free_from_discard() and
lock_rec_free_all_from_discard_page().

trx_t::commit_tables(): Replaces trx_update_mod_tables_timestamp().

lock_release(): Let trx_t::commit_tables() invalidate the query cache

storage/innobase/trx/trx0trx.cc
@@ -491,7 +491,7 @@ inline void trx_t::release_locks()
   if (UT_LIST_GET_LEN(lock.trx_locks))
   {
     lock_release(this);
-    lock.n_rec_locks = 0;
+    ut_ad(!lock.n_rec_locks);
     ut_ad(UT_LIST_GET_LEN(lock.trx_locks) == 0);
     ut_ad(ib_vector_is_empty(autoinc_locks));
     mem_heap_empty(lock.lock_heap);
@@ -1209,66 +1209,50 @@ trx_flush_log_if_needed(
 	trx->op_info = "";
 }
 
-/**********************************************************************//**
-For each table that has been modified by the given transaction: update
-its dict_table_t::update_time with the current timestamp. Clear the list
-of the modified tables at the end. */
-static
-void
-trx_update_mod_tables_timestamp(
-/*============================*/
-	trx_t*	trx)	/*!< in: transaction */
+/** Process tables that were modified by the committing transaction. */
+inline void trx_t::commit_tables()
 {
-	/* consider using trx->start_time if calling time() is too
-	expensive here */
-	const time_t now = time(NULL);
+  if (!undo_no || mod_tables.empty())
+    return;
 
 #if defined SAFE_MUTEX && defined UNIV_DEBUG
-	const bool preserve_tables = !innodb_evict_tables_on_commit_debug
-		|| trx->is_recovered /* avoid trouble with XA recovery */
+  const bool preserve_tables= !innodb_evict_tables_on_commit_debug ||
+    is_recovered || /* avoid trouble with XA recovery */
 # if 1 /* if dict_stats_exec_sql() were not playing dirty tricks */
-		|| dict_sys.mutex_is_locked()
+    dict_sys.mutex_is_locked();
 # else /* this would be more proper way to do it */
-		|| trx->dict_operation_lock_mode || trx->dict_operation
+    dict_operation_lock_mode || dict_operation;
 # endif
-		;
 #endif
 
-	for (const auto& p : trx->mod_tables) {
-		/* This could be executed by multiple threads concurrently
-		on the same table object. This is fine because time_t is
-		word size or less. And _purely_ _theoretically_, even if
-		time_t write is not atomic, likely the value of 'now' is
-		the same in all threads and even if it is not, getting a
-		"garbage" in table->update_time is justified because
-		protecting it with a latch here would be too performance
-		intrusive. */
-		dict_table_t* table = p.first;
-		table->update_time = now;
+  const trx_id_t max_trx_id= trx_sys.get_max_trx_id();
+  const auto now= start_time;
+
+  for (const auto& p : mod_tables)
+  {
+    dict_table_t *table= p.first;
+    table->update_time= now;
+    table->query_cache_inv_trx_id= max_trx_id;
+
 #if defined SAFE_MUTEX && defined UNIV_DEBUG
-		if (preserve_tables || table->get_ref_count()
-		    || table->is_temporary()
-		    || UT_LIST_GET_LEN(table->locks)) {
-			/* do not evict when committing DDL operations
-			or if some other transaction is holding the
-			table handle */
-			continue;
-		}
-		/* recheck while holding the mutex that blocks
-		table->acquire() */
-		dict_sys.mutex_lock();
-		{
-			LockMutexGuard g{SRW_LOCK_CALL};
-			if (!table->get_ref_count()
-			    && !UT_LIST_GET_LEN(table->locks)) {
-				dict_sys.remove(table, true);
-			}
-		}
-		dict_sys.mutex_unlock();
+    if (preserve_tables || table->get_ref_count() || table->is_temporary() ||
+        UT_LIST_GET_LEN(table->locks))
+      /* do not evict when committing DDL operations or if some other
+      transaction is holding the table handle */
+      continue;
+    /* recheck while holding the mutex that blocks
+    table->acquire() */
+    dict_sys.mutex_lock();
+    {
+      LockMutexGuard g{SRW_LOCK_CALL};
+      if (!table->get_ref_count() && !UT_LIST_GET_LEN(table->locks))
+        dict_sys.remove(table, true);
+    }
+    dict_sys.mutex_unlock();
 #endif
-	}
+  }
 
-	trx->mod_tables.clear();
+  mod_tables.clear();
 }
 
 /** Evict a table definition due to the rollback of ALTER TABLE.
@@ -1366,7 +1350,7 @@ inline void trx_t::commit_in_memory(const mtr_t *mtr)
     }
     else
     {
-      trx_update_mod_tables_timestamp(this);
+      commit_tables();
       MONITOR_INC(MONITOR_TRX_RW_COMMIT);
       is_recovered= false;
     }

storage/innobase/btr/btr0btr.cc
@@ -3355,11 +3355,10 @@ btr_lift_page_up(
 		const page_id_t id{block->page.id()};
 		/* Free predicate page locks on the block */
 		if (index->is_spatial()) {
-			LockMutexGuard g{SRW_LOCK_CALL};
-			lock_prdt_page_free_from_discard(
-				id, &lock_sys.prdt_page_hash);
+			lock_sys.prdt_page_free_from_discard(id);
+		} else {
+			lock_update_copy_and_discard(*father_block, id);
 		}
-		lock_update_copy_and_discard(*father_block, id);
 	}
 
 	/* Go upward to root page, decrementing levels by one. */
@@ -3609,10 +3608,7 @@ retry:
 			}
 
 			/* No GAP lock needs to be worrying about */
-			LockMutexGuard g{SRW_LOCK_CALL};
-			lock_prdt_page_free_from_discard(
-				id, &lock_sys.prdt_page_hash);
-			lock_rec_free_all_from_discard_page(id);
+			lock_sys.prdt_page_free_from_discard(id);
 		} else {
 			btr_cur_node_ptr_delete(&father_cursor, mtr);
 			if (!dict_table_is_locking_disabled(index->table)) {
@@ -3762,10 +3758,7 @@ retry:
 							 merge_page, mtr);
 			}
 			const page_id_t id{block->page.id()};
-			LockMutexGuard g{SRW_LOCK_CALL};
-			lock_prdt_page_free_from_discard(
-				id, &lock_sys.prdt_page_hash);
-			lock_rec_free_all_from_discard_page(id);
+			lock_sys.prdt_page_free_from_discard(id);
 		} else {
 
 			compressed = btr_cur_pessimistic_delete(&err, TRUE,

storage/innobase/btr/btr0cur.cc
@@ -1999,12 +1999,13 @@ retry_page_get:
 		trx_t*		trx = thr_get_trx(cursor->thr);
 		lock_prdt_t	prdt;
 
-		{
-			LockMutexGuard g{SRW_LOCK_CALL};
-			lock_init_prdt_from_mbr(
-				&prdt, &cursor->rtr_info->mbr, mode,
-				trx->lock.lock_heap);
-		}
+		lock_sys.rd_lock(SRW_LOCK_CALL);
+		trx->mutex_lock();
+		lock_init_prdt_from_mbr(
+			&prdt, &cursor->rtr_info->mbr, mode,
+			trx->lock.lock_heap);
+		lock_sys.rd_unlock();
+		trx->mutex_unlock();
 
 		if (rw_latch == RW_NO_LATCH && height != 0) {
 			block->lock.s_lock();
@@ -3242,8 +3243,7 @@ btr_cur_ins_lock_and_undo(
 			/* Use on stack MBR variable to test if a lock is
 			needed. If so, the predicate (MBR) will be allocated
 			from lock heap in lock_prdt_insert_check_and_lock() */
-			lock_init_prdt_from_mbr(
-				&prdt, &mbr, 0, NULL);
+			lock_init_prdt_from_mbr(&prdt, &mbr, 0, nullptr);
 
 			if (dberr_t err = lock_prdt_insert_check_and_lock(
 				    rec, btr_cur_get_block(cursor),

storage/innobase/dict/dict0dict.cc
@@ -1247,7 +1247,8 @@ inline void dict_sys_t::add(dict_table_t* table)
 
 	ulint fold = ut_fold_string(table->name.m_name);
 
-	new (&table->autoinc_mutex) std::mutex();
+	table->autoinc_mutex.init();
+	table->lock_mutex_init();
 
 	/* Look for a table with the same name: error if such exists */
 	{
@@ -2038,7 +2039,8 @@ void dict_sys_t::remove(dict_table_t* table, bool lru, bool keep)
 		UT_DELETE(table->vc_templ);
 	}
 
-	table->autoinc_mutex.~mutex();
+	table->autoinc_mutex.destroy();
+	table->lock_mutex_destroy();
 
 	if (keep) {
 		return;

storage/innobase/gis/gis0sea.cc
@@ -1197,9 +1197,7 @@ rtr_check_discard_page(
 
 	mysql_mutex_unlock(&index->rtr_track->rtr_active_mutex);
 
-	LockMutexGuard g{SRW_LOCK_CALL};
-	lock_prdt_page_free_from_discard(id, &lock_sys.prdt_hash);
-	lock_prdt_page_free_from_discard(id, &lock_sys.prdt_page_hash);
+	lock_sys.prdt_page_free_from_discard(id, true);
 }
 
 /** Structure acts as functor to get the optimistic access of the page.

storage/innobase/handler/ha_innodb.cc
@@ -2253,7 +2253,7 @@ ha_innobase::innobase_reset_autoinc(
 	if (error == DB_SUCCESS) {
 
 		dict_table_autoinc_initialize(m_prebuilt->table, autoinc);
-		m_prebuilt->table->autoinc_mutex.unlock();
+		m_prebuilt->table->autoinc_mutex.wr_unlock();
 	}
 
 	return(error);
@@ -2685,13 +2685,13 @@ static bool innobase_query_caching_table_check_low(
 	For read-only transaction: should satisfy (1) and (3)
 	For read-write transaction: should satisfy (1), (2), (3) */
 
-	if (trx->id && trx->id < table->query_cache_inv_trx_id) {
+	const trx_id_t inv = table->query_cache_inv_trx_id;
+
+	if (trx->id && trx->id < inv) {
 		return false;
 	}
 
-	if (trx->read_view.is_open()
-	    && trx->read_view.low_limit_id()
-	    < table->query_cache_inv_trx_id) {
+	if (trx->read_view.is_open() && trx->read_view.low_limit_id() < inv) {
 		return false;
 	}
 
@@ -5359,7 +5359,7 @@ initialize_auto_increment(dict_table_t* table, const Field* field)
 
 	const unsigned	col_no = innodb_col_no(field);
 
-	table->autoinc_mutex.lock();
+	table->autoinc_mutex.wr_lock();
 
 	table->persistent_autoinc = static_cast<uint16_t>(
 		dict_table_get_nth_col_pos(table, col_no, NULL) + 1)
@@ -5390,7 +5390,7 @@ initialize_auto_increment(dict_table_t* table, const Field* field)
 			innobase_get_int_col_max_value(field));
 	}
 
-	table->autoinc_mutex.unlock();
+	table->autoinc_mutex.wr_unlock();
 }
 
 /** Open an InnoDB table
@@ -7192,7 +7192,7 @@ ha_innobase::innobase_lock_autoinc(void)
 	switch (innobase_autoinc_lock_mode) {
 	case AUTOINC_NO_LOCKING:
 		/* Acquire only the AUTOINC mutex. */
-		m_prebuilt->table->autoinc_mutex.lock();
+		m_prebuilt->table->autoinc_mutex.wr_lock();
 		break;
 
 	case AUTOINC_NEW_STYLE_LOCKING:
@@ -7206,14 +7206,14 @@ ha_innobase::innobase_lock_autoinc(void)
 		case SQLCOM_REPLACE:
 		case SQLCOM_END: // RBR event
 			/* Acquire the AUTOINC mutex. */
-			m_prebuilt->table->autoinc_mutex.lock();
+			m_prebuilt->table->autoinc_mutex.wr_lock();
 			/* We need to check that another transaction isn't
 			already holding the AUTOINC lock on the table. */
 			if (!m_prebuilt->table->n_waiting_or_granted_auto_inc_locks) {
 				/* Do not fall back to old style locking. */
 				DBUG_RETURN(error);
 			}
-			m_prebuilt->table->autoinc_mutex.unlock();
+			m_prebuilt->table->autoinc_mutex.wr_unlock();
 		}
 		/* Use old style locking. */
 		/* fall through */
@@ -7225,7 +7225,7 @@ ha_innobase::innobase_lock_autoinc(void)
 		if (error == DB_SUCCESS) {
 
 			/* Acquire the AUTOINC mutex. */
-			m_prebuilt->table->autoinc_mutex.lock();
+			m_prebuilt->table->autoinc_mutex.wr_lock();
 		}
 		break;
 
@@ -7253,7 +7253,7 @@ ha_innobase::innobase_set_max_autoinc(
 	if (error == DB_SUCCESS) {
 
 		dict_table_autoinc_update_if_greater(m_prebuilt->table, auto_inc);
-		m_prebuilt->table->autoinc_mutex.unlock();
+		m_prebuilt->table->autoinc_mutex.wr_unlock();
 	}
 
 	return(error);
@@ -12634,7 +12634,7 @@ create_table_info_t::create_table_update_dict()
 			autoinc = 1;
 		}
 
-		innobase_table->autoinc_mutex.lock();
+		innobase_table->autoinc_mutex.wr_lock();
 		dict_table_autoinc_initialize(innobase_table, autoinc);
 
 		if (innobase_table->is_temporary()) {
@@ -12662,7 +12662,7 @@ create_table_info_t::create_table_update_dict()
 			}
 		}
 
-		innobase_table->autoinc_mutex.unlock();
+		innobase_table->autoinc_mutex.wr_unlock();
 	}
 
 	innobase_parse_hint_from_comment(m_thd, innobase_table, m_form->s);
@@ -15910,7 +15910,7 @@ ha_innobase::innobase_get_autoinc(
 		/* It should have been initialized during open. */
 		if (*value == 0) {
 			m_prebuilt->autoinc_error = DB_UNSUPPORTED;
-			m_prebuilt->table->autoinc_mutex.unlock();
+			m_prebuilt->table->autoinc_mutex.wr_unlock();
 		}
 	}
 
@@ -15934,7 +15934,7 @@ ha_innobase::innobase_peek_autoinc(void)
 
 	innodb_table = m_prebuilt->table;
 
-	innodb_table->autoinc_mutex.lock();
+	innodb_table->autoinc_mutex.wr_lock();
 
 	auto_inc = dict_table_autoinc_read(innodb_table);
 
@@ -15943,7 +15943,7 @@ ha_innobase::innobase_peek_autoinc(void)
 			" '" << innodb_table->name << "'";
 	}
 
-	innodb_table->autoinc_mutex.unlock();
+	innodb_table->autoinc_mutex.wr_unlock();
 
 	return(auto_inc);
 }
@@ -16050,7 +16050,7 @@ ha_innobase::get_auto_increment(
 		/* Out of range number. Let handler::update_auto_increment()
 		take care of this */
 		m_prebuilt->autoinc_last_value = 0;
-		m_prebuilt->table->autoinc_mutex.unlock();
+		m_prebuilt->table->autoinc_mutex.wr_unlock();
 		*nb_reserved_values= 0;
 		return;
 	}
@@ -16093,7 +16093,7 @@ ha_innobase::get_auto_increment(
 	m_prebuilt->autoinc_offset = offset;
 	m_prebuilt->autoinc_increment = increment;
 
-	m_prebuilt->table->autoinc_mutex.unlock();
+	m_prebuilt->table->autoinc_mutex.wr_unlock();
 }
 
 /*******************************************************************//**
@@ -17998,7 +17998,6 @@ int wsrep_innobase_kill_one_trx(THD *bf_thd, trx_t *victim_trx, bool signal)
 {
 	ut_ad(bf_thd);
 	ut_ad(victim_trx);
-	lock_sys.assert_locked();
 	ut_ad(victim_trx->mutex_is_owner());
 
 	DBUG_ENTER("wsrep_innobase_kill_one_trx");
@@ -18059,6 +18058,7 @@ int wsrep_innobase_kill_one_trx(THD *bf_thd, trx_t *victim_trx, bool signal)
 	} else if (victim_trx->lock.wait_lock) {
 		mysql_mutex_lock(&lock_sys.wait_mutex);
 		if (lock_t* wait_lock = victim_trx->lock.wait_lock) {
+			lock_sys.assert_locked(*wait_lock);
 			DBUG_ASSERT(victim_trx->is_wsrep());
 			WSREP_DEBUG("victim has wait flag: %lu",
 				    thd_get_thread_id(thd));

storage/innobase/ibuf/ibuf0ibuf.cc
@@ -3280,8 +3280,8 @@ commit_exit:
 		ibuf_mtr_commit(&bitmap_mtr);
 		goto fail_exit;
 	} else {
-		LockMutexGuard g{SRW_LOCK_CALL};
-		if (lock_sys.get_first(page_id)) {
+		LockGuard g{lock_sys.rec_hash, page_id};
+		if (lock_sys.rec_hash.get_first(page_id)) {
 			goto commit_exit;
 		}
 	}

storage/innobase/include/dict0mem.h
@@ -1958,6 +1958,26 @@ struct dict_table_t {
   /** Clear the table when rolling back TRX_UNDO_EMPTY */
   void clear(que_thr_t *thr);
 
+#ifdef UNIV_DEBUG
+  /** @return whether the current thread holds the lock_mutex */
+  bool lock_mutex_is_owner() const
+  { return lock_mutex_owner == os_thread_get_curr_id(); }
+#endif /* UNIV_DEBUG */
+  void lock_mutex_init() { lock_mutex.init(); }
+  void lock_mutex_destroy() { lock_mutex.destroy(); }
+  /** Acquire lock_mutex */
+  void lock_mutex_lock()
+  {
+    ut_ad(!lock_mutex_is_owner());
+    lock_mutex.wr_lock();
+    ut_ad(!lock_mutex_owner.exchange(os_thread_get_curr_id()));
+  }
+  /** Release lock_mutex */
+  void lock_mutex_unlock()
+  {
+    ut_ad(lock_mutex_owner.exchange(0) == os_thread_get_curr_id());
+    lock_mutex.wr_unlock();
+  }
 private:
 	/** Initialize instant->field_map.
 	@param[in]	table	table definition to copy from */
@@ -2118,18 +2138,12 @@ public:
 	/** Maximum recursive level we support when loading tables chained
 	together with FK constraints. If exceeds this level, we will stop
 	loading child table into memory along with its parent table. */
-	unsigned				fk_max_recusive_level:8;
+	byte					fk_max_recusive_level;
 
 	/** Count of how many foreign key check operations are currently being
 	performed on the table. We cannot drop the table while there are
 	foreign key checks running on it. */
 	Atomic_counter<int32_t>			n_foreign_key_checks_running;
-
-	/** Transactions whose view low limit is greater than this number are
-	not allowed to store to the MySQL query cache or retrieve from it.
-	When a trx with undo logs commits, it sets this to the value of the
-	transaction id. */
-	trx_id_t				query_cache_inv_trx_id;
 
 	/** Transaction id that last touched the table definition. Either when
 	loading the definition or CREATE TABLE, or ALTER TABLE (prepare,
@@ -2273,15 +2287,27 @@ public:
 	from a select. */
 	lock_t*					autoinc_lock;
 
-	/** Mutex protecting the autoincrement counter. */
-	std::mutex				autoinc_mutex;
+  /** Mutex protecting autoinc. */
+  srw_mutex autoinc_mutex;
+private:
+  /** Mutex protecting locks on this table. */
+  srw_mutex lock_mutex;
+#ifdef UNIV_DEBUG
+  /** The owner of lock_mutex (0 if none) */
+  Atomic_relaxed<os_thread_id_t> lock_mutex_owner{0};
+#endif
+public:
+  /** Autoinc counter value to give to the next inserted row. */
+  uint64_t autoinc;
 
-	/** Autoinc counter value to give to the next inserted row. */
-	ib_uint64_t				autoinc;
-
-	/** The transaction that currently holds the the AUTOINC lock on this
-	table. Protected by lock_sys.latch. */
-	const trx_t*				autoinc_trx;
+  /** The transaction that currently holds the the AUTOINC lock on this table.
+  Protected by lock_mutex.
+  The thread that is executing autoinc_trx may read this field without
+  holding a latch, in row_lock_table_autoinc_for_mysql().
+  Only the autoinc_trx thread may clear this field; it cannot be
+  modified on the behalf of a transaction that is being handled by a
+  different thread. */
+  Atomic_relaxed<const trx_t*> autoinc_trx;
 
   /** Number of granted or pending autoinc_lock on this table. This
   value is set after acquiring lock_sys.latch but
@@ -2293,7 +2319,7 @@ public:
 	/* @} */
 
   /** Number of granted or pending LOCK_S or LOCK_X on the table.
-  Protected by lock_sys.assert_locked(*this). */
+  Protected by lock_mutex. */
   uint32_t n_lock_x_or_s;
 
 	/** FTS specific state variables. */
@@ -2304,22 +2330,29 @@ public:
 	in X mode of this table's indexes. */
 	ib_quiesce_t				quiesce;
 
-	/** Count of the number of record locks on this table. We use this to
-	determine whether we can evict the table from the dictionary cache.
-	Protected by LockGuard. */
-	ulint n_rec_locks;
+  /** Count of the number of record locks on this table. We use this to
+  determine whether we can evict the table from the dictionary cache.
+  Modified when lock_sys.is_writer(), or
+  lock_sys.assert_locked(page_id) and trx->mutex_is_owner() hold.
+  @see trx_lock_t::trx_locks */
+  Atomic_counter<uint32_t> n_rec_locks;
 
 private:
-	/** Count of how many handles are opened to this table. Dropping of the
-	table is NOT allowed until this count gets to zero. MySQL does NOT
-	itself check the number of open handles at DROP. */
-	Atomic_counter<uint32_t>		n_ref_count;
+  /** Count of how many handles are opened to this table. Dropping of the
+  table is NOT allowed until this count gets to zero. MySQL does NOT
+  itself check the number of open handles at DROP. */
+  Atomic_counter<uint32_t> n_ref_count;
 public:
   /** List of locks on the table. Protected by lock_sys.assert_locked(lock). */
   table_lock_list_t locks;
 
-	/** Timestamp of the last modification of this table. */
-	time_t					update_time;
+  /** Timestamp of the last modification of this table. */
+  Atomic_relaxed<time_t> update_time;
+  /** Transactions whose view low limit is greater than this number are
+  not allowed to store to the query cache or retrieve from it.
+  When a trx with undo logs commits, it sets this to the value of the
+  transaction id. */
+  Atomic_relaxed<trx_id_t> query_cache_inv_trx_id;
 
 #ifdef UNIV_DEBUG
 	/** Value of 'magic_n'. */

commit 2e84846ec02ec73476c41cd6e7b16ca8977ecf73

MDEV-24861 Assertion `trx->rsegs.m_redo.rseg' failed in innodb_prepare_commit_versioned
trx_t::commit_tables(): Ensure that mod_tables will be empty.
This was broken in commit b08448de64f2af9c849154647bdd61d1725f8928
where the query cache invalidation was moved from lock_release().

storage/innobase/trx/trx0trx.cc
@@ -1226,44 +1226,46 @@ trx_flush_log_if_needed(
 /** Process tables that were modified by the committing transaction. */
 inline void trx_t::commit_tables()
 {
-  if (!undo_no || mod_tables.empty())
+  if (mod_tables.empty())
     return;
 
+  if (undo_no)
+  {
 #if defined SAFE_MUTEX && defined UNIV_DEBUG
-  const bool preserve_tables= !innodb_evict_tables_on_commit_debug ||
-    is_recovered || /* avoid trouble with XA recovery */
+    const bool preserve_tables= !innodb_evict_tables_on_commit_debug ||
+      is_recovered || /* avoid trouble with XA recovery */
 # if 1 /* if dict_stats_exec_sql() were not playing dirty tricks */
-    dict_sys.mutex_is_locked();
+      dict_sys.mutex_is_locked();
 # else /* this would be more proper way to do it */
-    dict_operation_lock_mode || dict_operation;
+      dict_operation_lock_mode || dict_operation;
 # endif
 #endif
 
-  const trx_id_t max_trx_id= trx_sys.get_max_trx_id();
-  const auto now= start_time;
+    const trx_id_t max_trx_id= trx_sys.get_max_trx_id();
+    const auto now= start_time;
 
-  for (const auto& p : mod_tables)
-  {
-    dict_table_t *table= p.first;
-    table->update_time= now;
-    table->query_cache_inv_trx_id= max_trx_id;
+    for (const auto& p : mod_tables)
+    {
+      dict_table_t *table= p.first;
+      table->update_time= now;
+      table->query_cache_inv_trx_id= max_trx_id;
 
 #if defined SAFE_MUTEX && defined UNIV_DEBUG
-    if (preserve_tables || table->get_ref_count() || table->is_temporary() ||
-        UT_LIST_GET_LEN(table->locks))
-      /* do not evict when committing DDL operations or if some other
-      transaction is holding the table handle */
-      continue;
-    /* recheck while holding the mutex that blocks
-    table->acquire() */
-    dict_sys.mutex_lock();
-    {
-      LockMutexGuard g{SRW_LOCK_CALL};
-      if (!table->get_ref_count() && !UT_LIST_GET_LEN(table->locks))
-        dict_sys.remove(table, true);
-    }
-    dict_sys.mutex_unlock();
+      if (preserve_tables || table->get_ref_count() || table->is_temporary() ||
+          UT_LIST_GET_LEN(table->locks))
+        /* do not evict when committing DDL operations or if some other
+        transaction is holding the table handle */
+        continue;
+      /* recheck while holding the mutex that blocks table->acquire() */
+      dict_sys.mutex_lock();
+      {
+        LockMutexGuard g{SRW_LOCK_CALL};
+        if (!table->get_ref_count() && !UT_LIST_GET_LEN(table->locks))
+          dict_sys.remove(table, true);
+      }
+      dict_sys.mutex_unlock();
 #endif
+    }
   }
 
   mod_tables.clear();

mysql-test/suite/innodb/r/innodb-truncate.result
@@ -91,3 +91,16 @@ ALTER TABLE t1 FORCE;
 TRUNCATE TABLE t1;
 ERROR 42000: Cannot truncate a table referenced in a foreign key constraint (`test`.`t2`, CONSTRAINT `t2_ibfk_1` FOREIGN KEY (`f2`) REFERENCES `test`.`t3` (`f2`))
 DROP TABLE t2, t1;
+#
+# MDEV-24861 Assertion `trx->rsegs.m_redo.rseg' failed
+# in innodb_prepare_commit_versioned
+#
+CREATE TABLE t1 (id INT PRIMARY KEY, f TEXT UNIQUE,
+s BIGINT UNSIGNED AS ROW START, e BIGINT UNSIGNED AS ROW END,
+PERIOD FOR SYSTEM_TIME(s,e))
+ENGINE=InnoDB WITH SYSTEM VERSIONING;
+CREATE TABLE t2 (id INT PRIMARY KEY) ENGINE=InnoDB;
+ALTER TABLE t1 FORCE;
+TRUNCATE TABLE t2;
+DROP TABLE t1, t2;
+# End of 10.6 tests

mysql-test/suite/innodb/t/innodb-truncate.test
@@ -92,3 +92,19 @@ ALTER TABLE t1 FORCE;
 --error ER_TRUNCATE_ILLEGAL_FK
 TRUNCATE TABLE t1;
 DROP TABLE t2, t1;
+
+--echo #
+--echo # MDEV-24861 Assertion `trx->rsegs.m_redo.rseg' failed
+--echo # in innodb_prepare_commit_versioned
+--echo #
+
+CREATE TABLE t1 (id INT PRIMARY KEY, f TEXT UNIQUE,
+                 s BIGINT UNSIGNED AS ROW START, e BIGINT UNSIGNED AS ROW END,
+                 PERIOD FOR SYSTEM_TIME(s,e))
+ENGINE=InnoDB WITH SYSTEM VERSIONING;
+CREATE TABLE t2 (id INT PRIMARY KEY) ENGINE=InnoDB;
+ALTER TABLE t1 FORCE;
+TRUNCATE TABLE t2;
+DROP TABLE t1, t2;
+
+--echo # End of 10.6 tests