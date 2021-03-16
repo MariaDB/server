d1ff2c583f452d8e1899f800609c329c998f0a33
mkaruza <mario.karuza@galeracluster.com>
2021-03-16 12:53:40 +0100

    MDEV-21697: Galera assertion !wsrep_has_changes(thd) || (thd->lex->sql_command == SQLCOM_CREATE_TABLE && !thd->is_current_stmt_binlog_format_row())
    Prevent adding WSREP keys with CTAS when table is is not InnoDB.
    
    Reviewed-by: Jan Lindström <jan.lindstrom@mariadb.com>

--- sql/sql_insert.cc
+++ sql/sql_insert.cc
@@ -4724,7 +4724,8 @@ bool select_create::send_eof()
   if (!table->s->tmp_table)
   {
 #ifdef WITH_WSREP
-    if (WSREP(thd))
+    if (WSREP(thd) &&
+        table->file->ht->db_type == DB_TYPE_INNODB)
     {
       if (thd->wsrep_trx_id() == WSREP_UNDEFINED_TRX_ID)
       {
