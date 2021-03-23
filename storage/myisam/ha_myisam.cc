f85afa5124b91d43105d32825a46584047975de7
Nikita Malyavin <nikitamalyavin@gmail.com>
2021-03-23 16:48:02 +0300

    MDEV-19011 Assertion `file->s->base.reclength < file->s->vreclength' failed
    The assertion is improved: storage engines like myisam always have to store
    at least one field, so the assertion does not cover tables with no stored
    columns.

--- storage/myisam/ha_myisam.cc
+++ storage/myisam/ha_myisam.cc
@@ -945,7 +945,8 @@ void ha_myisam::setup_vcols_for_repair(HA_CHECK *param)
       return;
     file->s->vreclength= new_vreclength;
   }
-  DBUG_ASSERT(file->s->base.reclength < file->s->vreclength);
+  DBUG_ASSERT(file->s->base.reclength < file->s->vreclength ||
+              !table->s->stored_fields);
   param->fix_record= compute_vcols;
   table->use_all_columns();
   table->vcol_set= &table->s->all_set;
