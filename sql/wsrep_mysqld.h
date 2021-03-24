619d852c3d394d69ad9e8b0a01239e2d6732efe9
Daniele Sciascia <daniele.sciascia@galeracluster.com>
2021-03-24 10:55:27 +0100

    MDEV-25226 Assertion when wsrep_on set OFF with SR transaction
    This patch makes the following changes around variable wsrep_on:
    
    1) Variable wsrep_on can no longer be updated from a session that has
    an active transaction running. The original behavior allowed cases
    like this:
    
         BEGIN;
         INSERT INTO t1 VALUES (1);
         SET SESSION wsrep_on = OFF;
         INSERT INTO t1 VALUES (2);
         COMMIT;
    
    With regular transactions this would result in no replication
    events (not even value 1). With streaming replication it would be
    unnecessarily complex to achieve the same behavior. In the above
    example, it would be possible for value 1 to be already replicated if
    it happened to fill a separate fragment, while value 2 wouldn't.
    
    2) Global variable wsrep_on no longer affects current sessions, only
    subsequent ones. This is to avoid a similar case to the above, just
    using just by using global wsrep_on instead session wsrep_on:
    
          --connection conn_1
          BEGIN;
          INSERT INTO t1 VALUES(1);
    
          --connection conn_2
          SET GLOBAL wsrep_on = OFF;
    
          --connection conn_1
          INSERT INTO t1 VALUES(2);
          COMMIT;
    
    The above example results in the transaction to be replicated, as
    global wsrep_on will only affect the session wsrep_on of new
    connections.

--- sql/wsrep_mysqld.h
+++ sql/wsrep_mysqld.h
@@ -20,6 +20,7 @@
 
 #ifdef WITH_WSREP
 extern bool WSREP_ON_;
+extern bool WSREP_PROVIDER_EXISTS_;
 
 #include <mysql/plugin.h>
 #include "mysql/service_wsrep.h"
@@ -221,7 +222,8 @@ extern wsrep_seqno_t wsrep_locked_seqno;
 /* use xxxxxx_NNULL macros when thd pointer is guaranteed to be non-null to
  * avoid compiler warnings (GCC 6 and later) */
 
-#define WSREP_NNULL(thd) (WSREP_ON && thd->variables.wsrep_on)
+#define WSREP_NNULL(thd) \
+  (WSREP_PROVIDER_EXISTS_ && thd->variables.wsrep_on)
 
 #define WSREP(thd) \
   (thd && WSREP_NNULL(thd))
@@ -277,8 +279,7 @@ void WSREP_LOG(void (*fun)(const char* fmt, ...), const char* fmt, ...);
     WSREP_INFO("context: %s:%d", __FILE__, __LINE__); \
   }
 
-#define WSREP_PROVIDER_EXISTS                                                  \
-  (wsrep_provider && strncasecmp(wsrep_provider, WSREP_NONE, FN_REFLEN))
+#define WSREP_PROVIDER_EXISTS (WSREP_PROVIDER_EXISTS_)
 
 static inline bool wsrep_cluster_address_exists()
 {
