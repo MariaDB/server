6fe624b5acf51383d39eb0e547a03779d41cf4a8
Sergei Golubchik <serg@mariadb.org>
2021-04-03 12:12:15 +0200

    MDEV-25242 Server crashes in check_grant upon invoking function with userstat enabled
    also fix index_stats table.
    
    followup for 5a798071199

--- plugin/userstat/index_stats.cc
+++ plugin/userstat/index_stats.cc
@@ -26,7 +26,7 @@ static int index_stats_fill(THD *thd, TABLE_LIST *tables, COND *cond)
     tmp_table.grant.privilege= 0;
     if (check_access(thd, SELECT_ACL, tmp_table.db,
                       &tmp_table.grant.privilege, NULL, 0, 1) ||
-        check_grant(thd, SELECT_ACL, &tmp_table, 1, UINT_MAX, 1))
+        check_grant(thd, SELECT_ACL, &tmp_table, 1, 1, 1))
       continue;
 
     index_name=         strend(tmp_table.table_name)+1;
