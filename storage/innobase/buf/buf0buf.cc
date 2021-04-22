MariaDB/server

commit 5e62b6a5e06eb02cbde1e34e95e26f42d87fce02

MDEV-16264  Use threadpool for Innodb background work.
Almost all threads have gone
- the "ticking" threads, that sleep a while then do some work)
(srv_monitor_thread, srv_error_monitor_thread, srv_master_thread)
were replaced with timers. Some timers are periodic,
e.g the "master" timer.

- The btr_defragment_thread is also replaced by a timer , which
reschedules it self when current defragment "item" needs throttling

- the buf_resize_thread and buf_dump_threads are substitutes with tasks
Ditto with page cleaner workers.

- purge workers threads are not tasks as well, and purge cleaner
coordinator is a combination of a task and timer.

- All AIO is outsourced to tpool, Innodb just calls thread_pool::submit_io()
and provides the callback.

- The srv_slot_t was removed, and innodb_debug_sync used in purge
is currently not working, and needs reimplementation.

storage/innobase/buf/buf0buf.cc
@@ -2021,7 +2021,7 @@ buf_pool_init_instance(
 	/* Initialize the temporal memory array and slots */
 	new(&buf_pool->io_buf) buf_pool_t::io_buf_t(
 		(srv_n_read_io_threads + srv_n_write_io_threads)
-		* (8 * OS_AIO_N_PENDING_IOS_PER_THREAD));
+		* OS_AIO_N_PENDING_IOS_PER_THREAD);
 
 	buf_pool_mutex_exit(buf_pool);
 
@@ -3186,47 +3186,38 @@ calc_buf_pool_size:
 	return;
 }
 
-/** This is the thread for resizing buffer pool. It waits for an event and
-when waked up either performs a resizing and sleeps again.
-@return	this function does not return, calls os_thread_exit()
-*/
-extern "C"
-os_thread_ret_t
-DECLARE_THREAD(buf_resize_thread)(void*)
+/* Thread pool task invoked by innodb_buffer_pool_size changes. */
+static void buf_resize_callback(void *)
 {
-	my_thread_init();
-
-	while (srv_shutdown_state == SRV_SHUTDOWN_NONE) {
-		os_event_wait(srv_buf_resize_event);
-		os_event_reset(srv_buf_resize_event);
-
-		if (srv_shutdown_state != SRV_SHUTDOWN_NONE) {
-			break;
-		}
-
-		buf_pool_mutex_enter_all();
-		if (srv_buf_pool_old_size == srv_buf_pool_size) {
-			buf_pool_mutex_exit_all();
-			std::ostringstream sout;
-			sout << "Size did not change (old size = new size = "
-				<< srv_buf_pool_size << ". Nothing to do.";
-			buf_resize_status(sout.str().c_str());
-
-			/* nothing to do */
-			continue;
-		}
+	ut_a(srv_shutdown_state == SRV_SHUTDOWN_NONE);
+	buf_pool_mutex_enter_all();
+	if (srv_buf_pool_old_size == srv_buf_pool_size) {
 		buf_pool_mutex_exit_all();
-
-		buf_pool_resize();
+		std::ostringstream sout;
+		sout << "Size did not change (old size = new size = "
+			<< srv_buf_pool_size << ". Nothing to do.";
+			buf_resize_status(sout.str().c_str());
+		return;
 	}
-
-	srv_buf_resize_thread_active = false;
-
-	my_thread_end();
-	os_thread_exit();
-
-	OS_THREAD_DUMMY_RETURN;
+	buf_pool_mutex_exit_all();
+	buf_pool_resize();
 }
+
+/* Ensure that task does not run in parallel, by setting max_concurrency to 1 for the thread group */
+static tpool::task_group single_threaded_group(1);
+static tpool::waitable_task buf_resize_task(buf_resize_callback,
+	nullptr, &single_threaded_group);
+
+void buf_resize_start()
+{
+	srv_thread_pool->submit_task(&buf_resize_task);
+}
+
+void buf_resize_shutdown()
+{
+	buf_resize_task.wait();
+}
+
 
 #ifdef BTR_CUR_HASH_ADAPT
 /** Clear the adaptive hash index on all pages in the buffer pool. */

extra/mariabackup/xtrabackup.cc
@@ -102,6 +102,7 @@ Street, Fifth Floor, Boston, MA 02110-1335 USA
 #include <srv0srv.h>
 #include <crc_glue.h>
 #include <log.h>
+#include <thr_timer.h>
 
 int sys_var_init();
 
@@ -4052,7 +4053,7 @@ fail:
                                                 especially in 64-bit
                                                 computers */
         }
-
+	srv_thread_pool_init();
 	sync_check_init();
 	ut_d(sync_check_enable());
 	/* Reset the system variables in the recovery module. */
@@ -6133,9 +6134,12 @@ int main(int argc, char **argv)
 		DBUG_SET(dbug_option);
 	}
 #endif
+	/* Main functions for library */
+	init_thr_timer(5);
 
 	int status = main_low(server_defaults);
 
+	end_thr_timer();
 	backup_cleanup();
 
 	if (innobackupex_mode) {

mysql-test/suite/gcol/r/innodb_virtual_debug_purge.result
@@ -233,48 +233,3 @@ set global debug_dbug= @saved_dbug;
 drop table t1;
 set debug_sync=reset;
 SET GLOBAL innodb_purge_rseg_truncate_frequency = @saved_frequency;
-#
-# MDEV-18546 ASAN heap-use-after-free
-# in innobase_get_computed_value / row_purge
-#
-CREATE TABLE t1 (
-pk INT AUTO_INCREMENT,
-b BIT(15),
-v BIT(15) AS (b) VIRTUAL,
-PRIMARY KEY(pk),
-UNIQUE(v)
-) ENGINE=InnoDB;
-INSERT IGNORE INTO t1 (b) VALUES
-(NULL),(b'011'),(b'000110100'),
-(b'01101101010'),(b'01111001001011'),(NULL);
-SET GLOBAL innodb_debug_sync = "ib_clust_v_col_before_row_allocated "
-                               "SIGNAL before_row_allocated "
-                               "WAIT_FOR flush_unlock";
-SET GLOBAL innodb_debug_sync = "ib_open_after_dict_open "
-                               "SIGNAL purge_open "
-                               "WAIT_FOR select_open";
-set @saved_dbug= @@global.debug_dbug;
-set global debug_dbug= "+d,ib_purge_virtual_index_callback";
-connect purge_waiter,localhost,root;
-SET debug_sync= "now WAIT_FOR before_row_allocated";
-connection default;
-REPLACE INTO t1 (pk, b) SELECT pk, b FROM t1;
-connection purge_waiter;
-connection default;
-disconnect purge_waiter;
-FLUSH TABLES;
-SET GLOBAL innodb_debug_sync = reset;
-SET debug_sync= "now SIGNAL flush_unlock WAIT_FOR purge_open";
-SET GLOBAL innodb_debug_sync = reset;
-SET debug_sync= "ib_open_after_dict_open SIGNAL select_open";
-SELECT * FROM t1;
-pk	b	v
-1	NULL	NULL
-2	 	 
-3	 4	 4
-4	j	j
-5	K	K
-6	NULL	NULL
-DROP TABLE t1;
-SET debug_sync= reset;
-set global debug_dbug= @saved_dbug;

mysql-test/suite/gcol/t/innodb_virtual_debug_purge.test
@@ -310,67 +310,3 @@ drop table t1;
 --source include/wait_until_count_sessions.inc
 set debug_sync=reset;
 SET GLOBAL innodb_purge_rseg_truncate_frequency = @saved_frequency;
-
---echo #
---echo # MDEV-18546 ASAN heap-use-after-free
---echo # in innobase_get_computed_value / row_purge
---echo #
-
-CREATE TABLE t1 (
-  pk INT AUTO_INCREMENT,
-  b BIT(15),
-  v BIT(15) AS (b) VIRTUAL,
-  PRIMARY KEY(pk),
-  UNIQUE(v)
-) ENGINE=InnoDB;
-INSERT IGNORE INTO t1 (b) VALUES
-  (NULL),(b'011'),(b'000110100'),
-  (b'01101101010'),(b'01111001001011'),(NULL);
-
-SET GLOBAL innodb_debug_sync = "ib_clust_v_col_before_row_allocated "
-                               "SIGNAL before_row_allocated "
-                               "WAIT_FOR flush_unlock";
-SET GLOBAL innodb_debug_sync = "ib_open_after_dict_open "
-                               "SIGNAL purge_open "
-                               "WAIT_FOR select_open";
-
-# In 10.2 trx_undo_roll_ptr_is_insert(t_roll_ptr) condition never pass in purge,
-# so this condition is forced to pass in row_vers_old_has_index_entry
-set @saved_dbug= @@global.debug_dbug;
-set global debug_dbug= "+d,ib_purge_virtual_index_callback";
-
-# The purge starts from REPLACE command. To avoid possible race, separate
-# connection is used.
---connect(purge_waiter,localhost,root)
---send
-SET debug_sync= "now WAIT_FOR before_row_allocated";
-
---connection default
-REPLACE INTO t1 (pk, b) SELECT pk, b FROM t1;
-
---connection purge_waiter
-# Now we will definitely catch ib_clust_v_col_before_row_allocated
---reap
---connection default
---disconnect purge_waiter
-
-# purge hangs on the sync point. table is purged, ref_count is set to 0
-FLUSH TABLES;
-
-# Avoid hang on repeating purge.
-# Reset Will be applied after first record is purged
-SET GLOBAL innodb_debug_sync = reset;
-
-SET debug_sync= "now SIGNAL flush_unlock WAIT_FOR purge_open";
-
-# Avoid hang on repeating purge
-SET GLOBAL innodb_debug_sync = reset;
-
-# select unblocks purge thread
-SET debug_sync= "ib_open_after_dict_open SIGNAL select_open";
-SELECT * FROM t1;
-
-# Cleanup
-DROP TABLE t1;
-SET debug_sync= reset;
-set global debug_dbug= @saved_dbug;

mysql-test/suite/innodb/r/purge_thread_shutdown.result
@@ -6,11 +6,6 @@ select user,state from information_schema.processlist order by 2;
 user	state
 root	
 root	Filling schema table
-system user	InnoDB purge coordinator
-system user	InnoDB purge worker
-system user	InnoDB purge worker
-system user	InnoDB purge worker
-system user	InnoDB shutdown handler
 set global debug_dbug='+d,only_kill_system_threads';
 set global innodb_fast_shutdown=0;
 shutdown;
@@ -19,11 +14,6 @@ disconnect con1;
 select user,state from information_schema.processlist order by 2;
 user	state
 root	Filling schema table
-system user	InnoDB purge coordinator
-system user	InnoDB purge worker
-system user	InnoDB purge worker
-system user	InnoDB purge worker
-system user	InnoDB slow shutdown wait
 set global innodb_fast_shutdown=1;
 select user,state from information_schema.processlist order by 2;
 user	state

mysql-test/suite/perfschema/r/threads_innodb.result
@@ -5,14 +5,5 @@ FROM performance_schema.threads
 WHERE name LIKE 'thread/innodb/%'
 GROUP BY name;
 name	type	processlist_user	processlist_host	processlist_db	processlist_command	processlist_time	processlist_state	processlist_info	parent_thread_id	role	instrumented
-thread/innodb/io_ibuf_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
-thread/innodb/io_log_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
-thread/innodb/io_read_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
-thread/innodb/io_write_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
 thread/innodb/page_cleaner_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
-thread/innodb/srv_error_monitor_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
-thread/innodb/srv_lock_timeout_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
-thread/innodb/srv_master_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
-thread/innodb/srv_monitor_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
-thread/innodb/srv_purge_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES
-thread/innodb/thd_destructor_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	1	NULL	YES
+thread/innodb/thread_pool_thread	BACKGROUND	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	YES

mysql-test/suite/sys_vars/r/sysvars_innodb.result
@@ -525,18 +525,6 @@ NUMERIC_BLOCK_SIZE	NULL
 ENUM_VALUE_LIST	OFF,ON
 READ_ONLY	NO
 COMMAND_LINE_ARGUMENT	REQUIRED
-VARIABLE_NAME	INNODB_DEBUG_SYNC
-SESSION_VALUE	NULL
-DEFAULT_VALUE	
-VARIABLE_SCOPE	GLOBAL
-VARIABLE_TYPE	VARCHAR
-VARIABLE_COMMENT	debug_sync for innodb purge threads. Use it to set up sync points for all purge threads at once. The commands will be applied sequentially at the beginning of purging the next undo record.
-NUMERIC_MIN_VALUE	NULL
-NUMERIC_MAX_VALUE	NULL
-NUMERIC_BLOCK_SIZE	NULL
-ENUM_VALUE_LIST	NULL
-READ_ONLY	NO
-COMMAND_LINE_ARGUMENT	NONE
 VARIABLE_NAME	INNODB_DEFAULT_ENCRYPTION_KEY_ID
 SESSION_VALUE	1
 DEFAULT_VALUE	1

storage/innobase/CMakeLists.txt
@@ -19,6 +19,7 @@
 
 
 INCLUDE(innodb.cmake)
+INCLUDE_DIRECTORIES(${PROJECT_SOURCE_DIR}/tpool)
 
 SET(INNOBASE_SOURCES
 	btr/btr0btr.cc
@@ -154,7 +155,8 @@ MYSQL_ADD_PLUGIN(innobase ${INNOBASE_SOURCES} STORAGE_ENGINE
 	${CRC32_LIBRARY}
 	${NUMA_LIBRARY}
 	${LIBSYSTEMD}
-	${LINKER_SCRIPT})
+	${LINKER_SCRIPT}
+	tpool)
 
 IF(NOT TARGET innobase)
   RETURN()

commit cbbca7edf658f59f9c6a1bcfb148f86ce0f0b9f3

MDEV-25483: Shutdown crash during innodb.innodb_buffer_pool_resize_temporary
buf_resize_callback(): Correct an invalid assertion, and enable
the assertion in debug builds only.

Between buf_resize_start() and buf_resize_shutdown(),
srv_shutdown_state must be less than SRV_SHUTDOWN_CLEANUP.

The incorrect assertion had been introduced in
commit 5e62b6a5e06eb02cbde1e34e95e26f42d87fce02 (MDEV-16264).
As a result, the server could crash if shutdown was initiated
concurrently with initiating a change of innodb_buffer_pool_size.

storage/innobase/buf/buf0buf.cc
@@ -2322,7 +2322,7 @@ calc_buf_pool_size:
 static void buf_resize_callback(void *)
 {
   DBUG_ENTER("buf_resize_callback");
-  ut_a(srv_shutdown_state == SRV_SHUTDOWN_NONE);
+  ut_ad(srv_shutdown_state < SRV_SHUTDOWN_CLEANUP);
   mysql_mutex_lock(&buf_pool.mutex);
   const auto size= srv_buf_pool_size;
   const bool work= srv_buf_pool_old_size != size;