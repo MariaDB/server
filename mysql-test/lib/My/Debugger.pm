af781f1ac435f4fc9e395417af2ad15ea524a502
Sergei Golubchik <serg@mariadb.org>
2021-05-03 18:00:09 +0200

    fix mtr --client-gdb to work
    mysqltest has no --gdb option

--- mysql-test/lib/My/Debugger.pm
+++ mysql-test/lib/My/Debugger.pm
@@ -147,7 +147,7 @@ sub do_args($$$$$) {
   my %vars = (
     vardir => $::opt_vardir,
     exe => $$exe,
-    args => join(' ', map { quote_from_mtr $_ } @$$args, '--gdb'),
+    args => join(' ', map { quote_from_mtr $_ } @$$args, '--loose-gdb'),
     input => $input,
     script => "$::opt_vardir/tmp/${k}init.$type",
     log => "$::opt_vardir/log/$type.$k",
