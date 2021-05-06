bafec28e4325e15d1b29b888cfd58ad529c4ce09
Sergei Golubchik <serg@mariadb.org>
2021-05-06 19:47:11 +0200

    rpm packaging: account for fedora > 31
    RocksDB failed to package on fedora 32 and 33 with
    
    *** ERROR: ambiguous python shebang in /usr/bin/myrocks_hotbackup: #!/usr/bin/env python. Change it to python3 (or python2) explicitly.

--- cmake/cpack_rpm.cmake
+++ cmake/cpack_rpm.cmake
@@ -247,7 +247,7 @@ ELSEIF(RPM MATCHES "sles")
     "mariadb-server = %{version}-%{release}"
   )
 ENDIF()
-IF(RPM MATCHES "fedora31" OR RPM MATCHES "(rhel|centos)8")
+IF(RPM MATCHES "fedora" OR RPM MATCHES "(rhel|centos)8")
   SET(PYTHON_SHEBANG "/usr/bin/python3" CACHE STRING "python shebang")
 ENDIF()
 
