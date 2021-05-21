46c3e7e3537c31a94289033bfeccf3faf8d4069e
Brandon Nesterenko <brandon.nesterenko@mariadb.com>
2021-05-21 14:53:43 -0600

    MDEV-20215: binlog.show_concurrent_rotate failed in buildbot with wrong result
    Problem:
    =======
    There are two issues that are addressed in this patch:
     1) SHOW BINARY LOGS uses caching to store the binary logs that exist
    in the log directory; however, if new events are written to the logs,
    the caching strategy is unaware. This is okay for users, as it is
    okay for SHOW to return slightly old data. The test, however, can
    result in inconsistent data. It runs two connections concurrently,
    where one shows the logs, and the other adds a new file. The output
    of SHOW BINARY LOGS then depends on when the cache is built, with
    respect to the time that the second connection rotates the logs.
     2) There is a race condition between RESET MASTER and SHOW BINARY
    LOGS. More specifically, where they both need the binary log lock to
    begin, SHOW BINARY LOGS only needs the lock to build its cache. If
    RESET MASTER is issued after SHOW BINARY LOGS has built its cache and
    before it has returned the results, the presented data may be
    incorrect.
    
    Solution:
    ========
     1) As it is okay for users to see stale data, to make the test
    consistent, use DEBUG_SYNC to force the race condition (problem 2) to
    make SHOW BINARY LOGS build a cache before RESET MASTER is called.
    Then, use additional logic from the next part of the solution to
    rebuild the cache.
     2) Use an Atomic_counter to keep track of the number of times RESET
    MASTER has been called. If the value of the counter changes after
    building the cache, the cache should be rebuilt and the analysis
    should be restarted.
    
    Reviewed By:
    ============
    Andrei Elkin: <andrei.elkin@mariadb.com>

--- sql/log.cc
+++ sql/log.cc
@@ -3256,7 +3256,7 @@ MYSQL_BIN_LOG::MYSQL_BIN_LOG(uint *sync_period)
    checksum_alg_reset(BINLOG_CHECKSUM_ALG_UNDEF),
    relay_log_checksum_alg(BINLOG_CHECKSUM_ALG_UNDEF),
    description_event_for_exec(0), description_event_for_queue(0),
-   current_binlog_id(0)
+   current_binlog_id(0), reset_master_count(0)
 {
   /*
     We don't want to initialize locks here as such initialization depends on
@@ -4350,6 +4350,7 @@ err:
     }
     mysql_cond_broadcast(&COND_xid_list);
     reset_master_pending--;
+    reset_master_count++;
     mysql_mutex_unlock(&LOCK_xid_list);
   }
 
