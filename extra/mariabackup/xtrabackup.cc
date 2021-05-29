MariaDB/server

commit cfd3d70ccbbfcf3fdec034be446317741dfae824

MDEV-22871: Remove pointer indirection for InnoDB hash_table_t
hash_get_n_cells(): Remove. Access n_cells directly.

hash_get_nth_cell(): Remove. Access array directly.

hash_table_clear(): Replaced with hash_table_t::clear().

hash_table_create(), hash_table_free(): Remove.

hash0hash.cc: Remove.

extra/mariabackup/xtrabackup.cc
@@ -157,16 +157,16 @@ typedef std::list<regex_t> regex_list_t;
 static regex_list_t regex_include_list;
 static regex_list_t regex_exclude_list;
 
-static hash_table_t* tables_include_hash = NULL;
-static hash_table_t* tables_exclude_hash = NULL;
+static hash_table_t tables_include_hash;
+static hash_table_t tables_exclude_hash;
 
 char *xtrabackup_databases = NULL;
 char *xtrabackup_databases_file = NULL;
 char *xtrabackup_databases_exclude = NULL;
-static hash_table_t* databases_include_hash = NULL;
-static hash_table_t* databases_exclude_hash = NULL;
+static hash_table_t databases_include_hash;
+static hash_table_t databases_exclude_hash;
 
-static hash_table_t* inc_dir_tables_hash;
+static hash_table_t inc_dir_tables_hash;
 
 struct xb_filter_entry_struct{
 	char*		name;
@@ -2256,7 +2256,7 @@ check_if_table_matches_filters(const char *name,
 	const regex_list_t& regex_list,
 	hash_table_t* tables_hash)
 {
-	if (regex_list.empty() && !tables_hash) {
+	if (regex_list.empty() && !tables_hash->array) {
 		return(FALSE);
 	}
 
@@ -2264,11 +2264,8 @@ check_if_table_matches_filters(const char *name,
 		return(TRUE);
 	}
 
-	if (tables_hash && find_filter_in_hashtable(name, tables_hash, NULL)) {
-		return(TRUE);
-	}
-
-	return FALSE;
+	return tables_hash->array &&
+		find_filter_in_hashtable(name, tables_hash, NULL);
 }
 
 enum skip_database_check_result {
@@ -2294,8 +2291,8 @@ check_if_skip_database(
 	/* There are some filters for databases, check them */
 	xb_filter_entry_t*	database = NULL;
 
-	if (databases_exclude_hash &&
-		find_filter_in_hashtable(name, databases_exclude_hash,
+	if (databases_exclude_hash.array &&
+		find_filter_in_hashtable(name, &databases_exclude_hash,
 					 &database) &&
 		!database->has_tables) {
 		/* Database is found and there are no tables specified,
@@ -2303,8 +2300,8 @@ check_if_skip_database(
 		return DATABASE_SKIP;
 	}
 
-	if (databases_include_hash) {
-		if (!find_filter_in_hashtable(name, databases_include_hash,
+	if (databases_include_hash.array) {
+		if (!find_filter_in_hashtable(name, &databases_include_hash,
 					      &database)) {
 		/* Database isn't found, skip the database */
 			return DATABASE_SKIP;
@@ -2328,8 +2325,7 @@ check_if_skip_database_by_path(
 	const char* path /*!< in: path to the db directory. */
 )
 {
-	if (databases_include_hash == NULL &&
-		databases_exclude_hash == NULL) {
+	if (!databases_include_hash.array && !databases_exclude_hash.array) {
 		return(FALSE);
 	}
 
@@ -2373,10 +2369,10 @@ check_if_skip_table(
 
 	if (regex_exclude_list.empty() &&
 		regex_include_list.empty() &&
-		tables_include_hash == NULL &&
-		tables_exclude_hash == NULL &&
-		databases_include_hash == NULL &&
-		databases_exclude_hash == NULL) {
+		!tables_include_hash.array &&
+		!tables_exclude_hash.array &&
+		!databases_include_hash.array &&
+		!databases_exclude_hash.array) {
 		return(FALSE);
 	}
 
@@ -2408,22 +2404,22 @@ check_if_skip_table(
 	without truncating the #P#... suffix so we can backup individual
 	partitions with regexps like '^test[.]t#P#p5' */
 	if (check_if_table_matches_filters(buf, regex_exclude_list,
-					   tables_exclude_hash)) {
+					   &tables_exclude_hash)) {
 		return(TRUE);
 	}
 	if (check_if_table_matches_filters(buf, regex_include_list,
-					   tables_include_hash)) {
+					   &tables_include_hash)) {
 		return(FALSE);
 	}
 	if ((eptr = strstr(buf, "#P#")) != NULL) {
 		*eptr = 0;
 
 		if (check_if_table_matches_filters(buf, regex_exclude_list,
-						   tables_exclude_hash)) {
+						   &tables_exclude_hash)) {
 			return (TRUE);
 		}
 		if (check_if_table_matches_filters(buf, regex_include_list,
-						   tables_include_hash)) {
+						   &tables_include_hash)) {
 			return(FALSE);
 		}
 	}
@@ -2436,7 +2432,7 @@ check_if_skip_table(
 
 	if (skip_database == DATABASE_SKIP_SOME_TABLES ||
 		!regex_include_list.empty() ||
-		tables_include_hash) {
+		tables_include_hash.array) {
 
 		/* Include lists are present, but qualified name
 		   failed to match any.*/
@@ -3461,17 +3457,17 @@ xb_filter_entry_t*
 xb_add_filter(
 /*========================*/
 	const char*	name,	/*!< in: name of table/database */
-	hash_table_t**	hash)	/*!< in/out: hash to insert into */
+	hash_table_t*	hash)	/*!< in/out: hash to insert into */
 {
 	xb_filter_entry_t*	entry;
 
 	entry = xb_new_filter_entry(name);
 
-	if (UNIV_UNLIKELY(*hash == NULL)) {
-		*hash = hash_create(1000);
+	if (UNIV_UNLIKELY(!hash->array)) {
+		hash->create(1000);
 	}
 	HASH_INSERT(xb_filter_entry_t,
-		name_hash, *hash,
+		name_hash, hash,
 		ut_fold_string(entry->name),
 		entry);
 
@@ -3509,8 +3505,8 @@ void
 xb_register_filter_entry(
 /*=====================*/
 	const char*	name,	/*!< in: name */
-	hash_table_t** databases_hash,
-	hash_table_t** tables_hash
+	hash_table_t* databases_hash,
+	hash_table_t* tables_hash
 	)
 {
 	const char*		p;
@@ -3527,8 +3523,8 @@ xb_register_filter_entry(
 		strncpy(dbname, name, p - name);
 		dbname[p - name] = 0;
 
-		if (*databases_hash) {
-			HASH_SEARCH(name_hash, (*databases_hash),
+		if (databases_hash) {
+			HASH_SEARCH(name_hash, databases_hash,
 					ut_fold_string(dbname),
 					xb_filter_entry_t*,
 					db_entry, (void) 0,
@@ -3727,7 +3723,7 @@ xb_filter_hash_free(hash_table_t* hash)
 	ulint	i;
 
 	/* free the hash elements */
-	for (i = 0; i < hash_get_n_cells(hash); i++) {
+	for (i = 0; i < hash->n_cells; i++) {
 		xb_filter_entry_t*	table;
 
 		table = static_cast<xb_filter_entry_t *>
@@ -3745,8 +3741,7 @@ xb_filter_hash_free(hash_table_t* hash)
 		}
 	}
 
-	/* free hash */
-	hash_table_free(hash);
+	hash->free();
 }
 
 static void xb_regex_list_free(regex_list_t* list)
@@ -3766,20 +3761,20 @@ xb_filters_free()
 	xb_regex_list_free(&regex_include_list);
 	xb_regex_list_free(&regex_exclude_list);
 
-	if (tables_include_hash) {
-		xb_filter_hash_free(tables_include_hash);
+	if (tables_include_hash.array) {
+		xb_filter_hash_free(&tables_include_hash);
 	}
 
-	if (tables_exclude_hash) {
-		xb_filter_hash_free(tables_exclude_hash);
+	if (tables_exclude_hash.array) {
+		xb_filter_hash_free(&tables_exclude_hash);
 	}
 
-	if (databases_include_hash) {
-		xb_filter_hash_free(databases_include_hash);
+	if (databases_include_hash.array) {
+		xb_filter_hash_free(&databases_include_hash);
 	}
 
-	if (databases_exclude_hash) {
-		xb_filter_hash_free(databases_exclude_hash);
+	if (databases_exclude_hash.array) {
+		xb_filter_hash_free(&databases_exclude_hash);
 	}
 }
 
@@ -4644,7 +4639,7 @@ exit:
 
 	table->name = ((char*)table) + sizeof(xb_filter_entry_t);
 	strcpy(table->name, dest_space_name);
-	HASH_INSERT(xb_filter_entry_t, name_hash, inc_dir_tables_hash,
+	HASH_INSERT(xb_filter_entry_t, name_hash, &inc_dir_tables_hash,
 			ut_fold_string(table->name), table);
 
 	mutex_enter(&fil_system.mutex);
@@ -5034,7 +5029,7 @@ rm_if_not_found(
 	/* Truncate ".ibd" */
 	name[strlen(name) - 4] = '\0';
 
-	HASH_SEARCH(name_hash, inc_dir_tables_hash, ut_fold_string(name),
+	HASH_SEARCH(name_hash, &inc_dir_tables_hash, ut_fold_string(name),
 		    xb_filter_entry_t*,
 		    table, (void) 0,
 		    !strcmp(table->name, name));
@@ -5410,7 +5405,7 @@ static bool xtrabackup_prepare_func(char** argv)
 			goto error_cleanup;
 		}
 
-		inc_dir_tables_hash = hash_create(1000);
+		inc_dir_tables_hash.create(1000);
 
 		ok = xtrabackup_apply_deltas();
 
@@ -5423,7 +5418,7 @@ static bool xtrabackup_prepare_func(char** argv)
 			xb_process_datadir("./", ".ibd", rm_if_not_found);
 		}
 
-		xb_filter_hash_free(inc_dir_tables_hash);
+		xb_filter_hash_free(&inc_dir_tables_hash);
 
 		fil_system.close();
 #ifdef WITH_INNODB_DISALLOW_WRITES

storage/innobase/CMakeLists.txt
@@ -57,7 +57,6 @@ SET(INNOBASE_SOURCES
 	fsp/fsp0sysspace.cc
 	fut/fut0lst.cc
 	ha/ha0storage.cc
-	ha/hash0hash.cc
 	fts/fts0fts.cc
 	fts/fts0ast.cc
 	fts/fts0blex.cc
@@ -163,7 +162,6 @@ SET(INNOBASE_SOURCES
 	include/ha0storage.ic
 	include/handler0alter.h
 	include/hash0hash.h
-	include/hash0hash.ic
 	include/ib0mutex.h
 	include/ibuf0ibuf.h
 	include/ibuf0ibuf.ic

storage/innobase/btr/btr0btr.cc
@@ -3349,7 +3349,7 @@ btr_lift_page_up(
 		if (dict_index_is_spatial(index)) {
 			lock_mutex_enter();
 			lock_prdt_page_free_from_discard(
-				block, lock_sys.prdt_page_hash);
+				block, &lock_sys.prdt_page_hash);
 			lock_mutex_exit();
 		}
 		lock_update_copy_and_discard(father_block, block);
@@ -3604,7 +3604,7 @@ retry:
 			/* No GAP lock needs to be worrying about */
 			lock_mutex_enter();
 			lock_prdt_page_free_from_discard(
-				block, lock_sys.prdt_page_hash);
+				block, &lock_sys.prdt_page_hash);
 			lock_rec_free_all_from_discard_page(block);
 			lock_mutex_exit();
 		} else {
@@ -3757,7 +3757,7 @@ retry:
 			}
 			lock_mutex_enter();
 			lock_prdt_page_free_from_discard(
-				block, lock_sys.prdt_page_hash);
+				block, &lock_sys.prdt_page_hash);
 			lock_rec_free_all_from_discard_page(block);
 			lock_mutex_exit();
 		} else {

storage/innobase/btr/btr0sea.cc
@@ -492,8 +492,7 @@ static bool ha_insert_for_fold(hash_table_t *table, mem_heap_t* heap,
 #endif /* UNIV_AHI_DEBUG || UNIV_DEBUG */
   ut_ad(btr_search_enabled);
 
-  ulint hash = hash_calc_hash(fold, table);
-  hash_cell_t *cell= hash_get_nth_cell(table, hash);
+  hash_cell_t *cell= &table->array[table->calc_hash(fold)];
 
   for (ha_node_t *prev= static_cast<ha_node_t*>(cell->node); prev;
        prev= prev->next)
@@ -564,7 +563,7 @@ static void ha_delete_hash_node(hash_table_t *table, mem_heap_t *heap,
   {
     /* Compact the heap of nodes by moving the top in the place of del_node. */
     *del_node= *top;
-    hash_cell_t *cell= hash_get_nth_cell(table, table->calc_hash(top->fold));
+    hash_cell_t *cell= &table->array[table->calc_hash(top->fold)];
 
     /* Look for the pointer to the top node, to update it */
     if (cell->node == top)
@@ -2163,7 +2162,7 @@ btr_search_hash_table_validate(ulint hash_table_id)
 
 	auto &part = btr_search_sys.parts[hash_table_id];
 
-	cell_count = hash_get_n_cells(&part.table);
+	cell_count = part.table.n_cells;
 
 	for (i = 0; i < cell_count; i++) {
 		/* We release search latches every once in a while to
@@ -2184,7 +2183,7 @@ btr_search_hash_table_validate(ulint hash_table_id)
 
 			mutex_enter(&buf_pool.mutex);
 
-			ulint curr_cell_count = hash_get_n_cells(&part.table);
+			ulint curr_cell_count = part.table.n_cells;
 
 			if (cell_count != curr_cell_count) {
 
@@ -2196,7 +2195,7 @@ btr_search_hash_table_validate(ulint hash_table_id)
 			}
 		}
 
-		node = (ha_node_t*) hash_get_nth_cell(&part.table, i)->node;
+		node = static_cast<ha_node_t*>(part.table.array[i].node);
 
 		for (; node != NULL; node = node->next) {
 			const buf_block_t*	block
@@ -2292,7 +2291,7 @@ state_ok:
 
 			mutex_enter(&buf_pool.mutex);
 
-			ulint curr_cell_count = hash_get_n_cells(&part.table);
+			ulint curr_cell_count = part.table.n_cells;
 
 			if (cell_count != curr_cell_count) {
 

storage/innobase/buf/buf0buddy.cc
@@ -359,7 +359,7 @@ buf_buddy_block_free(void* buf)
 	ut_ad(mutex_own(&buf_pool.mutex));
 	ut_a(!ut_align_offset(buf, srv_page_size));
 
-	HASH_SEARCH(hash, buf_pool.zip_hash, fold, buf_page_t*, bpage,
+	HASH_SEARCH(hash, &buf_pool.zip_hash, fold, buf_page_t*, bpage,
 		    ut_ad(bpage->state() == BUF_BLOCK_MEMORY
 			  && bpage->in_zip_hash),
 		    ((buf_block_t*) bpage)->frame == buf);
@@ -367,7 +367,7 @@ buf_buddy_block_free(void* buf)
 	ut_a(bpage->state() == BUF_BLOCK_MEMORY);
 	ut_ad(bpage->in_zip_hash);
 	ut_d(bpage->in_zip_hash = false);
-	HASH_DELETE(buf_page_t, hash, buf_pool.zip_hash, fold, bpage);
+	HASH_DELETE(buf_page_t, hash, &buf_pool.zip_hash, fold, bpage);
 
 	ut_d(memset(buf, 0, srv_page_size));
 	UNIV_MEM_INVALID(buf, srv_page_size);
@@ -395,7 +395,7 @@ buf_buddy_block_register(
 
 	ut_ad(!block->page.in_zip_hash);
 	ut_d(block->page.in_zip_hash = true);
-	HASH_INSERT(buf_page_t, hash, buf_pool.zip_hash, fold, &block->page);
+	HASH_INSERT(buf_page_t, hash, &buf_pool.zip_hash, fold, &block->page);
 
 	ut_d(buf_pool.buddy_n_frames++);
 }

storage/innobase/buf/buf0buf.cc
@@ -1523,11 +1523,11 @@ bool buf_pool_t::create()
   ut_a(srv_n_page_hash_locks != 0);
   ut_a(srv_n_page_hash_locks <= MAX_PAGE_HASH_LOCKS);
 
-  page_hash= hash_create(2 * curr_size);
+  page_hash.create(2 * curr_size);
   for (auto i= srv_n_page_hash_locks; i--; )
     rw_lock_create(hash_table_locks_key, &page_hash_latches[i],
                    SYNC_BUF_PAGE_HASH);
-  zip_hash= hash_create(2 * curr_size);
+  zip_hash.create(2 * curr_size);
   last_printout_time= time(NULL);
 
   mutex_create(LATCH_ID_FLUSH_LIST, &flush_list_mutex);
@@ -1606,8 +1606,8 @@ void buf_pool_t::close()
   chunks= nullptr;
   for (auto i= srv_n_page_hash_locks; i--; )
     rw_lock_free(&page_hash_latches[i]);
-  hash_table_free(page_hash);
-  hash_table_free(zip_hash);
+  page_hash.free();
+  zip_hash.free();
 
   io_buf.close();
   UT_DELETE(chunk_t::map_reg);
@@ -1683,7 +1683,7 @@ inline bool buf_pool_t::realloc(buf_block_t *block)
 		const ulint fold = id.fold();
 		ut_ad(&block->page == page_hash_get_low(id, fold));
 		ut_d(block->page.in_page_hash = false);
-		HASH_REPLACE(buf_page_t, hash, page_hash, fold,
+		HASH_REPLACE(buf_page_t, hash, &page_hash, fold,
 			     &block->page, &new_block->page);
 
 		buf_block_modify_clock_inc(block);
@@ -1924,44 +1924,46 @@ inline bool buf_pool_t::withdraw_blocks()
 /** resize page_hash and zip_hash */
 static void buf_pool_resize_hash()
 {
-  hash_table_t *new_hash_table= hash_create(2 * buf_pool.curr_size);
+  hash_table_t new_hash;
+  new_hash.create(2 * buf_pool.curr_size);
 
-  for (ulint i= 0; i < hash_get_n_cells(buf_pool.page_hash); i++)
+  for (ulint i= 0; i < buf_pool.page_hash.n_cells; i++)
   {
     while (buf_page_t *bpage= static_cast<buf_page_t*>
-           (HASH_GET_FIRST(buf_pool.page_hash, i)))
+           (HASH_GET_FIRST(&buf_pool.page_hash, i)))
     {
       buf_page_t *prev_bpage= bpage;
       ut_ad(bpage->in_page_hash);
       bpage= static_cast<buf_page_t*>(HASH_GET_NEXT(hash, prev_bpage));
       const ulint fold= prev_bpage->id().fold();
-      HASH_DELETE(buf_page_t, hash, buf_pool.page_hash, fold, prev_bpage);
-      HASH_INSERT(buf_page_t, hash, new_hash_table, fold, prev_bpage);
+      HASH_DELETE(buf_page_t, hash, &buf_pool.page_hash, fold, prev_bpage);
+      HASH_INSERT(buf_page_t, hash, &new_hash, fold, prev_bpage);
     }
   }
 
-  std::swap(buf_pool.page_hash->array, new_hash_table->array);
-  buf_pool.page_hash->n_cells= new_hash_table->n_cells;
-  hash_table_free(new_hash_table);
+  std::swap(buf_pool.page_hash.array, new_hash.array);
+  buf_pool.page_hash.n_cells= new_hash.n_cells;
+  new_hash.free();
 
   /* recreate zip_hash */
-  new_hash_table= hash_create(2 * buf_pool.curr_size);
+  new_hash.create(2 * buf_pool.curr_size);
 
-  for (ulint i= 0; i < hash_get_n_cells(buf_pool.zip_hash); i++)
+  for (ulint i= 0; i < buf_pool.zip_hash.n_cells; i++)
   {
     while (buf_page_t *bpage= static_cast<buf_page_t*>
-           (HASH_GET_FIRST(buf_pool.zip_hash, i)))
+           (HASH_GET_FIRST(&buf_pool.zip_hash, i)))
     {
       buf_page_t *prev_bpage= bpage;
       bpage= static_cast<buf_page_t*>(HASH_GET_NEXT(hash, prev_bpage));
       const ulint fold= BUF_POOL_ZIP_FOLD_BPAGE(prev_bpage);
-      HASH_DELETE(buf_page_t, hash, buf_pool.zip_hash, fold, prev_bpage);
-      HASH_INSERT(buf_page_t, hash, new_hash_table, fold, prev_bpage);
+      HASH_DELETE(buf_page_t, hash, &buf_pool.zip_hash, fold, prev_bpage);
+      HASH_INSERT(buf_page_t, hash, &new_hash, fold, prev_bpage);
     }
   }
 
-  hash_table_free(buf_pool.zip_hash);
-  buf_pool.zip_hash= new_hash_table;
+  std::swap(buf_pool.zip_hash.array, new_hash.array);
+  buf_pool.zip_hash.n_cells= new_hash.n_cells;
+  new_hash.free();
 }
 
 
@@ -2430,7 +2432,7 @@ static void buf_relocate(buf_page_t *bpage, buf_page_t *dpage)
   ut_ad(bpage->in_page_hash);
   ut_ad(dpage->in_page_hash);
   ut_d(bpage->in_page_hash= false);
-  HASH_REPLACE(buf_page_t, hash, buf_pool.page_hash, fold, bpage, dpage);
+  HASH_REPLACE(buf_page_t, hash, &buf_pool.page_hash, fold, bpage, dpage);
 }
 
 /** Register a watch for a page identifier. The caller must hold an
@@ -2502,7 +2504,7 @@ retry:
     w->buf_fix_count_= 1;
     ut_ad(!w->in_page_hash);
     ut_d(w->in_page_hash= true); /* Not holding buf_pool.mutex here! */
-    HASH_INSERT(buf_page_t, hash, page_hash, fold, w);
+    HASH_INSERT(buf_page_t, hash, &page_hash, fold, w);
     return nullptr;
   }
 
@@ -3772,8 +3774,7 @@ buf_page_create(fil_space_t *space, uint32_t offset,
   rw_lock_x_lock(hash_lock);
   block->page.set_state(BUF_BLOCK_FILE_PAGE);
   ut_d(block->page.in_page_hash= true);
-  HASH_INSERT(buf_page_t, hash, buf_pool.page_hash, page_id.fold(),
-              &block->page);
+  HASH_INSERT(buf_page_t, hash, &buf_pool.page_hash, fold, &block->page);
 
   if (UNIV_UNLIKELY(zip_size))
   {

storage/innobase/buf/buf0lru.cc
@@ -1239,7 +1239,7 @@ func_exit:
 		ut_ad(b->in_LRU_list);
 		ut_ad(b->in_page_hash);
 
-		HASH_INSERT(buf_page_t, hash, buf_pool.page_hash, fold, b);
+		HASH_INSERT(buf_page_t, hash, &buf_pool.page_hash, fold, b);
 
 		/* Insert b where bpage was in the LRU list. */
 		if (prev_b) {
@@ -1488,7 +1488,7 @@ static bool buf_LRU_block_remove_hashed(buf_page_t *bpage, const page_id_t id,
 	}
 
 	ut_ad(!bpage->in_zip_hash);
-	HASH_DELETE(buf_page_t, hash, buf_pool.page_hash, id.fold(), bpage);
+	HASH_DELETE(buf_page_t, hash, &buf_pool.page_hash, id.fold(), bpage);
 
 	switch (bpage->state()) {
 	case BUF_BLOCK_ZIP_PAGE:

storage/innobase/buf/buf0rea.cc
@@ -59,7 +59,7 @@ inline void buf_pool_t::watch_remove(buf_page_t *watch)
   {
     ut_ad(watch->in_page_hash);
     ut_d(watch->in_page_hash= false);
-    HASH_DELETE(buf_page_t, hash, page_hash, watch->id().fold(), watch);
+    HASH_DELETE(buf_page_t, hash, &page_hash, watch->id().fold(), watch);
     watch->set_buf_fix_count(0);
   }
   ut_ad(!watch->in_page_hash);
@@ -159,7 +159,7 @@ static buf_page_t* buf_page_init_for_read(ulint mode, const page_id_t page_id,
     block->page.set_state(BUF_BLOCK_FILE_PAGE);
     ut_ad(!block->page.in_page_hash);
     ut_d(block->page.in_page_hash= true);
-    HASH_INSERT(buf_page_t, hash, buf_pool.page_hash, fold, bpage);
+    HASH_INSERT(buf_page_t, hash, &buf_pool.page_hash, fold, bpage);
     rw_lock_x_unlock(hash_lock);
 
     /* The block must be put to the LRU list, to the old blocks */
@@ -232,7 +232,7 @@ static buf_page_t* buf_page_init_for_read(ulint mode, const page_id_t page_id,
 
     ut_ad(!bpage->in_page_hash);
     ut_d(bpage->in_page_hash= true);
-    HASH_INSERT(buf_page_t, hash, buf_pool.page_hash, fold, bpage);
+    HASH_INSERT(buf_page_t, hash, &buf_pool.page_hash, fold, bpage);
     bpage->set_io_fix(BUF_IO_READ);
     rw_lock_x_unlock(hash_lock);
 

commit 5bd517259f2bb3e48cd3dab5fa64ce2cf9b623df

MDEV-25815 mariabackup crash or debug assert with --backup --databases-exclude
Fix regression (debug assertion or division by 0)
caused by cfd3d70ccbbfcf3fdec034be446317741dfae824

extra/mariabackup/xtrabackup.cc
@@ -4048,7 +4048,7 @@ xb_register_filter_entry(
 		strncpy(dbname, name, p - name);
 		dbname[p - name] = 0;
 
-		if (databases_hash) {
+		if (databases_hash && databases_hash->array) {
 			HASH_SEARCH(name_hash, databases_hash,
 					ut_fold_string(dbname),
 					xb_filter_entry_t*,

mysql-test/suite/mariabackup/xbstream.test
@@ -8,7 +8,7 @@ mkdir $targetdir;
 let $streamfile=$MYSQLTEST_VARDIR/tmp/backup.xb;
 
 echo # xtrabackup backup to stream;
-exec $XTRABACKUP --defaults-file=$MYSQLTEST_VARDIR/my.cnf  --backup  --stream=xbstream > $streamfile 2>$targetdir/backup_stream.log;
+exec $XTRABACKUP --defaults-file=$MYSQLTEST_VARDIR/my.cnf  --backup --databases-exclude=foobar --stream=xbstream > $streamfile 2>$targetdir/backup_stream.log;
 echo # xbstream extract;
 --disable_result_log
 exec $XBSTREAM -x -C $targetdir  < $streamfile;