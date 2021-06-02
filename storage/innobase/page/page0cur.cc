fa0bbff032bc5717715fdf32ce4c8ebdfcf73944
Monty <monty@mariadb.org>
2021-06-02 14:05:12 +0300

    Fixed that compile-pentium64-valgrind-max works
    - Removed Tokudb (no need to test this anymore with valgrind)
    - Added __attribute__(unused)) to a few places to be able to compile even
      if valgrind/memcheck.h is not installed.
    
    Reviewer: Marko Mäkelä <marko.makela@mariadb.com>

--- storage/innobase/page/page0cur.cc
+++ storage/innobase/page/page0cur.cc
@@ -1299,12 +1299,12 @@ page_cur_insert_rec_low(
 
 #ifdef HAVE_valgrind
 	{
-		const void*	rec_start
-			= rec - rec_offs_extra_size(offsets);
-		ulint		extra_size
-			= rec_offs_extra_size(offsets)
-			- (rec_offs_comp(offsets)
-			   ? REC_N_NEW_EXTRA_BYTES
+          const void*   rec_start __attribute__((unused))
+                        = rec - rec_offs_extra_size(offsets);
+                ulint   extra_size __attribute__((unused))
+                        = rec_offs_extra_size(offsets)
+                        - (rec_offs_comp(offsets)
+                           ? REC_N_NEW_EXTRA_BYTES
 			   : REC_N_OLD_EXTRA_BYTES);
 
 		/* All data bytes of the record must be valid. */
@@ -1530,13 +1530,13 @@ page_cur_insert_rec_zip(
 
 #ifdef HAVE_valgrind
 	{
-		const void*	rec_start
-			= rec - rec_offs_extra_size(offsets);
-		ulint		extra_size
-			= rec_offs_extra_size(offsets)
-			- (rec_offs_comp(offsets)
-			   ? REC_N_NEW_EXTRA_BYTES
-			   : REC_N_OLD_EXTRA_BYTES);
+                const void*     rec_start __attribute__((unused))
+                        = rec - rec_offs_extra_size(offsets);
+                ulint           extra_size __attribute__((unused))
+                        = rec_offs_extra_size(offsets)
+                        - (rec_offs_comp(offsets)
+                           ? REC_N_NEW_EXTRA_BYTES
+                           : REC_N_OLD_EXTRA_BYTES);
 
 		/* All data bytes of the record must be valid. */
 		MEM_CHECK_DEFINED(rec, rec_offs_data_size(offsets));
