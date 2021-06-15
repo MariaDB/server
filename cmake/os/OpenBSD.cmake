093bd25f94fe84a61b4d2f3d2f1ea6be85aaf7f3
Daniel Black <daniel@mariadb.org>
2021-06-15 12:40:33 +1000

    cmake OpenBSD copyright notice correction
    Brad Smith made this OpenBSD file based of the
    FreeBSD cmake directives in commit ab589043670145c95ff372021bab19464b6036e2
    by the Monty Program Ab.
    
    As such the Oracle Copyright header isn't really applicable.

--- cmake/os/OpenBSD.cmake
+++ cmake/os/OpenBSD.cmake
@@ -1,5 +1,4 @@
-
-# Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+# Copyright (C) 2012 Monty Program Ab, 2021 Brad Smith
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
