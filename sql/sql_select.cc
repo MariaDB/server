c314fd8c1ad45d7a2bb2b901c7c7634e6a605939
Sergei Golubchik <serg@mariadb.org>
2021-07-17 21:17:41 +0200

    compilation failure on clang 11.1.0
    returning NULL (a.k.a. nullptr) from a bool function.
    
    followup for add782a13e5

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -18426,7 +18426,7 @@ static bool make_json_valid_expr(TABLE *table, Field *field)
   Item *expr, *item_field;
 
   if (!table->expr_arena && table->init_expr_arena(thd->mem_root))
-    return NULL;
+    return 1;
 
   thd->set_n_backup_active_arena(table->expr_arena, &backup_arena);
   if ((item_field= new (thd->mem_root) Item_field(thd, field)) &&
