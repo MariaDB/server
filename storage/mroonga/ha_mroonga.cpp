cf03f61aa45d00a6ac822e86cec2706679cb4346
Sutou Kouhei <kou@clear-code.com>
2021-07-20 16:32:57 +0900

    MDEV-39556 SIGSEGV in ha_mroonga::storage_set_keys_in_use on SELECT
    Many pathes exist for Mroonga to open tables without opening
    indexes. Altering from another storage engine and accessing
    the information schema statistics is one mechanism.
    
    The fix is a backport from upstream.
    
    Backport from https://github.com/mroonga/mroonga/commit/0b3541910ede845f6ccb62241a633310f2c27f52#diff-c026dc94c7f79d426e7a68e1e69fdc0f7743296a37d6cd19c9e7117a763f242e
    
    Fix a crash bug that may be caused after MySQL/MariaDB upgrade
    
    GitHub: fix GH-423
    
    MySQL/MariaDB may open Mroonga tables by "CHECK TABLES" when
    MySQL/MariaDB is upgraded. Mroonga didn't open indexes when a table is
    opened by "CHECK TABLES". If "REPAIR TABLE" is needed, a table is
    reopened. If "REPAIR TABLE" isn't needed, a table is NOT reopened. The
    table doesn't open indexes. "SELECT ... MATCH AGAINST" for the table
    causes a crash.
    
    Reported by Vincent Pelletier. Thanks!!!
    
    Backported to MariaDB by Daniel Black
    Reviews by: Sutou Kouhei and Oleksandr Byelkin

--- storage/mroonga/ha_mroonga.cpp
+++ storage/mroonga/ha_mroonga.cpp
@@ -4291,18 +4291,15 @@ int ha_mroonga::wrapper_open(const char *name, int mode, uint open_options)
   if (error)
     DBUG_RETURN(error);
 
-  if (!(open_options & HA_OPEN_FOR_REPAIR)) {
-    error = open_table(name);
-    if (error)
-      DBUG_RETURN(error);
+  error = open_table(name);
+  if (error && !(open_options & HA_OPEN_FOR_REPAIR))
+    goto error_exit;
 
-    error = wrapper_open_indexes(name);
-    if (error) {
-      grn_obj_unlink(ctx, grn_table);
-      grn_table = NULL;
-      DBUG_RETURN(error);
-    }
-  }
+  error = wrapper_open_indexes(name);
+  if (error && !(open_options & HA_OPEN_FOR_REPAIR))
+    goto error_exit;
+
+  error = 0;
 
   mrn_init_alloc_root(&mem_root, 1024, 0, MYF(0));
   wrap_key_info = mrn_create_key_info_for_table(share, table, &error);
@@ -4390,6 +4387,7 @@ int ha_mroonga::wrapper_open(const char *name, int mode, uint open_options)
     }
   }
 
+error_exit:
   if (error)
   {
     grn_obj_unlink(ctx, grn_table);
@@ -4662,37 +4660,35 @@ int ha_mroonga::storage_open(const char *name, int mode, uint open_options)
     DBUG_RETURN(error);
   }
 
+  error = storage_open_indexes(name);
+  if (error && !(open_options & HA_OPEN_FOR_REPAIR)) {
+    storage_close_columns();
+    grn_obj_unlink(ctx, grn_table);
+    grn_table = NULL;
+    DBUG_RETURN(error);
+  }
+
+  storage_set_keys_in_use();
+
   if (!(open_options & HA_OPEN_FOR_REPAIR)) {
-    error = storage_open_indexes(name);
-    if (error) {
-      storage_close_columns();
-      grn_obj_unlink(ctx, grn_table);
-      grn_table = NULL;
-      DBUG_RETURN(error);
-    }
-
-    storage_set_keys_in_use();
-
-    {
-      mrn::Lock lock(&mrn_operations_mutex);
-      mrn::PathMapper mapper(name);
-      const char *table_name = mapper.table_name();
-      size_t table_name_size = strlen(table_name);
-      if (db->is_broken_table(table_name, table_name_size)) {
-        GRN_LOG(ctx, GRN_LOG_NOTICE,
-                "Auto repair is started: <%s>",
-                name);
-        error = operations_->repair(table_name, table_name_size);
+    mrn::Lock lock(&mrn_operations_mutex);
+    mrn::PathMapper mapper(name);
+    const char *table_name = mapper.table_name();
+    size_t table_name_size = strlen(table_name);
+    if (db->is_broken_table(table_name, table_name_size)) {
+      GRN_LOG(ctx, GRN_LOG_NOTICE,
+              "Auto repair is started: <%s>",
+              name);
+      error = operations_->repair(table_name, table_name_size);
+      if (!error)
+        db->mark_table_repaired(table_name, table_name_size);
+      if (!share->disable_keys) {
         if (!error)
-          db->mark_table_repaired(table_name, table_name_size);
-        if (!share->disable_keys) {
-          if (!error)
-            error = storage_reindex();
-        }
-        GRN_LOG(ctx, GRN_LOG_NOTICE,
-                "Auto repair is done: <%s>: %s",
-                name, error == 0 ? "success" : "failure");
+          error = storage_reindex();
       }
+      GRN_LOG(ctx, GRN_LOG_NOTICE,
+              "Auto repair is done: <%s>: %s",
+              name, error == 0 ? "success" : "failure");
     }
   }
 
@@ -5237,7 +5233,7 @@ void ha_mroonga::storage_set_keys_in_use()
     if (i == table_share->primary_key) {
       continue;
     }
-    if (!grn_index_tables[i]) {
+    if (grn_index_tables && !grn_index_tables[i]) {
       /* disabled */
       table_share->keys_in_use.clear_bit(i);
       DBUG_PRINT("info", ("mroonga: key %u disabled", i));
