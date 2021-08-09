MariaDB/server

commit 751ebe44fda4deb715fc2235548517c287f2a559

Add feature summary at the end of cmake.
This gives a short overview over found/missing dependencies as well
as enabled/disabled features.

Initial author Heinz Wiesinger <heinz@m2mobi.com>
Additions by Vicențiu Ciorbaru <vicentiu@mariadb.org>
* Report all plugins enabled via MYSQL_ADD_PLUGIN
* Simplify code. Eliminate duplication by making use of WITH_xxx
  variable values to set feature "ON" / "OFF" state.

Reviewed by: wlad@mariadb.com (code details) serg@mariadb.com (the idea)

cmake/plugin.cmake
@@ -49,7 +49,7 @@ MACRO(MYSQL_ADD_PLUGIN)
   LIST(REMOVE_AT SOURCES 0)
   STRING(TOUPPER ${plugin} plugin)
   STRING(TOLOWER ${plugin} target)
-  
+
   IF (ARG_MANDATORY)
     UNSET(PLUGIN_${plugin} CACHE)
     SET(PLUGIN_${plugin} "YES")
@@ -110,11 +110,11 @@ MACRO(MYSQL_ADD_PLUGIN)
     SET(with_var "WITH_${plugin}")
   ENDIF()
   UNSET(${with_var} CACHE)
-  
+
   IF(NOT ARG_DEPENDENCIES)
     SET(ARG_DEPENDENCIES)
   ENDIF()
-  
+
   IF(NOT ARG_MODULE_OUTPUT_NAME)
     IF(ARG_STORAGE_ENGINE)
       SET(ARG_MODULE_OUTPUT_NAME "ha_${target}")
@@ -257,6 +257,16 @@ MACRO(MYSQL_ADD_PLUGIN)
     INSTALL_MYSQL_TEST("${CMAKE_CURRENT_SOURCE_DIR}/mysql-test/" "plugin/${subpath}")
   ENDIF()
 
+  GET_TARGET_PROPERTY(plugin_type ${target} TYPE)
+  STRING(REGEX REPLACE "_LIBRARY$" "" plugin_type ${plugin_type})
+  STRING(REGEX REPLACE "^NO$" "" plugin_type ${plugin_type})
+  IF(ARG_STORAGE_ENGINE)
+    ADD_FEATURE_INFO(${plugin} PLUGIN_${plugin} "Storage Engine ${plugin_type}")
+  ELSEIF(ARG_CLIENT)
+    ADD_FEATURE_INFO(${plugin} PLUGIN_${plugin} "Client plugin ${plugin_type}")
+  ELSE()
+    ADD_FEATURE_INFO(${plugin} PLUGIN_${plugin} "Server plugin ${plugin_type}")
+  ENDIF()
   ENDIF(NOT WITHOUT_SERVER OR ARG_CLIENT)
 ENDMACRO()
 

CMakeLists.txt
@@ -147,6 +147,7 @@ IF (NOT CPACK_GENERATOR)
   ENDIF(WIN32) 
 ENDIF(NOT CPACK_GENERATOR)
 
+INCLUDE(FeatureSummary)
 INCLUDE(misc)
 INCLUDE(mysql_version)
 INCLUDE(cpack_source_ignore_files)
@@ -418,11 +419,14 @@ ADD_SUBDIRECTORY(sql/share)
 IF(NOT WITHOUT_SERVER)
   ADD_SUBDIRECTORY(tests)
   ADD_SUBDIRECTORY(sql)
-  OPTION (WITH_EMBEDDED_SERVER "Compile MySQL with embedded server" OFF)
+  OPTION (WITH_EMBEDDED_SERVER "Compile MariaDB with embedded server" OFF)
   IF(WITH_EMBEDDED_SERVER) 
-   ADD_SUBDIRECTORY(libmysqld)
-   ADD_SUBDIRECTORY(libmysqld/examples)
+    ADD_SUBDIRECTORY(libmysqld)
+    ADD_SUBDIRECTORY(libmysqld/examples)
   ENDIF(WITH_EMBEDDED_SERVER)
+  IF(NOT WIN32)
+    ADD_FEATURE_INFO(EMBEDDED_SERVER WITH_EMBEDDED_SERVER "Embedded MariaDB Server Library")
+  ENDIF()
 
   IF(WITH_WSREP)
     ADD_SUBDIRECTORY(wsrep)
@@ -505,6 +509,13 @@ IF(WIN32 AND SIGNCODE)
   # Configure post-install script for authenticode signing
   CONFIGURE_FILE(${PROJECT_SOURCE_DIR}/cmake/sign.cmake.in sign.cmake @ONLY)
   INSTALL(SCRIPT ${PROJECT_BINARY_DIR}/sign.cmake)
+ENDIF()
+
+FEATURE_SUMMARY(WHAT ALL FATAL_ON_MISSING_REQUIRED_PACKAGES VAR MARIADB_FEATURE_SUMMARY)
+
+OPTION(FEATURE_SUMMARY "Print feature summary at the end of configure step" ON)
+IF (FEATURE_SUMMARY)
+  MESSAGE_ONCE(SUMMARY "${MARIADB_FEATURE_SUMMARY}")
 ENDIF()
 
 IF(NON_DISTRIBUTABLE_WARNING)

cmake/jemalloc.cmake
@@ -37,5 +37,6 @@ MACRO (CHECK_JEMALLOC)
     IF (NOT LIBJEMALLOC AND NOT WITH_JEMALLOC STREQUAL "auto")
       MESSAGE(FATAL_ERROR "jemalloc is not found")
     ENDIF()
+    ADD_FEATURE_INFO(JEMALLOC LIBJEMALLOC "Use the JeMalloc memory allocator")
   ENDIF()
 ENDMACRO()

cmake/numa.cmake
@@ -31,6 +31,7 @@ MACRO (MYSQL_CHECK_NUMA)
       ENDIF()
     ENDIF()
 
+    ADD_FEATURE_INFO(NUMA HAVE_LIBNUMA "NUMA memory allocation policy")
     IF(WITH_NUMA AND NOT HAVE_LIBNUMA)
       # Forget it in cache, abort the build.
       UNSET(WITH_NUMA CACHE)

cmake/readline.cmake
@@ -52,7 +52,7 @@ MACRO (MYSQL_CHECK_MULTIBYTE)
 ENDMACRO()
 
 MACRO (FIND_CURSES)
- FIND_PACKAGE(Curses) 
+ FIND_PACKAGE(Curses REQUIRED)
  MARK_AS_ADVANCED(CURSES_CURSES_H_PATH CURSES_FORM_LIBRARY CURSES_HAVE_CURSES_H)
  IF(NOT CURSES_FOUND)
    SET(ERRORMSG "Curses library not found. Please install appropriate package,

cmake/ssl.cmake
@@ -126,6 +126,7 @@ MACRO (MYSQL_CHECK_SSL)
       ENDIF()
     ENDIF()
     FIND_PACKAGE(OpenSSL)
+    SET_PACKAGE_PROPERTIES(OpenSSL PROPERTIES TYPE RECOMMENDED)
     IF(OPENSSL_FOUND)
       SET(OPENSSL_LIBRARY ${OPENSSL_SSL_LIBRARY})
       SET(SSL_SOURCES "")

cmake/systemd.cmake
@@ -63,5 +63,6 @@ MACRO(CHECK_SYSTEMD)
     ELSEIF(NOT WITH_SYSTEMD STREQUAL "no")
       MESSAGE(FATAL_ERROR "Invalid value for WITH_SYSTEMD. Must be 'yes', 'no', or 'auto'.")
     ENDIF()
+    ADD_FEATURE_INFO(SYSTEMD LIBSYSTEMD "Systemd scripts and notification support")
   ENDIF()
 ENDMACRO()

cmake/wsrep.cmake
@@ -41,3 +41,6 @@ SET(WSREP_PROC_INFO ${WITH_WSREP})
 IF(WITH_WSREP)
   SET(WSREP_PATCH_VERSION "wsrep_${WSREP_VERSION}")
 ENDIF()
+IF (NOT WIN32)
+  ADD_FEATURE_INFO(WSREP WITH_WSREP "WSREP replication API (to use, e.g. Galera Replication library)")
+ENDIF()

commit 78c7d50e4f2d80ad93aea9ead15b013c16366d15

Fix cmake warning caused by 751ebe44fda4deb715fc2235548517c287f2a559
CMake Warning (dev) at cmake/plugin.cmake:288 (GET_TARGET_PROPERTY):
  Policy CMP0045 is not set: Error on non-existent target...

Also, fix condition argument in ADD_FEATURE_INFO. It is not a string
but boolean

cmake/plugin.cmake
@@ -285,15 +285,20 @@ MACRO(MYSQL_ADD_PLUGIN)
     INSTALL_MYSQL_TEST("${CMAKE_CURRENT_SOURCE_DIR}/mysql-test/" "plugin/${subpath}")
   ENDIF()
 
-  GET_TARGET_PROPERTY(plugin_type ${target} TYPE)
-  STRING(REGEX REPLACE "_LIBRARY$" "" plugin_type ${plugin_type})
-  STRING(REGEX REPLACE "^NO$" "" plugin_type ${plugin_type})
-  IF(ARG_STORAGE_ENGINE)
-    ADD_FEATURE_INFO(${plugin} PLUGIN_${plugin} "Storage Engine ${plugin_type}")
-  ELSEIF(ARG_CLIENT)
-    ADD_FEATURE_INFO(${plugin} PLUGIN_${plugin} "Client plugin ${plugin_type}")
+  IF(TARGET ${target})
+    GET_TARGET_PROPERTY(plugin_type ${target} TYPE)
+    STRING(REPLACE "_LIBRARY" "" plugin_type ${plugin_type})
+    SET(have_target 1)
   ELSE()
-    ADD_FEATURE_INFO(${plugin} PLUGIN_${plugin} "Server plugin ${plugin_type}")
+    SET(plugin_type)
+    SET(have_target 0)
+  ENDIF()
+  IF(ARG_STORAGE_ENGINE)
+    ADD_FEATURE_INFO(${plugin} ${have_target} "Storage Engine ${plugin_type}")
+  ELSEIF(ARG_CLIENT)
+    ADD_FEATURE_INFO(${plugin} ${have_target} "Client plugin ${plugin_type}")
+  ELSE()
+    ADD_FEATURE_INFO(${plugin} ${have_target} "Server plugin ${plugin_type}")
   ENDIF()
   ENDIF(NOT WITHOUT_SERVER OR ARG_CLIENT)
 ENDMACRO()