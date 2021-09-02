MariaDB/server

commit 82b7c561b7919fa24e3d24b3f04a16046e24374f

MDEV-24258 Merge dict_sys.mutex into dict_sys.latch
In the parent commit, dict_sys.latch could theoretically have been
replaced with a mutex. But, we can do better and merge dict_sys.mutex
into dict_sys.latch. Generally, every occurrence of dict_sys.mutex_lock()
will be replaced with dict_sys.lock().

The PERFORMANCE_SCHEMA instrumentation for dict_sys_mutex
will be removed along with dict_sys.mutex. The dict_sys.latch
will remain instrumented as dict_operation_lock.

Some use of dict_sys.lock() will be replaced with dict_sys.freeze(),
which we will reintroduce for the new shared mode. Most notably,
concurrent table lookups are possible as long as the tables are present
in the dict_sys cache. In particular, this will allow more concurrency
among InnoDB purge workers.

Because dict_sys.mutex will no longer 'throttle' the threads that purge
InnoDB transaction history, a performance degradation may be observed
unless innodb_purge_threads=1.

The table cache eviction policy will become FIFO-like,
similar to what happened to fil_system.LRU
in commit 45ed9dd957eebc7fc84feb2509f4aa6baa908a95.
The name of the list dict_sys.table_LRU will become somewhat misleading;
that list contains tables that may be evicted, even though the
eviction policy no longer is least-recently-used but first-in-first-out.
(Note: Tables can never be evicted as long as locks exist on them or
the tables are in use by some thread.)

As demonstrated by the test perfschema.sxlock_func, there
will be less contention on dict_sys.latch, because some previous
use of exclusive latches will be replaced with shared latches.

fts_parse_sql_no_dict_lock(): Replaced with pars_sql().

fts_get_table_name_prefix(): Merged to fts_optimize_create().

dict_stats_update_transient_for_index(): Deduplicated some code.

ha_innobase::info_low(), dict_stats_stop_bg(): Use a combination
of dict_sys.latch and table->stats_mutex_lock() to cover the
changes of BG_STAT_SHOULD_QUIT, because the flag is being read

storage/innobase/row/row0purge.cc
@@ -114,11 +114,11 @@ row_purge_remove_clust_if_poss_low(
 	if (table_id) {
 retry:
 		purge_sys.check_stop_FTS();
-		dict_sys.mutex_lock();
+		dict_sys.lock(SRW_LOCK_CALL);
 		table = dict_table_open_on_id(
 			table_id, true, DICT_TABLE_OP_OPEN_ONLY_IF_CACHED);
 		if (!table) {
-			dict_sys.mutex_unlock();
+			dict_sys.unlock();
 		} else if (table->n_rec_locks) {
 			for (dict_index_t* ind = UT_LIST_GET_FIRST(
 				     table->indexes); ind;
@@ -142,7 +142,7 @@ removed:
 close_and_exit:
 		if (table) {
 			dict_table_close(table, true, false);
-			dict_sys.mutex_unlock();
+			dict_sys.unlock();
 		}
 		return success;
 	}
@@ -172,7 +172,7 @@ close_and_exit:
 					table->space = nullptr;
 					table->file_unreadable = true;
 				}
-				dict_sys.mutex_unlock();
+				dict_sys.unlock();
 				table = nullptr;
 			}
 			f = fil_delete_tablespace(space_id);
@@ -182,7 +182,7 @@ close_and_exit:
 
 		if (table) {
 			dict_table_close(table, true, false);
-			dict_sys.mutex_unlock();
+			dict_sys.unlock();
 			table = nullptr;
 		}
 

mysql-test/suite/perfschema/r/sxlock_func.result
@@ -35,10 +35,10 @@ SELECT DISTINCT event_name FROM performance_schema.events_waits_history_long
 WHERE event_name LIKE 'wait/synch/rwlock/innodb/%'
 AND event_name NOT IN
 ('wait/synch/rwlock/innodb/btr_search_latch',
+'wait/synch/rwlock/innodb/dict_operation_lock',
 'wait/synch/rwlock/innodb/trx_purge_latch')
 ORDER BY event_name;
 event_name
-wait/synch/rwlock/innodb/dict_operation_lock
 wait/synch/rwlock/innodb/fil_space_latch
 wait/synch/rwlock/innodb/lock_latch
 SELECT event_name FROM performance_schema.events_waits_history_long

mysql-test/suite/perfschema/t/sxlock_func.test
@@ -42,11 +42,13 @@ commit;
 drop table t1;
 
 # Make sure some rw-lock operations have been executed
+# (there will only occasionally be waits on some rw-locks)
 
 SELECT DISTINCT event_name FROM performance_schema.events_waits_history_long
 WHERE event_name LIKE 'wait/synch/rwlock/innodb/%'
 AND event_name NOT IN
 ('wait/synch/rwlock/innodb/btr_search_latch',
+ 'wait/synch/rwlock/innodb/dict_operation_lock',
  'wait/synch/rwlock/innodb/trx_purge_latch')
 ORDER BY event_name;
 

storage/innobase/btr/btr0sea.cc
@@ -230,12 +230,12 @@ void btr_search_disable()
 {
 	dict_table_t*	table;
 
-	dict_sys.mutex_lock();
+	dict_sys.freeze(SRW_LOCK_CALL);
 
 	btr_search_x_lock_all();
 
 	if (!btr_search_enabled) {
-		dict_sys.mutex_unlock();
+		dict_sys.unfreeze();
 		btr_search_x_unlock_all();
 		return;
 	}
@@ -256,7 +256,7 @@ void btr_search_disable()
 		btr_search_disable_ref_count(table);
 	}
 
-	dict_sys.mutex_unlock();
+	dict_sys.unfreeze();
 
 	/* Set all block->index = NULL. */
 	buf_pool.clear_hash_index();
@@ -1263,7 +1263,7 @@ retry:
 	ut_ad(page_is_leaf(block->frame));
 
 	/* We must not dereference block->index here, because it could be freed
-	if (index->table->n_ref_count == 0).
+	if (!index->table->get_ref_count() && !dict_sys.frozen()).
 	Determine the ahi_slot based on the block contents. */
 
 	const index_id_t	index_id
@@ -1436,10 +1436,8 @@ void btr_search_drop_page_hash_when_freed(const page_id_t page_id)
 			/* In all our callers, the table handle should
 			be open, or we should be in the process of
 			dropping the table (preventing eviction). */
-#ifdef SAFE_MUTEX
 			DBUG_ASSERT(block->index->table->get_ref_count()
-				    || dict_sys.mutex_is_locked());
-#endif /* SAFE_MUTEX */
+				    || dict_sys.locked());
 			btr_search_drop_page_hash_index(block);
 		}
 	}

storage/innobase/dict/dict0boot.cc
@@ -225,7 +225,7 @@ dict_boot(void)
 
 	heap = mem_heap_create(450);
 
-	dict_sys.mutex_lock();
+	dict_sys.lock(SRW_LOCK_CALL);
 
 	/* Get the dictionary header */
 	const byte* dict_hdr = &dict_hdr_get(&mtr)->frame[DICT_HDR];
@@ -413,10 +413,10 @@ dict_boot(void)
 		dict_load_sys_table(dict_sys.sys_columns);
 		dict_load_sys_table(dict_sys.sys_indexes);
 		dict_load_sys_table(dict_sys.sys_fields);
-		dict_sys.mutex_unlock();
+		dict_sys.unlock();
 		dict_sys.load_sys_tables();
 	} else {
-		dict_sys.mutex_unlock();
+		dict_sys.unlock();
 	}
 
 	return(err);

storage/innobase/dict/dict0crea.cc
@@ -343,7 +343,7 @@ dict_build_table_def_step(
 	que_thr_t*	thr,	/*!< in: query thread */
 	tab_node_t*	node)	/*!< in: table create node */
 {
-	dict_sys.assert_locked();
+	ut_ad(dict_sys.locked());
 	dict_table_t*	table = node->table;
 	ut_ad(!table->is_temporary());
 	ut_ad(!table->space);
@@ -403,7 +403,7 @@ dict_build_v_col_def_step(
 Based on an index object, this function builds the entry to be inserted
 in the SYS_INDEXES system table.
 @return the tuple which should be inserted */
-static
+static MY_ATTRIBUTE((nonnull, warn_unused_result))
 dtuple_t*
 dict_create_sys_indexes_tuple(
 /*==========================*/
@@ -416,7 +416,7 @@ dict_create_sys_indexes_tuple(
 	dfield_t*	dfield;
 	byte*		ptr;
 
-	dict_sys.assert_locked();
+	ut_ad(dict_sys.locked());
 	ut_ad(index);
 	ut_ad(index->table->space || !UT_LIST_GET_LEN(index->table->indexes)
 	      || index->table->file_unreadable);
@@ -646,7 +646,7 @@ dict_build_index_def_step(
 	dtuple_t*	row;
 	trx_t*		trx;
 
-	dict_sys.assert_locked();
+	ut_ad(dict_sys.locked());
 
 	trx = thr_get_trx(thr);
 
@@ -691,7 +691,7 @@ dict_build_index_def(
 	dict_index_t*		index,	/*!< in/out: index */
 	trx_t*			trx)	/*!< in/out: InnoDB transaction handle */
 {
-	dict_sys.assert_locked();
+	ut_ad(dict_sys.locked());
 
 	ut_ad((UT_LIST_GET_LEN(table->indexes) > 0)
 	      || dict_index_is_clust(index));
@@ -734,7 +734,7 @@ dict_create_index_tree_step(
 	dict_index_t*	index;
 	dtuple_t*	search_tuple;
 
-	dict_sys.assert_locked();
+	ut_ad(dict_sys.locked());
 
 	index = node->index;
 
@@ -803,7 +803,7 @@ dict_create_index_tree_in_mem(
 {
 	mtr_t		mtr;
 
-	dict_sys.assert_locked();
+	ut_ad(dict_sys.locked());
 	ut_ad(!(index->type & DICT_FTS));
 
 	mtr_start(&mtr);
@@ -833,7 +833,7 @@ uint32_t dict_drop_index_tree(btr_pcur_t *pcur, trx_t *trx, mtr_t *mtr)
 {
   rec_t *rec= btr_pcur_get_rec(pcur);
 
-  ut_d(if (trx) dict_sys.assert_locked());
+  ut_ad(!trx || dict_sys.locked());
   ut_ad(!dict_table_is_comp(dict_sys.sys_indexes));
   btr_pcur_store_position(pcur, mtr);
 
@@ -995,7 +995,7 @@ dict_create_table_step(
 	trx_t*		trx;
 
 	ut_ad(thr);
-	dict_sys.assert_locked();
+	ut_ad(dict_sys.locked());
 
 	trx = thr_get_trx(thr);
 
@@ -1171,7 +1171,7 @@ dict_create_index_step(
 	trx_t*		trx;
 
 	ut_ad(thr);
-	dict_sys.assert_locked();
+	ut_ad(dict_sys.locked());
 
 	trx = thr_get_trx(thr);
 
@@ -1319,7 +1319,7 @@ bool dict_sys_t::load_sys_tables()
 {
   ut_ad(!srv_any_background_activity());
   bool mismatch= false;
-  mutex_lock();
+  lock(SRW_LOCK_CALL);
   if (!(sys_foreign= load_table(SYS_TABLE[SYS_FOREIGN],
                                 DICT_ERR_IGNORE_FK_NOKEY)));
   else if (UT_LIST_GET_LEN(sys_foreign->indexes) == 3 &&
@@ -1354,7 +1354,7 @@ bool dict_sys_t::load_sys_tables()
     mismatch= true;
     ib::error() << "Invalid definition of SYS_VIRTUAL";
   }
-  mutex_unlock();
+  unlock();
   return mismatch;
 }
 
@@ -1453,13 +1453,13 @@ err_exit:
   trx->free();
   srv_file_per_table= srv_file_per_table_backup;
 
-  mutex_lock();
+  lock(SRW_LOCK_CALL);
   if (sys_foreign);
   else if (!(sys_foreign= load_table(SYS_TABLE[SYS_FOREIGN])))
   {
     tablename= SYS_TABLE[SYS_FOREIGN].data();
 load_fail:
-    mutex_unlock();
+    unlock();
     ib::error() << "Failed to CREATE TABLE " << tablename;
     return DB_TABLE_NOT_FOUND;
   }
@@ -1484,7 +1484,7 @@ load_fail:
   else
     prevent_eviction(sys_virtual);
 
-  mutex_unlock();
+  unlock();
   return DB_SUCCESS;
 }
 
@@ -1877,7 +1877,7 @@ dict_create_add_foreigns_to_dictionary(
 	const dict_table_t*	table,
 	trx_t*			trx)
 {
-  dict_sys.assert_locked();
+  ut_ad(dict_sys.locked());
 
   if (!dict_sys.sys_foreign)
   {

storage/innobase/dict/dict0defrag_bg.cc
@@ -145,7 +145,7 @@ dict_stats_defrag_pool_del(
 {
 	ut_a((table && !index) || (!table && index));
 	ut_ad(!srv_read_only_mode);
-	dict_sys.assert_locked();
+	ut_ad(dict_sys.frozen());
 
 	mysql_mutex_lock(&defrag_pool_mutex);
 
@@ -170,45 +170,34 @@ dict_stats_defrag_pool_del(
 /*****************************************************************//**
 Get the first index that has been added for updating persistent defrag
 stats and eventually save its stats. */
-static
-void
-dict_stats_process_entry_from_defrag_pool()
+static void dict_stats_process_entry_from_defrag_pool()
 {
-	table_id_t	table_id;
-	index_id_t	index_id;
+  table_id_t	table_id;
+  index_id_t	index_id;
 
-	ut_ad(!srv_read_only_mode);
+  ut_ad(!srv_read_only_mode);
 
-	/* pop the first index from the auto defrag pool */
-	if (!dict_stats_defrag_pool_get(&table_id, &index_id)) {
-		/* no index in defrag pool */
-		return;
-	}
+  /* pop the first index from the auto defrag pool */
+  if (!dict_stats_defrag_pool_get(&table_id, &index_id))
+    /* no index in defrag pool */
+    return;
 
-	dict_table_t*	table;
+  dict_sys.freeze(SRW_LOCK_CALL);
 
-	dict_sys.mutex_lock();
-
-	/* If the table is no longer cached, we've already lost the in
-	memory stats so there's nothing really to write to disk. */
-	table = dict_table_open_on_id(table_id, TRUE,
-				      DICT_TABLE_OP_OPEN_ONLY_IF_CACHED);
-
-	dict_index_t* index = table && !table->corrupted
-		? dict_table_find_index_on_id(table, index_id)
-		: NULL;
-
-	if (!index || index->is_corrupted()) {
-		if (table) {
-			dict_table_close(table, TRUE, FALSE);
-		}
-		dict_sys.mutex_unlock();
-		return;
-	}
-
-	dict_sys.mutex_unlock();
-	dict_stats_save_defrag_stats(index);
-	dict_table_close(table, FALSE, FALSE);
+  /* If the table is no longer cached, we've already lost the in
+  memory stats so there's nothing really to write to disk. */
+  dict_table_t *table= dict_sys.find_table(table_id);
+  dict_index_t *index= table && table->corrupted
+    ? nullptr : dict_table_find_index_on_id(table, index_id);
+  const bool save= index && !index->is_corrupted();
+  if (save)
+    table->acquire();
+  dict_sys.unfreeze();
+  if (save)
+  {
+    dict_stats_save_defrag_stats(index);
+    table->release();
+  }
 }
 
 /*****************************************************************//**
@@ -237,7 +226,7 @@ dict_stats_save_defrag_summary(
 		return DB_SUCCESS;
 	}
 
-	dict_sys_lock();
+	dict_sys.lock(SRW_LOCK_CALL);
 
 	ret = dict_stats_save_index_stat(index, time(NULL), "n_pages_freed",
 					 index->stat_defrag_n_pages_freed,
@@ -246,7 +235,7 @@ dict_stats_save_defrag_summary(
 					 " last defragmentation run.",
 					 NULL);
 
-	dict_sys_unlock();
+	dict_sys.unlock();
 
 	return (ret);
 }
@@ -331,7 +320,7 @@ dict_stats_save_defrag_stats(
 		return DB_SUCCESS;
 	}
 
-	dict_sys_lock();
+	dict_sys.lock(SRW_LOCK_CALL);
 	ret = dict_stats_save_index_stat(index, now, "n_page_split",
 					 index->stat_defrag_n_page_split,
 					 NULL,
@@ -361,6 +350,6 @@ dict_stats_save_defrag_stats(
 		NULL);
 
 end:
-	dict_sys_unlock();
+	dict_sys.unlock();
 	return ret;
 }

commit 4690442411b75ea0fc1a6aacabe767d6fb1d1f62

MDEV-24258 fixup: Throttle purge with exclusive dict_sys.latch
A performance regression was observed after
commit 82b7c561b7919fa24e3d24b3f04a16046e24374f
because purge tasks would end up waiting more elsewhere,
most notably trx_purge_get_next_rec() and trx_purge_truncate_history().

row_purge_parse_undo_rec(): Prevent the performance regression by
unnecessarily acquiring dict_sys.latch in exclusive mode after the
table lookup.

storage/innobase/row/row0purge.cc
@@ -1038,6 +1038,12 @@ try_again:
 		goto err_exit;
 	}
 
+	/* FIXME: We are acquiring exclusive dict_sys.latch only to
+	avoid increased wait times in
+	trx_purge_get_next_rec() and trx_purge_truncate_history(). */
+	dict_sys.lock(SRW_LOCK_CALL);
+	dict_sys.unlock();
+
 already_locked:
 	ut_ad(!node->table->is_temporary());
 