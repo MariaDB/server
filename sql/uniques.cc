da322f19c618538b26f46d818f5fe33eebf78b6d
Oleksandr Byelkin <sanja@mariadb.com>
2021-10-01 11:00:29 +0200

    MDEV-26459 Assertion `block_size <= 0xFFFFFFFFL' failed in calculate_block_sizes for 10.7 only
    Limit default allocation block in tree of Unique class

--- sql/uniques.cc
+++ sql/uniques.cc
@@ -90,8 +90,10 @@ Unique::Unique(qsort_cmp2 comp_func, void * comp_func_fixed_arg,
   if (min_dupl_count_arg)
     full_size+= sizeof(element_count);
   with_counters= MY_TEST(min_dupl_count_arg);
-  init_tree(&tree, (max_in_memory_size / 16), 0, size, comp_func,
-            NULL, comp_func_fixed_arg, MYF(MY_THREAD_SPECIFIC));
+
+  init_tree(&tree, MY_MIN(max_in_memory_size / 16, UINT_MAX32),
+            0, size, comp_func, NULL, comp_func_fixed_arg,
+            MYF(MY_THREAD_SPECIFIC));
   /* If the following fail's the next add will also fail */
   my_init_dynamic_array(PSI_INSTRUMENT_ME, &file_ptrs, sizeof(Merge_chunk), 16,
                         16, MYF(MY_THREAD_SPECIFIC));
