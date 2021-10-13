MariaDB/server

commit b67210b1054118fe14b6346bdee3f9bbd3416870

Deb: Sync build and runtime dependencies from downstream to upstream
- Add libconfig-inifiles-perl to mariadb-client depends as mytop uses it
  https://salsa.debian.org/mariadb-team/mariadb-10.5/-/commit/7a748da87a23eb874d0bddb64114a795e9d07e6d
  http://bugs.debian.org/875708

- Make libzstd dependency versioned as RocksDB need at least 1.3.3
  https://salsa.debian.org/mariadb-team/mariadb-10.5/-/commit/da4eb54fc0c2ded0e10243fe1d81bf7e105a25c3

  However, https://github.com/facebook/rocksdb/blob/master/INSTALL.md
  implies that zstandard 1.1.3 would suffice. Version 1.3.x is available
  from Debian Stretch-backports and Ubuntu Xenial onwards.

debian/control
@@ -38,7 +38,7 @@ Build-Depends: bison,
                libsystemd-dev [linux-any],
                liburing-dev [linux-any],
                libxml2-dev,
-               libzstd-dev,
+               libzstd-dev (>= 1.3.3),
                lsb-release,
                perl:any,
                po-debconf,
@@ -378,6 +378,7 @@ Description: MariaDB database core client binaries
 Package: mariadb-client-10.7
 Architecture: any
 Depends: debianutils (>=1.6),
+         libconfig-inifiles-perl,
          mariadb-client-core-10.7 (>= ${source:Version}),
          mariadb-common,
          ${misc:Depends},

commit 8c5e5e1be9353b90f341aad73cb3d2ed7c405a96

Deb: zstd to disabled in stretch
Debian stretch is supported until Jun 2022.

MyRocks definitavely does require 1.1.3+.

Mroonga has never been zstd enabled, though
support for it exists in the codebase.

While a 1.1.3+ version exists in stretch backports
we don't support this.

Fixes: b67210b1054118fe14b6346bdee3f9bbd3416870

debian/autobake-deb.sh
@@ -88,6 +88,15 @@ then
   sed '/-DWITH_PMEM=yes/d' -i debian/rules
 fi
 
+# Debian stretch doesn't support the zstd version 1.1.3 required
+# for RocksDB. zstd isn't enabled in Mroonga even though code exists
+# for it. If someone happens to have a non-default zstd installed
+# (not 1.1.2), assume its a backport and build with it.
+if [ "$(lsb_release -sc)" = stretch ] && [ "$(apt-cache madison 'libzstd-dev' | grep -v 1.1.2)" = '' ]
+then
+  sed '/libzstd-dev/d' -i debian/control
+fi
+
 # Adjust changelog, add new version
 echo "Incrementing changelog and starting build scripts"
 

debian/control
@@ -38,7 +38,7 @@ Build-Depends: bison,
                libsystemd-dev [linux-any],
                liburing-dev [linux-any],
                libxml2-dev,
-               libzstd-dev (>= 1.3.3),
+               libzstd-dev (>= 1.1.3),
                lsb-release,
                perl:any,
                po-debconf,