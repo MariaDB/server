30337addfc34c882fc6772aa3d820e0ffb52e3b9
Jan Lindström <jan.lindstrom@mariadb.com>
2021-10-21 13:48:59 +0300

    MDEV-25114: Crash: WSREP: invalid state ROLLED_BACK (FATAL)
    Revert "MDEV-23328 Server hang due to Galera lock conflict resolution"
    
    This reverts commit 29bbcac0ee841faaa68eeb09c86ff825eabbe6b6.

--- sql/wsrep_mysqld.cc
+++ sql/wsrep_mysqld.cc
@@ -2747,7 +2747,9 @@ extern "C" void wsrep_thd_awake(THD *thd, my_bool signal)
 {
   if (signal)
   {
-    thd->awake_no_mutex(KILL_QUERY);
+    mysql_mutex_lock(&thd->LOCK_thd_data);
+    thd->awake(KILL_QUERY);
+    mysql_mutex_unlock(&thd->LOCK_thd_data);
   }
   else
   {
