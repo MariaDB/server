6aa8eba99f491dea7679e64568454b823ab1c9b6
Vladislav Vaintroub <wlad@mariadb.com>
2021-10-28 23:39:29 +0200

    Fix broken clang-cl header files (declared xbegin instead of _xbegin)
    clang-cl won't have lock elision then

--- storage/innobase/include/transactional_lock_guard.h
+++ storage/innobase/include/transactional_lock_guard.h
@@ -20,7 +20,7 @@ this program; if not, write to the Free Software Foundation, Inc.,
 
 #if defined __powerpc64__ && defined __clang__ && defined __linux__
 #elif defined __powerpc64__&&defined __GNUC__&&defined __linux__&&__GNUC__ > 4
-#elif defined _MSC_VER && (defined _M_IX86 || defined _M_X64)
+#elif defined _MSC_VER && (defined _M_IX86 || defined _M_X64) && !defined(__clang__)
 #elif defined __GNUC__ && (defined __i386__ || defined __x86_64__)
 # if __GNUC__ >= 8
 # elif defined __clang_major__ && __clang_major__ > 6
