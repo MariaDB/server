81d7adb1e2cdfb1064279b8643a8c3d22b3dd423
Vladislav Vaintroub <wlad@mariadb.com>
2021-11-18 00:51:17 +0100

    MDEV-27075 mysql_upgrade_service.exe - using uninitialized memory 'defaults_file'
    Remove section that was trying to rename default-character-set to character-set-server
    
    This seems to be an old workaround for some upgrade warning, which did not
    work for some time already, because the ini filename was not initialized.

--- sql/mysql_upgrade_service.cc
+++ sql/mysql_upgrade_service.cc
@@ -317,9 +317,6 @@ void initiate_mysqld_shutdown()
 */
 static void change_service_config()
 {
-
-  char defaults_file[MAX_PATH];
-  char default_character_set[64];
   char buf[MAX_PATH];
   char commandline[3*MAX_PATH + 19];
   int i;
@@ -381,22 +378,6 @@ static void change_service_config()
     the new version, and will complain about mismatched message file.
   */
   WritePrivateProfileString("mysqld", "basedir",NULL, props.inifile);
-
-  /* 
-    Replace default-character-set  with character-set-server, to avoid 
-    "default-character-set is deprecated and will be replaced ..."
-    message.
-  */
-  default_character_set[0]= 0;
-  GetPrivateProfileString("mysqld", "default-character-set", NULL,
-    default_character_set, sizeof(default_character_set), defaults_file);
-  if (default_character_set[0])
-  {
-    WritePrivateProfileString("mysqld", "default-character-set", NULL, 
-      defaults_file);
-    WritePrivateProfileString("mysqld", "character-set-server",
-      default_character_set, defaults_file);
-  }
 
   sprintf(defaults_file_param,"--defaults-file=%s", props.inifile);
   sprintf_s(commandline, "\"%s\" \"%s\" \"%s\"", mysqld_path, 
