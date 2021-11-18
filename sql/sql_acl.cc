0d6862f1adb7f5ada4e73f84f4c3668043cfce98
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2021-11-18 20:02:50 +0200

    refactor: Remove "revoke' arg from set_access
    * This makes the table getters and setters idempotent.
      Slight performance decrease: When running on a Tabular user table, we
      set fields (in memory) that have previously been set.
    
    The code was "hiding' a get - set - get sequence of calls for
    User_table_json. This is now more apparent. The first "get" is required
    to obtain the "adjusted" privileges based on which MariaDB version wrote
    the privileges in global_priv.
    
    The second get is required for User_table_tabular, to map column
    privileges to actual newly introduced privileges
    For example SUPER column marked as YES maps to
    GLOBAL_SUPER_ADDED_SINCE_USER_TABLE_ACLS, which is a set of more
    granular privileges.

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -887,7 +887,7 @@ class User_table: public Grant_table_base
   virtual int get_auth(THD *, MEM_ROOT *, ACL_USER *u) const= 0;
   virtual bool set_auth(const ACL_USER &u) const = 0;
   virtual privilege_t get_access() const = 0;
-  virtual void set_access(const privilege_t rights, bool revoke) const = 0;
+  virtual void set_access(const privilege_t rights) const = 0;
 
   char *get_host(MEM_ROOT *root) const
   { return ::get_field(root, m_table->field[0]); }
@@ -1064,14 +1064,11 @@ class User_table_tabular: public User_table
     return access & GLOBAL_ACLS;
   }
 
-  void set_access(const privilege_t rights, bool revoke) const
+  void set_access(const privilege_t rights) const
   {
     ulonglong priv(SELECT_ACL);
     for (uint i= start_priv_columns; i < end_priv_columns; i++, priv <<= 1)
-    {
-      if (priv & rights)
-        m_table->field[i]->store(1 + !revoke, 0);
-    }
+      m_table->field[i]->store(priv & rights ? 2 : 1, 0);
   }
 
   SSL_type get_ssl_type () const
@@ -1610,14 +1607,9 @@ class User_table_json: public User_table
     return adjust_access(version_id, access) & GLOBAL_ACLS;
   }
 
-  void set_access(const privilege_t rights, bool revoke) const
+  void set_access(const privilege_t rights) const
   {
-    privilege_t access= get_access();
-    if (revoke)
-      access&= ~rights;
-    else
-      access|= rights;
-    set_int_value("access", (longlong) (access & GLOBAL_ACLS));
+    set_int_value("access", (longlong) (rights & GLOBAL_ACLS));
     set_int_value("version_id", (longlong) MYSQL_VERSION_ID);
   }
   const char *unsafe_str(const char *s) const
@@ -5404,9 +5396,11 @@ static bool test_if_create_new_users(THD *thd)
 ****************************************************************************/
 static USER_AUTH auth_no_password;
 
+
 static int replace_user_table(THD *thd, const User_table &user_table,
-                              LEX_USER * const combo, privilege_t rights,
-                              const bool revoke_grant,
+                              LEX_USER * const combo,
+                              privilege_t rights,
+                              const bool revoke,
                               const bool can_create_user,
                               const bool no_auto_create)
 {
@@ -5418,6 +5412,7 @@ static int replace_user_table(THD *thd, const User_table &user_table,
   LEX *lex= thd->lex;
   TABLE *table= user_table.table();
   ACL_USER new_acl_user, *old_acl_user= 0;
+  privilege_t access;
   DBUG_ENTER("replace_user_table");
 
   mysql_mutex_assert_owner(&acl_cache->lock);
@@ -5431,7 +5426,7 @@ static int replace_user_table(THD *thd, const User_table &user_table,
   if (table->file->ha_index_read_idx_map(table->record[0], 0, user_key,
                                          HA_WHOLE_KEY, HA_READ_KEY_EXACT))
   {
-    if (revoke_grant)
+    if (revoke)
     {
       if (combo->host.length)
         my_error(ER_NONEXISTING_GRANT, MYF(0), combo->user.str,
@@ -5494,8 +5489,13 @@ static int replace_user_table(THD *thd, const User_table &user_table,
   }
 
   /* Update table columns with new privileges */
-  user_table.set_access(rights, revoke_grant);
-  rights= user_table.get_access();
+  access= user_table.get_access();
+  if (revoke)
+    access&= ~rights;
+  else
+    access|= rights;
+  user_table.set_access(access);
+  access= user_table.get_access();
 
   if (handle_as_role)
   {
@@ -5521,9 +5521,9 @@ static int replace_user_table(THD *thd, const User_table &user_table,
       goto end;
     }
     new_acl_user= old_row_exists ? *old_acl_user :
-                  ACL_USER(thd, *combo, lex->account_options, rights);
+                  ACL_USER(thd, *combo, lex->account_options, access);
     if (acl_user_update(thd, &new_acl_user, nauth,
-                        *combo, lex->account_options, rights))
+                        *combo, lex->account_options, access))
       goto end;
 
     if (user_table.set_auth(new_acl_user))
@@ -5631,9 +5631,9 @@ end:
     if (handle_as_role)
     {
       if (old_row_exists)
-        acl_update_role(combo->user.str, rights);
+        acl_update_role(combo->user.str, access);
       else
-        acl_insert_role(combo->user.str, rights);
+        acl_insert_role(combo->user.str, access);
     }
     else
     {
@@ -7966,9 +7966,10 @@ int mysql_table_grant(THD *thd, TABLE_LIST *table_list,
     /* Create user if needed */
     error= copy_and_check_auth(Str, tmp_Str, thd) ||
            replace_user_table(thd, tables.user_table(), Str,
-                               NO_ACL, revoke_grant, create_new_users,
-                               MY_TEST(thd->variables.sql_mode &
-                                       MODE_NO_AUTO_CREATE_USER));
+                              NO_ACL, revoke_grant,
+                              create_new_users,
+                              MY_TEST(thd->variables.sql_mode &
+                                        MODE_NO_AUTO_CREATE_USER));
     if (unlikely(error))
     {
       result= TRUE;				// Remember error
@@ -8146,7 +8147,8 @@ bool mysql_routine_grant(THD *thd, TABLE_LIST *table_list,
     /* Create user if needed */
     if (copy_and_check_auth(Str, tmp_Str, thd) ||
         replace_user_table(thd, tables.user_table(), Str,
-			   NO_ACL, revoke_grant, create_new_users,
+                           NO_ACL, revoke_grant,
+                           create_new_users,
                            MY_TEST(thd->variables.sql_mode &
                                      MODE_NO_AUTO_CREATE_USER)))
     {
@@ -8417,9 +8419,9 @@ bool mysql_grant_role(THD *thd, List <LEX_USER> &list, bool revoke)
       user_combo.user = username;
 
       if (copy_and_check_auth(&user_combo, &user_combo, thd) ||
-          replace_user_table(thd, tables.user_table(), &user_combo, NO_ACL,
-                             false, create_new_user,
-                             no_auto_create_user))
+          replace_user_table(thd, tables.user_table(), &user_combo,
+                             NO_ACL, false,
+                             create_new_user, no_auto_create_user))
       {
         append_user(thd, &wrong_users, &username, &hostname);
         result= 1;
@@ -8529,7 +8531,7 @@ bool mysql_grant_role(THD *thd, List <LEX_USER> &list, bool revoke)
 }
 
 
-bool mysql_grant(THD *thd, const char *db, List <LEX_USER> &list,
+bool mysql_grant(THD *thd, const char *db, List<LEX_USER> &list,
                  privilege_t rights, bool revoke_grant, bool is_proxy)
 {
   List_iterator <LEX_USER> str_list (list);
@@ -8589,8 +8591,8 @@ bool mysql_grant(THD *thd, const char *db, List <LEX_USER> &list,
 
     if (copy_and_check_auth(Str, tmp_Str, thd) ||
         replace_user_table(thd, tables.user_table(), Str,
-                           (!db ? rights : NO_ACL),
-                           revoke_grant, create_new_users,
+                           (!db ? rights : NO_ACL), revoke_grant,
+                           create_new_users,
                            MY_TEST(thd->variables.sql_mode &
                                    MODE_NO_AUTO_CREATE_USER)))
       result= true;
@@ -11760,7 +11762,8 @@ bool mysql_create_user(THD *thd, List <LEX_USER> &list, bool handle_as_role)
     }
 
     if (replace_user_table(thd, tables.user_table(), user_name,
-                           NO_ACL, 0, 1, 0))
+                           NO_ACL, false,
+                           true, false))
     {
       append_user(thd, &wrong_users, user_name);
       result= TRUE;
@@ -12063,8 +12066,9 @@ int mysql_alter_user(THD* thd, List<LEX_USER> &users_list)
   {
     LEX_USER* lex_user= get_current_user(thd, tmp_lex_user, false);
     if (!lex_user ||
-        replace_user_table(thd, tables.user_table(), lex_user, NO_ACL,
-                           false, false, true))
+        replace_user_table(thd, tables.user_table(), lex_user,
+                           NO_ACL, false,
+                           false, true))
     {
       thd->clear_error();
       append_user(thd, &wrong_users, tmp_lex_user);
@@ -12192,7 +12196,8 @@ bool mysql_revoke_all(THD *thd,  List <LEX_USER> &list)
     }
 
     if (replace_user_table(thd, tables.user_table(), lex_user,
-                           ALL_KNOWN_ACL, 1, 0, 0))
+                           ALL_KNOWN_ACL, true,
+                           false, false))
     {
       result= -1;
       continue;
