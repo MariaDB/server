9859ce04a38af206d6fa0ad552f7a5708e8bdce6
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2021-11-24 23:39:52 +0200

    refactor: cache get_current_user result for Sql_cmd_grant
    There are a number of get_current_user calls which can cause a malloc.
    Reorganize code within mysql_grant_xxx such that it assumes LEX_USERS
    are already resolved.
    
    This eliminates a number of mallocs and also simplifies code in
    mysql_grant_xxx functions.
    
    grant_stage0 now resolves lex_users, places it in a list copy,
    copies the clone's auth structures. Sql_cmd_grant_*::execute now
    uses the list for all subsequent mysql_grant_xxx functions calls.

--- sql/sql_acl.h
+++ sql/sql_acl.h
@@ -321,8 +321,6 @@ get_cached_table_access(GRANT_INTERNAL_INFO *grant_internal_info,
                         const char *schema_name,
                         const char *table_name);
 
-bool acl_check_proxy_grant_access (THD *thd, const char *host, const char *user,
-                                   bool with_grant);
 int acl_setrole(THD *thd, const char *rolename, privilege_t access);
 int acl_check_setrole(THD *thd, const char *rolename, privilege_t *access);
 int acl_check_set_default_role(THD *thd, const char *host, const char *user,
@@ -350,10 +348,11 @@ class Sql_cmd_grant: public Sql_cmd
 {
 protected:
   enum_sql_command m_command;
+  List<LEX_USER> m_resolved_users;
 #ifndef NO_EMBEDDED_ACCESS_CHECKS
   void warn_hostname_requires_resolving(THD *thd, List<LEX_USER> &list);
-  bool user_list_reset_mqh(THD *thd, List<LEX_USER> &list);
-  void grant_stage0(THD *thd);
+  void user_list_reset_mqh();
+  bool grant_stage0(THD *thd);
 #endif
 public:
   Sql_cmd_grant(enum_sql_command command)
