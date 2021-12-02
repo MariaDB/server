bc90f5d6967b24b901b47228e8e9cb5e3cd394d6
Alexander Barkov <bar@mariadb.com>
2021-12-03 02:55:34 +0400

    MDEV-23570 deprecate keep_files_on_create

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -4902,7 +4902,9 @@ static Sys_var_mybool Sys_keep_files_on_create(
        "keep_files_on_create",
        "Don't overwrite stale .MYD and .MYI even if no directory is specified",
        SESSION_VAR(keep_files_on_create), CMD_LINE(OPT_ARG),
-       DEFAULT(FALSE));
+       DEFAULT(FALSE),
+       NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0), ON_UPDATE(0),
+       DEPRECATED("")); // since 10.8.0
 
 static char *license;
 static Sys_var_charptr Sys_license(
