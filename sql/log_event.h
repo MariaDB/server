20aae939c335336c446899fd5c42d0047238d24a
Nikita Malyavin <nikitamalyavin@gmail.com>
2021-12-12 11:41:14 +0300

    MDEV-16329 [4/5] Refactor MYSQL_BIN_LOG: extract Event_log ancestor
    Event_log is supposed to be a basic logging class that can write events in
    a single file.
    
    MYSQL_BIN_LOG in comparison will have:
    * rotation support
    * index files
    * purging
    * gtid and transactional information handling.
    * is dedicated for a general-purpose binlog

--- sql/log_event.h
+++ sql/log_event.h
@@ -4917,7 +4917,7 @@ public:
 #endif
 #if defined(MYSQL_SERVER) 
   static bool binlog_row_logging_function(THD *thd, TABLE *table,
-                                          MYSQL_BIN_LOG *bin_log,
+                                          Event_log *bin_log,
                                           binlog_cache_data *cache_data,
                                           bool is_transactional,
                                           const uchar *before_record
@@ -5003,7 +5003,7 @@ public:
 
 #ifdef MYSQL_SERVER
   static bool binlog_row_logging_function(THD *thd, TABLE *table,
-                                          MYSQL_BIN_LOG *bin_log,
+                                          Event_log *bin_log,
                                           binlog_cache_data *cache_data,
                                           bool is_transactional,
                                           const uchar *before_record,
@@ -5094,7 +5094,7 @@ public:
 #endif
 #ifdef MYSQL_SERVER
   static bool binlog_row_logging_function(THD *thd, TABLE *table,
-                                          MYSQL_BIN_LOG *bin_log,
+                                          Event_log *bin_log,
                                           binlog_cache_data *cache_data,
                                           bool is_transactional,
                                           const uchar *before_record,
