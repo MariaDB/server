28e166d6435741c46e0ea789657b739f78eb0425
Thirunarayanan Balathandayuthapani <thiru@mariadb.com>
2022-01-20 16:30:18 +0530

    MDEV-26784 [Warning] InnoDB: Difficult to find free blocks in the buffer pool
    Problem:
    =======
      InnoDB ran out of memory during recovery and it fails to
    flush the dirty LRU blocks. The reason is that buffer pool
    can ran out before the LRU list length reaches
    BUF_LRU_OLD_MIN_LEN(256) threshold.
    
    Fix:
    ====
    During recovery, InnoDB should write out and evict all
    dirty blocks.

--- storage/innobase/buf/buf0flu.cc
+++ storage/innobase/buf/buf0flu.cc
@@ -1288,9 +1288,11 @@ static void buf_flush_LRU_list_batch(ulint max, flush_counters_t *n)
   static_assert(FIL_NULL > SRV_SPACE_ID_UPPER_BOUND, "consistency");
 
   for (buf_page_t *bpage= UT_LIST_GET_LAST(buf_pool.LRU);
-       bpage && n->flushed + n->evicted < max &&
-       UT_LIST_GET_LEN(buf_pool.LRU) > BUF_LRU_MIN_LEN &&
-       UT_LIST_GET_LEN(buf_pool.free) < free_limit; ++scanned)
+       bpage &&
+       ((UT_LIST_GET_LEN(buf_pool.LRU) > BUF_LRU_MIN_LEN &&
+         UT_LIST_GET_LEN(buf_pool.free) < free_limit &&
+         n->flushed + n->evicted < max) ||
+        recv_recovery_is_on()); ++scanned)
   {
     buf_page_t *prev= UT_LIST_GET_PREV(LRU, bpage);
     const lsn_t oldest_modification= bpage->oldest_modification();
