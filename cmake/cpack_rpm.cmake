8d55113f8aa0e7dc639562adc7dd6bd68e36d8b1
Jan Lindström <jan.lindstrom@mariadb.com>
2022-02-10 08:51:48 +0200

    Small fixes   Add new requirement for pv tool for debian and rpm   Fix one test result difference.   Set message about missing progress reporting tool pv as info.

--- cmake/cpack_rpm.cmake
+++ cmake/cpack_rpm.cmake
@@ -225,7 +225,7 @@ SETA(CPACK_RPM_server_PACKAGE_REQUIRES
 IF(WITH_WSREP)
   SETA(CPACK_RPM_server_PACKAGE_REQUIRES
     "galera-4" "rsync" "lsof" "grep" "gawk" "iproute"
-    "coreutils" "findutils" "tar")
+    "coreutils" "findutils" "tar" "pv")
 ENDIF()
 
 SET(CPACK_RPM_server_PRE_INSTALL_SCRIPT_FILE ${CMAKE_SOURCE_DIR}/support-files/rpm/server-prein.sh)
