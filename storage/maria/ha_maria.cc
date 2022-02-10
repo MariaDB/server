9e39d0ae44595dbd1570805d97c9c874778a6be8
Sergei Golubchik <serg@mariadb.org>
2022-02-10 11:27:14 +0100

    MDEV-25787 Bug report: crash on SELECT DISTINCT thousands_blob_fields
    fix a debug assert to account for not opened temp tables

--- storage/maria/ha_maria.cc
+++ storage/maria/ha_maria.cc
@@ -2789,7 +2789,7 @@ int ha_maria::delete_table(const char *name)
 
 void ha_maria::drop_table(const char *name)
 {
-  DBUG_ASSERT(file->s->temporary);
+  DBUG_ASSERT(!file || file->s->temporary);
   (void) ha_close();
   (void) maria_delete_table_files(name, 1, MY_WME);
 }
