MariaDB/server

commit 0d44792a835128a83ff07f14dbbcdd621df9f7da

perfschema: native type for my_thread_os_id_t
Though these will all get case to unsigned long
long where it is populated into the perfschema's BIGINT
type.

Use uintptr_t for NetBSD per Nia Alarie's original #1836.

storage/perfschema/my_thread.h
@@ -17,7 +17,17 @@
 typedef pthread_key_t thread_local_key_t;
 typedef pthread_t my_thread_handle;
 typedef pthread_attr_t my_thread_attr_t;
+#if defined(HAVE_PTHREAD_THREADID_NP) || defined(HAVE_GETTID) || defined(HAVE_SYS_GETTID) || defined(HAVE_GETTHRID)
+typedef pid_t my_thread_os_id_t;
+#elif defined(_WIN32)
+typedef uint32 my_thread_os_id_t;
+#elif defined(HAVE_PTHREAD_GETTHREADID_NP)
+typedef int my_thread_os_id_t;
+#elif defined(HAVE_INTEGER_PTHREAD_SELF)
+typedef uintptr_t my_thread_os_id_t;
+#else
 typedef unsigned long long my_thread_os_id_t;
+#endif
 
 #define LOCK_plugin_delete LOCK_plugin
 
@@ -74,8 +84,8 @@ static inline my_thread_os_id_t my_thread_os_id()
   return getthrid();
 #else
 #ifdef HAVE_INTEGER_PTHREAD_SELF
-  /* Unknown platform, fallback. */
-  return (unsigned long long)pthread_self();
+  /* NetBSD, and perhaps something else, fallback. */
+  return (my_thread_os_id_t) pthread_self();
 #else
   /* Feature not available. */
   return 0;

commit a01a4f5ab7518999090d612ae06075fb8f78b487

MDEV-27804 Fails to build - perf schema - thread id of type uintptr_t requires header
While building on GNU/Hurd and kfreebsd.

On the C++ standard uintptr_t can be defined in <cstdint>
ref: https://www.cplusplus.com/reference/cstdint/

Fixes: 0d44792a835128a83ff07f14dbbcdd621df9f7da

storage/perfschema/my_thread.h
@@ -14,6 +14,10 @@
 #include <pthread_np.h>
 #endif
 
+#if defined(HAVE_INTEGER_PTHREAD_SELF)
+#include <cstdint>
+#endif
+
 typedef pthread_key_t thread_local_key_t;
 typedef pthread_t my_thread_handle;
 typedef pthread_attr_t my_thread_attr_t;