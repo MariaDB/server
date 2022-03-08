114476f2ec8244bd02b0f51fdaca05e055a2c1dc
Haidong Ji <ji@haidongji.com>
2022-03-07 21:17:39 -0600

    MDEV-27978 fix wrong name in error when max_session_mem_used exceeded
    Fixed typo in my_malloc_size_cb_func. There is no max-thread-mem-used
    sys variable in MariaDB, only max-session-mem-used. The relevant entry
    in sys_vars.cc is also fixed.
    
    Added a fallback case in case we could allocate the 256 bytes for the
    error message containing the exact setting.

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -5719,7 +5719,7 @@ static Sys_var_ulong Sys_log_tc_size(
        BLOCK_SIZE(my_getpagesize()));
 #endif
 
-static Sys_var_ulonglong Sys_max_thread_mem(
+static Sys_var_ulonglong Sys_max_session_mem_used(
        "max_session_mem_used", "Amount of memory a single user session "
        "is allowed to allocate. This limits the value of the "
        "session variable MEM_USED", SESSION_VAR(max_mem_used),
