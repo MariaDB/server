e4cda7bf2212ae153cfbfa5c30b7827ca88495c5
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2022-03-19 16:49:08 +0200

    Fix centos-7

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -151,14 +151,12 @@ public:
 
   Priv_spec(privilege_t access, bool revoke, bool deny=false):
     revoke{revoke}, deny{deny}, access{access}, spec_type{PRIV_TYPE::GLOBAL_PRIV},
-    db({nullptr, 0}), table({nullptr, 0}), column({nullptr, 0}),
-    routine({nullptr, 0})
+    db{}, table{}, column{}, routine{}
   { }
 
   Priv_spec(privilege_t access, bool revoke, const LEX_CSTRING &db, bool deny=false):
     revoke{revoke}, deny{deny}, access{access},
-    spec_type{PRIV_TYPE::DATABASE_PRIV}, db{db},
-    table({nullptr, 0}), column({nullptr, 0}), routine({nullptr, 0})
+    spec_type{PRIV_TYPE::DATABASE_PRIV}, db{db}, table{}, column{}, routine{}
   { /* TODO(cvicentiu) hack */
      if (!db.str)
        spec_type= PRIV_TYPE::GLOBAL_PRIV;
