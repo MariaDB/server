421153848f0002c9e9b5e204d4c6e85dfcfb5600
Sergei Golubchik <serg@mariadb.org>
2022-03-20 21:36:41 +0100

    MDEV-27980 file-key-management plugin disabled in mysql_install_db breaks automated deployments (and container initialization)
    Revert "Silence the file-key-management plugin during mysql_install_db"
    
    This reverts commit e99d3da6381023395c86f679bb76b00b4385dc2d.

--- scripts/mysql_install_db.sh
+++ scripts/mysql_install_db.sh
@@ -506,7 +506,7 @@ mysqld_install_cmd_line()
 {
   "$mysqld_bootstrap" $defaults $defaults_group_suffix "$mysqld_opt" --bootstrap $silent_startup\
   "--basedir=$basedir" "--datadir=$ldata" --log-warnings=0 --enforce-storage-engine="" \
-  "--plugin-dir=${plugindir}" --loose-disable-plugin-file-key-management \
+  "--plugin-dir=${plugindir}" \
   $args --max_allowed_packet=8M \
   --net_buffer_length=16K
 }
