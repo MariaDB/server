f54d6380d2662d7bc7f173bf96f5dc3d7cf3aec1
Sergei Golubchik <serg@mariadb.org>
2022-03-21 11:01:40 +0100

    MDEV-27980 file-key-management plugin disabled in mysql_install_db breaks automated deployments (and container initialization)
    fix a 2015 typo in build scripts.
    
    --without-plugin=plugin_file_key_management translates to
    -DPLUGIN_PLUGIN_FILE_KEY_MANAGEMENT=NO
    
    replace it with a line from 10.4 that builds the plugin
    dynamically.

--- BUILD/SETUP.sh
+++ BUILD/SETUP.sh
@@ -211,7 +211,7 @@ fi
 
 max_no_embedded_configs="$SSL_LIBRARY --with-plugins=max"
 max_no_qc_configs="$SSL_LIBRARY --with-plugins=max --without-query-cache"
-max_configs="$SSL_LIBRARY --with-plugins=max --with-embedded-server --with-libevent --without-plugin=plugin_file_key_management --with-plugin-rocksdb=dynamic --with-plugin-test_sql_discovery=DYNAMIC"
+max_configs="$SSL_LIBRARY --with-plugins=max --with-embedded-server --with-libevent --with-plugin-rocksdb=dynamic --with-plugin-test_sql_discovery=DYNAMIC --with-plugin-file_key_management=DYNAMIC"
 all_configs="$SSL_LIBRARY --with-plugins=max --with-embedded-server --with-innodb_plugin --with-libevent"
 
 #
