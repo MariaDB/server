44231dc6d5c7dfdf0f1e6a09c84a8ef6a960f7d7
Marko Mäkelä <marko.makela@mariadb.com>
2022-03-23 16:41:58 +0200

    Cleanup: have_sanitizer='ASAN,UBSAN'
    This was suggested by Sergei Golubhick.
    Fixes up commit b91a123d8c9c321d54256d4ba055857c9505632b

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -5082,7 +5082,7 @@ static Sys_var_have Sys_have_symlink(
 
 # ifdef __SANITIZE_ADDRESS__
 #  ifdef WITH_UBSAN
-#   define SANITIZER_MODE "ASAN+UBSAN"
+#   define SANITIZER_MODE "ASAN,UBSAN"
 #  else
 #   define SANITIZER_MODE "ASAN"
 #  endif
