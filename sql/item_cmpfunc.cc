e048289e557315b068a15083267329c443faadd3
Igor Babaev <igor@askmonty.org>
2022-03-25 11:04:56 -0700

    MDEV-27937 Assertion failure when executing prepared statement with ? in IN list
    This bug affected queries with IN predicates that contain parameter markers
    in the value list. Such queries are executed via prepared statements.
    The problem appeared only if the number of elements in the value list
    was greater than the set value of the system variable
    in_predicate_conversion_threshold.
    
    The patch unconditionally prohibits conversion of an IN predicate to the
    equivalent IN predicand if the value list of the IN predicate contains
    parameters markers.
    
    Approved by Oleksandr Byelkin <sanja@mariadb.com>

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -4472,10 +4472,11 @@ void Item_func_in::mark_as_condition_AND_part(TABLE_LIST *embedding)
   Query_arena *arena, backup;
   arena= thd->activate_stmt_arena_if_needed(&backup);
 
-  if (to_be_transformed_into_in_subq(thd))
+  if (!transform_into_subq_checked)
   {
-    transform_into_subq= true;
-    thd->lex->current_select->in_funcs.push_back(this, thd->mem_root);
+    if ((transform_into_subq= to_be_transformed_into_in_subq(thd)))
+      thd->lex->current_select->in_funcs.push_back(this, thd->mem_root);
+    transform_into_subq_checked= true;
   }
 
   if (arena)
