da1540d4b7f552c124cf4e1f4036d32788e38c37
Monty <monty@mariadb.org>
2022-04-05 20:12:29 +0300

    Make the most important optimizer constants user variables
    Variables added:
    - optimizer_index_block_copy_cost
    - optimizer_key_copy_cost
    - optimizer_key_next_find_cost
    - optimizer_key_compare_cost
    - optimizer_row_copy_cost
    - optimizer_where_compare_cost
    
    Some rename of defines was done to make the internal defines similar to
    the visible ones:
    TIME_FOR_COMPARE -> WHERE_COST; WHERE_COST was also "inverted" to be
    a number between 0 and 1 that is multiply with accepted records
    (similar to other optimizer variables).
    TIME_FOR_COMPARE_IDX -> KEY_COMPARE_COST. This is also inverted,
    similar to TIME_FOR_COMPARE.
    TIME_FOR_COMPARE_ROWID -> ROWID_COMPARE_COST. This is also inverted,
    similar to TIME_FOR_COMPARE.
    
    All default costs are identical to what they where before this patch.
    
    Other things:
    - Compare factor in get_merge_buffers_cost() was inverted.
    - Changed namespace to static in filesort_utils.cc

--- sql/multi_range_read.cc
+++ sql/multi_range_read.cc
@@ -343,7 +343,7 @@ handler::multi_range_read_info_const(uint keyno, RANGE_SEQ_IF *seq,
       cost->cpu_cost= ha_read_time(keyno, 0, total_rows);
       cost->copy_cost= rows2double(total_rows) * ROW_COPY_COST;
     }
-    cost->comp_cost= (rows2double(total_rows) / TIME_FOR_COMPARE +
+    cost->comp_cost= (rows2double(total_rows) * WHERE_COST +
                       MULTI_RANGE_READ_SETUP_COST);
   }
   DBUG_PRINT("statistics",
@@ -427,7 +427,8 @@ ha_rows handler::multi_range_read_info(uint keyno, uint n_ranges, uint n_rows,
     else
     {
       /*
-        Same as above, but take into account copying the key to the upper level.
+        Same as above, but take into account copying the key to the upper
+        level.
       */
       cost->idx_cpu_cost= (keyread_time(keyno, 1, n_rows) +
                            (n_ranges-1) * range_lookup_cost);
@@ -440,7 +441,7 @@ ha_rows handler::multi_range_read_info(uint keyno, uint n_ranges, uint n_rows,
     cost->cpu_cost= read_time(keyno, n_ranges, n_rows);
     cost->copy_cost= rows2double(n_rows) * ROW_COPY_COST;
   }
-  cost->comp_cost= rows2double(n_rows) / TIME_FOR_COMPARE;
+  cost->comp_cost= rows2double(n_rows) * WHERE_COST;
   return 0;
 }
 
@@ -2067,7 +2068,7 @@ void get_sort_and_sweep_cost(TABLE *table, ha_rows nrows, Cost_estimate *cost)
   {
     get_sweep_read_cost(table, nrows, FALSE, cost);
     /* Add cost of qsort call: n * log2(n) * cost(rowid_comparison) */
-    double cmp_op= rows2double(nrows) * (1.0 / TIME_FOR_COMPARE_ROWID);
+    double cmp_op= rows2double(nrows) * ROWID_COMPARE_COST_THD(table->in_use);
     if (cmp_op < 3)
       cmp_op= 3;
     cost->cpu_cost += cmp_op * log2(cmp_op);
