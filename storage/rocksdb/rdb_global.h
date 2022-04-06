tmax-cloud/opensql-mariadb

Make Rocksdb error message independent from engine messages in my_base.h
- Rocksdb unique error messages move to a range starting at 300
- Use same error code for Rocksdb error messages that exists in my_base.h

storage/rocksdb/rdb_global.h
@@ -235,11 +235,25 @@ const char *const RDB_TTL_COL_QUALIFIER = "ttl_col";
 #define MAX_INDEX_COL_LEN_SMALL 767
 
 /*
-  MyRocks specific error codes. NB! Please make sure that you will update
-  HA_ERR_ROCKSDB_LAST when adding new ones.  Also update the strings in
-  rdb_error_messages to include any new error messages.
+  MyRocks specific error codes are in a range of 300-400 to not
+  clash with other errors. (Default errors are from 120 and up)
 */
-#define HA_ERR_ROCKSDB_FIRST (HA_ERR_LAST + 1)
+#define HA_ERR_ROCKSDB_FIRST (300)
+#if HA_ERR_LAST >= HA_ERR_ROCKSDB_FIRST
+#error "Rocksdb error code overlapps default engine codes from my_base.h"
+#endif
+
+/* Rocksdb errors mapped to MariaDB errors */
+#define HA_ERR_ROCKSDB_STATUS_NOT_FOUND HA_ERR_KEY_NOT_FOUND
+#define HA_ERR_ROCKSDB_STATUS_CORRUPTION HA_ERR_WRONG_IN_RECORD
+#define HA_ERR_ROCKSDB_STATUS_NOT_SUPPORTED HA_ERR_WRONG_COMMAND
+#define HA_ERR_ROCKSDB_STATUS_NO_SPACE HA_ERR_RECORD_FILE_FULL
+#define HA_ERR_ROCKSDB_STATUS_TIMED_OUT HA_ERR_LOCK_WAIT_TIMEOUT
+#define HA_ERR_ROCKSDB_STATUS_LOCK_LIMIT HA_ERR_LOCK_TABLE_FULL
+#define HA_ERR_ROCKSDB_STATUS_DEADLOCK HA_ERR_LOCK_DEADLOCK
+
+/* Unique Rocksb errors */
+
 #define HA_ERR_ROCKSDB_PK_REQUIRED (HA_ERR_ROCKSDB_FIRST + 0)
 #define HA_ERR_ROCKSDB_TABLE_DATA_DIRECTORY_NOT_SUPPORTED \
   (HA_ERR_ROCKSDB_FIRST + 1)
@@ -256,22 +270,16 @@ const char *const RDB_TTL_COL_QUALIFIER = "ttl_col";
   Each error code below maps to a RocksDB status code found in:
   rocksdb/include/rocksdb/status.h
 */
-#define HA_ERR_ROCKSDB_STATUS_NOT_FOUND (HA_ERR_LAST + 10)
-#define HA_ERR_ROCKSDB_STATUS_CORRUPTION (HA_ERR_LAST + 11)
-#define HA_ERR_ROCKSDB_STATUS_NOT_SUPPORTED (HA_ERR_LAST + 12)
-#define HA_ERR_ROCKSDB_STATUS_INVALID_ARGUMENT (HA_ERR_LAST + 13)
-#define HA_ERR_ROCKSDB_STATUS_IO_ERROR (HA_ERR_LAST + 14)
-#define HA_ERR_ROCKSDB_STATUS_NO_SPACE (HA_ERR_LAST + 15)
-#define HA_ERR_ROCKSDB_STATUS_MERGE_IN_PROGRESS (HA_ERR_LAST + 16)
-#define HA_ERR_ROCKSDB_STATUS_INCOMPLETE (HA_ERR_LAST + 17)
-#define HA_ERR_ROCKSDB_STATUS_SHUTDOWN_IN_PROGRESS (HA_ERR_LAST + 18)
-#define HA_ERR_ROCKSDB_STATUS_TIMED_OUT (HA_ERR_LAST + 19)
-#define HA_ERR_ROCKSDB_STATUS_ABORTED (HA_ERR_LAST + 20)
-#define HA_ERR_ROCKSDB_STATUS_LOCK_LIMIT (HA_ERR_LAST + 21)
-#define HA_ERR_ROCKSDB_STATUS_BUSY (HA_ERR_LAST + 22)
-#define HA_ERR_ROCKSDB_STATUS_DEADLOCK (HA_ERR_LAST + 23)
-#define HA_ERR_ROCKSDB_STATUS_EXPIRED (HA_ERR_LAST + 24)
-#define HA_ERR_ROCKSDB_STATUS_TRY_AGAIN (HA_ERR_LAST + 25)
+
+#define HA_ERR_ROCKSDB_STATUS_INVALID_ARGUMENT (HA_ERR_ROCKSDB_FIRST + 10)
+#define HA_ERR_ROCKSDB_STATUS_IO_ERROR (HA_ERR_ROCKSDB_FIRST + 11)
+#define HA_ERR_ROCKSDB_STATUS_MERGE_IN_PROGRESS (HA_ERR_ROCKSDB_FIRST + 12)
+#define HA_ERR_ROCKSDB_STATUS_INCOMPLETE (HA_ERR_ROCKSDB_FIRST + 13)
+#define HA_ERR_ROCKSDB_STATUS_SHUTDOWN_IN_PROGRESS (HA_ERR_ROCKSDB_FIRST + 14)
+#define HA_ERR_ROCKSDB_STATUS_ABORTED (HA_ERR_ROCKSDB_FIRST + 15)
+#define HA_ERR_ROCKSDB_STATUS_BUSY (HA_ERR_ROCKSDB_FIRST + 16)
+#define HA_ERR_ROCKSDB_STATUS_EXPIRED (HA_ERR_ROCKSDB_FIRST + 17)
+#define HA_ERR_ROCKSDB_STATUS_TRY_AGAIN (HA_ERR_ROCKSDB_FIRST + 18)
 #define HA_ERR_ROCKSDB_LAST HA_ERR_ROCKSDB_STATUS_TRY_AGAIN
 
 const char *const rocksdb_hton_name = "ROCKSDB";

MariaDB/server

storage/rocksdb/rdb_global.h
@@ -235,11 +235,25 @@ const char *const RDB_TTL_COL_QUALIFIER = "ttl_col";
 #define MAX_INDEX_COL_LEN_SMALL 767
 
 /*
-  MyRocks specific error codes. NB! Please make sure that you will update
-  HA_ERR_ROCKSDB_LAST when adding new ones.  Also update the strings in
-  rdb_error_messages to include any new error messages.
+  MyRocks specific error codes are in a range of 300-400 to not
+  clash with other errors. (Default errors are from 120 and up)
 */
-#define HA_ERR_ROCKSDB_FIRST (HA_ERR_LAST + 1)
+#define HA_ERR_ROCKSDB_FIRST (300)
+#if HA_ERR_LAST >= HA_ERR_ROCKSDB_FIRST
+#error "Rocksdb error code overlapps default engine codes from my_base.h"
+#endif
+
+/* Rocksdb errors mapped to MariaDB errors */
+#define HA_ERR_ROCKSDB_STATUS_NOT_FOUND HA_ERR_KEY_NOT_FOUND
+#define HA_ERR_ROCKSDB_STATUS_CORRUPTION HA_ERR_WRONG_IN_RECORD
+#define HA_ERR_ROCKSDB_STATUS_NOT_SUPPORTED HA_ERR_UNSUPPORTED
+#define HA_ERR_ROCKSDB_STATUS_NO_SPACE HA_ERR_RECORD_FILE_FULL
+#define HA_ERR_ROCKSDB_STATUS_TIMED_OUT HA_ERR_LOCK_WAIT_TIMEOUT
+#define HA_ERR_ROCKSDB_STATUS_LOCK_LIMIT HA_ERR_LOCK_TABLE_FULL
+#define HA_ERR_ROCKSDB_STATUS_DEADLOCK HA_ERR_LOCK_DEADLOCK
+
+/* Unique Rocksb errors */
+
 #define HA_ERR_ROCKSDB_PK_REQUIRED (HA_ERR_ROCKSDB_FIRST + 0)
 #define HA_ERR_ROCKSDB_TABLE_DATA_DIRECTORY_NOT_SUPPORTED \
   (HA_ERR_ROCKSDB_FIRST + 1)
@@ -256,22 +270,16 @@ const char *const RDB_TTL_COL_QUALIFIER = "ttl_col";
   Each error code below maps to a RocksDB status code found in:
   rocksdb/include/rocksdb/status.h
 */
-#define HA_ERR_ROCKSDB_STATUS_NOT_FOUND (HA_ERR_LAST + 10)
-#define HA_ERR_ROCKSDB_STATUS_CORRUPTION (HA_ERR_LAST + 11)
-#define HA_ERR_ROCKSDB_STATUS_NOT_SUPPORTED (HA_ERR_LAST + 12)
-#define HA_ERR_ROCKSDB_STATUS_INVALID_ARGUMENT (HA_ERR_LAST + 13)
-#define HA_ERR_ROCKSDB_STATUS_IO_ERROR (HA_ERR_LAST + 14)
-#define HA_ERR_ROCKSDB_STATUS_NO_SPACE (HA_ERR_LAST + 15)
-#define HA_ERR_ROCKSDB_STATUS_MERGE_IN_PROGRESS (HA_ERR_LAST + 16)
-#define HA_ERR_ROCKSDB_STATUS_INCOMPLETE (HA_ERR_LAST + 17)
-#define HA_ERR_ROCKSDB_STATUS_SHUTDOWN_IN_PROGRESS (HA_ERR_LAST + 18)
-#define HA_ERR_ROCKSDB_STATUS_TIMED_OUT (HA_ERR_LAST + 19)
-#define HA_ERR_ROCKSDB_STATUS_ABORTED (HA_ERR_LAST + 20)
-#define HA_ERR_ROCKSDB_STATUS_LOCK_LIMIT (HA_ERR_LAST + 21)
-#define HA_ERR_ROCKSDB_STATUS_BUSY (HA_ERR_LAST + 22)
-#define HA_ERR_ROCKSDB_STATUS_DEADLOCK (HA_ERR_LAST + 23)
-#define HA_ERR_ROCKSDB_STATUS_EXPIRED (HA_ERR_LAST + 24)
-#define HA_ERR_ROCKSDB_STATUS_TRY_AGAIN (HA_ERR_LAST + 25)
+
+#define HA_ERR_ROCKSDB_STATUS_INVALID_ARGUMENT (HA_ERR_ROCKSDB_FIRST + 10)
+#define HA_ERR_ROCKSDB_STATUS_IO_ERROR (HA_ERR_ROCKSDB_FIRST + 11)
+#define HA_ERR_ROCKSDB_STATUS_MERGE_IN_PROGRESS (HA_ERR_ROCKSDB_FIRST + 12)
+#define HA_ERR_ROCKSDB_STATUS_INCOMPLETE (HA_ERR_ROCKSDB_FIRST + 13)
+#define HA_ERR_ROCKSDB_STATUS_SHUTDOWN_IN_PROGRESS (HA_ERR_ROCKSDB_FIRST + 14)
+#define HA_ERR_ROCKSDB_STATUS_ABORTED (HA_ERR_ROCKSDB_FIRST + 15)
+#define HA_ERR_ROCKSDB_STATUS_BUSY (HA_ERR_ROCKSDB_FIRST + 16)
+#define HA_ERR_ROCKSDB_STATUS_EXPIRED (HA_ERR_ROCKSDB_FIRST + 17)
+#define HA_ERR_ROCKSDB_STATUS_TRY_AGAIN (HA_ERR_ROCKSDB_FIRST + 18)
 #define HA_ERR_ROCKSDB_LAST HA_ERR_ROCKSDB_STATUS_TRY_AGAIN
 
 const char *const rocksdb_hton_name = "ROCKSDB";