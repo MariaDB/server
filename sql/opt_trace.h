9b907daf4557d13b83e6afb37b1ffbd2fe69b143
Monty <monty@mariadb.org>
2022-04-11 17:59:36 +0300

    Added 'records_out' and join_type to POSITION
    records_out is the numbers of rows expected to be accepted from a table.
    records_read is in contrast the number of rows that the optimizer excepts
    to read from the engine.
    
    This patch causes not plan changes. The differences in test results comes
    from renaming "records" to "records_read" and printing of record_out in
    the optimizer trace.
    
    Other things:
    - Renamed table_cond_selectivity() to table_after_join_selectivity()
      to make the purpose of the function more clear.

--- sql/opt_trace.h
+++ sql/opt_trace.h
@@ -109,8 +109,7 @@ void opt_trace_print_expanded_query(THD *thd, SELECT_LEX *select_lex,
 void add_table_scan_values_to_trace(THD *thd, JOIN_TAB *tab);
 void trace_plan_prefix(JOIN *join, uint idx, table_map join_tables);
 void print_final_join_order(JOIN *join);
-void print_best_access_for_table(THD *thd, POSITION *pos,
-                                 enum join_type type);
+void print_best_access_for_table(THD *thd, POSITION *pos);
 
 void trace_condition(THD * thd, const char *name, const char *transform_type,
                     Item *item, const char *table_name= nullptr);
