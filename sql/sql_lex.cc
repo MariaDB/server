8641e3169c18a9234da81b02c98c41ca2c889de3
Monty <monty@mariadb.org>
2022-05-04 17:26:43 +0300

    Derived tables and union can now create distinct keys
    The idea is that instead of marking all select_lex's with DISTINCT, we
    only mark those that really need distinct result.
    
    Benefits of this change:
    - Temporary tables used with derived tables, UNION, IN are now smaller
      as duplicates are removed already on the insert phase.
    - The optimizer can now produce better plans with EQ_REF. This can be
      seen from the tests where several queries does not anymore materialize
      derived tables twice.
    - Queries affected by 'in_predicate_conversion_threshold' where large IN
      lists are converted to sub query produces better plans.
    
    Other things:
    - Removed on duplicate call to sel->init_select() in
      LEX::add_primary_to_query_expression_body()
    - I moved the testing of
      tab->table->pos_in_table_list->is_materialized_derived()
      in join_read_const_table() to the caller as it caused problems for
      derived tables that could be proven to be const tables.
      This also is likely to fix some bugs as if join_read_const_table()
      was aborted, the table was left marked as JT_CONST, which cannot
      be good.  I added an ASSERT there for now that can be removed when
      the code has been properly tested.

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -2873,7 +2873,7 @@ void st_select_lex_node::init_query_common()
 {
   options= 0;
   set_linkage(UNSPECIFIED_TYPE);
-  distinct= TRUE;
+  distinct= FALSE;
   no_table_names_allowed= 0;
   uncacheable= 0;
 }
@@ -10144,6 +10144,7 @@ SELECT_LEX_UNIT *LEX::parsed_select_expr_start(SELECT_LEX *s1, SELECT_LEX *s2,
   if (res == NULL)
     return NULL;
   res->pre_last_parse= sel1;
+  res->distinct= distinct;
   push_select(res->fake_select_lex);
   return res;
 }
@@ -10390,7 +10391,6 @@ bool LEX::parsed_TVC_start()
   insert_list= 0;
   if (!(sel= alloc_select(TRUE)) || push_select(sel))
     return true;
-  sel->init_select();
   sel->braces= FALSE; // just initialisation
   return false;
 }
