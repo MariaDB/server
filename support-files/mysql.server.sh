MariaDB/server

commit 467011bcac3b3f42ae6f21dde8d88e78708b21d1

MDEV-26612 Two different ways to start MariaDB service can cause data corruption
RedHat systems have both files for lsb and init functions.
Old code was written as if/else, so second file (RedHat-specific) was not processed.
So, systemd redirect didn't work, because its logic is described in
RedHat-specific functions file

support-files/mysql.server.sh
@@ -91,16 +91,15 @@ datadir_set=
 
 #
 # Use LSB init script functions for printing messages, if possible
-#
+# Include non-LSB RedHat init functions to make systemctl redirect work
+init_functions="/etc/init.d/functions"
 lsb_functions="/lib/lsb/init-functions"
-if test -f $lsb_functions ; then
+if test -f $lsb_functions; then
   . $lsb_functions
-else
-  # Include non-LSB RedHat init functions to make systemctl redirect work
-  init_functions="/etc/init.d/functions"
-  if test -f $init_functions; then
-    . $init_functions
-  fi
+fi
+
+if test -f $init_functions; then
+  . $init_functions
   log_success_msg()
   {
     echo " SUCCESS! $@"

commit 5e3c5e2d604f5e060aa31dd05871a2daf84e85f8

mysql.server.sh fix for non-Red Hat platforms
The else condition is meant to be here to define the functions
if the Red Hat include file isn't there.

Fixes: commit 467011bcac3b3f42ae6f21dde8d88e78708b21d1 / MDEV-26614

RedHat -> Red Hat by Daniel Black

support-files/mysql.server.sh
@@ -9,7 +9,7 @@
 # When this is done the mysql server will be started when the machine is
 # started and shut down when the systems goes down.
 
-# Comments to support chkconfig on RedHat Linux
+# Comments to support chkconfig on Red Hat Linux
 # chkconfig: 2345 64 36
 # description: A very fast and reliable SQL database engine.
 
@@ -52,7 +52,7 @@ datadir=
 # Negative numbers mean to wait indefinitely
 service_startup_timeout=900
 
-# Lock directory for RedHat / SuSE.
+# Lock directory for Red Hat / SuSE.
 lockdir='/var/lock/subsys'
 lock_file_path="$lockdir/mysql"
 
@@ -91,7 +91,7 @@ datadir_set=
 
 #
 # Use LSB init script functions for printing messages, if possible
-# Include non-LSB RedHat init functions to make systemctl redirect work
+# Include non-LSB Red Hat init functions to make systemctl redirect work
 init_functions="/etc/init.d/functions"
 lsb_functions="/lib/lsb/init-functions"
 if test -f $lsb_functions; then
@@ -100,6 +100,7 @@ fi
 
 if test -f $init_functions; then
   . $init_functions
+else
   log_success_msg()
   {
     echo " SUCCESS! $@"
@@ -309,7 +310,7 @@ case "$mode" in
       $bindir/mysqld_safe --datadir="$datadir" --pid-file="$mysqld_pid_file_path" "$@" &
       wait_for_ready; return_value=$?
 
-      # Make lock for RedHat / SuSE
+      # Make lock for Red Hat / SuSE
       if test -w "$lockdir"
       then
         touch "$lock_file_path"
@@ -339,7 +340,7 @@ case "$mode" in
         rm "$mysqld_pid_file_path"
       fi
 
-      # Delete lock for RedHat / SuSE
+      # Delete lock for Red Hat / SuSE
       if test -f "$lock_file_path"
       then
         rm -f "$lock_file_path"