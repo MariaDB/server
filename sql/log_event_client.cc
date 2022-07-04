ba69c74112753e88d29701057de0abde100f253e
Sergei Golubchik <serg@mariadb.org>
2022-07-04 16:47:14 +0200

    MDEV-28632 Change default of explicit_defaults_for_timestamp to ON
    save it in binlog, so that CREATE TABLE could be replayed correctly
    on the slave

--- sql/log_event_client.cc
+++ sql/log_event_client.cc
@@ -1896,11 +1896,12 @@ bool Query_log_event::print_query_header(IO_CACHE* file,
                            "@@session.unique_checks", &need_comma) ||
           print_set_option(file, tmp, OPTION_NOT_AUTOCOMMIT, ~flags2,
                            "@@session.autocommit", &need_comma) ||
-          print_set_option(file, tmp, OPTION_NO_CHECK_CONSTRAINT_CHECKS,
-                           ~flags2,
+          print_set_option(file, tmp, OPTION_NO_CHECK_CONSTRAINT_CHECKS, ~flags2,
                            "@@session.check_constraint_checks", &need_comma) ||
           print_set_option(file, tmp, OPTION_IF_EXISTS, flags2,
                            "@@session.sql_if_exists", &need_comma)||
+          print_set_option(file, tmp, OPTION_EXPLICIT_DEF_TIMESTAMP, flags2,
+                           "@@session.explicit_defaults_for_timestamp", &need_comma)||
           my_b_printf(file,"%s\n", print_event_info->delimiter))
         goto err;
       print_event_info->flags2= flags2;
