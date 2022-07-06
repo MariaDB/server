e1f21590469239cdccf2759e035f60565fca6414
Thejaka Kanewala <thejaka.kanewala@servicenow.com>
2022-07-06 11:38:06 -0700

    PRB1587589: Fixing PRB1587589. Mariabackup --prepare only deals with the redo log and the data files. Currently, this is handled only for SRV_OPERATION_RESTORE only. Therefore, we see errors in prepare log when --prepare is executed with --export. Extended the logic to handle for SRV_OPERATION_RESTORE_EXPORT also.
    PRB1587589: Fixing a test case failure

--- storage/innobase/trx/trx0rseg.cc
+++ storage/innobase/trx/trx0rseg.cc
@@ -483,7 +483,8 @@ trx_rseg_mem_restore(trx_rseg_t* rseg, trx_id_t& max_trx_id, mtr_t* mtr)
 		}
 	}
 
-	if (srv_operation == SRV_OPERATION_RESTORE) {
+	if ((srv_operation == SRV_OPERATION_RESTORE) ||
+            (srv_operation == SRV_OPERATION_RESTORE_EXPORT)) {
 		/* mariabackup --prepare only deals with
 		the redo log and the data files, not with
 		transactions or the data dictionary. */
