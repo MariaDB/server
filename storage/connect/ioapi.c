356aaa0a8d8d83d6977903d640105c6891dd380d
Brad Smith <brad@comstyle.com>
2022-07-24 23:43:38 -0400

    Fix building Connect storage engine ioapi code on OpenBSD / NetBSD / DragonFly

--- storage/connect/ioapi.c
+++ storage/connect/ioapi.c
@@ -14,8 +14,7 @@
         #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-#if defined(__APPLE__) || defined(IOAPI_NO_64)
-// In darwin and perhaps other BSD variants off_t is a 64 bit value, hence no need for specific 64 bit functions
+#if defined(IOAPI_NO_64)
 #define FOPEN_FUNC(filename, mode) fopen(filename, mode)
 #define FTELLO_FUNC(stream) ftello(stream)
 #define FSEEKO_FUNC(stream, offset, origin) fseeko(stream, offset, origin)
