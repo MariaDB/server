9df047f264db3a114026849d64b691d1301d8109
Andrei <andrei.elkin@mariadb.com>
2022-08-09 19:28:49 +0300

    MDEV-24660 MYSQL_BIN_LOG::cleanup(): Assertion `b->xid_count == 0'
    The assert fired at shutdown time to indicate a missed decrement to
    the count. The counter could not have been decremented because
    the binlog background thread had already gone.
    
    Fixed with relocation the binlog background thread exit in the server
    shutdown module.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -1781,7 +1781,6 @@ static void close_connections(void)
 
   Events::deinit();
   slave_prepare_for_shutdown();
-  mysql_bin_log.stop_background_thread();
   ack_receiver.stop();
 
   /*
@@ -1965,6 +1964,7 @@ static void clean_up(bool print_message)
   logger.cleanup_base();
 
   injector::free_instance();
+  mysql_bin_log.stop_background_thread();
   mysql_bin_log.cleanup();
 
   my_tz_free();
