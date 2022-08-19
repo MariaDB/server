0631f9d178c5bea559e43e341de3530cd5d14cba
Igor Babaev <igor@askmonty.org>
2022-08-18 20:40:04 -0700

    Applied the changes introduced in the commit
    92a328099800486fe585a54d31ddf84c02ec6ae0
    Author:	Oleksandr Byelkin <sanja@mariadb.com>  Tue Jul 12 00:25:08 2022
    Committer:	Oleksandr Byelkin <sanja@mariadb.com>  Thu Jul 14 00:46:06 2022
    
    for the code of MDEV-28883.

--- sql/sql_lex.h
+++ sql/sql_lex.h
@@ -1778,15 +1778,6 @@ public:
     return (lock_tables_state == LTS_LOCKED);
   }
 
-  /**
-    Number of tables which were open by open_tables() and to be locked
-    by lock_tables().
-    Note that we set this member only in some cases, when this value
-    needs to be passed from open_tables() to lock_tables() which are
-    separated by some amount of code.
-  */
-  uint table_count;
-
    /*
     These constructor and destructor serve for creation/destruction
     of Query_tables_list instances which are used as backup storage.
