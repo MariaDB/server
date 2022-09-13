AllenZhanga/MariaDB-Server

Rewrite cost computation for filesort operations
This is a rework of how filesort calculates costs to allow functions
like test_if_skip_sort_order() to calculate the cost of filesort to
decide between filesort and using a key to resolve ORDER BY.

Changes:
- Split cost calculation of qsort + optional merge sort and priority queue
  to dedicated functions.
- Fixed some wrong calculations of cost in old code (use of log() instead
  of log2()).
- Added costs realted to fetching the rows if addon fields are not used.
- Updated get_merge_cost() to take into account that we are going to
  read data from temporary files in big chuncks (DISK_CHUNCK_SIZE (64K) and
  not in IO_SIZE (4K).
- More code documentation including various variables in Sort_param.

One effect of the cost update is that the cost of priority queue
with addon field has decreased slightly and is used in more cases.
When the rowid is large (like with InnoDB where rowid is the priority key),
using addon fields is in many cases preferable.

Reviewer: Monty

sql/filesort_utils.cc
@@ -23,20 +23,93 @@
 
 PSI_memory_key key_memory_Filesort_buffer_sort_keys;
 
+
+/*
+ Different ways to do sorting:
+ Merge Sort -> Without addon Fields, with fixed length
+ Merge Sort -> Without addon Fields, with dynamic length
+ Merge Sort -> With addon Fields, with fixed length
+ Merge Sort -> With addon Fields, with dynamic length
+
+ Priority queue -> Without addon fields
+ Priority queue -> With addon fields
+
+ With PQ (Priority queue) we could have a simple key (memcmp) or a
+ complex key (double & varchar for example). This cost difference
+ is currently not considered.
+*/
+
+
 /**
-  A local helper function. See comments for get_merge_buffers_cost().
+  Compute the cost of running qsort over a set of rows.
+  @param num_rows           How many rows will be sorted.
+  @param with_addon_fields  Set to true if the sorted rows include the whole
+                            row (with addon fields) or just the keys themselves.
+
+  @retval
+    Cost of the operation.
 */
 
 static
-double get_merge_cost(ha_rows num_elements,
-                      ha_rows num_buffers,
-                      uint elem_size,
-                      double key_compare_cost)
+double get_qsort_sort_cost(size_t num_rows, bool with_addon_fields)
 {
-  return (2.0 * ((double) num_elements * elem_size) / IO_SIZE
-          + (double) num_elements * log((double) num_buffers) *
-          key_compare_cost / M_LN2);
+  const double row_copy_cost= with_addon_fields ? DEFAULT_ROW_COPY_COST :
+                                                  DEFAULT_KEY_COPY_COST;
+  const double key_cmp_cost= DEFAULT_KEY_COMPARE_COST;
+  const double qsort_constant_factor= QSORT_SORT_SLOWNESS_CORRECTION_FACTOR *
+                                      (row_copy_cost + key_cmp_cost);
+
+  return qsort_constant_factor * num_rows * log2(1.0 + num_rows);
 }
+
+
+/**
+  Compute the cost of sorting num_rows and only retrieving queue_size rows.
+  @param num_rows           How many rows will be sorted.
+  @param queue_size         How many rows will be returned by the priority
+                            queue.
+  @param with_addon_fields  Set to true if the sorted rows include the whole
+                            row (with addon fields) or just the keys themselves.
+
+  @retval
+    Cost of the operation.
+*/
+
+double get_pq_sort_cost(size_t num_rows, size_t queue_size,
+                        bool with_addon_fields)
+{
+  const double row_copy_cost= with_addon_fields ? DEFAULT_ROW_COPY_COST :
+                                                  DEFAULT_KEY_COPY_COST;
+  const double key_cmp_cost= DEFAULT_KEY_COMPARE_COST;
+  /* 2 -> 1 insert, 1 pop from the queue*/
+  const double pq_sort_constant_factor= PQ_SORT_SLOWNESS_CORRECTION_FACTOR *
+                                        2.0 * (row_copy_cost + key_cmp_cost);
+
+  return pq_sort_constant_factor * num_rows * log2(1.0 + queue_size);
+}
+
+
+/**
+  Compute the cost of merging "num_buffers" sorted buffers using a priority
+  queue.
+
+  See comments for get_merge_buffers_cost().
+*/
+
+static
+double get_merge_cost(ha_rows num_elements, ha_rows num_buffers,
+                      size_t elem_size, double compare_cost)
+{
+  /* 2 -> 1 read + 1 write */
+  const double io_cost= (2.0 * (num_elements * elem_size +
+                                DISK_CHUNK_SIZE - 1) /
+                         DISK_CHUNK_SIZE);
+  /* 2 -> 1 insert, 1 pop for the priority queue used to merge the buffers. */
+  const double cpu_cost= (2.0 * num_elements * log2(1.0 + num_buffers) *
+                          compare_cost) * PQ_SORT_SLOWNESS_CORRECTION_FACTOR;
+  return io_cost + cpu_cost;
+}
+
 
 /**
   This is a simplified, and faster version of @see get_merge_many_buffs_cost().
@@ -48,28 +121,36 @@ double get_merge_cost(ha_rows num_elements,
 
 double get_merge_many_buffs_cost_fast(ha_rows num_rows,
                                       ha_rows num_keys_per_buffer,
-                                      uint    elem_size,
-                                      double  key_compare_cost)
+                                      size_t elem_size,
+                                      double key_compare_cost,
+                                      bool with_addon_fields)
 {
+  DBUG_ASSERT(num_keys_per_buffer != 0);
+
   ha_rows num_buffers= num_rows / num_keys_per_buffer;
   ha_rows last_n_elems= num_rows % num_keys_per_buffer;
   double total_cost;
+  double full_buffer_sort_cost;
 
-  // Calculate CPU cost of sorting buffers.
-  total_cost=
-    ((num_buffers * num_keys_per_buffer * log(1.0 + num_keys_per_buffer) +
-      last_n_elems * log(1.0 + last_n_elems)) * key_compare_cost);
+  /* Calculate cost for sorting all merge buffers + the last one. */
+  full_buffer_sort_cost= get_qsort_sort_cost(num_keys_per_buffer,
+                                             with_addon_fields);
+  total_cost= (num_buffers * full_buffer_sort_cost +
+               get_qsort_sort_cost(last_n_elems, with_addon_fields));
 
-  // Simulate behavior of merge_many_buff().
+  if (num_buffers >= MERGEBUFF2)
+    total_cost+= TMPFILE_CREATE_COST * 2;       // We are creating 2 files.
+
+  /* Simulate behavior of merge_many_buff(). */
   while (num_buffers >= MERGEBUFF2)
   {
-    // Calculate # of calls to merge_buffers().
-    const ha_rows loop_limit= num_buffers - MERGEBUFF*3/2;
-    const ha_rows num_merge_calls= 1 + loop_limit/MERGEBUFF;
+    /* Calculate # of calls to merge_buffers(). */
+    const ha_rows loop_limit= num_buffers - MERGEBUFF * 3 / 2;
+    const ha_rows num_merge_calls= 1 + loop_limit / MERGEBUFF;
     const ha_rows num_remaining_buffs=
       num_buffers - num_merge_calls * MERGEBUFF;
 
-    // Cost of merge sort 'num_merge_calls'.
+    /* Cost of merge sort 'num_merge_calls'. */
     total_cost+=
       num_merge_calls *
       get_merge_cost(num_keys_per_buffer * MERGEBUFF, MERGEBUFF, elem_size,
@@ -92,6 +173,130 @@ double get_merge_many_buffs_cost_fast(ha_rows num_rows,
   total_cost+= get_merge_cost(last_n_elems, 1 + num_buffers, elem_size,
                               key_compare_cost);
   return total_cost;
+}
+
+
+void Sort_costs::compute_fastest_sort()
+{
+  lowest_cost= DBL_MAX;
+  uint min_idx= NO_SORT_POSSIBLE_OUT_OF_MEM;
+  for (uint i= 0; i < FINAL_SORT_TYPE; i++)
+  {
+    if (lowest_cost > costs[i])
+    {
+      min_idx= i;
+      lowest_cost= costs[i];
+    }
+  }
+  fastest_sort= static_cast<enum sort_type>(min_idx);
+}
+
+
+/*
+  Calculate cost of using priority queue for filesort.
+  There are two options: using addon fields or not
+*/
+
+void Sort_costs::compute_pq_sort_costs(Sort_param *param, ha_rows num_rows,
+                                       size_t memory_available,
+                                       bool with_addon_fields)
+{
+  /*
+    Implementation detail of PQ. To be able to keep a PQ of size N we need
+    N+1 elements allocated so we can use the last element as "swap" space
+    for the "insert" operation.
+    TODO(cvicentiu): This should be left as an implementation detail inside
+    the PQ, not have the optimizer take it into account.
+  */
+  size_t queue_size= param->limit_rows + 1;
+  size_t row_length, num_available_keys;
+
+  costs[PQ_SORT_ALL_FIELDS]= DBL_MAX;
+  costs[PQ_SORT_ORDER_BY_FIELDS]= DBL_MAX;
+
+  /*
+    We can't use priority queue if there's no limit or the limit is
+    too big.
+  */
+  if (param->limit_rows == HA_POS_ERROR ||
+      param->limit_rows >= UINT_MAX - 2)
+    return;
+
+  /* Calculate cost without addon keys (probably using less memory) */
+  row_length=         param->sort_length + param->ref_length + sizeof(char*);
+  num_available_keys= memory_available / row_length;
+
+  if (queue_size < num_available_keys)
+  {
+    costs[PQ_SORT_ORDER_BY_FIELDS]=
+      get_pq_sort_cost(num_rows, queue_size, false) +
+      param->sort_form->file->ha_rndpos_time(MY_MIN(queue_size - 1, num_rows));
+  }
+
+  /* Calculate cost with addon fields */
+  if (with_addon_fields)
+  {
+    row_length=         param->rec_length + sizeof(char *);
+    num_available_keys= memory_available / row_length;
+
+    if (queue_size < num_available_keys)
+      costs[PQ_SORT_ALL_FIELDS]= get_pq_sort_cost(num_rows, queue_size, true);
+  }
+}
+
+/*
+  Calculate cost of using qsort optional merge sort for resolving filesort.
+  There are two options: using addon fields or not
+*/
+
+void Sort_costs::compute_merge_sort_costs(Sort_param *param,
+                                          ha_rows num_rows,
+                                          size_t memory_available,
+                                          bool with_addon_fields)
+{
+  size_t row_length= param->sort_length + param->ref_length + sizeof(char *);
+  size_t num_available_keys= memory_available / row_length;
+
+  costs[MERGE_SORT_ALL_FIELDS]= DBL_MAX;
+  costs[MERGE_SORT_ORDER_BY_FIELDS]= DBL_MAX;
+
+  if (num_available_keys)
+    costs[MERGE_SORT_ORDER_BY_FIELDS]=
+      get_merge_many_buffs_cost_fast(num_rows, num_available_keys,
+                                     row_length, DEFAULT_KEY_COMPARE_COST,
+                                     false) +
+      param->sort_form->file->ha_rndpos_time(MY_MIN(param->limit_rows,
+                                                    num_rows));
+
+  if (with_addon_fields)
+  {
+    /* Compute cost of merge sort *if* we strip addon fields. */
+    row_length= param->rec_length + sizeof(char *);
+    num_available_keys= memory_available / row_length;
+
+    if (num_available_keys)
+      costs[MERGE_SORT_ALL_FIELDS]=
+        get_merge_many_buffs_cost_fast(num_rows, num_available_keys,
+                                       row_length, DEFAULT_KEY_COMPARE_COST,
+                                       true);
+  }
+
+  /*
+     TODO(cvicentiu) we do not handle dynamic length fields yet.
+     The code should decide here if the format is FIXED length or DYNAMIC
+     and fill in the appropriate costs.
+  */
+}
+
+void Sort_costs::compute_sort_costs(Sort_param *param, ha_rows num_rows,
+                                    size_t memory_available,
+                                    bool with_addon_fields)
+{
+  compute_pq_sort_costs(param, num_rows, memory_available,
+                        with_addon_fields);
+  compute_merge_sort_costs(param, num_rows, memory_available,
+                           with_addon_fields);
+  compute_fastest_sort();
 }
 
 /*

MariaDB/server

sql/filesort_utils.cc
@@ -23,20 +23,93 @@
 
 PSI_memory_key key_memory_Filesort_buffer_sort_keys;
 
+
+/*
+ Different ways to do sorting:
+ Merge Sort -> Without addon Fields, with fixed length
+ Merge Sort -> Without addon Fields, with dynamic length
+ Merge Sort -> With addon Fields, with fixed length
+ Merge Sort -> With addon Fields, with dynamic length
+
+ Priority queue -> Without addon fields
+ Priority queue -> With addon fields
+
+ With PQ (Priority queue) we could have a simple key (memcmp) or a
+ complex key (double & varchar for example). This cost difference
+ is currently not considered.
+*/
+
+
 /**
-  A local helper function. See comments for get_merge_buffers_cost().
+  Compute the cost of running qsort over a set of rows.
+  @param num_rows           How many rows will be sorted.
+  @param with_addon_fields  Set to true if the sorted rows include the whole
+                            row (with addon fields) or just the keys themselves.
+
+  @retval
+    Cost of the operation.
 */
 
 static
-double get_merge_cost(ha_rows num_elements,
-                      ha_rows num_buffers,
-                      uint elem_size,
-                      double key_compare_cost)
+double get_qsort_sort_cost(size_t num_rows, bool with_addon_fields)
 {
-  return (2.0 * ((double) num_elements * elem_size) / IO_SIZE
-          + (double) num_elements * log((double) num_buffers) *
-          key_compare_cost / M_LN2);
+  const double row_copy_cost= with_addon_fields ? DEFAULT_ROW_COPY_COST :
+                                                  DEFAULT_KEY_COPY_COST;
+  const double key_cmp_cost= DEFAULT_KEY_COMPARE_COST;
+  const double qsort_constant_factor= QSORT_SORT_SLOWNESS_CORRECTION_FACTOR *
+                                      (row_copy_cost + key_cmp_cost);
+
+  return qsort_constant_factor * num_rows * log2(1.0 + num_rows);
 }
+
+
+/**
+  Compute the cost of sorting num_rows and only retrieving queue_size rows.
+  @param num_rows           How many rows will be sorted.
+  @param queue_size         How many rows will be returned by the priority
+                            queue.
+  @param with_addon_fields  Set to true if the sorted rows include the whole
+                            row (with addon fields) or just the keys themselves.
+
+  @retval
+    Cost of the operation.
+*/
+
+double get_pq_sort_cost(size_t num_rows, size_t queue_size,
+                        bool with_addon_fields)
+{
+  const double row_copy_cost= with_addon_fields ? DEFAULT_ROW_COPY_COST :
+                                                  DEFAULT_KEY_COPY_COST;
+  const double key_cmp_cost= DEFAULT_KEY_COMPARE_COST;
+  /* 2 -> 1 insert, 1 pop from the queue*/
+  const double pq_sort_constant_factor= PQ_SORT_SLOWNESS_CORRECTION_FACTOR *
+                                        2.0 * (row_copy_cost + key_cmp_cost);
+
+  return pq_sort_constant_factor * num_rows * log2(1.0 + queue_size);
+}
+
+
+/**
+  Compute the cost of merging "num_buffers" sorted buffers using a priority
+  queue.
+
+  See comments for get_merge_buffers_cost().
+*/
+
+static
+double get_merge_cost(ha_rows num_elements, ha_rows num_buffers,
+                      size_t elem_size, double compare_cost)
+{
+  /* 2 -> 1 read + 1 write */
+  const double io_cost= (2.0 * (num_elements * elem_size +
+                                DISK_CHUNK_SIZE - 1) /
+                         DISK_CHUNK_SIZE);
+  /* 2 -> 1 insert, 1 pop for the priority queue used to merge the buffers. */
+  const double cpu_cost= (2.0 * num_elements * log2(1.0 + num_buffers) *
+                          compare_cost) * PQ_SORT_SLOWNESS_CORRECTION_FACTOR;
+  return io_cost + cpu_cost;
+}
+
 
 /**
   This is a simplified, and faster version of @see get_merge_many_buffs_cost().
@@ -48,28 +121,36 @@ double get_merge_cost(ha_rows num_elements,
 
 double get_merge_many_buffs_cost_fast(ha_rows num_rows,
                                       ha_rows num_keys_per_buffer,
-                                      uint    elem_size,
-                                      double  key_compare_cost)
+                                      size_t elem_size,
+                                      double key_compare_cost,
+                                      bool with_addon_fields)
 {
+  DBUG_ASSERT(num_keys_per_buffer != 0);
+
   ha_rows num_buffers= num_rows / num_keys_per_buffer;
   ha_rows last_n_elems= num_rows % num_keys_per_buffer;
   double total_cost;
+  double full_buffer_sort_cost;
 
-  // Calculate CPU cost of sorting buffers.
-  total_cost=
-    ((num_buffers * num_keys_per_buffer * log(1.0 + num_keys_per_buffer) +
-      last_n_elems * log(1.0 + last_n_elems)) * key_compare_cost);
+  /* Calculate cost for sorting all merge buffers + the last one. */
+  full_buffer_sort_cost= get_qsort_sort_cost(num_keys_per_buffer,
+                                             with_addon_fields);
+  total_cost= (num_buffers * full_buffer_sort_cost +
+               get_qsort_sort_cost(last_n_elems, with_addon_fields));
 
-  // Simulate behavior of merge_many_buff().
+  if (num_buffers >= MERGEBUFF2)
+    total_cost+= TMPFILE_CREATE_COST * 2;       // We are creating 2 files.
+
+  /* Simulate behavior of merge_many_buff(). */
   while (num_buffers >= MERGEBUFF2)
   {
-    // Calculate # of calls to merge_buffers().
-    const ha_rows loop_limit= num_buffers - MERGEBUFF*3/2;
-    const ha_rows num_merge_calls= 1 + loop_limit/MERGEBUFF;
+    /* Calculate # of calls to merge_buffers(). */
+    const ha_rows loop_limit= num_buffers - MERGEBUFF * 3 / 2;
+    const ha_rows num_merge_calls= 1 + loop_limit / MERGEBUFF;
     const ha_rows num_remaining_buffs=
       num_buffers - num_merge_calls * MERGEBUFF;
 
-    // Cost of merge sort 'num_merge_calls'.
+    /* Cost of merge sort 'num_merge_calls'. */
     total_cost+=
       num_merge_calls *
       get_merge_cost(num_keys_per_buffer * MERGEBUFF, MERGEBUFF, elem_size,
@@ -92,6 +173,130 @@ double get_merge_many_buffs_cost_fast(ha_rows num_rows,
   total_cost+= get_merge_cost(last_n_elems, 1 + num_buffers, elem_size,
                               key_compare_cost);
   return total_cost;
+}
+
+
+void Sort_costs::compute_fastest_sort()
+{
+  lowest_cost= DBL_MAX;
+  uint min_idx= NO_SORT_POSSIBLE_OUT_OF_MEM;
+  for (uint i= 0; i < FINAL_SORT_TYPE; i++)
+  {
+    if (lowest_cost > costs[i])
+    {
+      min_idx= i;
+      lowest_cost= costs[i];
+    }
+  }
+  fastest_sort= static_cast<enum sort_type>(min_idx);
+}
+
+
+/*
+  Calculate cost of using priority queue for filesort.
+  There are two options: using addon fields or not
+*/
+
+void Sort_costs::compute_pq_sort_costs(Sort_param *param, ha_rows num_rows,
+                                       size_t memory_available,
+                                       bool with_addon_fields)
+{
+  /*
+    Implementation detail of PQ. To be able to keep a PQ of size N we need
+    N+1 elements allocated so we can use the last element as "swap" space
+    for the "insert" operation.
+    TODO(cvicentiu): This should be left as an implementation detail inside
+    the PQ, not have the optimizer take it into account.
+  */
+  size_t queue_size= param->limit_rows + 1;
+  size_t row_length, num_available_keys;
+
+  costs[PQ_SORT_ALL_FIELDS]= DBL_MAX;
+  costs[PQ_SORT_ORDER_BY_FIELDS]= DBL_MAX;
+
+  /*
+    We can't use priority queue if there's no limit or the limit is
+    too big.
+  */
+  if (param->limit_rows == HA_POS_ERROR ||
+      param->limit_rows >= UINT_MAX - 2)
+    return;
+
+  /* Calculate cost without addon keys (probably using less memory) */
+  row_length=         param->sort_length + param->ref_length + sizeof(char*);
+  num_available_keys= memory_available / row_length;
+
+  if (queue_size < num_available_keys)
+  {
+    costs[PQ_SORT_ORDER_BY_FIELDS]=
+      get_pq_sort_cost(num_rows, queue_size, false) +
+      param->sort_form->file->ha_rnd_pos_time(MY_MIN(queue_size - 1, num_rows));
+  }
+
+  /* Calculate cost with addon fields */
+  if (with_addon_fields)
+  {
+    row_length=         param->rec_length + sizeof(char *);
+    num_available_keys= memory_available / row_length;
+
+    if (queue_size < num_available_keys)
+      costs[PQ_SORT_ALL_FIELDS]= get_pq_sort_cost(num_rows, queue_size, true);
+  }
+}
+
+/*
+  Calculate cost of using qsort optional merge sort for resolving filesort.
+  There are two options: using addon fields or not
+*/
+
+void Sort_costs::compute_merge_sort_costs(Sort_param *param,
+                                          ha_rows num_rows,
+                                          size_t memory_available,
+                                          bool with_addon_fields)
+{
+  size_t row_length= param->sort_length + param->ref_length + sizeof(char *);
+  size_t num_available_keys= memory_available / row_length;
+
+  costs[MERGE_SORT_ALL_FIELDS]= DBL_MAX;
+  costs[MERGE_SORT_ORDER_BY_FIELDS]= DBL_MAX;
+
+  if (num_available_keys)
+    costs[MERGE_SORT_ORDER_BY_FIELDS]=
+      get_merge_many_buffs_cost_fast(num_rows, num_available_keys,
+                                     row_length, DEFAULT_KEY_COMPARE_COST,
+                                     false) +
+      param->sort_form->file->ha_rnd_pos_time(MY_MIN(param->limit_rows,
+                                                    num_rows));
+
+  if (with_addon_fields)
+  {
+    /* Compute cost of merge sort *if* we strip addon fields. */
+    row_length= param->rec_length + sizeof(char *);
+    num_available_keys= memory_available / row_length;
+
+    if (num_available_keys)
+      costs[MERGE_SORT_ALL_FIELDS]=
+        get_merge_many_buffs_cost_fast(num_rows, num_available_keys,
+                                       row_length, DEFAULT_KEY_COMPARE_COST,
+                                       true);
+  }
+
+  /*
+     TODO(cvicentiu) we do not handle dynamic length fields yet.
+     The code should decide here if the format is FIXED length or DYNAMIC
+     and fill in the appropriate costs.
+  */
+}
+
+void Sort_costs::compute_sort_costs(Sort_param *param, ha_rows num_rows,
+                                    size_t memory_available,
+                                    bool with_addon_fields)
+{
+  compute_pq_sort_costs(param, num_rows, memory_available,
+                        with_addon_fields);
+  compute_merge_sort_costs(param, num_rows, memory_available,
+                           with_addon_fields);
+  compute_fastest_sort();
 }
 
 /*