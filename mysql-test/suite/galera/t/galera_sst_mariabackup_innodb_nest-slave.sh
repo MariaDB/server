96b51bd23d303def6edb4d2fe26c025c6625b3de
Julius Goryavsky <julius.goryavsky@mariadb.com>
2022-09-13 13:48:16 +0200

    MDEV-28669 addendum: additional tests

---
+++ mysql-test/suite/galera/t/galera_sst_mariabackup_innodb_nest-slave.sh
@@ -0,0 +1,6 @@
+rm -rf $MYSQLTEST_VARDIR/mysqld.2/data/innodb_data2
+rm -rf $MYSQLTEST_VARDIR/mysqld.2/data/innodb_undo2
+rm -rf $MYSQLTEST_VARDIR/mysqld.2/data/innodb_group2
+mkdir -p $MYSQLTEST_VARDIR/mysqld.2/data/innodb_data2
+mkdir -p $MYSQLTEST_VARDIR/mysqld.2/data/innodb_undo2
+mkdir -p $MYSQLTEST_VARDIR/mysqld.2/data/innodb_group2
---
+++ mysql-test/suite/galera/t/galera_sst_rsync_buffer_pool-master.sh
@@ -0,0 +1,2 @@
+rm -rf $MYSQLTEST_VARDIR/mysqld.1/innodb_data1
+mkdir -p $MYSQLTEST_VARDIR/mysqld.1/innodb_data1
