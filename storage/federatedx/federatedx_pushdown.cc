vince-ma/mariadb-server

MDEV-25080 Allow pushdown of UNIONs to foreign engines
Allow queries of multiple SELECTs combined together with
UNIONs/EXCEPTs/INTERSECTs to be pushed down to foreign engines.
If the foreign engine provides an interface method "create_unit"
and the UNIT is a top-level unit of the SQL query then the server
tries to push the whole SELECT_LEX_UNIT down to the engine for execution.
The engine should perform necessary checks and if they succeed,
execute the query. If the engine is unable to execute the whole unit,
then another attempt is made to push down SELECTs composing the unit
separately using the "create_select" interface method. In this case
the results of separate SELECTs are combined at the server side
thus composing the final result

storage/federatedx/federatedx_pushdown.cc
@@ -35,35 +35,78 @@
 */
 
 
+static std::pair<handlerton *, TABLE *> get_handlerton(SELECT_LEX *sel_lex)
+{
+  handlerton *hton= nullptr;
+  TABLE *table= nullptr;
+  if (!sel_lex->join)
+    return {nullptr, nullptr};
+  for (TABLE_LIST *tbl= sel_lex->join->tables_list; tbl; tbl= tbl->next_local)
+  {
+    if (!tbl->table)
+      return {nullptr, nullptr};
+    if (!hton)
+    {
+      hton= tbl->table->file->partition_ht();
+      table= tbl->table;
+    }
+    else if (hton != tbl->table->file->partition_ht())
+      return {nullptr, nullptr};
+  }
+
+  for (SELECT_LEX_UNIT *un= sel_lex->first_inner_unit(); un;
+       un= un->next_unit())
+  {
+    for (SELECT_LEX *sl= un->first_select(); sl; sl= sl->next_select())
+    {
+      auto inner_ht= get_handlerton(sl);
+      if (!hton)
+      {
+        hton= inner_ht.first;
+        table= inner_ht.second;
+      }
+      else if (hton != inner_ht.first)
+        return {nullptr, nullptr};
+    }
+  }
+  return {hton, table};
+}
+
+
+static std::pair<handlerton *, TABLE *>
+get_handlerton_for_unit(SELECT_LEX_UNIT *lex_unit)
+{
+  handlerton *hton= nullptr;
+  TABLE *table= nullptr;
+  for (auto sel_lex= lex_unit->first_select(); sel_lex;
+       sel_lex= sel_lex->next_select())
+  {
+    auto next_ht= get_handlerton(sel_lex);
+    if (!hton)
+    {
+      hton= next_ht.first;
+      table= next_ht.second;
+    }
+    else if (hton != next_ht.first)
+      return {nullptr, nullptr};
+  }
+  return {hton, table};
+}
+
+
 static derived_handler*
 create_federatedx_derived_handler(THD* thd, TABLE_LIST *derived)
 {
   if (!use_pushdown)
     return 0;
 
-  ha_federatedx_derived_handler* handler = NULL;
-  handlerton *ht= 0;
-
   SELECT_LEX_UNIT *unit= derived->derived;
 
-  for (SELECT_LEX *sl= unit->first_select(); sl; sl= sl->next_select())
-  {
-    if (!(sl->join))
-      return 0;
-    for (TABLE_LIST *tbl= sl->join->tables_list; tbl; tbl= tbl->next_local)
-    {
-      if (!tbl->table)
-	return 0;
-      if (!ht)
-        ht= tbl->table->file->partition_ht();
-      else if (ht != tbl->table->file->partition_ht())
-        return 0;
-    }
-  }
+  auto hton= get_handlerton_for_unit(unit);
+  if (!hton.first)
+    return nullptr;
 
-  handler= new ha_federatedx_derived_handler(thd, derived);
-
-  return handler;
+  return new ha_federatedx_derived_handler(thd, derived);
 }
 
 
@@ -163,38 +206,36 @@ void ha_federatedx_derived_handler::print_error(int, unsigned long)
 }
 
 
-static select_handler*
-create_federatedx_select_handler(THD* thd, SELECT_LEX *sel)
+static select_handler *create_federatedx_select_handler(
+  THD *thd, SELECT_LEX *sel_lex)
 {
   if (!use_pushdown)
-    return 0;
+    return nullptr;
 
-  ha_federatedx_select_handler* handler = NULL;
-  handlerton *ht= 0;
+  auto hton= get_handlerton(sel_lex);
+  if (!hton.first)
+    return nullptr;
 
-  for (TABLE_LIST *tbl= thd->lex->query_tables; tbl; tbl= tbl->next_global)
-  {
-    if (!tbl->table)
-      return 0;
-    if (!ht)
-      ht= tbl->table->file->partition_ht();
-    else if (ht != tbl->table->file->partition_ht())
-      return 0;
-  }
-
-  /*
-    Currently, ha_federatedx_select_handler::init_scan just takes the
-    thd->query and sends it to the backend.
-    This obviously won't work if the SELECT uses an "INTO @var" or
-    "INTO OUTFILE". It is also unlikely to work if the select has some
-    other kind of side effect.
-  */
-  if (sel->uncacheable & UNCACHEABLE_SIDEEFFECT)
+  if (sel_lex->uncacheable & UNCACHEABLE_SIDEEFFECT)
     return NULL;
 
-  handler= new ha_federatedx_select_handler(thd, sel);
+  return new ha_federatedx_select_handler(thd, sel_lex, hton.second);
+}
 
-  return handler;
+static select_handler *create_federatedx_unit_handler(
+  THD* thd, SELECT_LEX_UNIT *sel_unit)
+{
+  if (!use_pushdown)
+    return nullptr;
+
+  auto hton= get_handlerton_for_unit(sel_unit);
+  if (!hton.first)
+    return nullptr;
+
+  if (sel_unit->uncacheable & UNCACHEABLE_SIDEEFFECT)
+    return nullptr;
+
+  return new ha_federatedx_select_handler(thd, sel_unit, hton.second);
 }
 
 /*
@@ -202,12 +243,31 @@ create_federatedx_select_handler(THD* thd, SELECT_LEX *sel)
   class implementation
 */
 
-ha_federatedx_select_handler::ha_federatedx_select_handler(THD *thd,
-                                                           SELECT_LEX *sel)
-  : select_handler(thd, federatedx_hton),
-    share(NULL), txn(NULL), iop(NULL), stored_result(NULL)
+
+ha_federatedx_select_handler::ha_federatedx_select_handler(
+    THD *thd, SELECT_LEX *select_lex, TABLE *tbl)
+  : select_handler(thd, federatedx_hton, select_lex),
+    share(NULL), txn(NULL), iop(NULL), stored_result(NULL), query_table(tbl),
+    query(query_buff, sizeof(query_buff), thd->charset())
 {
-  select= sel;
+  query.length(0);
+  select_lex->print(thd, &query,
+                    enum_query_type(QT_VIEW_INTERNAL |
+                                    QT_ITEM_ORIGINAL_FUNC_NULLIF |
+                                    QT_PARSABLE));
+}
+
+ha_federatedx_select_handler::ha_federatedx_select_handler(
+    THD *thd, SELECT_LEX_UNIT *lex_unit, TABLE *tbl)
+  : select_handler(thd, federatedx_hton, lex_unit), share(NULL), txn(NULL),
+    iop(NULL), stored_result(NULL), query_table(tbl),
+    query(query_buff, sizeof(query_buff), thd->charset())
+{
+  query.length(0);
+  lex_unit->print(&query,
+                  enum_query_type(QT_VIEW_INTERNAL |
+                                  QT_ITEM_ORIGINAL_FUNC_NULLIF |
+                                  QT_PARSABLE));
 }
 
 ha_federatedx_select_handler::~ha_federatedx_select_handler() {}
@@ -218,22 +278,14 @@ int ha_federatedx_select_handler::init_scan()
 
   DBUG_ENTER("ha_federatedx_select_handler::init_scan");
 
-  TABLE *table= 0;
-  for (TABLE_LIST *tbl= thd->lex->query_tables; tbl; tbl= tbl->next_global)
-  {
-    if (!tbl->table)
-      continue;
-    table= tbl->table;
-    break;
-  }
-  ha_federatedx *h= (ha_federatedx *) table->file;
+  ha_federatedx *h= (ha_federatedx *) query_table->file;
   iop= &h->io;
-  share= get_share(table->s->table_name.str, table);
+  share= get_share(query_table->s->table_name.str, query_table);
   txn= h->get_txn(thd);
   if ((rc= txn->acquire(share, thd, TRUE, iop)))
     DBUG_RETURN(rc);
 
-  if ((*iop)->query(thd->query(), thd->query_length()))
+  if ((*iop)->query(query.ptr(), query.length()))
     goto err;
 
   stored_result= (*iop)->store_result();

MariaDB/server

storage/federatedx/federatedx_pushdown.cc
@@ -34,6 +34,66 @@
     set global federated_pushdown=1;
 */
 
+/*
+  Check that all tables in the sel_lex use the FederatedX storage engine
+  and return one of them
+
+  @return
+    One of the tables from sel_lex
+*/
+static TABLE *get_fed_table_for_pushdown(SELECT_LEX *sel_lex)
+{
+  TABLE *table= nullptr;
+  if (!sel_lex->join)
+    return nullptr;
+  for (TABLE_LIST *tbl= sel_lex->join->tables_list; tbl; tbl= tbl->next_local)
+  {
+    if (!tbl->table)
+      return nullptr;
+    if (tbl->table->file->partition_ht() != federatedx_hton)
+      return nullptr;
+    if (!table)
+      table= tbl->table;
+  }
+
+  for (SELECT_LEX_UNIT *un= sel_lex->first_inner_unit(); un;
+       un= un->next_unit())
+  {
+    for (SELECT_LEX *sl= un->first_select(); sl; sl= sl->next_select())
+    {
+      auto inner_tbl= get_fed_table_for_pushdown(sl);
+      if (!inner_tbl)
+        return nullptr;
+      if (!table)
+        table= inner_tbl;
+    }
+  }
+  return table;
+}
+
+
+/*
+  Check that all tables in the lex_unit use the FederatedX storage engine
+  and return one of them
+
+  @return
+    One of the tables from lex_unit
+*/
+static TABLE *get_fed_table_for_unit_pushdown(SELECT_LEX_UNIT *lex_unit)
+{
+  TABLE *table= nullptr;
+  for (auto sel_lex= lex_unit->first_select(); sel_lex;
+       sel_lex= sel_lex->next_select())
+  {
+    auto next_tbl= get_fed_table_for_pushdown(sel_lex);
+    if (!next_tbl)
+      return nullptr;
+    if (!table)
+      table= next_tbl;
+  }
+  return table;
+}
+
 
 static derived_handler*
 create_federatedx_derived_handler(THD* thd, TABLE_LIST *derived)
@@ -41,29 +101,13 @@ create_federatedx_derived_handler(THD* thd, TABLE_LIST *derived)
   if (!use_pushdown)
     return 0;
 
-  ha_federatedx_derived_handler* handler = NULL;
-  handlerton *ht= 0;
-
   SELECT_LEX_UNIT *unit= derived->derived;
 
-  for (SELECT_LEX *sl= unit->first_select(); sl; sl= sl->next_select())
-  {
-    if (!(sl->join))
-      return 0;
-    for (TABLE_LIST *tbl= sl->join->tables_list; tbl; tbl= tbl->next_local)
-    {
-      if (!tbl->table)
-	return 0;
-      if (!ht)
-        ht= tbl->table->file->partition_ht();
-      else if (ht != tbl->table->file->partition_ht())
-        return 0;
-    }
-  }
+  auto tbl= get_fed_table_for_unit_pushdown(unit);
+  if (!tbl)
+    return nullptr;
 
-  handler= new ha_federatedx_derived_handler(thd, derived);
-
-  return handler;
+  return new ha_federatedx_derived_handler(thd, derived, tbl);
 }
 
 
@@ -73,81 +117,22 @@ create_federatedx_derived_handler(THD* thd, TABLE_LIST *derived)
 */
 
 ha_federatedx_derived_handler::ha_federatedx_derived_handler(THD *thd,
-                                                             TABLE_LIST *dt)
+                                                             TABLE_LIST *dt,
+                                                             TABLE *tbl)
   : derived_handler(thd, federatedx_hton),
-    share(NULL), txn(NULL), iop(NULL), stored_result(NULL)
+    federatedx_handler_base(thd, tbl)
 {
   derived= dt;
+
+  query.length(0);
+  dt->derived->print(&query,
+                     enum_query_type(QT_VIEW_INTERNAL |
+                                     QT_ITEM_ORIGINAL_FUNC_NULLIF |
+                                     QT_PARSABLE));
 }
 
-ha_federatedx_derived_handler::~ha_federatedx_derived_handler() {}
 
-int ha_federatedx_derived_handler::init_scan()
-{
-  THD *thd;
-  int rc= 0;
-
-  DBUG_ENTER("ha_federatedx_derived_handler::init_scan");
-
-  TABLE *table= derived->get_first_table()->table;
-  ha_federatedx *h= (ha_federatedx *) table->file;
-  iop= &h->io;
-  share= get_share(table->s->table_name.str, table);
-  thd= table->in_use;
-  txn= h->get_txn(thd);
-  if ((rc= txn->acquire(share, thd, TRUE, iop)))
-    DBUG_RETURN(rc);
-
-  if ((*iop)->query(derived->derived_spec.str, derived->derived_spec.length))
-    goto err;
-
-  stored_result= (*iop)->store_result();
-  if (!stored_result)
-      goto err;
-
-  DBUG_RETURN(0);
-
-err:
-  DBUG_RETURN(HA_FEDERATEDX_ERROR_WITH_REMOTE_SYSTEM);
-}
-
-int ha_federatedx_derived_handler::next_row()
-{
-  int rc;
-  FEDERATEDX_IO_ROW *row;
-  ulong *lengths;
-  Field **field;
-  int column= 0;
-  Time_zone *saved_time_zone= table->in_use->variables.time_zone;
-  DBUG_ENTER("ha_federatedx_derived_handler::next_row");
-
-  if ((rc= txn->acquire(share, table->in_use, TRUE, iop)))
-    DBUG_RETURN(rc);
-
-  if (!(row= (*iop)->fetch_row(stored_result)))
-    DBUG_RETURN(HA_ERR_END_OF_FILE);
-
-  /* Convert row to internal format */
-  table->in_use->variables.time_zone= UTC;
-  lengths= (*iop)->fetch_lengths(stored_result);
-
-  for (field= table->field; *field; field++, column++)
-  {
-    if ((*iop)->is_column_null(row, column))
-       (*field)->set_null();
-    else
-    {
-      (*field)->set_notnull();
-      (*field)->store((*iop)->get_column_data(row, column),
-                      lengths[column], &my_charset_bin);
-    }
-  }
-  table->in_use->variables.time_zone= saved_time_zone;
-
-  DBUG_RETURN(rc);
-}
-
-int ha_federatedx_derived_handler::end_scan()
+int federatedx_handler_base::end_scan_()
 {
   DBUG_ENTER("ha_federatedx_derived_handler::end_scan");
 
@@ -158,43 +143,42 @@ int ha_federatedx_derived_handler::end_scan()
   DBUG_RETURN(0);
 }
 
+
 void ha_federatedx_derived_handler::print_error(int, unsigned long)
 {
 }
 
 
-static select_handler*
-create_federatedx_select_handler(THD* thd, SELECT_LEX *sel)
+static select_handler *create_federatedx_select_handler(
+  THD *thd, SELECT_LEX *sel_lex)
 {
   if (!use_pushdown)
-    return 0;
+    return nullptr;
 
-  ha_federatedx_select_handler* handler = NULL;
-  handlerton *ht= 0;
+  auto tbl= get_fed_table_for_pushdown(sel_lex);
+  if (!tbl)
+    return nullptr;
 
-  for (TABLE_LIST *tbl= thd->lex->query_tables; tbl; tbl= tbl->next_global)
-  {
-    if (!tbl->table)
-      return 0;
-    if (!ht)
-      ht= tbl->table->file->partition_ht();
-    else if (ht != tbl->table->file->partition_ht())
-      return 0;
-  }
-
-  /*
-    Currently, ha_federatedx_select_handler::init_scan just takes the
-    thd->query and sends it to the backend.
-    This obviously won't work if the SELECT uses an "INTO @var" or
-    "INTO OUTFILE". It is also unlikely to work if the select has some
-    other kind of side effect.
-  */
-  if (sel->uncacheable & UNCACHEABLE_SIDEEFFECT)
+  if (sel_lex->uncacheable & UNCACHEABLE_SIDEEFFECT)
     return NULL;
 
-  handler= new ha_federatedx_select_handler(thd, sel);
+  return new ha_federatedx_select_handler(thd, sel_lex, tbl);
+}
 
-  return handler;
+static select_handler *create_federatedx_unit_handler(
+  THD* thd, SELECT_LEX_UNIT *sel_unit)
+{
+  if (!use_pushdown)
+    return nullptr;
+
+  auto tbl= get_fed_table_for_unit_pushdown(sel_unit);
+  if (!tbl)
+    return nullptr;
+
+  if (sel_unit->uncacheable & UNCACHEABLE_SIDEEFFECT)
+    return nullptr;
+
+  return new ha_federatedx_select_handler(thd, sel_unit, tbl);
 }
 
 /*
@@ -202,38 +186,53 @@ create_federatedx_select_handler(THD* thd, SELECT_LEX *sel)
   class implementation
 */
 
-ha_federatedx_select_handler::ha_federatedx_select_handler(THD *thd,
-                                                           SELECT_LEX *sel)
-  : select_handler(thd, federatedx_hton),
-    share(NULL), txn(NULL), iop(NULL), stored_result(NULL)
+federatedx_handler_base::federatedx_handler_base(THD *thd_arg, TABLE *tbl_arg)
+ : share(NULL), txn(NULL), iop(NULL), stored_result(NULL),
+   query(thd_arg->charset()),
+   query_table(tbl_arg)
+{}
+
+ha_federatedx_select_handler::ha_federatedx_select_handler(
+    THD *thd, SELECT_LEX *select_lex, TABLE *tbl)
+  : select_handler(thd, federatedx_hton, select_lex),
+    federatedx_handler_base(thd, tbl)
 {
-  select= sel;
+  query.length(0);
+  select_lex->print(thd, &query,
+                    enum_query_type(QT_VIEW_INTERNAL |
+                                    QT_ITEM_ORIGINAL_FUNC_NULLIF |
+                                    QT_PARSABLE));
 }
 
-ha_federatedx_select_handler::~ha_federatedx_select_handler() {}
 
-int ha_federatedx_select_handler::init_scan()
+ha_federatedx_select_handler::ha_federatedx_select_handler(
+    THD *thd, SELECT_LEX_UNIT *lex_unit, TABLE *tbl)
+  : select_handler(thd, federatedx_hton, lex_unit), 
+    federatedx_handler_base(thd, tbl)
 {
+  query.length(0);
+  lex_unit->print(&query,
+                  enum_query_type(QT_VIEW_INTERNAL |
+                                  QT_ITEM_ORIGINAL_FUNC_NULLIF |
+                                  QT_PARSABLE));
+}
+
+
+int federatedx_handler_base::init_scan_()
+{
+  THD *thd= query_table->in_use;
   int rc= 0;
 
   DBUG_ENTER("ha_federatedx_select_handler::init_scan");
 
-  TABLE *table= 0;
-  for (TABLE_LIST *tbl= thd->lex->query_tables; tbl; tbl= tbl->next_global)
-  {
-    if (!tbl->table)
-      continue;
-    table= tbl->table;
-    break;
-  }
-  ha_federatedx *h= (ha_federatedx *) table->file;
+  ha_federatedx *h= (ha_federatedx *) query_table->file;
   iop= &h->io;
-  share= get_share(table->s->table_name.str, table);
+  share= get_share(query_table->s->table_name.str, query_table);
   txn= h->get_txn(thd);
   if ((rc= txn->acquire(share, thd, TRUE, iop)))
     DBUG_RETURN(rc);
 
-  if ((*iop)->query(thd->query(), thd->query_length()))
+  if ((*iop)->query(query.ptr(), query.length()))
     goto err;
 
   stored_result= (*iop)->store_result();
@@ -246,7 +245,7 @@ err:
   DBUG_RETURN(HA_FEDERATEDX_ERROR_WITH_REMOTE_SYSTEM);
 }
 
-int ha_federatedx_select_handler::next_row()
+int federatedx_handler_base::next_row_(TABLE *table)
 {
   int rc= 0;
   FEDERATEDX_IO_ROW *row;
@@ -284,19 +283,8 @@ int ha_federatedx_select_handler::next_row()
 
 int ha_federatedx_select_handler::end_scan()
 {
-  DBUG_ENTER("ha_federatedx_derived_handler::end_scan");
-
   free_tmp_table(thd, table);
   table= 0;
 
-  (*iop)->free_result(stored_result);
-
-  free_share(txn, share);
-
-  DBUG_RETURN(0);
-}
-
-void ha_federatedx_select_handler::print_error(int error, myf error_flag)
-{
-  select_handler::print_error(error, error_flag);
+  return federatedx_handler_base::end_scan_();
 }