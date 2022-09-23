009db2288ba8d22c5e8e5e047d8f8694f7097923
Monty <monty@mariadb.org>
2022-09-23 14:48:13 +0300

    Fixed limit optimization in range optimizer
    The issue was that when limit is used,
    SQL_SELECT::test_quick_select would set the cost of table scan to be
    unreasonable high to force a range to be used.
    The problem with this approach was that range was used even when the
    cost of range, when it would only read 'limit rows' would be higher
    than the cost of a table scan.
    
    This patch fixes it by not accepting ranges when the range can never
    have a lower cost than a table scan, even if every row would match the
    WHERE clause.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -19972,6 +19972,7 @@ ha_innobase::multi_range_read_info_const(
 	uint		n_ranges,
 	uint*		bufsz,
 	uint*		flags,
+        ha_rows         limit,
 	Cost_estimate*	cost)
 {
 	/* See comments in ha_myisam::multi_range_read_info_const */
@@ -19981,8 +19982,9 @@ ha_innobase::multi_range_read_info_const(
 		*flags |= HA_MRR_USE_DEFAULT_IMPL;
 	}
 
-	ha_rows res= m_ds_mrr.dsmrr_info_const(keyno, seq, seq_init_param, n_ranges,
-			bufsz, flags, cost);
+	ha_rows res= m_ds_mrr.dsmrr_info_const(keyno, seq, seq_init_param,
+                                               n_ranges,
+                                               bufsz, flags, limit, cost);
 	return res;
 }
 
