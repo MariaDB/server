d6db7071f6dc81a09d53ba1f0053afa499b83e9a
Jan Lindström <jan.lindstrom@mariadb.com>
2022-10-11 09:39:33 +0300

    MDEV-29602 : Galera debug build crashes when the spider plugin is enabled
    Spider system tables should be created so that wsrep_on=OFF.

--- storage/spider/spd_table.cc
+++ storage/spider/spd_table.cc
@@ -10046,6 +10046,8 @@ void *spider_table_bg_sts_action(
       thd->mysys_var->current_cond = &thread->cond;
       thd->mysys_var->current_mutex = &thread->mutex;
     }
+    bool spd_wsrep_on = thd->variables.wsrep_on;
+    thd->variables.wsrep_on = false;
     while (spider_init_queries[i].length && !thd->killed)
     {
       dispatch_command(COM_QUERY, thd, spider_init_queries[i].str,
@@ -10058,6 +10060,7 @@ void *spider_table_bg_sts_action(
       }
       ++i;
     }
+    thd->variables.wsrep_on = spd_wsrep_on;
     thd->mysys_var->current_cond = &thread->cond;
     thd->mysys_var->current_mutex = &thread->mutex;
     thd->client_capabilities -= CLIENT_MULTI_RESULTS;
