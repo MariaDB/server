e44c32f8fd55c6bef39f09bef0cca699266dd649
Denis Protivensky <denis.protivensky@galeracluster.com>
2022-10-20 13:07:05 +0300

    MDEV-29281 Report events from provider (add node eviction event)
    Reviewed-by: Jan Lindström <jan.lindstrom@mariadb.com>

--- sql/wsrep_server_state.cc
+++ sql/wsrep_server_state.cc
@@ -17,6 +17,7 @@
 #include "wsrep_api.h"
 #include "wsrep_server_state.h"
 #include "wsrep_allowlist_service.h"
+#include "wsrep_event_service.h"
 #include "wsrep_binlog.h" /* init/deinit group commit */
 #include "wsrep_plugin.h" /* make/destroy sysvar helpers */
 
@@ -144,6 +145,7 @@ void Wsrep_server_state::destroy()
 void Wsrep_server_state::init_provider_services()
 {
   m_provider_services.allowlist_service= wsrep_allowlist_service_init();
+  m_provider_services.event_service= Wsrep_event_service::instance();
 }
 
 void Wsrep_server_state::deinit_provider_services()
