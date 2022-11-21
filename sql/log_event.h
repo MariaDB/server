4d03cf12ccea89bfae52311008752f8b3261f4aa
Nikita Malyavin <nikitamalyavin@gmail.com>
2022-11-21 12:25:48 +0100

    MDEV-29069 ER_KEY_NOT_FOUND on online autoinc addition + concurrent DELETE
    We can't rely on keys formed with columns that were added during this
    ALTER. These columns can be set with non-deterministic values, which can
    end up with broken or incorrect search.
    
    The same applies to the keys that contain reliable columns, but also have
    bogus ones. Using them can narrow the search, but they're also ignored.
    
    Also, added columns shouldn't be considered during the record match. To
    determine them, table->has_value_set bitmap is used.
    
    To fill has_value_set bitmap in the find_key call, extra unpack_row call
    has been added.
    
    For replication case, extra replica columns can be considered for this
    case. We try to ignore them, too.

--- sql/log_event.h
+++ sql/log_event.h
@@ -4744,7 +4744,10 @@ protected:
   uint      m_key_nr;   /* Key number */
   bool master_had_triggers;     /* set after tables opening */
 
-  int find_key(); // Find a best key to use in find_row()
+  int find_key(const rpl_group_info *); // Find a best key to use in find_row()
+  bool is_key_usable(const KEY *key) const;
+  bool use_pk_position() const;
+
   int find_row(rpl_group_info *);
   int write_row(rpl_group_info *, const bool);
   int update_sequence();
@@ -4810,8 +4813,8 @@ private:
       The member function will return 0 if all went OK, or a non-zero
       error code otherwise.
   */
-  virtual 
-  int do_before_row_operations(const Slave_reporting_capability *const log) = 0;
+  virtual int do_before_row_operations(const rpl_group_info *rgi) = 0;
+
 
   /*
     Primitive to clean up after a sequence of row executions.
@@ -4900,7 +4903,7 @@ private:
 #endif
 
 #if defined(MYSQL_SERVER) && defined(HAVE_REPLICATION)
-  virtual int do_before_row_operations(const Slave_reporting_capability *const);
+  virtual int do_before_row_operations(const rpl_group_info *rgi);
   virtual int do_after_row_operations(const Slave_reporting_capability *const,int);
   virtual int do_exec_row(rpl_group_info *);
 #endif
@@ -4990,7 +4993,7 @@ protected:
 #endif
 
 #if defined(MYSQL_SERVER) && defined(HAVE_REPLICATION)
-  virtual int do_before_row_operations(const Slave_reporting_capability *const);
+  virtual int do_before_row_operations(const rpl_group_info *rgi);
   virtual int do_after_row_operations(const Slave_reporting_capability *const,int);
   virtual int do_exec_row(rpl_group_info *);
 #endif /* defined(MYSQL_SERVER) && defined(HAVE_REPLICATION) */
@@ -5077,7 +5080,7 @@ protected:
 #endif
 
 #if defined(MYSQL_SERVER) && defined(HAVE_REPLICATION)
-  virtual int do_before_row_operations(const Slave_reporting_capability *const);
+  virtual int do_before_row_operations(const rpl_group_info *rgi);
   virtual int do_after_row_operations(const Slave_reporting_capability *const,int);
   virtual int do_exec_row(rpl_group_info *);
 #endif
