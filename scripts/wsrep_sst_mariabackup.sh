63d19ac576921348f472ca014479861bcca17546
Nigel Kukard <nkukard@lbsd.net>
2022-11-30 04:42:09 +0000

    Fix wsrep_sst_mariabackup with timeout from busybox
    timeout from busybox outputs to stderr, redirect the
    output from timeout --help to stdout so our grep
    works in all cases.

--- scripts/wsrep_sst_mariabackup.sh
+++ scripts/wsrep_sst_mariabackup.sh
@@ -793,7 +793,7 @@ recv_joiner()
     local ltcmd="$tcmd"
     if [ $tmt -gt 0 ]; then
         if [ -n "$(commandex timeout)" ]; then
-            if timeout --help | grep -qw -F -- '-k'; then
+            if timeout --help 2>&1 | grep -qw -F -- '-k'; then
                 ltcmd="timeout -k $(( tmt+10 )) $tmt $tcmd"
             else
                 ltcmd="timeout -s9 $tmt $tcmd"
