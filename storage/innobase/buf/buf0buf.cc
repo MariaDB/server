ee1d9cc3dd3cfb18177e188e3db5440bc286080d
Marko Mäkelä <marko.makela@mariadb.com>
2022-12-07 10:13:35 +0200

    MDEV-29986 Set innodb_undo_log_truncate=ON and innodb_undo_tablespaces=3
    Starting with commit baf276e6d4a44fe7cdf3b435c0153da0a42af2b6 (MDEV-19229)
    the parameter innodb_undo_tablespaces can be increased from its
    previous default value 0 while allowing an upgrade from old databases.
    
    We will change the default settings to innodb_undo_tablespaces=3
    and innodb_undo_log_truncate=ON, so that the space occupied by
    possible bursts of undo log records will be eventually reclaimed.

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -589,7 +589,7 @@ bool buf_page_is_corrupted(bool check_lsn, const byte *read_buf,
 		DBUG_EXECUTE_IF(
 			"page_intermittent_checksum_mismatch", {
 			static int page_counter;
-			if (page_counter++ == 3) {
+			if (page_counter++ == 6) {
 				crc32++;
 			}
 		});
@@ -724,7 +724,8 @@ bool buf_page_is_corrupted(bool check_lsn, const byte *read_buf,
 			DBUG_EXECUTE_IF(
 				"page_intermittent_checksum_mismatch", {
 				static int page_counter;
-				if (page_counter++ == 3) return true;
+				if (page_counter++ == 6)
+					return true;
 			});
 
 			if ((checksum_field1 != crc32
