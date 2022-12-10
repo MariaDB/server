c43bd3b41213b3a48f6f6c40ca3522d063d67fd0
Aleksey Midenkov <midenok@gmail.com>
2022-12-10 14:25:49 +0300

    MDEV-25292 Cleanups
    ddl_log_write_execute_entry() cleanup
    
    Rename functions renamed:
      do_rename() -> rename_table_and_triggers()
      do_rename_temporary() -> rename_temporary_table()
      check_rename() -> rename_check_preconditions()

--- sql/handler.cc
+++ sql/handler.cc
@@ -8640,8 +8640,8 @@ bool Table_scope_and_contents_source_st::check_fields(
   THD *thd, Alter_info *alter_info,
   const Lex_table_name &table_name, const Lex_table_name &db)
 {
-  return vers_check_system_fields(thd, alter_info, table_name, db) ||
-    check_period_fields(thd, alter_info);
+  return (vers_check_system_fields(thd, alter_info, table_name, db) ||
+          check_period_fields(thd, alter_info));
 }
 
 bool Table_scope_and_contents_source_st::check_period_fields(
