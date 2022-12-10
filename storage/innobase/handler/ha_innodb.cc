3e62acb6655818e0af9f8e751169dda8f0882b0d
Aleksey Midenkov <midenok@gmail.com>
2022-12-10 14:25:51 +0300

    MDEV-28933 CREATE OR REPLACE fails to recreate same constraint name
    Use temporary constraint names for temporary tables. The constraints
    are not added to cache (skipped in dict_table_rename_in_cache()).
    
    The scheme for temporary constraint names is as follows:
    
        for old table: db_name/\xFFconstraint_name
        for new table: db_name/\xFF\xFFconstraint_name
    
    normalize_table_name_c_low(): wrong comparison "less than FN_REFLEN -
    1". Somewhere array of FN_REFLEN includes the trailing 0, somewhere
    array of FN_REFLEN + 1 includes trailing 0, but nowhere array of
    FN_REFLEN - 1 must include trailing 0.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -5196,7 +5196,7 @@ normalize_table_name_c_low(
 	db_ptr = ptr + 1;
 
 	norm_len = db_len + name_len + sizeof "/";
-	ut_a(norm_len < FN_REFLEN - 1);
+	ut_a(norm_len < FN_REFLEN);
 
 	memcpy(norm_name, db_ptr, db_len);
 
@@ -12370,6 +12370,7 @@ create_table_info_t::create_foreign_keys()
 
 		if (fk->constraint_name.str) {
 			ulint db_len;
+			const bool tmp= m_create_info->is_atomic_replace();
 
 			/* Catenate 'databasename/' to the constraint name
 			specified by the user: we conceive the constraint as
@@ -12379,13 +12380,17 @@ create_table_info_t::create_foreign_keys()
 			db_len = dict_get_db_name_len(table->name.m_name);
 
 			foreign->id = static_cast<char*>(mem_heap_alloc(
-				foreign->heap,
-				db_len + fk->constraint_name.length + 2));
+				foreign->heap, (tmp ? 3 : 2)
+				+ db_len + fk->constraint_name.length));
 
-			memcpy(foreign->id, table->name.m_name, db_len);
-			foreign->id[db_len] = '/';
-			strcpy(foreign->id + db_len + 1,
-			       fk->constraint_name.str);
+			char *pos = foreign->id;
+			memcpy(pos, table->name.m_name, db_len);
+			pos += db_len;
+			*(pos++) = '/';
+			if (tmp) {
+				*(pos++) = '\xFF';
+			}
+			strcpy(pos, fk->constraint_name.str);
 		}
 
 		if (foreign->id == NULL) {
