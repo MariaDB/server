6b8528c47f78fa846b2f827a29285259f5663d42
Julius Goryavsky <julius.goryavsky@mariadb.com>
2022-12-19 10:29:55 +0100

    MDEV-28669 addendum: additional tests and mtr changes
    This commit contains additions to mtr:
    
    1) Running .sh scripts (which can be executed before running
       the test) moved to an earlier stage so that they can create
       directories or perform other useful actions before the boostrap
       phase of the test;
    2) Changes have been added that allow the user to set a custom
       location for datadir during a test, both as an option on the
       command line and via a configuration parameter - so that tests
       can be created that use a custom datadir location, and also
       check work with this parameter;
    3) Added more precise filters for options that require a new
       bootstrap with rebuilding the database before starting the test;
    4) Now mtr sets an environment variable MTR_BOOTSTRAP_OPTS_<suffix>
       that can be used in scripts if these scripts need to know with
       what additional bootstrap parameters was used when server was
       started - which is relevant for those tests that require individual
       bootstrap and then re-start server from within - these tests need
       to know what additional the parameters must be passed to the
       server (except for those specific to this particular text itself),
       for example, this facilitates the launch of tests for galera
       recovery.
    
    This commit also contains tests for Galera: for MDEV-28669,
    MDEV-30157, MDEV-30220, MDEV-29591, which depend on the presence
    of the above changes in mtr and are themselves a check for these
    changes.

---
+++ mysql-test/suite/galera/t/galera_sst_rsync_data_dir-slave.sh
@@ -0,0 +1,2 @@
+rm -rf $MYSQLTEST_VARDIR/mysqld.2/datadir2
+mkdir -p $MYSQLTEST_VARDIR/mysqld.2/datadir2
---
+++ mysql-test/suite/galera/t/galera_sst_rsync_innodb_dir-master.sh
@@ -0,0 +1,6 @@
+rm -rf $MYSQLTEST_VARDIR/mysqld.1/innodb_data1
+rm -rf $MYSQLTEST_VARDIR/mysqld.1/innodb_undo1
+rm -rf $MYSQLTEST_VARDIR/mysqld.1/innodb_group1
+mkdir -p $MYSQLTEST_VARDIR/mysqld.1/innodb_data1
+mkdir -p $MYSQLTEST_VARDIR/mysqld.1/innodb_undo1
+mkdir -p $MYSQLTEST_VARDIR/mysqld.1/innodb_group1
