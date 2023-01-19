148a8c20155def2ceb3d4818a013228f440d566d
Anel Husakovic <anel@mariadb.org>
2023-01-19 14:51:17 +0100

    MDEV-21322: show replica hosts update result with GTID states
    + We are trying to extend the command `SHOW REPLICA HOSTS` that is
    executed on master, with columns `GTID_state_sent` and `GTID_state_ack`.
    In order to achieve that we have to extend `thd->slave_info` struct with
    2 new entries. This struct needs to be populated and  must be accessable to primary.
    
    + The first column `GTID_state_sent`:
      - It is populated by the last event that primary has sent to replica
        (only for semi-sync slave connection, but without need to know
        that replica actually obtained request,
        like it is case in asynchronous replication).
      - Primary executes `mysql_binlog` that first starts the `binlog_dump` thread.
        That thread starts `ack_receiver` thread, that returns second column
        of interest (in first iteration we don't care about).
        After `binlog_dump` thread creating, file is sent with
        `send_one_binlog_file()` function.
        This function is sending event with `send_events()` function,
        during which `send_event_to_slave()` function is executed.
        On that place we are creating the `thd->slave_info->gtid_state_sent` struct.
    
    + The second column `GTID_state_ack`:
      - It is populated by the `reply_packet_binlog` (a.k.a. `ack`) that is called,
        only for semi-sync replication. It is called on 2 places:
        a) during `binlog_dump` creation, after thread is added to
        `ack_reciver` and after thread is added as semi-sync slave. This
        should be first ack received from replica to primary.
        b) constantly in running phase of `ack_receiver` thread `run()`.
      - A the end of function `reply_packet_binlog` we are creating the
      struct `GTID_state_ack`
    
    + The third column `Sync_Status`
      - It should be filled with values `asynchronous`,`semi-sync active`,
        `semi-sync stale` in order to express the state of replica.
      - `semi-sync [active/stale]` related states depend on the value of `need_sync`
         that is updated on master and will be `semi-sync active` iff:
        - primary and replicas are configured to be in semi-sync mode and
        - if the event is a transaction's ending event.
        Otherwise state is `semi-sync stale`.
      - state is `asynchronous` if semi-sync is disabled on master as fast
      check and if there thd is not semi-sync thread.
    
    + Handling of `rpl_semi_sync_master_timeout= 0` means that primary status
      `rpl_semi_sync_master_status` will remain ON instead of switching off to async
      and incrementing the counter updates.
    
    + PR closes #2374, #1427
    
    Reviewer:<brandon.nesterenko@mariadb.com>

--- sql/repl_failsafe.cc
+++ sql/repl_failsafe.cc
@@ -37,18 +37,7 @@
 #include "rpl_filter.h"
 #include "log_event.h"
 #include <mysql.h>
-
-
-struct Slave_info
-{
-  uint32 server_id;
-  uint32 master_id;
-  char host[HOSTNAME_LENGTH*SYSTEM_CHARSET_MBMAXLEN+1];
-  char user[USERNAME_LENGTH+1];
-  char password[MAX_PASSWORD_LENGTH*SYSTEM_CHARSET_MBMAXLEN+1];
-  uint16 port;
-};
-
+#include "semisync_master.h"
 
 Atomic_counter<uint32_t> binlog_dump_thread_count;
 ulong rpl_status=RPL_NULL;
@@ -125,8 +114,11 @@ int THD::register_slave(uchar *packet, size_t packet_length)
   if (check_access(this, PRIV_COM_REGISTER_SLAVE, any_db.str, NULL,NULL,0,0))
     return 1;
   if (!(si= (Slave_info*)my_malloc(key_memory_SLAVE_INFO, sizeof(Slave_info),
-                                   MYF(MY_WME))))
+                                   MYF(MY_ZEROFILL))))
     return 1;
+  memset(si->gtid_state_sent.log_file, '\0', FN_REFLEN);
+  memset(si->gtid_state_ack.log_file, '\0', FN_REFLEN);
+  si->semi_sync_trans_status= false;
 
   variables.server_id= si->server_id= uint4korr(p);
   p+= 4;
@@ -179,7 +171,11 @@ static my_bool show_slave_hosts_callback(THD *thd, Protocol *protocol)
 {
   my_bool res= FALSE;
   mysql_mutex_lock(&thd->LOCK_thd_data);
-  if (auto si= thd->slave_info)
+  String gtid_sent[GTID_MAX_STR_LENGTH];
+  String gtid_ack[GTID_MAX_STR_LENGTH];
+  const char *sync_str_opt[]= {"asynchronous","semi-sync active","semi-sync stale"};
+  char *sync_str= (char *)sync_str_opt[0];
+  if (const Slave_info *si= thd->slave_info)
   {
     protocol->prepare_for_resend();
     protocol->store(si->server_id);
@@ -189,8 +185,20 @@ static my_bool show_slave_hosts_callback(THD *thd, Protocol *protocol)
       protocol->store(si->user, safe_strlen(si->user), &my_charset_bin);
       protocol->store(si->password, safe_strlen(si->password), &my_charset_bin);
     }
+    gtid_state_from_binlog_pos(si->gtid_state_sent.log_file,
+                               (uint32)si->gtid_state_sent.log_pos, gtid_sent);
+    if (rpl_semi_sync_master_enabled && thd->semi_sync_slave)
+    {
+      gtid_state_from_binlog_pos(si->gtid_state_ack.log_file,
+                                 (uint32)si->gtid_state_ack.log_pos, gtid_ack);
+      sync_str= si->semi_sync_trans_status ?
+                  (char *)sync_str_opt[1] :(char *)sync_str_opt[2];
+    }
     protocol->store((uint32) si->port);
     protocol->store(si->master_id);
+    protocol->store(gtid_sent);
+    protocol->store(gtid_ack);
+    protocol->store(sync_str, safe_strlen(sync_str), &my_charset_bin);
     res= protocol->write();
   }
   mysql_mutex_unlock(&thd->LOCK_thd_data);
@@ -234,6 +242,18 @@ bool show_slave_hosts(THD* thd)
   field_list.push_back(new (mem_root)
                        Item_return_int(thd, "Master_id", 10, MYSQL_TYPE_LONG),
                        thd->mem_root);
+
+ field_list.push_back(new (mem_root)
+                        Item_empty_string(thd, "Gtid_State_Sent", GTID_MAX_STR_LENGTH),
+                        thd->mem_root);
+
+ field_list.push_back(new (mem_root)
+                        Item_empty_string(thd, "Gtid_State_Ack", GTID_MAX_STR_LENGTH),
+                        thd->mem_root);
+
+ field_list.push_back(new (mem_root)
+                        Item_empty_string(thd, "Sync_Status", GTID_MAX_STR_LENGTH),
+                        thd->mem_root);
 
   if (protocol->send_result_set_metadata(&field_list,
                             Protocol::SEND_NUM_ROWS | Protocol::SEND_EOF))
