6c16a182f2c04c7b45e50be365f5391a0b3ee088
Trevor Gross <tmgross@umich.edu>
2023-01-19 13:58:18 -0500

    Reworked some example organization

--- rust/examples/keymgt-clevis/src/lib.rs
+++ rust/examples/keymgt-clevis/src/lib.rs
@@ -82,32 +82,6 @@ impl KeyManager for DebugKeyMgmt {
     }
 }
 
-// bindings::st_maria_plugin {
-//     type_: PluginType::Encryption,
-//     info: *mut c_void,
-//     name: cstr::cstr!($name).as_ptr(),
-//     author: cstr::cstr!($author).as_ptr(),
-//     descr: cstr::cstr!($description).as_ptr(),
-//     license: c_int,
-//     init: Option<unsafe extern "C" fn(arg1: *mut c_void) -> c_int>,
-//     deinit: Option<unsafe extern "C" fn(arg1: *mut c_void) -> c_int>,
-//     version: vers as c_uint,
-//     status_vars: *mut st_mysql_show_var,
-//     system_vars: *mut *mut st_mysql_sys_var,
-//     version_info: plugin!(
-//         @def
-//         cstr::cstr!($vers).as_ptr(),
-//         $(cstr::cstr!($comment).as_ptr())?
-//     ),,
-//     maturity: maturity as c_uint,
-// }
-
-// #![crate_type = "cdylib"]
-
-// builtin_debug_key_plugin_interface_version
-// builtin_debug_key_sizeof_struct_st_plugin
-// builtin_debug_key_plugin
-
 register_plugin! {
     DebugKeyMgmt,
     ptype: PluginType::MariaEncryption,
@@ -128,44 +102,3 @@ register_plugin! {
 //         KEY_VERSION_SYSVAR.as_ptr().cast_mut(),
 //         ::std::ptr::null_mut(),
 //     ]));
-
-// static _ENCRYPTION_ST: UcWrap<mariadb::bindings::st_mariadb_encryption> =
-//     UcWrap(UnsafeCell::new(mariadb::bindings::st_mariadb_encryption {
-//         interface_version: mariadb::bindings::MariaDB_ENCRYPTION_INTERFACE_VERSION as i32,
-//         get_latest_key_version: Some(DebugKeyMgmt::wrap_get_latest_key_version),
-//         get_key: Some(DebugKeyMgmt::wrap_get_key),
-//         crypt_ctx_size: None,
-//         crypt_ctx_init: None,
-//         crypt_ctx_update: None,
-//         crypt_ctx_finish: None,
-//         encrypted_length: None,
-//     }));
-
-// #[no_mangle]
-// static _maria_plugin_interface_version_: ::std::ffi::c_int =
-//     mariadb::bindings::MARIA_PLUGIN_INTERFACE_VERSION as ::std::ffi::c_int;
-
-// #[no_mangle]
-// static _maria_sizeof_struct_st_plugin_: ::std::ffi::c_int =
-//     ::std::mem::size_of::<mariadb::bindings::st_maria_plugin>() as ::std::ffi::c_int;
-
-// #[no_mangle]
-// static mut _maria_plugin_declarations_: [mariadb::bindings::st_maria_plugin; 2] = [
-//     mariadb::bindings::st_maria_plugin {
-//         type_: PluginType::MariaEncryption as i32,
-//         info: _ENCRYPTION_ST.as_ptr().cast_mut().cast(),
-//         name: mariadb::cstr::cstr!("debug_key_management").as_ptr(),
-//         author: mariadb::cstr::cstr!("Trevor Gross").as_ptr(),
-//         descr: mariadb::cstr::cstr!("Debug key management plugin").as_ptr(),
-//         license: License::Gpl as i32,
-//         init: Some(DebugKeyMgmt::wrap_init),
-//         deinit: Some(DebugKeyMgmt::wrap_deinit),
-//         version: 0x0010,
-//         status_vars: ::std::ptr::null_mut(),
-//         system_vars: _INTERNAL_SYSVARS.0.get().cast(),
-//         version_info: mariadb::cstr::cstr!("0.1").as_ptr(),
-//         maturity: Maturity::Experimental as u32,
-//     },
-//     // End with a null object
-//     ::mariadb::plugin::wrapper::new_null_st_maria_plugin(),
-// ];
