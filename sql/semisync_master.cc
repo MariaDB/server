1e2bf2b9971d826b2a04d9c3d5292948543626ea
Anel Husakovic <anel@mariadb.org>
2023-01-19 14:51:17 +0100

    MDEV-21322: report slave progress to the master
    This patch extends the command `SHOW REPLICA HOSTS` with three
    columns:
    
     1) Gtid_State_Sent. This represents that latest GTIDs sent to the
    replica in each domain. It will always be populated, regardless of
    the semi-sync status (i.e. asynchronous connections will still
    update this column with the latest GTID state sent to the replica).
    
     2) Gtid_State_Ack. For semi-synchronous connections (only), this
    column represents the last GTID in each domain that the replica has
    acknowledged.
    
     3) Sync_Status. This value represents the synchronization status of
    the replica, and is used to help determine how to interpret the
    Gtid_State_Ack column. There are four possible values:
    
       3.1) Initializing. This means the binlog dump thread is still
    initializing, and has not yet determined the synchronization status
    of the replica.
    
       3.2) Asynchronous: This means the replica is not configured for
    semi-sync replication, and thereby, Gtid_State_Ack should always be
    empty.
    
       3.3) Semi-sync Stale: This means the replica is configured for
    semi-sync replication, however, connected using an old state, and is
    not readily able to send ACKs for new transactions. Functionally,
    this means that the primary will try to catch the replica up-to-date
    by sending transactions which will not be ACKed. Additionally, the
    value shown by Gtid_State_Ack will be empty until the replica
    catches up and ACKs its first transaction.
    
       3.4) Semi-sync Active: This means the replica is configured for
    semi-sync replication, and is readily sending ACKs for new
    transactions it receives. It is possible for Gtid_State_Ack to be
    empty while Sync_Status is "Semi-sync Active" if no new transactions
    have been executed on the primary since the replica has connected.
    
    Additionally, this patch creates a new semantic for the
    configuration rpl_semi_sync_master_timeout=0. That is, now when 0,
    1) new transactions will not attempt to wait for an ACK before
    completing, and 2) the primary will still request ACKs from the
    replica for new transactions. This means that Gtid_State_Ack will be
    updated for each ACK from the replica and Sync_Status will read as
    "Semi-sync Active". Effectively, this creates a mode to mimic the
    asynchronous connection behavior, while allowing one to monitor the
    progress at which the primary is sending transactions to the replica
    via the new columns Gtid_State_Sent and Gtid_State_Ack.
    
    Also note that a new error message was added to account for the case
    that Gtid_State_(Sent/Ack) represents a binary log file that was
    purged/cannot be found.
    
    The overall implementation is rather simple. It leverages the
    existing semi-sync framework, where the replica uses binlog file:pos
    to ACK transactions, in order to infer GTID state by performing a
    binlog lookup at the time `SHOW REPLICA HOSTS` is executed. In
    particular, the Slave_info struct is extended to store 1) the binlog
    file:pos pair of the transaction which was last sent to the replica,
    2) the binlog file:pos pair that was last ACKed by the replica, and
    3) and enum to represent the Sync_Status.
    
    This patch was initially started by @JackSlateur in PR#1427, where
    it was then transferred to @an3l who buffed it out in PR#2374, and
    final touches were put on by @bnestere.
    
    Reviewed By:
    ============
    <TODO>

--- sql/semisync_master.cc
+++ sql/semisync_master.cc
@@ -53,13 +53,6 @@ ulonglong rpl_semi_sync_master_trx_wait_time = 0;
 Repl_semi_sync_master repl_semisync_master;
 Ack_receiver ack_receiver;
 
-/*
-  structure to save transaction log filename and position
-*/
-typedef struct Trans_binlog_info {
-  my_off_t log_pos;
-  char log_file[FN_REFLEN];
-} Trans_binlog_info;
 
 static int get_wait_time(const struct timespec& start_ts);
 
@@ -547,7 +540,7 @@ void Repl_semi_sync_master::remove_slave()
   @retval -1  Slave is going down (ok)
 */
 
-int Repl_semi_sync_master::report_reply_packet(uint32 server_id,
+int Repl_semi_sync_master::report_reply_packet(Slave_info *slave_info,
                                                const uchar *packet,
                                                ulong packet_len)
 {
@@ -591,12 +584,12 @@ int Repl_semi_sync_master::report_reply_packet(uint32 server_id,
 
   DBUG_ASSERT(dirname_length(log_file_name) == 0);
 
-  DBUG_PRINT("semisync", ("%s: Got reply(%s, %lu) from server %u",
-                          "Repl_semi_sync_master::report_reply_packet",
-                          log_file_name, (ulong)log_file_pos, server_id));
-
+  DBUG_PRINT("semisync",
+             ("%s: Got reply(%s, %lu) from server %u",
+              "Repl_semi_sync_master::report_reply_packet", log_file_name,
+              (ulong) log_file_pos, slave_info->server_id));
   rpl_semi_sync_master_get_ack++;
-  report_reply_binlog(server_id, log_file_name, log_file_pos);
+  report_reply_binlog(slave_info, log_file_name, log_file_pos);
   DBUG_RETURN(0);
 
 l_end:
@@ -605,13 +598,13 @@ l_end:
     octet2hex(buf, (const unsigned char*) packet,
               MY_MIN(sizeof(buf)-1, (size_t) packet_len));
     sql_print_information("First bytes of the packet from semisync slave "
-                          "server-id %d: %s", server_id, buf);
+                          "server-id %d: %s", slave_info->server_id, buf);
 
   }
   DBUG_RETURN(result);
 }
 
-int Repl_semi_sync_master::report_reply_binlog(uint32 server_id,
+int Repl_semi_sync_master::report_reply_binlog(Slave_info *slave_info,
                                                const char *log_file_name,
                                                my_off_t log_file_pos)
 {
@@ -632,7 +625,7 @@ int Repl_semi_sync_master::report_reply_binlog(uint32 server_id,
 
   if (!is_on())
     /* We check to see whether we can switch semi-sync ON. */
-    try_switch_on(server_id, log_file_name, log_file_pos);
+    try_switch_on(slave_info->server_id, log_file_name, log_file_pos);
 
   /* The position should increase monotonically, if there is only one
    * thread sending the binlog to the slave.
@@ -691,6 +684,9 @@ int Repl_semi_sync_master::report_reply_binlog(uint32 server_id,
       m_wait_file_name_inited = false;
     }
   }
+
+  strncpy(slave_info->gtid_state_ack.log_file, log_file_name, strlen(log_file_name));
+  slave_info->gtid_state_ack.log_pos= log_file_pos;
 
  l_end:
   unlock();
@@ -801,7 +797,7 @@ int Repl_semi_sync_master::dump_start(THD* thd,
   }
 
   add_slave();
-  report_reply_binlog(thd->variables.server_id,
+  report_reply_binlog(thd->slave_info,
                       log_file + dirname_length(log_file), log_pos);
   sql_print_information("Start semi-sync binlog_dump to slave "
                         "(server_id: %ld), pos(%s, %lu)",
@@ -830,6 +826,16 @@ int Repl_semi_sync_master::commit_trx(const char* trx_wait_binlog_name,
 {
   bool success= 0;
   DBUG_ENTER("Repl_semi_sync_master::commit_trx");
+
+  /*
+    If the semi-sync timeout is set to 0, we effectively are configured for
+    asynchronous replication; except we still want to request/receive ACKs from
+    slaves so we can monitor replication status via SHOW SLAVE HOSTS columns
+    Gtid_State_Sent and Gtid_State_Ack. Thereby, we should quit now before
+    updating rpl_semi_sync_master_(no/yes)_transactions.
+  */
+  if (!m_wait_timeout)
+    DBUG_RETURN(0);
 
   if (!rpl_semi_sync_master_clients && !rpl_semi_sync_master_wait_no_slave)
   {
@@ -1178,6 +1184,14 @@ int Repl_semi_sync_master::update_sync_header(THD* thd, unsigned char *packet,
   *need_sync= sync;
 
  l_end:
+  if (is_on())
+  {
+    thd->slave_info->sync_status=
+        sync ? thd->slave_info->sync_status=
+                   Slave_info::SYNC_STATE_SEMI_SYNC_ACTIVE
+             : thd->slave_info->sync_status=
+                   Slave_info::SYNC_STATE_SEMI_SYNC_STALE;
+  }
   unlock();
 
   /*
