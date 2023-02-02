a777a8a6a3c71ede8b88d20ab5c1a95590138611
Sergei Golubchik <serg@mariadb.org>
2023-02-02 19:29:03 +0100

    KILL USER and missing privileges
    note that `KILL USER foo` should *not* fail with ER_KILL_DENIED_ERROR
    when SHOW PROCESSLIST doesn't show connections of that user.
    Because no connections exist or because the caller has no PROCESS -
    doesn't matter.
    
    also, fix the error message to make sense
    ("You are not owner of thread <current connection id>" is ridiculous)

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -9258,7 +9258,9 @@ static my_bool kill_threads_callback(THD *thd, kill_threads_callback_arg *arg)
     {
       if (!(arg->thd->security_ctx->master_access & SUPER_ACL) &&
           !arg->thd->security_ctx->user_matches(thd->security_ctx))
-        return 1;
+      {
+        return MY_TEST(arg->thd->security_ctx->master_access & PROCESS_ACL);
+      }
       if (!arg->threads_to_kill.push_back(thd, arg->thd->mem_root))
       {
         mysql_mutex_lock(&thd->LOCK_thd_kill); // Lock from delete
@@ -9380,7 +9382,10 @@ void sql_kill_user(THD *thd, LEX_USER *user, killed_state state)
     my_ok(thd, rows);
     break;
   case ER_KILL_DENIED_ERROR:
-    my_error(error, MYF(0), (long long) thd->thread_id);
+    char buf[DEFINER_LENGTH+1];
+    strxnmov(buf, sizeof(buf), user->user.str, "@", user->host.str, NULL);
+    my_printf_error(ER_KILL_DENIED_ERROR, ER_THD(thd, ER_CANNOT_USER), MYF(0),
+                    "KILL USER", buf);
     break;
   case ER_OUT_OF_RESOURCES:
   default:
