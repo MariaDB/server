snorlaxrin/MariaDB-Server

MDEV-28152 create sequences with various signed value types
- Support signed types. Unsigned types to be worked out in the next patch.
- Sequence (definition) tables uses the value type for the field type
  of minvalue, maxvalue, startvalue, next_not_cached_value, so that
  the value type can be inferred from this type.

sql/sql_yacc.yy
@@ -2433,7 +2433,7 @@ create:
             }
 
             /* No fields specified, generate them */
-            if (unlikely(prepare_sequence_fields(thd,
+            if (unlikely(lex->create_info.seq_create_info->prepare_sequence_fields(thd,
                          &lex->alter_info.create_list)))
                MYSQL_YYABORT;
 
@@ -2609,7 +2609,14 @@ sequence_defs:
         ;
 
 sequence_def:
-          MINVALUE_SYM opt_equal ulonglong_num
+          AS int_type
+          {
+            if (unlikely(Lex->create_info.seq_create_info->used_fields &
+                         seq_field_used_as))
+              my_yyabort_error((ER_DUP_ARGUMENT, MYF(0), "AS"));
+            Lex->create_info.seq_create_info->value_type = $2->field_type();
+          }
+        | MINVALUE_SYM opt_equal ulonglong_num
           {
             if (unlikely(Lex->create_info.seq_create_info->used_fields &
                          seq_field_used_min_value))
@@ -2619,6 +2626,7 @@ sequence_def:
             else
               Lex->create_info.seq_create_info->min_value= (longlong) $3;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_min_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_min_value;
           }
         | MINVALUE_SYM opt_equal minus_ulonglong_num
           {
@@ -2630,6 +2638,7 @@ sequence_def:
               else
                   Lex->create_info.seq_create_info->min_value= - (longlong) $3;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_min_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_min_value;
           }
         | MINVALUE_SYM opt_equal DECIMAL_NUM
           {
@@ -2638,6 +2647,7 @@ sequence_def:
               my_yyabort_error((ER_DUP_ARGUMENT, MYF(0), "MINVALUE"));
             Lex->create_info.seq_create_info->min_value= LONGLONG_MAX;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_min_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_min_value;
           }
         | MINVALUE_SYM opt_equal '-' DECIMAL_NUM
           {
@@ -2646,6 +2656,7 @@ sequence_def:
               my_yyabort_error((ER_DUP_ARGUMENT, MYF(0), "MINVALUE"));
             Lex->create_info.seq_create_info->min_value= LONGLONG_MIN;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_min_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_min_value;
           }
         | NO_SYM MINVALUE_SYM
           {
@@ -2669,6 +2680,7 @@ sequence_def:
             else
               Lex->create_info.seq_create_info->max_value= (longlong) $3;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_max_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_max_value;
           }
         | MAXVALUE_SYM opt_equal minus_ulonglong_num
           {
@@ -2680,6 +2692,7 @@ sequence_def:
             else
               Lex->create_info.seq_create_info->max_value= - (longlong) $3;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_max_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_max_value;
           }
         | MAXVALUE_SYM opt_equal DECIMAL_NUM
           {
@@ -2688,6 +2701,7 @@ sequence_def:
               my_yyabort_error((ER_DUP_ARGUMENT, MYF(0), "MAXVALUE"));
             Lex->create_info.seq_create_info->max_value= LONGLONG_MAX;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_max_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_max_value;
           }
         | MAXVALUE_SYM opt_equal '-' DECIMAL_NUM
           {
@@ -2696,6 +2710,7 @@ sequence_def:
               my_yyabort_error((ER_DUP_ARGUMENT, MYF(0), "MAXVALUE"));
             Lex->create_info.seq_create_info->max_value= LONGLONG_MIN;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_max_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_max_value;
           }
         | NO_SYM MAXVALUE_SYM
           {

MariaDB/server

sql/sql_yacc.yy
@@ -2429,8 +2429,8 @@ create:
             }
 
             /* No fields specified, generate them */
-            if (unlikely(prepare_sequence_fields(thd,
-                         &lex->alter_info.create_list)))
+            if (unlikely(lex->create_info.seq_create_info->prepare_sequence_fields(
+                &lex->alter_info.create_list)))
                MYSQL_YYABORT;
 
             /* CREATE SEQUENCE always creates a sequence */
@@ -2605,13 +2605,21 @@ sequence_defs:
         ;
 
 sequence_def:
-          MINVALUE_SYM opt_equal sequence_truncated_value_num
+          AS int_type
+          {
+            if (unlikely(Lex->create_info.seq_create_info->used_fields &
+                         seq_field_used_as))
+              my_yyabort_error((ER_DUP_ARGUMENT, MYF(0), "AS"));
+            Lex->create_info.seq_create_info->value_type = $2->field_type();
+          }
+        | MINVALUE_SYM opt_equal sequence_truncated_value_num
           {
             if (unlikely(Lex->create_info.seq_create_info->used_fields &
                          seq_field_used_min_value))
               my_yyabort_error((ER_DUP_ARGUMENT, MYF(0), "MINVALUE"));
             Lex->create_info.seq_create_info->min_value= $3;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_min_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_min_value;
           }
         | NO_SYM MINVALUE_SYM
           {
@@ -2632,6 +2640,7 @@ sequence_def:
               my_yyabort_error((ER_DUP_ARGUMENT, MYF(0), "MAXVALUE"));
             Lex->create_info.seq_create_info->max_value= $3;
             Lex->create_info.seq_create_info->used_fields|= seq_field_used_max_value;
+            Lex->create_info.seq_create_info->used_fields|= seq_field_specified_max_value;
           }
         | NO_SYM MAXVALUE_SYM
           {