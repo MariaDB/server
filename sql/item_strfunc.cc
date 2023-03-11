745fd544598b07b35cad39539a6233b6a9481120
Weijun Huang <huangweijun1001@gmail.com>
2023-03-11 11:06:03 +0100

    MDEV-30351 crash in Item_func_left::val_str
    When using LEFT() function with a string that is without a charset,
    the function crashes. This is because the function assumes that
    the string has a charset, and tries to use it to calculate the
    length of the string.
    
    Two functions, UNHEX and WEIGHT_STRING, returned a string without
    the charset being set to a not null value.
    
    The fix is to set charset when calling val_str on these two functions.
    
    Reviewed-by: Alexander Barkov <bar@mariadb.com>
    Reviewed-by: Daniel Black <daniel@mariadb.org>

--- sql/item_strfunc.cc
+++ sql/item_strfunc.cc
@@ -3729,6 +3729,7 @@ String *Item_func_weight_string::val_str(String *str)
                                  flags);
   DBUG_ASSERT(frm_length <= tmp_length);
 
+  str->set_charset(&my_charset_bin);
   str->length(frm_length);
   null_value= 0;
   return str;
@@ -3808,6 +3809,7 @@ String *Item_func_unhex::val_str(String *str)
 
   from= res->ptr();
   null_value= 0;
+  str->set_charset(&my_charset_bin);
   str->length(length);
   to= (char*) str->ptr();
   if (res->length() % 2)
