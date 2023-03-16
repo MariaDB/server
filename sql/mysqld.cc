84bb01b1d6eea76172f217d57bc9b611514a0815
Sergei Golubchik <serg@mariadb.org>
2023-03-16 21:48:08 +0100

    don't abort if invalid SSL setup
    THIS INTRODUCES A SECURITY ISSUE - THE SERVER AUTOMATICALLY DISABLES
    SSL WHEN IT'S REQUIRED, SO A USER WILL THINK THE CONNECTION IS
    SECURE WHILE IN FACT IT SILENTLY WON'T BE.
    
    MUST NOT BE PUSHED INTO A MAIN BRANCH

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -4438,8 +4438,6 @@ static void init_ssl()
     {
       sql_print_error("Failed to setup SSL");
       sql_print_error("SSL error: %s", sslGetErrString(error));
-      if (!opt_bootstrap)
-        unireg_abort(1);
       opt_use_ssl = 0;
       have_ssl= SHOW_OPTION_DISABLED;
     }
