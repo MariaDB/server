7e7e12e747a8284efea697518940f6a647ff915c
Oleg Smirnov <olernov@gmail.com>
2023-04-24 18:38:42 +0700

    MDEV-30765 SHOW TABLES not working properly with lower_case_table_names=2
    lower_case_table_names=2 means "table names and database names are
    stored as declared, but they are compared in lowercase".
    But names of objects in grants are stored in lowercase for any value
    of lower_case_table_names. This caused an error when checking grants
    for objects containing uppercase letters since table_hash_search()
    didn't take into account lower_case_table_names value

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -5462,7 +5462,7 @@ table_hash_search(const char *host, const char *ip, const char *db,
 		  const char *user, const char *tname, bool exact)
 {
   return (GRANT_TABLE*) name_hash_search(&column_priv_hash, host, ip, db,
-					 user, tname, exact, FALSE);
+					 user, tname, exact, (lower_case_table_names > 0));
 }
 
 static bool column_priv_insert(GRANT_TABLE *grant)
