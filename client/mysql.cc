f272463b028d3c377acf02b4818ba8d607f71d1c
Monty <monty@mariadb.org>
2023-04-25 21:16:43 +0300

    Cleanup of MDEV-14974: --port ignored for --host=localhost
    The old code added to 10.6 was inconsisting in how TCP/IP and
    socket connection was chosen. One got also a confusing warning
    in some cases.
    
    Examples:
    > ../client/mysql --print-defaults
    ../client/mysql would have been started with the following arguments:
    --socket=/tmp/mariadbd.sock --port=3307 --no-auto-rehash
    > ../client/mysql
    ERROR 2002 (HY000): Can't connect to local server through socket '/tmp/mariadbd.sock' (2)
    > ../client/mysql --print-defaults
    ../client/mysql would have been started with the following arguments:
    --socket=/tmp/mariadbd.sock --port=3307 --no-auto-rehash
    > ../client/mysql --port=3333
    WARNING: Forcing protocol to  TCP  due to option specification. Please explicitly state intended protocol.
    ERROR 2002 (HY000): Can't connect to server on 'localhost' (111)
    > ../client/mysql --port=3333 --socket=sss
    ERROR 2002 (HY000): Can't connect to local server through socket 'sss' (2)
    > ../client/mysql --socket=sss --port=3333
    ERROR 2002 (HY000): Can't connect to local server through socket 'sss' (2)
    
    Some notable things:
    - One gets a warning if one uses just --port if config file sets socket
    - Using port and socket gives no warning
    - Using socket and then port still uses socket
    
    This patch changes things the following ways:
    If --port= is given on the command line, the the protocol is automatically
      changed to "TCP/IP".
    - If --socket= is given on the command line, the protocol is automatically
      changed to "socket".
    - The last option wins
    - No warning is given if protocol changes automatically.

--- client/mysql.cc
+++ client/mysql.cc
@@ -210,8 +210,6 @@ static uint opt_protocol=0;
 static const char *opt_protocol_type= "";
 static CHARSET_INFO *charset_info= &my_charset_latin1;
 
-static uint protocol_to_force= MYSQL_PROTOCOL_DEFAULT;
-
 #include "sslopt-vars.h"
 
 const char *default_dbug_option="d:t:o,/tmp/mariadb.trace";
@@ -1180,14 +1178,6 @@ int main(int argc,char *argv[])
     exit(status.exit_status);
   }
 
-  /* Command line options override configured protocol */
-  if (protocol_to_force > MYSQL_PROTOCOL_DEFAULT
-      && protocol_to_force != opt_protocol)
-  {
-    warn_protocol_override(current_host, &opt_protocol, protocol_to_force);
-  }
-
-
   if (status.batch && !status.line_buff &&
       !(status.line_buff= batch_readline_init(MAX_BATCH_BUFFER_SIZE, stdin)))
   {
@@ -1737,11 +1727,9 @@ static void usage(int version)
 
 
 my_bool
-get_one_option(const struct my_option *opt, const char *argument, const char *filename)
+get_one_option(const struct my_option *opt, const char *argument,
+               const char *filename)
 {
-  /* Track when protocol is set via CLI to not force port TCP protocol override */
-  static my_bool ignore_protocol_override = FALSE;
-
   switch(opt->id) {
   case OPT_CHARSETS_DIR:
     strmake_buf(mysql_charsets_dir, argument);
@@ -1802,18 +1790,11 @@ get_one_option(const struct my_option *opt, const char *argument, const char *fi
 #ifndef EMBEDDED_LIBRARY
     if (!argument[0])
       opt_protocol= 0;
-    else if ((opt_protocol= find_type_with_warning(argument, &sql_protocol_typelib,
+    else if ((opt_protocol=
+              find_type_with_warning(argument, &sql_protocol_typelib,
                                                    opt->name)) <= 0)
       exit(1);
 #endif
-
-    /* Specification of protocol via CLI trumps implicit overrides */
-    if (filename[0] == '\0')
-    {
-      ignore_protocol_override = TRUE;
-      protocol_to_force = MYSQL_PROTOCOL_DEFAULT;
-    }
-
     break;
   case OPT_SERVER_ARG:
 #ifdef EMBEDDED_LIBRARY
@@ -1913,13 +1894,6 @@ get_one_option(const struct my_option *opt, const char *argument, const char *fi
 #ifdef _WIN32
     opt_protocol = MYSQL_PROTOCOL_PIPE;
     opt_protocol_type= "pipe";
-
-    /* Prioritize pipe if explicit via command line */
-    if (filename[0] == '\0')
-    {
-      ignore_protocol_override = TRUE;
-      protocol_to_force = MYSQL_PROTOCOL_DEFAULT;
-    }
 #endif
     break;
 #include <sslopt-case.h>
@@ -1932,35 +1906,17 @@ get_one_option(const struct my_option *opt, const char *argument, const char *fi
     mysql_end(-1);
     break;
   case 'P':
-    /* If port and socket are set, fall back to default behavior */
-    if (protocol_to_force == SOCKET_PROTOCOL_TO_FORCE)
+    if (filename[0] == '\0')
     {
-      ignore_protocol_override = TRUE;
-      protocol_to_force = MYSQL_PROTOCOL_DEFAULT;
-    }
-
-    /* If port is set via CLI, try to force protocol to TCP */
-    if (filename[0] == '\0' &&
-        !ignore_protocol_override &&
-        protocol_to_force == MYSQL_PROTOCOL_DEFAULT)
-    {
-      protocol_to_force = MYSQL_PROTOCOL_TCP;
+      /* Port given on command line, switch protocol to use TCP */
+      opt_protocol= MYSQL_PROTOCOL_TCP;
     }
     break;
   case 'S':
-    /* If port and socket are set, fall back to default behavior */
-    if (protocol_to_force == MYSQL_PROTOCOL_TCP)
+    if (filename[0] == '\0')
     {
-      ignore_protocol_override = TRUE;
-      protocol_to_force = MYSQL_PROTOCOL_DEFAULT;
-    }
-
-    /* Prioritize socket if set via command line */
-    if (filename[0] == '\0' &&
-        !ignore_protocol_override &&
-        protocol_to_force == MYSQL_PROTOCOL_DEFAULT)
-    {
-      protocol_to_force = SOCKET_PROTOCOL_TO_FORCE;
+      /* Socket given on command line, switch protocol to use SOCKETSt */
+      opt_protocol= MYSQL_PROTOCOL_SOCKET;
     }
     break;
   case 'I':
