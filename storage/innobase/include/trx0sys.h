MariaDB/server

commit 0de3be8cfdfc26f5c236eaefe12d03c7b4af22c8

MDEV-30671 InnoDB undo log truncation fails to wait for purge of history
It is not safe to invoke trx_purge_free_segment() or execute
innodb_undo_log_truncate=ON before all undo log records in
the rollback segment has been processed.

A prominent failure that would occur due to premature freeing of
undo log pages is that trx_undo_get_undo_rec() would crash when
trying to copy an undo log record to fetch the previous version
of a record.

If trx_undo_get_undo_rec() was not invoked in the unlucky time frame,
then the symptom would be that some committed transaction history is
never removed. This would be detected by CHECK TABLE...EXTENDED that
was impleented in commit ab0190101b0587e0e03b2d75a967050b9a85fd1b.
Such a garbage collection leak should be possible even when using
innodb_undo_log_truncate=OFF, just involving trx_purge_free_segment().

trx_rseg_t::needs_purge: Change the type from Boolean to a transaction
identifier, noting the most recent non-purged transaction, or 0 if
everything has been purged. On transaction start, we initialize this
to 1 more than the transaction start ID. On recovery, the field may be
adjusted to the transaction end ID (TRX_UNDO_TRX_NO) if it is larger.

The field TRX_UNDO_NEEDS_PURGE becomes write-only; only some debug
assertions that would validate the value. The field reflects the old
inaccurate Boolean field trx_rseg_t::needs_purge.

trx_undo_mem_create_at_db_start(), trx_undo_lists_init(),
trx_rseg_mem_restore(): Remove the parameter max_trx_id.
Instead, store the maximum in trx_rseg_t::needs_purge,
where trx_rseg_array_init() will find it.

trx_purge_free_segment(): Contiguously hold a lock on
trx_rseg_t to prevent any concurrent allocation of undo log.

trx_purge_truncate_rseg_history(): Only invoke trx_purge_free_segment()
if the rollback segment is empty and there are no pending transactions
associated with it.

trx_purge_truncate_history(): Only proceed with innodb_undo_log_truncate=ON

storage/innobase/trx/trx0purge.cc
@@ -221,6 +221,7 @@ trx_purge_add_undo_to_history(const trx_t* trx, trx_undo_t*& undo, mtr_t* mtr)
 	trx_ulogf_t*	undo_header	= undo_page->frame + undo->hdr_offset;
 
 	ut_ad(mach_read_from_2(undo_header + TRX_UNDO_NEEDS_PURGE) <= 1);
+	ut_ad(rseg->needs_purge > trx->id);
 
 	if (UNIV_UNLIKELY(mach_read_from_4(TRX_RSEG + TRX_RSEG_FORMAT
 					   + rseg_header->frame))) {
@@ -309,7 +310,6 @@ trx_purge_add_undo_to_history(const trx_t* trx, trx_undo_t*& undo, mtr_t* mtr)
 		rseg->last_page_no = undo->hdr_page_no;
 		rseg->set_last_commit(undo->hdr_offset,
 				      trx->rw_trx_hash_element->no);
-		rseg->needs_purge = true;
 	}
 
 	trx_sys.rseg_history_len++;
@@ -339,16 +339,15 @@ static void trx_purge_remove_log_hdr(buf_block_t *rseg, buf_block_t* log,
 }
 
 /** Free an undo log segment, and remove the header from the history list.
+@param[in,out]	mtr		mini-transaction
 @param[in,out]	rseg		rollback segment
 @param[in]	hdr_addr	file address of log_hdr */
 static
-void
-trx_purge_free_segment(trx_rseg_t* rseg, fil_addr_t hdr_addr)
+void trx_purge_free_segment(mtr_t &mtr, trx_rseg_t* rseg, fil_addr_t hdr_addr)
 {
-	mtr_t		mtr;
-
+	mtr.commit();
 	mtr.start();
-	mutex_enter(&rseg->mutex);
+	ut_ad(mutex_own(&rseg->mutex));
 
 	buf_block_t* rseg_hdr = trx_rsegf_get(rseg->space, rseg->page_no, &mtr);
 	buf_block_t* block = trx_undo_page_get(
@@ -365,12 +364,8 @@ trx_purge_free_segment(trx_rseg_t* rseg, fil_addr_t hdr_addr)
 	while (!fseg_free_step_not_header(
 		       TRX_UNDO_SEG_HDR + TRX_UNDO_FSEG_HEADER
 		       + block->frame, &mtr)) {
-		mutex_exit(&rseg->mutex);
-
 		mtr.commit();
 		mtr.start();
-
-		mutex_enter(&rseg->mutex);
 
 		rseg_hdr = trx_rsegf_get(rseg->space, rseg->page_no, &mtr);
 
@@ -410,10 +405,6 @@ trx_purge_free_segment(trx_rseg_t* rseg, fil_addr_t hdr_addr)
 	ut_ad(rseg->curr_size >= seg_size);
 
 	rseg->curr_size -= seg_size;
-
-	mutex_exit(&(rseg->mutex));
-
-	mtr_commit(&mtr);
 }
 
 /** Remove unnecessary history data from a rollback segment.
@@ -431,8 +422,6 @@ trx_purge_truncate_rseg_history(
 	trx_id_t	undo_trx_no;
 
 	mtr.start();
-	ut_ad(rseg.is_persistent());
-	mutex_enter(&rseg.mutex);
 
 	buf_block_t* rseg_hdr = trx_rsegf_get(rseg.space, rseg.page_no, &mtr);
 
@@ -444,7 +433,6 @@ trx_purge_truncate_rseg_history(
 loop:
 	if (hdr_addr.page == FIL_NULL) {
 func_exit:
-		mutex_exit(&rseg.mutex);
 		mtr.commit();
 		return;
 	}
@@ -470,30 +458,26 @@ func_exit:
 	prev_hdr_addr.boffset = static_cast<uint16_t>(prev_hdr_addr.boffset
 						      - TRX_UNDO_HISTORY_NODE);
 
-	if (mach_read_from_2(TRX_UNDO_SEG_HDR + TRX_UNDO_STATE + block->frame)
+	if (!rseg.trx_ref_count
+	    && rseg.needs_purge <= (purge_sys.head.trx_no
+				    ? purge_sys.head.trx_no
+				    : purge_sys.tail.trx_no)
+	    && mach_read_from_2(TRX_UNDO_SEG_HDR + TRX_UNDO_STATE
+				+ block->frame)
 	    == TRX_UNDO_TO_PURGE
 	    && !mach_read_from_2(block->frame + hdr_addr.boffset
 				 + TRX_UNDO_NEXT_LOG)) {
-
-		/* We can free the whole log segment */
-
-		mutex_exit(&rseg.mutex);
-		mtr.commit();
-
-		/* calls the trx_purge_remove_log_hdr()
-		inside trx_purge_free_segment(). */
-		trx_purge_free_segment(&rseg, hdr_addr);
+		/* We can free the whole log segment.
+		This will call trx_purge_remove_log_hdr(). */
+		trx_purge_free_segment(mtr, &rseg, hdr_addr);
 	} else {
 		/* Remove the log hdr from the rseg history. */
 		trx_purge_remove_log_hdr(rseg_hdr, block, hdr_addr.boffset,
 					 &mtr);
-
-		mutex_exit(&rseg.mutex);
-		mtr.commit();
 	}
 
+	mtr.commit();
 	mtr.start();
-	mutex_enter(&rseg.mutex);
 
 	rseg_hdr = trx_rsegf_get(rseg.space, rseg.page_no, &mtr);
 
@@ -568,7 +552,10 @@ static void trx_purge_truncate_history()
     if (trx_rseg_t *rseg= trx_sys.rseg_array[i])
     {
       ut_ad(rseg->id == i);
+      ut_ad(rseg->is_persistent());
+      mutex_enter(&rseg->mutex);
       trx_purge_truncate_rseg_history(*rseg, head);
+      mutex_exit(&rseg->mutex);
     }
   }
 
@@ -612,46 +599,42 @@ static void trx_purge_truncate_history()
     DBUG_LOG("undo", "marking for truncate: " << file->name);
 
     for (ulint i= 0; i < TRX_SYS_N_RSEGS; ++i)
-      if (trx_rseg_t *rseg= trx_sys.rseg_array[i])
-        if (rseg->space == &space)
-          /* Once set, this rseg will not be allocated to subsequent
-          transactions, but we will wait for existing active
-          transactions to finish. */
-          rseg->skip_allocation= true;
-
-    for (ulint i= 0; i < TRX_SYS_N_RSEGS; ++i)
     {
       trx_rseg_t *rseg= trx_sys.rseg_array[i];
       if (!rseg || rseg->space != &space)
         continue;
-      mutex_enter(&rseg->mutex);
-      ut_ad(rseg->skip_allocation);
       ut_ad(rseg->is_persistent());
-      if (rseg->trx_ref_count)
+
+      mutex_enter(&rseg->mutex);
+      /* Once set, this rseg will not be allocated to subsequent
+      transactions, but we will wait for existing active
+      transactions to finish and to be purged. */
+      rseg->skip_allocation = true;
+
+      if (rseg->trx_ref_count || rseg->needs_purge > head.trx_no)
       {
-not_free:
+      not_free:
         mutex_exit(&rseg->mutex);
         return;
       }
 
-      if (rseg->curr_size != 1)
+      ut_ad(UT_LIST_GET_LEN(rseg->undo_list) == 0);
+      /* Check if all segments are cached and safe to remove. */
+      ulint cached= 0;
+
+      for (const trx_undo_t *undo= UT_LIST_GET_FIRST(rseg->undo_cached); undo;
+           undo= UT_LIST_GET_NEXT(undo_list, undo))
       {
-        /* Check if all segments are cached and safe to remove. */
-        ulint cached= 0;
-        for (trx_undo_t *undo= UT_LIST_GET_FIRST(rseg->undo_cached); undo;
-             undo= UT_LIST_GET_NEXT(undo_list, undo))
-        {
-          if (head.trx_no < undo->trx_id)
-            goto not_free;
-          else
-            cached+= undo->size;
-        }
-
-        ut_ad(rseg->curr_size > cached);
-
-        if (rseg->curr_size > cached + 1)
+        if (head.trx_no < undo->trx_id)
           goto not_free;
+        else
+          cached+= undo->size;
       }
+
+      ut_ad(rseg->curr_size > cached);
+
+      if (rseg->curr_size > cached + 1)
+        goto not_free;
 
       mutex_exit(&rseg->mutex);
     }
@@ -753,6 +736,8 @@ not_free:
 
       ut_ad(rseg->id == i);
       ut_ad(rseg->is_persistent());
+      ut_ad(!rseg->trx_ref_count);
+      ut_ad(rseg->needs_purge <= head.trx_no);
       ut_d(const auto old_page= rseg->page_no);
 
       buf_block_t *rblock= trx_rseg_header_create(&space, i,
@@ -775,9 +760,6 @@ not_free:
         ut_free(undo);
       }
 
-      UT_LIST_INIT(rseg->undo_list, &trx_undo_t::undo_list);
-      UT_LIST_INIT(rseg->undo_cached, &trx_undo_t::undo_list);
-
       /* These were written by trx_rseg_header_create(). */
       ut_ad(!mach_read_from_4(TRX_RSEG + TRX_RSEG_FORMAT + rblock->frame));
       ut_ad(!mach_read_from_4(TRX_RSEG + TRX_RSEG_HISTORY_SIZE +
@@ -786,9 +768,10 @@ not_free:
       the rseg header */
       rseg->curr_size= 1;
       rseg->trx_ref_count= 0;
+      rseg->needs_purge= 0;
+      rseg->skip_allocation= false;
       rseg->last_page_no= FIL_NULL;
       rseg->last_commit_and_offset= 0;
-      rseg->needs_purge= false;
     }
 
     mtr.commit_shrink(space);
@@ -811,17 +794,6 @@ not_free:
     DBUG_EXECUTE_IF("ib_undo_trunc", ib::info() << "ib_undo_trunc";
                     log_buffer_flush_to_disk();
                     DBUG_SUICIDE(););
-
-    for (ulint i= 0; i < TRX_SYS_N_RSEGS; ++i)
-    {
-      if (trx_rseg_t *rseg= trx_sys.rseg_array[i])
-      {
-        ut_ad(rseg->id == i);
-        ut_ad(rseg->is_persistent());
-        if (rseg->space == &space)
-          rseg->skip_allocation= false;
-      }
-    }
 
     ib::info() << "Truncated " << file->name;
     purge_sys.truncate.last= purge_sys.truncate.current;
@@ -891,7 +863,6 @@ static void trx_purge_rseg_get_next_history_log(
 
 	trx_no = mach_read_from_8(log_hdr + TRX_UNDO_TRX_NO);
 	ut_ad(mach_read_from_2(log_hdr + TRX_UNDO_NEEDS_PURGE) <= 1);
-	const byte needs_purge = log_hdr[TRX_UNDO_NEEDS_PURGE + 1];
 
 	mtr.commit();
 
@@ -899,7 +870,6 @@ static void trx_purge_rseg_get_next_history_log(
 
 	purge_sys.rseg->last_page_no = prev_log_addr.page;
 	purge_sys.rseg->set_last_commit(prev_log_addr.boffset, trx_no);
-	purge_sys.rseg->needs_purge = needs_purge != 0;
 
 	/* Purge can also produce events, however these are already ordered
 	in the rollback segment and any user generated event will be greater

mysql-test/suite/gcol/r/gcol_purge.result
@@ -1,7 +1,11 @@
+SET @save_frequency=@@GLOBAL.innodb_purge_rseg_truncate_frequency;
+SET @save_dbug=@@GLOBAL.debug_dbug;
+SET GLOBAL innodb_purge_rseg_truncate_frequency=1;
 CREATE TABLE t1(f1 INT NOT NULL, f2 int not null,
 f3 int generated always as (f2 * 2) VIRTUAL,
 primary key(f1), INDEX (f3))ENGINE=InnoDB;
 connect con1,localhost,root,,,;
+InnoDB		0 transactions not purged
 START TRANSACTION WITH CONSISTENT SNAPSHOT;
 connection default;
 INSERT INTO t1(f1, f2) VALUES(1,2);
@@ -18,5 +22,6 @@ commit;
 disconnect con1;
 disconnect con2;
 connection default;
-set global debug_dbug=default;
+SET GLOBAL innodb_purge_rseg_truncate_frequency=@save_frequency;
+SET GLOBAL debug_dbug=@save_dbug;
 DROP TABLE t1;

mysql-test/suite/gcol/t/gcol_purge.test
@@ -1,9 +1,14 @@
 --source include/have_innodb.inc
 --source include/have_debug.inc
+
+SET @save_frequency=@@GLOBAL.innodb_purge_rseg_truncate_frequency;
+SET @save_dbug=@@GLOBAL.debug_dbug;
+SET GLOBAL innodb_purge_rseg_truncate_frequency=1;
 CREATE TABLE t1(f1 INT NOT NULL, f2 int not null,
 		f3 int generated always as (f2 * 2) VIRTUAL,
 		primary key(f1), INDEX (f3))ENGINE=InnoDB;
 connect(con1,localhost,root,,,);
+--source ../innodb/include/wait_all_purged.inc
 START TRANSACTION WITH CONSISTENT SNAPSHOT;
 
 connection default;
@@ -26,5 +31,6 @@ commit;
 disconnect con1;
 disconnect con2;
 connection default;
-set global debug_dbug=default;
+SET GLOBAL innodb_purge_rseg_truncate_frequency=@save_frequency;
+SET GLOBAL debug_dbug=@save_dbug;
 DROP TABLE t1;

mysql-test/suite/innodb/r/cursor-restore-locking.result
@@ -1,4 +1,7 @@
+SET @save_freq=@@GLOBAL.innodb_purge_rseg_truncate_frequency;
+SET GLOBAL innodb_purge_rseg_truncate_frequency=1;
 CREATE TABLE t (a int PRIMARY KEY, b int NOT NULL UNIQUE) engine = InnoDB;
+InnoDB		0 transactions not purged
 connect prevent_purge,localhost,root,,;
 start transaction with consistent snapshot;
 connect con_del_1,localhost,root,,;
@@ -34,3 +37,4 @@ disconnect con_del_2;
 connection default;
 SET DEBUG_SYNC = 'RESET';
 DROP TABLE t;
+SET GLOBAL innodb_purge_rseg_truncate_frequency=@save_freq;

mysql-test/suite/innodb/r/dml_purge.result
@@ -7,6 +7,7 @@ SET GLOBAL innodb_purge_rseg_truncate_frequency = 1;
 SET GLOBAL innodb_purge_rseg_truncate_frequency = 1;
 CREATE TABLE t1(a INT PRIMARY KEY, b INT NOT NULL)
 ROW_FORMAT=REDUNDANT ENGINE=InnoDB;
+InnoDB		0 transactions not purged
 connect  prevent_purge,localhost,root;
 START TRANSACTION WITH CONSISTENT SNAPSHOT;
 connection default;
@@ -19,7 +20,11 @@ UPDATE t1 SET b=4 WHERE a=3;
 disconnect prevent_purge;
 connection default;
 InnoDB		0 transactions not purged
+connection con1;
+ROLLBACK;
 disconnect con1;
+connection default;
+InnoDB		0 transactions not purged
 FLUSH TABLE t1 FOR EXPORT;
 Clustered index root page contents:
 N_RECS=3; LEVEL=0

mysql-test/suite/innodb/r/gap_lock_split.result
@@ -3,6 +3,7 @@ SET GLOBAL innodb_purge_rseg_truncate_frequency=1;
 CREATE TABLE t1(id INT PRIMARY key, val VARCHAR(16000)) ENGINE=InnoDB;
 INSERT INTO t1 (id,val) SELECT 2*seq,'x' FROM seq_0_to_1023;
 connect con1,localhost,root,,;
+InnoDB		0 transactions not purged
 START TRANSACTION WITH CONSISTENT SNAPSHOT;
 connection default;
 DELETE FROM t1 WHERE id=1788;

mysql-test/suite/innodb/r/innodb_bug84958.result
@@ -9,12 +9,10 @@ SET GLOBAL innodb_purge_rseg_truncate_frequency= 1;
 CREATE PROCEDURE insert_n(start int, end int)
 BEGIN
 DECLARE i INT DEFAULT start;
-START TRANSACTION;
 WHILE i <= end do
 INSERT INTO t1 VALUES (1, 2, 3) ON DUPLICATE KEY UPDATE c = i;
 SET i = i + 1;
 END WHILE;
-COMMIT;
 END~~
 CREATE FUNCTION num_pages_get()
 RETURNS INT
@@ -29,7 +27,8 @@ END~~
 # Create a table with one record in it and start an RR transaction
 #
 CREATE TABLE t1 (a INT, b INT, c INT, PRIMARY KEY(a,b), KEY (b,c))
-ENGINE=InnoDB;
+ENGINE=InnoDB STATS_PERSISTENT=0;
+InnoDB		0 transactions not purged
 BEGIN;
 SELECT * FROM t1;
 a	b	c
@@ -38,20 +37,24 @@ a	b	c
 #
 connect  con2, localhost, root,,;
 connection con2;
+BEGIN;
 INSERT INTO t1 VALUES (1, 2, 3) ON DUPLICATE KEY UPDATE c = NULL;
 CALL insert_n(1, 50);;
 connect  con3, localhost, root,,;
 connection con3;
+BEGIN;
 CALL insert_n(51, 100);;
 connection con2;
+COMMIT;
 connection con3;
 INSERT INTO t1 VALUES (1, 2, 1) ON DUPLICATE KEY UPDATE c = NULL;
+COMMIT;
 connection default;
 #
 # Connect to default and record how many pages were accessed
 # when selecting the record using the secondary key.
 #
-InnoDB		4 transactions not purged
+InnoDB		2 transactions not purged
 SET @num_pages_1 = num_pages_get();
 SELECT * FROM t1 force index (b);
 a	b	c

mysql-test/suite/innodb/t/cursor-restore-locking.test
@@ -3,8 +3,11 @@
 source include/have_debug.inc;
 source include/have_debug_sync.inc;
 
+SET @save_freq=@@GLOBAL.innodb_purge_rseg_truncate_frequency;
+SET GLOBAL innodb_purge_rseg_truncate_frequency=1;
 CREATE TABLE t (a int PRIMARY KEY, b int NOT NULL UNIQUE) engine = InnoDB;
 
+--source include/wait_all_purged.inc
 --connect(prevent_purge,localhost,root,,)
 start transaction with consistent snapshot;
 
@@ -80,4 +83,5 @@ INSERT INTO t VALUES(30, 20);
 
 SET DEBUG_SYNC = 'RESET';
 DROP TABLE t;
+SET GLOBAL innodb_purge_rseg_truncate_frequency=@save_freq;
 --source include/wait_until_count_sessions.inc

commit e0084b9d315f10e3ceb578b65e144d751b208bf1

MDEV-31234 InnoDB does not free UNDO after the fix of MDEV-30671
trx_purge_truncate_history(): Only call trx_purge_truncate_rseg_history()
if the rollback segment is safe to process. This will avoid leaking undo
log pages that are not yet ready to be processed. This fixes a regression
that was introduced in
commit 0de3be8cfdfc26f5c236eaefe12d03c7b4af22c8 (MDEV-30671).

trx_sys_t::any_active_transactions(): Separately count XA PREPARE
transactions.

srv_purge_should_exit(): Terminate slow shutdown if the history size
does not change and XA PREPARE transactions exist in the system.
This will avoid a hang of the test innodb.recovery_shutdown.

Tested by: Matthias Leich

storage/innobase/include/trx0sys.h
@@ -1055,7 +1055,7 @@ public:
   void close();
 
   /** @return total number of active (non-prepared) transactions */
-  ulint any_active_transactions();
+  size_t any_active_transactions(size_t *prepared= nullptr);
 
 
   /**

storage/innobase/srv/srv0srv.cc
@@ -1697,7 +1697,7 @@ void srv_master_callback(void*)
 }
 
 /** @return whether purge should exit due to shutdown */
-static bool srv_purge_should_exit()
+static bool srv_purge_should_exit(size_t old_history_size)
 {
   ut_ad(srv_shutdown_state <= SRV_SHUTDOWN_CLEANUP);
 
@@ -1708,7 +1708,12 @@ static bool srv_purge_should_exit()
     return true;
 
   /* Slow shutdown was requested. */
-  if (const size_t history_size= trx_sys.rseg_history_len)
+  size_t prepared, active= trx_sys.any_active_transactions(&prepared);
+  const size_t history_size= trx_sys.rseg_history_len;
+
+  if (!history_size);
+  else if (!active && history_size == old_history_size && prepared);
+  else
   {
     static time_t progress_time;
     time_t now= time(NULL);
@@ -1725,7 +1730,7 @@ static bool srv_purge_should_exit()
     return false;
   }
 
-  return !trx_sys.any_active_transactions();
+  return !active;
 }
 
 /*********************************************************************//**
@@ -1845,7 +1850,7 @@ static size_t srv_do_purge(ulint* n_total_purged)
 
 		*n_total_purged += n_pages_purged;
 	} while (n_pages_purged > 0 && !purge_sys.paused()
-		 && !srv_purge_should_exit());
+		 && !srv_purge_should_exit(rseg_history_len));
 
 	return(rseg_history_len);
 }
@@ -1960,7 +1965,7 @@ static void purge_coordinator_callback_low()
     }
   }
   while ((purge_sys.enabled() && !purge_sys.paused()) ||
-         !srv_purge_should_exit());
+         !srv_purge_should_exit(trx_sys.rseg_history_len));
 }
 
 static void purge_coordinator_callback(void*)
@@ -2031,15 +2036,19 @@ ulint srv_get_task_queue_length()
 /** Shut down the purge threads. */
 void srv_purge_shutdown()
 {
-	if (purge_sys.enabled()) {
-		if (!srv_fast_shutdown && !opt_bootstrap)
-			srv_update_purge_thread_count(innodb_purge_threads_MAX);
-		while(!srv_purge_should_exit()) {
-			ut_a(!purge_sys.paused());
-			srv_wake_purge_thread_if_not_active();
-			purge_coordinator_task.wait();
-		}
-		purge_sys.coordinator_shutdown();
-		srv_shutdown_purge_tasks();
-	}
+  if (purge_sys.enabled())
+  {
+    if (!srv_fast_shutdown && !opt_bootstrap)
+      srv_update_purge_thread_count(innodb_purge_threads_MAX);
+    size_t history_size= trx_sys.rseg_history_len;
+    while (!srv_purge_should_exit(history_size))
+    {
+      history_size= trx_sys.rseg_history_len;
+      ut_a(!purge_sys.paused());
+      srv_wake_purge_thread_if_not_active();
+      purge_coordinator_task.wait();
+    }
+    purge_sys.coordinator_shutdown();
+    srv_shutdown_purge_tasks();
+  }
 }

storage/innobase/trx/trx0purge.cc
@@ -448,12 +448,7 @@ func_exit:
 	prev_hdr_addr.boffset = static_cast<uint16_t>(prev_hdr_addr.boffset
 						      - TRX_UNDO_HISTORY_NODE);
 
-	if (!rseg.trx_ref_count
-	    && rseg.needs_purge <= (purge_sys.head.trx_no
-				    ? purge_sys.head.trx_no
-				    : purge_sys.tail.trx_no)
-	    && mach_read_from_2(TRX_UNDO_SEG_HDR + TRX_UNDO_STATE
-				+ block->frame)
+	if (mach_read_from_2(TRX_UNDO_SEG_HDR + TRX_UNDO_STATE + block->frame)
 	    == TRX_UNDO_TO_PURGE
 	    && !mach_read_from_2(block->frame + hdr_addr.boffset
 				 + TRX_UNDO_NEXT_LOG)) {
@@ -544,7 +539,8 @@ static void trx_purge_truncate_history()
       ut_ad(rseg->id == i);
       ut_ad(rseg->is_persistent());
       mutex_enter(&rseg->mutex);
-      trx_purge_truncate_rseg_history(*rseg, head);
+      if (!rseg->trx_ref_count && rseg->needs_purge <= head.trx_no)
+        trx_purge_truncate_rseg_history(*rseg, head);
       mutex_exit(&rseg->mutex);
     }
   }

storage/innobase/trx/trx0sys.cc
@@ -325,15 +325,29 @@ trx_sys_t::close()
 }
 
 /** @return total number of active (non-prepared) transactions */
-ulint trx_sys_t::any_active_transactions()
+size_t trx_sys_t::any_active_transactions(size_t *prepared)
 {
-  uint32_t total_trx= 0;
+  size_t total_trx= 0, prepared_trx= 0;
 
-  trx_sys.trx_list.for_each([&total_trx](const trx_t &trx) {
-    if (trx.state == TRX_STATE_COMMITTED_IN_MEMORY ||
-        (trx.state == TRX_STATE_ACTIVE && trx.id))
+  trx_sys.trx_list.for_each([&](const trx_t &trx) {
+    switch (trx.state) {
+    case TRX_STATE_NOT_STARTED:
+      break;
+    case TRX_STATE_ACTIVE:
+      if (!trx.id)
+        break;
+      /* fall through */
+    case TRX_STATE_COMMITTED_IN_MEMORY:
       total_trx++;
+      break;
+    case TRX_STATE_PREPARED:
+    case TRX_STATE_PREPARED_RECOVERED:
+      prepared_trx++;
+    }
   });
+
+  if (prepared)
+    *prepared= prepared_trx;
 
   return total_trx;
 }