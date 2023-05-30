c2d44ecb904dd5558d116433b6cceb5592182447
Brandon Nesterenko <brandon.nesterenko@mariadb.com>
2023-05-30 09:20:22 -0600

    MDEV-29894: Calling a function from a different database in a slave side trigger crashes
    When opening and locking tables, if triggers will be invoked in a
    separate database, thd->set_db() is invoked, thus freeeing the memory
    and headers which thd->db had previously pointed to. In row based
    replication, the event execution logic initializes thd->db to point
    to the database which the event targets, which is owned by the
    corresponding table share (introduced in d9898c9 for MDEV-7409).
    The problem then, is that during the table opening and locking
    process for a row event, memory which belongs to the table share
    would be freed, which is not valid.
    
    This patch replaces the thd->reset_db() calls to thd->set_db(),
    which copies-by-value, rather than by reference. Then when the
    memory is freed, our copy of memory is freed, rather than memory
    which belongs to a table share.
    
    Notes:
      1. The call to change thd->db now happens on a higher-level, in
    Rows_log_event::do_apply_event() rather than ::do_exec_row(), in the
    call stack. This is because do_exec_row() is called within a loop,
    and each invocation would redundantly set and unset the db to the
    same value.
      2. thd->set_db() is only used if triggers are to be invoked, as
    there is no vulnerability in the non-trigger case, and copying
    memory would be an unnecessary inefficiency.
    
    Reviewed By:
    ============
    Andrei Elkin <andrei.elkin@mariadb.com>

--- sql/log_event_server.cc
+++ sql/log_event_server.cc
@@ -5676,6 +5676,7 @@ int Rows_log_event::do_apply_event(rpl_group_info *rgi)
                        " (master had triggers)" : ""));
   if (table)
   {
+    Rows_log_event::Db_restore_ctx restore_ctx(this);
     master_had_triggers= table->master_had_triggers;
     bool transactional_table= table->file->has_transactions_and_rollback();
     table->file->prepare_for_insert(get_general_type_code() != WRITE_ROWS_EVENT);
@@ -7601,7 +7602,6 @@ Write_rows_log_event::do_exec_row(rpl_group_info *rgi)
 {
   DBUG_ASSERT(m_table != NULL);
   const char *tmp= thd->get_proc_info();
-  LEX_CSTRING tmp_db= thd->db;
   char *message, msg[128];
   const LEX_CSTRING &table_name= m_table->s->table_name;
   const char quote_char=
@@ -7609,7 +7609,6 @@ Write_rows_log_event::do_exec_row(rpl_group_info *rgi)
   my_snprintf(msg, sizeof msg,
               "Write_rows_log_event::write_row() on table %c%.*s%c",
               quote_char, int(table_name.length), table_name.str, quote_char);
-  thd->reset_db(&m_table->s->db);
   message= msg;
   int error;
 
@@ -7631,7 +7630,6 @@ Write_rows_log_event::do_exec_row(rpl_group_info *rgi)
     my_error(ER_UNKNOWN_ERROR, MYF(0));
   }
 
-  thd->reset_db(&tmp_db);
   return error;
 }
 
@@ -8237,7 +8235,6 @@ int Delete_rows_log_event::do_exec_row(rpl_group_info *rgi)
 {
   int error;
   const char *tmp= thd->get_proc_info();
-  LEX_CSTRING tmp_db= thd->db;
   char *message, msg[128];
   const LEX_CSTRING &table_name= m_table->s->table_name;
   const char quote_char=
@@ -8245,7 +8242,6 @@ int Delete_rows_log_event::do_exec_row(rpl_group_info *rgi)
   my_snprintf(msg, sizeof msg,
               "Delete_rows_log_event::find_row() on table %c%.*s%c",
               quote_char, int(table_name.length), table_name.str, quote_char);
-  thd->reset_db(&m_table->s->db);
   message= msg;
   const bool invoke_triggers= (m_table->triggers && do_invoke_trigger());
   DBUG_ASSERT(m_table != NULL);
@@ -8305,7 +8301,6 @@ int Delete_rows_log_event::do_exec_row(rpl_group_info *rgi)
       error= HA_ERR_GENERIC; // in case if error is not set yet
     m_table->file->ha_index_or_rnd_end();
   }
-  thd->reset_db(&tmp_db);
   thd_proc_info(thd, tmp);
   return error;
 }
@@ -8406,7 +8401,6 @@ Update_rows_log_event::do_exec_row(rpl_group_info *rgi)
   const bool invoke_triggers= (m_table->triggers && do_invoke_trigger());
   const char *tmp= thd->get_proc_info();
   DBUG_ASSERT(m_table != NULL);
-  LEX_CSTRING tmp_db= thd->db;
   char *message, msg[128];
   const LEX_CSTRING &table_name= m_table->s->table_name;
   const char quote_char=
@@ -8414,7 +8408,6 @@ Update_rows_log_event::do_exec_row(rpl_group_info *rgi)
   my_snprintf(msg, sizeof msg,
               "Update_rows_log_event::find_row() on table %c%.*s%c",
               quote_char, int(table_name.length), table_name.str, quote_char);
-  thd->reset_db(&m_table->s->db);
   message= msg;
 
 #ifdef WSREP_PROC_INFO
@@ -8443,7 +8436,6 @@ Update_rows_log_event::do_exec_row(rpl_group_info *rgi)
     if ((m_curr_row= m_curr_row_end))
       unpack_current_row(rgi, &m_cols_ai);
     thd_proc_info(thd, tmp);
-    thd->reset_db(&tmp_db);
     return error;
   }
 
@@ -8532,7 +8524,6 @@ Update_rows_log_event::do_exec_row(rpl_group_info *rgi)
 
 err:
   thd_proc_info(thd, tmp);
-  thd->reset_db(&tmp_db);
   m_table->file->ha_index_or_rnd_end();
   return error;
 }
