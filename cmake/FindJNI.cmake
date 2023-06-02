dc9498beb6f79f08edd02fb7f7153e5150a33f7d
Sergei Golubchik <serg@mariadb.org>
2023-06-02 09:02:09 +0200

    Revert "MDEV-31230: Fix CONNECT_JDBC in CMake"
    This reverts commit 1d0e3d80d84c2b375a444cdbb41c28afd78e98b0.

---
+++ cmake/FindJNI.cmake
@@ -0,0 +1,13 @@
+if(JAVA_AWT_LIBRARY AND JAVA_INCLUDE_PATH)
+  set(JNI_FOUND TRUE)
+  return()
+endif()
+if(DEFINED JAVA_AWT_LIBRARY)
+  set(JNI_FOUND FALSE)
+  return()
+endif()
+
+set(orig_CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
+unset(CMAKE_MODULE_PATH)
+include(FindJNI)
+set(CMAKE_MODULE_PATH ${orig_CMAKE_MODULE_PATH})
