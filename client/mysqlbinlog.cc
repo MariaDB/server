6d911219d6b1232b0edb6047da322e791c312ae5
Oleg Smirnov <olernov@gmail.com>
2023-06-29 18:34:34 +0700

    MDEV-30639 Upgrade to 10.8 and later does not work on Windows
    During the upgrade procedure on Windows mysqld.exe is started with
    the named pipe connection protocol. mysqladmin.exe then pings the
    server to check if is up and running. Command line looks like:
       mysqladmin.exe --protocol=pipe --socket=mysql_upgrade_service_xxx ping
    But the "socket" parameter resets the "protocol" which was previously
    initialized with the "pipe" value, setting it to "socket".
    As a result, connection cannot be established and the upgrade
    procedure fails.
    "socket" in Windows is used to pass the name of the pipe so resetting
    the protocol is not valid in this case.
    
    This commit fixes resetting of the "protocol" parameter with "socket"
    parameter in the case when protocol has been previously initialized
    to "pipe" value

--- client/mysqlbinlog.cc
+++ client/mysqlbinlog.cc
@@ -2036,8 +2036,15 @@ get_one_option(const struct my_option *opt, const char *argument,
   case 'S':
     if (filename[0] == '\0')
     {
-      /* Socket given on command line, switch protocol to use SOCKETSt */
-      opt_protocol= MYSQL_PROTOCOL_SOCKET;
+      /*
+        Socket given on command line, switch protocol to use SOCKETSt
+        Except on Windows if 'protocol= pipe' has been provided in
+        the config file or command line.
+      */
+      if (opt_protocol != MYSQL_PROTOCOL_PIPE)
+      {
+        opt_protocol= MYSQL_PROTOCOL_SOCKET;
+      }
     }
     break;
   case 'v':
