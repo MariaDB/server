615b97a0108ed2f7185860f8b9aa0ed7fb945fa1
Vanislavsky <serzh7214@mail,ru>
2023-07-13 00:46:46 +1000

    MDEV-30182: add open_address_hash
    usae:
    buld taret open_address_hash-t (i.e. cmake your-build-dir --build open_address_hash-t)
    run ctest open_address_hash (i.e. ctest -R open_address_hash, from your-build-dir)

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -2586,6 +2586,40 @@ struct thd_async_state
   }
 };
 
+#include "open_address_hash.h"
+
+class table_trait
+{
+public:
+  using elem_type= TABLE_LIST *;
+  using find_type= TABLE_LIST *;
+  using erase_type= TABLE_LIST *;
+
+  static bool is_equal(const TABLE_LIST *lhs, const TABLE_LIST *rhs)
+  {
+    return lhs == rhs;
+  }
+  static bool is_empty(const elem_type el) { return el == nullptr; }
+  static void set_null(elem_type &el) { el= nullptr; }
+
+  static my_hash_value_type get_hash_value(TABLE_LIST *elem)
+  {
+    return elem->mdl_request.key.tc_hash_value();
+  }
+};
+
+class table_key
+{
+public:
+  using hash_value_type= decltype(MDL_key().tc_hash_value());
+  using key_type= MDL_key;
+
+  static MDL_key *get_key(TABLE_LIST *elem) { return &elem->mdl_request.key; }
+  static my_hash_value_type get_hash_value_from_key(const MDL_key *key)
+  {
+    return key->tc_hash_value();
+  }
+};
 
 /**
   @class THD
@@ -2700,6 +2734,8 @@ public:
     - Also ensures that THD is not deleted while mutex is hold
   */
   mutable mysql_mutex_t LOCK_thd_kill;
+
+  open_address_hash<table_trait, table_key> pr_table_hash;
 
   /* all prepared statements and cursors of this connection */
   Statement_map stmt_map;
---
+++ unittest/sql/open_address_hash-t.cc
@@ -0,0 +1,78 @@
+#include <my_global.h>
+#include <tap.h>
+
+#include "open_address_hash.h"
+uint32 unary_hf(uint32 key)
+{
+  return key;
+}
+
+struct test_key_trait
+{
+  using hash_value_type= uint32;
+  using key_type= hash_value_type;
+
+  static uint32 get_key(const key_type* elem) { return *elem; }
+  static hash_value_type get_hash_value_from_key(const key_type* elem) { return *elem; }
+};
+
+template<typename T>
+struct pointer_trait
+{
+  using elem_type= T*;
+  using find_type= T*;
+  using erase_type= T*;
+  static bool is_equal(const T *lhs, const T *rhs)
+  {
+    return lhs == rhs;
+  }
+  static bool is_empty(const elem_type el) { return el == nullptr; }
+  static void set_null(elem_type &el) { el= nullptr; }
+};
+
+struct test_value_trait: public pointer_trait<uint32>
+{
+  static const uint32 &get_key(uint32 *elem) { return *elem; }
+  static uint32 get_hash_value(uint32* elem)
+  { 
+      return *elem;
+  }
+};
+
+open_address_hash<test_value_trait, test_key_trait> hashie;
+
+uint32 data[4][10]= {
+        {0,1,2,3,4,5,6,7,8,9,},
+        {0,1,2,3,4,5,6,7,8,9,},
+        {0,1,2,3,4,5,6,7,8,9,},
+        {0,1,2,3,4,5,6,7,8,9,},
+};
+
+int main(int argc __attribute__((unused)),char *argv[])
+{
+  plan(123); // xz 4to eto
+
+  auto *found= hashie.find(data[0]);
+  ok(found == nullptr, "Something found in a empty hash!");
+  hashie.insert(data[0] + 1); // 1
+  found= hashie.find(data[0] + 1);
+  ok(*found == 1, "1 is not found");
+
+
+  found= hashie.find(data[1] + 1);
+  ok(found == nullptr, "wrong val with key=1 is found");
+
+  // expand
+  hashie.insert(data[0]+4);
+  ok(hashie.size() == 2, "wrong size");
+  ok(hashie.buffer_size() == 0, "two elements, why buffer?");
+  hashie.insert(data[0]+5);
+  ok(hashie.size() == 3, "wrong size, %u", hashie.size());
+  // collision
+  hashie.insert(data[1] + 1); 
+  found= hashie.find(data[1] + 1);
+  hashie.insert(data[1] + 1); // 1
+  auto found2= hashie.find(data[1] + 1);
+  ok(found2 != found && *found == *found2, "collision misbehavior");
+}
+
