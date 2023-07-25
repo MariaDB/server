81cd93bbb81d75e7cf14dedede0c9ec0712ace68
Yuchen Pei <ycp@mariadb.com>
2023-07-25 13:10:52 +1000

    MDEV-31400 Simple plugin dependency resolution
    We introduce simple plugin dependency. A plugin init function may
    return HA_ERR_RETRY_INIT. If this happens during server startup when
    the server is trying to initialise all plugins, the failed plugins
    will be retried, until no more plugins succeed in initialisation or
    want to be retried.
    
    This will fix spider init bugs which is caused in part by its
    dependency on Aria for initialisation.
    
    The reason we need a new return code, instead of treating every
    failure as a request for retry, is that it may be impossible to clean
    up after a failed plugin initialisation. Take InnoDB for example, it
    has a global variable `buf_page_cleaner_is_active`, which may not
    satisfy an assertion during a second initialisation try, probably
    because InnoDB does not expect the initialisation to be called
    twice.

--- sql/handler.cc
+++ sql/handler.cc
@@ -620,6 +620,7 @@ int ha_initialize_handlerton(st_plugin_int *plugin)
 {
   handlerton *hton;
   static const char *no_exts[]= { 0 };
+  int ret= 0;
   DBUG_ENTER("ha_initialize_handlerton");
   DBUG_PRINT("plugin", ("initialize plugin: '%s'", plugin->name.str));
 
@@ -629,6 +630,7 @@ int ha_initialize_handlerton(st_plugin_int *plugin)
   {
     sql_print_error("Unable to allocate memory for plugin '%s' handlerton.",
                     plugin->name.str);
+    ret= 1;
     goto err_no_hton_memory;
   }
 
@@ -639,12 +641,8 @@ int ha_initialize_handlerton(st_plugin_int *plugin)
   hton->slot= HA_SLOT_UNDEF;
   /* Historical Requirement */
   plugin->data= hton; // shortcut for the future
-  if (plugin->plugin->init && plugin->plugin->init(hton))
-  {
-    sql_print_error("Plugin '%s' init function returned error.",
-                    plugin->name.str);
+  if (plugin->plugin->init && (ret= plugin->plugin->init(hton)))
     goto err;
-  }
 
   // hton_ext_based_table_discovery() works only when discovery
   // is supported and the engine if file-based.
@@ -682,6 +680,7 @@ int ha_initialize_handlerton(st_plugin_int *plugin)
     if (idx == (int) DB_TYPE_DEFAULT)
     {
       sql_print_warning("Too many storage engines!");
+      ret= 1;
       goto err_deinit;
     }
     if (hton->db_type != DB_TYPE_UNKNOWN)
@@ -709,6 +708,7 @@ int ha_initialize_handlerton(st_plugin_int *plugin)
     {
       sql_print_error("Too many plugins loaded. Limit is %lu. "
                       "Failed on '%s'", (ulong) MAX_HA, plugin->name.str);
+      ret= 1;
       goto err_deinit;
     }
     hton->slot= total_ha++;
@@ -758,7 +758,7 @@ int ha_initialize_handlerton(st_plugin_int *plugin)
   resolve_sysvar_table_options(hton);
   update_discovery_counters(hton, 1);
 
-  DBUG_RETURN(0);
+  DBUG_RETURN(ret);
 
 err_deinit:
   /* 
@@ -776,7 +776,7 @@ err:
   my_free(hton);
 err_no_hton_memory:
   plugin->data= NULL;
-  DBUG_RETURN(1);
+  DBUG_RETURN(ret);
 }
 
 int ha_init()
