bf3f1a3e56a10237cb976b193008e0df996550c4
Sergei Golubchik <serg@mariadb.org>
2023-09-08 12:29:48 +0200

    disable SSL via named pipes in the internal client
    because it doesn't work. CONC-635.

--- sql-common/client.c
+++ sql-common/client.c
@@ -2052,6 +2052,9 @@ static int send_client_reply_packet(MCPVIO_EXT *mpvio,
   if (mpvio->db)
     mysql->client_flag|= CLIENT_CONNECT_WITH_DB;
 
+  if (mysql->net.vio->type == VIO_TYPE_NAMEDPIPE)
+    mysql->server_capabilities&= ~(CLIENT_SSL | CLIENT_SSL_VERIFY_SERVER_CERT);
+
   /* Remove options that server doesn't support */
   mysql->client_flag= mysql->client_flag &
                        (~(CLIENT_COMPRESS | CLIENT_SSL | CLIENT_PROTOCOL_41) 
