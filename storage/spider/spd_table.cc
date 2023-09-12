a1bfaff3cb54af58ca4c5a536119f72911788147
Yuchen Pei <ycp@mariadb.com>
2023-09-12 18:06:40 +1000

    MDEV-28856 Spider: Tests, documentation, small fixes and cleanups
    Removed some redundant hint related string literals from
    spd_db_conn.cc
    
    Clean up SPIDER_PARAM_*_[CHAR]LEN[S]
    
    Adding tests covering monitoring_kind=2. What it does is that it reads
    from mysql.spider_link_mon_servers with matching db_name, table_name,
    link_id, and does not do anything about that...
    
    How monitoring_* can be useful: in the deprecated spider high
    availability feature, when one remote fails, spider will try another
    remote, which apparently makes use of these table parameters.
    
    A test covering the query_cache_sync table param. Some further tests
    on some spider table params.
    
    Wrapper should be case insensitive.
    
    Code documentation on spider priority binary tree.
    
    Add an assertion that static_key_cardinality is always -1. All tests
    pass still

--- storage/spider/spd_table.cc
+++ storage/spider/spd_table.cc
@@ -1563,7 +1563,9 @@ static int spider_set_ll_value(
   DBUG_RETURN(error_num);
 }
 
-#define SPIDER_PARAM_STR_LEN(name) name ## _length
+#define SPIDER_PARAM_LEN(name) name ## _length
+#define SPIDER_PARAM_LENS(name) name ## _lengths
+#define SPIDER_PARAM_CHARLEN(name) name ## _charlen
 #define SPIDER_PARAM_STR(title_name, param_name) \
   if (!strncasecmp(parse.start_title, title_name, title_length)) \
   { \
@@ -1572,7 +1574,7 @@ static int spider_set_ll_value(
     { \
       if ((share->param_name = spider_create_string(parse.start_value, \
                                                     value_length))) \
-        share->SPIDER_PARAM_STR_LEN(param_name) = strlen(share->param_name); \
+        share->SPIDER_PARAM_LEN(param_name) = strlen(share->param_name); \
       else { \
         error_num= parse.fail(true); \
         goto error; \
@@ -1581,21 +1583,19 @@ static int spider_set_ll_value(
     } \
     break; \
   }
-#define SPIDER_PARAM_STR_LENS(name) name ## _lengths
-#define SPIDER_PARAM_STR_CHARLEN(name) name ## _charlen
 #define SPIDER_PARAM_STR_LIST(title_name, param_name) \
   if (!strncasecmp(parse.start_title, title_name, title_length)) \
   { \
     DBUG_PRINT("info", ("spider " title_name " start")); \
     if (!share->param_name) \
     { \
-      share->SPIDER_PARAM_STR_CHARLEN(param_name)= value_length; \
+      share->SPIDER_PARAM_CHARLEN(param_name)= value_length; \
       if ((error_num= spider_create_string_list( \
              &share->param_name, \
-             &share->SPIDER_PARAM_STR_LENS(param_name), \
-             &share->SPIDER_PARAM_STR_LEN(param_name), \
+             &share->SPIDER_PARAM_LENS(param_name), \
+             &share->SPIDER_PARAM_LEN(param_name), \
              parse.start_value, \
-             share->SPIDER_PARAM_STR_CHARLEN(param_name)))) \
+             share->SPIDER_PARAM_CHARLEN(param_name)))) \
         goto error; \
     } \
     break; \
@@ -1655,7 +1655,6 @@ static int spider_set_ll_value(
     } \
     break; \
   }
-#define SPIDER_PARAM_LONG_LEN(name) name ## _length
 #define SPIDER_PARAM_LONG_LIST_WITH_MAX(title_name, param_name, \
                                         min_val, max_val) \
   if (!strncasecmp(parse.start_title, title_name, title_length)) \
@@ -1665,7 +1664,7 @@ static int spider_set_ll_value(
     { \
       if ((error_num = spider_create_long_list( \
              &share->param_name, \
-             &share->SPIDER_PARAM_LONG_LEN(param_name), \
+             &share->SPIDER_PARAM_LEN(param_name), \
              parse.start_value, \
              value_length, \
              min_val, max_val))) \
@@ -1673,7 +1672,6 @@ static int spider_set_ll_value(
     } \
     break; \
   }
-#define SPIDER_PARAM_LONGLONG_LEN(name) name ## _length
 #define SPIDER_PARAM_LONGLONG_LIST_WITH_MAX(title_name, param_name, \
                                             min_val, max_val) \
   if (!strncasecmp(parse.start_title, title_name, title_length)) \
@@ -1683,7 +1681,7 @@ static int spider_set_ll_value(
     { \
       if ((error_num = spider_create_longlong_list( \
              &share->param_name, \
-             &share->SPIDER_PARAM_LONGLONG_LEN(param_name), \
+             &share->SPIDER_PARAM_LEN(param_name), \
              parse.start_value, \
              value_length, \
              min_val, max_val))) \
@@ -1747,6 +1745,10 @@ static int spider_set_ll_value(
     break; \
   }
 
+/**
+  Assign -1 to some `SPIDER_SHARE' numeric fields, to indicate they
+  have not been specified by the user yet.
+*/
 static void spider_minus_1(SPIDER_SHARE *share, TABLE_SHARE *table_share)
 {
 #ifndef WITHOUT_SPIDER_BG_SEARCH
@@ -2113,6 +2115,7 @@ int spider_parse_connect_info(
       connect_string = NULL;
     }
 
+    /* Get the correct connect info for the current level. */
     int error_num_1 = spider_get_connect_info(i, part_elem, sub_elem,
                                               table_share, connect_string);
     if (error_num_1 == 1)
@@ -3140,7 +3143,7 @@ int spider_parse_connect_info(
       {
         if (
           spider_dbton[roop_count2].wrapper &&
-          !strcmp(share->tgt_wrappers[roop_count],
+          !strcasecmp(share->tgt_wrappers[roop_count],
             spider_dbton[roop_count2].wrapper)
         ) {
           break;
