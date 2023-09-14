MariaDB/server

commit e938d7c18f6f117b63fcfc50dd975029d92409a7

MDEV-32028  InnoDB scrubbing doesn't write zero while freeing the extent
Problem:
========
InnoDB fails to mark the page status as FREED during
freeing of an extent of a segment. This behaviour affects
scrubbing and doesn't write all zeroes in file even though
pages are freed.

Solution:
========
InnoDB should mark the page status as FREED before
reinitialize the extent descriptor entry.

storage/innobase/fsp/fsp0fsp.cc
@@ -2683,14 +2683,14 @@ fseg_free_extent(
 			      not_full_n_used - descr_n_used);
 	}
 
-	fsp_free_extent(space, page, mtr);
-
 	for (uint32_t i = 0; i < FSP_EXTENT_SIZE; i++) {
 		if (!xdes_is_free(descr, i)) {
 			buf_page_free(space, first_page_in_extent + i, mtr,
 				      __FILE__, __LINE__);
 		}
 	}
+
+	fsp_free_extent(space, page, mtr);
 }
 
 /**********************************************************************//**

mysql-test/suite/innodb/r/scrub_debug.result
@@ -1,0 +1,22 @@
+SET @save_debug=@@GLOBAL.INNODB_LIMIT_OPTIMISTIC_INSERT_DEBUG;
+SET @save_scrub=@@GLOBAL.INNODB_IMMEDIATE_SCRUB_DATA_UNCOMPRESSED;
+SET @save_freq=@@GLOBAL.INNODB_PURGE_RSEG_TRUNCATE_FREQUENCY;
+SET GLOBAL INNODB_PURGE_RSEG_TRUNCATE_FREQUENCY=1;
+SET GLOBAL INNODB_IMMEDIATE_SCRUB_DATA_UNCOMPRESSED=1;
+SET GLOBAL INNODB_LIMIT_OPTIMISTIC_INSERT_DEBUG=2;
+CREATE TABLE t1(f1 INT AUTO_INCREMENT PRIMARY KEY,
+f2 VARCHAR(256) GENERATED ALWAYS as('repairman'),
+INDEX idx(f2))ENGINE= InnoDB;
+INSERT INTO t1(f1) SELECT seq FROM seq_1_to_50;
+FLUSH TABLE t1 FOR EXPORT;
+FOUND 108 /repairman/ in t1.ibd
+UNLOCK TABLES;
+ALTER TABLE t1 DROP INDEX idx;
+InnoDB		0 transactions not purged
+FLUSH TABLE t1 FOR EXPORT;
+NOT FOUND /repairman/ in t1.ibd
+UNLOCK TABLES;
+DROP TABLE t1;
+SET GLOBAL INNODB_LIMIT_OPTIMISTIC_INSERT_DEBUG=@save_debug;
+SET GLOBAL INNODB_IMMEDIATE_SCRUB_DATA_UNCOMPRESSED=@save_scrub;
+SET GLOBAL INNODB_PURGE_RSEG_TRUNCATE_FREQUENCY = @save_freq;

mysql-test/suite/innodb/t/scrub_debug.test
@@ -1,0 +1,31 @@
+--source include/have_innodb.inc
+--source include/have_sequence.inc
+--source include/have_debug.inc
+
+SET @save_debug=@@GLOBAL.INNODB_LIMIT_OPTIMISTIC_INSERT_DEBUG;
+SET @save_scrub=@@GLOBAL.INNODB_IMMEDIATE_SCRUB_DATA_UNCOMPRESSED;
+SET @save_freq=@@GLOBAL.INNODB_PURGE_RSEG_TRUNCATE_FREQUENCY;
+
+SET GLOBAL INNODB_PURGE_RSEG_TRUNCATE_FREQUENCY=1;
+SET GLOBAL INNODB_IMMEDIATE_SCRUB_DATA_UNCOMPRESSED=1;
+SET GLOBAL INNODB_LIMIT_OPTIMISTIC_INSERT_DEBUG=2;
+let $MYSQLD_DATADIR=`select @@datadir`;
+CREATE TABLE t1(f1 INT AUTO_INCREMENT PRIMARY KEY,
+		f2 VARCHAR(256) GENERATED ALWAYS as('repairman'),
+		INDEX idx(f2))ENGINE= InnoDB;
+INSERT INTO t1(f1) SELECT seq FROM seq_1_to_50;
+FLUSH TABLE t1 FOR EXPORT;
+let SEARCH_PATTERN= repairman;
+let SEARCH_FILE= $MYSQLD_DATADIR/test/t1.ibd;
+-- source include/search_pattern_in_file.inc
+UNLOCK TABLES;
+
+ALTER TABLE t1 DROP INDEX idx;
+-- source include/wait_all_purged.inc
+FLUSH TABLE t1 FOR EXPORT;
+-- source include/search_pattern_in_file.inc
+UNLOCK TABLES;
+DROP TABLE t1;
+SET GLOBAL INNODB_LIMIT_OPTIMISTIC_INSERT_DEBUG=@save_debug;
+SET GLOBAL INNODB_IMMEDIATE_SCRUB_DATA_UNCOMPRESSED=@save_scrub;
+SET GLOBAL INNODB_PURGE_RSEG_TRUNCATE_FREQUENCY = @save_freq;

commit 81e60f1a0a9bd94f6ffca0c78a9b8cb768dd2fb1

MDEV-32163 Crash recovery fails after DROP TABLE in system tablespace
fseg_free_extent(): After fsp_free_extent() succeeded, properly
mark the affected pages as freed. We failed to write FREE_PAGE records.

This bug was revealed or caused by
commit e938d7c18f6f117b63fcfc50dd975029d92409a7 (MDEV-32028).

storage/innobase/fsp/fsp0fsp.cc
@@ -2683,14 +2683,23 @@ fseg_free_extent(
 			      not_full_n_used - descr_n_used);
 	}
 
+	std::vector<uint8_t> going_to_free;
+	static_assert(FSP_EXTENT_SIZE_MIN == 256, "compatibility");
+	static_assert(FSP_EXTENT_SIZE_MAX == 64, "compatibility");
+
 	for (uint32_t i = 0; i < FSP_EXTENT_SIZE; i++) {
 		if (!xdes_is_free(descr, i)) {
-			buf_page_free(space, first_page_in_extent + i, mtr,
-				      __FILE__, __LINE__);
+			going_to_free.emplace_back(uint8_t(i));
 		}
 	}
 
 	fsp_free_extent(space, page, mtr);
+
+	for (uint32_t i : going_to_free) {
+		mtr->free(*space, first_page_in_extent + i);
+		buf_page_free(space, first_page_in_extent + i, mtr,
+			      __FILE__, __LINE__);
+	}
 }
 
 /**********************************************************************//**

mysql-test/suite/innodb/r/scrub.result
@@ -1,0 +1,13 @@
+SET GLOBAL
+innodb_file_per_table=OFF,
+innodb_purge_rseg_truncate_frequency=1,
+innodb_immediate_scrub_data_uncompressed=ON;
+CREATE TABLE t1(f1 INT NOT NULL, f2 INT NOT NULL,
+f3 INT NOT NULL, INDEX(f1),
+INDEX(f2), INDEX(f3))ENGINE=InnoDB;
+INSERT INTO t1 SELECT seq, seq, seq FROM seq_1_to_16384;
+INSERT INTO t1 SELECT seq, seq, seq FROM seq_1_to_16384;
+INSERT INTO t1 SELECT seq, seq, seq FROM seq_1_to_16384;
+DROP TABLE t1;
+SET GLOBAL innodb_max_purge_lag_wait=0;
+# restart

mysql-test/suite/innodb/t/scrub.test
@@ -1,0 +1,19 @@
+--source include/have_innodb.inc
+--source include/have_sequence.inc
+--source include/not_embedded.inc
+
+SET GLOBAL
+  innodb_file_per_table=OFF,
+  innodb_purge_rseg_truncate_frequency=1,
+  innodb_immediate_scrub_data_uncompressed=ON;
+CREATE TABLE t1(f1 INT NOT NULL, f2 INT NOT NULL,
+                f3 INT NOT NULL, INDEX(f1),
+                INDEX(f2), INDEX(f3))ENGINE=InnoDB;
+INSERT INTO t1 SELECT seq, seq, seq FROM seq_1_to_16384;
+INSERT INTO t1 SELECT seq, seq, seq FROM seq_1_to_16384;
+INSERT INTO t1 SELECT seq, seq, seq FROM seq_1_to_16384;
+DROP TABLE t1;
+SET GLOBAL innodb_max_purge_lag_wait=0;
+# The crash recovery would occasionally fail.
+let $shutdown_timeout=0;
+--source include/restart_mysqld.inc