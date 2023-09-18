480456a3da2b139995673ccaaed041496ccb94a4
Monty <monty@mariadb.org>
2023-09-18 17:30:22 +0300

    MDEV-32188 make TIMESTAMP use whole 32-bit unsigned range
    More windows changes:
    - Changed usage of timeval to my_timeval as the timeval parts on windows
      are 32-bit long, which causes some compiler issues on windows.

--- sql/structs.h
+++ sql/structs.h
@@ -1013,7 +1013,7 @@ public:
 };
 
 
-class Timeval: public timeval
+class Timeval: public my_timeval
 {
 protected:
   Timeval() = default;
@@ -1029,10 +1029,15 @@ public:
     DBUG_ASSERT(usec < 1000000);
     tv_usec= (uint)usec;
   }
-  explicit Timeval(const timeval &tv)
-   :timeval(tv)
-  { }
+  explicit Timeval(const my_timeval &tv)
+    :my_timeval(tv)
+  {}
 };
+
+static inline void my_timeval_trunc(struct my_timeval *tv, uint decimals)
+{
+  tv->tv_usec-= (suseconds_t) my_time_fraction_remainder(tv->tv_usec, decimals);
+}
 
 
 #endif /* STRUCTS_INCLUDED */
