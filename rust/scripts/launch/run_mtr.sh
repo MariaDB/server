4bb80135f0fd9431f6755469b82a35bfde1f142e
Trevor Gross <tmgross@umich.edu>
2023-09-18 00:53:43 -0400

    Update workflow to test in docker

--- rust/scripts/launch/run_mtr.sh
+++ rust/scripts/launch/run_mtr.sh
@@ -3,9 +3,13 @@ set -eaux
 echo running tests
 
 /obj/build-mariadb/mysql-test/mtr \
-    --mem \
     --force \
     --max-test-fail=40 \
     "--parallel=$(nproc)"
+
+# --mem \
+# export MTR_BINDIR=/obj/build-mariadb
+# mkdir /test
+# cd /test
 
 # --suite=unit
