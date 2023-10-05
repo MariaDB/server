0a9fd54d1c6a0e3922c9cd2675570b978868a5f7
Yuchen Pei <ycp@mariadb.com>
2023-10-05 13:19:58 +1100

    MDEV-26247 Re-implement spider gbh query rewrite of tables
    Spider GBH's query rewrite of table joins is overly complex and
    error-prone. We replace it with something closer to what
    dbug_print() (more specifically, print_join()) does, but catered to
    spider.
    
    More specifically, we replace the body of
    spider_db_mbase_util::append_from_and_tables() with a call to
    spider_db_mbase_util::append_join(), and remove downstream append_X
    functions.
    
    We make it handle const tables by rewriting them as (select 1). This
    fixes the main issue in MDEV-26247.
    
    We also ban semijoin from spider gbh, which fixes MDEV-31645 and
    MDEV-30392, as semi-join is an "internal" join, and "semi join" does
    not parse, and it is different from "join" in that it deduplicates the
    right hand side
    
    Not all queries passed to a group by handler are valid (MDEV-32273),
    for example, a join on expr may refer outer fields not in the current
    context. We detect this during the handler creation when walking the
    join. See also gbh_outer_fields_in_join.test.
    
    It also skips eliminated tables, which fixes MDEV-26193.

--- storage/spider/spd_group_by_handler.cc
+++ storage/spider/spd_group_by_handler.cc
@@ -25,6 +25,7 @@
 #include "probes_mysql.h"
 #include "sql_class.h"
 #include "sql_partition.h"
+#include "sql_select.h"
 #include "ha_partition.h"
 #endif
 #include "sql_common.h"
@@ -1282,14 +1283,16 @@ static int spider_send_query(
   DBUG_RETURN(0);
 }
 
+/*
+ Prepare and send query to data nodes and store the query results.
+*/
 int spider_group_by_handler::init_scan()
 {
   int error_num;
   DBUG_ENTER("spider_group_by_handler::init_scan");
   store_error = 0;
 #ifndef DBUG_OFF
-  Field **field;
-  for (field = table->field; *field; field++)
+  for (Field **field = table->field; *field; field++)
     DBUG_PRINT("info",("spider field_name=%s", SPIDER_field_name_str(*field)));
 #endif
 
@@ -1439,8 +1442,6 @@ group_by_handler *spider_create_group_by_handler(
   from = query->from;
   do {
     DBUG_PRINT("info",("spider from=%p", from));
-    if (from->table->const_table)
-      continue;
     ++table_count;
     if (from->table->part_info)
     {
@@ -1468,15 +1469,6 @@ group_by_handler *spider_create_group_by_handler(
 
   table_idx = 0;
   from = query->from;
-  while (from && from->table->const_table)
-  {
-    from = from->next_local;
-  }
-  if (!from)
-  {
-    /* all tables are const_table */
-    goto skip_free_table_holder;
-  }
 #if defined(PARTITION_HAS_GET_CHILD_HANDLERS)
   if (from->table->part_info)
   {
@@ -1512,8 +1504,6 @@ group_by_handler *spider_create_group_by_handler(
   }
   while ((from = from->next_local))
   {
-    if (from->table->const_table)
-      continue;
 #if defined(PARTITION_HAS_GET_CHILD_HANDLERS)
     if (from->table->part_info)
     {
@@ -1556,8 +1546,6 @@ group_by_handler *spider_create_group_by_handler(
 
   from = query->from;
   do {
-    if (from->table->const_table)
-      continue;
 #if defined(PARTITION_HAS_GET_CHILD_HANDLERS)
     if (from->table->part_info)
     {
@@ -1701,10 +1689,6 @@ group_by_handler *spider_create_group_by_handler(
     goto skip_free_table_holder;
 
   from = query->from;
-  while (from->table->const_table)
-  {
-    from = from->next_local;
-  }
 #if defined(PARTITION_HAS_GET_CHILD_HANDLERS)
   if (from->table->part_info)
   {
@@ -1777,8 +1761,6 @@ group_by_handler *spider_create_group_by_handler(
 
   while ((from = from->next_local))
   {
-    if (from->table->const_table)
-      continue;
     fields->clear_conn_holder_from_conn();
 
 #if defined(PARTITION_HAS_GET_CHILD_HANDLERS)
