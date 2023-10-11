09047842a72e99075cd9d324bb12daec7f0a14c0
Yuchen Pei <ycp@mariadb.com>
2023-10-11 14:25:50 +1100

    [experimental] A no-op change to trigger spider CI for mdev_31524 failures
    Failures occur in multiple builders in the branch bb-11.0-mdev-30014,
    e.g.
    
    https://buildbot.mariadb.org/#/builders/146/builds/26320

--- storage/spider/ha_spider.h
+++ storage/spider/ha_spider.h
@@ -14,6 +14,8 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335 USA */
 
+/* no-op change */
+
 #ifdef USE_PRAGMA_INTERFACE
 #pragma interface
 #endif
