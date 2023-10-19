380bde5984a429929803e8766a9c1967456c3bda
Elena Stepanova <elenst@mariadb.com>
2023-10-19 19:01:13 +0300

    Test commit: run feature-related suites on Windows

--- mysql-test/collections/buildbot_suites.bat
+++ mysql-test/collections/buildbot_suites.bat
@@ -1,12 +1,12 @@
 if "%MTR_PARALLEL%"=="" set MTR_PARALLEL=%NUMBER_OF_PROCESSORS%
 perl mysql-test-run.pl --verbose-restart --force --suite-timeout=120 --max-test-fail=10 --retry=3  --suite=^
-vcol,gcol,perfschema,^
+funcs_1,perfschema,^
 main,^
-innodb,^
+period,^
 versioning,^
 plugins,^
-mariabackup,^
+type_inet,^
 roles,^
-auth_gssapi,^
+type_uuid,^
 rocksdb,^
 sysschema
