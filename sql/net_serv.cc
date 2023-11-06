d6bc1a5edff9f1b1bc19360b3d0901eafa7bcf29
Monty <monty@mariadb.org>
2023-11-06 18:51:34 +0200

    Fixed wrong define USE_NET_CLEAR
    This fix has only an effect if the debug option -DUSE_NET_CLEAR is used

--- sql/net_serv.cc
+++ sql/net_serv.cc
@@ -271,7 +271,7 @@ my_bool net_realloc(NET *net, size_t length)
     -1   Don't know if data is ready or not
 */
 
-#if !defined(EMBEDDED_LIBRARY) && defined(DBUG_OFF)
+#if (!defined(EMBEDDED_LIBRARY) && defined(DBUG_OFF)) || defined(USE_NET_CLEAR)
 
 static int net_data_is_ready(my_socket sd)
 {
@@ -334,6 +334,7 @@ static int net_data_is_ready(my_socket sd)
   @param net			NET handler
   @param clear_buffer           if <> 0, then clear all data from comm buff
 */
+
 
 void net_clear(NET *net, my_bool clear_buffer __attribute__((unused)))
 {
