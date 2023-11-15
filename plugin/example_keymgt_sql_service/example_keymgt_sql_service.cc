80ddddc92c187bebb584acbc789a65d85a51f3dd
Trevor Gross <tmgross@umich.edu>
2023-11-14 23:39:15 -0500

    MDEV-32808: using sql_service within encryption key management plugin segfaults
    There is an assumption in the way the SQL service initializes threads
    that assumes a thread already exists. For a key management plugin
    this isn't the case. As such we perform the initializations on
    fil_crypt_thread() so all the SQL layer functionality doesn't
    segfault on the lack of a THD.
    
    Create a THD option for the innodb crypt thread.
    
    Co-author: Daniel Black <daniel@mariadb.org>

---
+++ plugin/example_keymgt_sql_service/example_keymgt_sql_service.cc
@@ -0,0 +1,95 @@
+#include <mysql/plugin_encryption.h>
+#include <cstring>
+
+#define STRING_WITH_LEN(X) (X ""), ((size_t) (sizeof(X) - 1))
+
+/* my_base.h without compile errors */
+#define HA_ERR_RETRY_INIT 129
+
+// AES128-GCM 128-bit key
+#define KEY_LEN 16
+
+static int run_sql(const char *query, size_t query_len)
+{
+
+  MYSQL *mysql= mysql_init(NULL);
+  if (!mysql)
+    return 1;
+
+  if (mysql_real_connect_local(mysql) == NULL)
+    return 1;
+
+  if (mysql_real_query(mysql, query, query_len))
+    return 1;
+
+  mysql_close(mysql);
+  return 0;
+}
+
+static unsigned int
+get_latest_key_version(unsigned int key_id)
+{
+  return run_sql(STRING_WITH_LEN("SELECT * FROM test.t1"));
+}
+
+static unsigned int
+get_key(unsigned int key_id, unsigned int version,
+        unsigned char* dstbuf, unsigned *buflen)
+{
+
+  if (run_sql(STRING_WITH_LEN("SELECT * FROM test.t1")))
+    return 1;
+
+  if (dstbuf == NULL) {
+    *buflen = KEY_LEN;
+  } else {
+    memset(dstbuf, 9, *buflen);
+  }
+
+  return 0;
+}
+
+static int example_keymgt_sql_service_init(void *p)
+{
+  return run_sql(STRING_WITH_LEN("CREATE TABLE test.t1 (id int)")) ? HA_ERR_RETRY_INIT : 0;
+}
+
+static int example_keymgt_sql_service_deinit(void *p)
+{
+  /* MDEV-33047 using sql_service within deinit of plugin segfaults on shutdown
+  return run_sql(STRING_WITH_LEN("DROP TABLE test.t1"));
+  */
+  return 0;
+}
+
+struct st_mariadb_encryption example_keymgt_sql_service= {
+  MariaDB_ENCRYPTION_INTERFACE_VERSION,
+  get_latest_key_version,
+  get_key,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+};
+
+/*
+  Plugin library descriptor
+*/
+maria_declare_plugin(example_keymgt_sql_service)
+{
+  MariaDB_ENCRYPTION_PLUGIN,
+  &example_keymgt_sql_service,
+  "example_keymgt_sql_service",
+  "Trevor, Daniel",
+  "Example keymgt plugin that uses sql service",
+  PLUGIN_LICENSE_GPL,
+  example_keymgt_sql_service_init,
+  example_keymgt_sql_service_deinit,
+  0x0100 /* 1.0 */,
+  NULL,	/* status variables */
+  NULL,	/* system variables */
+  "1.0",
+  MariaDB_PLUGIN_MATURITY_STABLE
+}
+maria_declare_plugin_end;
