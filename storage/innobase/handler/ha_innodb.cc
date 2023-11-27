d3bccbcac0a4dd412f88359bc9c347b51529883a
Vlad Lesin <vlad_lesin@mail.ru>
2023-11-27 21:03:21 +0300

    MDEV-28682 gcol.gcol_purge contaminates further execution of innodb.gap_locks
    ha_innobase::extra() invokes check_trx_exists() unconditionally even for
    not supported operations. check_trx_exists() creates and registers trx_t
    object if THD does not contain pointer to it. If ha_innobase::extra() does
    not support some operation, it just invokes check_trx_exists() and quites.
    If check_trx_exists() creates and registers new trx_t object for such
    operation, it will never be freed and deregistered.
    
    For example, if ha_innobase::extra() is invoked from purge thread with
    operation = HA_EXTRA_IS_ATTACHED_CHILDREN, like it goes in
    gcol.gcol_purge test, trx_t object will be registered, but not
    deregisreted, and this causes innodb.gap_lock failure, as "SHOW ENGINE
    INNODB STATUS" shows information about unexpected transaction at the end
    of trx_sys.trx_list.
    
    The fix is not to invoke check_trx_exists() for unsupported operations
    in ha_innobase::extra().
    
    Reviewed by: Marko Mäkelä
    
    MDEV-32859 gcol.gcol_purge causes innodb.gap_lock test failure
    
    ha_innobase::extra() invokes check_trx_exists() even for not supported
    operations. For some cases trx_t object, allocated in
    check_trx_exists(), will never be freed. For example, if
    ha_innobase::extra() is invoked from purge thread. And this can cause
    innodb.gap_lock failure, as SHOW ENGINE INNODB STATUS shows unexpected
    transactions info.
    
    The fix is not to invoke check_trx_exists() for unsupported operations
    in ha_innobase::extra().

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -15737,15 +15737,17 @@ ha_innobase::extra(
 {
 	/* Warning: since it is not sure that MariaDB calls external_lock()
 	before calling this function, m_prebuilt->trx can be obsolete! */
-	trx_t* trx = check_trx_exists(ha_thd());
+	trx_t* trx;
 
 	switch (operation) {
 	case HA_EXTRA_FLUSH:
+		(void)check_trx_exists(ha_thd());
 		if (m_prebuilt->blob_heap) {
 			row_mysql_prebuilt_free_blob_heap(m_prebuilt);
 		}
 		break;
 	case HA_EXTRA_RESET_STATE:
+		trx = check_trx_exists(ha_thd());
 		reset_template();
 		trx->duplicates = 0;
 	stmt_boundary:
@@ -15753,18 +15755,23 @@ ha_innobase::extra(
 		trx->bulk_insert = false;
 		break;
 	case HA_EXTRA_NO_KEYREAD:
+		(void)check_trx_exists(ha_thd());
 		m_prebuilt->read_just_key = 0;
 		break;
 	case HA_EXTRA_KEYREAD:
+		(void)check_trx_exists(ha_thd());
 		m_prebuilt->read_just_key = 1;
 		break;
 	case HA_EXTRA_KEYREAD_PRESERVE_FIELDS:
+		(void)check_trx_exists(ha_thd());
 		m_prebuilt->keep_other_fields_on_keyread = 1;
 		break;
 	case HA_EXTRA_INSERT_WITH_UPDATE:
+		trx = check_trx_exists(ha_thd());
 		trx->duplicates |= TRX_DUP_IGNORE;
 		goto stmt_boundary;
 	case HA_EXTRA_NO_IGNORE_DUP_KEY:
+		trx = check_trx_exists(ha_thd());
 		trx->duplicates &= ~TRX_DUP_IGNORE;
 		if (trx->is_bulk_insert()) {
 			/* Allow a subsequent INSERT into an empty table
@@ -15773,9 +15780,11 @@ ha_innobase::extra(
 		}
 		goto stmt_boundary;
 	case HA_EXTRA_WRITE_CAN_REPLACE:
+		trx = check_trx_exists(ha_thd());
 		trx->duplicates |= TRX_DUP_REPLACE;
 		goto stmt_boundary;
 	case HA_EXTRA_WRITE_CANNOT_REPLACE:
+		trx = check_trx_exists(ha_thd());
 		trx->duplicates &= ~TRX_DUP_REPLACE;
 		if (trx->is_bulk_insert()) {
 			/* Allow a subsequent INSERT into an empty table
@@ -15784,6 +15793,7 @@ ha_innobase::extra(
 		}
 		goto stmt_boundary;
 	case HA_EXTRA_BEGIN_ALTER_COPY:
+		trx = check_trx_exists(ha_thd());
 		m_prebuilt->table->skip_alter_undo = 1;
 		if (m_prebuilt->table->is_temporary()
 		    || !m_prebuilt->table->versioned_by_id()) {
@@ -15796,6 +15806,7 @@ ha_innobase::extra(
 			.first->second.set_versioned(0);
 		break;
 	case HA_EXTRA_END_ALTER_COPY:
+		trx = check_trx_exists(ha_thd());
 		m_prebuilt->table->skip_alter_undo = 0;
 		if (!m_prebuilt->table->is_temporary()
 		    && !high_level_read_only) {
