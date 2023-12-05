0018df2b553e302a97a85e3f6120c2f5126fe758
Daniele Sciascia <daniele.sciascia@galeracluster.com>
2023-12-05 01:40:57 +0100

    galera fix: Assertion `WSREP(thd)` failed in wsrep_restore_kill_after_commit()
    Wsrep_commit_empty happens too early when wsrep is disabled. Let the
    cleanup happen at end of statement.
    
    Signed-off-by: Julius Goryavsky <julius.goryavsky@mariadb.com>

--- sql/handler.cc
+++ sql/handler.cc
@@ -1660,7 +1660,7 @@ int ha_commit_trans(THD *thd, bool all)
       thd->m_transaction_psi= NULL;
     }
 #ifdef WITH_WSREP
-    if (wsrep_is_active(thd) && is_real_trans && !error)
+    if (WSREP(thd) && wsrep_is_active(thd) && is_real_trans && !error)
       wsrep_commit_empty(thd, all);
 #endif /* WITH_WSREP */
 
