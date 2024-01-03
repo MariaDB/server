96130b1898d34222d1f0543d75dc1acb6cdb8b09
Marko Mäkelä <marko.makela@mariadb.com>
2024-01-03 08:52:50 +0200

    MDEV-33157 WSREP: Fix function pointer mismatch
    wsrep_plugin_init(), wsrep_plugin_deinit(): Remove these dummy functions
    in order to fix an error that would be flagged by cmake -DWITH_UBSAN=ON
    when using clang.
    
    wsrep_show_ready(), wsrep_show_bf_aborts(): Correct the signature.

--- sql/wsrep_mysqld.h
+++ sql/wsrep_mysqld.h
@@ -148,7 +148,8 @@ extern char*       wsrep_cluster_capabilities;
 
 int  wsrep_show_status(THD *thd, SHOW_VAR *var, void *buff,
                        system_status_var *status_var, enum_var_type scope);
-int  wsrep_show_ready(THD *thd, SHOW_VAR *var, char *buff);
+int  wsrep_show_ready(THD *thd, SHOW_VAR *var, void *buff,
+                      system_status_var *, enum_var_type);
 void wsrep_free_status(THD *thd);
 void wsrep_update_cluster_state_uuid(const char* str);
 
