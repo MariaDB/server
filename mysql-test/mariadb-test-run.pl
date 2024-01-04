5041284169e0e47754cde3ba3ebd9c757df09b3f
Väinö Mäkelä <vaino.makela@iki.fi>
2024-01-04 13:33:57 +0200

    my_convert: Add tests
    Currently only --update mode is tested to avoid having the test output
    depend on the file paths selected by the test system.

--- mysql-test/mariadb-test-run.pl
+++ mysql-test/mariadb-test-run.pl
@@ -2267,6 +2267,14 @@ sub environment_setup {
   $ENV{'MYSQL_MY_PRINT_DEFAULTS'}= native_path($exe_my_print_defaults);
 
   # ----------------------------------------------------
+  # my_convert
+  # ----------------------------------------------------
+  my $exe_my_convert=
+    mtr_exe_exists("$bindir/extra$multiconfig/my_convert",
+		   "$path_client_bindir/my_convert");
+  $ENV{'MYSQL_MY_CONVERT'}= native_path($exe_my_convert);
+
+  # ----------------------------------------------------
   # myisam tools
   # ----------------------------------------------------
   $ENV{'MYISAMLOG'}= tool_arguments("storage/myisam", "myisamlog", );
