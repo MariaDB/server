pranavJibhakate/MariaDBserver

MDEV-4991 don't use ulong for sysvars
it's not portable

sql/mysqld.cc
@@ -445,10 +445,10 @@ my_bool sp_automatic_privileges= 1;
 ulong opt_binlog_rows_event_max_size;
 ulong binlog_row_metadata;
 my_bool opt_binlog_gtid_index= TRUE;
-ulong opt_binlog_gtid_index_page_size= 4096;
-ulong opt_binlog_gtid_index_sparse= 10;
-ulong opt_binlog_gtid_index_span_min= 4096;
-ulong opt_binlog_gtid_index_span_max= 65536;
+uint opt_binlog_gtid_index_page_size= 4096;
+uint opt_binlog_gtid_index_sparse= 10;
+uint opt_binlog_gtid_index_span_min= 4096;
+uint opt_binlog_gtid_index_span_max= 65536;
 my_bool opt_master_verify_checksum= 0;
 my_bool opt_slave_sql_verify_checksum= 1;
 const char *binlog_format_names[]= {"MIXED", "STATEMENT", "ROW", NullS};

MariaDB/server

sql/mysqld.cc
@@ -445,8 +445,8 @@ my_bool sp_automatic_privileges= 1;
 ulong opt_binlog_rows_event_max_size;
 ulong binlog_row_metadata;
 my_bool opt_binlog_gtid_index= TRUE;
-ulong opt_binlog_gtid_index_page_size= 4096;
-ulong opt_binlog_gtid_index_span_min= 65536;
+uint opt_binlog_gtid_index_page_size= 4096;
+uint opt_binlog_gtid_index_span_min= 65536;
 my_bool opt_master_verify_checksum= 0;
 my_bool opt_slave_sql_verify_checksum= 1;
 const char *binlog_format_names[]= {"MIXED", "STATEMENT", "ROW", NullS};