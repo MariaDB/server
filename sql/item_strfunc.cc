7828aadd3a529588f508dc9a49391319acc8e570
Sergei Golubchik <serg@mariadb.org>
2024-01-26 21:50:51 +0100

    MDEV-33318 ORDER BY COLLATE improperly applied to non-character columns
    when changing charset from latin1 to utf8, adjust max_length accordingly

--- sql/item_strfunc.cc
+++ sql/item_strfunc.cc
@@ -3590,7 +3590,9 @@ bool Item_func_set_collation::fix_length_and_dec()
   }
   collation.set(m_set_collation, DERIVATION_EXPLICIT,
                 args[0]->collation.repertoire);
-  max_length= args[0]->max_length;
+  ulonglong max_char_length= (ulonglong) args[0]->max_char_length();
+  fix_char_length_ulonglong(max_char_length * collation.collation->mbmaxlen);
+
   return FALSE;
 }
 
