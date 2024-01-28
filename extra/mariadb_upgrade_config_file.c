c49a629351992bf6ed65819e41c3907776bae236
Väinö Mäkelä <vaino.makela@iki.fi>
2024-01-28 21:02:18 +0200

    upgrade-config-file: Fix copyright typo

--- extra/mariadb_upgrade_config_file.c
+++ extra/mariadb_upgrade_config_file.c
@@ -1,6 +1,6 @@
 /* Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
    Copyright (c) 2011, 2018, MariaDB Corporation
-   Copyrigth (c) 2024, Väinö Mäkelä
+   Copyright (c) 2024, Väinö Mäkelä
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
