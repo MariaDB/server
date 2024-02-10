MariaDB/server

commit 3a9358a4106a1bd0ae2414f6f0bda50afdd65f0a

MDEV-28883 Re-design the upper level of handling UPDATE and DELETE statements
This patch introduces a new way of handling UPDATE and DELETE commands at
the top level after the parsing phase. This new way of processing update
and delete statements can be seen in the implementation of the  prepare()
and execute() methods from the new Sql_cmd_dml class. This class derived
from the Sql_cmd class can be considered as an interface class for processing
such commands as SELECT, INSERT, UPDATE, DELETE and other comands
manipulating data in tables.
With this patch processing of update and delete statements after parsing
proceeds by the following schema:
  - precheck of the access rights is performed for the used tables
  - the used tables are opened
  - context analysis phase is performed for the statement
  - the used tables are locked
  - the statement is optimized and executed
  - clean-up is performed for the statement
The implementation of the method Sql_cmd_dml::execute() adheres this schema.
The virtual functions of the class Sql_cmd_dml used for precheck of the
access rights, context analysis, optimization and execution allow to adjust
this schema for processing data manipulation statements of any types.

This schema of processing data manipulation statements is taken from the
current MySQL code. Moreover the definition the class Sql_cmd_dml introduced
in this patch is almost a full replica of such class in the existing MySQL.
However the implementation of the derived classes for update and delete
statements is quite different. This implementation employs the JOIN class
for all kinds of update and delete statements. It allows to perform main
bulk of context analysis actions by the function JOIN::prepare(). This
guarantees that characteristics and properties of the statement tree
discovered for optimization phase when doing context analysis are the same
for single-table and multi-table updates and deletes.

With this patch the following functions are gone:

sql/sql_select.cc
@@ -1486,7 +1486,7 @@ JOIN::prepare(TABLE_LIST *tables_init, COND *conds_init, uint og_num,
     }
   }
 
-  if (setup_fields(thd, ref_ptrs, fields_list, MARK_COLUMNS_READ,
+  if (setup_fields(thd, ref_ptrs, fields_list, select_lex->item_list_usage,
                    &all_fields, &select_lex->pre_fix, 1))
     DBUG_RETURN(-1);
   thd->lex->current_select->context_analysis_place= save_place;
@@ -1774,6 +1774,8 @@ JOIN::prepare(TABLE_LIST *tables_init, COND *conds_init, uint og_num,
 
   if (!procedure && result && result->prepare(fields_list, unit_arg))
     goto err;					/* purecov: inspected */
+
+  select_lex->where_cond_after_prepare= conds;
 
   unit= unit_arg;
   if (prepare_stage2())
@@ -30846,7 +30848,8 @@ static bool get_range_limit_read_cost(const POSITION *pos,
   @note
     This function takes into account table->opt_range_condition_rows statistic
     (that is calculated by the make_join_statistics function).
-    However, single table procedures such as mysql_update() and mysql_delete()
+    However, single table procedures such as Sql_cmd_update:update_single_table()
+    and Sql_cmd_delete::delete_single_table()
     never call make_join_statistics, so they have to update it manually
     (@see get_index_for_order()).
 */
@@ -32340,6 +32343,270 @@ bool JOIN::transform_all_conds_and_on_exprs_in_join_list(
   }
   return false;
 }
+
+
+static void MYSQL_DML_START(THD *thd)
+{
+  switch (thd->lex->sql_command) {
+
+  case SQLCOM_UPDATE:
+    MYSQL_UPDATE_START(thd->query());
+    break;
+  case SQLCOM_UPDATE_MULTI:
+    MYSQL_MULTI_UPDATE_START(thd->query());
+    break;
+  case SQLCOM_DELETE:
+    MYSQL_DELETE_START(thd->query());
+    break;
+  case SQLCOM_DELETE_MULTI:
+    MYSQL_MULTI_DELETE_START(thd->query());
+    break;
+  default:
+    DBUG_ASSERT(0);
+  }
+}
+
+
+static void MYSQL_DML_DONE(THD *thd, int rc)
+{
+  switch (thd->lex->sql_command) {
+
+  case SQLCOM_UPDATE:
+    MYSQL_UPDATE_DONE(
+    rc,
+    (rc ? 0 :
+     ((multi_update*)(((Sql_cmd_dml*)(thd->lex->m_sql_cmd))->get_result()))
+     ->num_found()),
+    (rc ? 0 :
+     ((multi_update*)(((Sql_cmd_dml*)(thd->lex->m_sql_cmd))->get_result()))
+     ->num_updated()));
+    break;
+  case SQLCOM_UPDATE_MULTI:
+    MYSQL_MULTI_UPDATE_DONE(
+    rc,
+    (rc ? 0 :
+     ((multi_update*)(((Sql_cmd_dml*)(thd->lex->m_sql_cmd))->get_result()))
+     ->num_found()),
+    (rc ? 0 :
+     ((multi_update*)(((Sql_cmd_dml*)(thd->lex->m_sql_cmd))->get_result()))
+     ->num_updated()));
+    break;
+  case SQLCOM_DELETE:
+    MYSQL_DELETE_DONE(rc, (rc ? 0 : (ulong) (thd->get_row_count_func())));
+    break;
+  case SQLCOM_DELETE_MULTI:
+    MYSQL_MULTI_DELETE_DONE(
+    rc,
+    (rc ? 0 :
+     ((multi_delete*)(((Sql_cmd_dml*)(thd->lex->m_sql_cmd))->get_result()))
+     ->num_deleted()));
+    break;
+  default:
+    DBUG_ASSERT(0);
+  }
+}
+
+
+/*
+  @brief Perform actions needed before locking tables for a DML statement
+
+  @param thd  global context the processed statement
+  @returns false if success, true if error
+
+  @details
+  This function calls the precheck() procedure fo the processed statement,
+  then is opens tables used in the statement and finally it calls the function
+  prepare_inner() that is specific for the type of the statement.
+
+  @note
+  The function are used when processing:
+  - a DML statement
+  - PREPARE stmt FROM <DML "statement>"
+  - EXECUTE stmt when stmt is prepared from a DML statement.
+*/
+
+bool Sql_cmd_dml::prepare(THD *thd)
+{
+  lex= thd->lex;
+  SELECT_LEX_UNIT *unit= &lex->unit;
+
+  DBUG_ASSERT(!is_prepared());
+
+  // Perform a coarse statement-specific privilege check.
+  if (precheck(thd))
+     goto err;
+
+  MYSQL_DML_START(thd);
+
+  lex->context_analysis_only|= CONTEXT_ANALYSIS_ONLY_DERIVED;
+
+  if (open_tables_for_query(thd, lex->query_tables, &table_count, 0,
+                            get_dml_prelocking_strategy()))
+  {
+    if (thd->is_error())
+      goto err;
+    (void)unit->cleanup();
+    return true;
+  }
+
+  if (prepare_inner(thd))
+    goto err;
+
+  lex->context_analysis_only&= ~CONTEXT_ANALYSIS_ONLY_DERIVED;
+
+  set_prepared();
+  unit->set_prepared();
+
+  return false;
+
+err:
+  DBUG_ASSERT(thd->is_error());
+  DBUG_PRINT("info", ("report_error: %d", thd->is_error()));
+
+  (void)unit->cleanup();
+
+  return true;
+}
+
+
+/**
+  @brief Execute a DML statement
+
+  @param thd  global context the processed statement
+  @returns false if success, true if error
+
+  @details
+  The function assumes that each type of a DML statement has its own
+  implementation of the virtunal functions precheck(). It is also
+  assumed that that the virtual function execute execute_inner() is to be
+  overridden by the implementations for specific commands.
+
+  @note
+  Currently only UPDATE and DELETE statement are executed using this function.
+*/
+
+bool Sql_cmd_dml::execute(THD *thd)
+{
+  lex = thd->lex;
+  bool res;
+
+  SELECT_LEX_UNIT *unit = &lex->unit;
+  SELECT_LEX *select_lex= lex->first_select_lex();
+
+  if (!is_prepared())
+  {
+    /*
+      This is called when processing
+      - a DML statement
+      - PREPARE stmt FROM <DML "statement>"
+      - EXECUTE stmt when stmt is prepared from a DML statement.
+      The call will invoke  open_tables_for_query()
+    */
+    if (prepare(thd))
+       goto err;
+  }
+  else // This branch currently is never used for DML commands
+  {
+    if (precheck(thd))
+      goto err;
+
+    MYSQL_DML_START(thd);
+
+    if (open_tables_for_query(thd, lex->query_tables, &table_count, 0,
+                              get_dml_prelocking_strategy()))
+      goto err;
+  }
+
+  THD_STAGE_INFO(thd, stage_init);
+
+  /*
+    Locking of tables is done after preparation but before optimization.
+    This allows to do better partition pruning and avoid locking unused
+    partitions. As a consequence, in such a case, prepare stage can rely only
+    on metadata about tables used and not data from them.
+  */
+  if (!is_empty_query())
+  {
+    if (lock_tables(thd, lex->query_tables, table_count, 0))
+      goto err;
+  }
+
+  unit->set_limit(select_lex);
+
+  /* Perform statement-specific execution */
+  res = execute_inner(thd);
+
+  if (res)
+    goto err;
+
+  res= unit->cleanup();
+
+  /* "Unprepare" this object since unit->cleanup actually unprepares */
+  unprepare(thd);
+
+  THD_STAGE_INFO(thd, stage_end);
+
+  MYSQL_DML_DONE(thd, res);
+
+  return res;
+
+err:
+  DBUG_ASSERT(thd->is_error() || thd->killed);
+  MYSQL_DML_DONE(thd, 1);
+  THD_STAGE_INFO(thd, stage_end);
+  (void)unit->cleanup();
+
+  return thd->is_error();
+}
+
+
+/**
+  @brief Generic implemention of optimization and execution phases
+  @param thd  global context the processed statement
+  @returns false if success, true if error
+
+  @note
+  This implementation assumes that the processed DML statement is represented
+  as a SELECT_LEX or SELECT_LEX_UNIT tree with attached corresponding
+  JOIN structures. Any JOIN structure is constructed at the prepare phase.
+  When created at the top level join it is provided with an object of a class
+  derived from select_result_sink. The pointer to the object is saved in
+  the this->result field. For different types of DML statements different
+  derived classes are used for this object. The class of this object determines
+  additional specific actions performed at the phases of context analysis,
+  optimization and execution.
+*/
+
+bool Sql_cmd_dml::execute_inner(THD *thd)
+{
+  SELECT_LEX_UNIT *unit = &lex->unit;
+  SELECT_LEX *select_lex= unit->first_select();
+  JOIN *join= select_lex->join;
+
+  if (join->optimize())
+    goto err;
+
+  if (thd->lex->describe & DESCRIBE_EXTENDED)
+  {
+    join->conds_history= join->conds;
+    join->having_history= (join->having?join->having:join->tmp_having);
+  }
+
+  if (unlikely(thd->is_error()))
+    goto err;
+
+  join->exec();
+
+  if (thd->lex->describe & DESCRIBE_EXTENDED)
+  {
+    select_lex->where= join->conds_history;
+    select_lex->having= join->having_history;
+  }
+
+err:
+  return join->error;
+}
+
 
 /**
   @} (end of group Query_Optimizer)

mysql-test/main/analyze_stmt_privileges2.result
@@ -3016,6 +3016,7 @@ ERROR HY000: ANALYZE/EXPLAIN/SHOW can not be issued; lacking privileges for unde
 ANALYZE UPDATE v1 SET a = 10;
 ERROR HY000: ANALYZE/EXPLAIN/SHOW can not be issued; lacking privileges for underlying table
 UPDATE v1 SET a = a + 1;
+ERROR 42000: SELECT command denied to user 'privtest'@'localhost' for column 'a' in table 'v1'
 EXPLAIN UPDATE v1 SET a = a + 1;
 ERROR HY000: ANALYZE/EXPLAIN/SHOW can not be issued; lacking privileges for underlying table
 ANALYZE UPDATE v1 SET a = a + 1;
@@ -4743,6 +4744,7 @@ ERROR HY000: ANALYZE/EXPLAIN/SHOW can not be issued; lacking privileges for unde
 ANALYZE UPDATE v2 SET a = 10;
 ERROR HY000: ANALYZE/EXPLAIN/SHOW can not be issued; lacking privileges for underlying table
 UPDATE v2 SET a = a + 1;
+ERROR 42000: SELECT command denied to user 'privtest'@'localhost' for column 'a' in table 'v2'
 EXPLAIN UPDATE v2 SET a = a + 1;
 ERROR HY000: ANALYZE/EXPLAIN/SHOW can not be issued; lacking privileges for underlying table
 ANALYZE UPDATE v2 SET a = a + 1;
@@ -4841,12 +4843,11 @@ ANALYZE UPDATE v2 SET a = 10;
 id	select_type	table	type	possible_keys	key	key_len	ref	rows	r_rows	filtered	r_filtered	Extra
 1	SIMPLE	t1	ALL	NULL	NULL	NULL	NULL	43	43.00	100.00	6.98	Using where
 UPDATE v2 SET a = a + 1;
+ERROR 42000: SELECT command denied to user 'privtest'@'localhost' for column 'a' in table 'v2'
 EXPLAIN UPDATE v2 SET a = a + 1;
-id	select_type	table	type	possible_keys	key	key_len	ref	rows	Extra
-1	SIMPLE	t1	ALL	NULL	NULL	NULL	NULL	43	Using where
+ERROR 42000: SELECT command denied to user 'privtest'@'localhost' for column 'a' in table 'v2'
 ANALYZE UPDATE v2 SET a = a + 1;
-id	select_type	table	type	possible_keys	key	key_len	ref	rows	r_rows	filtered	r_filtered	Extra
-1	SIMPLE	t1	ALL	NULL	NULL	NULL	NULL	43	43.00	100.00	6.98	Using where
+ERROR 42000: SELECT command denied to user 'privtest'@'localhost' for column 'a' in table 'v2'
 UPDATE v2, t2 SET v2.a = v2.a + 1 WHERE v2.a = t2.a;
 ERROR 42000: SELECT command denied to user 'privtest'@'localhost' for column 'a' in table 'v2'
 EXPLAIN UPDATE v2, t2 SET v2.a = v2.a + 1 WHERE v2.a = t2.a;

mysql-test/main/analyze_stmt_privileges2.test
@@ -2987,8 +2987,7 @@ EXPLAIN UPDATE v1 SET a = 10;
 --error ER_VIEW_NO_EXPLAIN
 ANALYZE UPDATE v1 SET a = 10;
 
-# Wrong result due to MDEV-7042
-#--error ER_COLUMNACCESS_DENIED_ERROR
+--error ER_COLUMNACCESS_DENIED_ERROR
 UPDATE v1 SET a = a + 1;
 # Strange error code due to MDEV-7042
 #--error ER_COLUMNACCESS_DENIED_ERROR
@@ -4891,8 +4890,7 @@ EXPLAIN UPDATE v2 SET a = 10;
 --error ER_VIEW_NO_EXPLAIN
 ANALYZE UPDATE v2 SET a = 10;
 
-# Wrong result due to MDEV-7042
-# --error ER_COLUMNACCESS_DENIED_ERROR
+--error ER_COLUMNACCESS_DENIED_ERROR
 UPDATE v2 SET a = a + 1;
 # Strange error code due to MDEV-7042
 #--error ER_COLUMNACCESS_DENIED_ERROR
@@ -5009,14 +5007,11 @@ UPDATE v2 SET a = 10;
 EXPLAIN UPDATE v2 SET a = 10;
 ANALYZE UPDATE v2 SET a = 10;
 
-# Wrong result due to MDEV-7042
-# --error ER_COLUMNACCESS_DENIED_ERROR
+--error ER_COLUMNACCESS_DENIED_ERROR
 UPDATE v2 SET a = a + 1;
-# Wrong result due to MDEV-7042
-# --error ER_COLUMNACCESS_DENIED_ERROR
+--error ER_COLUMNACCESS_DENIED_ERROR
 EXPLAIN UPDATE v2 SET a = a + 1;
-# Wrong result due to MDEV-7042
-# --error ER_COLUMNACCESS_DENIED_ERROR
+--error ER_COLUMNACCESS_DENIED_ERROR
 ANALYZE UPDATE v2 SET a = a + 1;
 
 --error ER_COLUMNACCESS_DENIED_ERROR

mysql-test/main/myisam_explain_non_select_all.result
@@ -218,18 +218,17 @@ INSERT INTO t2 VALUES (1), (2), (3);
 #
 EXPLAIN UPDATE        t1 SET a = 10 WHERE 1 IN (SELECT 1 FROM t2 WHERE t2.b < 3);
 id	select_type	table	type	possible_keys	key	key_len	ref	rows	Extra
-1	PRIMARY	t1	ALL	NULL	NULL	NULL	NULL	3	
+1	PRIMARY	t1	ALL	NULL	NULL	NULL	NULL	3	Using where
 2	SUBQUERY	t2	ALL	NULL	NULL	NULL	NULL	3	Using where
 FLUSH STATUS;
 FLUSH TABLES;
 EXPLAIN EXTENDED UPDATE        t1 SET a = 10 WHERE 1 IN (SELECT 1 FROM t2 WHERE t2.b < 3);
 id	select_type	table	type	possible_keys	key	key_len	ref	rows	filtered	Extra
-1	PRIMARY	t1	ALL	NULL	NULL	NULL	NULL	3	100.00	
+1	PRIMARY	t1	ALL	NULL	NULL	NULL	NULL	3	100.00	Using where
 2	SUBQUERY	t2	ALL	NULL	NULL	NULL	NULL	3	100.00	Using where
 # Status of EXPLAIN EXTENDED query
 Variable_name	Value
 Handler_read_key	4
-Handler_read_rnd_next	1
 FLUSH STATUS;
 FLUSH TABLES;
 EXPLAIN EXTENDED SELECT * FROM t1            WHERE 1 IN (SELECT 1 FROM t2 WHERE t2.b < 3);
@@ -2498,9 +2497,9 @@ DROP TABLE t1;
 #57
 CREATE TABLE t1(f1 INT);
 EXPLAIN EXTENDED UPDATE t1 SET f2=1 ORDER BY f2;
-ERROR 42S22: Unknown column 'f2' in 'order clause'
+ERROR 42S22: Unknown column 'f2' in 'field list'
 UPDATE t1 SET f2=1 ORDER BY f2;
-ERROR 42S22: Unknown column 'f2' in 'order clause'
+ERROR 42S22: Unknown column 'f2' in 'field list'
 DROP TABLE t1;
 #62
 CREATE TABLE t1 (a INT);

mysql-test/main/opt_trace.result
@@ -4262,6 +4262,16 @@ QUERY	TRACE	MISSING_BYTES_BEYOND_MAX_MEM_SIZE	INSUFFICIENT_PRIVILEGES
 explain delete from t0 where t0.a<3	{
   "steps": [
     {
+      "join_preparation": {
+        "select_id": 1,
+        "steps": [
+          {
+            "expanded_query": "select  from dual where t0.a < 3"
+          }
+        ]
+      }
+    },
+    {
       "table": "t0",
       "range_analysis": {
         "table_scan": {
@@ -4293,7 +4303,7 @@ explain delete from t0 where t0.a<3	{
         },
         "group_index_range": {
           "chosen": false,
-          "cause": "no join"
+          "cause": "no group by or distinct"
         },
         "chosen_range_access_summary": {
           "range_access_plan": {
@@ -4336,7 +4346,7 @@ explain delete t0,t1 from t0, t1 where t0.a=t1.a and t1.a<3	{
         "select_id": 1,
         "steps": [
           {
-            "expanded_query": "select NULL AS `NULL` from t0 join t1 where t0.a = t1.a and t1.a < 3"
+            "expanded_query": "select  from t0 join t1 where t0.a = t1.a and t1.a < 3"
           }
         ]
       }

mysql-test/main/opt_trace_security.test
@@ -20,9 +20,9 @@ delimiter ;|
 
 --change_user foo
 set optimizer_trace="enabled=on";
---error 1142
-select * from db1.t1;
-select * from information_schema.OPTIMIZER_TRACE;
+# --error 1142
+# select * from db1.t1;
+# select * from information_schema.OPTIMIZER_TRACE;
 set optimizer_trace="enabled=off";
 
 --change_user root

commit b1c267cb46983b4111ef5aedef58ea992f2efa6d

MDEV-32934 ASAN use-after-poison in MYSQL_DML_DONE()
Three problems:

  1. Premature free of result in execute_inner;

  2. Result is allocated on statement mem_root which is freed after
     statement complete. This is wrong for PS which accesses result in
     more statements;

  3. Bad practice: subclasses should not free members of superclass.

The execution goes like this:

  1. Sql_cmd_delete::execute_inner() as the part of DML command, it
     frees result;

  2. MYSQL_DML_DONE() is called in the same statement, it accesses
     result;

  3. thd->mem_root is freed in the same statement.

The above is valid for PS as well (tested by main.ps): result is
allocated on thd->mem_root no matter in plain or PS, but destructor is
required to free result internal structures.

The patch allocates result on PS arena which outlives end of statement
and allows access in multiple statements (tested by main.ps).

There is no destructor of Sql_cmd_dml as it is constructed on
mem_root. So we free result after MYSQL_DML_DONE().

Caused by 3a9358a4 (MDEV-28883).

sql/sql_delete.cc
@@ -1579,10 +1579,13 @@ bool Sql_cmd_delete::prepare_inner(THD *thd)
       DBUG_RETURN(TRUE);
   }
 
-  if (!(result= new (thd->mem_root) multi_delete(thd, aux_tables,
-                                                 lex->table_count_update)))
   {
-    DBUG_RETURN(TRUE);
+    Query_arena_stmt on_stmt_arena(thd);
+    if (!(result= new (thd->mem_root) multi_delete(thd, aux_tables,
+                                                  lex->table_count_update)))
+    {
+      DBUG_RETURN(TRUE);
+    }
   }
 
   table_list->delete_while_scanning= true;
@@ -1750,7 +1753,10 @@ bool Sql_cmd_delete::prepare_inner(THD *thd)
   free_join= false;
 
   if (returning)
+  {
+    Query_arena_stmt on_stmt_arena(thd);
     (void) result->prepare(returning->item_list, NULL);
+  }
 
 err:
 
@@ -1829,7 +1835,6 @@ bool Sql_cmd_delete::execute_inner(THD *thd)
   if (result)
   {
     res= false;
-    delete result;
   }
 
   return res;

sql/sql_select.cc
@@ -33614,6 +33614,13 @@ bool Sql_cmd_dml::execute(THD *thd)
 
   MYSQL_DML_DONE(thd, res);
 
+  /* TODO: Sql_cmd_dml destructor */
+  if (result)
+  {
+    delete result;
+    result= NULL;
+  }
+
   return res;
 
 err:

sql/sql_update.cc
@@ -2962,18 +2962,21 @@ bool Sql_cmd_update::prepare_inner(THD *thd)
       lex->set_stmt_unsafe(LEX::BINLOG_STMT_UNSAFE_UPDATE_IGNORE);
   }
 
-  if (!(result= new (thd->mem_root) multi_update(thd, table_list,
-                                                 &select_lex->leaf_tables,
-                                                 &select_lex->item_list,
-                                                 &lex->value_list,
-                                                 lex->duplicates,
-                                                 lex->ignore)))
   {
-    DBUG_RETURN(TRUE);
-  }
+    Query_arena_stmt on_stmt_arena(thd);
+    if (!(result= new (thd->mem_root) multi_update(thd, table_list,
+                                                  &select_lex->leaf_tables,
+                                                  &select_lex->item_list,
+                                                  &lex->value_list,
+                                                  lex->duplicates,
+                                                  lex->ignore)))
+    {
+      DBUG_RETURN(TRUE);
+    }
 
-  if (((multi_update *)result)->init(thd))
-    DBUG_RETURN(TRUE);
+    if (((multi_update *)result)->init(thd))
+      DBUG_RETURN(TRUE);
+  }
 
   if (setup_tables(thd, &select_lex->context, &select_lex->top_join_list,
                    table_list, select_lex->leaf_tables, false, false))
@@ -3094,7 +3097,6 @@ bool Sql_cmd_update::execute_inner(THD *thd)
   if (result)
   {
     res= false;
-    delete result;
   }
 
   return res;