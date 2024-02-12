92f87f2cf0193309247c5e5b52ca99cfe76c6ed1
Marko Mäkelä <marko.makela@mariadb.com>
2024-02-12 17:01:35 +0200

    Cleanup: Remove changed_pages_bitmap
    The innodb_changed_pages plugin only was part of XtraDB, never InnoDB.
    It would be useful for incremental backups.
    We will remove the code from mariadb-backup for now, because it cannot
    serve any useful purpose until the server part has been implemented.

--- extra/mariabackup/xtrabackup.cc
+++ extra/mariabackup/xtrabackup.cc
@@ -101,7 +101,6 @@ Street, Fifth Floor, Boston, MA 02110-1335 USA
 #include "ds_buffer.h"
 #include "ds_tmpfile.h"
 #include "xbstream.h"
-#include "changed_page_bitmap.h"
 #include "read_filt.h"
 #include "backup_wsrep.h"
 #include "innobackupex.h"
@@ -155,7 +154,6 @@ char *xtrabackup_incremental;
 lsn_t incremental_lsn;
 lsn_t incremental_to_lsn;
 lsn_t incremental_last_lsn;
-xb_page_bitmap *changed_page_bitmap;
 
 char *xtrabackup_incremental_basedir; /* for --backup */
 char *xtrabackup_extra_lsndir; /* for --backup with --extra-lsndir */
@@ -424,6 +422,7 @@ char orig_argv1[FN_REFLEN];
 pthread_mutex_t backup_mutex;
 pthread_cond_t  scanned_lsn_cond;
 
+typedef decltype(fil_space_t::id) space_id_t;
 typedef std::map<space_id_t,std::string> space_id_to_name_t;
 
 struct ddl_tracker_t {
@@ -2855,12 +2854,7 @@ static my_bool xtrabackup_copy_datafile(ds_ctxt *ds_data,
 		goto skip;
 	}
 
-	if (!changed_page_bitmap) {
-		read_filter = &rf_pass_through;
-	}
-	else {
-		read_filter = &rf_bitmap;
-	}
+	read_filter = &rf_pass_through;
 
 	res = xb_fil_cur_open(&cursor, read_filter, node, thread_n, ULLONG_MAX);
 	if (res == XB_FIL_CUR_SKIP) {
@@ -4788,11 +4782,6 @@ fail_before_log_copying_thread_start:
 	log_copying_stop = os_event_create(0);
 	os_thread_create(log_copying_thread);
 
-	/* FLUSH CHANGED_PAGE_BITMAPS call */
-	if (!flush_changed_page_bitmaps()) {
-		goto fail;
-	}
-
 	ut_a(xtrabackup_parallel > 0);
 
 	if (xtrabackup_parallel > 1) {
@@ -4876,9 +4865,6 @@ fail_before_log_copying_thread_start:
 		goto fail;
 	}
 
-	if (changed_page_bitmap) {
-		xb_page_bitmap_deinit(changed_page_bitmap);
-	}
 	backup_datasinks.destroy();
 
 	msg("Redo log (from LSN " LSN_PF " to " LSN_PF
