6b7508edb8144653aa7a4ac4be97b3cbd434bdb5
Monty <monty@mariadb.org>
2024-02-20 17:49:19 +0200

    This is just to get columnstore to compile
    This commit can be ignored when columnstore changes to use
    rows_stats.updated+rows_stats.rows_inserted+rows_stats.rows_deleted
    instead of 'rows_changed'
    in storage/columnstore/columnstore/dbcon/mysql/ha_mcs_impl.cpp

--- sql/handler.h
+++ sql/handler.h
@@ -3295,6 +3295,7 @@ public:
     inserter.
   */
   /* Statistics  variables */
+  ulonglong rows_changed;   /* This is just to get columnstore to compile */
   struct rows_stats rows_stats;
   /* One bigger than needed to avoid to test if key == MAX_KEY */
   ulonglong index_rows_read[MAX_KEY+1];
