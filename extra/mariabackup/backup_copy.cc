8a5d0787e1166c93a5ee9a0252d044681ec198a2
Yuchen Pei <ycp@mariadb.com>
2024-04-18 17:16:44 +1000

    [merge fixup] spider: restore extern declarations of some vars
    How did they get removed during the merge?? The removal did not appear
    in the resolution diff at all.

--- extra/mariabackup/backup_copy.cc
+++ extra/mariabackup/backup_copy.cc
@@ -1509,10 +1509,9 @@ ibx_copy_incremental_over_full()
 		if (!(ret = backup_files_from_datadir(ds_data,
 						      xtrabackup_incremental_dir,
 						      "aws-kms-key")) ||
-                    (aria_log_dir_path &&
-		     !(ret = backup_files_from_datadir(ds_data,
-						       xtrabackup_incremental_dir,
-						       "aria_log"))))
+                    !(ret = backup_files_from_datadir(ds_data,
+                                                      xtrabackup_incremental_dir,
+                                                      "aria_log")))
 			goto cleanup;
 
 		/* copy supplementary files */
