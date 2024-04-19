8e663f5e9041e8e0998c792bf8d0c297a8b0721a
Marko Mäkelä <marko.makela@mariadb.com>
2024-04-19 11:04:51 +0300

    MDEV-32791 MariaDB cannot be installed on Red Hat ubi9
    The libpmem dependency that had been added in
    commit 3daef523af25e4f1e4e75d2c26a9b25475f0c679 (MDEV-17084)
    did not achieve any measurable performance improvement when
    comparing the same PMEM device with and without "mount -o dax"
    using the Linux ext4 file system.
    
    Because Red Hat has deprecated libpmem, let us remove the code
    altogether.
    
    Note: This is a 10.6 version of
    commit 3f9f5ca48e6be55613926964314f550c8ca8542d
    which will retain PMEM support in MariaDB Server 10.11.

--- debian/autobake-deb.sh
+++ debian/autobake-deb.sh
@@ -61,12 +61,6 @@ replace_uring_with_aio()
       -e '/-DWITH_URING=ON/d' -i debian/rules
 }
 
-disable_pmem()
-{
-  sed '/libpmem-dev/d' -i debian/control
-  sed '/-DWITH_PMEM=ON/d' -i debian/rules
-}
-
 architecture=$(dpkg-architecture -q DEB_BUILD_ARCH)
 
 # Parse release name and number from Linux standard base release
@@ -99,18 +93,10 @@ in
   # Debian
   "buster")
     replace_uring_with_aio
-    if [ ! "$architecture" = amd64 ]
-    then
-      disable_pmem
-    fi
     ;&
   "bullseye"|"bookworm")
     # mariadb-plugin-rocksdb in control is 4 arches covered by the distro rocksdb-tools
     # so no removal is necessary.
-    if [[ ! "$architecture" =~ amd64|arm64|ppc64el ]]
-    then
-      disable_pmem
-    fi
     if [[ ! "$architecture" =~ amd64|arm64|armel|armhf|i386|mips64el|mipsel|ppc64el|s390x ]]
     then
       replace_uring_with_aio
@@ -131,10 +117,6 @@ in
     if [[ ! "$architecture" =~ amd64|arm64|ppc64el|s390x ]]
     then
       remove_rocksdb_tools
-    fi
-    if [[ ! "$architecture" =~ amd64|arm64|ppc64el ]]
-    then
-      disable_pmem
     fi
     if [[ ! "$architecture" =~ amd64|arm64|armhf|ppc64el|s390x ]]
     then
