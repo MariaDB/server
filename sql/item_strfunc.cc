4375245d5d9f01cabb6e3fd6c637535e724eae38
Daniel Black <daniel@mariadb.org>
2024-05-22 17:43:17 +1000

    MDEV-34206 compile failure: fmt use incompatible with libfmt-10.2.[2]+
    Upstream libfmt commit https://github.com/fmtlib/fmt/commit/d70729215fba1d54862e407b626abf86ddf409bf
    now requires the format function to be const.
    
    Adjust the function prototype so it is const and can compile.

--- sql/item_strfunc.cc
+++ sql/item_strfunc.cc
@@ -1382,7 +1382,7 @@ bool Item_func_sformat::fix_length_and_dec(THD *thd)
 namespace fmt {
   template <> struct formatter<String>: formatter<string_view> {
     template <typename FormatContext>
-    auto format(String c, FormatContext& ctx) -> decltype(ctx.out()) {
+    auto format(String c, FormatContext& ctx) const -> decltype(ctx.out()) {
       string_view name = { c.ptr(), c.length() };
       return formatter<string_view>::format(name, ctx);
     };
