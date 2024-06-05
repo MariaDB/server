ce9efb4e0223c978dcf4511ed33f5118e3b3c3ce
Vladislav Vaintroub <vvaintroub@gmail.com>
2024-06-05 16:34:50 +0200

    MDEV-34296 tpool - declare thread_local_waiter "static thread_local"

--- storage/innobase/log/log0sync.cc
+++ storage/innobase/log/log0sync.cc
@@ -186,7 +186,7 @@ void group_commit_lock::set_pending(group_commit_lock::value_type num)
 }
 
 const unsigned int MAX_SPINS = 1; /** max spins in acquire */
-thread_local group_commit_waiter_t thread_local_waiter;
+static thread_local group_commit_waiter_t thread_local_waiter;
 
 group_commit_lock::lock_return_code group_commit_lock::acquire(value_type num)
 {
