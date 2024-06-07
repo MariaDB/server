0d85c905c44f9d88e4a17f56594cc389d777abaf
Julius Goryavsky <julius.goryavsky@mariadb.com>
2024-06-07 14:53:11 +0200

    MDEV-34269: post-fix code simplification
    The code is slightly simplified taking into account
    the fact that partition_ht() always returns a normal
    hton when there is no partitioning.

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -4747,9 +4747,7 @@ mysql_execute_command(THD *thd)
 #ifdef WITH_WSREP
       if (wsrep && !first_table->view)
       {
-        const handlerton *hton = first_table->table->file->partition_ht() ?
-          first_table->table->file->partition_ht() : first_table->table->file->ht;
-        bool is_innodb= (hton->db_type == DB_TYPE_INNODB);
+        bool is_innodb= first_table->table->file->partition_ht()->db_type == DB_TYPE_INNODB;
 
         // For consistency check inserted table needs to be InnoDB
         if (!is_innodb && thd->wsrep_consistency_check != NO_CONSISTENCY_CHECK)
