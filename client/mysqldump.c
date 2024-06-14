d60f5c11ea9008fa57444327526e3d2c8633ba06
Sergei Golubchik <serg@mariadb.org>
2024-06-14 17:32:52 +0200

    MDEV-34318 mariadb-dump SQL syntax error with MAX_STATEMENT_TIME against Percona MySQL server
    protect MariaDB conditional comments from a bug
    in Percona MySQL comment parser

--- client/mysqldump.c
+++ client/mysqldump.c
@@ -6951,11 +6951,11 @@ int main(int argc, char **argv)
     write_header(md_result_file, *argv);
 
   /* Set MAX_STATEMENT_TIME to 0 unless set in client */
-  my_snprintf(query, sizeof(query), "/*!100100 SET @@MAX_STATEMENT_TIME=%f */", opt_max_statement_time);
+  my_snprintf(query, sizeof(query), "/*M!100100 SET @@MAX_STATEMENT_TIME=%f */", opt_max_statement_time);
   mysql_query(mysql, query);
 
   /* Set server side timeout between client commands to server compiled-in default */
-  mysql_query(mysql, "/*!100100 SET WAIT_TIMEOUT=DEFAULT */");
+  mysql_query(mysql, "/*M!100100 SET WAIT_TIMEOUT=DEFAULT */");
 
   /* Check if the server support multi source */
   if (mysql_get_server_version(mysql) >= 100000)
