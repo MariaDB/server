284a095e2b243d029531149e3a3ab7ebfb4295e3
Yuchen Pei <ycp@mariadb.com>
2024-06-19 13:35:05 +0800

    MDEV-28894 Spider: remove #ifdef HA_EXTRA_HAS_STARTING_ORDERED_INDEX_SCAN
    HA_EXTRA_STARTING_ORDERED_INDEX_SCAN was added latest 2018:
    921c5e93145

--- storage/spider/ha_spider.cc
+++ storage/spider/ha_spider.cc
@@ -1161,10 +1161,7 @@ int ha_spider::extra(
       if (!(wide_handler->trx = spider_get_trx(ha_thd(), TRUE, &error_num)))
         DBUG_RETURN(error_num);
       break;
-#if defined(HA_EXTRA_HAS_STARTING_ORDERED_INDEX_SCAN) || defined(HA_EXTRA_HAS_HA_EXTRA_USE_CMP_REF)
-#ifdef HA_EXTRA_HAS_STARTING_ORDERED_INDEX_SCAN
     case HA_EXTRA_STARTING_ORDERED_INDEX_SCAN:
-#endif
 #ifdef HA_EXTRA_HAS_HA_EXTRA_USE_CMP_REF
     case HA_EXTRA_USE_CMP_REF:
 #endif
@@ -1195,7 +1192,6 @@ int ha_spider::extra(
         }
       }
       break;
-#endif
     default:
       break;
   }
