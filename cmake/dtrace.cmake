267fba2cee3908495c3fbe598406272a8990518e
Aleksey Midenkov <midenok@gmail.com>
2024-06-25 17:59:01 +0300

    Dtrace fix
    When there are GCC-incompatible compiler flags dtrace fails like this:
    
    gcc: error: unrecognized command-line option â-fno-limit-debug-infoâ
    gcc: error: unrecognized command-line option â-mbranches-within-32B-boundariesâ
    "gcc .dtrace-temp.3fd6bacf.c" failed
    Usage /usr/bin/dtrace [--help] [-h | -G] [-C [-I<Path>]] -s File.d [-o <File>]

--- cmake/dtrace.cmake
+++ cmake/dtrace.cmake
@@ -125,16 +125,25 @@ FUNCTION(DTRACE_INSTRUMENT target)
         WORKING_DIRECTORY ${objdir}
       )
     ELSEIF(CMAKE_SYSTEM_NAME MATCHES "Linux")
-      # dtrace on Linux runs gcc and uses flags from environment
-      SET(CFLAGS_SAVED $ENV{CFLAGS})
-      SET(ENV{CFLAGS} ${CMAKE_C_FLAGS})
+      IF (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        # dtrace on Linux runs gcc and uses flags from environment
+        SET(CFLAGS_SAVED $ENV{CFLAGS})
+        # We want to strip off all warning flags, including -Werror=xxx-xx,
+        # but keep flags like
+        #  -Wp,-D_FORTIFY_SOURCE=2
+        STRING(REGEX REPLACE "-W[A-Za-z0-9][-A-Za-z0-9=]+" ""
+          C_FLAGS "${CMAKE_C_FLAGS}")
+        SET(ENV{CFLAGS} ${CMAKE_C_FLAGS})
+      ENDIF()
       SET(outfile "${CMAKE_BINARY_DIR}/probes_mysql.o")
       # Systemtap object
       EXECUTE_PROCESS(
         COMMAND ${DTRACE} -G -s ${CMAKE_SOURCE_DIR}/include/probes_mysql.d.base
         -o ${outfile}
         )
-      SET(ENV{CFLAGS} ${CFLAGS_SAVED})
+      IF (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        SET(ENV{CFLAGS} ${CFLAGS_SAVED})
+      ENDIF()
     ENDIF()
 
     # Do not try to extend the library if we have not built the .o file
