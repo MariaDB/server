a059f5145353f1c4ffd9ad2a6cb3b9ebe394c1b1
Sergey Vojtovich <svoj@mariadb.org>
2024-07-03 19:52:53 +0400

    Disable VECTOR indexes with partitioned tables

--- sql/sql_table.cc
+++ sql/sql_table.cc
@@ -3244,6 +3244,11 @@ mysql_prepare_create_table_finalize(THD *thd, HA_CREATE_INFO *create_info,
       key_number--;                             // Skip this key
       continue;
     case Key::VECTOR:
+        if (thd->work_part_info)
+        {
+          my_error(ER_FEATURE_NOT_SUPPORTED_WITH_PARTITIONING, MYF(0), "VECTOR");
+          DBUG_RETURN(TRUE);
+        }
         if (key->key_create_info.algorithm == HA_KEY_ALG_UNDEF)
           key->key_create_info.algorithm= HA_KEY_ALG_VECTOR;
         break;
