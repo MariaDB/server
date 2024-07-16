05d744ec73b725b4aab8bb83bc73cfac09b66dad
Hugo Wen <wenhug@amazon.com>
2024-07-16 15:15:17 +0200

    MDEV-33408 Initial support for vector DELETE and UPDATE
    When the source row is deleted, mark the corresponding node in HNSW
    index by setting `tref` to null. An index is added for the `tref` in
    secondary table for faster searching of the to-be-marked nodes.
    
    The nodes marked as deleted will still be used for search, but will not
    be included in the final query results.
    
    As skipping deleted nodes and not adding deleted nodes for new-inserted
    nodes' neighbor list could impact the performance, we now only skip
    these nodes in search results.
    
    - for some reason the bitmap is not set for hlindex during the delete so
      I had to temporarily comment out one line
    
    All new code of the whole pull request, including one or several files
    that are either new files or modified ones, are contributed under the
    BSD-new license. I am contributing on behalf of my employer Amazon Web
    Services, Inc.

--- sql/vector_mhnsw.h
+++ sql/vector_mhnsw.h
@@ -25,6 +25,7 @@ const LEX_CSTRING mhnsw_hlindex_table_def(THD *thd, uint ref_length);
 int mhnsw_insert(TABLE *table, KEY *keyinfo);
 int mhnsw_read_first(TABLE *table, KEY *keyinfo, Item *dist, ulonglong limit);
 int mhnsw_read_next(TABLE *table);
+int mhnsw_invalidate(TABLE *table, uchar *rec, KEY *keyinfo);
 void mhnsw_free(TABLE_SHARE *share);
 
 extern ulonglong mhnsw_cache_size;
