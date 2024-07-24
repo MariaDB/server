d072a29601834cd61252158c95f068aa13eb55fa
Galina Shalygina <galina.sh@mariadb.com>
2024-07-24 13:55:55 +0200

    MDEV-23983: Crash caused by query containing constant having clause
    Before this patch the crash occured when a single row dataset is used and
    Item::remove_eq_conds() is called for HAVING. This function is not supposed
    to be called after the elimination of multiple equalities.
    
    To fix this problem instead of Item::remove_eq_conds() Item::val_int() is
    used. In this case the optimizer tries to evaluate the condition for the
    single row dataset and discovers impossible HAVING immediately. So, the
    execution phase is skipped.
    
    Approved by Igor Babaev <igor@maridb.com>

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -2971,19 +2971,22 @@ int JOIN::optimize_stage2()
     which do not use aggregate functions. In such case
     temporary table may not be used and const condition
     elements may be lost during further having
-    condition transformation in JOIN::exec.
+    condition transformation.
   */
   if (having && const_table_map && !having->with_sum_func())
   {
     having->update_used_tables();
-    having= having->remove_eq_conds(thd, &select_lex->having_value, true);
-    if (select_lex->having_value == Item::COND_FALSE)
+    if (having->const_item() && !having->is_expensive())
     {
-      having= new (thd->mem_root) Item_bool(thd, false);
-      zero_result_cause= "Impossible HAVING noticed after reading const tables";
-      error= 0;
-      select_lex->mark_const_derived(zero_result_cause);
-      goto setup_subq_exit;
+      bool having_value= having->val_int();
+      having= new (thd->mem_root) Item_bool(thd, having_value);
+      if (!having_value)
+      {
+        zero_result_cause= "Impossible HAVING noticed after reading const tables";
+        error= 0;
+        select_lex->mark_const_derived(zero_result_cause);
+        goto setup_subq_exit;
+      }
     }
   }
 
