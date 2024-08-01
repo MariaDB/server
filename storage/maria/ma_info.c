2047483417d0325e90be52739a53070a9d299247
ParadoxV5 <paradox.ver5@gmail.com>
2024-07-31 22:42:56 -0600

    Tag `my_printf_error` with `ATTRIBUTE_FORMAT`
    [Breaking]
    The `my_print_error` service passes formats and args directly
    to `my_vsnprintf`. Just like the `my_snprintf` service,
    I increased this service’s major version because:
    * Custom suffixes are now a thing
      (and custom specifiers will soon no longer be).
    * GCC `-Wformat` now checks formats sent to them.

--- storage/maria/ma_info.c
+++ storage/maria/ma_info.c
@@ -140,7 +140,7 @@ void _ma_report_error(int errcode, const LEX_STRING *name, myf flags)
       file_name+= length - 64;
     }
   }
-  my_printf_error(errcode, "Got error '%M' for '%s'",
+  my_printf_error(errcode, "Got error '%iE' for '%s'",
                   flags, errcode, file_name);
   DBUG_VOID_RETURN;
 }
