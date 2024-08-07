8a52639ede533affdc35272894819daff581e7c8
Yuchen Pei <ycp@mariadb.com>
2024-08-07 13:27:44 +1000

    MDEV-34716 spider: some trivial cleanups and documentation
    - document tmp_share, which are temporary spider shares with only one
    link (no ha)
    - simplify spider_get_sys_tables_connect_info() where link_idx is
    always 0

--- storage/spider/spd_include.h
+++ storage/spider/spd_include.h
@@ -1440,6 +1440,7 @@ typedef struct st_spider_mon_table_result
 
 typedef struct st_spider_table_mon
 {
+  /* This share has only one link. */
   SPIDER_SHARE               *share;
   uint32                     server_id;
   st_spider_table_mon_list   *parent;
@@ -1466,6 +1467,7 @@ typedef struct st_spider_table_mon_list
   SPIDER_TABLE_MON           *current;
   volatile int               mon_status;
 
+  /* This share has only one link */
   SPIDER_SHARE               *share;
 
   pthread_mutex_t            caller_mutex;
@@ -1479,6 +1481,7 @@ typedef struct st_spider_table_mon_list
 
 typedef struct st_spider_copy_table_conn
 {
+  /* This share has only one link. */
   SPIDER_SHARE               *share;
   int                        link_idx;
   SPIDER_CONN                *conn;
