c0a0eb805e0743654cbf02be70ede1627523babd
Sergey Vojtovich <svoj@mariadb.org>
2024-08-12 13:27:48 +0400

    ALTER TABLE fixes for high-level indexes
    Fixes for ALTER TABLE ... ADD/DROP COLUMN, ALGORITHM=COPY.
    
    Let quick_rm_table() remove high-level indexes along with original table.
    
    Avoid locking uninitialized LOCK_share for INTERNAL_TMP_TABLEs.
    
    Don't enable bulk insert when altering a table containing vector index.
    InnoDB can't handle situation when bulk insert is enabled for one table
    but disabled for another. We can't do bulk insert on vector index as it
    does table updates currently.

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -9838,10 +9838,12 @@ int TABLE::hlindex_open(uint nr)
   DBUG_ASSERT(nr == s->keys);
   if (!hlindex)
   {
-    mysql_mutex_lock(&s->LOCK_share);
+    if (s->tmp_table == NO_TMP_TABLE)
+      mysql_mutex_lock(&s->LOCK_share);
     if (!s->hlindex)
     {
-      mysql_mutex_unlock(&s->LOCK_share);
+      if (s->tmp_table == NO_TMP_TABLE)
+        mysql_mutex_unlock(&s->LOCK_share);
       TABLE_SHARE *share;
       char *path= NULL;
       size_t path_len= s->normalized_path.length + HLINDEX_BUF_LEN;
@@ -9865,20 +9867,26 @@ int TABLE::hlindex_open(uint nr)
         return 1;
       }
 
-      mysql_mutex_lock(&s->LOCK_share);
+      if (s->tmp_table == NO_TMP_TABLE)
+        mysql_mutex_lock(&s->LOCK_share);
       if (!s->hlindex)
       {
         s->hlindex= share;
-        mysql_mutex_unlock(&s->LOCK_share);
+        if (s->tmp_table == NO_TMP_TABLE)
+          mysql_mutex_unlock(&s->LOCK_share);
       }
       else
       {
-        mysql_mutex_unlock(&s->LOCK_share);
+        if (s->tmp_table == NO_TMP_TABLE)
+          mysql_mutex_unlock(&s->LOCK_share);
         free_table_share(share);
       }
     }
     else
-      mysql_mutex_unlock(&s->LOCK_share);
+    {
+      if (s->tmp_table == NO_TMP_TABLE)
+        mysql_mutex_unlock(&s->LOCK_share);
+    }
     TABLE *table= (TABLE*)alloc_root(&mem_root, sizeof(*table));
     if (!table || open_table_from_share(in_use, s->hlindex, &empty_clex_str,
                     db_stat, EXTRA_RECORD, in_use->open_options, table, 0))
