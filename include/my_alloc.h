d6feaa7114db3a953e4ece9c6ab40e272905950f
Sergei Golubchik <serg@mariadb.org>
2024-09-26 11:52:05 +0200

    memroot improvement: fix savepoint support
    savepoint support was added a year ago as get_last_memroot_block()
    and free_all_new_blocks(), but it didn't work and was disabled.
    
    * fix it to work
    * instead of freeing the memory, only mark blocks free - this feature
      is supposed to be used inside a loop (otherwise there is no need
      to free anything, end of statement will do it anyway). And freeing
      blocks inside a loop is a bad idea, as they'll be all malloc-ed
      on the next iteration again. So, don't.
    * fix a bug in mark_blocks_free() - it doesn't change the number of blocks

--- include/my_alloc.h
+++ include/my_alloc.h
@@ -60,6 +60,14 @@ typedef struct st_mem_root
   PSI_memory_key psi_key;
 } MEM_ROOT;
 
+typedef struct st_mem_root_savepoint
+{
+  MEM_ROOT *root;
+  USED_MEM *free;
+  USED_MEM *used;
+  unsigned short first_block_usage;
+} MEM_ROOT_SAVEPOINT;
+
 #ifdef  __cplusplus
 }
 #endif
