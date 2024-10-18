43816876c793cd0c6ce88c700b0abe7f3bf44807
Daniel Black <daniel@mariadb.org>
2024-10-18 14:59:12 +1100

    rename second sql_mode_string_representation to sql_mode_string_representation_for_bit

--- sql/set_var.h
+++ sql/set_var.h
@@ -463,7 +463,7 @@ inline bool IS_SYSVAR_AUTOSIZE(void *ptr)
 bool fix_delay_key_write(sys_var *self, THD *thd, enum_var_type type);
 
 sql_mode_t expand_sql_mode(sql_mode_t sql_mode);
-const char *sql_mode_string_representation(uint bit_number);
+const char *sql_mode_string_representation_for_bit(uint bit_number);
 bool sql_mode_string_representation(THD *thd, sql_mode_t sql_mode,
                                     LEX_CSTRING *ls);
 int default_regex_flags_pcre(THD *thd);
