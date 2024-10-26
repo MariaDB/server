844899b555434afb731507fdec90564edeedd261
Brandon Nesterenko <brandon.nesterenko@mariadb.com>
2024-10-26 13:53:51 -0600

    MDEV-34348: Fix casting related to plugins
    Partial commit of the greater MDEV-34348 scope.
    MDEV-34348: MariaDB is violating clang-16 -Wcast-function-type-strict
    
    Reviewed By:
    ============
    Marko Mäkelä <marko.makela@mariadb.com>

--- sql/handler.cc
+++ sql/handler.cc
@@ -585,9 +585,10 @@ static int hton_drop_table(handlerton *hton, const char *path)
 }
 
 
-int ha_finalize_handlerton(st_plugin_int *plugin)
+int ha_finalize_handlerton(void *plugin_)
 {
   int deinit_status= 0;
+  st_plugin_int *plugin= static_cast<st_plugin_int *>(plugin_);
   handlerton *hton= (handlerton *)plugin->data;
   DBUG_ENTER("ha_finalize_handlerton");
 
@@ -630,8 +631,9 @@ int ha_finalize_handlerton(st_plugin_int *plugin)
 const char *hton_no_exts[]= { 0 };
 static bool ddl_recovery_done= false;
 
-int ha_initialize_handlerton(st_plugin_int *plugin)
+int ha_initialize_handlerton(void *plugin_)
 {
+  st_plugin_int *plugin= static_cast<st_plugin_int *>(plugin_);
   handlerton *hton;
   int ret= 0;
   DBUG_ENTER("ha_initialize_handlerton");
