3e6bea8ca177b46fb29f1ea462fa440c2e9c7890
Daniel Black <daniel@mariadb.org>
2024-11-10 12:43:58 +1100

    Adjust s3 suite.pm to respect env vars
    If the s3 host isn't specified configure
    to be a localhost connection on port 9000
    with http only.

--- mysql-test/suite/s3/suite.pm
+++ mysql-test/suite/s3/suite.pm
@@ -6,62 +6,63 @@ use Socket;
 
 return "Need S3 engine" unless $::mysqld_variables{'s3'} eq "ON" or $ENV{HA_S3_SO};
 
-my $paddr = sockaddr_in(9000, INADDR_ANY);
-my $protocol = getprotobyname("tcp");
-socket(SOCK, PF_INET, SOCK_STREAM, $protocol);
+if (!$ENV{'S3_REGION'})
+{
+  $ENV{'S3_REGION'} = "";
+}
 
-if(connect(SOCK, $paddr))
+if (!$ENV{'S3_ACCESS_KEY'})
+{
+  $ENV{'S3_ACCESS_KEY'} = "minio";
+}
+
+if (!$ENV{'S3_SECRET_KEY'})
+{
+  $ENV{'S3_SECRET_KEY'} = "minioadmin";
+}
+
+if (!$ENV{'S3_PROTOCOL_VERSION'})
+{
+  $ENV{'S3_PROTOCOL_VERSION'} = "Auto";
+}
+
+if (!$ENV{'S3_PORT'})
+{
+  $ENV{'S3_PORT'} = 0;
+}
+
+if (!$ENV{'S3_SSL_NO_VERIFY'})
+{
+  $ENV{'S3_SSL_NO_VERIFY'} = "OFF";
+}
+
+if (!$ENV{'S3_PROVIDER'})
+{
+  $ENV{'S3_PROVIDER'} = "Default";
+}
+
+if (!$ENV{'S3_HOST_NAME'})
 {
   $ENV{'S3_HOST_NAME'} = "127.0.0.1";
-  $ENV{'S3_PORT'} = 9000;
-  $ENV{'S3_BUCKET'} = "storage-engine";
-  $ENV{'S3_ACCESS_KEY'} = "minio";
-  $ENV{'S3_SECRET_KEY'} = "minioadmin";
-  $ENV{'S3_REGION'} = "";
-  $ENV{'S3_PROTOCOL_VERSION'} = "Auto";
-  $ENV{'S3_USE_HTTP'} = "ON";
-}
-else
-{
-  if (!$ENV{'S3_HOST_NAME'})
-  {
-    $ENV{'S3_HOST_NAME'} = "s3.amazonaws.com";
-  }
-
-  if (!$ENV{'S3_BUCKET'})
-  {
-    $ENV{'S3_BUCKET'} = "MariaDB";
-  }
-
-  if (!$ENV{'S3_REGION'})
-  {
-    $ENV{'S3_REGION'} = "";
-  }
-
-  if (!$ENV{'S3_ACCESS_KEY'})
-  {
-    return "Environment variable S3_ACCESS_KEY need to be set";
-  }
-
-  if (!$ENV{'S3_SECRET_KEY'})
-  {
-    return "Environment variable S3_SECRET_KEY need to be set";
-  }
-
-  if (!$ENV{'S3_PROTOCOL_VERSION'})
-  {
-    $ENV{'S3_PROTOCOL_VERSION'} = "Auto";
-  }
 
   if (!$ENV{'S3_PORT'})
   {
-    $ENV{'S3_PORT'} = 0;
+    $ENV{'S3_PORT'} = 9000;
   }
-
   if (!$ENV{'S3_USE_HTTP'})
   {
-    $ENV{'S3_USE_HTTP'} = "OFF";
+    $ENV{'S3_USE_HTTP'} = "ON";
   }
 }
-bless { };
 
+if (!$ENV{'S3_USE_HTTP'})
+{
+  $ENV{'S3_USE_HTTP'} = "OFF";
+}
+
+if (!$ENV{'S3_BUCKET'})
+{
+  $ENV{'S3_BUCKET'} = "MariaDB";
+}
+
+bless { };
