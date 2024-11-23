55d1e5204f0cb980173b57cc6472982d4f468642
Brandon Nesterenko <brandon.nesterenko@mariadb.com>
2024-11-23 09:34:11 -0700

    MDEV-34348: 11.4 hash merge fixes

--- client/mysqldump.cc
+++ client/mysqldump.cc
@@ -844,11 +844,10 @@ static void write_footer(FILE *sql_file)
 } /* write_footer */
 
 
-const uchar *get_table_key(const void *entry, size_t *length,
-                           my_bool not_used __attribute__((unused)))
+const uchar *get_table_key(const void *entry, size_t *length, my_bool)
 {
-  *length= strlen(entry);
-  return entry;
+  *length= strlen(static_cast<const char *>(entry));
+  return static_cast<const uchar *>(entry);
 }
 
 
