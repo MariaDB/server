f3e3219a314937bce8ad71d3d8c38ec2c9195190
Marko Mäkelä <marko.makela@mariadb.com>
2024-12-03 11:08:52 +0200

    MDEV-35049: Improve btr_search_drop_page_hash_index()
    btr_search_drop_page_hash_index(): Replace the Boolean parameter
    with const dict_index_t *not_garbage. If buf_block_t::index points
    to that, there is no need to acquire btr_sea::partition::latch.
    
    The old parameter bool garbage_collect=false is equivalent to the
    parameter not_garbage=nullptr. The parameter garbage_collect=true
    will be replaced either with the actual index that is associated
    with the buffer page, or with a bogus pointer not_garbage=-1 to
    indicate that any lazily entries for a freed index need to be removed.
    
    buf_page_get_low(), buf_page_get_gen(), mtr_t::page_lock(),
    mtr_t::upgrade_buffer_fix(): Do not invoke
    btr_search_drop_page_hash_index(). Our caller will have to do it
    when appropriate.
    
    buf_page_create_low(): Keep invoking btr_search_drop_page_hash_index().
    This is the normal way of lazily dropping the adaptive hash index
    after a DDL operation such as DROP INDEX operation.
    
    btr_block_get(), btr_root_block_get(), btr_root_adjust_on_import(),
    btr_read_autoinc_with_fallback(), btr_cur_instant_init_low(),
    btr_cur_t::search_leaf(), btr_cur_t::pessimistic_search_leaf(),
    btr_pcur_optimistic_latch_leaves(), dict_stats_analyze_index_below_cur():
    Invoke btr_search_drop_page_hash_index(block, index) for pages that
    may be leaf pages. No adaptive hash index may have been created on
    anything else than a B-tree leaf page.
    
    btr_cur_search_to_nth_level(): Do not invoke
    btr_search_drop_page_hash_index(), because we are only accessing
    non-leaf pages and the adaptive hash index may only have been created
    on leaf pages.
    
    btr_page_alloc_for_ibuf() and many other callers of buf_page_get_gen()
    or similar functions do not invoke btr_search_drop_page_hash_index(),
    because the adaptive hash index is never created on such pages.
    If a page in the tablespace was freed as part of a DDL operation and
    reused for something else, then buf_page_create_low() will take care
    of dropping the adaptive hash index before the freed page will be
    modified.
    
    It is notable that while the flst_ functions may access pages that are
    related to allocating B-tree index pages (the BTR_SEG_TOP and BTR_SEG_LEAF
    linked from the index root page), those pages themselves can never be
    stored in the adaptive hash index. Therefore, it is not necessary to
    invoke btr_search_drop_page_hash_index() on them.
    
    Reviewed by: Vladislav Lesin

--- storage/innobase/btr/btr0sea.cc
+++ storage/innobase/btr/btr0sea.cc
@@ -635,7 +635,7 @@ func_exit:
   else if (UNIV_UNLIKELY(block_index != index))
   {
     ut_ad(block_index->id == index->id);
-    btr_search_drop_page_hash_index(block, false);
+    btr_search_drop_page_hash_index(block, nullptr);
   }
   else if (cursor.flag == BTR_CUR_HASH_FAIL)
     btr_search_update_hash_ref(cursor, block, left_bytes_fields);
@@ -1224,15 +1224,15 @@ static constexpr size_t REC_FOLD_IN_STACK= 128;
 /** Drop any adaptive hash index entries that point to an index page.
 @param block        latched block containing index page, or a buffer-unfixed
                     index page or a block in state BUF_BLOCK_REMOVE_HASH
-@param garbage_collect drop ahi only if the index is marked as freed
+@param not_garbage  drop only if the index is set and NOT this
 @param folds        work area for REC_FOLD_IN_STACK rec_fold() values */
 static void btr_search_drop_page_hash_index(buf_block_t *block,
-                                            bool garbage_collect,
+                                            const dict_index_t *not_garbage,
                                             uint32_t *folds) noexcept
 {
 retry:
   dict_index_t *index= block->index;
-  if (!index)
+  if (!index || index == not_garbage)
     return;
 
   ut_d(const auto state= block->page.state());
@@ -1272,8 +1272,12 @@ retry:
       goto retry;
     }
   }
-  else if (garbage_collect)
+  else if (not_garbage != nullptr)
+  {
+    ut_ad(!index || index == not_garbage ||
+          not_garbage == reinterpret_cast<dict_index_t*>(-1));
     goto unlock_and_return;
+  }
 
   assert_block_ahi_valid(block);
 
@@ -1399,10 +1403,10 @@ cleanup:
 }
 
 void btr_search_drop_page_hash_index(buf_block_t *block,
-                                     bool garbage_collect) noexcept
+                                     const dict_index_t *not_garbage) noexcept
 {
   uint32_t folds[REC_FOLD_IN_STACK];
-  btr_search_drop_page_hash_index(block, garbage_collect, folds);
+  btr_search_drop_page_hash_index(block, not_garbage, folds);
 }
 
 void btr_search_drop_page_hash_when_freed(const page_id_t page_id) noexcept
@@ -1416,14 +1420,11 @@ void btr_search_drop_page_hash_when_freed(const page_id_t page_id) noexcept
   if (buf_block_t *block= buf_page_get_gen(page_id, 0, RW_X_LATCH, nullptr,
                                            BUF_PEEK_IF_IN_POOL, &mtr))
   {
-    if (IF_DBUG(dict_index_t *index=,) block->index)
-    {
-      /* In all our callers, the table handle should be open, or we
-      should be in the process of dropping the table (preventing
-      eviction). */
-      DBUG_ASSERT(index->table->get_ref_count() || dict_sys.locked());
-      btr_search_drop_page_hash_index(block, false);
-    }
+    /* In all our callers, the table handle should be open, or we
+    should be in the process of dropping the table (preventing eviction). */
+    ut_d(if (dict_index_t *i= block->index))
+      ut_ad(i->table->get_ref_count() || dict_sys.locked());
+    btr_search_drop_page_hash_index(block, nullptr);
   }
 
   mtr.commit();
@@ -1471,7 +1472,7 @@ static void btr_search_build_page_hash_index(dict_index_t *index,
   struct{uint32_t fold;uint32_t offset;} fr[REC_FOLD_IN_STACK / 2];
 
   if (rebuild)
-    btr_search_drop_page_hash_index(block, false, &fr[0].fold);
+    btr_search_drop_page_hash_index(block, nullptr, &fr[0].fold);
 
   const uint32_t n_bytes_fields{left_bytes_fields & ~buf_block_t::LEFT_SIDE};
 
@@ -1619,7 +1620,7 @@ void btr_search_move_or_delete_hash_entries(buf_block_t *new_block,
   {
     ut_ad(!index || index == new_block_index);
 drop_exit:
-    btr_search_drop_page_hash_index(block, false);
+    btr_search_drop_page_hash_index(block, nullptr);
     return;
   }
 
@@ -1666,7 +1667,7 @@ void btr_search_update_hash_on_delete(btr_cur_t *cursor) noexcept
 
   if (UNIV_UNLIKELY(index != cursor->index()))
   {
-    btr_search_drop_page_hash_index(block, false);
+    btr_search_drop_page_hash_index(block, nullptr);
     return;
   }
 
@@ -1722,7 +1723,7 @@ void btr_search_update_hash_on_insert(btr_cur_t *cursor, bool reorg) noexcept
   {
     ut_ad(index->id == cursor->index()->id);
   drop:
-    btr_search_drop_page_hash_index(block, false);
+    btr_search_drop_page_hash_index(block, nullptr);
     return;
   }
 
