170e0e8ff1dd3c99e2c60c2f5de8f3e7dcba116d
Marko Mäkelä <marko.makela@mariadb.com>
2024-12-03 11:08:10 +0200

    MDEV-35049: Fix bogus BTR_CUR_HASH_FAIL on contention
    btr_search_guess_on_hash(): Only set BTR_CUR_HASH_FAIL on actual mismatch.
    If the page latch cannot be acquired, the hash search might very well
    have succeeded. Do not count that as a failure, that is, do not
    unnecessarily invoke btr_search_update_hash_ref() after a normal search.
    Set cursor->flag=BTR_CUR_HASH_ABORT if the current parameters of the
    adaptive hash index are not suitable for the search and a call to
    btr_cur_t::search_info_update() might help.
    
    btr_cur_t::search_leaf(): Do not invoke search_info_update()
    if btr_search_guess_on_hash() failed due to contention.
    
    btr_cur_t::pessimistic_search_leaf(): Do not invoke search_info_update()
    on the change buffer tree. Preivously, this condition was being checked
    inside search_info_update().

--- storage/innobase/btr/btr0sea.cc
+++ storage/innobase/btr/btr0sea.cc
@@ -505,14 +505,12 @@ skip:
 @retval 0 if the adaptive hash index should not be rebuilt */
 static uint32_t btr_search_info_update_hash(const btr_cur_t &cursor) noexcept
 {
-  ut_ad(cursor.flag != BTR_CUR_HASH);
+  ut_ad(cursor.flag == BTR_CUR_HASH_FAIL ||
+        cursor.flag == BTR_CUR_HASH_ABORT ||
+        cursor.flag == BTR_CUR_BINARY);
 
   dict_index_t *const index= cursor.index();
-
-  if (index->is_ibuf())
-    /* Too many deletes are performed on the change buffer */
-    return 0;
-
+  ut_ad(!index->is_ibuf());
   const uint16_t n_uniq{dict_index_get_n_unique_in_tree(index)};
   dict_index_t::ahi &info= index->search_info;
 
@@ -971,11 +969,20 @@ btr_search_guess_on_hash(
   ut_ad(mtr->is_active());
   ut_ad(index->is_btree() || index->is_ibuf());
   ut_ad(latch_mode == BTR_SEARCH_LEAF || latch_mode == BTR_MODIFY_LEAF);
+  ut_ad(cursor->flag == BTR_CUR_BINARY);
 
-  if ((tuple->info_bits & REC_INFO_MIN_REC_FLAG) ||
-      !index->search_info.last_hash_succ ||
-      !index->search_info.n_hash_potential)
+  if ((tuple->info_bits & REC_INFO_MIN_REC_FLAG))
     return false;
+
+  if (!index->search_info.last_hash_succ ||
+      !index->search_info.n_hash_potential)
+  {
+  ahi_unusable:
+    if (!index->is_ibuf() && !index->table->is_temporary() &&
+        btr_search.enabled)
+      cursor->flag= BTR_CUR_HASH_ABORT;
+    return false;
+  }
 
   ut_ad(index->is_btree());
   ut_ad(!index->table->is_temporary());
@@ -987,7 +994,7 @@ btr_search_guess_on_hash(
     ~buf_block_t::LEFT_SIDE;
 
   if (dtuple_get_n_fields(tuple) < btr_search_get_n_fields(cursor))
-    return false;
+    goto ahi_unusable;
 
   const index_id_t index_id= index->id;
 
@@ -996,7 +1003,6 @@ btr_search_guess_on_hash(
 #endif
   const uint32_t fold= dtuple_fold(tuple, cursor);
   cursor->fold= fold;
-  cursor->flag= BTR_CUR_HASH;
   btr_sea::partition &part= btr_search.get_part(*index);
 
   part.latch.rd_lock(SRW_LOCK_CALL);
@@ -1006,8 +1012,6 @@ btr_search_guess_on_hash(
   ahi_release_and_fail:
     part.latch.rd_unlock();
   fail:
-    cursor->flag= BTR_CUR_HASH_FAIL;
-
 #ifdef UNIV_SEARCH_PERF_STAT
     ++index->search_info.n_hash_fail;
     if (index->search_info.n_hash_succ > 0)
@@ -1022,7 +1026,10 @@ btr_search_guess_on_hash(
     { return node->fold == fold; });
 
   if (!node)
+  {
+    cursor->flag= BTR_CUR_HASH_FAIL;
     goto ahi_release_and_fail;
+  }
 
   const rec_t *rec= node->rec;
   buf_block_t *block= buf_pool.block_from(rec);
@@ -1053,6 +1060,7 @@ btr_search_guess_on_hash(
       block->page.lock.s_unlock();
     else
       block->page.lock.x_unlock();
+    cursor->flag= BTR_CUR_HASH_FAIL;
     goto ahi_release_and_fail;
   }
 
@@ -1063,6 +1071,7 @@ btr_search_guess_on_hash(
   if (index != block_index && index_id == block_index->id)
   {
     ut_a(block_index->freed());
+    cursor->flag= BTR_CUR_HASH_FAIL;
     goto block_and_ahi_release_and_fail;
   }
 
@@ -1095,6 +1104,7 @@ btr_search_guess_on_hash(
     default:
     mismatch:
       mtr->release_last_page();
+      cursor->flag= BTR_CUR_HASH_FAIL;
       goto fail;
     }
 
@@ -1109,6 +1119,7 @@ btr_search_guess_on_hash(
     index->search_info.n_hash_potential= n_hash_potential;
 
   index->search_info.last_hash_succ= true;
+  cursor->flag= BTR_CUR_HASH;
 
 #ifdef UNIV_SEARCH_PERF_STAT
   btr_search_n_succ++;
