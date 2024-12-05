4de74a308a9114a532bf7558b729297441e9cd8d
Rex <rex.johnston@mariadb.com>
2024-12-05 14:14:35 +1200

    MDEV-35565 Server crashes simplifying group by <subquery>
    During optimization, we may call update_depend_map_for_order()
    on a group by list containing an expression with an outer reference.
    The table map for this outer reference will be collected by
    Item_subselect::recalc_used_tables and used to return a depend_map.
    This map will indicate the presence of tables not in this select and
    can cause erroneous results or crashes.  We update the class used to
    collect these bitmaps to check whether the containing select_lex is
    applicable, and if not, set the OUTER_BIT_TABLE_REF bit in the table map.

--- sql/item_subselect.cc
+++ sql/item_subselect.cc
@@ -472,15 +472,14 @@ public:
   st_select_lex *new_parent; /* Select we're in */
   void visit_field(Item_field *item) override
   {
-    //for (TABLE_LIST *tbl= new_parent->leaf_tables; tbl; tbl= tbl->next_local)
-    //{
-    //  if (tbl->table == field->table)
-    //  {
-        used_tables|= item->field->table->map;
-    //    return;
-    //  }
-    //}
-    //used_tables |= OUTER_REF_TABLE_BIT;
+    st_select_lex *cmp= new_parent;
+    while (cmp->merged_into)
+      cmp= cmp->merged_into;
+    if (item->field->table->pos_in_table_list &&
+        (item->field->table->pos_in_table_list->select_lex == cmp))
+      used_tables|= item->field->table->map;
+    else
+      used_tables|= OUTER_REF_TABLE_BIT;
   }
 };
 
