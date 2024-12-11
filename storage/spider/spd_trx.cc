2db2c65a1e3e9b58d83f01bfa7d109cd684b7acd
Yuchen Pei <ycp@mariadb.com>
2024-12-11 11:44:07 +1100

    MDEV-34925 Fix segv on thd in spider recovery.
    Create temporary thd whenever needed in spider xa commit / recovery.
    This prevents crash during recovery.

--- storage/spider/spd_trx.cc
+++ storage/spider/spd_trx.cc
@@ -2598,7 +2598,17 @@ int spider_internal_xa_commit_by_xid(
   SPIDER_Open_tables_backup open_tables_backup;
   bool table_xa_opened = FALSE;
   bool table_xa_member_opened = FALSE;
+  bool created_tmp_thd = FALSE;
   DBUG_ENTER("spider_internal_xa_commit_by_xid");
+  if (!thd)
+  {
+    if (!(thd = spider_create_tmp_thd()))
+    {
+      error_num = HA_ERR_OUT_OF_MEM;
+      goto error;
+    }
+    created_tmp_thd= TRUE;
+  }
   /*
     select
       status
@@ -2798,6 +2808,8 @@ xa_delete:
     goto error;
   spider_close_sys_table(thd, table_xa, &open_tables_backup, TRUE);
   table_xa_opened = FALSE;
+  if (created_tmp_thd)
+    spider_free_tmp_thd(thd);
   DBUG_RETURN(0);
 
 error:
@@ -2806,6 +2818,8 @@ error:
   if (table_xa_member_opened)
     spider_close_sys_table(thd, table_xa_member, &open_tables_backup, TRUE);
 error_open_table:
+  if (created_tmp_thd)
+    spider_free_tmp_thd(thd);
   DBUG_RETURN(error_num);
 }
 
@@ -2830,7 +2844,17 @@ int spider_internal_xa_rollback_by_xid(
   SPIDER_Open_tables_backup open_tables_backup;
   bool table_xa_opened = FALSE;
   bool table_xa_member_opened = FALSE;
+  bool created_tmp_thd= FALSE;
   DBUG_ENTER("spider_internal_xa_rollback_by_xid");
+  if (!thd)
+  {
+    if (!(thd = spider_create_tmp_thd()))
+    {
+      error_num = HA_ERR_OUT_OF_MEM;
+      goto error;
+    }
+    created_tmp_thd= TRUE;
+  }
   /*
     select
       status
@@ -3028,6 +3052,8 @@ xa_delete:
     goto error;
   spider_close_sys_table(thd, table_xa, &open_tables_backup, TRUE);
   table_xa_opened = FALSE;
+  if (created_tmp_thd)
+    spider_free_tmp_thd(thd);
   DBUG_RETURN(0);
 
 error:
@@ -3036,6 +3062,8 @@ error:
   if (table_xa_member_opened)
     spider_close_sys_table(thd, table_xa_member, &open_tables_backup, TRUE);
 error_open_table:
+  if (created_tmp_thd)
+    spider_free_tmp_thd(thd);
   DBUG_RETURN(error_num);
 }
 
@@ -3360,15 +3388,12 @@ int spider_xa_commit_by_xid(
   DBUG_ENTER("spider_xa_commit_by_xid");
 
   if (!(trx = spider_get_trx(thd, TRUE, &error_num)))
-    goto error_get_trx;
+    DBUG_RETURN(error_num);
 
-  if ((error_num = spider_internal_xa_commit_by_xid(thd, trx, xid)))
-    goto error;
+  error_num = spider_internal_xa_commit_by_xid(thd, trx, xid);
 
-  DBUG_RETURN(0);
-
-error:
-error_get_trx:
+  if (!thd)
+    spider_free_trx(trx, FALSE);
   DBUG_RETURN(error_num);
 }
 
@@ -3382,15 +3407,12 @@ int spider_xa_rollback_by_xid(
   DBUG_ENTER("spider_xa_rollback_by_xid");
 
   if (!(trx = spider_get_trx(thd, TRUE, &error_num)))
-    goto error_get_trx;
+    DBUG_RETURN(error_num);
 
-  if ((error_num = spider_internal_xa_rollback_by_xid(thd, trx, xid)))
-    goto error;
+  error_num = spider_internal_xa_rollback_by_xid(thd, trx, xid);
 
-  DBUG_RETURN(0);
-
-error:
-error_get_trx:
+  if (!thd)
+    spider_free_trx(trx, FALSE);
   DBUG_RETURN(error_num);
 }
 
