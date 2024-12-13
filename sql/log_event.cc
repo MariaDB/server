9f5805fa9ce8b6810027ed0ab7908182aa504247
Monty <monty@mariadb.org>
2024-12-13 15:41:59 +0200

    MDEV-35643 Add support for MySQL 8.0 binlog events
    MDEV-29533 Crash when MariaDB is replica of MySQL 8.0
    
    MySQL 8.0 has added the following new events in the MySQL binary log
    
    PARTIAL_UPDATE_ROWS_EVENT
    TRANSACTION_PAYLOAD_EVENT
    HEARTBEAT_LOG_EVENT_V2
    
    - PARTIAL_UPDATE_ROWS_EVENT is used by MySQL to generate update
      statements using JSON_SET, JSON_REPLACE and JSON_REMOVE to make
      update of JSON columns more efficient.  These events can be
      disabled by setting 'binlog-row-value-options=""'
    - TRANSACTION_PAYLOAD_EVENT is used by MySQL to signal that a
      row event is compressed. It an be disably by setting
      'binlog_transaction_compression=0'.
    - HEARTBEAT_LOG_EVENT_V2 is written to the binary log many times
      per seconds. It can be ignored by the server.
    
    What this patch does:
    
    - If PARTIAL_UPDATE_ROWS_EVENT or TRANSACTION_PAYLOAD_EVENT is found,
      the server will stop with an error message of how to disable the
      MySQL server to generate such events.
    - HEARTBEAT_LOG_EVENT_V2 events are ignored.
    - mariadb-binlog will write the name of the new events.
    - mariadb-binlog will stop if PARTIAL_UPDATE_ROWS_EVENT or
      TRANSACTION_PAYLOAD_EVENT is found, unless --force is given.
    - Fixes a crash in mariadb-binlog if a character set unknown to
      MariaDB is found. (MDEV-29533)
    
    From Kristian Nielsen:
    - Add test case for MySQL 8.0 to MariaDB replication and fixed a
      a small typo in post_header_len initialization.
    
    Reviewer: knielsen@mariadb.org

--- sql/log_event.cc
+++ sql/log_event.cc
@@ -694,6 +694,9 @@ const char* Log_event::get_type_str(Log_event_type type)
   case TRANSACTION_CONTEXT_EVENT: return "Transaction_context";
   case VIEW_CHANGE_EVENT: return "View_change";
   case XA_PREPARE_LOG_EVENT: return "XA_prepare";
+  case PARTIAL_UPDATE_ROWS_EVENT: return "MySQL Update_rows_partial";
+  case TRANSACTION_PAYLOAD_EVENT: return "MySQL Transaction_payload";
+  case HEARTBEAT_LOG_EVENT_V2: return "MySQL Heartbeat";
   case QUERY_COMPRESSED_EVENT: return "Query_compressed";
   case WRITE_ROWS_COMPRESSED_EVENT: return "Write_rows_compressed";
   case UPDATE_ROWS_COMPRESSED_EVENT: return "Update_rows_compressed";
@@ -1017,6 +1020,7 @@ Log_event* Log_event::read_log_event(const uchar *buf, uint event_len,
   DBUG_PRINT("info", ("binlog_version: %d", fdle->binlog_version));
   DBUG_DUMP_EVENT_BUF(buf, event_len);
 
+  *error= 0;
   /*
     Check the integrity; This is needed because handle_slave_io() doesn't
     check if packet is of proper length.
@@ -1237,6 +1241,7 @@ Log_event* Log_event::read_log_event(const uchar *buf, uint event_len,
     case ANONYMOUS_GTID_LOG_EVENT:
     case PREVIOUS_GTIDS_LOG_EVENT:
     case TRANSACTION_CONTEXT_EVENT:
+    case HEARTBEAT_LOG_EVENT_V2:                // MySQL 8.0
     case VIEW_CHANGE_EVENT:
       ev= new Ignorable_log_event(buf, fdle,
                                   get_type_str((Log_event_type) event_type));
@@ -1261,6 +1266,21 @@ Log_event* Log_event::read_log_event(const uchar *buf, uint event_len,
     case START_ENCRYPTION_EVENT:
       ev= new Start_encryption_log_event(buf, event_len, fdle);
       break;
+    case TRANSACTION_PAYLOAD_EVENT:             // MySQL 8.0
+      *error=
+        "Found incompatible MySQL 8.0 TRANSACTION_PAYLOAD_EVENT event. "
+        "You can avoid this event by specifying "
+        "'binlog_transaction_compression=0' in the MySQL server";
+      ev= NULL;
+      break;
+    case PARTIAL_UPDATE_ROWS_EVENT:             // MySQL 8.0
+      *error=
+        "Found incompatible MySQL 8.0 PARTIAL_UPDATE_ROWS_EVENT event. "
+        "You can avoid this event by specifying "
+        "'binlog-row-value-options=\"\"' in the MySQL server";
+      ev= NULL;
+      break;
+
     default:
       DBUG_PRINT("error",("Unknown event code: %d",
                           (uchar) buf[EVENT_TYPE_OFFSET]));
@@ -1303,12 +1323,14 @@ exit:
 #ifdef MYSQL_CLIENT
     if (!force_opt) /* then mysqlbinlog dies */
     {
-      *error= "Found invalid event in binary log";
+      if (!*error)
+        *error= "Found invalid event in binary log";
       DBUG_RETURN(0);
     }
     ev= new Unknown_log_event(buf, fdle);
 #else
-    *error= "Found invalid event in binary log";
+    if (!*error)
+      *error= "Found invalid event in binary log";
     DBUG_RETURN(0);
 #endif
   }
@@ -2126,6 +2148,9 @@ Format_description_log_event(uint8 binlog_ver, const char* server_ver)
       post_header_len[TRANSACTION_CONTEXT_EVENT-1]= 0;
       post_header_len[VIEW_CHANGE_EVENT-1]= 0;
       post_header_len[XA_PREPARE_LOG_EVENT-1]= 0;
+      post_header_len[PARTIAL_UPDATE_ROWS_EVENT-1]= ROWS_HEADER_LEN_V2;
+      post_header_len[TRANSACTION_PAYLOAD_EVENT-1]= ROWS_HEADER_LEN_V2;
+      post_header_len[HEARTBEAT_LOG_EVENT_V2-1]= ROWS_HEADER_LEN_V2;
       post_header_len[WRITE_ROWS_EVENT-1]=  ROWS_HEADER_LEN_V2;
       post_header_len[UPDATE_ROWS_EVENT-1]= ROWS_HEADER_LEN_V2;
       post_header_len[DELETE_ROWS_EVENT-1]= ROWS_HEADER_LEN_V2;
