4037ed0164f44c546d0705b9adfd607b8f788222
Yuchen Pei <ycp@mariadb.com>
2025-01-07 17:05:59 +1100

    [debug] MDEV-35783
    not reacheable in main or innodb suites

--- sql/sql_insert.cc
+++ sql/sql_insert.cc
@@ -4738,6 +4738,7 @@ TABLE *select_create::create_table_from_items(THD *thd, List<Item> *items,
       */
       if (open_table(thd, table_list, &ot_ctx))
       {
+        abort();
         quick_rm_table(thd, create_info->db_type, &table_list->db,
                        table_case_name(create_info, &table_list->table_name),
                        0);
