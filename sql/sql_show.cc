38d3b6027b6637d7be0da2060a6aa4c314d2337d
Sergei Golubchik <serg@mariadb.org>
2025-01-26 21:24:53 +0100

    MDEV-35943 ASAN errors in Query_arena::free_items / fill_schema_table_from_frm
    shouldn't try to close the table if open failed.
    
    followup for deb20fb751f6

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -5093,9 +5093,11 @@ static int fill_schema_table_from_frm(THD *thd, MEM_ROOT *mem_root,
     tbl.s= share;
     table_list.table= &tbl;
     table_list.view= (LEX*) share->is_view;
-    res= schema_table->process_table(thd, &table_list, table,
-                                     res, db_name, table_name);
-    closefrm(&tbl);
+    bool res2= schema_table->process_table(thd, &table_list, table, res,
+                                           db_name, table_name);
+    if (res == 0)
+      closefrm(&tbl);
+    res= res2;
   }
 
 
