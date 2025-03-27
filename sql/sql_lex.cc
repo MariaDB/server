ba8feeac6171cfe8e51146acb61d7025c5331815
Sergei Petrunia <sergey@mariadb.com>
2025-03-27 11:25:50 +0200

    Input: fix coding style, add comments (3)

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -8675,20 +8675,27 @@ Item *LEX::create_item_ident(THD *thd,
 }
 
 
+/*
+  @detail
+    This is called when we've parsed Oracle's outer join syntax, that is
+
+      [[db_name.]table_name.]column_name(+)
+
+    Check if the parse context allows it, if yes, mark the Item_field with
+    ORA_JOIN flag and return it.
+*/
+
 bool LEX::mark_item_ident_for_ora_join(THD *thd, Item *item)
 {
+  Item_field *item_field;
   DBUG_ASSERT(item);
 
-  if (thd->variables.sql_mode & MODE_ORACLE)
+  if ((thd->variables.sql_mode & MODE_ORACLE) &&
+      current_select && current_select->parsing_place == IN_WHERE &&
+      (item_field= dynamic_cast<Item_field*>(item)))
   {
-    if (current_select && current_select->parsing_place == IN_WHERE)
-    {
-      if (Item_field *item_field= dynamic_cast<Item_field*>(item))
-      {
-        item_field->with_flags|= item_with_t::ORA_JOIN;
-        return false;
-      }
-    }
+    item_field->with_flags|= item_with_t::ORA_JOIN;
+    return false;
   }
 
   thd->parse_error(ER_SYNTAX_ERROR);
--- sql/sql_oracle_outer_join.cc
+++ sql/sql_oracle_outer_join.cc
@@ -456,7 +456,7 @@ bool setup_oracle_join(THD *thd, COND **conds,
 {
   DBUG_ENTER("setup_oracle_join");
   uint n_tables= select_table_list.elements;
-  Item_cond_and *and_item= NULL;
+
   if (!(*conds)->with_ora_join() || n_tables == 0)
     DBUG_RETURN(FALSE); // no oracle joins
 
@@ -486,7 +486,7 @@ bool setup_oracle_join(THD *thd, COND **conds,
   DBUG_ASSERT(i == n_tables);
   if (is_cond_and(*conds))
   {
-    and_item= (Item_cond_and *)(*conds);
+    Item_cond_and *and_item= (Item_cond_and *)(*conds);
     Item *item;
     List_iterator<Item> it(*and_item->argument_list());
     while ((item= it++))
