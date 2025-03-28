f6cf1bee1aee548fba335546f905af3bcb920177
slawomir-pryczek <pslawek83@o2.pl>
2025-03-28 15:53:44 +0100

    Fix for no partitions mode

--- client/mysqldump.cc
+++ client/mysqldump.cc
@@ -6311,7 +6311,7 @@ static int dump_selected_tables(char *db, char **table_names, int tables)
 
       // move partition name, if we're skipping a table
       if (partition_names != NULL) {
-        *partition_names_pos = partition_names[table_num];
+        *partition_names_pos= partition_names[table_num];
         partition_names_pos++;
       }
     }
@@ -6396,7 +6396,7 @@ static int dump_selected_tables(char *db, char **table_names, int tables)
   for (pos= dump_tables; pos < end; pos++)
   {
     char *partition = NULL;
-    if (*partition_names_pos != NULL) {
+    if (partition_names_pos != NULL && *partition_names_pos != NULL) {
       partition = (*partition_names_pos)->str;
       partition_names_pos++;
     }
