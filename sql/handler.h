c2e8fd1f1abeb50500cbae1f3eaf91074116400a
Kristian Nielsen <knielsen@knielsen-hq.org>
2025-04-07 11:49:55 +0200

    MDEV-34705: Binlog-in-engine: Work-around compiler warning
    Signed-off-by: Kristian Nielsen <knielsen@knielsen-hq.org>

--- sql/handler.h
+++ sql/handler.h
@@ -5980,6 +5980,8 @@ struct handler_binlog_purge_info {
     is given.
   */
   char nonpurge_filename[FN_REFLEN];
+  /* Default constructor to silence compiler warnings -Wuninitialized. */
+  handler_binlog_purge_info()= default;
 };
 
 #endif /* HANDLER_INCLUDED */
