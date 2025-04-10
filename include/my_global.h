26671bf3de6e34c01a878889a6d8cd875c8b74a7
Daniel Black <daniel@mariadb.org>
2025-04-10 15:15:51 +1000

    MDEV-36542 Remove UNINIT_VAR(x)=x under UBSAN
    Clang processes the "int x=x" code from UNINIT_VAR
    literally resulting in an uninitialized read and write.
    This is something we want to avoid. Gcc does the same
    without emitting warnings.
    
    As the UNINIT_VAR was around avoiding compiler false detection,
    and clang doesn't false detect, is default action is a
    noop.
    
    Static analysers (examined Infer and SonarQube) are
    clang based and have the same detection.
    
    Using a __clang__ instead of WITH_UBSAN would acheived
    a better result, however reviewer wanted to keep WITH_UBSAN
    only.
    
    LINT_INIT_STRUCT is no longer required, even a gcc-4.8.5
    doesn't warn with this construct removed which matches
    the comment that it was fixed in gcc ~4.7.
    
    mysql.cc - all paths in com_go populate buff before use.
    
    json: Item_func_json_merge::val_str
      LINT_INIT(js2) unneeded as usage in the previous statements
      it is explicitly initialized to NULL.
    
    Item_func_json_contains_path::val_bool n_found is guarded
    by an uninitialized read by mode_one and from
    gcc-13.3.0 in Ubuntu 24.04 this is detected. As the only
    remaining use of LINIT_INIT this usage has been applied
    with the expanded macro with the unused _lint define removed.
    
    The LINT_INIT macro is removed.
    
    _ma_ck_delete - org_key only valid under share->now_transactional
    likewise with _ma_ck_write_btree_with_log
    
    connect engine never used anything that FORCE_INIT_OF_VARS
    would change.
    
    Reviewer: Monty

--- include/my_global.h
+++ include/my_global.h
@@ -448,20 +448,17 @@ extern "C" int madvise(void *addr, size_t len, int behav);
 /*
    Suppress uninitialized variable warning without generating code.
 */
-#if defined(__GNUC__)
-/* GCC specific self-initialization which inhibits the warning. */
+#if defined(__GNUC__) && !defined(WITH_UBSAN)
+/*
+  GCC specific self-initialization which inhibits the warning.
+  clang and static analysis will complain loudly about this
+  so compile those under WITH_UBSAN.
+*/
 #define UNINIT_VAR(x) x= x
-#elif defined(_lint) || defined(FORCE_INIT_OF_VARS)
+#elif defined(FORCE_INIT_OF_VARS)
 #define UNINIT_VAR(x) x= 0
 #else
 #define UNINIT_VAR(x) x
-#endif
-
-/* This is only to be used when resetting variables in a class constructor */
-#if defined(_lint) || defined(FORCE_INIT_OF_VARS)
-#define LINT_INIT(x) x= 0
-#else
-#define LINT_INIT(x)
 #endif
 
 #if !defined(HAVE_UINT)
