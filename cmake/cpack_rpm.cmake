2fa50befbd0ddc8046c9de78983670c27ecc7586
Sergei Golubchik <serg@mariadb.org>
2025-04-23 10:50:39 +0200

    rpm: restore MariaDB-test dependency on MariaDB-common
    after 41b036bff0bc

--- cmake/cpack_rpm.cmake
+++ cmake/cpack_rpm.cmake
@@ -223,7 +223,7 @@ IF(WITH_WSREP)
     "galera-4" "rsync" "grep" "gawk" "iproute"
     "coreutils" "findutils" "tar")
   SETA(CPACK_RPM_server_PACKAGE_RECOMMENDS "lsof" "socat")
-  SETA(CPACK_RPM_test_PACKAGE_REQUIRES "socat")
+  SETA(CPACK_RPM_test_PACKAGE_REQUIRES "${CPACK_RPM_PACKAGE_REQUIRES}" "socat")
 ENDIF()
 
 SET(CPACK_RPM_server_PRE_INSTALL_SCRIPT_FILE ${CMAKE_SOURCE_DIR}/support-files/rpm/server-prein.sh)
