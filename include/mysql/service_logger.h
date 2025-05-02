bbe271ad10427f0db592859e5ca829a21492bb1d
Alexey Botchkov <holyfoot@askmonty.org>
2025-05-02 18:04:49 +0400

    MDEV-34680 Asynchronous and Buffered Logging for Audit Plugin.
    Buffering with the IO_CACHE added to the file logger.

--- include/mysql/service_logger.h
+++ include/mysql/service_logger.h
@@ -64,41 +64,48 @@ extern struct logger_service_st {
   void (*logger_init_mutexes)();
   LOGGER_HANDLE* (*open)(const char *path,
                          unsigned long long size_limit,
-                         unsigned int rotations);
+                         unsigned int rotations, size_t buffer_size);
   int (*close)(LOGGER_HANDLE *log);
   int (*vprintf)(LOGGER_HANDLE *log, const char *fmt, va_list argptr)
     ATTRIBUTE_FORMAT_FPTR(printf, 2, 0);
   int (*printf)(LOGGER_HANDLE *log, const char *fmt, ...)
     ATTRIBUTE_FORMAT_FPTR(printf, 2, 3);
-  int (*write)(LOGGER_HANDLE *log, const char *buffer, size_t size);
+  int (*write)(LOGGER_HANDLE *log, const void *data, size_t size);
   int (*rotate)(LOGGER_HANDLE *log);
+  int (*sync)(LOGGER_HANDLE *log);
+  int (*resize_buffer)(LOGGER_HANDLE *log, size_t new_buffer_size);
 } *logger_service;
 
 #ifdef MYSQL_DYNAMIC_PLUGIN
 
 #define logger_init_mutexes logger_service->logger_init_mutexes
-#define logger_open(path, size_limit, rotations) \
-  (logger_service->open(path, size_limit, rotations))
+#define logger_open(path, size_limit, rotations, buffer_size) \
+  (logger_service->open(path, size_limit, rotations, buffer_size))
 #define logger_close(log) (logger_service->close(log))
 #define logger_rotate(log) (logger_service->rotate(log))
 #define logger_vprintf(log, fmt, argptr) (logger_service->\
     vprintf(log, fmt, argptr))
 #define logger_printf (*logger_service->printf)
-#define logger_write(log, buffer, size) \
-  (logger_service->write(log, buffer, size))
+#define logger_write(log, data, size) \
+  (logger_service->write(log, data, size))
+#define logger_sync(log) (logger_service->sync(log))
+#define logger_resize_buffer(log, new_buffer_size) \
+  (logger_service->resize_buffer(log, new_buffer_size))
 #else
 
   void logger_init_mutexes();
   LOGGER_HANDLE *logger_open(const char *path,
                              unsigned long long size_limit,
-                             unsigned int rotations);
+                             unsigned int rotations, size_t buffer_size);
   int logger_close(LOGGER_HANDLE *log);
   int logger_vprintf(LOGGER_HANDLE *log, const char *fmt, va_list argptr)
     ATTRIBUTE_FORMAT(printf, 2, 0);
   int logger_printf(LOGGER_HANDLE *log, const char *fmt, ...)
     ATTRIBUTE_FORMAT(printf, 2, 3);
-  int logger_write(LOGGER_HANDLE *log, const char *buffer, size_t size);
+  int logger_write(LOGGER_HANDLE *log, const void *data, size_t size);
   int logger_rotate(LOGGER_HANDLE *log); 
+  int logger_sync(LOGGER_HANDLE *log); 
+  int logger_resize_buffer(LOGGER_HANDLE *log, size_t new_buffer_size); 
 #endif
 
 
