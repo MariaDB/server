834333c92390a017f55965df0239a37e9276622f
Sergei Golubchik <serg@mariadb.org>
2025-05-17 22:06:14 +0200

    openssl 1 compat

--- plugin/auth_mysql_sha2/openssl1-compat.c
+++ plugin/auth_mysql_sha2/openssl1-compat.c
@@ -15,10 +15,25 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1335  USA */
 
 #include "mysql_sha2.h"
+#include <openssl/rsa.h>
 
 #if OPENSSL_VERSION_NUMBER < 0x30000000L
 EVP_PKEY *EVP_RSA_gen(unsigned int bits)
 {
-  return NULL;
+  EVP_PKEY_CTX *ctx;
+  EVP_PKEY *pkey = NULL;
+  ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+  if (!ctx)
+    return NULL;
+
+  if (EVP_PKEY_keygen_init(ctx) <= 0 ||
+      EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, bits) <= 0)
+    goto err;
+
+  EVP_PKEY_keygen(ctx, &pkey);
+
+err:
+  EVP_PKEY_CTX_free(ctx);
+  return pkey;
 }
 #endif
