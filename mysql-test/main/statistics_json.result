MariaDB/server

commit f699010c0fc570786f6fe271f4dc3b2c84f8521d

json_unescape: don't fill unconverted characters with ?
Return an error, and handle let the caller handler the SQL error.

strings/json_lib.c
@@ -1656,15 +1656,7 @@ int json_unescape(CHARSET_INFO *json_cs,
     }
     if (c_len == MY_CS_ILUNI)
     {
-      /*
-        Result charset doesn't support the json's character.
-        Let's replace it with the '?' symbol.
-      */
-      if ((c_len= my_ci_wc_mb(res_cs, '?', res, res_end)) > 0)
-      {
-        res+= c_len;
-        continue;
-      }
+      return -1;
     }
     /* Result buffer is too small. */
     return -1;

commit 5de720ed9003a3afd6627858524bc84474e2a711

MDEV-36765: JSON Histogram cannot handle >1 byte characters
When json_escape changed[1] to return a -1 in the case of
a character that didn't match the character set, json_unescape_to_string
assumed the -1 meant out of memory and just looped with more
memory.

Problem 1 - json_escape needs to return a different code
so that the different between charset incompatibility and out
of memory needs to occur. This enables json_escape_to_string
to handle the it correctly (ignore and fail seems the best
option).

Problem 2 - JSON histograms need to support character with
where the column json min/maximum value aren't a character
set represented by a single byte.

Problem 2 was previously hidden as ? was a result of the conversion.

As JSON histograms can relate to columns when have an explict
character set, use that and fall back to bin which was the
previous default for non-string columns.

Replaces -1/-2 constants and handling with JSON_ERROR_ILLEGAL_SYMBOL /
JSON_ERROR_OUT_OF_SPACE defines.

[1] regression from: f699010c0fc570786f6fe271f4dc3b2c84f8521d

sql/opt_histogram_json.cc
@@ -52,10 +52,17 @@ static bool json_unescape_to_string(const char *val, int val_len, String* out)
       out->length(res);
       return false; // Ok
     }
+    if (res == JSON_ERROR_ILLEGAL_SYMBOL)
+      return true; // Invalid character
 
     // We get here if the unescaped string didn't fit into memory.
-    if (out->alloc(out->alloced_length()*2))
-      return true;
+    if (res == JSON_ERROR_OUT_OF_SPACE)
+    {
+      if (out->alloc(out->alloced_length()*2))
+        return true;
+    }
+    else
+      return true; // unknown error
   }
 }
 
@@ -493,7 +500,7 @@ bool read_bucket_endpoint(json_engine_t *je, Field *field, String *out,
   const char* je_value= (const char*)je->value;
   if (je->value_type == JSON_VALUE_STRING && je->value_escaped)
   {
-    StringBuffer<128> unescape_buf;
+    StringBuffer<128> unescape_buf(field->charset() ? field->charset() : &my_charset_bin);
     if (json_unescape_to_string(je_value, je->value_len, &unescape_buf))
     {
       *err= "Un-escape error";
@@ -600,10 +607,14 @@ int Histogram_json_hb::parse_bucket(json_engine_t *je, Field *field,
   bool have_start= false;
   bool have_size= false;
   bool have_ndv= false;
+  CHARSET_INFO *cs;
+
+  if (!(cs= field->charset()))
+    cs= &my_charset_bin;
 
   double size_d;
   longlong ndv_ll= 0;
-  StringBuffer<128> value_buf;
+  StringBuffer<128> value_buf(cs);
   int rc;
 
   while (!(rc= json_scan_next(je)) && je->state != JST_OBJ_END)

strings/json_lib.c
@@ -1656,13 +1656,13 @@ int json_unescape(CHARSET_INFO *json_cs,
     }
     if (c_len == MY_CS_ILUNI)
     {
-      return -1;
+      return JSON_ERROR_ILLEGAL_SYMBOL;
     }
     /* Result buffer is too small. */
-    return -1;
+    return JSON_ERROR_OUT_OF_SPACE;
   }
 
-  return s.error==JE_EOS ? (int)(res - res_b) : -1;
+  return s.error==JE_EOS ? (int)(res - res_b) : JSON_ERROR_OUT_OF_SPACE;
 }
 
 

mysql-test/main/statistics_json.result
@@ -7796,3 +7796,21 @@ EXPLAIN
   }
 }
 drop table t1;
+#
+# MDEV-36765 JSON Histogram cannot handle >1 byte characters
+#
+CREATE TABLE t1 (f varchar(50)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO t1 VALUES (UNHEX('E983A8E996800AE983A8E99680'));
+SET STATEMENT histogram_type=JSON_HB FOR ANALYZE TABLE t1 PERSISTENT FOR ALL;
+Table	Op	Msg_type	Msg_text
+test.t1	analyze	status	Engine-independent statistics collected
+test.t1	analyze	status	OK
+SELECT min_value, max_value, hist_type
+FROM mysql.column_stats WHERE db_name = 'test' AND table_name = 't1';
+min_value	max_value	hist_type
+部門
+部門	部門
+部門	JSON_HB
+DROP TABLE t1;
+DELETE FROM mysql.column_stats;
+# End of 10.11 tests

mysql-test/main/statistics_json.test
@@ -481,3 +481,17 @@ explain format=json select * from t1 where a > 'y';
 
 drop table t1;
 
+--echo #
+--echo # MDEV-36765 JSON Histogram cannot handle >1 byte characters
+--echo #
+
+CREATE TABLE t1 (f varchar(50)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO t1 VALUES (UNHEX('E983A8E996800AE983A8E99680'));
+SET STATEMENT histogram_type=JSON_HB FOR ANALYZE TABLE t1 PERSISTENT FOR ALL;
+SELECT min_value, max_value, hist_type
+FROM mysql.column_stats WHERE db_name = 'test' AND table_name = 't1';
+
+DROP TABLE t1;
+DELETE FROM mysql.column_stats;
+
+--echo # End of 10.11 tests