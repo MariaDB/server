2a3dec10f47623543cd149cfee2ad1fe911ae6a8
Nikita Malyavin <nikitamalyavin@gmail.com>
2025-06-12 22:12:26 +0200

    MDEV-35915 Global temporary tables: separate the tmp tables namespace
    Global temporary tables will exist solely in global tables namespace.
    This should allow creating and using local temporary tables while open
    global temporary tables' descriptors with the same name exist.

--- sql/sql_alter.h
+++ sql/sql_alter.h
@@ -427,7 +427,7 @@ public:
   Lex_ident_db new_db;
   Lex_ident_table new_name;
   LEX_CSTRING  new_alias;
-  LEX_CSTRING  tmp_name;
+  Lex_ident_table tmp_name;
   LEX_CSTRING  tmp_storage_engine_name;
   LEX_CUSTRING tmp_id, id;
   char         tmp_buff[80];
