b576b8fe874b8d95ced228fe3ec019ec87953c23
bsrikanth-mariadb <srikanth.bondalapati@mariadb.com>
2025-06-22 07:27:49 -0400

    MDEV-37207: dumping tables for multi delete query doesn't work always
    when doing multiple DELETE on tables:
    -> if there was data to delete, then no ddls were dumped to the trace.
    -> However, when there was no data that is being deleted from the tables,
       then ddls of the tables were getting dumped to the trace.
    
    The problem is that store_tables_context_in_trace() is not getting
    invoked, when there was data to delete from the tables. Although, there
    was no error when executing the delete query, the result of basic cleanup was
    non-zero and failure of the the result check was prohibiting from invoking
    store_tables_context_in_trace() method.
    
    Removed this additional result check for invoking
    store_tables_context_in_trace() from mysql_execute_command().

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -5882,8 +5882,8 @@ wsrep_error_label:
   res= true;
 
 finish:
-  if (!thd->is_error() && !res)
-    res= store_tables_context_in_trace(thd);
+  if (!thd->is_error())
+    res = store_tables_context_in_trace(thd) || res;
 
   thd->reset_query_timer();
   DBUG_ASSERT(!thd->in_active_multi_stmt_transaction() ||
