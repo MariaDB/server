e5ad42e26ccf11a7ac2b56d25246be4cd2c53358
apostolis1 <apostolis.stamatis1@gmail.com>
2025-06-23 18:50:24 +0300

    Modifies the HTTP mockup server to use different endpoints for different responses. This means that a single HTTP mockup server can be used for testing, minimizing configurations and mariadb server restarts.

--- plugin/gen_embedding/mysql-test/gen_embedding/suite.pm
+++ plugin/gen_embedding/mysql-test/gen_embedding/suite.pm
@@ -8,29 +8,27 @@ use mtr_report;
 
 sub openai_start {
     my ($openai, $test) = @_; # My::Config::Group, My::Test
-
+    if ($openai->{proc}) {
+      # Already started
+      return ;
+    }
     my $python_path="python3"; # TODO: Maybe use a config option, could also be "python" or a specific path
-    my $file_path=$openai->value('filename');
-    my $http_code=$openai->value('http_code');
+    my $file_path=$openai->value('success_filename');
     my $python_server_path="$ENV{MTR_SUITE_DIR}/minimal_socket_server.py";
-    # PORT = sys.argv[1]
-    # FILENAME = sys.argv[2]
-    # RETURN_CODE = sys.argv[3]
     my $args;
     &::mtr_init_args(\$args);
     &::mtr_add_arg($args, $python_server_path);
     &::mtr_add_arg($args, "$ENV{OPENAI_PORT}");
     &::mtr_add_arg($args, $file_path);
-    &::mtr_add_arg($args, $http_code);
+    my $http_server_log= "openai.log";
 
-    my $flask_log= "openai.log";
     $openai->{'proc'}= My::SafeProcess->new
     (
      name         => 'myname-openai',
      path         => $python_path,
      args         => \$args,
-     output       => $flask_log,
-     error        => $flask_log,
+     output       => $http_server_log,
+     error        => $http_server_log,
      append       => 1,
      nocore       => 1,
     );
