9ab9ee2024ece7ce3e4e6495c5ff71d6cfc80db5
Sergei Golubchik <serg@mariadb.org>
2025-07-05 00:21:53 +0200

    MDEV-25415 CASE function handles NULL inconsistently
    SQL Standard specifies that a <simple case> should return the <result>
    from the left-most <simple when clause> where the comparison evaluates
    to TRUE.
    
    That is, it should not stop comparing on NULLs

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -3096,7 +3096,8 @@ Item *Item_func_case_simple::find_item()
 {
   /* Compare every WHEN argument with it and return the first match */
   uint idx;
-  if (!Predicant_to_list_comparator::cmp(this, &idx, NULL))
+  bool found_unknown_values;
+  if (!Predicant_to_list_comparator::cmp(this, &idx, &found_unknown_values))
     return args[idx + when_count()];
   Item **pos= Item_func_case_simple::else_expr_addr();
   return pos ? pos[0] : 0;
