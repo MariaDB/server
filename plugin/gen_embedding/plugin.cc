1804e622cc97e26e1127d2cd7ece8d30cc661b58
Apostolis Stamatis <apostolis.stamatis1@gmail.com>
2025-08-01 21:26:56 +0300

    Fixed copyright date

--- plugin/gen_embedding/plugin.cc
+++ plugin/gen_embedding/plugin.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2019, MariaDB Corporation
+   Copyright (c) 2025, MariaDB Corporation
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
