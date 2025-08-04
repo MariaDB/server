81c3828b6a60d9deb511501b0c8999f71a89a1b9
Vladislav Vaintroub <vvaintroub@gmail.com>
2025-08-05 01:29:39 +0200

    increase MY_AES_CTX_SIZE for WolfSSL / ARM64 , when assembly is used

--- include/mysql/service_my_crypt.h
+++ include/mysql/service_my_crypt.h
@@ -45,7 +45,7 @@ extern "C" {
 /* The max key length of all supported algorithms */
 #define MY_AES_MAX_KEY_LENGTH 32
 
-#define MY_AES_CTX_SIZE 1040
+#define MY_AES_CTX_SIZE 1400
 
 enum my_aes_mode {
     MY_AES_ECB, MY_AES_CBC
