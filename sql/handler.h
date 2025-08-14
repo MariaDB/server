c3ce535530844fa3f6991e1d9748729a98623301
Kristian Nielsen <knielsen@knielsen-hq.org>
2025-08-14 16:35:51 +0200

    Binlog-in-engine: Support for new binlog format in mysqlbinlog
    Signed-off-by: Kristian Nielsen <knielsen@knielsen-hq.org>

--- sql/handler.h
+++ sql/handler.h
@@ -38,6 +38,7 @@
 #include "vers_string.h"
 #include "ha_handler_stats.h"
 #include "optimizer_costs.h"
+#include "handler_binlog_reader.h"
 
 #include "sql_analyze_stmt.h" // for Exec_time_tracker 
 
@@ -5936,77 +5937,6 @@ struct handler_binlog_event_group_info {
     at the end of the IO_CACHE containing the data to be binlogged.
   */
   my_off_t gtid_offset;
-};
-
-
-/*
-  Class for reading a binlog implemented in an engine.
-*/
-class handler_binlog_reader {
-public:
-  /*
-    Approximate current position (from which next call to read_binlog_data()
-    will need to read). Updated by the engine. Used to know which binlog files
-    the active dump threads are currently reading from, to avoid purging
-    actively used binlogs.
-  */
-  uint64_t cur_file_no;
-  uint64_t cur_file_pos;
-
-private:
-  /* Position and length of any remaining data in buf[]. */
-  uint32_t buf_data_pos;
-  uint32_t buf_data_remain;
-  /* Buffer used when reading data out via read_binlog_data(). */
-  static constexpr size_t BUF_SIZE= 32768;
-  uchar *buf;
-
-public:
-  handler_binlog_reader()
-    : cur_file_no(~(uint64_t)0), cur_file_pos(~(uint64_t)0),
-      buf_data_pos(0), buf_data_remain(0)
-  {
-    buf= (uchar *)my_malloc(PSI_INSTRUMENT_ME, BUF_SIZE, MYF(0));
-  }
-  virtual ~handler_binlog_reader() {
-    my_free(buf);
-  };
-  virtual int read_binlog_data(uchar *buf, uint32_t len) = 0;
-  virtual bool data_available()= 0;
-  /*
-    Wait for data to be available to read, for kill, or for timeout.
-    Returns true in case of timeout reached, false otherwise.
-    Caller should check for kill before calling again (to avoid busy-loop).
-  */
-  virtual bool wait_available(THD *thd, const struct timespec *abstime) = 0;
-  /*
-    This initializes the current read position to the point of the slave GTID
-    position passed in as POS. It is permissible to start at a position a bit
-    earlier in the binlog, only cost is the extra read cost of reading not
-    needed event data.
-
-    If position is found, must return the corresponding binlog state in the
-    STATE output parameter and initialize cur_file_no and cur_file_pos members.
-
-    Returns:
-     -1  Error
-      0  The requested GTID position not found, needed binlogs have been purged
-      1  Ok, position found and returned.
-  */
-  virtual int init_gtid_pos(slave_connection_state *pos,
-                            rpl_binlog_state_base *state) = 0;
-  /*
-    Initialize to a legacy-type position (filename, offset). This mostly to
-    support legacy SHOW BINLOG EVENTS.
-  */
-  virtual int init_legacy_pos(const char *filename, ulonglong offset) = 0;
-  /*
-    Can be called after init_gtid_pos() or init_legacy_pos() to make the reader
-    stop (return EOF) at the end of the binlog file. Used for SHOW BINLOG
-    EVENTS, which has a file-based interface based on legacy file name.
-  */
-  virtual void enable_single_file() = 0;
-  int read_log_event(String *packet, uint32_t ev_offset, size_t max_allowed);
 };
 
 
