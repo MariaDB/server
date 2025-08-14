24ae0d5cf46dfdebf83d5e2ba349c522f6783f2a
Nikita Malyavin <nikitamalyavin@gmail.com>
2025-08-15 00:44:13 +0200

    MDEV-37394 SIGSEGV in handler::ha_external_lock on CREATE GTT ... INNODB SELECT
    The local table handler of GTT wasn't properly unlocked before drop on
    transaction commit.
    
    It should normally happen in THD::free_temporary_table:
    mysql_lock_remove(this, lock, table)
    
    But THD::lock is not the one containing this table's lock - it's m_plock
    of select_create.
    
    Only transactional tmp tables are locked, so it wasn't a problem for
    myisam.
    
    Solution:
    Unlock the tables before implicit commit.

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -6623,6 +6623,7 @@ private:
                                   List<Item> *items,
                                   MYSQL_LOCK **lock);
   int postlock(THD *thd, TABLE **tables);
+  void unlock_tables();
 };
 
 #include <myisam.h>
