25077539d79cf6a587c7a14c24e589e9ed38607f
Monty <monty@mariadb.org>
2025-08-31 16:50:35 +0300

    Moved things in TABLE to have all 'bool' variables in one place
    This reduced size of TABLE from 1024 bytes to 992

--- sql/table.h
+++ sql/table.h
@@ -1284,7 +1284,6 @@ private:
 
 public:
 
-  uint32 instance; /** Table cache instance this TABLE is belonging to */
   THD	*in_use;                        /* Which thread uses this */
 
   uchar *record[3];			/* Pointer to records */
@@ -1438,7 +1437,13 @@ public:
     NULLable (and have NULL values when null_row=true)
   */
   uint maybe_null;
-  int		current_lock;           /* Type of lock on table */
+  uint max_keys; /* Size of allocated key_info array. */
+  int current_lock;           /* Type of lock on table */
+  /* Number of cost info elements for possible range filters */
+  uint range_rowid_filter_cost_info_elems;
+  uint32 instance; /** Table cache instance this TABLE is belonging to */
+
+  /* variables of type bool */
   bool copy_blobs;			/* copy_blobs when storing */
   /*
     Set if next_number_field is in the UPDATE fields of INSERT ... ON DUPLICATE
@@ -1525,7 +1530,6 @@ public:
   */
   bool alias_name_used;              /* true if table_name is alias */
   bool get_fields_in_item_tree;      /* Signal to fix_field */
-  List<Virtual_column_info> vcol_refix_list;
 private:
   bool m_needs_reopen;
   bool created;    /* For tmp tables. TRUE <=> tmp table was actually created.*/
@@ -1536,7 +1540,15 @@ public:
 #endif
   /* Temporary value used by binlog_write_table_maps(). Does not need init */
   bool restore_row_logging;
+  bool stats_is_read;     /* Persistent statistics is read for the table */
+  bool histograms_are_read;
+#ifdef WITH_PARTITION_STORAGE_ENGINE
+  /* If true, all partitions have been pruned away */
+  bool all_partitions_pruned_away;
+#endif
+  bool vers_write;                      // For versioning
 
+  List<Virtual_column_info> vcol_refix_list;
   REGINFO reginfo;			/* field connections */
   MEM_ROOT mem_root;
   /* this is for temporary tables created inside Item_func_group_concat */
@@ -1555,12 +1567,7 @@ public:
   Query_arena *expr_arena;
 #ifdef WITH_PARTITION_STORAGE_ENGINE
   partition_info *part_info;            /* Partition related information */
-  /* If true, all partitions have been pruned away */
-  bool all_partitions_pruned_away;
 #endif
-  uint max_keys; /* Size of allocated key_info array. */
-  bool stats_is_read;     /* Persistent statistics is read for the table */
-  bool histograms_are_read;
   MDL_ticket *mdl_ticket;
 
   /*
@@ -1772,8 +1779,6 @@ public:
 
   key_map with_impossible_ranges;
 
-  /* Number of cost info elements for possible range filters */
-  uint range_rowid_filter_cost_info_elems;
   /* Pointer to the array of cost info elements for range filters */
   Range_rowid_filter_cost_info *range_rowid_filter_cost_info;
   /* The array of pointers to cost info elements for range filters */
@@ -1790,8 +1795,6 @@ public:
   /**
     System Versioning support
    */
-  bool vers_write;
-
   bool versioned() const
   {
     return s->versioned;
