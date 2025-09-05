85bfd94a8d3238847b666fde2966e1eeda0c4dfa
Nikita Malyavin <nikitamalyavin@gmail.com>
2025-09-05 02:40:36 +0200

    MDEV-37382 crash on CREATE OR REPLACE GTT under LOCK TABLES
    The rules of opening become uneasy in CREATE TABLE for global
    temporary tables:
    * CREATE OR REPLACE t means that global handle of t should be opened
    for the first time;
    * CREATE ... SELECT t means that local handle of t should be opened;
    * LOCK TABLES adds an extra complexity layer:
      1. It re-opens the tables a one extra time, not in a single operation
      (so we couldn't rely on the order of table_list),
      2. MDL on the SELECT tables is not necessarily MDL_READ, and so
      table lock is.
      3. CREATE OR REPLACE needs to open a global handle to substitute old
      table in locked_tables_list
      4. CREATE...SELECT additionally needs to open a local handle to
      insert into a correct sink.
    
    This patch presents the following solution:
    * Provide a way to distinguish tables to be opened: extend
    TABLE_LIST::enum_open_strategy with a new item,
    OPEN_FOR_LOCKED_TABLES_LIST.
    * Every locked_table_list item, i.e. TABLE_LIST, will have
    open_strategy=OPEN_FOR_LOCKED_TABLES_LIST.
    * GTT tables with OPEN_FOR_LOCKED_TABLES_LIST are always opened as
    global handles. The same applies to OPEN_STUB -- it represents the old
    table in OR REPLACE case, opened for the first time (comes from
    parser). Importantly, OPEN_STUB tables don't reach open_table(), nor
    open_global_temporary_table, unless it's in locked tables mode.
    
    * In CREATE...SELECT, open both global and local handles:
    the former is for replacing the old table in locked_tables_list and
    the latter is for inserting the data.
    
    * Add check_global_temporary_tables_for_create to report if a table to
    replace is GTT and has open handles. Since there are three different
    implementations for CREATE, it has to be called from different places.
    
    Also fixes:
    MDEV-37395 SIGSEGV on CREATE TABLE ... SELECT where source table is a
    GTT
    
    Parent issue:
    MDEV-35915 Implement Global temporary tables

--- sql/table.h
+++ sql/table.h
@@ -2946,7 +2946,9 @@ struct TABLE_LIST
     /* Associate a table share only if the table exists. */
     OPEN_IF_EXISTS,
     /* Don't associate a table share. */
-    OPEN_STUB
+    OPEN_STUB,
+    /* Table to be opened will be used in locked tables list. */
+    OPEN_FOR_LOCKED_TABLES_LIST,
   } open_strategy;
   /** TRUE if an alias for this table was specified in the SQL. */
   bool          is_alias;
