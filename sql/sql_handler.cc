3d09fe6b6418acb82b0b3d1189b4fd4781784f9b
Nikita Malyavin <nikitamalyavin@gmail.com>
2025-09-11 21:46:08 +0200

    MDEV-37595 Assertion '...' failed using HANDLER+mrg_myisam+GTT
    Assertion `thd->transaction->stmt.is_empty()' failed in mysql_ha_open
    when using mrg_myisam GTT.
    
    mysql_ha_open invokes close_thread_tables itself per error handling,
    even though it should be done globally in mysql_parse after statement
    execution.
    
    The right order is:
    1. commit/rollback transaction
    2. close tables
    3. unlock
    
    Since mysql_parse does it, there is no sense in doing this, unless it's
    a sub-statement, where we might've wanted a better grain of tables
    locked in case of sub-statement failure.
    
    Parent issue:
    MDEV-35915 Implement Global temporary tables

--- sql/sql_handler.cc
+++ sql/sql_handler.cc
@@ -473,14 +473,20 @@ bool mysql_ha_open(THD *thd, TABLE_LIST *tables, SQL_HANDLER *reopen)
 
 err:
   /*
-    No need to rollback statement transaction, it's not started.
+    No need to rollback statement transaction (unless global temporary table
+    has been opened) -- it's not started.
     If called with reopen flag, no need to rollback either,
     it will be done at statement end.
+    Also, no need to close the tables. All this is done at the statement end.
+    However, we may consider closing them early in sub-statement for a better
+    granularity of a complex statement's mdl locks.
   */
-  DBUG_ASSERT(thd->transaction->stmt.is_empty());
-  close_thread_tables(thd);
-  thd->mdl_context.rollback_to_savepoint(mdl_savepoint);
-  thd->set_open_tables(backup_open_tables);
+  if (thd->in_sub_stmt)
+  {
+    close_thread_tables(thd);
+    thd->mdl_context.rollback_to_savepoint(mdl_savepoint);
+    thd->set_open_tables(backup_open_tables);
+  }
   if (sql_handler)
   {
     if (!reopen)
