6094fbca7869c731b99a09d505444c75989dec1a
Sergei Golubchik <serg@mariadb.org>
2025-09-18 17:03:40 +0200

    Revert "MDEV-37394 SIGSEGV in handler::ha_external_lock on CREATE GTT ... INNODB SELECT"
    This reverts commit 80b9c78acea59ecc7008c5eec95e2cfcf55582f9.
    
    but keeps tests that miraculously work.

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -6642,7 +6642,6 @@ private:
                                   List<Item> *items,
                                   MYSQL_LOCK **lock);
   int postlock(THD *thd, TABLE **tables);
-  void unlock_tables();
 };
 
 #include <myisam.h>
