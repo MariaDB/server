5dc7bc519d16cdccbd70c2b5ba1d92eb7948b092
Nikita Malyavin <nikitamalyavin@gmail.com>
2025-10-16 16:11:22 +0200

    MDEV-37771 pseudo_slave_mode=1 must not let pseudo_thread_id=0 assignment
    Narrow the range to start from 1, set default to MY_THREAD_ID_MAX

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -1816,7 +1816,7 @@ Sys_pseudo_thread_id(
        "pseudo_thread_id",
        "This variable is for internal server use",
        SESSION_ONLY(pseudo_thread_id),
-       NO_CMD_LINE, VALID_RANGE(0, MY_THREAD_ID_MAX), DEFAULT(0),
+       NO_CMD_LINE, VALID_RANGE(1, MY_THREAD_ID_MAX), DEFAULT(MY_THREAD_ID_MAX),
        BLOCK_SIZE(1), NO_MUTEX_GUARD, IN_BINLOG);
 
 static bool
