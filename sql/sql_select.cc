95fe80f9f9abbf0e4f44008abb9b9e42c75ad13b
Sergei Petrunia <sergey@mariadb.com>
2025-10-19 17:01:02 +0300

    JOIN::get_examined_rows() should use records_out for fanout

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -10988,7 +10988,7 @@ double JOIN::get_examined_rows()
   while ((tab= next_breadth_first_tab(first_breadth_first_tab(),
                                       top_join_tab_count, tab)))
   {
-    prev_fanout= COST_MULT(prev_fanout, prev_tab->records_init);
+    prev_fanout= COST_MULT(prev_fanout, prev_tab->records_out);
     records=
       COST_ADD(records,
                COST_MULT((double) (tab->get_examined_rows()), prev_fanout));
