f12ff3600f3feed089462d111d4e4fcc0d940616
Daniel Black <daniel@mariadb.org>
2025-10-21 16:22:40 +1100

    MDEV-34902: debian-start erroneously reports issues
    Remove the complexity of check_for_crashed_tables
    by removing it all together. When check_for_crashed_tables
    was written there was a desire to recover MyISAM tables.
    
    With Aria being default from 10.4 for system tables,
    and all non-MyISAM engines being able to do crash
    recovery there isn't the need to autocheck as part
    of a system service.
    
    With check_for_crashed_tables removed there is no need
    for a mailx package recommendation.

--- debian/additions/debian-start.inc.sh
+++ debian/additions/debian-start.inc.sh
@@ -3,50 +3,6 @@
 # This file is included by /etc/mysql/debian-start
 #
 
-## Check MyISAM and Aria unclosed tables.
-# - Requires the server to be up.
-# - Is supposed to run silently in background.
-function check_for_crashed_tables() {
-  set -e
-  set -u
-
-  # But do it in the background to not stall the boot process.
-  logger -p daemon.info -i -t"$0" "Triggering myisam-recover for all MyISAM tables and aria-recover for all Aria tables"
-
-  # Checking for $? is unreliable so the size of the output is checked.
-  # Some table handlers like HEAP do not support CHECK TABLE.
-  tempfile=$(mktemp)
-
-  # We have to use xargs in this case, because a for loop barfs on the
-  # spaces in the thing to be looped over.
-
-  # If a crashed table is encountered, the "mariadb" command will return with a status different from 0
-  set +e
-
-  LC_ALL=C $MARIADB --skip-column-names --batch -e  '
-      select concat('\''select count(*) into @discard from `'\'',
-                    TABLE_SCHEMA, '\''`.`'\'', TABLE_NAME, '\''`'\'')
-      from information_schema.TABLES where TABLE_SCHEMA<>'\''INFORMATION_SCHEMA'\'' and TABLE_SCHEMA<>'\''PERFORMANCE_SCHEMA'\'' and ( ENGINE='\''MyISAM'\'' or ENGINE='\''Aria'\'' )' | \
-    xargs -i ${MARIADB} --skip-column-names --silent --batch \
-                    --force -e "{}" &>"${tempfile}"
-  set -e
-
-  if [ -s "$tempfile" ]; then
-    (
-      /bin/echo -e "\n" \
-        "Improperly closed tables are also reported if clients are accessing\n" \
- 	"the tables *now*. A list of current connections is below.\n";
-       $MYADMIN processlist status
-    ) >> "${tempfile}"
-    # Check for presence as a dependency on mailx would require an MTA.
-    if [ -x /usr/bin/mailx ]; then
-      mailx -e -s"$MYCHECK_SUBJECT" $MYCHECK_RCPT < "$tempfile"
-    fi
-    (echo "$MYCHECK_SUBJECT"; cat "${tempfile}") | logger -p daemon.warn -i -t"$0"
-  fi
-  rm "${tempfile}"
-}
-
 ## Check for tables needing an upgrade.
 # - Requires the server to be up.
 # - Is supposed to run silently in background.
