0d0381e7ef3746280a85f6dfcc7e02a3e97d87d9
Jan Lindström <jan.lindstrom@mariadb.com>
2025-12-18 09:58:38 +0200

    MDEV-38218 : Galera test failure on galera_bf_abort_flush_for_export
    Problem was in wsrep_handle_mdl_conflict function was comparing
    thd->lex->sql_command variable for granted MDL-lock.
    
    There is two possible schedules:
    
        (1) FLUSH TABLES ... FOR EXPORT that will take MDL-lock (granted_thd).
            INSERT from other node is conflicting operation (request_thd)
            and sees MDL-conflict. Because granted_thd has not executed anything
            else thd->lex->sql_command == SQLCOM_FLUSH and this case was
            correctly handled in wsrep_handle_mdl_conflict i.e. INSERT needs
            to wait.
    
        (2) FLUSH TABLES ... FOR EXPORT that will take MDL-lock (granted_thd).
            SET SESSION wsrep_sync_wait=0; (granted_thd)
            INSERT from other node is conflicting operation (request_thd)
    
            However, thd->lex->sql_command is not stored to taken MDL-lock. Now
            as granted_thd is executing SET thd->lex->sql_command != SQLCOM_FLUSH
            and INSERT that is BF will abort it and that means also FTFE is
            killed and MDL-lock relesed. This is incorrect as FTFE has written
            file on filesystem and it can't be really killed.
    
    In this fix wsrep_handle_mdl_conflict is refactored not to use
    thd->lex->sql_command as a variable used for decisions. Instead
    connection state can be determined also via THD members. E.g.:
    
        * wsrep_thd_is_toi() || wsrep_thd_is_applying - ongoing TOI or applier
        * wsrep_thd_is_BF - thread is brute force
        * wsrep_thd_is_SR - thread is streaming replication thread
        * thd->current_backup_stage != BACKUP_FINISHED - there's ongoing BACKUP
        * thd->global_read_lock.is_acquired() - ongoing FTWRL
        * thd->locked_tables_mode == LTM_LOCK_TABLES - ongoing FTFE or LOCK TABLES

--- sql/wsrep_thd.cc
+++ sql/wsrep_thd.cc
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013-2023 Codership Oy <info@codership.com>
+/* Copyright (C) 2013-2025 Codership Oy <info@codership.com>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
