6beb7179b8ef176b9b0bdc72b122c953490c4466
Alexey Botchkov <holyfoot@askmonty.org>
2025-12-23 01:26:19 +0400

    MDEV-38372 ASAN error in InnoDB on malformed WKB
    Geometry::is_binary_valid() method added, and
    used to check WKB before storing in the engine.

--- sql/sql_type_geom.cc
+++ sql/sql_type_geom.cc
@@ -854,6 +854,12 @@ int Field_geom::store(const char *from, size_t length, CHARSET_INFO *cs)
 	wkb_type > (uint32) Geometry::wkb_last)
       goto err;
 
+    const char *dummy;
+    Geometry_buffer buffer;
+    Geometry *geom= Geometry::construct(&buffer, from, uint32(length));
+    if (!geom || !geom->is_binary_valid())
+      goto err;
+
     if (m_type_handler->geometry_type() != Type_handler_geometry::GEOM_GEOMETRY &&
         m_type_handler->geometry_type() != Type_handler_geometry::GEOM_GEOMETRYCOLLECTION &&
         (uint32) m_type_handler->geometry_type() != wkb_type)
@@ -866,21 +872,14 @@ int Field_geom::store(const char *from, size_t length, CHARSET_INFO *cs)
       if (!tab_name)
         tab_name= "";
 
-      Geometry_buffer buffer;
-      Geometry *geom= NULL;
-      String wkt;
-      const char *dummy;
-      wkt.set_charset(&my_charset_latin1);
-      if (!(geom= Geometry::construct(&buffer, from, uint32(length))) ||
-          geom->as_wkt(&wkt, &dummy))
+      StringBuffer<STRING_BUFFER_USUAL_SIZE> wkt(&my_charset_latin1);
+      if (geom->as_wkt(&wkt, &dummy))
         goto err;
 
       my_error(ER_TRUNCATED_WRONG_VALUE_FOR_FIELD, MYF(0),
                Geometry::ci_collection[m_type_handler->geometry_type()]->m_name.str,
-	       wkt.c_ptr_safe(),
-               db, tab_name, field_name.str,
-               (ulong) table->in_use->get_stmt_da()->
-               current_row_for_warning());
+               wkt.c_ptr_safe(), db, tab_name, field_name.str,
+               (ulong) table->in_use->get_stmt_da()->current_row_for_warning());
       goto err_exit;
     }
 
