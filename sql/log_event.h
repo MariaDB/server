9307d85ac54b0da14ab552e0bd60b50874bda80a
SharonIV0x86 <anirudhlakhanpal123@gmail.com>
2025-12-24 21:58:23 +0530

    Added m_colcnt to the ctor declaration of Optional_metadata_fields

--- sql/log_event.h
+++ sql/log_event.h
@@ -4441,7 +4441,8 @@ public:
       @param[in] optional_metadata_len length of optional_metadata field.
      */
     Optional_metadata_fields(unsigned char* optional_metadata,
-                             unsigned int optional_metadata_len);
+                             unsigned int optional_metadata_len,
+                            size_t column_count);
   };
 
   /**
