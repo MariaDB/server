78ac771c67fdc97be8ae08872c637d5e77c56bf5
Rucha Deodhar <rucha.deodhar@mariadb.com>
2026-01-06 18:51:57 +0530

    MDEV-22675: Assertion `offset < table->s->reclength' failed in dump_leaf_key
    Analysis:
    The reclength depends on the size of each field of the temporary table plus
    the null_bytes. Since null_bytes is 0 (because we’re in non-strict mode and
    the source table fields can never be null), there is no need to keep track
    of null values. Hence the total record length is just the sum of the sizes
    of both fields. The second field will always have size 0 because its value
    will always be an empty string. Hence it starts exactly where the first
    field ends which equal to reclength. Hence the assertion failure.
    
    Fix:
    If pack_length() is 0 (which means the size of field is 0) so assigning the
    result as empty string directly, the assertion is not valid in that case.

--- sql/item_sum.cc
+++ sql/item_sum.cc
@@ -3880,7 +3880,17 @@ int dump_leaf_key(void* key_arg, element_count count __attribute__((unused)),
       {
         uint offset= (field->offset(field->table->record[0]) -
                       table->s->null_bytes);
-        DBUG_ASSERT(offset < table->s->reclength);
+        /*
+          When there are no extra null bytes (example when the field
+          can never be null) the offset matches the record length.
+          Otherwise offset < record length.
+        */
+        if (field->pack_length())
+          DBUG_ASSERT(offset < table->s->reclength);
+
+        DBUG_ASSERT(offset + field->pack_length() <=
+                    table->s->reclength - table->s->null_bytes);
+
         res= item->get_str_from_field(*arg, field, &tmp, key,
                                       offset + item->get_null_bytes());
       }
