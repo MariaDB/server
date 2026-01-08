e2d509fca779b9e8480581be5c45f9a6e2729c38
Dave Gosselin <dave.gosselin@mariadb.com>
2026-01-08 13:57:41 -0500

    MDEV-38502: FULL OUTER JOIN get correct sargable condition
    Move the temporary gate against FULL OUTER JOIN deeper into the
    codebase, which causes the FULL OUTER JOIN query plans to have
    more relevant information (hence the change).  In some cases, the
    join order of nested INNER JOINs within the FULL OUTER JOIN changed.
    
    Small cleanups in get_sargable_cond ahead of the feature work in
    the next commit.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -2784,15 +2784,8 @@ setup_subq_exit:
   with_two_phase_optimization= check_two_phase_optimization(thd);
   if (with_two_phase_optimization)
     optimization_state= JOIN::OPTIMIZATION_PHASE_1_DONE;
-  else if (thd->lex->full_join_count == 0)
+  else
   {
-    /*
-      Only during the FULL JOIN development cycle, disable second stage
-      optimization for FULL JOIN queries until the implementation is
-      mature enough to correctly execute the queries.  But for now
-      this allows for some EXPLAIN EXTENDED support.
-    */
-
     if (optimize_stage2())
       DBUG_RETURN(1);
   }
@@ -3072,6 +3065,19 @@ int JOIN::optimize_stage2()
 
   if (setup_semijoin_loosescan(this))
     DBUG_RETURN(1);
+
+  /*
+    Temporary gate.  As the FULL JOIN implementation matures, this keeps moving
+    deeper into the server until it's eventually eliminated.
+  */
+  if (thd->lex->full_join_count)
+  {
+    if (!thd->lex->describe)
+      my_error(ER_NOT_SUPPORTED_YET, MYF(0),
+               "FULL JOINs that cannot be converted to LEFT, RIGHT, or "
+               "INNER JOINs");
+    DBUG_RETURN(0);
+  }
 
   if (make_join_select(this, select, conds))
   {
@@ -5616,23 +5622,25 @@ void mark_join_nest_as_const(JOIN *join,
 
 static Item **get_sargable_cond(JOIN *join, TABLE *table)
 {
-  Item **retval;
-  if (table->pos_in_table_list->on_expr)
+  Item **retval= nullptr;
+  TABLE_LIST *sql_table= table->pos_in_table_list;
+
+  if (sql_table->on_expr)
   {
     /*
       This is an inner table from a single-table LEFT JOIN, "t1 LEFT JOIN
       t2 ON cond". Use the condition cond.
     */
-    retval= &table->pos_in_table_list->on_expr;
+    retval= &sql_table->on_expr;
   }
-  else if (table->pos_in_table_list->embedding &&
-           !table->pos_in_table_list->embedding->sj_on_expr)
+  else if (sql_table->embedding &&
+           !sql_table->embedding->sj_on_expr)
   {
     /*
       This is the inner side of a multi-table outer join. Use the
-      appropriate ON expression.
+      ON expression from the nested join containing the table.
     */
-    retval= &(table->pos_in_table_list->embedding->on_expr);
+    retval= &(sql_table->embedding->on_expr);
   }
   else
   {
@@ -10390,18 +10398,6 @@ choose_plan(JOIN *join, table_map join_tables, TABLE_LIST *emb_sjm_nest)
       join->best_read= limit_cost;
       join->join_record_count= limit_record_count;
     }
-  }
-
-  /*
-    Temporary gate.  As the FULL JOIN implementation matures, this keeps moving
-    deeper into the server until it's eventually eliminated.
-  */
-  if (thd->lex->full_join_count && !thd->lex->describe)
-  {
-    my_error(ER_NOT_SUPPORTED_YET, MYF(0),
-             "FULL JOINs that cannot be converted to LEFT, RIGHT, or "
-             "INNER JOINs");
-    DBUG_RETURN(TRUE);
   }
 
   join->emb_sjm_nest= 0;
