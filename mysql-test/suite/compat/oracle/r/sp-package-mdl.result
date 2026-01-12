MariaDB/server

commit 0eebe3a98f05bd2a386f4334eafd586ce8b64523

MDEV-38539 - compat/oracle.sp-package-mdl fails sporadically
Test output was affected by incompletely closed preceding connections.

Make test agnostic to concurrent connections by querying
information_schema.processlist only for connections that it uses.

Avoid ID-CONNECTION_ID() expression, it is not deterministic.

mysql-test/suite/compat/oracle/r/sp-package-mdl.result
@@ -25,46 +25,48 @@ connect  conn2,localhost,root,,;
 SET sql_mode=ORACLE;
 DROP PACKAGE pkg1;
 connection default;
-SELECT ID-CONNECTION_ID() AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
+SELECT ID AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
 FROM INFORMATION_SCHEMA.PROCESSLIST
 LEFT JOIN INFORMATION_SCHEMA.METADATA_LOCK_INFO
 ON (ID=THREAD_ID)
+WHERE ID IN (CONNECTION_ID(), $conn1_id, $conn2_id)
 ORDER BY ID,TABLE_NAME,LOCK_MODE,LOCK_TYPE;
-CONN	0
-INFO	SELECT ID-CONNECTION_ID() AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
+CONN	default_id
+INFO	SELECT ID AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
 FROM INFORMATION_SCHEMA.PROCESSLIST
 LEFT JOIN INFORMATION_SCHEMA.METADATA_LOCK_INFO
 ON (ID=THREAD_ID)
+WHERE ID IN (CONNECTION_ID(), conn1_id, conn2_id)
 ORDER BY ID,TABLE_NAME,LOCK_MODE,LOCK_TYPE
 STATE	Filling schema table
 LOCK_MODE	MDL_SHARED_NO_WRITE
 LOCK_TYPE	User lock
 TABLE_NAME	
-CONN	1
+CONN	conn1_id
 INFO	DO GET_LOCK('lock',300)
 STATE	User lock
 LOCK_MODE	MDL_SHARED
 LOCK_TYPE	Stored package body metadata lock
 TABLE_NAME	pkg1
-CONN	1
+CONN	conn1_id
 INFO	DO GET_LOCK('lock',300)
 STATE	User lock
 LOCK_MODE	MDL_SHARED
 LOCK_TYPE	Stored function metadata lock
 TABLE_NAME	pkg1.f1
-CONN	1
+CONN	conn1_id
 INFO	DO GET_LOCK('lock',300)
 STATE	User lock
 LOCK_MODE	MDL_SHARED
 LOCK_TYPE	Stored procedure metadata lock
 TABLE_NAME	pkg1.p1
-CONN	2
+CONN	conn2_id
 INFO	DROP PACKAGE pkg1
 STATE	Waiting for stored package body metadata lock
 LOCK_MODE	MDL_BACKUP_DDL
 LOCK_TYPE	Backup lock
 TABLE_NAME	
-CONN	2
+CONN	conn2_id
 INFO	DROP PACKAGE pkg1
 STATE	Waiting for stored package body metadata lock
 LOCK_MODE	MDL_INTENTION_EXCLUSIVE

mysql-test/suite/compat/oracle/t/sp-package-mdl.test
@@ -85,10 +85,13 @@ WHERE ID=$conn2_id
 #                to DROP the package pkg1
 #
 --vertical_results
-SELECT ID-CONNECTION_ID() AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
+let $default_id= `SELECT CONNECTION_ID()`;
+--replace_result $conn1_id conn1_id $conn2_id conn2_id $default_id default_id
+evalp SELECT ID AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
   FROM INFORMATION_SCHEMA.PROCESSLIST
   LEFT JOIN INFORMATION_SCHEMA.METADATA_LOCK_INFO
    ON (ID=THREAD_ID)
+  WHERE ID IN (CONNECTION_ID(), $conn1_id, $conn2_id)
   ORDER BY ID,TABLE_NAME,LOCK_MODE,LOCK_TYPE;
 --horizontal_results
 

commit 3339a14de218aa2b34928f018a4eca72ec81794d

Fixed mtr --embedded compat/oracle.sp-package-mdl
Embedded mode produces different thread identifiers, which makes
replace_result replace unwanted occurences. Use temporary table
for filtering out unwanted threads such that replace_result is
not needed anymore.

This is regression after 0eebe3a98f0.

mysql-test/suite/compat/oracle/r/sp-package-mdl.result
@@ -25,18 +25,22 @@ connect  conn2,localhost,root,,;
 SET sql_mode=ORACLE;
 DROP PACKAGE pkg1;
 connection default;
-SELECT ID AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
+CREATE TEMPORARY TABLE mythreads(t_id INT UNSIGNED NOT NULL, t_name VARCHAR(16));
+INSERT INTO mythreads VALUES(CONNECTION_ID(), 'default_id'),($conn1_id, 'conn1_id'),($conn2_id, 'conn2_id');
+SELECT t_name AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
 FROM INFORMATION_SCHEMA.PROCESSLIST
+INNER JOIN mythreads
+ON (ID=t_id)
 LEFT JOIN INFORMATION_SCHEMA.METADATA_LOCK_INFO
 ON (ID=THREAD_ID)
-WHERE ID IN (CONNECTION_ID(), $conn1_id, $conn2_id)
 ORDER BY ID,TABLE_NAME,LOCK_MODE,LOCK_TYPE;
 CONN	default_id
-INFO	SELECT ID AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
+INFO	SELECT t_name AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
 FROM INFORMATION_SCHEMA.PROCESSLIST
+INNER JOIN mythreads
+ON (ID=t_id)
 LEFT JOIN INFORMATION_SCHEMA.METADATA_LOCK_INFO
 ON (ID=THREAD_ID)
-WHERE ID IN (CONNECTION_ID(), conn1_id, conn2_id)
 ORDER BY ID,TABLE_NAME,LOCK_MODE,LOCK_TYPE
 STATE	Filling schema table
 LOCK_MODE	MDL_SHARED_NO_WRITE
@@ -72,6 +76,7 @@ STATE	Waiting for stored package body metadata lock
 LOCK_MODE	MDL_INTENTION_EXCLUSIVE
 LOCK_TYPE	Schema metadata lock
 TABLE_NAME	
+DROP TABLE mythreads;
 DO RELEASE_LOCK('lock');
 connection conn1;
 pkg1.f1()

mysql-test/suite/compat/oracle/t/sp-package-mdl.test
@@ -84,16 +84,18 @@ WHERE ID=$conn2_id
 #                to acquire an exclusive MDL on 'package body pkg1',
 #                to DROP the package pkg1
 #
+CREATE TEMPORARY TABLE mythreads(t_id INT UNSIGNED NOT NULL, t_name VARCHAR(16));
+evalp INSERT INTO mythreads VALUES(CONNECTION_ID(), 'default_id'),($conn1_id, 'conn1_id'),($conn2_id, 'conn2_id');
 --vertical_results
-let $default_id= `SELECT CONNECTION_ID()`;
---replace_result $conn1_id conn1_id $conn2_id conn2_id $default_id default_id
-evalp SELECT ID AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
+SELECT t_name AS CONN,INFO,STATE,LOCK_MODE,LOCK_TYPE,TABLE_NAME
   FROM INFORMATION_SCHEMA.PROCESSLIST
+  INNER JOIN mythreads
+    ON (ID=t_id)
   LEFT JOIN INFORMATION_SCHEMA.METADATA_LOCK_INFO
    ON (ID=THREAD_ID)
-  WHERE ID IN (CONNECTION_ID(), $conn1_id, $conn2_id)
   ORDER BY ID,TABLE_NAME,LOCK_MODE,LOCK_TYPE;
 --horizontal_results
+DROP TABLE mythreads;
 
 #
 # Now let conn1 finish the package routine execution