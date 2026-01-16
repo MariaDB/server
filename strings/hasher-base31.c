b19f945d5fe3b1b44b34898acfffbb6fb7623632
Yuchen Pei <ycp@mariadb.com>
2026-01-16 11:40:15 +1100

    MDEV-9826 [to-squash] Fix a typo in base31 hash

--- strings/hasher-base31.c
+++ strings/hasher-base31.c
@@ -5,12 +5,12 @@ static void my_hasher_base31_hash_str(my_hasher_st *hasher, const uchar *key,
                                       size_t len)
 {
   for (const uchar *u= key; u < key + len; u++)
-    hasher->m_nr= ((uint) *u) * 31 + hasher->m_nr;
+    hasher->m_nr= hasher->m_nr * 31 + ((uint) *u);
 }
 
 static void my_hasher_base31_hash_byte(my_hasher_st *hasher, uchar byte)
 {
-  hasher->m_nr= ((uint) byte) * 31 + hasher->m_nr;
+  hasher->m_nr= hasher->m_nr * 31 + ((uint) byte);
 }
 
 static uint32 my_hasher_base31_finalize(my_hasher_st *hasher)
