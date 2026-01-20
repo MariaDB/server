MariaDB/server

commit d50663198c013333742fb3231a6ab967dd16882f

DDL recovery for high-level indexes

sql/ddl_log.cc
@@ -1084,7 +1084,8 @@ static handler *create_handler(THD *thd, MEM_ROOT *mem_root,
   like connect, needs the .frm file to exists to be able to do an rename.
 */
 
-static void execute_rename_table(DDL_LOG_ENTRY *ddl_log_entry, handler *file,
+static void execute_rename_table(THD *thd, DDL_LOG_ENTRY *ddl_log_entry,
+                                 handler *file,
                                  const LEX_CSTRING *from_db,
                                  const LEX_CSTRING *from_table,
                                  const LEX_CSTRING *to_db,
@@ -1112,6 +1113,24 @@ static void execute_rename_table(DDL_LOG_ENTRY *ddl_log_entry, handler *file,
                                     to_db->str, to_table->str, "",
                                     flags & FN_TO_IS_TMP);
   }
+
+  uint keys, total_keys;
+
+  if (!get_hlindex_keys_by_open(thd, from_db, from_table, from_path, &keys,
+                                &total_keys))
+  {
+    char idx_from[FN_REFLEN + 1], idx_to[FN_REFLEN + 1];
+    char *idx_from_end= strmov(idx_from, from_path);
+    char *idx_to_end= strmov(idx_to, to_path);
+
+    for (uint i= keys; i < total_keys; i++)
+    {
+      my_snprintf(idx_from_end, HLINDEX_BUF_LEN, HLINDEX_TEMPLATE, i);
+      my_snprintf(idx_to_end, HLINDEX_BUF_LEN, HLINDEX_TEMPLATE, i);
+      file->ha_rename_table(idx_from, idx_to);
+    }
+  }
+
   file->ha_rename_table(from_path, to_path);
   if (file->needs_lower_case_filenames())
   {
@@ -1134,6 +1153,36 @@ static void execute_rename_table(DDL_LOG_ENTRY *ddl_log_entry, handler *file,
   if (!access(from_path, F_OK))
     (void) mysql_file_rename(key_file_frm, from_path, to_path, MYF(MY_WME));
   DBUG_VOID_RETURN;
+}
+
+
+static int execute_drop_table(THD *thd, handlerton *hton, const LEX_CSTRING *db,
+                              const LEX_CSTRING *table, const char *path)
+{
+  uint keys, total_keys;
+  int error, first_error= 0;
+  DBUG_ENTER("execute_drop_table");
+
+  if (get_hlindex_keys_by_open(thd, db, table, path, &keys, &total_keys) == 0)
+  {
+    char idx_path[FN_REFLEN + 1];
+    char *idx_path_end= strmov(idx_path, path);
+
+    for (uint i= keys; i < total_keys; i++)
+    {
+      my_snprintf(idx_path_end, HLINDEX_BUF_LEN, HLINDEX_TEMPLATE, i);
+      error= hton->drop_table(hton, idx_path);
+      if (!non_existing_table_error(error) && !first_error)
+        first_error= error;
+    }
+  }
+  if (!first_error)
+  {
+    error= hton->drop_table(hton, path);
+    if (!non_existing_table_error(error))
+      first_error= error;
+  }
+  DBUG_RETURN(first_error);
 }
 
 
@@ -1463,7 +1512,7 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
     /* fall through */
     case DDL_RENAME_PHASE_TABLE:
       /* Restore frm and table to original names */
-      execute_rename_table(ddl_log_entry, file,
+      execute_rename_table(thd, ddl_log_entry, file,
                            &ddl_log_entry->db, &ddl_log_entry->name,
                            &ddl_log_entry->from_db, &ddl_log_entry->from_name,
                            0,
@@ -1551,14 +1600,10 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
       if (hton)
       {
         no_such_table_handler.only_ignore_non_existing_errors= 1;
-        error= hton->drop_table(hton, path.str);
+        error= execute_drop_table(thd, hton, &db, &table, path.str);
         no_such_table_handler.only_ignore_non_existing_errors= 0;
         if (error)
-        {
-          if (!non_existing_table_error(error))
-            break;
-          error= -1;
-        }
+          break;
       }
       else
         error= ha_delete_table_force(thd, path.str, &db, &table);
@@ -1754,14 +1799,7 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
     if (ddl_log_entry->flags == 0)
     {
       if (hton)
-      {
-        if ((error= hton->drop_table(hton, path.str)))
-        {
-          if (!non_existing_table_error(error))
-            break;
-          error= -1;
-        }
-      }
+        error= execute_drop_table(thd, hton, &db, &table, path.str);
       else
         error= ha_delete_table_force(thd, path.str, &db, &table);
     }
@@ -1988,7 +2026,7 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
       quick_rm_table(thd, hton, &db, &table, QRMT_DEFAULT | FN_IS_TMP);
       if (!is_renamed)
       {
-        execute_rename_table(ddl_log_entry, file,
+        execute_rename_table(thd, ddl_log_entry, file,
                              &ddl_log_entry->from_db,
                              &ddl_log_entry->extra_name, // #sql-backup
                              &ddl_log_entry->from_db,
@@ -2081,7 +2119,7 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
         /* After the renames above, the original table is now in from_name */
         ddl_log_entry->name= ddl_log_entry->from_name;
         /* Rename db.name -> db.extra_name */
-        execute_rename_table(ddl_log_entry, file,
+        execute_rename_table(thd, ddl_log_entry, file,
                              &ddl_log_entry->db, &ddl_log_entry->name,
                              &ddl_log_entry->db, &ddl_log_entry->extra_name,
                              0,
@@ -2109,9 +2147,8 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
       from_end= strend(from_path);
       if (likely(org_hton))
       {
-        error= org_hton->drop_table(org_hton, from_path);
-        if (non_existing_table_error(error))
-          error= 0;
+        error= execute_drop_table(thd, org_hton, &ddl_log_entry->from_db,
+                                  &ddl_log_entry->from_name, from_path);
       }
       strmov(from_end, reg_ext);
       mysql_file_delete(key_file_frm, from_path,
@@ -2132,7 +2169,7 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
       {
         uint length;
         /* Rename new "temporary" table to the original wanted name */
-        execute_rename_table(ddl_log_entry, file,
+        execute_rename_table(thd, ddl_log_entry, file,
                              &ddl_log_entry->db,
                              &ddl_log_entry->name,
                              &ddl_log_entry->from_db,
@@ -2158,19 +2195,19 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
                                  ddl_log_entry->from_db.str,
                                  ddl_log_entry->from_name.str,
                                  "", 0);
-            error= org_hton->drop_table(org_hton, to_path);
+            error= execute_drop_table(thd, org_hton, &ddl_log_entry->from_db,
+                                      &ddl_log_entry->from_name, to_path);
           }
           else
-            error= org_hton->drop_table(org_hton, from_path);
-          if (non_existing_table_error(error))
-            error= 0;
+            error= execute_drop_table(thd, org_hton, &ddl_log_entry->from_db,
+                                      &ddl_log_entry->from_name, from_path);
         }
         strmov(from_path + length, reg_ext);
         mysql_file_delete(key_file_frm, from_path,
                           MYF(MY_WME|MY_IGNORE_ENOENT));
       }
       else
-        execute_rename_table(ddl_log_entry, file,
+        execute_rename_table(thd, ddl_log_entry, file,
                              &ddl_log_entry->db, &ddl_log_entry->name,
                              &ddl_log_entry->db, &ddl_log_entry->extra_name,
                              FN_FROM_IS_TMP,
@@ -2236,9 +2273,9 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
       */
       if (likely(hton))
       {
-        error= hton->drop_table(hton, ddl_log_entry->tmp_name.str);
-        if (non_existing_table_error(error))
-          error= 0;
+        error= execute_drop_table(thd, hton, &ddl_log_entry->from_db,
+                                  &ddl_log_entry->from_name,
+                                  ddl_log_entry->tmp_name.str);
       }
       (void) update_phase(entry_pos, DDL_ALTER_TABLE_PHASE_INIT);
     }

mysql-test/suite/atomic/alter_table.test
@@ -49,6 +49,11 @@ let $crash_count=13;
 let $crash_points='ddl_log_alter_after_create_frm', 'ddl_log_alter_after_create_table', 'ddl_log_alter_after_prepare_inplace','ddl_log_alter_after_copy', 'ddl_log_alter_after_log', 'ddl_log_alter_after_rename_to_backup', 'ddl_log_alter_after_rename_to_backup_log', 'ddl_log_alter_rename_frm', 'ddl_log_alter_after_rename_to_original', 'ddl_log_alter_before_rename_triggers', 'ddl_log_alter_after_rename_triggers', 'ddl_log_alter_after_delete_backup', 'ddl_log_alter_after_drop_original_table';
 
 let $statement_count=16;
+if ($skip_vector == "")
+{
+  let $statement_count=21;
+}
+
 let $statements='ALTER TABLE t1 ADD COLUMN c INT, COMMENT "new"',
                 'ALTER TABLE t1 COMMENT "new"',
                 'ALTER TABLE t1 change column a c int COMMENT "new"',
@@ -64,7 +69,12 @@ let $statements='ALTER TABLE t1 ADD COLUMN c INT, COMMENT "new"',
                 'ALTER TABLE t1 ADD COLUMN c INT, COMMENT "new", rename test2.t2',
                 'ALTER TABLE t1 COMMENT "new", rename test2.t2',
                 'ALTER TABLE t1 ADD key(b), COMMENT "new"',
-                'ALTER TABLE t1 DROP INDEX a';
+                'ALTER TABLE t1 DROP INDEX a',
+                'ALTER TABLE t1 ADD COLUMN v VECTOR(5) NOT NULL, ADD VECTOR INDEX(v)',
+                'ALTER TABLE t1 ADD COLUMN v VECTOR(5) NOT NULL, ADD VECTOR INDEX(v), RENAME t2',
+                'ALTER TABLE t1 ADD COLUMN v VECTOR(5) NOT NULL, ADD VECTOR INDEX(v), ENGINE=$extra_engine',
+                'ALTER TABLE t1 ADD COLUMN v VECTOR(5) NOT NULL, ADD VECTOR INDEX(v), RENAME t2, ENGINE=$extra_engine',
+                'ALTER TABLE t1 ADD COLUMN v VECTOR(5) NOT NULL, ADD VECTOR INDEX(v), RENAME test2.t2';
 
 # If there is a need of testing one specific state (crash point and query),
 # one can use the comments below to execute one specific test combination
@@ -106,8 +116,8 @@ while ($e < $engine_count)
       inc $c;
       let $crash=`select ELT($c, $crash_points)`;
 
-      --eval create table t1 (a int, b int, key(a)) engine=$engine
-      insert into t1 values (1,1),(2,2);
+      --eval create table t1 (a int, b int, key(a) $extra_fields) engine=$engine
+      insert into t1 (a,b) values (1,1),(2,2);
       commit;
       flush tables;
 

mysql-test/suite/atomic/alter_table_rocksdb.test
@@ -2,5 +2,6 @@
 
 let $engine_count=1;
 let $engines='rocksdb';
+let $skip_vector=1;
 set global rocksdb_flush_log_at_trx_commit=1;
 --source alter_table.test

mysql-test/suite/atomic/create_table.result
@@ -63,6 +63,77 @@ t2.MYD
 t2.MYI
 t2.frm
 master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a int)
+query: CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_before_create_frm
+t2.MYD
+t2.MYI
+t2.frm
+crash point: storage_engine_middle_of_create
+"No crash!"
+t1#i#00.MYD
+t1#i#00.MYI
+t1.MYD
+t1.MYI
+t1.frm
+t2.MYD
+t2.MYI
+t2.frm
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_before_create_table
+t2.MYD
+t2.MYI
+t2.frm
+crash point: ddl_log_create_after_create_table
+t2.MYD
+t2.MYI
+t2.frm
+crash point: ddl_log_create_after_drop
+"No crash!"
+t1#i#00.MYD
+t1#i#00.MYI
+t1.MYD
+t1.MYI
+t1.frm
+t2.MYD
+t2.MYI
+t2.frm
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_before_binlog
+t2.MYD
+t2.MYI
+t2.frm
+crash point: ddl_log_create_after_prepare_eof
+"No crash!"
+t1#i#00.MYD
+t1#i#00.MYI
+t1.MYD
+t1.MYI
+t1.frm
+t2.MYD
+t2.MYI
+t2.frm
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_after_binlog
+t1#i#00.MYD
+t1#i#00.MYI
+t1.MYD
+t1.MYI
+t1.frm
+t2.MYD
+t2.MYI
+t2.frm
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_log_complete
+"No crash!"
+t1#i#00.MYD
+t1#i#00.MYI
+t1.MYD
+t1.MYI
+t1.frm
+t2.MYD
+t2.MYI
+t2.frm
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
 query: CREATE OR REPLACE TABLE t2 (a int)
 crash point: ddl_log_create_before_create_frm
 master-bin.000002	#	Query	#	#	use `test`; DROP TABLE IF EXISTS `t2` /* generated by ddl recovery */
@@ -348,6 +419,58 @@ t1.ibd
 t2.frm
 t2.ibd
 master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a int)
+query: CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_before_create_frm
+t2.frm
+t2.ibd
+crash point: storage_engine_middle_of_create
+"No crash!"
+t1#i#00.ibd
+t1.frm
+t1.ibd
+t2.frm
+t2.ibd
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_before_create_table
+t2.frm
+t2.ibd
+crash point: ddl_log_create_after_create_table
+t2.frm
+t2.ibd
+crash point: ddl_log_create_after_drop
+"No crash!"
+t1#i#00.ibd
+t1.frm
+t1.ibd
+t2.frm
+t2.ibd
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_before_binlog
+t2.frm
+t2.ibd
+crash point: ddl_log_create_after_prepare_eof
+"No crash!"
+t1#i#00.ibd
+t1.frm
+t1.ibd
+t2.frm
+t2.ibd
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_after_binlog
+t1#i#00.ibd
+t1.frm
+t1.ibd
+t2.frm
+t2.ibd
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
+crash point: ddl_log_create_log_complete
+"No crash!"
+t1#i#00.ibd
+t1.frm
+t1.ibd
+t2.frm
+t2.ibd
+master-bin.000001	#	Query	#	#	use `test`; CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))
 query: CREATE OR REPLACE TABLE t2 (a int)
 crash point: ddl_log_create_before_create_frm
 master-bin.000002	#	Query	#	#	use `test`; DROP TABLE IF EXISTS `t2` /* generated by ddl recovery */

mysql-test/suite/atomic/create_table.test
@@ -30,8 +30,9 @@ if ($engine_count == "")
 let $crash_count=9;
 let $crash_points='ddl_log_create_before_create_frm', 'storage_engine_middle_of_create', 'ddl_log_create_before_create_table', 'ddl_log_create_after_create_table', 'ddl_log_create_after_drop', 'ddl_log_create_before_binlog', 'ddl_log_create_after_prepare_eof', 'ddl_log_create_after_binlog', 'ddl_log_create_log_complete';
 
-let $statement_count=6;
+let $statement_count=7;
 let $statements='CREATE TABLE t1 (a int)',
+                'CREATE TABLE t1 (a vector(5) not null, VECTOR INDEX(a))',
                 'CREATE OR REPLACE TABLE t2 (a int)',
                 'CREATE TABLE t1 LIKE const_table',
                 'CREATE OR REPLACE TABLE t2 LIKE const_table',

commit 194bb9d4ca3bc91a22379722775dadd05864685b

MDEV-37795 - MariaDB start up spits DDL_LOG: Got error 1033
innodb_fts.crash_recovery,release fails sporadically. In release
build it silently disables sync points, which allows server to
crash at aribitrary point of DDL, specifically when .frm file
is created but not yet written.

Subsequent attempt to open such table at recovery (needed to obtain
number of high-level indexes) leads to OPEN_FRM_CORRUPTED error
rather than something like "file not found" (which is ignored by
recovery). Emitted errors were not handler by mtr.

Silently ignore newly created corrupt .frm during recovery. High-level
indexes were most probably not created at this point.

It is still probable that .frm got corrupted after high-level indexes
were created. In this case recovery won't be able to handle stale
high-level indexes and they will be preserved.

This regression was introduced by "d50663198c0 DDL recovery for
high-level indexes".

sql/ddl_log.cc
@@ -919,9 +919,11 @@ public:
   int unhandled_errors;
   int first_error;
   bool only_ignore_non_existing_errors;
+  bool ignore_corrupt_frm_error;
 
   ddl_log_error_handler() : handled_errors(0), unhandled_errors(0),
-                            first_error(0), only_ignore_non_existing_errors(0)
+                            first_error(0), only_ignore_non_existing_errors(0),
+                            ignore_corrupt_frm_error(false)
   {}
 
   bool handle_condition(THD *,
@@ -935,7 +937,8 @@ public:
     if (non_existing_table_error(sql_errno) ||
         (!only_ignore_non_existing_errors &&
          (sql_errno == EE_LINK ||
-          sql_errno == EE_DELETE || sql_errno == ER_TRG_NO_DEFINER)))
+          sql_errno == EE_DELETE || sql_errno == ER_TRG_NO_DEFINER)) ||
+        (ignore_corrupt_frm_error && sql_errno == ER_NOT_FORM_FILE))
     {
       handled_errors++;
       return TRUE;
@@ -1799,7 +1802,11 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
     if (ddl_log_entry->flags == 0)
     {
       if (hton)
+      {
+        no_such_table_handler.ignore_corrupt_frm_error= true;
         error= execute_drop_table(thd, hton, &db, &table, path.str);
+        no_such_table_handler.ignore_corrupt_frm_error= false;
+      }
       else
         error= ha_delete_table_force(thd, path.str, &db, &table);
     }
@@ -2273,9 +2280,11 @@ static int ddl_log_execute_action(THD *thd, MEM_ROOT *mem_root,
       */
       if (likely(hton))
       {
+        no_such_table_handler.ignore_corrupt_frm_error= true;
         error= execute_drop_table(thd, hton, &ddl_log_entry->from_db,
                                   &ddl_log_entry->from_name,
                                   ddl_log_entry->tmp_name.str);
+        no_such_table_handler.ignore_corrupt_frm_error= false;
       }
       (void) update_phase(entry_pos, DDL_ALTER_TABLE_PHASE_INIT);
     }

sql/discover.cc
@@ -137,6 +137,7 @@ int writefile(const char *path, const char *db, const char *table,
   }
   else
   {
+    DEBUG_SYNC(current_thd, "writefile_after_create_before_write");
     error= (int)mysql_file_write(file, data, len, MYF(MY_WME | MY_NABP));
 
     if (!error && !tmp_table && opt_sync_frm)

mysql-test/suite/atomic/frm.result
@@ -1,0 +1,21 @@
+#
+# MDEV-37795 - MariaDB start up spits DDL_LOG: Got error 1033
+#
+connect ddl1, localhost, root;
+CREATE TABLE t1(v VECTOR(5) NOT NULL, VECTOR INDEX(v));
+SET debug_sync='writefile_after_create_before_write SIGNAL ready1 WAIT_FOR ever';
+ALTER TABLE t1 ADD COLUMN b INT, ALGORITHM=COPY;
+connect ddl2, localhost, root;
+SET debug_sync='writefile_after_create_before_write SIGNAL ready2 WAIT_FOR ever';
+CREATE TABLE t2(v VECTOR(5) NOT NULL, VECTOR INDEX(v));
+connection default;
+SET debug_sync='now WAIT_FOR ready1';
+SET debug_sync='now WAIT_FOR ready2';
+# restart
+DROP TABLE t1;
+DROP TABLE t2;
+ERROR 42S02: Unknown table 'test.t2'
+SET debug_sync='RESET';
+disconnect ddl1;
+disconnect ddl2;
+db.opt

mysql-test/suite/atomic/frm.test
@@ -1,0 +1,28 @@
+source include/have_debug_sync.inc;
+
+--echo #
+--echo # MDEV-37795 - MariaDB start up spits DDL_LOG: Got error 1033
+--echo #
+connect(ddl1, localhost, root);
+CREATE TABLE t1(v VECTOR(5) NOT NULL, VECTOR INDEX(v));
+SET debug_sync='writefile_after_create_before_write SIGNAL ready1 WAIT_FOR ever';
+send ALTER TABLE t1 ADD COLUMN b INT, ALGORITHM=COPY;
+
+connect(ddl2, localhost, root);
+SET debug_sync='writefile_after_create_before_write SIGNAL ready2 WAIT_FOR ever';
+send CREATE TABLE t2(v VECTOR(5) NOT NULL, VECTOR INDEX(v));
+
+connection default;
+SET debug_sync='now WAIT_FOR ready1';
+SET debug_sync='now WAIT_FOR ready2';
+let $shutdown_timeout=0;
+source include/restart_mysqld.inc;
+
+DROP TABLE t1;
+error ER_BAD_TABLE_ERROR;
+DROP TABLE t2;
+SET debug_sync='RESET';
+disconnect ddl1;
+disconnect ddl2;
+let $datadir=`select @@datadir`;
+list_files $datadir/test;