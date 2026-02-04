0ced5f193275c4a2d74014ca540dd80aa5343140
Monty <monty@mariadb.org>
2026-02-04 15:52:32 +0200

    Have mariadbd server read [mariadb-X] and [mariadb-X] sections
    X is major version, like mariadb-11 and mariadbd-11
    
    This simplifies my.cnf files supporting many MariaDB versions

--- include/mysqld_default_groups.h
+++ include/mysqld_default_groups.h
@@ -1,7 +1,7 @@
 const char *load_default_groups[]= {
 "mysqld", "server", MYSQL_BASE_VERSION,
-"mariadb", MARIADB_BASE_VERSION,
-"mariadbd", MARIADBD_BASE_VERSION,
+"mariadb", MARIADB_BASE_VERSION, MARIADB_MAJOR_VERSION,
+"mariadbd", MARIADBD_BASE_VERSION, MARIADBD_MAJOR_VERSION,
 "client-server",
 #ifdef WITH_WSREP
 "galera",
