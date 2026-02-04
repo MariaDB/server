771c7040581fbea6ace7a836089cf6e03cfa7ad8
Marko Mäkelä <marko.makela@mariadb.com>
2026-02-04 11:36:42 +0200

    MDEV-38748: Merge recv_recovery_read_checkpoint() to srv_start()

--- storage/innobase/include/log0recv.h
+++ storage/innobase/include/log0recv.h
@@ -44,11 +44,6 @@ ATTRIBUTE_COLD MY_ATTRIBUTE((nonnull, warn_unused_result))
 @return whether the page was recovered correctly */
 bool recv_recover_page(fil_space_t* space, buf_page_t* bpage);
 
-/** Read the latest checkpoint information from log file
-and store it in log_sys.next_checkpoint and recv_sys.file_checkpoint
-@return error code or DB_SUCCESS */
-dberr_t recv_recovery_read_checkpoint();
-
 /** Start recovering from a redo log checkpoint.
 of first system tablespace page
 @return error code or DB_SUCCESS */
