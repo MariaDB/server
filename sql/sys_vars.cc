a06e81cbdf9b517d784801fb4db7f34ee9314628
Ahmad <ahmedshapan913@gmail.com>
2026-02-17 00:51:54 +0200

    MDEV-27837: Disallow SET @@session.server_id within transaction
    Setting server_id within a transaction is dangerous for replication.
    Added check_not_in_transaction validation to server_id variable.

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -3375,6 +3375,13 @@ static bool check_server_id(sys_var *self, THD *thd, set_var *var)
     return true;
   }
 #endif /* WITH_WSREP */
+
+  if (error_if_in_trans_or_substatement(
+        thd,
+        ER_CANT_DO_THIS_DURING_AN_TRANSACTION,
+        ER_CANT_DO_THIS_DURING_AN_TRANSACTION))
+    return true;
+
   return false;
 }
 
