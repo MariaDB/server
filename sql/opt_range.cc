520fc4bffbd40968e6dd3bf6dfd63440586ebb1c
Dave Gosselin <dave.gosselin@mariadb.com>
2026-02-23 15:45:12 -0500

    MDEV-38649: Wrong result upon condition pushdown on table with DESC key
    QUICK_SELECT_DESC::get_next() sets a stale end range on the storage engine.
    When checking the first range, it sets last_range to the first range and
    then, if it cannot avoid descending scan, it sets the minimum endpoint on
    the storage engine.  However, get_next() may decide that this range is not
    a match after updating the minimum endpoint on the storage engine.  In this
    case, it will continue to the next range.  When continuing, it needs to
    reset not only the state of 'last_range' to point to the next range that
    it's checking, but it also needs to clear the now-stale end range set on
    the storage engine.  While this explanation covers the first and second
    ranges, the issue at hand extends to the general case of ranges 1...N-1 and N.
    
    Before this change and when get_next() decided that a range was not a match, it
    would clear last_range at each loop continuation point.  Rather than clearing
    the minimum endpoint on the storage engine at each continuation, we move all
    loop cleanup to a lambda that's invoked by the for loop directly.  This
    consolidates any logic that needs to be evaluated on loop continuation to one
    place and reduces code duplication.
    
    MySQL fixed this same problem at sha b65ca959efd6ec5369165b1849407318b4886634
    with a different implementation.

--- sql/opt_range.cc
+++ sql/opt_range.cc
@@ -13904,7 +13904,7 @@ int QUICK_SELECT_DESC::get_next()
    *     step back once, and move backwards
    */
 
-  for (;;)
+  for (;; last_range= nullptr, file->set_end_range(nullptr))
   {
     int result;
     if (last_range)
@@ -13953,7 +13953,7 @@ int QUICK_SELECT_DESC::get_next()
 	DBUG_RETURN(local_error);		// Empty table
       if (cmp_prev(last_range) == 0)
 	DBUG_RETURN(0);
-      last_range= 0;                            // No match; go to next range
+      // No match; go to next range
       continue;
     }
 
@@ -13989,7 +13989,7 @@ int QUICK_SELECT_DESC::get_next()
     {
       if (result != HA_ERR_KEY_NOT_FOUND && result != HA_ERR_END_OF_FILE)
 	DBUG_RETURN(result);
-      last_range= 0;                            // Not found, to next range
+      // Not found, to next range
       continue;
     }
     if (cmp_prev(last_range) == 0)
@@ -13998,7 +13998,7 @@ int QUICK_SELECT_DESC::get_next()
 	last_range= 0;				// Stop searching
       DBUG_RETURN(0);				// Found key is in range
     }
-    last_range= 0;                              // To next range
+    // To next range
   }
 }
 
