bdbd63eeb8557462264a5ca2bf604d4f5233623e
Abhishek Bansal <abhibansal593@gmail.com>
2026-02-24 20:00:02 +0530

    MDEV-9247: Make default_master_connection settable globally
    Currently, default_master_connection can only be set on session level. Using SET GLOBAL we get the following error:
    ERROR 1228 (HY000): Variable 'default_master_connection' is a SESSION variable and can't be used with SET GLOBAL
    
    The solution is to change the variable scope from SESSION_ONLY to SESSION_VAR.
    
    Reviewed-by: andrei.elkin@mariadb.com

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -1481,7 +1481,7 @@ static bool check_master_connection(sys_var *self, THD *thd, set_var *var)
 static Sys_var_lexstring Sys_default_master_connection(
        "default_master_connection",
        "Master connection to use for all slave variables and slave commands",
-       SESSION_ONLY(default_master_connection), NO_CMD_LINE, DEFAULT(""),
+       SESSION_VAR(default_master_connection), CMD_LINE(REQUIRED_ARG), DEFAULT(""),
        NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(check_master_connection));
 #endif
 
