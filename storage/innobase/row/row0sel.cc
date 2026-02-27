e96b0012318064bb77a0a7ec24da94103cb18f7f
Sergei Petrunia <sergey@mariadb.com>
2026-02-27 16:45:37 +0300

    MDEV-24813: Signal full scan to storage engines.
    Basic InnoDB Implementation.
    Adds its own tests.
    23 tests in innodb test suite fail.

--- storage/innobase/row/row0sel.cc
+++ storage/innobase/row/row0sel.cc
@@ -4718,8 +4718,10 @@ aborted:
 		} else {
 wait_table_again:
 			err = lock_table(prebuilt->table, nullptr,
-					 prebuilt->select_lock_type == LOCK_S
-					 ? LOCK_IS : LOCK_IX, thr);
+                       prebuilt->full_table_scan ?
+                       prebuilt->select_lock_type :
+                       (prebuilt->select_lock_type == LOCK_S
+                        ? LOCK_IS : LOCK_IX), thr);
 
 			if (err != DB_SUCCESS) {
 
