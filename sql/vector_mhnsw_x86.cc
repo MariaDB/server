4162e01f68dd1a70191b0ff8e2e7b6b9d8984cf2
hadeer <hadderramadan7@gmail.com>
2026-03-05 07:52:03 +0200

    Vector Search: Add OS support verification
    Use xgetbv to check before enabling the right paths.

--- sql/vector_mhnsw_x86.cc
+++ sql/vector_mhnsw_x86.cc
@@ -25,34 +25,70 @@
 
 constexpr uint32_t cpuid_ecx_AVX_AND_XSAVE= 1U << 28 | 1U << 27;
 constexpr uint32_t cpuid_ebx_AVX2= 1U << 5;
-constexpr uint32_t cpuid_ebx_AVX512= 1U << 16 | 1U << 30; // AVX512F and AVX512BW
-
+constexpr uint32_t cpuid_ebx_AVX512= 1U << 16 | 1U << 30;
 
 static uint32_t cpuid_ecx()
 {
 #ifdef __GNUC__
-  uint32_t eax, ebx, ecx, edx;
+  uint32_t eax=0, ebx=0, ecx=0, edx=0;
   __cpuid(1, eax, ebx, ecx, edx);
   return ecx;
 #elif defined _MSC_VER
   int regs[4];
   __cpuid(regs, 1);
   return regs[2];
+#else
+# error "unknown compiler"
 #endif
 }
 
 static uint32_t cpuid_ebx_7()
 {
 #ifdef __GNUC__
-  uint32_t eax, ebx, ecx, edx;
+  uint32_t eax=0, ebx=0, ecx=0, edx=0;
   __cpuid_count(7, 0, eax, ebx, ecx, edx);
   return ebx;
 #elif defined _MSC_VER
   int regs[4];
   __cpuidex(regs, 7, 0);
   return regs[1];
+#else
+# error "unknown compiler"
 #endif
 }
+
+
+static uint64_t xgetbv() {
+#ifdef _MSC_VER
+    return _xgetbv(0);
+#else
+/* builtin xgetbv is only supported in clang 9+, so use inline assembly directly*/
+    uint32_t eax, edx;
+    __asm__ volatile ("xgetbv" : "=a"(eax), "=d"(edx) : "c"(0));
+    return ((uint64_t)edx << 32) | eax;
+#endif
+}
+
+static bool os_have_avx2()
+{
+  return (xgetbv() & 0x06) == 0x06;
+}
+static bool os_have_avx512()
+{
+  return (xgetbv() & 0xe6) == 0xe6;
+}
+static bool cpu_has_avx2()
+{
+  uint32_t ebx7 = cpuid_ebx_7();
+  return (ebx7 & cpuid_ebx_AVX2) == cpuid_ebx_AVX2 && os_have_avx2();
+}
+
+static bool cpu_has_avx512()
+{
+  uint32_t ebx7 = cpuid_ebx_7();
+  return (ebx7 & cpuid_ebx_AVX512) == cpuid_ebx_AVX512 && os_have_avx512();
+}
+
 
 struct FVector;
 typedef struct Vector_ops
@@ -78,11 +114,10 @@ extern "C" Vector_ops vector_ops_x86_available(void)
   if (~ecx & cpuid_ecx_AVX_AND_XSAVE)
     return {nullptr, nullptr, nullptr, nullptr};
 
-  const uint32_t ebx = cpuid_ebx_7();
-  if ((ebx & cpuid_ebx_AVX512) == cpuid_ebx_AVX512)
+  if (cpu_has_avx512())
     return {dot_product_avx512, alloc_size_avx512, align_ptr_avx512, fix_tail_avx512};
 
-  if (ebx & cpuid_ebx_AVX2)
+  if (cpu_has_avx2())
     return {dot_product_avx2, alloc_size_avx2, align_ptr_avx2, fix_tail_avx2};
 
   return {nullptr, nullptr, nullptr, nullptr};
