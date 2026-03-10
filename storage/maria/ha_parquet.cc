730678d5568120ad3ef4b741bb581cd8a61fcd6a
aryan-kancherla <aryankancherla21@gmail.com>
2026-03-10 17:26:03 -0500

    implemented basic boilerplate in ha_parquet.cc

--- storage/maria/ha_parquet.cc
+++ storage/maria/ha_parquet.cc
@@ -19,10 +19,35 @@
 handlerton *parquet_hton= 0;
 
 
+static handler *parquet_create_handler(handlerton *p_hton,
+                                  TABLE_SHARE * table,
+                                  MEM_ROOT *mem_root)
+{
+  return new (mem_root) ha_s3(p_hton, table);
+}
 
 static int ha_parquet_init(void *p)
 {
+    parquet_hton->create = parquet_create_handler;
+}
 
+struct st_mysql_storage_engine parquet_storage_engine=
+{ MYSQL_HANDLERTON_INTERFACE_VERSION };
+
+maria_declare_plugin(pquet)
+{
+  MYSQL_STORAGE_ENGINE_PLUGIN,
+  &parquet_storage_engine,
+  "Parquet",
+  "UIUC Disruption Lab",
+  //PLUGIN_LICENSE_GPL,
+  ha_parquet_init,                   /* Plugin Init      */
+  //ha_s3_deinit,                 /* Plugin Deinit    */
+  //0x0100,                       /* 1.0              */
+  //status_variables,             /* status variables */
+  //system_variables,             /* system variables */
+  //"1.0",                        /* string version   */
+  //MariaDB_PLUGIN_MATURITY_STABLE/* maturity         */
 }
 
 
