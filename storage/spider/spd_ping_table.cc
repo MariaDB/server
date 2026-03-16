e059090ea238f89338bb74505f00269daaf94dd4
Oleksandr Byelkin <sanja@mariadb.com>
2026-03-16 17:50:41 +0100

    MDEV-33714 pert 4 Fix mac compilation for spider

--- storage/spider/spd_ping_table.cc
+++ storage/spider/spd_ping_table.cc
@@ -237,8 +237,8 @@ int spider_release_ping_table_mon_list(
   DBUG_PRINT("info", ("spider conv_name=%s", conv_name));
   DBUG_PRINT("info", ("spider conv_name_length=%u", conv_name_length));
   DBUG_PRINT("info", ("spider link_idx=%d", link_idx));
-  link_idx_str_length = my_sprintf(link_idx_str, (link_idx_str, "%010d",
-    link_idx));
+  link_idx_str_length = my_snprintf(link_idx_str, sizeof(link_idx_str),
+                                    "%010d", link_idx);
   char *buf = (char *) my_alloca(conv_name_length + link_idx_str_length + 1);
   if (!buf)
   {
@@ -1181,8 +1181,8 @@ long long spider_ping_table_body(
     }
   } else {
     link_idx = (int) (args->args[1] ? *((longlong *) args->args[1]) : 0);
-    link_idx_str_length = my_sprintf(link_idx_str, (link_idx_str, "%010d",
-      link_idx));
+    link_idx_str_length = my_snprintf(link_idx_str, sizeof(link_idx_str),
+                                      "%010d", link_idx);
   }
   flags = (int) (args->args[2] ? *((longlong *) args->args[2]) : 0);
   limit = args->args[3] ? *((longlong *) args->args[3]) : 0;
@@ -1640,8 +1640,8 @@ int spider_ping_table_mon_from_table(
       share->static_link_ids_lengths[all_link_idx] + 1);
     link_idx_str_length = share->static_link_ids_lengths[all_link_idx];
   } else {
-    link_idx_str_length = my_sprintf(link_idx_str, (link_idx_str, "%010d",
-      all_link_idx));
+    link_idx_str_length = my_snprintf(link_idx_str, sizeof(link_idx_str),
+                                     "%010d", all_link_idx);
   }
   char *buf = (char *) my_alloca(conv_name_length + link_idx_str_length + 1);
   if (!buf)
