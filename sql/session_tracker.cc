658f83016cc1b03c525d975281535ba55e16558c
Yuchen Pei <ycp@mariadb.com>
2026-03-31 17:08:53 +1100

    MDEV-39217 Fix the hash key calculation in session sysvar tracker
    MDEV-31751 changed the key from the sys_var pointer to its offset.
    This was useful for non-plugin variable aliases, but not so much for
    plugin variables which all have offset 0.

--- sql/session_tracker.cc
+++ sql/session_tracker.cc
@@ -63,6 +63,11 @@ void Session_sysvars_tracker::vars_list::copy(vars_list* from, THD *thd)
 Session_sysvars_tracker::
 sysvar_node_st *Session_sysvars_tracker::vars_list::search(const sys_var *svar)
 {
+  if (((sys_var *)svar)->cast_pluginvar())
+    return reinterpret_cast<sysvar_node_st*>(
+             my_hash_search(&m_registered_sysvars,
+                           reinterpret_cast<const uchar*>(&svar),
+                           sizeof(sys_var*)));
   return reinterpret_cast<sysvar_node_st*>(
            my_hash_search(&m_registered_sysvars,
                          reinterpret_cast<const uchar*>(&svar->offset),
@@ -571,8 +576,13 @@ void Session_sysvars_tracker::mark_as_changed(THD *thd, const sys_var *var)
 const uchar *Session_sysvars_tracker::sysvars_get_key(const void *entry,
                                                       size_t *length, my_bool)
 {
-  ptrdiff_t *key=
-      &((static_cast<const sysvar_node_st *>(entry))->m_svar->offset);
+  sys_var *svar= (static_cast<const sysvar_node_st *>(entry))->m_svar;
+  if (svar->cast_pluginvar())
+  {
+    *length= sizeof(sys_var *);
+    return (uchar *) &(((sysvar_node_st *) entry)->m_svar);
+  }
+  ptrdiff_t *key= &(svar->offset);
   *length= sizeof(*key);
   return reinterpret_cast<const uchar *>(key);
 }
