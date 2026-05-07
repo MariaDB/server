3e86ab4d00f4b20904f378bcaa66132a77a5093f
ParadoxV5 <paradox.ver5@gmail.com>
2026-05-06 19:00:16 -0600

    MDEV-39418: Revert most of MDEV-39240 for MDEV-32188
    MDEV-39240 fixed how servers before 11.5/11.4-enterprise
    accepted timestamps beyond Year 2038 from row-based replication,
    which were invalid until 11.5/11.4-enterprise’s MDEV-32188.
    MDEV-39240 does not apply after MDEV-32188 extended the valid range,
    so those versions should exclude this fix,
    as if MDEV-32188 already covers it.
    
    This commit reverts commits 3234045953 and most of f9c34a1442, keeping
    only the tweak to the MTR script `include/check_type` for consistency.

--- sql/rpl_record.cc
+++ sql/rpl_record.cc
@@ -100,15 +100,11 @@ pack_row(TABLE *table, MY_BITMAP const* cols,
           length is stored in little-endian format, since this is the
           format used for the binlog.
         */
-#ifndef DBUG_OFF
-        uchar *old_pack_ptr= pack_ptr;
+#if !defined DBUG_OFF && defined DBUG_TRACE
+        const uchar *old_pack_ptr= pack_ptr;
 #endif
         pack_ptr= field->pack(pack_ptr, field->ptr + offset,
                               field->max_data_length());
-        DBUG_EXECUTE_IF("rpl_pack_simulate_negation",
-          for (uchar *byte= old_pack_ptr; byte < pack_ptr; ++byte)
-            *byte= ~*byte;
-        );
         DBUG_PRINT("debug", ("field: %s; real_type: %d, pack_ptr: %p;"
                              " pack_ptr':%p; bytes: %d",
                              field->field_name.str, field->real_type(),
@@ -340,8 +336,6 @@ static void convert_field(Field *f, Field *result_field, Field *conv_field)
    A generic, internal, error caused the unpacking to fail.
    @retval HA_ERR_CORRUPT_EVENT
    Found error when trying to unpack fields.
-   @retval HA_ERR_ROWS_EVENT_APPLY
-   Found error when validating field values.
  */
 
 int unpack_row(const rpl_group_info *rgi, TABLE *table, uint const colcnt,
@@ -413,42 +407,6 @@ int unpack_row(const rpl_group_info *rgi, TABLE *table, uint const colcnt,
                     table->s->table_name.str);
         DBUG_RETURN(HA_ERR_CORRUPT_EVENT);
       }
-
-        // Validate this external data
-        switch (f->type()) {
-          case MYSQL_TYPE_TIMESTAMP:
-          {
-            ulong microseconds;
-            my_time_t seconds= f->get_timestamp(&microseconds);
-            if (likely(microseconds <= TIME_MAX_SECOND_PART))
-            {
-              if (likely(seconds >= 0 && seconds <= TIMESTAMP_MAX_VALUE))
-                break;
-              /*
-                `my_time_t` is signed, whereas `UINT_MAX32` is
-                unsigned if `long` is 32-bit and signed otherwise.
-              */
-              else if (likely(seconds == static_cast<my_time_t>(UINT_MAX32)))
-              {
-                // Normalize MariaDB 11.5.1+ Epochalypse
-                f->store_timestamp(TIMESTAMP_MAX_VALUE, microseconds);
-                break;
-              }
-            }
-            static const char unixtime_format[]=
-              "FROM_UNIXTIME(%ld + %lu/1""000""000)";
-            // + strlen("2147483648""16777215") - strlen("%ld""%lu")
-            char unixtime[sizeof(unixtime_format) + 12];
-            snprintf(unixtime, sizeof(unixtime), unixtime_format,
-              seconds, microseconds);
-            rgi->rli->report(ERROR_LEVEL, ER_TRUNCATED_WRONG_VALUE_FOR_FIELD,
-              rgi->gtid_info(), ER(ER_TRUNCATED_WRONG_VALUE_FOR_FIELD),
-              f->type_handler()->name().ptr(), unixtime, table->s->db.str,
-              table->s->table_name.str, f->field_name.str, 0lu);
-            DBUG_RETURN(HA_ERR_ROWS_EVENT_APPLY);
-          }
-          default:;
-        }
 
       /*
         If conv_field is set, then we are doing a conversion. In this
