6fc40d4d70bd259da1692624b3e47d3a131143d8
Sergei Petrunia <sergey@mariadb.com>
2026-05-29 15:37:25 +0300

    [No MDEV#] Invalid conversion is done when reading JSON
    When reading JSON strings. we call json_unescape_to_string()
    with a StringBuffer with "binary" charset.
    
    That causes conversion from utf8mb4 (data in JSON is in this
    charset) to "wide characters".
    
    Create the StringBuffer with utf8mb4 to make sure no conversion
    is done.
    
    Now with unit test.

--- unittest/sql/json_reader-t.cc
+++ unittest/sql/json_reader-t.cc
@@ -44,33 +44,43 @@ int main(int args, char **argv)
                               sizeof(int), NULL, JSON_DEPTH_DEFAULT,
                               JSON_DEPTH_INC, MYF(0));
   system_charset_info= &my_charset_utf8mb3_bin;
-  const char *js_doc="{ \"str_val\": \"abc\", \"double_val\": 1234.5 }";
-  json_scan_start(&je, &my_charset_utf8mb3_bin, (const uchar *) js_doc,
-                  (const uchar *) js_doc + strlen(js_doc));
-  
-  char *parsed_name;
-  double parsed_dbl;
-  Read_named_member array[]= {
-      {"str_val",    Read_string(&alloc, &parsed_name), false},
-      {"double_val", Read_double(&parsed_dbl), false},
-      {NULL, Read_double(NULL), false }
-  };
   String err_buf;
 
-  rc= json_read_object(&je, array, &err_buf);
-  ok(!rc, "Basic object read");
-  free_root(&alloc, 0);
-#if 0
-
+  /* Basic json_read_object test */
   {
-    Json_writer w;
-    w.start_object();
-    w.add_member("foo");
-    w.end_object();
-    ok(w.invalid_json, "Started a name but didn't add a value");
+    const char *js_doc="{ \"str_val\": \"abc\", \"double_val\": 1234.5 }";
+    json_scan_start(&je, &my_charset_utf8mb3_bin, (const uchar *) js_doc,
+                    (const uchar *) js_doc + strlen(js_doc));
+
+    char *parsed_name;
+    double parsed_dbl;
+    Read_named_member array[]= {
+        {"str_val",    Read_string(&alloc, &parsed_name), false},
+        {"double_val", Read_double(&parsed_dbl), false},
+        {NULL, Read_double(NULL), false }
+    };
+    rc= json_read_object(&je, array, &err_buf);
+    ok(!rc, "Basic object read");
   }
 
-#endif
+  /* Test that reading handles characters correctly. */
+  {
+    /* utf8mb4 character, U+1F600 Grinning Face: */
+    const char *field_val="\xF0\x9F\x98\x80";
+    const char *js_doc="{ \"str\" : \"\xF0\x9F\x98\x80\"}";
+    json_scan_start(&je, &my_charset_utf8mb4_bin, (const uchar *) js_doc,
+                    (const uchar *) js_doc + strlen(js_doc));
+    char *str;
+    Read_named_member array[]= {
+        {"str", Read_string(&alloc, &str), false},
+        {NULL,  Read_double(NULL), false }
+    };
+    rc= json_read_object(&je, array, &err_buf);
+    ok(!rc, "Success reading UTF-8 grinning face (with wchar_t > 255)");
+    ok(!strcmp(str, field_val), "Got correct value for grinning face");
+  }
+
+  free_root(&alloc, 0);
   diag("Done");
 
   my_end(MY_CHECK_ERROR);
