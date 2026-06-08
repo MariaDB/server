4fd628028c8a0d18c7a03072bafeffd73d29bf89
Jaeheon Shim <git@jaeheonshim.com>
2026-06-07 22:52:44 -0400

    Modify Item_field::fix_fields and Item_field::fix_outer_field to push to non_agg_fields only if BOTH in_sum_func and in_any_value are false

--- sql/item.cc
+++ sql/item.cc
@@ -6365,6 +6365,7 @@ Item_field::fix_outer_field(THD *thd, Field **from_field, Item **reference)
       if (*from_field)
       {
         if (thd->variables.sql_mode & MODE_ONLY_FULL_GROUP_BY &&
+            !thd->lex->in_sum_func && !thd->lex->in_any_value &&
             select->cur_pos_in_select_list != UNDEF_POS)
         {
           /*
@@ -6897,7 +6898,7 @@ bool Item_field::fix_fields(THD *thd, Item **reference)
   if (check_ora_join(reference, outer_fixed))
     goto error;
   if (thd->variables.sql_mode & MODE_ONLY_FULL_GROUP_BY &&
-      !outer_fixed && !thd->lex->in_sum_func &&
+      !outer_fixed && !thd->lex->in_sum_func && !thd->lex->in_any_value &&
       select &&
       select->cur_pos_in_select_list != UNDEF_POS &&
       select->join)
