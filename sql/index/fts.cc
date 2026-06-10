0aa4865c7269ed08100be23e46c3911e97ce272c
Sergei Golubchik <serg@mariadb.org>
2026-06-10 18:54:42 +0200

    w 5.4

--- sql/index/fts.cc
+++ sql/index/fts.cc
@@ -21,6 +21,13 @@
 #include "create_options.h"
 #include "ft_global.h"
 #include "mysql/plugin_ftparser.h"
+#include "sql_tree.h"
+#include <math.h>
+
+extern struct st_mysql_ftparser ft_default_parser; // XXX
+
+constexpr double pivot= 0.0115;
+static inline double lws(double tf) { return log(tf) + 1; }
 
 ha_create_table_option fts_index_options[]=
 {
@@ -48,27 +55,61 @@ public:
   bool reading() override { return false; }
 };
 
-struct fts_word_ctx
+struct fts_word
+{
+  const uchar *ptr;
+  uint len;
+};
+
+static int cmpw(void* cs, const void* a_, const void* b_)
+{
+  auto a= static_cast<const fts_word*>(a_), b= static_cast<const fts_word*>(b_);
+  return ha_compare_word((CHARSET_INFO*)cs, a->ptr, a->len, b->ptr, b->len);
+}
+
+typedef Tree<fts_word, CHARSET_INFO> fts_word_tree;
+
+struct fts_collect_ctx
+{
+  fts_word_tree *wtree;
+  MEM_ROOT *mem_root;
+};
+
+static int fts_collect_word(MYSQL_FTPARSER_PARAM *param,
+                            const char *word, int word_len,
+                            MYSQL_FTPARSER_BOOLEAN_INFO *)
+{
+  auto *ctx= static_cast<fts_collect_ctx *>(param->ftparser_state);
+  fts_word w;
+  w.ptr= (uchar *)memdup_root(ctx->mem_root, word, word_len);
+  if (!w.ptr)
+    return 1;
+  w.len= (uint)word_len;
+  return ctx->wtree->insert(w);
+}
+
+struct fts_docstat
+{
+  uint uniq;
+  double sum;
+};
+
+struct fts_write_ctx
 {
   TABLE *table;
   const uchar *tref;
   uint tref_len;
+  CHARSET_INFO *cs;
+  double sum;
+  uint uniq;
+  int err;
 };
 
-static int fts_add_word(MYSQL_FTPARSER_PARAM *param,
-                        const char *word, int word_len,
-                        MYSQL_FTPARSER_BOOLEAN_INFO *)
-{
-  auto *ctx= static_cast<fts_word_ctx *>(param->ftparser_state);
-  TABLE *t= ctx->table;
-  restore_record(t, s->default_values);
-  t->field[0]->store(word, word_len, param->cs);
-  t->field[1]->store_binary(ctx->tref, ctx->tref_len);
-  return t->file->ha_write_row(t->record[0]);
-}
+static inline bool fts_word_char(int ctype, uchar c)
+{ return (ctype & (_MY_U | _MY_L | _MY_NMR)) || (c) == '_'; }
 
-#define fts_word_char(ctype, c) ((ctype) & (_MY_U | _MY_L | _MY_NMR) || (c) == '_')
-#define fts_misc_word_char(c)   ((c) == '\'')
+static inline bool fts_misc_word_char(uchar c)
+{ return c == '\''; }
 
 static int fts_mysql_parse(MYSQL_FTPARSER_PARAM *param,
                            const char *doc_arg, int doc_len)
@@ -100,23 +141,48 @@ static int fts_mysql_parse(MYSQL_FTPARSER_PARAM *param,
         mwc++;
     }
     uint wbytes= (uint)(doc - wstart) - mwc;
-    if (wchars >= ft_min_word_len && wchars < ft_max_word_len)
+    if (wchars >= ft_min_word_len && wchars <= ft_max_word_len)
       if (param->mysql_add_word(param, (char *)wstart, wbytes, nullptr))
         return 1;
   }
   return 0;
 }
 
+static int fts_sum_words(const fts_word &, element_count cnt, fts_docstat &stat)
+{
+  stat.sum+= lws(cnt);
+  stat.uniq++; // XXX Isn't it tree->element_count ?
+  return 0;
+}
+
+static int fts_write_word(const fts_word &word, element_count cnt,
+                          fts_write_ctx &ctx)
+{
+  double weight= lws(cnt) / ctx.sum * ctx.uniq / (1 + pivot * ctx.uniq);
+  TABLE *t= ctx.table;
+  restore_record(t, s->default_values);
+  t->field[0]->store((char *)word.ptr, word.len, ctx.cs);
+  t->field[1]->store_binary(ctx.tref, ctx.tref_len);
+  t->field[2]->store(weight, false);
+  return ctx.err= t->file->ha_write_row(t->record[0]);
+}
+
 int fts_index::insert_row(TABLE *tbl, KEY *keyinfo)
 {
   MY_BITMAP *old_map= dbug_tmp_use_all_columns(tbl, &tbl->read_set);
-  fts_word_ctx ctx= { table, tbl->file->ref, tbl->file->ref_length };
+  CHARSET_INFO *cs= keyinfo->key_part[0].field->charset();
+  MEM_ROOT mem_root;
+  fts_word_tree wtree(cmpw, cs);
 
+  init_alloc_root(PSI_INSTRUMENT_MEM, &mem_root, 4096, 4096, MYF(0));
+
+  fts_collect_ctx cctx= { &wtree, &mem_root };
   MYSQL_FTPARSER_PARAM param {};
   param.mysql_parse=    fts_mysql_parse;
-  param.mysql_add_word= fts_add_word;
-  param.ftparser_state= &ctx;
+  param.mysql_add_word= fts_collect_word;
+  param.ftparser_state= &cctx;
   param.mode=           MYSQL_FTPARSER_SIMPLE_MODE;
+  param.cs=             cs;
 
   struct st_mysql_ftparser *parser= keyinfo->parser
       ? plugin_data(keyinfo->parser, struct st_mysql_ftparser *)
@@ -131,13 +197,24 @@ int fts_index::insert_row(TABLE *tbl, KEY *keyinfo)
     Field *field= keyinfo->key_part[i].field;
     if (field->is_null())
       continue;
-    String *val= field->val_str(&buf);
-    param.cs=     field->charset();
+    String *val=  field->val_str(&buf);
     param.doc=    val->ptr();
     param.length= val->length();
     err= parser->parse(&param);
   }
 
+  if (!err)
+  {
+    fts_docstat stat= { 0, 0.0 };
+    wtree.walk(fts_sum_words, stat);
+
+    fts_write_ctx wctx { table, tbl->file->ref, tbl->file->ref_length,
+                         cs, stat.sum, stat.uniq, 0 };
+    wtree.walk(fts_write_word, wctx);
+    err= wctx.err;
+  }
+
+  free_root(&mem_root, MYF(0));
   dbug_tmp_restore_column_map(&tbl->read_set, old_map);
   return err;
 }
@@ -152,10 +229,11 @@ public:
 
 static const LEX_CSTRING fts_hlindex_table_def(THD *thd, uint ref_length)
 {
-  const char templ[]="CREATE TABLE i (              "
-                     "  word varchar(84) NOT NULL,  "
-                     "  tref varbinary(%u) NOT NULL,"
-                     "  KEY (word))                 ";
+  const char templ[]="CREATE TABLE i (                "
+                     "  word varchar(84) NOT NULL,    "
+                     "  tref varbinary(%u) NOT NULL,  "
+                     "  weight float NOT NULL,        "
+                     "  KEY (word))                   ";
   size_t len= sizeof(templ) + 32;
   char *s= thd->alloc(len);
   len= my_snprintf(s, len, templ, ref_length);
