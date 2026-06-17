5a3d98a36cf31d2d886135afb27c483c4c5da82b
Yuchen Pei <ycp@mariadb.com>
2026-06-17 16:17:57 +1000

    MDEV-24813 Signal full scan to storage engines.
    When starting to do a full table/index scan without a WHERE or JOIN
    condition, tell the storage engine so and the corresponding
    ulong-truncated LIMIT.
    
    Include an innodb implementation: added an innodb switch
    table_lock_on_full_scan, so that when the switch is on, on receiving
    the full scan signal from the sql layer, if the truncated LIMIT is
    ULONG_MAX (likely no LIMIT), attempt to acquire a table lock.
    
    Updated tests that have different results with the switch on.
    
    The three deadlock_*_race tests cannot reach their DEBUG_SYNC race
    under a table lock (it degenerates to a timeout), and the three I_S
    tests only restate a lock-mode change already covered
    by innodb_full_scan.test.
    
    (Comment and code edited by Sergei Petrunia <sergey@mariadb.com> and
    Thirunarayanan Balathandayuthapani <thiru@mariadb.com>)

--- sql/ha_partition.cc
+++ sql/ha_partition.cc
@@ -9749,6 +9749,8 @@ int ha_partition::extra(enum ha_extra_function operation)
   case HA_EXTRA_ABORT_COPY:
   case HA_EXTRA_BEGIN_ALTER_IGNORE_COPY:
     DBUG_RETURN(loop_partitions(extra_cb, &operation));
+  case HA_EXTRA_FULL_SCAN:
+    break;
   default:
   {
     /* Temporary crash to discover what is wrong */
@@ -9825,6 +9827,8 @@ int ha_partition::extra_opt(enum ha_extra_function operation, ulong arg)
       DBUG_RETURN(loop_read_partitions(start_keyread_cb, &arg));
     case HA_EXTRA_CACHE:
       prepare_extra_cache(arg);
+      DBUG_RETURN(0);
+    case HA_EXTRA_FULL_SCAN:
       DBUG_RETURN(0);
     default:
       DBUG_ASSERT(0);
