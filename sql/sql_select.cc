8b3533c1c09e6523f825465e8686c8b2812a9767
Sergei Golubchik <serg@mariadb.org>
2026-06-20 12:00:14 +0200

    w 6.6 EXPLAIN

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -31130,7 +31130,7 @@ int append_possible_keys(MEM_ROOT *alloc, String_list &list, TABLE *table,
                          key_map possible_keys)
 {
   uint j;
-  for (j=0 ; j < table->s->keys ; j++)
+  for (j=0 ; j < table->s->total_keys ; j++)
   {
     if (possible_keys.is_set(j))
       if (!(list.append_str(alloc, table->key_info[j].name.str)))
