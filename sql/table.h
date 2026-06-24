f35b302b28450e9595a1252f507af6da4a790112
Aleksey Midenkov <midenok@gmail.com>
2026-06-25 01:11:44 +0300

    MDEV-39384 Use index in TR_table::query
    TR_table::query() used table scan. Now utilize the index if possible,
    with minimum validity detection. Getting trx_id by commit_ts is still
    suboptimal is it does two index accesses (as limited by fields in
    commit_ts index).
    
    When the index detection fails TR_table::query() falls back to
    original table scanning method.

--- sql/table.h
+++ sql/table.h
@@ -3524,6 +3524,14 @@ public:
     FIELD_COUNT
   };
 
+  enum index_id_t {
+    IDX_TRX_ID= 0,
+    IDX_COMMIT_ID,
+    IDX_BEGIN_TS,
+    IDX_COMMIT_TS,
+    IDX_count /* Number of indexes */
+  };
+
   enum enabled {NO, MAYBE, YES};
   static enum enabled use_transaction_registry;
 
@@ -3641,6 +3649,24 @@ public:
   bool operator!= (const TABLE_LIST &subj) const
   {
     return !(*this == subj);
+  }
+  bool is_idx_correct(uint idx, field_index_t fld) const
+  {
+    DBUG_ASSERT(table->s->keys == IDX_count);
+
+    if (table->s->keys <= idx)
+      return false;
+
+    handler *file= table->file;
+    const KEY *key= &table->key_info[idx];
+    if (HA_READ_ORDER == (file->index_flags(idx, 0, false) & (HA_READ_ORDER | HA_ONLY_WHOLE_INDEX)) &&
+        key->key_part->fieldnr - 1 == fld &&
+        !(key->key_part->key_part_flag & HA_REVERSE_SORT))
+    {
+      DBUG_ASSERT(key->key_part->fieldnr - 1 == fld);
+      return true;
+    }
+    return false;
   }
 };
 
