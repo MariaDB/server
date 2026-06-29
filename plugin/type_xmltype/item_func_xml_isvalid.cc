736309a8faaaa9229044577cbd00b325730b129a
Alexey (Holyfoot) Botchkov <holyfoot@mariadb.com>
2026-06-29 22:51:06 +0300

    MDEV-40019 Xmlisvalid shows wrong result if calling it multiple times from query with UNION ALL.
    Call the validation_prepare() for the tail of the list.

--- plugin/type_xmltype/item_func_xml_isvalid.cc
+++ plugin/type_xmltype/item_func_xml_isvalid.cc
@@ -1772,6 +1772,9 @@ turn_over:
       if (!m_cur_tag->is_validate_done())
         return false;
       m_cur_tag= m_cur_tag->m_next_tag;
+      if (m_cur_tag)
+        m_cur_tag->validate_prepare();
+
     }
 
     return true;
