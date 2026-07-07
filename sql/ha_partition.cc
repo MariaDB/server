f09e3b61e3a03bc4a1773f98a901acd11740c583
Monty <monty@mariadb.org>
2026-07-07 14:59:38 +0300

    Removed some not needed checks and add a DBUG_ASSERT() for not covered code
    - In ha_partition.cc:check_parallel_search(), remove check if
      item_field->field is null. This is not needed as the function is run
      after fix_field() which guarnatees that the field is always set.
    - Added DBUG_ASSERT(new_field) to Item_field::fix_fields() to check if a
      select-list item, found by name or alias when resolving ORDER BY/GROUP
      BY/HAVING, can have field == 0. This error path is not covered by any
      mtr test.

--- sql/ha_partition.cc
+++ sql/ha_partition.cc
@@ -7680,7 +7680,7 @@ bool ha_partition::check_parallel_search()
       {
         Field *order_field= ((Item_field *)item)->field;
         DBUG_PRINT("info",("partition order_field: %p", order_field));
-        if (order_field && order_field->table == table_list->table)
+        if (order_field->table == table_list->table)
         {
           Field *part_field= m_part_info->full_part_field_array[0];
           DBUG_PRINT("info",("partition order_field: %p", order_field));
@@ -7723,7 +7723,7 @@ bool ha_partition::check_parallel_search()
       {
         Field *group_field= ((Item_field *)item)->field;
         DBUG_PRINT("info",("partition group_field: %p", group_field));
-        if (group_field && group_field->table == table_list->table)
+        if (group_field->table == table_list->table)
         {
           Field *part_field= m_part_info->full_part_field_array[0];
           DBUG_PRINT("info",("partition group_field: %p", group_field));
