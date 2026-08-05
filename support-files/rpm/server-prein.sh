d842d6e994a8b0d67c280bf785a11357d6b316a2
forkfun <alice.sherepa@gmail.com>
2026-08-05 15:02:05 +0200

    on rpm: don't set mysql user's $HOME to datadir
    useradd set --home to %{mysqldatadir}, matching datadir.
    Set --home to /nonexistent (as in deb)

--- support-files/rpm/server-prein.sh
+++ support-files/rpm/server-prein.sh
@@ -68,6 +68,8 @@ fi
 
 # Create a MariaDB user and group. Do not report any problems if it already exists.
 groupadd -r %{mysqld_group} 2> /dev/null || true
-useradd -M -r --home %{mysqldatadir} --shell /sbin/nologin --comment "MariaDB server" --gid %{mysqld_group} %{mysqld_user} 2> /dev/null || true
+useradd -M -r --home /nonexistent --shell /sbin/nologin --comment "MariaDB server" --gid %{mysqld_group} %{mysqld_user} 2> /dev/null || true
 # The user may already exist, make sure it has the proper group nevertheless (BUG#12823)
 usermod --gid %{mysqld_group} %{mysqld_user} 2> /dev/null || true
+# Make sure the home dir is correct too (must not be datadir)
+usermod -d /nonexistent %{mysqld_user} 2> /dev/null || true
