5c50d1471f8d122b25fbe73452669e97c7df05e2
Sergei Golubchik <serg@mariadb.org>
2026-08-17 17:16:39 +0200

    MDEV-40445 TLS session resumption (wolfssl)
    * enable HAVE_SESSION_TICKET to allow resumption
    * enable OPENSSL_ALL+KEEP_PEER_CERT to keep peer cert in the ticket,
      otherwise REQUIRE SUBJECT doesn't work after resumption
    * disable OPENSSL_EXTRA which was enabled as a replacement
      when OPENSSL_ALL was disabled in 136e8661197
    * disable NO_WOLFSSL_STUB to get SSL_CTX_sess_hits/etc stubs
    * but they're stubs, always return 0, so add a wolfssl combination to
      the test that checks for these values

--- mysql-test/suite.pm
+++ mysql-test/suite.pm
@@ -108,6 +108,8 @@ sub skip_combinations {
   $skip{'main/chained_ssl_certificates.test'} = 'Skip if on Windows without OpenSSL'
     unless !IS_WINDOWS || $openssl_ver;
 
+  $skip{'include/ssl_library.combinations'} = [ $openssl_ver ? 'wolfssl' : 'openssl' ];
+
   %skip;
 }
 
