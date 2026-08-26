7005494e9039d0472cd5d7f63fb5ab54e15382f1
Daniel Handzus <dhandzus@redhat.com>
2026-08-26 12:01:19 +0200

    MDEV-40936 Install test binaries to libexecdir instead of bindir
    Test-component binaries (mariadb-test, mariadb-client-test,
    mariadb-test-embedded, mariadb-client-test-embedded, test-connect-t)
    are not intended for direct invocation by users and are not meant to
    appear in $PATH, therefore install them to libexecdir instead of bindir.
    
    Changes:
    -  Add INSTALL_LIBEXECDIR to all cmake install layouts
       (STANDALONE, RPM, DEB, SVR4)
    -  Set DESTINATION ${INSTALL_LIBEXECDIR} for mariadb-test,
       mariadb-client-test, mariadb-test-embedded,
       mariadb-client-test-embedded, and test-connect-t
    -  Update mariadb-test-run.pl to find test binaries under
       libexec/ for installed-layout runs
    -  Update debian/mariadb-test.install paths accordingly

--- cmake/install_layout.cmake
+++ cmake/install_layout.cmake
@@ -45,6 +45,7 @@
 # - INSTALL_SYSCONF2DIR     (additional config files, e.g. /etc/mysql/conf.d)
 #
 # - INSTALL_LIBDIR          (directory with client end embedded libraries)
+# - INSTALL_LIBEXECDIR      (directory with binaries that are not executed directly)
 # - INSTALL_PLUGINDIR       (directory for plugins)
 #
 # - INSTALL_INCLUDEDIR      (directory for MySQL headers)
@@ -108,6 +109,7 @@ SET(INSTALL_SBINDIR_STANDALONE          "bin")
 SET(INSTALL_SCRIPTDIR_STANDALONE        "scripts")
 #
 SET(INSTALL_LIBDIR_STANDALONE           "lib")
+SET(INSTALL_LIBEXECDIR_STANDALONE       "libexec")
 SET(INSTALL_PLUGINDIR_STANDALONE        "lib/plugin")
 #
 SET(INSTALL_INCLUDEDIR_STANDALONE       "include/mysql")
@@ -142,6 +144,7 @@ IF(CMAKE_SIZEOF_VOID_P EQUAL 8)
 ELSE()
   SET(INSTALL_LIBDIR_RPM                "lib")
 ENDIF()
+SET(INSTALL_LIBEXECDIR_RPM              "libexec")
 SET(INSTALL_PLUGINDIR_RPM               "${INSTALL_LIBDIR_RPM}/mysql/plugin")
 #
 SET(INSTALL_INCLUDEDIR_RPM              "include/mysql")
@@ -182,6 +185,7 @@ SET(INSTALL_SYSCONFDIR_DEB              "/etc")
 SET(INSTALL_SYSCONF2DIR_DEB             "/etc/mysql/mariadb.conf.d")
 #
 SET(INSTALL_LIBDIR_DEB                  "lib/${CMAKE_CXX_LIBRARY_ARCHITECTURE}")
+SET(INSTALL_LIBEXECDIR_DEB              "libexec")
 SET(INSTALL_PLUGINDIR_DEB               "lib/mysql/plugin")
 #
 SET(INSTALL_INCLUDEDIR_DEB              "include/mariadb")
@@ -215,6 +219,7 @@ SET(INSTALL_SBINDIR_SVR4                "bin")
 SET(INSTALL_SCRIPTDIR_SVR4              "scripts")
 #
 SET(INSTALL_LIBDIR_SVR4                 "lib")
+SET(INSTALL_LIBEXECDIR_SVR4             "libexec")
 SET(INSTALL_PLUGINDIR_SVR4              "lib/plugin")
 #
 SET(INSTALL_INCLUDEDIR_SVR4             "include/mysql")
