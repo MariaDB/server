d5027ae19b5a1bc615fd48b2cd876020a66bfc6e
Marko Mäkelä <marko.makela@mariadb.com>
2026-09-21 13:42:25 +0300

    MDEV-41152: Fix FILE_CREATE recovery
    fil_name_process(): Treat FILE_CREATE in the same way as FILE_MODIFY
    that led to a FIL_LOAD_DEFER return. Remove the parameter lsn,
    and return file_name_t& in which the caller may assign create_lsn
    when processing a FILE_CREATE record.
    
    deferred_spaces.reinit_all(): Never create anything for deleted
    tablespaces. Doing so could cause a legitimate file to be deleted
    if files are being deleted and re-created with the same name.
    
    deferred_space.create(): Remove some duplicated code. Missing
    tablespace files will be created in fil_node_open_file_low()
    starting with
    commit 759e3523e3d832b174cf0a612704da38b2557b40 (MDEV-38026).
    
    deferred_spaces::item::lsn: Remove. Starting with
    commit 37d8577aee3bd87b5b04464144d064063b169039 (MDEV-40728)
    each FILE_ record is parsed only once.
    
    recv_sys_t::parse_store_if_exists(): Tell the caller to skip
    tablespaces for which both FILE_CREATE and FILE_DELETE was parsed.
    This improves performance, not correctness.
    
    recv_validate_tablespace(): Avoid duplicated tablespace lookup
    and remove a redundant deferred_spaces.add(); fil_name_process
    already keeps deferred_spaces in sync with recv_spaces.
    
    fil_space_t::rename(): If !log, assert !replace and that
    the target file name does not exist.
    
    os_file_rename_func(): Do not check that the target path
    does not exist. This is already checked by every caller.
    This fixes a debug assertion failure that could otherwise
    occur when recovering from a crash in
    fil_space_t::rename() between the write of the FILE_RENAME
    and the actual rename.
    
    Reviewed by: Thirunarayanan Balathandayuthapani
    Tested by: Saahil Alam
    
    (cherry picked from commit bdff5b8f2455acd226d242e16e8c2107f741d3da)

--- storage/innobase/os/os0file.cc
+++ storage/innobase/os/os0file.cc
@@ -1378,10 +1378,6 @@ os_file_rename_func(
 	os_file_type_t	type;
 	bool		exists;
 
-	/* New path must not exist. */
-	ut_ad(os_file_status(newpath, &exists, &type));
-	ut_ad(!exists);
-
 	/* Old path must exist. */
 	ut_ad(os_file_status(oldpath, &exists, &type));
 	ut_ad(exists);
@@ -2300,10 +2296,6 @@ os_file_rename_func(
 #ifdef UNIV_DEBUG
 	os_file_type_t	type;
 	bool		exists;
-
-	/* New path must not exist. */
-	ut_ad(os_file_status(newpath, &exists, &type));
-	ut_ad(!exists);
 
 	/* Old path must exist. */
 	ut_ad(os_file_status(oldpath, &exists, &type));
